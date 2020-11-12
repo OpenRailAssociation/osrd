31dcdf8c5bca6d400a20f6c764a41a645f8a00b0
pguenezan <paul@guenezan.me>
2020-11-12 09:30:05 +0100

    Initial commit

---
+++ src/main/java/fr/sncf/osrd/App.java
@@ -0,0 +1,11 @@
+package fr.sncf.osrd;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/test/java/fr/sncf/osrd/AppTest.java
@@ -0,0 +1,11 @@
+package fr.sncf.osrd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
