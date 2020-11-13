e00110dcf1aa58c3ac63a770144094bcab235e7e
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-13 15:32:56 +0100

    infra: add external metadata to tracks

--- src/main/java/fr/sncf/osrd/infra/Track.java
+++ src/main/java/fr/sncf/osrd/infra/Track.java
@@ -8,4 +8,8 @@ public class Track {
         this.line = line;
         this.name = name;
     }
+
+    public final StairSequence<Double> slope = new StairSequence<>();
+    public final StairSequence<BlockSection> blockSections = new StairSequence<>();
+    public final StairSequence<Double> speedLimit = new StairSequence<>();
 }
