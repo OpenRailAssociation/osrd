d73c0920b37e9c3bc8efbe1ad8778663925a6a08
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-13 16:01:01 +0100

    infra: integrate block sections

--- src/main/java/fr/sncf/osrd/infra/BlockSection.java
+++ src/main/java/fr/sncf/osrd/infra/BlockSection.java
@@ -7,8 +7,14 @@ public class BlockSection extends AbstractEdge<SectionSignalNode> {
     public final String id;
     public final double length;
 
-    public BlockSection(String id, SectionSignalNode startNode, SectionSignalNode endNode, double length) {
+    /**
+     * The edges the topological section spans over.
+     */
+    public final StairSequence<TopoEdge> edges;
+
+    public BlockSection(String id, StairSequence<TopoEdge> edges, SectionSignalNode startNode, SectionSignalNode endNode, double length) {
         super(startNode, endNode);
+        this.edges = edges;
         this.id = id;
         this.length = length;
     }
--- src/main/java/fr/sncf/osrd/infra/Infra.java
+++ src/main/java/fr/sncf/osrd/infra/Infra.java
@@ -4,18 +4,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Infra {
+    /**
+     * The topology graph.
+     *
+     * Each TopoEdge contains a reference to a Track,
+     * which stores most of the data in SortedSequences.
+     */
     public final ArrayList<TopoNode> topoNodes = new ArrayList<>();
     public final ArrayList<TopoEdge> topoEdges = new ArrayList<>();
+
+    /**
+     * The block sections graph.
+     *
+     * A block section may span multiple topological edges, and thus be on multiple lines.
+     * Each block section has a StairSequence of the edges it spans over.
+     */
+    public final ArrayList<SectionSignalNode> sectionSignals = new ArrayList<>();
+    public final ArrayList<BlockSection> blockSections = new ArrayList<>();
+
     public final HashMap<String, Line> lines = new HashMap<>();
+
+    public void register(TopoNode node) {
+        node.setIndex(topoNodes.size());
+        topoNodes.add(node);
+    }
 
     public void register(TopoEdge edge) {
         edge.setIndex(topoEdges.size());
         topoEdges.add(edge);
     }
 
-    public void register(TopoNode node) {
-        node.setIndex(topoNodes.size());
-        topoNodes.add(node);
+    public void register(SectionSignalNode node) {
+        node.setIndex(sectionSignals.size());
+        sectionSignals.add(node);
+    }
+
+    public void register(BlockSection edge) {
+        edge.setIndex(blockSections.size());
+        blockSections.add(edge);
     }
 
     public void register(Line line) throws DataIntegrityException {
