2f3999d3c8584d3d8717333d23341f89aa2ada5e
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-16 18:18:11 +0100

    train: refactor and fix the TrainPositionTracker

--- src/main/java/fr/sncf/osrd/train/TrainPositionTracker.java
+++ src/main/java/fr/sncf/osrd/train/TrainPositionTracker.java
@@ -5,14 +5,21 @@ import fr.sncf.osrd.infra.TopoEdge;
 import java.util.ArrayDeque;
 
 public class TrainPositionTracker {
+    /** The planned path the train shall follow. */
     private final TrainPath path;
-    private final double trainLength;
 
-    private final ArrayDeque<TopoEdge> currentEdges = new ArrayDeque<>();
+    /** The index of the edge the head of the train currently is at. */
     private int currentPathIndex = 0;
 
-    // we only track the head's position, as the tail's position can be computed from the current edges and head
-    // position
+    /** The length of the train, in meters. */
+    private final double trainLength;
+
+    /** The list of edges the train currently spans over. */
+    private final ArrayDeque<TopoEdge> currentEdges = new ArrayDeque<>();
+
+    /* This code only track the position of the head, as the tail's can be computed
+     from the list of edges the train currently spans over, and the position of
+     the head on the head edge. */
     private double headEdgePosition;
 
     public TrainPositionTracker(TrainPath path, double trainLength) {
@@ -20,14 +27,60 @@ public class TrainPositionTracker {
         this.trainLength = trainLength;
     }
 
+    private TopoEdge headEdge() {
+        return currentEdges.getFirst();
+    }
+
+    private TopoEdge nextPathEdge() {
+        ++currentPathIndex;
+        return path.edges.get(currentPathIndex);
+    }
+
+    private boolean hasNextPathEdge() {
+        return currentPathIndex < path.edges.size() - 1;
+    }
+
     public void updatePosition(double speed, double deltaTime) {
         headEdgePosition += speed * deltaTime;
-        while (currentEdges.getFirst().length >= headEdgePosition) {
-            headEdgePosition -= currentEdges.getFirst().length;
-            ++currentPathIndex;
-            currentEdges.addFirst(path.edges.get(currentPathIndex));
+
+        // add edges to the current edges queue as the train moves forward
+        while (hasNextPathEdge()) {
+            var headEdgeLength = headEdge().length;
+            // stop adding edges when the head position lies inside the current head edge
+            if (headEdgeLength < headEdgePosition)
+                break;
+
+            // add the next edge on the path to the current edges queue
+            currentEdges.addFirst(nextPathEdge());
+
+            // as the head edge changed, so does the position
+            headEdgePosition -= headEdgeLength;
         }
-        while (currentEdges.stream().mapToDouble(edge -> edge.length).sum() - headEdgePosition >= trainLength) {
+
+        // remove edges off the tail as the train leaves those
+        //
+        //   removable           train
+        //                   ===========>
+        // +------------+-----------+---------+
+        //                   ^          ^
+        //                  tail        head
+        // \__________________________________/
+        //          total edges span
+        // \____________/\____________________/
+        //  tail edge len    next edges span
+        //               \_____________/\_____/
+        //          new available space     `head edge headroom
+
+        var headEdgeHeadroom = headEdge().length - headEdgePosition;
+        assert headEdgeHeadroom > 0.;
+        while (currentEdges.size() > 1) {
+            var tailEdgeLength = currentEdges.getLast().length;
+            var totalEdgesSpan = currentEdges.stream().mapToDouble(edge -> edge.length).sum();
+            var nextEdgesSpan = totalEdgesSpan - tailEdgeLength;
+            var newAvailableSpace = nextEdgesSpan - headEdgeHeadroom;
+            if (newAvailableSpace < trainLength)
+                break;
+
             currentEdges.removeLast();
         }
     }
