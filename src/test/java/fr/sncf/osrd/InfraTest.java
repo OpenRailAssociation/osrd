becc41e4303f4638f0ee00fc5b907ab55e60410b
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-16 16:09:34 +0100

    tests: rename from AppTest to InfraTest, fix API usage

--- src/test/java/fr/sncf/osrd/InfraTest.java
+++ src/test/java/fr/sncf/osrd/InfraTest.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import fr.sncf.osrd.infra.*;
 import org.junit.jupiter.api.Test;
 
-class AppTest {
+class InfraTest {
     @Test
-    public void testInfra() throws DataIntegrityException {
+    public void simpleInfraBuild() throws DataIntegrityException {
         var line = new Line("test line", "1");
-        var track = Track.createAndRegister(line, "test track", "1");
+        var track = Track.createAndRegister(line, "1", "test track");
+        var infra = new Infra();
+        infra.register(line);
+
         var nodeA = new NoOpNode("A");
         var nodeB = new NoOpNode("B");
         var nodeC = new NoOpNode("C");
-        var infra = new Infra();
         infra.register(nodeA);
         infra.register(nodeB);
         infra.register(nodeC);
