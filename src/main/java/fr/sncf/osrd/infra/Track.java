c830b3b6b4160cb8ace2913d2c46fd8948565e0d
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-16 14:23:07 +0100

    infra: make the API harder to misuse

--- src/main/java/fr/sncf/osrd/infra/Track.java
+++ src/main/java/fr/sncf/osrd/infra/Track.java
@@ -4,10 +4,21 @@ public class Track {
     public final Line line;
     public final String name;
 
-    public Track(Line line, String name) {
+    private Track(Line line, String name) {
         this.line = line;
         this.name = name;
     }
+
+    /**
+     * Creates a track and registers it with a line.
+     * @throws DataIntegrityException when another track with the same name is already registered.
+     */
+    public static Track createAndRegister(Line line, String trackName) throws DataIntegrityException {
+        var track = new Track(line, trackName);
+        line.register(track);
+        return track;
+    }
+
 
     public final StairSequence<Double> slope = new StairSequence<>();
     public final StairSequence<BlockSection> blockSections = new StairSequence<>();
--- src/test/java/fr/sncf/osrd/AppTest.java
+++ src/test/java/fr/sncf/osrd/AppTest.java
@@ -2,7 +2,24 @@ package fr.sncf.osrd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import fr.sncf.osrd.infra.*;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
+    @Test
+    public void testInfra() throws DataIntegrityException {
+        var line = new Line("test line");
+        var track = Track.createAndRegister(line, "test track");
+        var nodeA = new NoOpNode("A");
+        var nodeB = new NoOpNode("B");
+        var nodeC = new NoOpNode("C");
+        var infra = new Infra();
+        infra.register(nodeA);
+        infra.register(nodeB);
+        infra.register(nodeC);
+        infra.register(TopoEdge.link(
+                nodeA, nodeA::addEdge,
+                nodeB, nodeB::addEdge,
+                track, "e1", 42.0));
+    }
 }
