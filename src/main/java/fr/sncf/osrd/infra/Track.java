f291b30b975ddcebab0c0ae62fef03e6ac033d28
pguenezan <paul@guenezan.me>
2020-11-16 14:31:23 +0100

    infra: add id to track and line

--- src/main/java/fr/sncf/osrd/infra/Track.java
+++ src/main/java/fr/sncf/osrd/infra/Track.java
@@ -2,10 +2,12 @@ package fr.sncf.osrd.infra;
 
 public class Track {
     public final Line line;
+    public final String id;
     public final String name;
 
-    private Track(Line line, String name) {
+    private Track(Line line, String id, String name) {
         this.line = line;
+        this.id = id;
         this.name = name;
     }
 
@@ -13,8 +15,8 @@ public class Track {
      * Creates a track and registers it with a line.
      * @throws DataIntegrityException when another track with the same name is already registered.
      */
-    public static Track createAndRegister(Line line, String trackName) throws DataIntegrityException {
-        var track = new Track(line, trackName);
+    public static Track createAndRegister(Line line, String id, String trackName) throws DataIntegrityException {
+        var track = new Track(line, id, trackName);
         line.register(track);
         return track;
     }
--- src/test/java/fr/sncf/osrd/AppTest.java
+++ src/test/java/fr/sncf/osrd/AppTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test;
 class AppTest {
     @Test
     public void testInfra() throws DataIntegrityException {
-        var line = new Line("test line");
-        var track = Track.createAndRegister(line, "test track");
+        var line = new Line("test line", "1");
+        var track = Track.createAndRegister(line, "test track", "1");
         var nodeA = new NoOpNode("A");
         var nodeB = new NoOpNode("B");
         var nodeC = new NoOpNode("C");
