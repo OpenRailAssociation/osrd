bc084e8e9cdea716ef685e8f9707c623969481b8
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-18 15:09:57 +0100

    util: make {Point,Stair}Sequences slicable

--- src/main/java/fr/sncf/osrd/util/PointSequence.java
+++ src/main/java/fr/sncf/osrd/util/PointSequence.java
@@ -5,6 +5,23 @@ package fr.sncf.osrd.util;
  * @param <E> The type of the point objects
  */
 public final class PointSequence<E> extends SortedSequence<E> {
+    /** A slice of a PointSequence. */
+    public final class Slice {
+        public final PointSequence<E> parentSequence;
+
+        /** Included start bound. */
+        public final int start;
+
+        /** Excluded end bound. */
+        public final int end;
+
+        public Slice(PointSequence<E> parentSequence, int start, int end) {
+            this.parentSequence = parentSequence;
+            this.start = start;
+            this.end = end;
+        }
+    }
+
     public final class Cursor {
         private int currentIndex = 0;
         private PointSequence<E> seq;
@@ -26,6 +43,26 @@ public final class PointSequence<E> extends SortedSequence<E> {
             currentIndex++;
             return res;
         }
+    }
+
+    /**
+     * Gets a slice of a sequence.
+     * @param startPosition the included start slice bound
+     * @param endPosition the included end slice bound
+     * @return a PointSequence slice
+     */
+    public Slice slice(double startPosition, double endPosition) {
+        int start = 0;
+        for (; start < data.size(); start++)
+            if (data.get(start).position >= startPosition)
+                break;
+
+        int end = start;
+        for (; end < data.size(); end++)
+            if (data.get(end).position > endPosition)
+                break;
+
+        return new Slice(this, start, end);
     }
 
     public Cursor cursor() {
