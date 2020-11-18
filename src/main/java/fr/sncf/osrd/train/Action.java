66770048dd23dda1eaa2282c7b958a02c5dc542f
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-18 15:14:56 +0100

    train: Action: remove ActionType, force in newtons

--- src/main/java/fr/sncf/osrd/train/Action.java
+++ src/main/java/fr/sncf/osrd/train/Action.java
@@ -1,40 +1,78 @@
 package fr.sncf.osrd.train;
 
 public class Action implements Comparable<Action> {
-    final ActionType type;
+    /**
+     * Encodes the force the driver decided to apply, in newton.
+     * It can be nan in case the action does not involve any force.
+     */
     final double force;
 
     /**
-     * Create a new force-less action.
-     * @param type the type of the action
+     * Whether the action is an emergency event.
      */
-    public Action(ActionType type) {
-        assert type != ActionType.ACCELERATE && type != ActionType.BRAKE;
-        this.type = type;
-        this.force = Double.NaN;
+    final boolean emergencyBrake;
+
+    /**
+     * Whether to delete the speed controller.
+     */
+    final boolean deleteController;
+
+    public static Action accelerate(double force, boolean deleteController) {
+        return new Action(force, false, deleteController);
     }
 
+    public static Action brake(double force, boolean deleteController) {
+        return new Action(force, false, deleteController);
+    }
+
+    public static Action emergencyBrake(boolean deleteController) {
+        return new Action(true, deleteController);
+    }
+
+    /**
+     * Create a new force-less action.
+     * @param emergencyBrake whether the action is an emergency event
+     * @param deleteController whether to delete the controller
+     */
+    private Action(boolean emergencyBrake, boolean deleteController) {
+        this.force = Double.NaN;
+        this.emergencyBrake = emergencyBrake;
+        this.deleteController = deleteController;
+    }
+
+    boolean hasForce() {
+        return !Double.isNaN(force);
+    }
 
     /**
      * Create a new force action.
-     * @param type the type of the action
      * @param force the force to apply
+     * @param emergencyBrake whether the action is an emergency event
+     * @param deleteController whether to delete the controller
      */
-    public Action(ActionType type, double force) {
-        assert type == ActionType.ACCELERATE || type == ActionType.BRAKE;
-        assert force > 0. || type != ActionType.ACCELERATE;
-        assert force < 0. || type != ActionType.BRAKE;
-        this.type = type;
+    private Action(double force, boolean emergencyBrake, boolean deleteController) {
         this.force = force;
+        this.emergencyBrake = emergencyBrake;
+        this.deleteController = deleteController;
     }
 
     @Override
     public int compareTo(Action other) {
-        int compare = Integer.compare(this.type.order, other.type.order);
-        if (compare != 0)
-            return compare;
-        assert type == ActionType.ACCELERATE || type == ActionType.BRAKE;
-        assert !Double.isNaN(this.force) && Double.isNaN(other.force);
+        // compare the level of emergency first.
+        int cmp = Boolean.compare(this.emergencyBrake, other.emergencyBrake);
+        if (cmp != 0)
+            return cmp;
+
+        // if both have the same level of emergency, compare whether they have a force
+        cmp = Boolean.compare(this.hasForce(), other.hasForce());
+        if (cmp != 0)
+            return cmp;
+
+        // if neither actions have a force, they are equal (Nan isn't even equal to itself)
+        if (Double.isNaN(force))
+            return 0;
+
+        // if both have a force and have same same level of emergency, compare by force
         return Double.compare(this.force, other.force);
     }
 }
