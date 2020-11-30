4fc6d12fa0fc7985f02d76b84c4de8c1fb88c4a7
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-30 16:54:17 +0100

    util: fix hash and equal for Pair

--- src/main/java/fr/sncf/osrd/util/Pair.java
+++ src/main/java/fr/sncf/osrd/util/Pair.java
@@ -1,12 +1,28 @@
 package fr.sncf.osrd.util;
 
-public class Pair<U, V>  {
+import java.util.Objects;
+
+public final class Pair<U, V>  {
     public final U first;
     public final V second;
 
     public Pair(U first, V second) {
         this.first = first;
         this.second = second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        var o = (Pair<U, V>)obj;
+        return first.equals(o.first) && second.equals(o.second);
     }
 }
 
