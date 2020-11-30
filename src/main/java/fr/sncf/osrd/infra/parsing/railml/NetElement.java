645ab1fb5bb181143c3c1e226433d37116a99eb4
pguenezan <paul@guenezan.me>
2020-11-30 22:35:00 +0100

    infra: add lrs parsing to railml

---
+++ src/main/java/fr/sncf/osrd/infra/parsing/railml/NetElement.java
@@ -0,0 +1,40 @@
+package fr.sncf.osrd.infra.parsing.railml;
+
+import fr.sncf.osrd.infra.topological.TopoEdge;
+import fr.sncf.osrd.util.FloatCompare;
+import fr.sncf.osrd.util.MutPair;
+import org.dom4j.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class NetElement {
+    final TopoEdge topoEdge;
+    final Map<String, Double> lrsDeltas = new HashMap<>();
+
+    NetElement(TopoEdge topoEdge, Node netElement) {
+        this.topoEdge = topoEdge;
+
+        var lrsMap = new HashMap<String, MutPair<Double, Double>>();
+
+        for (var intrinsicCoordinate: netElement.selectNodes("associatedPositioningSystem/intrinsicCoordinate")) {
+            var intrinsicCoord = Double.valueOf(intrinsicCoordinate.valueOf("@intrinsicCoord"));
+            assert FloatCompare.eq(intrinsicCoord, 0) || FloatCompare.eq(intrinsicCoord, 1);
+            var positioningSystemRef = intrinsicCoordinate.valueOf("linearCoordinate/@positioningSystemRef");
+            if (positioningSystemRef.isEmpty())
+                continue;
+            var measure = Double.valueOf(intrinsicCoordinate.valueOf("linearCoordinate/@measure"));
+            lrsMap.putIfAbsent(positioningSystemRef, new MutPair<>(Double.NaN, Double.NaN));
+            if (FloatCompare.eq(intrinsicCoord, 0))
+                lrsMap.get(positioningSystemRef).first = measure;
+            else
+                lrsMap.get(positioningSystemRef).second = measure;
+        }
+
+        for (var lrsName : lrsMap.keySet()) {
+            var range = lrsMap.get(lrsName);
+            assert FloatCompare.eq(range.second - range.first, topoEdge.length);
+            lrsDeltas.put(lrsName, range.first);
+        }
+    }
+}
