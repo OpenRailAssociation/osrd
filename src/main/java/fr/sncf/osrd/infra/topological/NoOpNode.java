a10c4f99afffe361998c9a3f339ae9f415572946
Victor "multun" Collod <victor.collod@epita.fr>
2020-11-30 10:58:56 +0100

    infra: NoOpNode: remove duplicate neighbors list

--- src/main/java/fr/sncf/osrd/infra/topological/NoOpNode.java
+++ src/main/java/fr/sncf/osrd/infra/topological/NoOpNode.java
@@ -1,22 +1,15 @@
 package fr.sncf.osrd.infra.topological;
 
-import fr.sncf.osrd.util.CryoList;
-import java.util.List;
-
+/**
+ * A placeholder node type, without any special purpose.
+ * Its list of neighbors is held by {@link fr.sncf.osrd.infra.graph.Graph}.
+ */
 public class NoOpNode extends TopoNode {
-    private final CryoList<TopoEdge> neighbors = new CryoList<>();
-
     public NoOpNode(String id) {
         super(id);
     }
 
-    public NoOpNode addEdge(TopoEdge edge) {
-        neighbors.add(edge);
-        return this;
-    }
-
     @Override
     public void freeze() {
-        this.neighbors.freeze();
     }
 }
