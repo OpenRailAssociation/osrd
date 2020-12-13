9268d52491530e8673db6e34d98e8e6c48d4904c
Victor "multun" Collod <victor.collod@epita.fr>
2020-12-13 20:18:02 +0100

    simulation: isolate core simulation components

--- src/main/java/fr/sncf/osrd/simulation/Event.java
+++ src/main/java/fr/sncf/osrd/simulation/Event.java
@@ -1,24 +1,28 @@
 package fr.sncf.osrd.simulation;
 
+import fr.sncf.osrd.simulation.core.AbstractEvent;
+import fr.sncf.osrd.simulation.core.Simulation;
+import fr.sncf.osrd.simulation.core.SimulationError;
+
 public final class Event<T extends BaseT, BaseT> extends AbstractEvent<T, BaseT> {
     final EventSource<T, BaseT> source;
 
-    Event(double scheduledTime, long revision, T value, EventSource<T, BaseT> source) {
-        super(scheduledTime, revision, value);
+    Event(Simulation<BaseT> sim, double scheduledTime, T value, EventSource<T, BaseT> source) throws SimulationError {
+        super(sim, scheduledTime, value);
         this.source = source;
     }
 
     @Override
-    void updateState(Simulation<BaseT> sim, EventState newState) throws SimulationError {
+    public void updateState(Simulation<BaseT> sim, EventState newState) throws SimulationError {
         assert this.state.hasTransitionTo(newState);
         this.state = newState;
 
         switch (newState) {
+            case UNREGISTERED:
+                throw new SimulationError("updated an event's state to UNINITIALIZED");
             // event sinks don't need to be notified when the event is scheduled
             case SCHEDULED:
                 return;
-            case UNINITIALIZED:
-                throw new SimulationError("updated an event's state to UNINITIALIZED");
 
             // for now, notify subscribers for these two states
             case HAPPENED:
--- src/main/java/fr/sncf/osrd/simulation/EventSource.java
+++ src/main/java/fr/sncf/osrd/simulation/EventSource.java
@@ -1,5 +1,8 @@
 package fr.sncf.osrd.simulation;
 
+import fr.sncf.osrd.simulation.core.Simulation;
+import fr.sncf.osrd.simulation.core.SimulationError;
+
 import java.util.ArrayList;
 
 public class EventSource<T extends BaseT, BaseT> {
@@ -8,5 +11,22 @@ public class EventSource<T extends BaseT, BaseT> {
     public void subscribe(EventSink<T, BaseT> sink) {
         assert !subscribers.contains(sink);
         subscribers.add(sink);
+    }
+
+    public void unsubscribe(EventSink<T, BaseT> sink) {
+        assert subscribers.contains(sink);
+        subscribers.remove(sink);
+    }
+
+    /**
+     * Creates a new event.
+     * @param sim the simulation this events belongs to
+     * @param scheduledTime the simulation time this event should happen at
+     * @param value the value associated with the event
+     * @return a new event
+     * @throws SimulationError if a logic error occurs
+     */
+    public Event<T, BaseT> event(Simulation<BaseT> sim, double scheduledTime, T value) throws SimulationError {
+        return new Event<>(sim, scheduledTime, value, this);
     }
 }
