6a665584f0e30e0fbd73c63237a0559beac4f299
Victor "multun" Collod <victor.collod@epita.fr>
2020-12-13 16:54:53 +0100

    simulation: remove the HAPPENING state, check transitions

--- src/main/java/fr/sncf/osrd/simulation/Event.java
+++ src/main/java/fr/sncf/osrd/simulation/Event.java
@@ -3,17 +3,31 @@ package fr.sncf.osrd.simulation;
 public final class Event<T extends BaseT, BaseT> extends AbstractEvent<T, BaseT> {
     final EventSource<T, BaseT> source;
 
-
     Event(double scheduledTime, long revision, T value, EventSource<T, BaseT> source) {
         super(scheduledTime, revision, value);
         this.source = source;
     }
 
     @Override
-    public void updateState(Simulation<BaseT> sim, EventState state) throws SimulationError {
-        this.state = state;
+    void updateState(Simulation<BaseT> sim, EventState newState) throws SimulationError {
+        assert this.state.hasTransitionTo(newState);
+        this.state = newState;
+
+        switch (newState) {
+            // event sinks don't need to be notified when the event is scheduled
+            case SCHEDULED:
+                return;
+            case UNINITIALIZED:
+                throw new SimulationError("updated an event's state to UNINITIALIZED");
+
+            // for now, notify subscribers for these two states
+            case HAPPENED:
+            case CANCELLED:
+                break;
+        }
+
         for (var sink : source.subscribers) {
-            sink.feed(sim, this, state);
+            sink.feed(sim, this, newState);
         }
     }
 }
