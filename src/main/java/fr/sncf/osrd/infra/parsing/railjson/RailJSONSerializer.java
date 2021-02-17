5e99fb5897bcf4c7388a1484abe57a6828040805
Victor "multun" Collod <victor.collod@epita.fr>
2021-02-17 15:49:14 +0100

    add a convert command

---
+++ src/main/java/fr/sncf/osrd/infra/parsing/railjson/RailJSONSerializer.java
@@ -0,0 +1,30 @@
+package fr.sncf.osrd.infra.parsing.railjson;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import fr.sncf.osrd.infra.parsing.railjson.schema.RJSRoot;
+import okio.Okio;
+import okio.Sink;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class RailJSONSerializer {
+    /**
+     * Serializes to JSON the in-memory RailJSON representation
+     * @param rjsRoot the RailJSON root node
+     * @param outputPath the output file path
+     * @throws IOException {@inheritDoc}
+     */
+    @SuppressFBWarnings(
+            value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
+            justification = "that's a spotbugs bug :)"
+    )
+    public static void serialize(RJSRoot rjsRoot, Path outputPath) throws IOException {
+        try (
+                Sink fileSink = Okio.sink(outputPath);
+                var bufferedSink = Okio.buffer(fileSink)
+        ) {
+            RJSRoot.adapter.toJson(bufferedSink, rjsRoot);
+        }
+    }
+}
