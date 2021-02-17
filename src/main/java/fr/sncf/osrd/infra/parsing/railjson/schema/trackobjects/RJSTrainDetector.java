88d19745ea7ce278325f88cbd36d8dba82ab956b
Florian Amsallem <florian.amsallem@epita.fr>
2021-02-17 16:33:48 +0100

    railml: parse TDE

--- src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSTrainDetector.java
+++ src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSTrainDetector.java
@@ -10,15 +10,7 @@ import fr.sncf.osrd.infra.parsing.railjson.schema.RJSTVDSection;
 public class RJSTrainDetector extends DirectionalRJSTrackObject {
     public final String id;
 
-    /** The tvd in the forward direction on the edge */
-    @Json(name = "forward_TVD")
-    ID<RJSTVDSection> forwardTVD;
-
-    /** The tvd in the backward direction on the edge */
-    @Json(name = "backward_TVD")
-    ID<RJSTVDSection> backwardTVD;
-
-    RJSTrainDetector(String id, ApplicableDirections applicableDirections, double position) {
+    public RJSTrainDetector(String id, ApplicableDirections applicableDirections, double position) {
         super(applicableDirections, position);
         this.id = id;
     }
