4053bf6147ac5320b8e4560f4ad15528d5ac676d
Florian Amsallem <florian.amsallem@epita.fr>
2021-02-18 09:47:21 +0100

    railml: parse TVD

--- src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSBufferStop.java
+++ src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSBufferStop.java
@@ -2,13 +2,18 @@ package fr.sncf.osrd.infra.parsing.railjson.schema.trackobjects;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.infra.graph.ApplicableDirections;
+import fr.sncf.osrd.infra.parsing.railjson.schema.Identified;
 
-@SuppressFBWarnings({"URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"})
-public class RJSBufferStop extends DirectionalRJSTrackObject {
+public class RJSBufferStop extends DirectionalRJSTrackObject implements Identified {
     public final String id;
 
     public RJSBufferStop(String id, ApplicableDirections applicableDirections, double position) {
         super(applicableDirections, position);
         this.id = id;
+    }
+
+    @Override
+    public String getID() {
+        return id;
     }
 }
--- src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSTrainDetector.java
+++ src/main/java/fr/sncf/osrd/infra/parsing/railjson/schema/trackobjects/RJSTrainDetector.java
@@ -4,14 +4,19 @@ import com.squareup.moshi.Json;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.infra.graph.ApplicableDirections;
 import fr.sncf.osrd.infra.parsing.railjson.schema.ID;
+import fr.sncf.osrd.infra.parsing.railjson.schema.Identified;
 import fr.sncf.osrd.infra.parsing.railjson.schema.RJSTVDSection;
 
-@SuppressFBWarnings({"URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"})
-public class RJSTrainDetector extends DirectionalRJSTrackObject {
+public class RJSTrainDetector extends DirectionalRJSTrackObject implements Identified {
     public final String id;
 
     public RJSTrainDetector(String id, ApplicableDirections applicableDirections, double position) {
         super(applicableDirections, position);
         this.id = id;
+    }
+
+    @Override
+    public String getID() {
+        return id;
     }
 }
