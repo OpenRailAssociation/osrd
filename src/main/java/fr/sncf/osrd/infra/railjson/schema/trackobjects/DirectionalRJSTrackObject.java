bb1f7f5c0140e7e2bae7d0e52743f90c04beaebb
Victor "multun" Collod <victor.collod@epita.fr>
2021-03-02 10:22:37 +0100

    railjson: rename applicable{D,_d}irections

--- src/main/java/fr/sncf/osrd/infra/railjson/schema/trackobjects/DirectionalRJSTrackObject.java
+++ src/main/java/fr/sncf/osrd/infra/railjson/schema/trackobjects/DirectionalRJSTrackObject.java
@@ -1,8 +1,10 @@
 package fr.sncf.osrd.infra.railjson.schema.trackobjects;
 
+import com.squareup.moshi.Json;
 import fr.sncf.osrd.utils.graph.ApplicableDirections;
 
 public class DirectionalRJSTrackObject extends RJSTrackObject {
+    @Json(name = "application_directions")
     public final ApplicableDirections applicableDirections;
 
     DirectionalRJSTrackObject(ApplicableDirections applicableDirections, double position) {
