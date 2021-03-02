0054eecb0ff2c920a695217ba96291dcbf0a309b
Florian Amsallem <florian.amsallem@epita.fr>
2021-03-02 20:08:56 +0100

    infra: RouteGraph fix routes link

--- src/main/java/fr/sncf/osrd/infra/routegraph/Route.java
+++ src/main/java/fr/sncf/osrd/infra/routegraph/Route.java
@@ -3,6 +3,7 @@ package fr.sncf.osrd.infra.routegraph;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.infra.waypointgraph.TVDSectionPath;
 import fr.sncf.osrd.utils.graph.BiNEdge;
+import fr.sncf.osrd.utils.graph.EdgeDirection;
 
 import java.util.List;
 
@@ -10,17 +11,26 @@ public class Route extends BiNEdge<Route> {
     @SuppressFBWarnings({"URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"})
     public final String id;
     /** List of tvdSectionPath forming the route */
-    public final List<TVDSectionPath> tvdSectionPaths;
+    public final List<TVDSectionPath> tvdSectionsPath;
+    @SuppressFBWarnings({"URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"})
+    public final List<EdgeDirection> tvdSectionsPathDirection;
 
-    protected Route(String id, RouteGraph graph, double length, List<TVDSectionPath> tvdSectionPaths) {
+    protected Route(
+            String id,
+            RouteGraph graph,
+            double length,
+            List<TVDSectionPath> tvdSectionsPath,
+            List<EdgeDirection> tvdSectionsPathDirection
+    ) {
         super(
                 graph.nextEdgeIndex(),
-                tvdSectionPaths.get(0).startNode,
-                tvdSectionPaths.get(tvdSectionPaths.size() - 1).startNode,
+                tvdSectionsPath.get(0).startNode,
+                tvdSectionsPath.get(tvdSectionsPath.size() - 1).startNode,
                 length
         );
+        this.tvdSectionsPathDirection = tvdSectionsPathDirection;
         graph.registerEdge(this);
         this.id = id;
-        this.tvdSectionPaths = tvdSectionPaths;
+        this.tvdSectionsPath = tvdSectionsPath;
     }
 }
--- src/main/java/fr/sncf/osrd/infra/routegraph/RouteGraph.java
+++ src/main/java/fr/sncf/osrd/infra/routegraph/RouteGraph.java
@@ -6,11 +6,11 @@ import fr.sncf.osrd.infra.trackgraph.Waypoint;
 import fr.sncf.osrd.infra.waypointgraph.TVDSectionPath;
 import fr.sncf.osrd.utils.SortedArraySet;
 import fr.sncf.osrd.utils.graph.BiNGraph;
+import fr.sncf.osrd.utils.graph.EdgeDirection;
 import fr.sncf.osrd.utils.graph.EdgeEndpoint;
 import fr.sncf.osrd.infra.waypointgraph.WaypointGraph;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class RouteGraph extends BiNGraph<Route, Waypoint> {
@@ -18,8 +18,8 @@ public class RouteGraph extends BiNGraph<Route, Waypoint> {
     @Override
     public List<Route> getNeighborRels(Route route, EdgeEndpoint endpoint) {
         if (endpoint == EdgeEndpoint.BEGIN)
-            return getNode(route.tvdSectionPaths.get(0).startNode).stopToStartRoutes;
-        return getNode(route.tvdSectionPaths.get(route.tvdSectionPaths.size() - 1).endNode).startToStopRoutes;
+            return getNode(route.tvdSectionsPath.get(0).startNode).stopToStartRoutes;
+        return getNode(route.tvdSectionsPath.get(route.tvdSectionsPath.size() - 1).endNode).startToStopRoutes;
     }
 
     public static class Builder {
@@ -45,30 +45,42 @@ public class RouteGraph extends BiNGraph<Route, Waypoint> {
             }
             var length = 0;
             var tvdSectionsPath = new ArrayList<TVDSectionPath>();
+            var tvdSectionsPathDirection = new ArrayList<EdgeDirection>();
 
             // Find the list of tvd section path
             for (var i = 1; i < waypoints.size(); i++) {
                 var startIndex = waypoints.get(i - 1).index;
                 var endIndex = waypoints.get(i).index;
                 var tvdSectionPath = waypointGraph.getTVDSectionPath(startIndex, endIndex);
+
                 if (tvdSectionPath == null)
                     throw new InvalidInfraException(String.format(
                             "Route: '%s' couldn't find tvd section path: (%d, %d)", id, startIndex, endIndex));
                 if (tvdSections.intersect(tvdSectionPath.tvdSections).isEmpty())
                     throw new InvalidInfraException(
                             String.format("Route '%s' has a tvd section path outside tvd section", id));
+
+                if (tvdSectionPath.startNode == startIndex)
+                    tvdSectionsPathDirection.add(EdgeDirection.START_TO_STOP);
+                else
+                    tvdSectionsPathDirection.add(EdgeDirection.STOP_TO_START);
+
                 tvdSectionsPath.add(tvdSectionPath);
                 length += tvdSectionPath.length;
             }
 
             // Create route
-            var route = new Route(id, routeGraph, length, tvdSectionsPath);
+            var route = new Route(id, routeGraph, length, tvdSectionsPath, tvdSectionsPathDirection);
 
-            // Add route to the waypoints start and end nodes
+            // Link route to the starting waypoint
             var startWaypoint = waypointGraph.getNode(tvdSectionsPath.get(0).startNode);
-            var endWaypoint = waypointGraph.getNode(tvdSectionsPath.get(tvdSectionsPath.size() - 1).endNode);
-            startWaypoint.startToStopRoutes.add(route);
-            endWaypoint.stopToStartRoutes.add(route);
+            var firstTVDSectionPath = tvdSectionsPath.get(0);
+            var waypointDirection = firstTVDSectionPath.startNodeDirection;
+            if (tvdSectionsPathDirection.get(0) == EdgeDirection.STOP_TO_START)
+                waypointDirection = firstTVDSectionPath.endNodeDirection;
+
+            startWaypoint.getRouteNeighbors(waypointDirection).add(route);
+
             return route;
         }
 
