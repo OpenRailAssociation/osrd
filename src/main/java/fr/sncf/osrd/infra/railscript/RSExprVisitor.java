44239dd865c0894c1ba84838d709e964df0767c6
Florian Amsallem <florian.amsallem@epita.fr>
2021-03-04 11:23:42 +0100

    railscript: fix visitor

--- src/main/java/fr/sncf/osrd/infra/railscript/RSExprVisitor.java
+++ src/main/java/fr/sncf/osrd/infra/railscript/RSExprVisitor.java
@@ -3,71 +3,69 @@ package fr.sncf.osrd.infra.railscript;
 import fr.sncf.osrd.infra.InvalidInfraException;
 
 public class RSExprVisitor {
-    public void visit(RSExpr<?> expr) {
+
+    public void visit(RSExpr.OrExpr expr) throws InvalidInfraException {
+        for (var e : expr.expressions)
+            e.accept(this);
     }
 
-    public void visit(RSExpr.OrExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.AndExpr expr) throws InvalidInfraException {
+        for (var e : expr.expressions)
+            e.accept(this);
     }
 
-    public void visit(RSExpr.AndExpr expr) {
-        visit((RSExpr<?>) expr);
-    }
-
-    public void visit(RSExpr.NotExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.NotExpr expr) throws InvalidInfraException {
+        expr.expr.accept(this);
     }
 
     public void visit(RSExpr.TrueExpr expr) {
-        visit((RSExpr<?>) expr);
     }
 
     public void visit(RSExpr.FalseExpr expr) {
-        visit((RSExpr<?>) expr);
     }
 
-    public void visit(RSExpr.AspectSetExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.AspectSetExpr expr) throws InvalidInfraException {
+        for (var condition : expr.conditions)
+            condition.accept(this);
     }
 
     public void visit(RSExpr.SignalRefExpr expr) throws InvalidInfraException {
-        visit((RSExpr<?>) expr);
     }
 
     public void visit(RSExpr.RouteRefExpr expr) throws InvalidInfraException {
-        visit((RSExpr<?>) expr);
     }
 
-    // TODO
-    // public void visit(Expr.SwitchExpr expr) {
-    //     visit((Expr<?>) expr);
-    // }
-
-    public void visit(RSExpr.IfExpr<?> expr) {
-        visit((RSExpr<?>) expr);
+    /** Visit method */
+    public void visit(RSExpr.IfExpr<?> expr) throws InvalidInfraException {
+        expr.ifExpr.accept(this);
+        expr.thenExpr.accept(this);
+        expr.elseExpr.accept(this);
     }
 
-    public void visit(RSExpr.CallExpr<?> expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.CallExpr<?> expr) throws InvalidInfraException {
+        for (var arg : expr.arguments)
+            arg.accept(this);
     }
 
-    public void visit(RSExpr.EnumMatchExpr<?, ?> expr) {
-        visit((RSExpr<?>) expr);
+    /** Visit method */
+    public void visit(RSExpr.EnumMatchExpr<?, ?> expr) throws InvalidInfraException {
+        expr.expr.accept(this);
+        for (var branch : expr.branches)
+            branch.accept(this);
     }
 
     public void visit(RSExpr.ArgumentRefExpr<?> expr) {
-        visit((RSExpr<?>) expr);
     }
 
-    public void visit(RSExpr.SignalAspectCheckExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.SignalAspectCheckExpr expr) throws InvalidInfraException {
+        expr.signalExpr.accept(this);
     }
 
-    public void visit(RSExpr.RouteStateCheckExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.RouteStateCheckExpr expr) throws InvalidInfraException {
+        expr.routeExpr.accept(this);
     }
 
-    public void visit(RSExpr.AspectSetContainsExpr expr) {
-        visit((RSExpr<?>) expr);
+    public void visit(RSExpr.AspectSetContainsExpr expr) throws InvalidInfraException {
+        expr.expr.accept(this);
     }
 }
