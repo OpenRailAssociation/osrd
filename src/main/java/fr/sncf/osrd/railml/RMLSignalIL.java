f55a02441755476b93286ed72caa94f292703b27
Florian Amsallem <florian.amsallem@epita.fr>
2021-03-05 20:10:04 +0100

    railml: add and parse signalIL and signalIS

---
+++ src/main/java/fr/sncf/osrd/railml/RMLSignalIL.java
@@ -0,0 +1,41 @@
+package fr.sncf.osrd.railml;
+
+import fr.sncf.osrd.infra.InvalidInfraException;
+import fr.sncf.osrd.infra.railjson.schema.ID;
+import fr.sncf.osrd.infra.railjson.schema.RJSTrackSection;
+import fr.sncf.osrd.infra.railjson.schema.railscript.RJSRSExpr;
+import fr.sncf.osrd.infra.railjson.schema.signaling.RJSAspect;
+import fr.sncf.osrd.infra.railjson.schema.trackobjects.RJSSignal;
+import fr.sncf.osrd.railml.tracksectiongraph.NetElement;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RMLSignalIL {
+    static void parse(
+            Map<String, NetElement> netElements,
+            Document document,
+            HashMap<String, RJSTrackSection> rjsTrackSections,
+            HashMap<String, RMLSignalIS> rmlSignalsIS
+    ) throws InvalidInfraException {
+        var xpath = "/railML/interlocking/assetsForIL/signalsIL/signalIL";
+        for (var signalNode : document.selectNodes(xpath)) {
+            var signal = (Element) signalNode;
+            // locate the track netElement the signal is on
+            var id = signal.attributeValue("id");
+
+            var refSignalIS = signal.element("refersTo").attributeValue("ref");
+
+            // TODO: parse signal functions and create AST expr
+            var greenAspectSetMember = new RJSRSExpr.AspectSet.AspectSetMember(ID.from(new RJSAspect("GREEN")), null);
+            var expr = new RJSRSExpr.AspectSet(new RJSRSExpr.AspectSet.AspectSetMember[]{greenAspectSetMember});
+
+            // add the signal to the RJSTrackSection
+            var rmlSignalIS = rmlSignalsIS.get(refSignalIS);
+            var rjsTrackSection = rmlSignalIS.rjsTrackSection;
+            rjsTrackSection.signals.add(new RJSSignal(id, rmlSignalIS.navigability, rmlSignalIS.position, expr));
+        }
+    }
+}
---
+++ src/main/java/fr/sncf/osrd/railml/RMLSignalIS.java
@@ -0,0 +1,50 @@
+package fr.sncf.osrd.railml;
+
+import fr.sncf.osrd.infra.InvalidInfraException;
+import fr.sncf.osrd.infra.railjson.schema.ID;
+import fr.sncf.osrd.infra.railjson.schema.RJSTrackSection;
+import fr.sncf.osrd.infra.railjson.schema.railscript.RJSRSExpr;
+import fr.sncf.osrd.infra.railjson.schema.signaling.RJSAspect;
+import fr.sncf.osrd.infra.railjson.schema.trackobjects.RJSSignal;
+import fr.sncf.osrd.railml.tracksectiongraph.NetElement;
+import fr.sncf.osrd.utils.graph.ApplicableDirections;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RMLSignalIS {
+    final RJSTrackSection rjsTrackSection;
+    final double position;
+    final ApplicableDirections navigability;
+
+    private RMLSignalIS(RJSTrackSection rjsTrackSection, ApplicableDirections navigability, double position) {
+        this.rjsTrackSection = rjsTrackSection;
+        this.navigability = navigability;
+        this.position = position;
+    }
+
+    static HashMap<String, RMLSignalIS> parse(
+            Map<String, NetElement> netElements,
+            Document document,
+            HashMap<String, RJSTrackSection> rjsTrackSections
+    ) throws InvalidInfraException {
+        var signals = new HashMap<String, RMLSignalIS>();
+        var xpath = "/railML/infrastructure/functionalInfrastructure/signalsIS/signalIS";
+        for (var signalNode : document.selectNodes(xpath)) {
+            var signal = (Element) signalNode;
+            // locate the track netElement the signal is on
+            var id = signal.attributeValue("id");
+
+            // parse the location, which should only reference a single element
+            var location = SpotLocation.parseSingle(netElements, signal);
+            if (location == null)
+                throw new InvalidInfraException(String.format("missing spotLocation on signal %s", id));
+
+            var rjsTrackSection = rjsTrackSections.get(location.netElement.id);
+            signals.put(id, new RMLSignalIS(rjsTrackSection, location.appliesTo, location.position));
+        }
+        return signals;
+    }
+}
