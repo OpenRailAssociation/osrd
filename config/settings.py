86d178eede30578def27f42568472443caca2a73
Paul Guénézan <paul.guenezan@epita.fr>
2021-03-15 15:10:46 +0100

    infra: add basic models

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # osrd apps
+    'infra.apps.InfraConfig',
     'osrd.apps.MainServiceAppConfig',
 ]
 
---
+++ infra/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InfraConfig(AppConfig):
+    name = 'infra'
