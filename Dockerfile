7836f4ef7b7a68d0e324e07fba64be172937df7f
Paul Guénézan <paul.guenezan@epita.fr>
2021-03-22 17:03:47 +0100

    api: add basic api

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:6.6.1-jdk11 AS build
+
+WORKDIR /home/gradle/src
+COPY --chown=gradle:gradle . .
+RUN gradle jar --no-daemon
+
+FROM amazoncorretto:11
+
+COPY --from=build /home/gradle/src/build/libs/*.jar /app/osrd_core.jar
+CMD ["java", "-jar", "/app/osrd_core.jar", "api"]
---
+++ src/main/java/fr/sncf/osrd/api/ApiServerCommand.java
@@ -0,0 +1,34 @@
+package fr.sncf.osrd.api;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import org.takes.facets.fallback.FbChain;
+import org.takes.facets.fallback.FbStatus;
+import org.takes.facets.fallback.TkFallback;
+import org.takes.facets.fork.FkRegex;
+import org.takes.facets.fork.TkFork;
+import org.takes.http.Exit;
+import org.takes.http.FtBasic;
+import org.takes.rs.RsText;
+import org.takes.rs.RsWithStatus;
+import org.takes.tk.TkSlf4j;
+
+import java.io.IOException;
+
+@Parameters(commandDescription = "HTTP API server mode")
+public final class ApiServerCommand {
+    @Parameter(
+            names = { "-p", "--port" },
+            description = "The TCP port to listen on"
+    )
+    private int port = 8000;
+
+    public void run() throws IOException {
+        var routes = new TkFork(
+            new FkRegex("/health", "")
+        );
+        var server_config = new TkSlf4j(new TkFallback(routes, new FbChain(new FbStatus(404, new RsWithStatus(404)))));
+        var server = new FtBasic(server_config, port);
+        server.start(Exit.NEVER);
+    }
+}
