d9f1ab97d5962183786bdf80c64008d487fff249
Florian Amsallem <florian.amsallem@epita.fr>
2021-04-02 11:54:40 +0200

    api: add simulation endpoint output

--- src/main/java/fr/sncf/osrd/infra/railscript/value/RSAspectSet.java
+++ src/main/java/fr/sncf/osrd/infra/railscript/value/RSAspectSet.java
@@ -2,7 +2,6 @@ package fr.sncf.osrd.infra.railscript.value;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.infra.signaling.Aspect;
-import fr.sncf.osrd.utils.DeepComparable;
 import fr.sncf.osrd.utils.SortedArraySet;
 
 public final class RSAspectSet extends SortedArraySet<Aspect> implements RSValue {
