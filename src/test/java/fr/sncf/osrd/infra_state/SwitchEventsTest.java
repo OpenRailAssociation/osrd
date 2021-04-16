63e54fe4eed8ea72cf0b3fb2528d1c7b96bf83f2
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-04-16 15:16:32 +0200

    infra: tests: factorize and clean up switch event tests

--- src/test/java/fr/sncf/osrd/infra_state/SwitchEventsTest.java
+++ src/test/java/fr/sncf/osrd/infra_state/SwitchEventsTest.java
@@ -7,7 +7,6 @@ import fr.sncf.osrd.config.Config;
 import fr.sncf.osrd.config.JsonConfig;
 import fr.sncf.osrd.infra.Infra;
 import fr.sncf.osrd.infra.InvalidInfraException;
-import fr.sncf.osrd.infra.trackgraph.Switch;
 import fr.sncf.osrd.infra.trackgraph.SwitchPosition;
 import fr.sncf.osrd.railjson.parser.exceptions.InvalidRollingStock;
 import fr.sncf.osrd.railjson.parser.exceptions.InvalidSchedule;
@@ -21,12 +20,15 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class SwitchEventsTest {
 
+    /** Generates the defaults infra from tiny_infra/infra.json, to be edited for each test */
     public Infra getBaseInfra() {
         try {
             ClassLoader classLoader = getClass().getClassLoader();
@@ -39,6 +41,7 @@ public class SwitchEventsTest {
         }
     }
 
+    /** Go through all the events in the simulation */
     public ArrayList<TimelineEvent> run(Simulation sim) {
         ClassLoader classLoader = getClass().getClassLoader();
         var configPath = classLoader.getResource("tiny_infra/config_railjson.json");
@@ -57,15 +60,35 @@ public class SwitchEventsTest {
         }
     }
 
+    /** Generates an event that runs an assertion at a certain point in the simulation */
     public void makeAssertEvent(Simulation sim, double time, Function<Simulation, Boolean> predicate) {
         BiConsumer<Simulation, SimulationTest.TestEvent> consumer = (s, test) -> assertTrue(predicate.apply(s));
         SimulationTest.TestEvent.plan(sim, time, null, consumer);
+    }
+
+    /** Sets a field in an object, bypassing `final`.
+     * This is used to make small changes in the infra for each test
+     * */
+    public void setField(Object o, String fieldName, Object value) {
+
+        try {
+            Field field = o.getClass().getDeclaredField(fieldName);
+            AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+                field.setAccessible(true);
+                return null;
+            });
+            field.set(o, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e);
+        }
     }
 
     @Test
     public void testSwitchNoMove() {
         var infra = getBaseInfra();
         var sim = Simulation.createFromInfra(infra, 0, null);
+        RouteState routeState = sim.infraState.getRouteState(3);
+        makeAssertEvent(sim, 21, (s) -> routeState.status == RouteStatus.RESERVED);
         var events = run(sim);
 
         // The switch starts in the correct position, no switch move event should happen
@@ -75,7 +98,7 @@ public class SwitchEventsTest {
     }
 
     @Test
-    public void testSimpleSwitch() throws NoSuchFieldException, IllegalAccessException {
+    public void testSimpleSwitch() {
         var infra = getBaseInfra();
 
         // Set all the switch expected positions to RIGHT, to trigger a change
@@ -85,10 +108,7 @@ public class SwitchEventsTest {
             positions.replaceAll((k, v) -> SwitchPosition.RIGHT);
         });
 
-        Switch mainSwitch = infra.switches.get(0);
-        Field delayField = Switch.class.getDeclaredField("positionChangeDelay");
-        delayField.setAccessible(true);
-        delayField.set(mainSwitch, 6);
+        setField(infra.switches.get(0), "positionChangeDelay", 6);
 
         var sim = Simulation.createFromInfra(infra, 0, null);
 
@@ -104,7 +124,7 @@ public class SwitchEventsTest {
     }
 
     @Test
-    public void testSwitchLongerDelay() throws NoSuchFieldException, IllegalAccessException {
+    public void testSwitchLongerDelay() {
         var infra = getBaseInfra();
 
         // Set all the switch expected positions to RIGHT, to trigger a change
@@ -114,10 +134,7 @@ public class SwitchEventsTest {
             positions.replaceAll((k, v) -> SwitchPosition.RIGHT);
         });
 
-        Switch mainSwitch = infra.switches.get(0);
-        Field delayField = Switch.class.getDeclaredField("positionChangeDelay");
-        delayField.setAccessible(true);
-        delayField.set(mainSwitch, 10);
+        setField(infra.switches.get(0), "positionChangeDelay", 10);
 
         var sim = Simulation.createFromInfra(infra, 0, null);
 
