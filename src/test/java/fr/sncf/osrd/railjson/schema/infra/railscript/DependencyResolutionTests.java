fb894bad658c2ef40a315f5b35f3424bb270f635
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-05-06 16:10:21 +0200

    fix style

--- src/test/java/fr/sncf/osrd/railjson/schema/infra/railscript/DependencyResolutionTests.java
+++ src/test/java/fr/sncf/osrd/railjson/schema/infra/railscript/DependencyResolutionTests.java
@@ -1,14 +1,14 @@
 package fr.sncf.osrd.railjson.schema.infra.railscript;
 
+import static fr.sncf.osrd.Helpers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import fr.sncf.osrd.infra.InvalidInfraException;
 import fr.sncf.osrd.railjson.parser.RailJSONParser;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.stream.Collectors;
-
-import static fr.sncf.osrd.Helpers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DependencyResolutionTests {
 
@@ -19,7 +19,6 @@ public class DependencyResolutionTests {
         var config = getBaseConfig("tiny_infra/config_railjson_optional.json");
         assert config != null;
 
-        var signals = RailJSONParser.parse(infra).signals;
         var expectedMap = new HashMap<String, HashSet<String>>();
         expectedMap.put("il.sig.C1", new HashSet<>());
         expectedMap.put("il.sig.C2", new HashSet<>());
@@ -28,7 +27,7 @@ public class DependencyResolutionTests {
         expectedMap.put("il.sig.S7", new HashSet<>(Collections.singletonList("il.sig.W5")));
         expectedMap.put("il.sig.W4", new HashSet<>(Collections.singletonList("il.sig.C2")));
         expectedMap.put("il.sig.W5", new HashSet<>(Arrays.asList("il.sig.C3", "il.sig.C1")));
-        for (var s : signals) {
+        for (var s : RailJSONParser.parse(infra).signals) {
             var res = s.signalSubscribers.stream().map(x -> x.id).collect(Collectors.toSet());
             assertEquals(expectedMap.get(s.id), res);
         }
