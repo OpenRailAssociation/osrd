707010a52fc2c97bb57ea80a9dc6707494a2e301
Nicolas Wurtz <gitlab@nethenic.net>
2021-05-11 16:53:11 +0200

    traincompo: selector hooked

--- src/applications/osrd/views/OSRDConfig/TrainCompoSelector.js
+++ src/applications/osrd/views/OSRDConfig/TrainCompoSelector.js
@@ -1,72 +1,59 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import ModalSNCF from 'common/BootstrapSNCF/ModalSNCF/ModalSNCF';
 import ModalBodySNCF from 'common/BootstrapSNCF/ModalSNCF/ModalBodySNCF';
 import ModalFooterSNCF from 'common/BootstrapSNCF/ModalSNCF/ModalFooterSNCF';
 import TrainCompo from 'applications/osrd/components/TrainCompo/TrainCompo';
 import TrainCompoCard from 'applications/osrd/components/TrainCompo/TrainCompoCard';
 
-class TrainCompoSelector extends React.Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-    modalID: PropTypes.string.isRequired,
-    osrdconf: PropTypes.object.isRequired,
-  }
+export default function TrainCompoSelector(props) {
+  const osrdconf = useSelector((state) => state.osrdconf);
+  const { modalID } = props;
+  const { t } = useTranslation();
 
-  displayChosenCompo = () => {
-    const { osrdconf } = this.props;
-    return (
-      <TrainCompoCard
-        data={osrdconf.trainCompo}
-        displayDetails={() => {}}
-        active={false}
-      />
-    );
-  }
+  const displayChosenCompo = () => (
+    <TrainCompoCard
+      data={osrdconf.trainCompo}
+      displayDetails={() => {}}
+      active={false}
+    />
+  );
 
-  render() {
-    const {
-      t, modalID, osrdconf,
-    } = this.props;
-
-    return (
-      <>
-        <div className="osrd-config-item mb-2">
-          <div className="osrd-config-item-container">
-            {osrdconf.trainCompo !== undefined && osrdconf.trainCompo.codenbengin !== undefined ? (
-              this.displayChosenCompo()
-            ) : (
-              <span className="mr-2 text-muted text-italic">
-                {t('osrd.config.noTrainCompo')}
-              </span>
-            )}
-            <button type="button" className="btn btn-sm btn-secondary ml-auto" data-toggle="modal" data-target={`#${modalID}`}>
-              {t('osrd.config.chooseTrainCompo')}
-              <i className="icons-itinerary-train ml-2" />
+  return (
+    <>
+      <div className="osrd-config-item mb-2">
+        <div className="osrd-config-item-container">
+          {osrdconf.trainCompo !== undefined && osrdconf.trainCompo.codenbengin !== undefined ? (
+            displayChosenCompo()
+          ) : (
+            <span className="mr-2 text-muted text-italic">
+              {t('osrd.config.noTrainCompo')}
+            </span>
+          )}
+          <button type="button" className="btn btn-sm btn-secondary ml-auto" data-toggle="modal" data-target={`#${modalID}`}>
+            {t('osrd.config.chooseTrainCompo')}
+            <i className="icons-itinerary-train ml-2" />
+          </button>
+        </div>
+      </div>
+      <ModalSNCF htmlID={modalID} optionalClasses="traincompo-modal">
+        <ModalBodySNCF>
+          <TrainCompo />
+        </ModalBodySNCF>
+        <ModalFooterSNCF>
+          <div className="d-flex flex-row-reverse w-100">
+            <button className="btn btn-secondary btn-sm" type="button" data-dismiss="modal">
+              {t('common.close')}
             </button>
           </div>
-        </div>
-        <ModalSNCF htmlID={modalID} optionalClasses="traincompo-modal">
-          <ModalBodySNCF>
-            <TrainCompo />
-          </ModalBodySNCF>
-          <ModalFooterSNCF>
-            <div className="d-flex flex-row-reverse w-100">
-              <button className="btn btn-secondary btn-sm" type="button" data-dismiss="modal">
-                {t('common.close')}
-              </button>
-            </div>
-          </ModalFooterSNCF>
-        </ModalSNCF>
-      </>
-    );
-  }
+        </ModalFooterSNCF>
+      </ModalSNCF>
+    </>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  osrdconf: state.osrdconf,
-});
-
-export default connect(mapStateToProps)(withTranslation()(TrainCompoSelector));
+TrainCompoSelector.propTypes = {
+  modalID: PropTypes.string.isRequired,
+};
