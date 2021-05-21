82e54abb4992475689d6b559c4af60e126d6d7b0
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-05-21 14:04:44 +0200

    add some SpeedControllerSet documentation

--- src/main/java/fr/sncf/osrd/speedcontroller/SpeedControllerSet.java
+++ src/main/java/fr/sncf/osrd/speedcontroller/SpeedControllerSet.java
@@ -7,14 +7,23 @@ import fr.sncf.osrd.speedcontroller.generators.SpeedControllerGenerator;
 import java.util.HashSet;
 import java.util.Set;
 
+/** Contains all the pre-computed speed controller and indications.
+ * For now it contains data about desired speed (with margin or mareco), and max speed for when the train is
+ * running late. Later on, we may add other indications such as when to coast. */
 public class SpeedControllerSet {
     public final Set<SpeedController> maxSpeedControllers;
     public final Set<SpeedController> desiredSpeedControllers;
 
+    /** Generates the base speed controllers, where desired speed = max speed
+     * @param schedule train schedule */
     public SpeedControllerSet(TrainSchedule schedule) {
         this(schedule, new HashSet<>());
     }
 
+    /** Generates the desired speed controllers from the given generators. Max speed is always determined
+     * from a `new MaxSpeedGenerator`.
+     * @param schedule train schedule
+     * @param desiredSpeedGenerators set of generators used for desired speed controllers */
     public SpeedControllerSet(TrainSchedule schedule, Set<SpeedControllerGenerator> desiredSpeedGenerators) {
         maxSpeedControllers = new MaxSpeedGenerator().generate(schedule);
         desiredSpeedControllers = new HashSet<>(maxSpeedControllers);
@@ -22,6 +31,7 @@ public class SpeedControllerSet {
             desiredSpeedControllers.addAll(gen.generate(schedule));
     }
 
+    /** Copy constructor */
     public SpeedControllerSet(SpeedControllerSet other) {
         this.maxSpeedControllers = new HashSet<>(other.maxSpeedControllers);
         this.desiredSpeedControllers = new HashSet<>(other.desiredSpeedControllers);
