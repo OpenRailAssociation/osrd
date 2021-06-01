ce10f57896d25097c36405862eef471b1ba8a7bc
Florian Amsallem <florian.amsallem@epita.fr>
2021-06-01 18:20:00 +0200

    infra: add timetable and train schedule models

--- osrd_infra/models/simulation.py
+++ osrd_infra/models/simulation.py
@@ -1,9 +1,22 @@
 from django.contrib.gis.db import models
 from osrd_infra.models.infra import Infra
+from osrd_infra.models.rolling_stock import RollingStock
+from osrd_infra.models.pathfinding import Path
 
 
 class Timetable(models.Model):
     infra = models.ForeignKey(Infra, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=128)
+
+
+class TrainSchedule(models.Model):
+    train_id = models.CharField(max_length=128)
+    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
+    rolling_stock = models.ForeignKey(RollingStock, on_delete=models.DO_NOTHING)
+    departure_time = models.FloatField()
+    path = models.ForeignKey(Path, on_delete=models.CASCADE)
+    phases = models.JSONField()
+    initial_speed = models.FloatField()
 
 
 class Simulation(models.Model):
@@ -11,7 +24,3 @@ class Simulation(models.Model):
     timetable = models.ForeignKey(Timetable, on_delete=models.DO_NOTHING)
     start_time = models.DateTimeField(auto_now_add=True)
     started_by = models.UUIDField()
-
-
-class RollingStock(models.Model):
-    pass
