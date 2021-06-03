3d275c4d10f73a8a1b8b66889599fb78d0b794fb
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-06-03 14:07:13 +0200

    operational points: update infra with positions instead of ranges

--- src/main/java/fr/sncf/osrd/infra/OperationalPoint.java
+++ src/main/java/fr/sncf/osrd/infra/OperationalPoint.java
@@ -24,7 +24,7 @@ public class OperationalPoint implements ActionPoint {
      * Add a reference to an operational point into a track section
      * @param section the track section to reference the op from
      * @param position the position of the operational point section
-     * @param opBuilder
+     * @param opBuilder Builder for sequence of operational point, to add the point in it
      */
     public void addRef(TrackSection section, double position, PointSequence.Builder<OperationalPoint> opBuilder) {
         this.refs.add(section);
