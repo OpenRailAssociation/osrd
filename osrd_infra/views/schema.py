6b337b4b3e7b8f3e9afdb8134bbbb774e2131613
Paul Guénézan <paul.guenezan@epita.fr>
2021-06-08 16:19:43 +0200

    infra: improve schema endpoint

--- osrd_infra/views/schema.py
+++ osrd_infra/views/schema.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.apps import apps
 
-from osrd_infra.models import Entity, Component
+from osrd_infra.models import Entity, Component, get_entity_meta, get_component_meta
 
 
 class SchemaView(APIView):
@@ -11,17 +11,17 @@ class SchemaView(APIView):
         for model in apps.get_app_config("osrd_infra").get_models():
             if Entity not in model.__bases__:
                 continue
-            entity_meta = model._entity_meta
+            entity_meta = get_entity_meta(model)
             entities.append({
-                'entity_name': model.__name__,
-                'components': list(map(lambda component: component.__name__, entity_meta.components)),
+                'entity_name': entity_meta.name,
+                'components': list(entity_meta.component_names()),
             })
         components = []
         for model in apps.get_app_config("osrd_infra").get_models():
             if Component not in model.__bases__:
                 continue
             components.append({
-                'component_name': model.__name__,
-                'fields': list(map(lambda field: field.name, model._meta.fields)),
+                'component_name': get_component_meta(model).name,
+                'fields': [field.name for field in model._meta.fields],
             })
         return Response({'entities': entities, 'components': components})
