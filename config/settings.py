9119260c936755c1e6c4ed6091827cdd9a79aa3f
Florian Amsallem <florian.amsallem@epita.fr>
2021-06-10 10:53:05 +0200

    revert: dags: add hardcoded data

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
