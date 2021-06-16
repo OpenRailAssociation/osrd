3b41edd2e57458e2027c487da0f4010ee562ec0a
Benoit Simard <github@bsimard.com>
2021-06-16 13:34:06 +0200

    editor: Increase the timeout for notifications

--- src/common/Notifications.tsx
+++ src/common/Notifications.tsx
@@ -5,7 +5,7 @@ import { MainState, deleteNotification } from '../reducers/main';
 import { Notification } from '../types';
 import './Notifications.scss';
 
-const TIMEOUT_MS = 2000;
+const TIMEOUT_MS = 4000;
 
 const NotificationWrapper: FC<Notification> = (notif) => {
   const [timeoutId, setTimeoutId] = useState<number | null>(null);
