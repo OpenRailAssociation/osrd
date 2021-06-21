682253af8bdb9c09a60ba17af9ebd98f836a7fa7
Nicolas Wurtz <gitlab@nethenic.net>
2021-06-21 20:18:00 +0200

    timetable: adding trainList wip

--- src/applications/osrd/views/OSRDConfig/TimetableSelector.js
+++ src/applications/osrd/views/OSRDConfig/TimetableSelector.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import nextId from 'react-id-generator';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { get } from 'common/requests';
@@ -7,9 +8,18 @@ import icon from 'assets/pictures/layersicons/timetable.svg';
 import DotsLoader from 'common/DotsLoader/DotsLoader';
 
 const timetableURL = '/osrd/timetable';
+const trainscheduleURI = '/osrd/train_schedule';
+
+const formatTrainList = (trainList) => trainList.map((train) => (
+  <div key={nextId()}>
+    <span>{train.train_name}</span>
+    <span>{train.departure_time}</span>
+  </div>
+));
 
 export default function TimetableSelector() {
   const [selectedTimetable, setselectedTimetable] = useState(undefined);
+  const [trainList, setTrainList] = useState(undefined);
   const osrdconf = useSelector((state) => state.osrdconf);
   const { t } = useTranslation();
 
@@ -22,6 +32,18 @@ export default function TimetableSelector() {
     }
   };
 
+  const getTrainList = async () => {
+    const trainListLocal = [];
+    for (const trainscheduleID of selectedTimetable.train_schedules) {
+      try {
+        trainListLocal.push(await get(`${trainscheduleURI}/${trainscheduleID}`));
+      } catch (e) {
+        console.log('ERROR', e);
+      }
+    }
+    setTrainList(trainListLocal);
+  };
+
   useEffect(() => {
     if (osrdconf.timetableID !== undefined) {
       getTimetable(osrdconf.timetableID);
@@ -29,6 +51,12 @@ export default function TimetableSelector() {
       setselectedTimetable(undefined);
     }
   }, [osrdconf.timetableID]);
+
+  useEffect(() => {
+    if (selectedTimetable !== undefined) {
+      getTrainList();
+    }
+  }, [selectedTimetable]);
 
   return (
     <>
@@ -40,7 +68,7 @@ export default function TimetableSelector() {
           data-toggle="modal"
           data-target="#timetable-selector-modal"
         >
-          <div className="h2 mb-0">
+          <div className="h2 mb-0 d-flex align-items-center">
             <img className="mr-1" src={icon} alt="timetableIcon" />
             <span className="text-muted">{t('osrdconf:timetable')}</span>
             {osrdconf.timetableID !== undefined && selectedTimetable === undefined
@@ -50,12 +78,16 @@ export default function TimetableSelector() {
                   {selectedTimetable !== undefined ? (
                     <>
                       <span className="ml-1">{selectedTimetable.name}</span>
-                      <small className="ml-1 text-primary">{selectedTimetable.id}</small>
+                      <small className="ml-1 text-primary flex-grow-1">{selectedTimetable.id}</small>
+                      <span className="ml-2 badge badge-primary">
+                        {`${selectedTimetable.train_schedules.length} ${t('translation:common.train(s)')}`}
+                      </span>
                     </>
                   ) : <span className="ml-1">{t('osrdconf:noTimetable')}</span> }
                 </>
               )}
           </div>
+          {trainList !== undefined ? formatTrainList(trainList) : null}
         </div>
       </div>
       <TimetableSelectorModal />
