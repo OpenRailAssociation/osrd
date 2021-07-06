ed9bed583755595bfd44ffdd8681c44d116c420a
Nicolas Wurtz <gitlab@nethenic.net>
2021-07-06 12:05:45 +0200

    osrdconf: speed up snapped marker on track

--- src/common/Map/Layers/SnappedMarker.js
+++ src/common/Map/Layers/SnappedMarker.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Source, Layer } from 'react-map-gl';
-
-const circle = {
-  type: 'circle',
-  paint: {
-    'circle-color': '#ffffff',
-    'circle-opacity': 1,
-    'circle-radius': 4,
-    'circle-stroke-color': '#0088ce',
-    'circle-stroke-width': 3,
-  },
-};
+import { Marker } from 'react-map-gl';
 
 const SearchMarker = (props) => {
   const { geojson } = props;
   return (
-    <Source type="geojson" data={geojson}>
-      <Layer {...circle} />
-    </Source>
+    <Marker
+      className="map-search-marker"
+      longitude={geojson.geometry.coordinates[0]}
+      latitude={geojson.geometry.coordinates[1]}
+      captureClick={false}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style={{ cursor: 'pointer' }}>
+        <circle style={{ fill: '#0088ce' }} cx="16" cy="16" r="6" />
+        <circle style={{ fill: '#ffffff' }} cx="16" cy="16" r="4" />
+      </svg>
+    </Marker>
   );
 };
 
