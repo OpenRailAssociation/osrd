d1580feafacd3b11e7172431fe326536c5e7fc2a
Victor "multun" Collod <victor.collod@epita.fr>
2021-08-06 17:57:59 +0200

    api: implement parallel request handling

--- src/test/java/fr/sncf/osrd/api/ApiTest.java
+++ src/test/java/fr/sncf/osrd/api/ApiTest.java
@@ -3,6 +3,7 @@ package fr.sncf.osrd.api;
 import static org.mockito.Mockito.when;
 
 import fr.sncf.osrd.Helpers;
+import fr.sncf.osrd.api.InfraManager.InfraLoadException;
 import fr.sncf.osrd.config.JsonConfig;
 import fr.sncf.osrd.infra.Infra;
 import fr.sncf.osrd.infra.InvalidInfraException;
@@ -16,11 +17,11 @@ import java.io.IOException;
 @ExtendWith(MockitoExtension.class)
 public class ApiTest {
     @Mock
-    static InfraHandler infraHandlerMock;
+    static InfraManager infraHandlerMock;
 
     /** Setup infra handler mock */
     @BeforeEach
-    public void setUp() throws InvalidInfraException, IOException {
+    public void setUp() throws InvalidInfraException, IOException, InfraLoadException, InterruptedException {
         final var infra = "tiny_infra/infra.json";
         var tinyInfra = Infra.parseFromFile(JsonConfig.InfraType.UNKNOWN, Helpers.getResourcePath(infra).toString());
         when(infraHandlerMock.load(infra)).thenReturn(tinyInfra);
