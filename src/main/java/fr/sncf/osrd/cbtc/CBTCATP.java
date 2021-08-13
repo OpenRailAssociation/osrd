098bf91de0c11d2a889de94260d838fc6d52dfc5
Tristan François <tpv.francois@gmail.com>
2021-08-13 10:55:20 +0200

    CBTCATP : suppressFBWarnings

--- src/main/java/fr/sncf/osrd/cbtc/CBTCATP.java
+++ src/main/java/fr/sncf/osrd/cbtc/CBTCATP.java
@@ -1,6 +1,7 @@
 package fr.sncf.osrd.cbtc;
 
 import java.util.ArrayList;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.simulation.Simulation;
 import fr.sncf.osrd.speedcontroller.SpeedController;
 import fr.sncf.osrd.train.TrainSchedule;
@@ -10,8 +11,11 @@ import fr.sncf.osrd.train.TrainState;
  * Class dedicated to the calculation of speed constraints related to the CBTC. 
  */
 public class CBTCATP {
+    @SuppressFBWarnings("URF_UNREAD_FIELD")
     private Simulation sim;
+    @SuppressFBWarnings("URF_UNREAD_FIELD")
     private TrainSchedule trainSchedule;
+    @SuppressFBWarnings("URF_UNREAD_FIELD")
     private TrainState trainState;
 
     /**
