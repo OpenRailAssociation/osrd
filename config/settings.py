1f1a9c414d159e50c49ed38717b6bb0b2b327e1a
Victor "multun" Collod <victor.collod@epita.fr>
2021-08-19 11:29:36 +0200

    config: make it easier to control debug settings

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
+from importlib.util import find_spec
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'BeZPeRmxngJAbJECmraxvpvLQebOYPNACjqGwujizGcIGHuEIz'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
 
 OSRD_INFRA_SRID = 3857
+
 
 # Application definition
 
@@ -141,3 +141,87 @@ STATIC_URL = '/static/'
 APPEND_SLASH = False
 
 WORKSPACE = False
+
+
+def getenv_bool(var_name, default=False):
+    env_var = os.getenv(var_name, "")
+    if not env_var:
+        return default
+    return env_var.lower() in ("1", "true")
+
+
+OSRD_DEV_SETUP = getenv_bool("OSRD_DEV_SETUP")
+OSRD_DEBUG = getenv_bool("OSRD_DEBUG", default=OSRD_DEV_SETUP)
+OSRD_SKIP_AUTH = getenv_bool("OSRD_SKIP_AUTH", default=OSRD_DEBUG)
+
+
+if OSRD_DEV_SETUP:
+    ALLOWED_HOSTS = ["*"]
+    INTERNAL_IPS = ['127.0.0.1']
+else:
+    ALLOWED_HOSTS = []
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = OSRD_DEBUG
+
+# enable django debug toolbar if it is installed
+if OSRD_DEBUG and find_spec("debug_toolbar") is not None:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]
+
+if OSRD_SKIP_AUTH:
+    MIDDLEWARE += ['config.test_middleware.LocalUserMiddleware']
+    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ['config.test_middleware.TestGatewayAuth']
+
+logging_handlers = {}
+for handler in os.getenv("LOGGERS", "").split(","):
+    handler_name = handler
+    handler_level = "DEBUG"
+
+    split_handler = handler.split(":")
+    if len(split_handler) == 2:
+        handler_name, handler_level = split_handler
+    logging_handlers[handler_name] = {
+        'level': handler_level.upper(),
+        'handlers': ['console'],
+    }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        }
+    },
+    'loggers': logging_handlers,
+}
+
+OSRD_BACKEND_URL = os.getenv("OSRD_BACKEND_URL", "http://localhost:8080/")
+OSRD_BACKEND_TOKEN = os.getenv("OSRD_BACKEND_TOKEN", "")
--- config/test.py
+++ config/test.py
@@ -1,5 +1,11 @@
+from os import getenv, environ
+
+# when developping, enable debug mode and skip authentication
+environ.setdefault("OSRD_DEV_SETUP", "1")
+environ.setdefault("OSRD_DEBUG", "1")
+environ.setdefault("OSRD_SKIP_AUTH", "1")
+
 from config.settings import *  # noqa
-from os import getenv
 
 DATABASES = {
     'default': {
@@ -22,38 +28,4 @@ ROOT_PATH = ''
 
 STATIC_URL = ROOT_PATH + '/static/'
 
-
-INSTALLED_APPS += [  # noqa
-    'debug_toolbar',
-]
-
 ROOT_URLCONF = 'config.workspace_urls'
-
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa
-MIDDLEWARE += ['config.test_middleware.LocalUserMiddleware']
-
-REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ['config.test_middleware.TestGatewayAuth']  # noqa
-
-DEBUG = True
-
-INTERNAL_IPS = ['127.0.0.1']
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
-
-OSRD_BACKEND_URL = getenv("OSRD_BACKEND_URL", "http://localhost:8080/")
-OSRD_BACKEND_TOKEN = getenv("OSRD_BACKEND_TOKEN", "")
