bdceab16c23060ced218c6d4ea88baf338ce54f6
Victor "multun" Collod <victor.collod@epita.fr>
2021-08-23 16:46:14 +0200

    config: add a custom request logger
    Django's request logger waits for the response to be ready before logging the request,
    which is a debugging pain

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.request_logger.RequestLogger',
 ]
 
 REST_FRAMEWORK = {
