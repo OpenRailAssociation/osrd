f4f9b312c8182e341eb8c0dcff47bfc62dc90fa6
Nicolas Wurtz <gitlab@nethenic.net>
2021-08-24 15:56:16 +0200

    home: translated & upgraded

--- src/main/Home/index.js
+++ src/main/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import NavBarSNCF from 'common/BootstrapSNCF/NavBarSNCF';
 import Card from 'common/BootstrapSNCF/CardSNCF/CardSNCF';
 import osrdLogo from 'assets/pictures/osrd.png';
+import timetablePic from 'assets/pictures/timetable.png';
 import logo from 'assets/logo_osrd_seul_blanc.svg';
 import cartoPic from 'assets/pictures/carto.png';
 import editorPic from 'assets/pictures/editor.png';
@@ -10,6 +12,7 @@ import './Home.css';
 
 export default function Home() {
   const user = useSelector((state) => state.user);
+  const { t } = useTranslation();
 
   return (
     <>
@@ -22,13 +25,13 @@ export default function Home() {
         <div className="cardscontainer">
           <div className="row">
             <div className="col-sm-6 col-md-3 col-lg-2 mb-2">
-              <Card img={osrdLogo} title="OSRD" link="/osrd" />
+              <Card img={timetablePic} title={t('Home.timetable')} link="/osrd" />
             </div>
             <div className="col-sm-6 col-md-3 col-lg-2 mb-2">
-              <Card img={cartoPic} title="Cartographie" link="/carto" />
+              <Card img={cartoPic} title={t('Home.map')} link="/carto" />
             </div>
             <div className="col-sm-6 col-md-3 col-lg-2 mb-2">
-              <Card img={editorPic} title="Editor" link="/editor" />
+              <Card img={editorPic} title={t('Home.editor')} link="/editor" />
             </div>
           </div>
         </div>
