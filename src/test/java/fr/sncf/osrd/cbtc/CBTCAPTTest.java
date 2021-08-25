dbdb89172e18a1ac4277eede4a9e674d4d4c021a
ThoLes94 <59646128+ThoLes94@users.noreply.github.com>
2021-08-25 10:34:08 +0200

    CBTCATP : test MA and following trains

--- src/test/java/fr/sncf/osrd/cbtc/CBTCAPTTest.java
+++ src/test/java/fr/sncf/osrd/cbtc/CBTCAPTTest.java
@@ -1,10 +1,9 @@
-//package fr.sncf.osrd.cbtc;
+package fr.sncf.osrd.cbtc;
 
 
-/*import fr.sncf.osrd.simulation.Simulation;
+import fr.sncf.osrd.simulation.Simulation;
 
 import static fr.sncf.osrd.Helpers.*;
-import static fr.sncf.osrd.speedcontroller.MarginTests.saveGraph;
 import static org.junit.jupiter.api.Assertions.assertEquals; 
 
 import fr.sncf.osrd.infra.trackgraph.SwitchPosition;
@@ -14,6 +13,9 @@ import fr.sncf.osrd.train.events.TrainMoveEvent;
 import fr.sncf.osrd.train.events.TrainReachesActionPoint;
 import fr.sncf.osrd.train.phases.SignalNavigatePhase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 import fr.sncf.osrd.infra.InvalidInfraException;
 import fr.sncf.osrd.infra.trackgraph.SwitchPosition;
@@ -31,22 +33,37 @@ public class CBTCAPTTest {
         final var infra = getBaseInfra();
         assert infra != null;
 
-        // base run, no margin
-        final var config = makeConfigWithSpeedParams(null);
-        var sim = Simulation.createFromInfra(RailJSONParser.parse(infra), 0, null);
-        var eventsBase = run(sim, config);
+        final var config = getBaseConfig("tiny_infra/config_railjson_cbtc.json");
+        var sim = Simulation.createFromInfraAndEmptySuccessions(RailJSONParser.parse(infra), 0, null);
         var baseSimTime = sim.getTime();
         makeAssertEvent(sim, 1, () -> distanceDanger(sim));
+        var eventsBase = run(sim, config);
         // run(sim, config);
     }
     
     public Boolean distanceDanger(Simulation sim){
         var trainState = sim.trains.get("Test.").getLastState();
         var trainSchedule = sim.trains.get("Test.").schedule;
-
         CBTCATP cbtc = new CBTCATP(sim, trainSchedule, trainState);
-
         return Math.abs(cbtc.getNextDangerDistance() - 2100.0) < 0.001 || true;
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"_5", "_10", "_15"})
+    public void testTrainBehindSlowTrain(String path)throws InvalidInfraException{
+        final var infra = getBaseInfra("line_infra/infra_cbtc.json");
+        assert infra != null;
+
+        final var config = getBaseConfig("line_infra/config_cbtc"+path+".json");
+        var sim = Simulation.createFromInfraAndEmptySuccessions(RailJSONParser.parse(infra), 0, null);
+        var baseSimTime = sim.getTime();
+        makeAssertEvent(sim, 300, () -> speedTrain(sim));
+        var eventsBase = run(sim, config);
+    }
+
+    public Boolean speedTrain(Simulation sim){
+        var trainState1 = sim.trains.get("train.0").getLastState();
+        var trainState2 = sim.trains.get("train.1").getLastState();
+        return Math.abs((trainState1.speed - trainState2.speed)/trainState1.speed) < 0.5;
+    }
 }
-*/
