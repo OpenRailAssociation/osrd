0e030c28ccd0ef03b90f25be32059b0b674d9a80
Eloi Charpentier <--get>
2021-08-25 14:02:14 +0200

    api: add exception reporting to pathfinding endpoints

--- src/main/java/fr/sncf/osrd/api/PathfindingRoutesEndpoint.java
+++ src/main/java/fr/sncf/osrd/api/PathfindingRoutesEndpoint.java
@@ -51,136 +51,141 @@ public class PathfindingRoutesEndpoint extends PathfindingEndpoint {
     @SuppressWarnings({"unchecked", "rawtypes"})
     @SuppressFBWarnings({"BC_UNCONFIRMED_CAST"})
     public Response act(Request req) throws IOException {
-        var body = new RqPrint(req).printBody();
-        var request = adapterRequest.fromJson(body);
-        if (request == null)
-            return new RsWithStatus(new RsText("missing request body"), 400);
-
-        var reqWaypoints = request.waypoints;
-
-        // load infra
-        Infra infra;
         try {
-            infra = infraManager.load(request.infra);
-        } catch (InfraLoadException | InterruptedException e) {
-            return new RsWithStatus(new RsText(
-                    String.format("Error loading infrastructure '%s'%n%s", request.infra, e.getMessage())), 400);
-        }
+            var body = new RqPrint(req).printBody();
+            var request = adapterRequest.fromJson(body);
+            if (request == null)
+                return new RsWithStatus(new RsText("missing request body"), 400);
 
-        // parse the waypoints
-        var waypoints = (ArrayList<RouteLocation>[]) new ArrayList[reqWaypoints.length];
-        for (int i = 0; i < waypoints.length; i++) {
-            var stopWaypoints = new ArrayList<RouteLocation>();
-            for (var stopWaypoint : reqWaypoints[i]) {
-                var edge = infra.trackGraph.trackSectionMap.get(stopWaypoint.trackSection);
-                if (edge == null)
-                    return new RsWithStatus(new RsText(
-                            String.format("Couldn't find track section '%s'", stopWaypoint.trackSection)), 400);
-                if (stopWaypoint.offset < 0 || stopWaypoint.offset > edge.length)
-                    return new RsWithStatus(new RsText(String.format(
-                            "'%f' is an invalid offset for the track section '%s'",
+            var reqWaypoints = request.waypoints;
+
+            // load infra
+            Infra infra;
+            try {
+                infra = infraManager.load(request.infra);
+            } catch (InfraLoadException | InterruptedException e) {
+                return new RsWithStatus(new RsText(
+                        String.format("Error loading infrastructure '%s'%n%s", request.infra, e.getMessage())), 400);
+            }
+
+            // parse the waypoints
+            var waypoints = (ArrayList<RouteLocation>[]) new ArrayList[reqWaypoints.length];
+            for (int i = 0; i < waypoints.length; i++) {
+                var stopWaypoints = new ArrayList<RouteLocation>();
+                for (var stopWaypoint : reqWaypoints[i]) {
+                    var edge = infra.trackGraph.trackSectionMap.get(stopWaypoint.trackSection);
+                    if (edge == null)
+                        return new RsWithStatus(new RsText(
+                                String.format("Couldn't find track section '%s'", stopWaypoint.trackSection)), 400);
+                    if (stopWaypoint.offset < 0 || stopWaypoint.offset > edge.length)
+                        return new RsWithStatus(new RsText(String.format(
+                                "'%f' is an invalid offset for the track section '%s'",
+                                stopWaypoint.offset,
+                                stopWaypoint.trackSection)),
+                                400);
+                    edge.getRoutes(stopWaypoint.direction).findOverlappingIntervals(
+                            routeFragment -> {
+                                var trackOffset = stopWaypoint.offset - routeFragment.begin;
+                                if (routeFragment.direction == EdgeDirection.STOP_TO_START)
+                                    trackOffset = routeFragment.end - stopWaypoint.offset;
+                                var offset = routeFragment.routeOffset + trackOffset;
+                                stopWaypoints.add(new RouteLocation(routeFragment.route, offset));
+                            },
                             stopWaypoint.offset,
-                            stopWaypoint.trackSection)),
-                            400);
-                edge.getRoutes(stopWaypoint.direction).findOverlappingIntervals(
-                        routeFragment -> {
-                            var trackOffset = stopWaypoint.offset - routeFragment.begin;
-                            if (routeFragment.direction == EdgeDirection.STOP_TO_START)
-                                trackOffset = routeFragment.end - stopWaypoint.offset;
-                            var offset = routeFragment.routeOffset + trackOffset;
-                            stopWaypoints.add(new RouteLocation(routeFragment.route, offset));
+                            stopWaypoint.offset
+                    );
+                }
+                waypoints[i] = stopWaypoints;
+            }
+
+            var costFunction = new DistCostFunction<Route>();
+            var candidatePaths = Dijkstra.<Route>makePriorityQueue();
+            for (var startWaypoint : waypoints[0])
+                candidatePaths.add(new BasicPathNode<>(startWaypoint.route, startWaypoint.offset));
+
+            var pathsToGoal = new ArrayList<BasicPathNode<Route>>();
+
+            // Compute the paths from the entry waypoint to the exit waypoint
+            for (int i = 1; i < waypoints.length; i++) {
+                var destinationWaypoints = waypoints[i];
+
+                var found = Dijkstra.findPaths(
+                        infra.routeGraph,
+                        candidatePaths,
+                        costFunction,
+                        (pathNode) -> {
+                            for (var goalEdge : destinationWaypoints) {
+                                if (goalEdge.route != pathNode.edge)
+                                    continue;
+                                var addedCost = costFunction.evaluate(
+                                        goalEdge.route,
+                                        pathNode.position,
+                                        goalEdge.route.length
+                                );
+                                return pathNode.end(addedCost, goalEdge.route, goalEdge.offset);
+                            }
+                            return null;
                         },
-                        stopWaypoint.offset,
-                        stopWaypoint.offset
-                );
-            }
-            waypoints[i] = stopWaypoints;
-        }
+                        (pathToGoal) -> {
+                            pathsToGoal.add(pathToGoal);
+                            return false;
+                        });
 
-        var costFunction = new DistCostFunction<Route>();
-        var candidatePaths = Dijkstra.<Route>makePriorityQueue();
-        for (var startWaypoint : waypoints[0])
-            candidatePaths.add(new BasicPathNode<>(startWaypoint.route, startWaypoint.offset));
+                if (found == 0)
+                    return new RsWithStatus(new RsText("No path could be found"), 400);
 
-        var pathsToGoal = new ArrayList<BasicPathNode<Route>>();
-
-        // Compute the paths from the entry waypoint to the exit waypoint
-        for (int i = 1; i < waypoints.length; i++) {
-            var destinationWaypoints = waypoints[i];
-
-            var found = Dijkstra.findPaths(
-                    infra.routeGraph,
-                    candidatePaths,
-                    costFunction,
-                    (pathNode) -> {
-                        for (var goalEdge : destinationWaypoints) {
-                            if (goalEdge.route != pathNode.edge)
-                                continue;
-                            var addedCost = costFunction.evaluate(
-                                    goalEdge.route,
-                                    pathNode.position,
-                                    goalEdge.route.length
-                            );
-                            return pathNode.end(addedCost, goalEdge.route, goalEdge.offset);
-                        }
-                        return null;
-                    },
-                    (pathToGoal) -> {
-                        pathsToGoal.add(pathToGoal);
-                        return false;
-                    });
-
-            if (found == 0)
-                return new RsWithStatus(new RsText("No path could be found"), 400);
-
-            candidatePaths.clear();
-            var lastStop = pathsToGoal.get(pathsToGoal.size() - 1);
-            var newCandidate = new BasicPathNode<>(lastStop.edge, lastStop.position);
-            candidatePaths.add(newCandidate);
-        }
-
-        var res = new PathfindingResult();
-
-        for (var routeBasicPathNode : pathsToGoal) {
-            var path = FullPathArray.from(routeBasicPathNode);
-
-            var routeBeginLoc = pathNodeToRouteLocation(path.pathNodes.get(0));
-            var beginLoc = routeBeginLoc.getTrackSectionLocation();
-            var routeEndLoc = pathNodeToRouteLocation(path.pathNodes.get(path.pathNodes.size() - 1));
-            var endLoc = routeEndLoc.getTrackSectionLocation();
-
-            var routes = new ArrayList<Route>();
-            for (var node : path.pathNodes) {
-                if (routes.isEmpty() || routes.get(routes.size() - 1) != node.edge)
-                    routes.add(node.edge);
+                candidatePaths.clear();
+                var lastStop = pathsToGoal.get(pathsToGoal.size() - 1);
+                var newCandidate = new BasicPathNode<>(lastStop.edge, lastStop.position);
+                candidatePaths.add(newCandidate);
             }
 
-            for (int j = 0; j < routes.size(); j++) {
-                TrackSectionLocation begin = null;
-                TrackSectionLocation end = null;
-                if (j == 0)
-                    begin = beginLoc;
-                if (j == routes.size() - 1)
-                    end = endLoc;
-                var route = routes.get(j);
-                var trackSections = Route.routesToTrackSectionRange(
-                        Collections.singletonList(route), begin, end);
-                if (j == 0) {
-                    // Add the given origin location to the steps output
-                    var firstTrack = trackSections.get(0);
-                    var newStep = new PathfindingResult.StepResult(firstTrack.edge, firstTrack.getBeginPosition());
-                    res.addStep(newStep);
+            var res = new PathfindingResult();
+
+            for (var routeBasicPathNode : pathsToGoal) {
+                var path = FullPathArray.from(routeBasicPathNode);
+
+                var routeBeginLoc = pathNodeToRouteLocation(path.pathNodes.get(0));
+                var beginLoc = routeBeginLoc.getTrackSectionLocation();
+                var routeEndLoc = pathNodeToRouteLocation(path.pathNodes.get(path.pathNodes.size() - 1));
+                var endLoc = routeEndLoc.getTrackSectionLocation();
+
+                var routes = new ArrayList<Route>();
+                for (var node : path.pathNodes) {
+                    if (routes.isEmpty() || routes.get(routes.size() - 1) != node.edge)
+                        routes.add(node.edge);
                 }
-                res.add(route, trackSections);
-                if (j == routes.size() - 1) {
-                    // Add the given destination location to the steps output
-                    var lastTrack = trackSections.get(trackSections.size() - 1);
-                    var newStep = new PathfindingResult.StepResult(lastTrack.edge, lastTrack.getEndPosition());
-                    res.addStep(newStep);
+
+                for (int j = 0; j < routes.size(); j++) {
+                    TrackSectionLocation begin = null;
+                    TrackSectionLocation end = null;
+                    if (j == 0)
+                        begin = beginLoc;
+                    if (j == routes.size() - 1)
+                        end = endLoc;
+                    var route = routes.get(j);
+                    var trackSections = Route.routesToTrackSectionRange(
+                            Collections.singletonList(route), begin, end);
+                    if (j == 0) {
+                        // Add the given origin location to the steps output
+                        var firstTrack = trackSections.get(0);
+                        var newStep = new PathfindingResult.StepResult(firstTrack.edge, firstTrack.getBeginPosition());
+                        res.addStep(newStep);
+                    }
+                    res.add(route, trackSections);
+                    if (j == routes.size() - 1) {
+                        // Add the given destination location to the steps output
+                        var lastTrack = trackSections.get(trackSections.size() - 1);
+                        var newStep = new PathfindingResult.StepResult(lastTrack.edge, lastTrack.getEndPosition());
+                        res.addStep(newStep);
+                    }
                 }
             }
+            return new RsJson(new RsWithBody(adapterResult.toJson(res)));
+        } catch (Throwable ex) {
+            ex.printStackTrace(System.err);
+            throw ex;
         }
-        return new RsJson(new RsWithBody(adapterResult.toJson(res)));
     }
 
     @SuppressFBWarnings({"BC_UNCONFIRMED_CAST"})
