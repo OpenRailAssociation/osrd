54ee672a980b95ef4f62603bb9b2a7c403d27feb
Eloi Charpentier <--get>
2021-08-25 11:31:10 +0200

    tests: pathfinding: reproduce simple st-lo bug in a test

--- src/test/java/fr/sncf/osrd/api/PathfindingTest.java
+++ src/test/java/fr/sncf/osrd/api/PathfindingTest.java
@@ -2,27 +2,34 @@ package fr.sncf.osrd.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import fr.sncf.osrd.api.PathfindingEndpoint.PathfindingWaypoint;
 import fr.sncf.osrd.utils.graph.EdgeDirection;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.takes.rq.RqFake;
 import org.takes.rs.RsPrint;
 
 public class PathfindingTest extends ApiTest {
+    private static PathfindingWaypoint[] makeBidirectionalEndPoint(PathfindingWaypoint point) {
+        var waypointInverted = new PathfindingWaypoint(point.trackSection, point.offset, point.direction.opposite());
+        return new PathfindingWaypoint[]{point, waypointInverted};
+    }
+
     @Test
     public void simpleRoutes() throws Exception {
-        var waypointStart = new PathfindingEndpoint.PathfindingWaypoint(
+        var waypointStart = new PathfindingWaypoint(
                 "ne.micro.foo_b",
                 100,
                 EdgeDirection.START_TO_STOP
         );
-        var waypointsStart = new PathfindingEndpoint.PathfindingWaypoint[]{waypointStart};
-        var waypointEnd = new PathfindingEndpoint.PathfindingWaypoint(
+        var waypointEnd = new PathfindingWaypoint(
                 "ne.micro.bar_a",
                 100,
                 EdgeDirection.START_TO_STOP
         );
-        var waypointsEnd = new PathfindingEndpoint.PathfindingWaypoint[]{waypointEnd};
-        var waypoints = new PathfindingEndpoint.PathfindingWaypoint[2][];
+        var waypointsStart = makeBidirectionalEndPoint(waypointStart);
+        var waypointsEnd = makeBidirectionalEndPoint(waypointEnd);
+        var waypoints = new PathfindingWaypoint[2][];
         waypoints[0] = waypointsStart;
         waypoints[1] = waypointsEnd;
         var requestBody = PathfindingEndpoint.adapterRequest.toJson(
@@ -48,20 +55,84 @@ public class PathfindingTest extends ApiTest {
     }
 
     @Test
-    public void simpleTracks() throws Exception {
-        var waypointStart = new PathfindingEndpoint.PathfindingWaypoint(
-                "ne.micro.foo_b",
-                100,
-                EdgeDirection.START_TO_STOP
-        );
-        var waypointsStart = new PathfindingEndpoint.PathfindingWaypoint[]{waypointStart};
-        var waypointEnd = new PathfindingEndpoint.PathfindingWaypoint(
+    public void simpleRoutesInverted() throws Exception {
+        var waypointStart = new PathfindingWaypoint(
                 "ne.micro.bar_a",
                 100,
                 EdgeDirection.START_TO_STOP
         );
-        var waypointsEnd = new PathfindingEndpoint.PathfindingWaypoint[]{waypointEnd};
-        var waypoints = new PathfindingEndpoint.PathfindingWaypoint[2][];
+        var waypointEnd = new PathfindingWaypoint(
+                "ne.micro.foo_b",
+                100,
+                EdgeDirection.START_TO_STOP
+        );
+        var waypointsStart = makeBidirectionalEndPoint(waypointStart);
+        var waypointsEnd = makeBidirectionalEndPoint(waypointEnd);
+        var waypoints = new PathfindingWaypoint[2][];
+        waypoints[0] = waypointsStart;
+        waypoints[1] = waypointsEnd;
+        var requestBody = PathfindingEndpoint.adapterRequest.toJson(
+                new PathfindingEndpoint.PathfindingRequest(waypoints, "tiny_infra/infra.json"));
+
+        var result = new RsPrint(
+                new PathfindingRoutesEndpoint(infraHandlerMock).act(
+                        new RqFake("POST", "/pathfinding/routes", requestBody))
+        ).printBody();
+
+        var response = PathfindingRoutesEndpoint.adapterResult.fromJson(result);
+        assert response != null;
+    }
+
+    @org.junit.jupiter.params.ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void simpleRoutesSameEdge(boolean inverted) throws Exception {
+        var waypointStart = new PathfindingWaypoint(
+                "ne.micro.bar_a",
+                100,
+                EdgeDirection.START_TO_STOP
+        );
+        var waypointEnd = new PathfindingWaypoint(
+                "ne.micro.bar_a",
+                110,
+                EdgeDirection.START_TO_STOP
+        );
+        if (inverted) {
+            var tmp = waypointEnd;
+            waypointEnd = waypointStart;
+            waypointStart = tmp;
+        }
+        var waypointsStart = makeBidirectionalEndPoint(waypointStart);
+        var waypointsEnd = makeBidirectionalEndPoint(waypointEnd);
+        var waypoints = new PathfindingWaypoint[2][];
+        waypoints[0] = waypointsStart;
+        waypoints[1] = waypointsEnd;
+        var requestBody = PathfindingEndpoint.adapterRequest.toJson(
+                new PathfindingEndpoint.PathfindingRequest(waypoints, "tiny_infra/infra.json"));
+
+        var result = new RsPrint(
+                new PathfindingRoutesEndpoint(infraHandlerMock).act(
+                        new RqFake("POST", "/pathfinding/routes", requestBody))
+        ).printBody();
+
+        var response = PathfindingRoutesEndpoint.adapterResult.fromJson(result);
+        assert response != null;
+    }
+
+    @Test
+    public void simpleTracks() throws Exception {
+        var waypointStart = new PathfindingWaypoint(
+                "ne.micro.foo_b",
+                100,
+                EdgeDirection.START_TO_STOP
+        );
+        var waypointsStart = new PathfindingWaypoint[]{waypointStart};
+        var waypointEnd = new PathfindingWaypoint(
+                "ne.micro.bar_a",
+                100,
+                EdgeDirection.START_TO_STOP
+        );
+        var waypointsEnd = new PathfindingWaypoint[]{waypointEnd};
+        var waypoints = new PathfindingWaypoint[2][];
         waypoints[0] = waypointsStart;
         waypoints[1] = waypointsEnd;
         var requestBody = PathfindingRoutesEndpoint.adapterRequest.toJson(
