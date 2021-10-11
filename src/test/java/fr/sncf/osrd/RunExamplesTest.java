11f630e90df5338721d8ab6efd12ba5ad281133e
Eloi Charpentier <eloi.charpentier@epita.fr>
2021-10-11 16:24:15 +0200

    tests: add tests with new simulations
    train: fix deep clone of TrainState
    
    train: clean up disappear
    
    tests: disable two tests while waiting for reservation rework

--- src/test/java/fr/sncf/osrd/RunExamplesTest.java
+++ src/test/java/fr/sncf/osrd/RunExamplesTest.java
@@ -1,6 +1,8 @@
 package fr.sncf.osrd;
 
 import static fr.sncf.osrd.Helpers.*;
+import static fr.sncf.osrd.train.TrainStatus.REACHED_DESTINATION;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import fr.sncf.osrd.config.Config;
 import fr.sncf.osrd.infra.InvalidInfraException;
@@ -15,14 +17,17 @@ import fr.sncf.osrd.simulation.changelog.ArrayChangeLog;
 import fr.sncf.osrd.simulation.changelog.ChangeConsumer;
 import fr.sncf.osrd.simulation.changelog.ChangeConsumerMultiplexer;
 import fr.sncf.osrd.simulation.changelog.ChangeLogSummarizer;
+import fr.sncf.osrd.train.Train;
 import fr.sncf.osrd.train.events.TrainCreatedEvent;
 import okio.Buffer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class RunExamplesTest {
-    private static void runGivenConfigInfra(String path) throws InvalidRollingStock, InvalidSuccession,
+    private static void runGivenConfigInfra(String path, int nExpectedTrainsArrived)
+            throws InvalidRollingStock, InvalidSuccession,
             InvalidSchedule, IOException, InvalidInfraException, SimulationError {
         Config config = Config.readFromFile(getResourcePath(path));
 
@@ -45,23 +50,43 @@ public class RunExamplesTest {
         ChangeLogSummarizer.summarize(changelog);
 
         ChangeSerializer.serializeChangeLog(changelog, new Buffer());
+
+        var nTrainsArrived = sim.trains.values().stream()
+                .map(Train::getLastState)
+                .filter(trainState -> trainState.status.equals(REACHED_DESTINATION))
+                .count();
+        assertEquals(nExpectedTrainsArrived, nTrainsArrived);
     }
 
     @Test
     public void testTinyInfra() throws InvalidRollingStock, InvalidSuccession,
             InvalidSchedule, IOException, InvalidInfraException, SimulationError {
-        runGivenConfigInfra("tiny_infra/config_railjson.json");
+        runGivenConfigInfra("tiny_infra/config_railjson.json", 1);
     }
 
     @Test
     public void testCircularInfra() throws InvalidRollingStock, InvalidSuccession,
             InvalidSchedule, IOException, InvalidInfraException, SimulationError {
-        runGivenConfigInfra("circular_infra/config.json");
+        runGivenConfigInfra("circular_infra/config.json", 0);
     }
 
     @Test
     public void testSlowMaxSpeedInfra() throws InvalidRollingStock, InvalidSuccession,
             InvalidSchedule, IOException, InvalidInfraException, SimulationError {
-        runGivenConfigInfra("bug_slow_max_speed/config.json");
+        runGivenConfigInfra("bug_slow_max_speed/config.json", 2);
+    }
+
+    @Disabled("requires the ongoing route rework")
+    @Test
+    public void testConvergencesDelayed() throws InvalidRollingStock, InvalidSuccession,
+            InvalidSchedule, IOException, InvalidInfraException, SimulationError {
+        runGivenConfigInfra("bug_convergences/config_delayed.json", 3);
+    }
+
+    @Disabled("requires the ongoing route rework")
+    @Test
+    public void testConvergences() throws InvalidRollingStock, InvalidSuccession,
+            InvalidSchedule, IOException, InvalidInfraException, SimulationError {
+        runGivenConfigInfra("bug_convergences/config.json", 3);
     }
 }
