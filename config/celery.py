7f54fa451e4731640123963a7ca2a8ea30036690
Paul Guénézan <paul.guenezan@epita.fr>
2021-10-11 14:49:59 +0000

    new template

--- config/celery.py
+++ config/celery.py
@@ -1,27 +1,27 @@
 from celery import Celery
 from django.conf import settings
 
-app = Celery('osrd',
+app = Celery("osrd",
              broker=settings.BROKER_URL,
              backend=settings.BACKEND_URL,
-             include=['osrd.tasks'])
+             include=["osrd.tasks"])
 
-app.conf.broker_use_ssl = {'ssl_cert_reqs': 'none'}
-app.conf.redis_backend_use_ssl = {'ssl_cert_reqs': 'none'}
+app.conf.broker_use_ssl = {"ssl_cert_reqs": "none"}
+app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": "none"}
 
 app.conf.update(
     result_expires=3600
 )
 
-# Using a string here means the worker doesn't have to serialize
+# Using a string here means the worker doesn"t have to serialize
 # the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
+# - namespace="CELERY" means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
-if __name__ == '__main__':
-    print('starting celery worker')
+if __name__ == "__main__":
+    print("starting celery worker")
     app.start()
