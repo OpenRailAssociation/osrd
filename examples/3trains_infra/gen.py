ef3a20068ae58879e1c17893816bf956b59e0b50
giuliana barbarino <giuliana.barbarino@reseau.sncf.fr>
2021-10-20 12:11:08 +0200

    tests: add test TST change

--- examples/3trains_infra/gen.py
+++ examples/3trains_infra/gen.py
@@ -3,7 +3,7 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 grand_parent_dir = os.path.dirname(parent_dir)
 sys.path.insert(0, grand_parent_dir)
-import examples_generator.libgen as gen
+import libgen as gen
 
 L = 1000 * 2 ** -0.5
 
@@ -21,14 +21,13 @@ infra.set_buffer_stop_coordinates(5, 2 * L, 1000 + 2 * L)
 
 # build the trains
 sim = gen.Simulation(infra)
-sim.add_schedule(0, 4, 1)
+sim.add_schedule(0, 4, 0)
 sim.add_schedule(0, 0, 5)
 sim.add_schedule(0, 1, 6)
 
 # build the successions
-succession = gen.Succession()
-succession.add_table(2, 0, 1, [2, 0, 1])
-succession.add_table(2, 3, 6, [2, 0, 1])
-succession.add_table(3, 4, 5, [0, 1])
+sim.add_tst(2, 0, 1, [1, 2, 0])
+sim.add_tst(2, 3, 6, [1, 2, 0])
+sim.add_tst(3, 4, 5, [1, 0])
 
-gen.write_all_files(infra, sim, succession)
+gen.write_all_files(infra, sim)
