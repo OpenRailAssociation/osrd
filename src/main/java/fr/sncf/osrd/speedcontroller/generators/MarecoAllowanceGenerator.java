8469df72d3b1017146bb2f9d017f72c192f7c66c
eckter <eckter@users.noreply.github.com>
2021-11-05 11:45:16 +0100

    Reproduce mareco problem in unit test (#266)
    * test: reproduce mareco problem in unit test
    
    * ech/add-mareco-test: accelerating slopes are now calculated using a mean value
    
    * ech/add-mareco-test: fix bugs on slope under the train evaluation, fixing the new testEcoMarginShortSlopes
    
    Co-authored-by: Alex Rolland <alex.rolland@reseau.sncf.fr>

--- src/main/java/fr/sncf/osrd/speedcontroller/generators/MarecoAllowanceGenerator.java
+++ src/main/java/fr/sncf/osrd/speedcontroller/generators/MarecoAllowanceGenerator.java
@@ -210,6 +210,7 @@ public class MarecoAllowanceGenerator extends DichotomyControllerGenerator {
                 .collect(Collectors.toList());
     }
 
+    @SuppressWarnings({"checkstyle:WhitespaceAfter", "checkstyle:WhitespaceAround"})
     private ArrayList<AcceleratingSlope> findAcceleratingSlopes(
             SortedDoubleMap speeds, RollingStock rollingStock, double vf) {
 
@@ -217,13 +218,14 @@ public class MarecoAllowanceGenerator extends DichotomyControllerGenerator {
         double previousPosition = 0.0;
         double previousSpeed = 0.0;
         double previousAcceleration = 0.0;
+        double meanAcceleration = 0.0;
+        double accelerationLength = 0.0;
         var currentAcceleratingSlope = new AcceleratingSlope(0, 0, 0, 0, 0);
         for (var element : speeds.entrySet()) {
             double position = element.getKey();
             var location = convertPosition(schedule, sim, position);
             double speed = element.getValue();
-            var integrator =
-                    new TrainPhysicsIntegrator(TIME_STEP, rollingStock, location, speed);
+            var integrator = new TrainPhysicsIntegrator(TIME_STEP, rollingStock, location, speed);
             var naturalAcceleration = integrator.computeAcceleration(Action.coast());
             if (speed < vf || speed > previousSpeed) { // if v < vf or the train is accelerating, just update variables
                 previousAcceleration = naturalAcceleration;
@@ -231,24 +233,34 @@ public class MarecoAllowanceGenerator extends DichotomyControllerGenerator {
                 previousPosition = location.getPathPosition();
                 continue;
             }
-            // beginning of accelerating slope
-            if (naturalAcceleration > 0 && previousAcceleration < 0) {
-                currentAcceleratingSlope.acceleration = naturalAcceleration;
-                currentAcceleratingSlope.previousAcceleration = previousAcceleration;
-                currentAcceleratingSlope.beginPosition = location.getPathPosition();
-            } else if (naturalAcceleration < 0
-                    && previousAcceleration > 0
-                    && currentAcceleratingSlope.acceleration != 0) {
-                // end of accelerating slope
+            if (naturalAcceleration > 0) {
+                // beginning of accelerating slope
+                // add acceleration * distance step to the weighted mean acceleration
+                var distanceStep = location.getPathPosition() - previousPosition;
+                meanAcceleration += naturalAcceleration * distanceStep;
+                accelerationLength += distanceStep;
+                if (previousAcceleration < 0) {
+                    currentAcceleratingSlope.previousAcceleration = previousAcceleration;
+                    currentAcceleratingSlope.beginPosition = location.getPathPosition();
+                }
+            } else if (naturalAcceleration < 0 && accelerationLength > 0) {
+                // end the accelerating slope
                 currentAcceleratingSlope.endPosition = previousPosition;
                 currentAcceleratingSlope.targetSpeed = speed;
+                meanAcceleration /= accelerationLength;
+                currentAcceleratingSlope.acceleration = meanAcceleration;
                 res.add(currentAcceleratingSlope);
+                // reset meanAcceleration, accelerationLength, and accelerating slope
+                meanAcceleration = 0;
+                accelerationLength = 0;
                 currentAcceleratingSlope = new AcceleratingSlope(0, 0, 0, 0, 0);
             }
             previousAcceleration = naturalAcceleration;
             previousSpeed = speed;
             previousPosition = location.getPathPosition();
         }
+        if (accelerationLength > 0)
+            res.add(currentAcceleratingSlope);
         return res;
     }
 
