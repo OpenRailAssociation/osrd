53cbc04f19d6f466c95365a566a1a3738f617614
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-11-19 16:01:06 +0100

    core: infras: one_line: make some edges stop to start

--- core/examples/one_line/gen.py
+++ core/examples/one_line/gen.py
@@ -14,15 +14,23 @@ builder = InfraBuilder()
 
 # Create track sections
 tracks = [builder.add_track_section(length=1000) for _ in range(10)]
+for i, track in enumerate(tracks):
+    track.inverted = i % 2 == 1
 
 # Set coordinates
 for i, track in enumerate(tracks):
-    track.begin().set_coords(1000 * i, 0)
-    track.end().set_coords(1000 * (i + 1), 0)
+    if track.inverted:
+        track.begin().set_coords(1000 * i, 0)
+        track.end().set_coords(1000 * (i + 1), 0)
+    else:
+        track.end().set_coords(1000 * i, 0)
+        track.begin().set_coords(1000 * (i + 1), 0)
 
 # Add links
 for first_track, second_track in zip(tracks[:-1], tracks[1:]):
-    builder.add_link(first_track.end(), second_track.begin(), ApplicableDirection.BOTH)
+    first_endpoint = first_track.begin() if first_track.inverted else first_track.end()
+    second_endpoint = second_track.end() if second_track.inverted else second_track.begin()
+    builder.add_link(first_endpoint, second_endpoint, ApplicableDirection.BOTH)
 
 # Add detector and signals
 for track in tracks:
