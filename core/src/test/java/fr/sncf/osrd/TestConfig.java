71f1d8f149d5f7752d8790156e5bcf6e29a81337
Alex Rolland <alex.rolland@reseau.sncf.fr>
2021-11-23 11:59:48 +0100

    core: double_timestep: rebase construction_mareco

--- core/src/test/java/fr/sncf/osrd/TestConfig.java
+++ core/src/test/java/fr/sncf/osrd/TestConfig.java
@@ -43,7 +43,6 @@ public class TestConfig {
     public RJSSimulation rjsSimulation;
     public ChangeConsumer changeConsumer = null;
     public HashMap<String, RollingStock> extraRollingStocks;
-    public float timeStep = 1;
 
     private TestConfig(
             RJSInfra rjsInfra,
