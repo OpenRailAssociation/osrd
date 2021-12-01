0c4c3fa7f4545ced0d33facd08c9acfd315e943f
Florian Amsallem <florian.amsallem@epita.fr>
2021-12-01 17:52:17 +0100

    api: add railjson v2 schema and new models

--- api/osrd_infra/admin.py
+++ api/osrd_infra/admin.py
@@ -1,31 +1,29 @@
+from typing import Type
+
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from typing import Type
-from osrd_infra.models import (
-    # ecs
-    Entity,
-    Component,
-    get_entity_meta,
-    get_component_meta,
-    ALL_ENTITY_TYPES,
-    # misc
+
+from osrd_infra.models import (  # misc; timetable
+    MODEL_TO_OBJ,
+    AspectModel,
     Infra,
     Path,
+    RailScriptFunctionModel,
     RollingStock,
-    # timetable
+    Simulation,
     Timetable,
     TrainSchedule,
-    Simulation,
     TrainScheduleLabel,
 )
 
 
 @admin.register(Infra)
 class InfraAdmin(admin.ModelAdmin):
-    list_display = ("name", "namespace")
-    readonly_fields = ("namespace", "created", "modified")
+    list_display = ("name",)
+    readonly_fields = ("created", "modified")
 
 
+""" TODO: Remove or adapt
 def generate_inline(component: Type[Component]):
     component_meta = get_component_meta(component)
     attrs = {"model": component, "extra": 1}
@@ -35,9 +33,7 @@ def generate_inline(component: Type[Component]):
 
 
 def generate_inlines(model: Type[Entity]):
-    return tuple(
-        generate_inline(component) for component in get_entity_meta(model).components
-    )
+    return tuple(generate_inline(component) for component in get_entity_meta(model).components)
 
 
 def generate_entity_admin(model: Type[Entity]):
@@ -48,6 +44,7 @@ def generate_entity_admin(model: Type[Entity]):
 for entity_type in ALL_ENTITY_TYPES.values():
     entity_admin = generate_entity_admin(entity_type)
     admin.register(entity_type)(entity_admin)
+"""
 
 
 admin.site.register(
@@ -60,5 +57,8 @@ admin.site.register(
         TrainSchedule,
         Simulation,
         TrainScheduleLabel,
+        RailScriptFunctionModel,
+        AspectModel,
     ]
+    + [model for model in MODEL_TO_OBJ]
 )
--- api/osrd_infra/serializers.py
+++ api/osrd_infra/serializers.py
@@ -8,23 +8,15 @@ from rest_framework.serializers import (
 from rest_framework_gis.fields import GeometryField
 
 from osrd_infra.models import (
-    ALL_COMPONENT_TYPES,
-    ALL_ENTITY_TYPES,
-    ApplicableDirection,
-    ApplicableDirectionComponent,
-    Component,
-    Endpoint,
     Infra,
     Path,
     RollingStock,
     Timetable,
-    TrackSectionLinkComponent,
     TrainSchedule,
     TrainScheduleLabel,
-    get_component_meta,
-    get_entity_meta,
 )
 from osrd_infra.models.common import EnumSerializer
+from osrd_infra.models.schemas import ApplicableDirections, Endpoint
 
 
 # monkey patch rest_framework_gis so that it properly converts
@@ -43,63 +35,10 @@ def wrap_to_representation(original_to_representation):
 GeometryField.to_representation = wrap_to_representation(GeometryField.to_representation)
 
 
-class ComponentSerializer(ModelSerializer):
-    registry = {}
-
-    def __init_subclass__(cls, **kwargs):
-        model = cls.Meta.model
-        assert issubclass(model, Component), f"{model} isn't a subclass of Component"
-        cls.registry[cls.Meta.model] = cls
-        super().__init_subclass__(**kwargs)
-
-    def __init__(self, *args, omit_entity_id=False, **kwargs):
-        # Instantiate the superclass normally
-        super().__init__(*args, **kwargs)
-
-        assert "entity" in self.fields
-        assert "component_id" in self.fields
-
-        if omit_entity_id:
-            self.fields.pop("entity")
-
-
-def serialize_components(entity):
-    entity_meta = get_entity_meta(entity.get_concrete_type())
-
-    res = {}
-    for comp_model in entity_meta.components:
-        comp_meta = get_component_meta(comp_model)
-        comp_serializer = ComponentSerializer.registry[comp_model]
-        field = getattr(entity, comp_meta.related_name, None)
-        if not field:
-            continue
-        if comp_meta.unique:
-            serialized = comp_serializer(field, omit_entity_id=True).data
-        else:
-            serialized = [comp_serializer(related, omit_entity_id=True).data for related in field.all()]
-        res[comp_meta.name] = serialized
-    return res
-
-
-def serialize_entity(entity):
-    return {
-        "entity_id": entity.entity_id,
-        "entity_type": get_entity_meta(entity.get_concrete_type()).name,
-        "components": serialize_components(entity),
-    }
-
-
-class EntitySerializer(ModelSerializer):
-    components = SerializerMethodField("get_components")
-
-    def get_components(self, obj):
-        return serialize_components(obj)
-
-
 class InfraSerializer(ModelSerializer):
     class Meta:
         model = Infra
-        exclude = ["namespace"]
+        fields = "__all__"
 
 
 class RollingStockSerializer(ModelSerializer):
@@ -167,54 +106,3 @@ class TrainScheduleSerializer(ModelSerializer):
             "base_simulation_log",
             "eco_simulation_log",
         ]
-
-
-# COMPONENT SERIALIZERS
-
-
-class TrackSectionLinkComponentSerializer(ComponentSerializer):
-    begin_endpoint = EnumSerializer(Endpoint)
-    end_endpoint = EnumSerializer(Endpoint)
-
-    class Meta:
-        model = TrackSectionLinkComponent
-        fields = "__all__"
-
-
-class ApplicableDirectionComponentSerializer(ComponentSerializer):
-    applicable_direction = EnumSerializer(ApplicableDirection)
-
-    class Meta:
-        model = ApplicableDirectionComponent
-        fields = "__all__"
-
-
-# generate component serializers which weren't explicitly declared
-
-
-def generate_serializer(model, parent_class, extra_meta={}):
-    meta_attrs = {
-        "model": model,
-        **extra_meta,
-    }
-    meta = type("Meta", (), meta_attrs)
-    attrs = {"Meta": meta}
-    serializer_name = model.__name__ + "Serializer"
-    return type(serializer_name, (parent_class,), attrs)
-
-
-for component_type in ALL_COMPONENT_TYPES.values():
-    if component_type in ComponentSerializer.registry:
-        continue
-    serializer = generate_serializer(component_type, ComponentSerializer, {"fields": "__all__"})
-    globals()[serializer.__name__] = serializer
-
-
-# ENTITY SERIALIZERS
-
-ALL_ENTITY_SERIALIZERS = []
-
-for entity_type in ALL_ENTITY_TYPES.values():
-    serializer = generate_serializer(entity_type, EntitySerializer, {"fields": ["entity_id", "components"]})
-    ALL_ENTITY_SERIALIZERS.append(serializer)
-    globals()[serializer.__name__] = serializer
