251b15cac19728ba5749aa6f39e719c5afaf992e
Florian Amsallem <florian.amsallem@epita.fr>
2021-12-07 18:26:49 +0100

    api: adapt layer generation

--- api/config/test.py
+++ api/config/test.py
@@ -1,4 +1,4 @@
-from os import getenv, environ
+from os import environ, getenv
 
 # when developping, enable debug mode and skip authentication
 environ.setdefault("OSRD_DEV_SETUP", "1")
@@ -8,7 +8,6 @@ environ.setdefault("OSRD_SKIP_AUTH", "1")
 
 from config.settings import *  # noqa
 
-
 PSQL_DATABASE = getenv("PSQL_DATABASE", "osrd")
 PSQL_USERNAME = getenv("PSQL_USERNAME", "osrd")
 PSQL_PASSWORD = getenv("PSQL_PASSWORD", "password")
@@ -16,29 +15,29 @@ PSQL_HOST = getenv("PSQL_HOST", "localhost")
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': PSQL_DATABASE,
-        'USER': PSQL_USERNAME,
-        'PASSWORD': PSQL_PASSWORD,
-        'HOST': PSQL_HOST,
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": PSQL_DATABASE,
+        "USER": PSQL_USERNAME,
+        "PASSWORD": PSQL_PASSWORD,
+        "HOST": PSQL_HOST,
     },
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake',
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
     }
 }
 
-ROOT_PATH = ''
+ROOT_PATH = ""
 
-STATIC_URL = ROOT_PATH + '/static/'
+STATIC_URL = ROOT_PATH + "/static/"
 
 INSTALLED_APPS.append("corsheaders")
-MIDDLEWARE.append('corsheaders.middleware.CorsMiddleware')
+MIDDLEWARE.append("corsheaders.middleware.CorsMiddleware")
 CORS_ORIGIN_ALLOW_ALL = True
 
-CHARTIS_URL = getenv("CHARTIS_URL", "http://localhost:7000/chartis/")
+CHARTIS_URL = getenv("CHARTIS_URL", "http://localhost:7000")
 CHARTIS_TOKEN = getenv("CHARTIS_TOKEN", "")
