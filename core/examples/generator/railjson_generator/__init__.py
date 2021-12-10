949b81bb02ea9fb874e73f437fe2571da414464e
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-12-10 09:52:41 +0100

    core: generator: remove symlink

--- core/examples/generator/railjson_generator/__init__.py
+++ core/examples/generator/railjson_generator/__init__.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+schema_path = Path(__file__).parents[4] / "api" / "osrd_infra" / "models"
+sys.path.append(str(schema_path))
+
 from railjson_generator.infra_builder import InfraBuilder  # noqa
 from railjson_generator.simulation_builder import SimulationBuilder  # noqa
 from railjson_generator.schema.infra.direction import ApplicableDirection  # noqa
