6691f4bf9126a946e8085e260d89086172e24a30
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-12-10 09:29:46 +0100

    core: railjson: remove unused elements from java schema

--- core/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackobjects/RJSTrainDetector.java
+++ core/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackobjects/RJSTrainDetector.java
@@ -5,8 +5,7 @@ import fr.sncf.osrd.railjson.schema.infra.RJSTrackSection;
 import fr.sncf.osrd.utils.graph.ApplicableDirection;
 
 public class RJSTrainDetector extends RJSRouteWaypoint {
-    public RJSTrainDetector(String id, ApplicableDirection applicableDirection,
-                            double position, RJSObjectRef<RJSTrackSection> track) {
-        super(id, applicableDirection, position, track);
+    public RJSTrainDetector(String id, double position, RJSObjectRef<RJSTrackSection> track) {
+        super(id, position, track);
     }
 }
