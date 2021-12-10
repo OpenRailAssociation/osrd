5cef9762f876eba1a8f759ebfff43928b69478a4
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2021-12-10 09:29:46 +0100

    core: railjson: remove unused elements from java schema

--- core/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackobjects/RJSRouteWaypoint.java
+++ core/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackobjects/RJSRouteWaypoint.java
@@ -1,25 +1,18 @@
 package fr.sncf.osrd.railjson.schema.infra.trackobjects;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fr.sncf.osrd.railjson.schema.common.Identified;
 import fr.sncf.osrd.railjson.schema.common.RJSObjectRef;
 import fr.sncf.osrd.railjson.schema.infra.RJSTrackSection;
-import fr.sncf.osrd.utils.graph.ApplicableDirection;
 import fr.sncf.osrd.utils.graph.IPointValue;
 
 public class RJSRouteWaypoint extends RJSTrackObject implements Identified, IPointValue<RJSRouteWaypoint> {
     public String id;
 
-    @SuppressFBWarnings("URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-    public ApplicableDirection direction;
-
-    RJSRouteWaypoint(String id, ApplicableDirection applicableDirection,
-                     double position, RJSObjectRef<RJSTrackSection> track) {
+    RJSRouteWaypoint(String id, double position, RJSObjectRef<RJSTrackSection> track) {
         super(track, position);
         this.id = id;
         this.track = track;
         this.position = position;
-        this.direction = applicableDirection;
     }
 
     @Override
