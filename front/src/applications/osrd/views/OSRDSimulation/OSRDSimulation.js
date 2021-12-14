0e7c60394bfd0959e5119dbf04b49a3e231b0038
Nicolas Wurtz <gitlab@nethenic.net>
2021-12-14 09:05:03 +0100

    front: choose projection path from contextmenu

--- front/src/applications/osrd/views/OSRDSimulation/OSRDSimulation.js
+++ front/src/applications/osrd/views/OSRDSimulation/OSRDSimulation.js
@@ -84,7 +84,6 @@ const OSRDSimulation = () => {
             path: selectedProjection.path,
           },
         );
-        console.log(simulationLocal);
         simulationLocal.sort((a, b) => a.base.stops[0].time > b.base.stops[0].time);
         dispatch(updateSimulation({ trains: simulationLocal }));
         // Create margins settings for each train if not set
