5bbcb6d932981b1f4bfc3ea72bc5b402f0b814fd
Florian Amsallem <florian.amsallem@epita.fr>
2021-12-17 10:31:44 +0100

    load_database: fix hardcoded infra id

--- load-database.sh
+++ load-database.sh
@@ -11,4 +11,4 @@ fi
 docker cp "$1" osrd-postgres:/backup
 # restoring the backend can partialy fail, and that's sometimes ok
 docker exec osrd-postgres pg_restore -d osrd -x -c /backup || true
-docker exec osrd-api python manage.py generate_layers 24 27 28 29
+docker exec osrd-api python manage.py generate_layers
