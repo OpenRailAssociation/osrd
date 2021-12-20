da4f7b43423efb5e786bf546e883f30b007c6812
Florian Amsallem <florian.amsallem@epita.fr>
2021-12-20 14:12:50 +0100

    api: add import railjson endpoint

--- api/osrd_infra/views/infra.py
+++ api/osrd_infra/views/infra.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.core.cache import cache
 from django.http import HttpResponse
-from rest_framework import mixins
+from rest_framework import mixins, status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from osrd_infra.models import Infra
 from osrd_infra.serializers import InfraSerializer
-from osrd_infra.views.railjson import railjson_serialize_infra
+from osrd_infra.views.railjson import import_infra, serialize_infra
 
 
 class InfraView(
@@ -23,18 +24,18 @@ class InfraView(
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user.sub)
 
-    @action(detail=True, methods=["get"])
-    def railjson(self, request, pk=None):
+    @action(url_path="railjson", detail=True, methods=["get"])
+    def get_railjson(self, request, pk=None):
         cache_key = f"osrd.infra.{pk}"
-        infra = cache.get(cache_key)
-        if infra is not None:
-            return HttpResponse(infra, content_type="application/json")
-        infra = railjson_serialize_infra(self.get_object())
-        cache.set(cache_key, infra, timeout=settings.CACHE_TIMEOUT)
-        return HttpResponse(infra, content_type="application/json")
+        railjson = cache.get(cache_key)
+        if railjson is not None:
+            return HttpResponse(railjson, content_type="application/json")
+        railjson = serialize_infra(self.get_object())
+        cache.set(cache_key, railjson, timeout=settings.CACHE_TIMEOUT)
+        return HttpResponse(railjson, content_type="application/json")
 
-    """TODO: Fix with new models
-    @action(detail=True, methods=["post"])
-    def edit(self, request, pk=None):
-        return edit_infra(self.get_object(), request.data)
-    """
+    @action(url_path="railjson", detail=False, methods=["post"])
+    def post_railjson(self, request):
+        infra_name = request.query_params.get("name", "unknown")
+        infra = import_infra(request.data, infra_name)
+        return Response({"id": infra.id}, status=status.HTTP_201_CREATED)
