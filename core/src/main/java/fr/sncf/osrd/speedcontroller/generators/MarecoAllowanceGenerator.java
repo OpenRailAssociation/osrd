ba768b222aea94783f30ae53a344aeddb91621a7
Kylian Meulin <59285425+KikiManjaro@users.noreply.github.com>
2022-01-06 10:32:40 +0100

    Fix imports of MarecoAllowanceGenerator

--- core/src/main/java/fr/sncf/osrd/speedcontroller/generators/MarecoAllowanceGenerator.java
+++ core/src/main/java/fr/sncf/osrd/speedcontroller/generators/MarecoAllowanceGenerator.java
@@ -3,7 +3,6 @@ package fr.sncf.osrd.speedcontroller.generators;
 import static fr.sncf.osrd.train.TrainPhysicsIntegrator.nextStep;
 import static java.lang.Math.abs;
 import static java.util.Collections.max;
-
 import fr.sncf.osrd.railjson.schema.schedule.RJSAllowance.MarginType;
 import fr.sncf.osrd.simulation.SimulationError;
 import fr.sncf.osrd.train.RollingStock;
@@ -17,10 +16,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static fr.sncf.osrd.train.TrainPhysicsIntegrator.nextStep;
-import static java.lang.Math.abs;
-import static java.util.Collections.max;
 
 public class MarecoAllowanceGenerator extends DichotomyControllerGenerator {
 
