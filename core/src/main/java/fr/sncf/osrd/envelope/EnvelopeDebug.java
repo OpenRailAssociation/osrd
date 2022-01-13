1e07dc5f6d0fc966285ed61e3f4bf073f2fc4f3a
Victor "multun" Collod <victor.collod@epita.fr>
2022-01-13 21:42:48 +0100

    core: envelope: add the ability to plot multiple envelopes

--- core/src/main/java/fr/sncf/osrd/envelope/EnvelopeDebug.java
+++ core/src/main/java/fr/sncf/osrd/envelope/EnvelopeDebug.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class EnvelopeDebug {
@@ -25,14 +26,10 @@ public class EnvelopeDebug {
         }
     }
 
-    /** Creates a plot panel for this envelope */
-    public static JPanel plotPanel(Envelope envelope) {
-        // create your PlotPanel (you can use it as a JPanel)
-        Plot2DPanel plot = new Plot2DPanel();
-
+    private static void plotEnvelope(Plot2DPanel plot, Envelope envelope, String envelopeName) {
         // add a line plot to the PlotPanel
         for (int i = 0; i < envelope.size(); i++) {
-            var lineName = String.format("part %d", i);
+            var lineName = String.format("%s.parts[%d]", envelopeName, i);
             var part = envelope.get(i);
             var positions = part.clonePositions();
             var speeds = part.cloneSpeeds();
@@ -45,11 +42,56 @@ public class EnvelopeDebug {
             }
             plot.addLinePlot(lineName, positions, speeds);
         }
+    }
+
+    /** Creates a plot panel for this envelope */
+    public static JPanel plotPanel(Envelope envelope) {
+        // create your PlotPanel (you can use it as a JPanel)
+        Plot2DPanel plot = new Plot2DPanel();
+
+        // add a line plot to the PlotPanel
+        plotEnvelope(plot, envelope, "");
+
         return plot;
     }
 
     /** Shows an interactive plot of the envelope */
     public static void plot(Envelope envelope) {
         SwingUtils.debugPanel("Envelope", () -> plotPanel(envelope));
+    }
+
+    /** Enables showing multiple envelopes on the same graph:
+     * <pre>
+     *   EnvelopeDebug.plotBuilder()
+     *           .add(doubleCappedEnvelope, "double_capped")
+     *           .add(cappedEnvelope, "capped")
+     *           .plot();
+     * </pre>
+     */
+    public static PlotBuilder plotBuilder() {
+        return new PlotBuilder();
+    }
+
+    /** A tool to plot multiple envelopes in the same graph */
+    public static final class PlotBuilder {
+        private final ArrayList<Envelope> envelopes = new ArrayList<>();
+        private final ArrayList<String> names = new ArrayList<>();
+
+        /** Add an envelope to the plot */
+        public PlotBuilder add(Envelope envelope, String name) {
+            envelopes.add(envelope);
+            names.add(name);
+            return this;
+        }
+
+        /** Build and show the plot */
+        public void plot() {
+            SwingUtils.debugPanel("Envelope", () -> {
+                var plot = new Plot2DPanel();
+                for (int i = 0; i < envelopes.size(); i++)
+                    plotEnvelope(plot, envelopes.get(i), names.get(i));
+                return plot;
+            });
+        }
     }
 }
