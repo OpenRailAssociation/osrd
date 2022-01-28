fa33e2704d362a88dcfc9e409c82e02c5a7bc53a
Florian Amsallem <florian.amsallem@epita.fr>
2022-01-28 14:00:04 +0100

    tests: fix using standalone simulations

--- tests/tests/run_simulation.py
+++ tests/tests/run_simulation.py
@@ -11,25 +11,28 @@ def run_simulation(base_url, infra_id):
     path_id = run_pathfinding(base_url, infra_id)
     rolling_stock_id = get_rolling_stock(base_url)
     schedule_payload = make_payload_schedule(base_url, infra_id, path_id, rolling_stock_id)
-    r = requests.post(base_url + "train_schedule/", json=schedule_payload)
+    r = requests.post(base_url + "train_schedule/standalone_simulation/", json=schedule_payload)
     if r.status_code // 100 != 2:
         raise RuntimeError(f"Schedule error {r.status_code}: {r.content}, payload={json.dumps(schedule_payload)}")
 
-    schedule_id = r.json()["id"]
+    schedule_id = r.json()["ids"][0]
     return schedule_id
 
 
 def make_payload_schedule(base_url, infra, path, rolling_stock):
     return {
-        "train_name": "foo",
-        "labels": [],
-        "departure_time": 0,
-        "phases": [],
-        "margins": [],
-        "initial_speed": 0,
         "timetable": get_schedule(base_url, infra),
-        "rolling_stock": rolling_stock,
-        "path": path
+        "path": path,
+        "schedules": [
+            {
+                "train_name": "foo",
+                "labels": [],
+                "allowances": [],
+                "departure_time": 0,
+                "initial_speed": 0,
+                "rolling_stock": rolling_stock,
+            }
+        ],
     }
 
 
