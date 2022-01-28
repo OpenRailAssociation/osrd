f6deda950dba2ad1c707b43355cd4f7310dbd90d
giuliana barbarino <giuliana.barbarino@reseau.sncf.fr>
2022-01-28 18:00:06 +0100

    test: envelope: add tests with non constant deceleration

--- core/src/test/java/fr/sncf/osrd/envelope_sim/MaxEffortEnvelopeTest.java
+++ core/src/test/java/fr/sncf/osrd/envelope_sim/MaxEffortEnvelopeTest.java
@@ -4,18 +4,15 @@ import static fr.sncf.osrd.envelope.EnvelopeShape.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static fr.sncf.osrd.envelope_sim.MaxSpeedEnvelopeTest.TIME_STEP;
 
-import fr.sncf.osrd.envelope.Envelope;
-import fr.sncf.osrd.envelope.EnvelopePart;
-import fr.sncf.osrd.envelope.EnvelopeShape;
-import fr.sncf.osrd.envelope.EnvelopeTransitions;
+import fr.sncf.osrd.envelope.*;
 import fr.sncf.osrd.envelope_sim.pipelines.MaxEffortEnvelope;
 import fr.sncf.osrd.envelope_sim.pipelines.MaxSpeedEnvelope;
+import fr.sncf.osrd.train.RollingStock;
 import fr.sncf.osrd.train.TestTrains;
 import org.junit.jupiter.api.Test;
 
 public class MaxEffortEnvelopeTest {
-    private Envelope makeMaxEffortEnvelope(double slope) {
-        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN;
+    private Envelope makeMaxEffortEnvelope(RollingStock rollingStock, double slope) {
         var length = 10000;
         var testPath = new FlatPath(length, slope);
         var stops = new double[] { 6000, length };
@@ -23,23 +20,43 @@ public class MaxEffortEnvelopeTest {
         var flatMRSP = Envelope.make(
                 EnvelopePart.generateTimes(null, new double[] { 0, length }, new double[] { 44.4, 44.4})
         );
-        var maxSpeedEnvelope = MaxSpeedEnvelope.from(testRollingStock, testPath, stops, flatMRSP, TIME_STEP);
-        return MaxEffortEnvelope.from(testRollingStock, testPath, 0, maxSpeedEnvelope, TIME_STEP);
+        var maxSpeedEnvelope = MaxSpeedEnvelope.from(rollingStock, testPath, stops, flatMRSP, TIME_STEP);
+        return MaxEffortEnvelope.from(rollingStock, testPath, 0, maxSpeedEnvelope, TIME_STEP);
     }
 
     @Test
     public void testFlat() {
-        var maxEffortEnvelope = makeMaxEffortEnvelope(0);
+        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN;
+        var maxEffortEnvelope = makeMaxEffortEnvelope(testRollingStock, 0);
         EnvelopeShape.check(maxEffortEnvelope, INCREASING, CONSTANT, DECREASING, INCREASING, DECREASING);
         EnvelopeTransitions.checkPositions(maxEffortEnvelope, 1.0, 2730, 4198, 6000, 8392);
         assertTrue(maxEffortEnvelope.continuous);
     }
 
     @Test
+    public void testFlatNonConstDec() {
+        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN_MAX_DEC_TYPE;
+        var maxEffortEnvelope = makeMaxEffortEnvelope(testRollingStock, 0);
+        EnvelopeShape.check(maxEffortEnvelope, INCREASING, CONSTANT, DECREASING, INCREASING, CONSTANT, DECREASING);
+        EnvelopeTransitions.checkPositions(maxEffortEnvelope, 1.0, 2730, 5157, 6000, 8731, 9157);
+        assertTrue(maxEffortEnvelope.continuous);
+    }
+
+    @Test
     public void testSteep() {
-        var maxEffortEnvelope = makeMaxEffortEnvelope(20);
+        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN;
+        var maxEffortEnvelope = makeMaxEffortEnvelope(testRollingStock, 20);
         EnvelopeShape.check(maxEffortEnvelope, INCREASING, DECREASING, INCREASING, DECREASING);
         EnvelopeTransitions.checkPositions(maxEffortEnvelope, 1.0, 4499, 6000, 8924);
+        assertTrue(maxEffortEnvelope.continuous);
+    }
+
+    @Test
+    public void testSteepNonConstDec() {
+        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN_MAX_DEC_TYPE;
+        var maxEffortEnvelope = makeMaxEffortEnvelope(testRollingStock, 20);
+        EnvelopeShape.check(maxEffortEnvelope, INCREASING, DECREASING, INCREASING, DECREASING);
+        EnvelopeTransitions.checkPositions(maxEffortEnvelope, 1.0, 5355, 6000, 9532);
         assertTrue(maxEffortEnvelope.continuous);
     }
 }
--- core/src/test/java/fr/sncf/osrd/envelope_sim/MaxSpeedEnvelopeTest.java
+++ core/src/test/java/fr/sncf/osrd/envelope_sim/MaxSpeedEnvelopeTest.java
@@ -2,10 +2,7 @@ package fr.sncf.osrd.envelope_sim;
 
 import static fr.sncf.osrd.envelope.EnvelopeShape.*;
 
-import fr.sncf.osrd.envelope.Envelope;
-import fr.sncf.osrd.envelope.EnvelopePart;
-import fr.sncf.osrd.envelope.EnvelopeShape;
-import fr.sncf.osrd.envelope.EnvelopeTransitions;
+import fr.sncf.osrd.envelope.*;
 import fr.sncf.osrd.envelope_sim.pipelines.MaxSpeedEnvelope;
 import fr.sncf.osrd.train.TestTrains;
 import org.junit.jupiter.api.Test;
@@ -36,12 +33,26 @@ public class MaxSpeedEnvelopeTest {
         var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN;
         var length = 10000;
         var testPath = new FlatPath(length, 0);
-        var stops = new double[] { 0 };
+        var stops = new double[]{0};
+        var flatMRSP = Envelope.make(
+                EnvelopePart.generateTimes(null, new double[]{0, length}, new double[]{44.4, 44.4})
+        );
+        var maxSpeedEnvelope = MaxSpeedEnvelope.from(testRollingStock, testPath, stops, flatMRSP, TIME_STEP);
+        EnvelopeShape.check(maxSpeedEnvelope, CONSTANT);
+    }
 
+    @Test
+    public void testFlatNonConstDec() {
+        var testRollingStock = TestTrains.REALISTIC_FAST_TRAIN_MAX_DEC_TYPE;
+        var length = 10000;
+        var testPath = new FlatPath(length, 0);
+        var stops = new double[] { 8500 };
         var flatMRSP = Envelope.make(
                 EnvelopePart.generateTimes(null, new double[] { 0, length}, new double[] { 44.4, 44.4})
         );
         var maxSpeedEnvelope = MaxSpeedEnvelope.from(testRollingStock, testPath, stops, flatMRSP, TIME_STEP);
-        EnvelopeShape.check(maxSpeedEnvelope, CONSTANT);
+        EnvelopeShape.check(maxSpeedEnvelope, CONSTANT, DECREASING, CONSTANT);
+        EnvelopeTransitions.checkPositions(maxSpeedEnvelope, 1.0, 7657, 8500);
+        EnvelopeTransitions.checkContinuity(maxSpeedEnvelope, true, false);
     }
 }
