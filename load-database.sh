44d4a9c8c9c593fbaf99fca261dc03dbedf4ba28
Florian Amsallem <florian.amsallem@epita.fr>
2022-02-07 14:22:29 +0100

    load-database: add chartos dump

--- load-database.sh
+++ load-database.sh
@@ -3,12 +3,13 @@
 set -e
 
 # Check arguments
-if [ $# -ne 1 ]; then
-  echo "usage: $0 osrd.backup"
+if [ $# -ne 2 ]; then
+  echo "usage: $0 osrd.backup chartos.backup"
   exit 2
 fi
 
-docker cp "$1" osrd-postgres:/backup
+docker cp "$1" osrd-postgres:/backup-osrd
+docker cp "$2" osrd-postgres:/backup-chartos
 # restoring the backend can partialy fail, and that's sometimes ok
-docker exec osrd-postgres pg_restore -d osrd -x -c /backup || true
-docker exec osrd-api python manage.py generate_layers
+docker exec osrd-postgres pg_restore -d osrd -x -c /backup-osrd || true
+docker exec osrd-postgres pg_restore -d chartos -x -c /backup-chartos || true
