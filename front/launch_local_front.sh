77d8be051200020ae35905144a719a9fe0537f16
Alexandre Damiron <alexandredamiron@gmail.com>
2022-02-07 17:59:15 +0100

    Mise en forme et correction bug sur MapSearchSignal, ajout llimitation de dépendance sur react-overlay-error

--- front/launch_local_front.sh
+++ front/launch_local_front.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-REACT_APP_LOCAL_BACKEND=True REACT_APP_API_URL=http://localhost:8000 REACT_APP_CHARTIS_URL=http://localhost:7000 yarn start
+TSC_COMPILE_ON_ERROR=true REACT_APP_LOCAL_BACKEND=True REACT_APP_API_URL=http://localhost:8000 REACT_APP_CHARTIS_URL=http://localhost:7000 yarn start
