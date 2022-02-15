61c458a629aa4f230609a7694df7326d71471cd7
Alexandre Damiron <alexandredamiron@gmail.com>
2022-02-15 17:15:46 +0100

    merge and restore MapSignal Fix

--- front/src/applications/osrd/components/Simulation/enableInteractivity.js
+++ front/src/applications/osrd/components/Simulation/enableInteractivity.js
@@ -1,7 +1,11 @@
 import * as d3 from 'd3';
-import { store } from 'Store';
-import drawGuideLines from 'applications/osrd/components/Simulation/drawGuideLines';
-import { gridX, gridY, interpolateOnPosition } from 'applications/osrd/components/Helpers/ChartHelpers';
+
+import {
+  gridX,
+  gridY,
+  interpolateOnPosition,
+  interpolateOnTime,
+} from 'applications/osrd/components/Helpers/ChartHelpers';
 import {
   updateChartXGEV,
   updateContextMenu,
@@ -10,24 +14,33 @@ import {
   updateTimePositionValues,
 } from 'reducers/osrdsimulation';
 
+import { LIST_VALUES_NAME_SPACE_TIME } from 'applications/osrd/components/Simulation/consts';
+import drawGuideLines from 'applications/osrd/components/Simulation/drawGuideLines';
+import { store } from 'Store';
+
 export const displayGuide = (chart, opacity) => {
   chart.svg.selectAll('#vertical-line').style('opacity', opacity);
   chart.svg.selectAll('#horizontal-line').style('opacity', opacity);
   chart.svg.selectAll('.pointer').style('opacity', opacity);
 };
 
-export const updatePointers = (
-  chart, keyValues, listValues, positionValues, rotate,
-) => {
+export const updatePointers = (chart, keyValues, listValues, positionValues, rotate) => {
   listValues.forEach((name) => {
     if (positionValues[name]) {
-      chart.svg.selectAll(`#pointer-${name}`)
-        .attr('cx', (rotate
-          ? chart.x(positionValues[name][keyValues[1]])
-          : chart.x(positionValues[name][keyValues[0]])))
-        .attr('cy', (rotate
-          ? chart.y(positionValues[name][keyValues[0]])
-          : chart.y(positionValues[name][keyValues[1]])));
+      chart.svg
+        .selectAll(`#pointer-${name}`)
+        .attr(
+          'cx',
+          rotate
+            ? chart.x(positionValues[name][keyValues[1]])
+            : chart.x(positionValues[name][keyValues[0]])
+        )
+        .attr(
+          'cy',
+          rotate
+            ? chart.y(positionValues[name][keyValues[0]])
+            : chart.y(positionValues[name][keyValues[1]])
+        );
     }
   });
 };
@@ -43,18 +56,20 @@ const updateChart = (chart, keyValues, rotate) => {
     ? chart.y
     : d3.event.transform.rescaleY(chart.y); */
 
-  const newX = (d3.event.sourceEvent.shiftKey && rotate)
-    ? chart.x
-    : d3.event.transform.rescaleX(chart.x);
-  const newY = (d3.event.sourceEvent.shiftKey && !rotate)
-    ? chart.y
-    : d3.event.transform.rescaleY(chart.y);
+  const newX =
+    d3.event.sourceEvent.shiftKey && rotate ? chart.x : d3.event.transform.rescaleX(chart.x);
+  const newY =
+    d3.event.sourceEvent.shiftKey && !rotate ? chart.y : d3.event.transform.rescaleY(chart.y);
 
   // update axes with these new boundaries
-  const axisBottomX = !rotate && keyValues[0] === 'time'
-    ? d3.axisBottom(newX).tickFormat(d3.timeFormat('%H:%M:%S')) : d3.axisBottom(newX);
-  const axisLeftY = rotate && keyValues[0] === 'time'
-    ? d3.axisLeft(newY).tickFormat(d3.timeFormat('%H:%M:%S')) : d3.axisLeft(newY);
+  const axisBottomX =
+    !rotate && keyValues[0] === 'time'
+      ? d3.axisBottom(newX).tickFormat(d3.timeFormat('%H:%M:%S'))
+      : d3.axisBottom(newX);
+  const axisLeftY =
+    rotate && keyValues[0] === 'time'
+      ? d3.axisLeft(newY).tickFormat(d3.timeFormat('%H:%M:%S'))
+      : d3.axisLeft(newY);
   chart.xAxis.call(axisBottomX);
   chart.yAxis.call(axisLeftY);
 
@@ -62,25 +77,30 @@ const updateChart = (chart, keyValues, rotate) => {
   chart.yAxisGrid.call(gridY(newY, chart.width));
 
   // update lines & areas
-  chart.drawZone
-    .selectAll('.line')
-    .attr('d', d3.line()
-      .x((d) => newX((rotate ? d[keyValues[1]] : d[keyValues[0]])))
-      .y((d) => newY((rotate ? d[keyValues[0]] : d[keyValues[1]]))));
+  chart.drawZone.selectAll('.line').attr(
+    'd',
+    d3
+      .line()
+      .x((d) => newX(rotate ? d[keyValues[1]] : d[keyValues[0]]))
+      .y((d) => newY(rotate ? d[keyValues[0]] : d[keyValues[1]]))
+  );
 
-  chart.drawZone
-    .selectAll('.area')
-    .attr('d', (rotate
-      ? d3.area()
-        .y((d) => newY(d[keyValues[0]]))
-        .x0((d) => newX(d.value0))
-        .x1((d) => newX(d.value1))
-        .curve(keyValues[0] === 'time' ? d3.curveStepAfter : d3.curveLinear)
-      : d3.area()
-        .x((d) => newX(d[keyValues[0]]))
-        .y0((d) => newY(d.value0))
-        .y1((d) => newY(d.value1))
-        .curve(keyValues[0] === 'time' ? d3.curveStepAfter : d3.curveLinear)));
+  chart.drawZone.selectAll('.area').attr(
+    'd',
+    rotate
+      ? d3
+          .area()
+          .y((d) => newY(d[keyValues[0]]))
+          .x0((d) => newX(d.value0))
+          .x1((d) => newX(d.value1))
+          .curve(keyValues[0] === 'time' ? d3.curveStepAfter : d3.curveLinear)
+      : d3
+          .area()
+          .x((d) => newX(d[keyValues[0]]))
+          .y0((d) => newY(d.value0))
+          .y1((d) => newY(d.value1))
+          .curve(keyValues[0] === 'time' ? d3.curveStepAfter : d3.curveLinear)
+  );
 
   // OPERATIONNAL POINTS
   if (rotate) {
@@ -88,60 +108,85 @@ const updateChart = (chart, keyValues, rotate) => {
       .selectAll('#get-operationalPointsZone .op-line')
       .attr('x1', (d) => newX(d))
       .attr('x2', (d) => newX(d));
-    chart.drawZone
-      .selectAll('#get-operationalPointsZone .op-text')
-      .attr('x', (d) => newX(d));
+    chart.drawZone.selectAll('#get-operationalPointsZone .op-text').attr('x', (d) => newX(d));
     chart.drawZone
       .selectAll('#gev-operationalPointsZone .op-line')
       .attr('y1', (d) => newY(d))
       .attr('y2', (d) => newY(d));
-    chart.drawZone
-      .selectAll('#gev-operationalPointsZone .op-text')
-      .attr('y', (d) => newY(d));
+    chart.drawZone.selectAll('#gev-operationalPointsZone .op-text').attr('y', (d) => newY(d));
   } else {
     chart.drawZone
       .selectAll('#get-operationalPointsZone .op-line')
       .attr('y1', (d) => newY(d))
       .attr('y2', (d) => newY(d));
-    chart.drawZone
-      .selectAll('#get-operationalPointsZone .op-text')
-      .attr('y', (d) => newY(d));
+    chart.drawZone.selectAll('#get-operationalPointsZone .op-text').attr('y', (d) => newY(d));
     chart.drawZone
       .selectAll('#gev-operationalPointsZone .op-line')
       .attr('x1', (d) => newX(d))
       .attr('x2', (d) => newX(d));
-    chart.drawZone
-      .selectAll('#gev-operationalPointsZone .op-text')
-      .attr('x', (d) => newX(d));
+    chart.drawZone.selectAll('#gev-operationalPointsZone .op-text').attr('x', (d) => newX(d));
   }
 
-  chart.drawZone
-    .selectAll('.curve-label')
-    .attr('transform', d3.event.transform);
+  chart.drawZone.selectAll('.curve-label').attr('transform', d3.event.transform);
 
   chart.drawZone
     .selectAll('.conflictsPoints')
-    .attr('cx', (d) => newX((rotate ? d[keyValues[1]] : d[keyValues[0]])))
-    .attr('cy', (d) => newY((rotate ? d[keyValues[0]] : d[keyValues[1]])));
+    .attr('cx', (d) => newX(rotate ? d[keyValues[1]] : d[keyValues[0]]))
+    .attr('cy', (d) => newY(rotate ? d[keyValues[0]] : d[keyValues[1]]));
   return { newX, newY };
 };
 
 // Factorizes func to update VerticalLine on 3 charts: SpaceTime, SpeedSpaceChart, SpaceCurvesSlopes
 export const traceVerticalLine = (
-  chart, dataSimulation, keyValues, listValues,
-  positionValues, refValueName, rotate, timePosition,
+  chart,
+  dataSimulation,
+  keyValues,
+  listValues,
+  positionValues,
+  refValueName,
+  rotate,
+  timePosition
 ) => {
-  if (chart !== undefined
-    && d3.event === null) {
+  if (chart !== undefined && d3.event === null) {
     displayGuide(chart, 1);
     if (rotate) {
-      chart.svg.selectAll('#horizontal-line')
-        .attr('y1', chart.y(keyValues[0] !== 'time' && positionValues.speed ? positionValues.speed.position : timePosition))
-        .attr('y2', chart.y(keyValues[0] !== 'time' && positionValues.speed ? positionValues.speed.position : timePosition));
+      chart.svg
+        .selectAll('#horizontal-line')
+        .attr(
+          'y1',
+          chart.y(
+            keyValues[0] !== 'time' && positionValues.speed
+              ? positionValues.speed.position
+              : timePosition
+          )
+        )
+        .attr(
+          'y2',
+          chart.y(
+            keyValues[0] !== 'time' && positionValues.speed
+              ? positionValues.speed.position
+              : timePosition
+          )
+        );
     } else {
-      chart.svg.selectAll('#vertical-line')
-        .attr('x1', chart.x(keyValues[0] !== 'time' && positionValues.speed ? positionValues.speed.position : timePosition))
-        .attr('x2', chart.x(keyValues[0] !== 'time' && positionValues.speed ? positionValues.speed.position : timePosition));
+      chart.svg
+        .selectAll('#vertical-line')
+        .attr(
+          'x1',
+          chart.x(
+            keyValues[0] !== 'time' && positionValues.speed
+              ? positionValues.speed.position
+              : timePosition
+          )
+        )
+        .attr(
+          'x2',
+          chart.x(
+            keyValues[0] !== 'time' && positionValues.speed
+              ? positionValues.speed.position
+              : timePosition
+          )
+        );
     }
 
     updatePointers(chart, keyValues, listValues, positionValues, rotate);
@@ -149,16 +194,29 @@ export const traceVerticalLine = (
 };
 
 const enableInteractivity = (
-  chart, dataSimulation, dispatch, keyValues,
-  listValues, positionValues, rotate,
-  setChart, setYPosition, setZoomLevel, yPosition, zoomLevel,
+  chart,
+  dataSimulation,
+  dispatch,
+  keyValues,
+  listValues,
+  positionValues,
+  rotate,
+  setChart,
+  setYPosition,
+  setZoomLevel,
+  yPosition,
+  zoomLevel
 ) => {
   let newHoverPosition;
 
   let lastChartX;
-  const zoom = d3.zoom(newHoverPosition)
+  const zoom = d3
+    .zoom(newHoverPosition)
     .scaleExtent([0.5, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
-    .extent([[0, 0], [chart.width, chart.height]])
+    .extent([
+      [0, 0],
+      [chart.width, chart.height],
+    ])
     .on('zoom', () => {
       dispatch(updateContextMenu(undefined));
       // Permit zoom if shift pressed, if only move or if factor > .5
@@ -182,7 +240,7 @@ const enableInteractivity = (
       dispatch(updateMustRedraw(true));
     });
 
-  // ADN: to move in helpers
+  let debounceTimeoutId;
   function debounceUpdateTimePosition(timePositionLocal, interval) {
     clearTimeout(debounceTimeoutId);
     debounceTimeoutId = setTimeout(() => {
@@ -206,15 +264,12 @@ const enableInteractivity = (
         const timePositionLocal = rotate
           ? chart.y.invert(d3.mouse(d3.event.currentTarget)[1])
           : chart.x.invert(d3.mouse(d3.event.currentTarget)[0]);
-        // ADN do that after debounce
-        //dispatch(updateTimePosition(timePositionLocal));
 
-        //debounceUpdateTimePosition(timePositionLocal, 150);
         const immediatePositionsValues = interpolateOnTime(
           dataSimulation,
           keyValues,
           LIST_VALUES_NAME_SPACE_TIME,
-          timePositionLocal,
+          timePositionLocal
         );
 
         debounceUpdateTimePositionValues(timePositionLocal, immediatePositionsValues, 5);
@@ -227,15 +282,11 @@ const enableInteractivity = (
           : chart.x.invert(d3.mouse(d3.event.currentTarget)[0]);
         const timePositionLocal = interpolateOnPosition(dataSimulation, keyValues, positionLocal);
         if (timePositionLocal) {
-          // ADN do that after debounce
-          //dispatch(updateTimePosition(timePositionLocal));
-
-          //debounceUpdateTimePosition(timePositionLocal, 150);
           const immediatePositionsValues = interpolateOnTime(
             dataSimulation,
             keyValues,
             LIST_VALUES_NAME_SPACE_TIME,
-            timePositionLocal,
+            timePositionLocal
           );
 
           debounceUpdateTimePositionValues(timePositionLocal, immediatePositionsValues, 5);
@@ -244,10 +295,12 @@ const enableInteractivity = (
       }
 
       // Update guideLines
-      chart.svg.selectAll('#vertical-line')
+      chart.svg
+        .selectAll('#vertical-line')
         .attr('x1', d3.mouse(d3.event.currentTarget)[0])
         .attr('x2', d3.mouse(d3.event.currentTarget)[0]);
-      chart.svg.selectAll('#horizontal-line')
+      chart.svg
+        .selectAll('#horizontal-line')
         .attr('y1', d3.mouse(d3.event.currentTarget)[1])
         .attr('y2', d3.mouse(d3.event.currentTarget)[1]);
     }
