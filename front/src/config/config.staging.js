9d1591982c7b6b1beeec7b687e48b8fb6addbd89
Florian Amsallem <florian.amsallem@epita.fr>
2022-02-18 18:20:13 +0100

    front: fix staging config

--- front/src/config/config.staging.js
+++ front/src/config/config.staging.js
@@ -4,7 +4,7 @@ export const MAIN_API = {
     : 'https://gateway.staging.dgexsol.fr/osrd',
   proxy_chartis: process.env.REACT_APP_LOCAL_BACKEND
     ? `http://${window.location.hostname}:7000/chartis`
-    : 'https://gateway.staging.dgexsol.fr/chartis',
+    : 'https://gateway.staging.dgexsol.fr/chartos',
   version: '0.0.1.20200408.1',
   editor: {
     component_identifier: { database: 'gaia', name: 'Test' },
