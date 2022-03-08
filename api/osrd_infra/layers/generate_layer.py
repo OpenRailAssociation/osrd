6e3d21ef5454f0bbe5ee49296297595da222c232
Florian Amsallem <florian.amsallem@epita.fr>
2022-03-08 12:44:39 +0100

    api: generate signal layer

--- api/osrd_infra/layers/generate_layer.py
+++ api/osrd_infra/layers/generate_layer.py
@@ -1,27 +1,32 @@
-from dataclasses import dataclass
-from typing import Type
+import json
 
-from django.contrib.gis.geos import LineString
+from django.contrib.gis.geos import GEOSGeometry
 
-from osrd_infra.models import Infra, TrackSectionModel
-from osrd_infra.schemas.infra import TrackSection
+from osrd_infra.models import Infra, SignalModel, TrackSectionModel
 
 from .layer_creator import LayerCreator
 
 
-@dataclass
-class CachedTrackSection:
-    track: TrackSection
-    geo: LineString
-    sch: LineString
-
-
 def generate_layers(infra: Infra):
-    generate_generic_layer(infra, TrackSectionModel)
+    tracks = {}
+    for track in TrackSectionModel.objects.filter(infra=infra):
+        tracks[track.obj_id] = track.data
+
+    generate_track_sections(infra, tracks)
+    generate_signals(infra, tracks)
 
 
-def generate_generic_layer(infra: Infra, model_type: Type):
-    layer_name = model_type._meta.verbose_name_plural.replace(" ", "_")
-    with LayerCreator(layer_name, infra.id) as creator:
-        for entity in model_type.objects.filter(infra=infra):
-            creator.add(entity.data)
+def generate_track_sections(infra: Infra, tracks):
+    with LayerCreator("track_sections", infra.id) as creator:
+        creator.add_all(tracks.values())
+
+
+def generate_signals(infra: Infra, tracks):
+    with LayerCreator("signals", infra.id) as creator:
+        for signal in SignalModel.objects.filter(infra=infra):
+            track = tracks[signal.data["track"]["id"]]
+            normalized_pos = signal.data["position"]
+            geo = json.loads(GEOSGeometry(json.dumps(track["geo"])).interpolate_normalized(normalized_pos).json)
+            sch = json.loads(GEOSGeometry(json.dumps(track["sch"])).interpolate_normalized(normalized_pos).json)
+            signal_data = {"geo": geo, "sch": sch, **signal.data}
+            creator.add(signal_data)
