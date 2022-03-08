232104cbac0f8bdf7385ddaf2b9b1080b0d764ea
Florian Amsallem <florian.amsallem@epita.fr>
2022-03-08 17:16:51 +0100

    editoast: init rocket webserver

--- editoast/src/main.rs
+++ editoast/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+mod client;
+mod views;
+
+use clap::Parser;
+use client::{Client, RunserverCommand};
+
+#[rocket::main]
+async fn main() {
+    let client = Client::parse();
+    match client {
+        Client::Runserver(args) => runserver(args).await.expect("An error occured..."),
+    };
+}
+
+async fn runserver(command: RunserverCommand) -> Result<(), rocket::Error> {
+    let config = rocket::Config::figment().merge(("port", command.port));
+    let root_url = if command.root_url.starts_with('/') {
+        command.root_url
+    } else {
+        String::from("/") + command.root_url.as_str()
+    };
+
+    rocket::custom(config)
+        .mount(root_url, views::routes())
+        .launch()
+        .await
 }
