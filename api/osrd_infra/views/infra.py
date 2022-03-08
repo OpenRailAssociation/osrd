524f4ef2d06f730cced252146a1380be0da19b68
Florian Amsallem <florian.amsallem@epita.fr>
2022-03-08 12:44:09 +0100

    api: stop using cache for infra serialization

--- api/osrd_infra/views/infra.py
+++ api/osrd_infra/views/infra.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.core.cache import cache
 from django.http import HttpResponse
 from rest_framework import mixins, status
 from rest_framework.decorators import action
@@ -7,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from osrd_infra.models import Infra
-from osrd_infra.schemas.infra import RAILJSON_VERSION
 from osrd_infra.serializers import InfraSerializer
 from osrd_infra.views.railjson import import_infra, serialize_infra
 
@@ -27,22 +24,7 @@ class InfraView(
 
     @action(url_path="railjson", detail=True, methods=["get"])
     def get_railjson(self, request, pk=None):
-        infra = self.get_object()
-        # Check version
-        if infra.railjson_version != RAILJSON_VERSION:
-            return Response(
-                f"Invalid infra version expected: '{RAILJSON_VERSION}' got '{infra.railjson_version}'", status=500
-            )
-
-        # Check redis cache
-        cache_key = f"osrd.infra.{pk}"
-        railjson = cache.get(cache_key)
-        if railjson is not None:
-            return HttpResponse(railjson, content_type="application/json")
-
-        # Serialize infra
-        railjson = serialize_infra(infra)
-        cache.set(cache_key, railjson, timeout=settings.CACHE_TIMEOUT)
+        railjson = serialize_infra(self.get_object())
         return HttpResponse(railjson, content_type="application/json")
 
     @action(url_path="railjson", detail=False, methods=["post"])
