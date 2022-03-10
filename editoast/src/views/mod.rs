eee213c53505fd2fdbfb8c460d1d6edfe7b36d9a
Florian Amsallem <florian.amsallem@epita.fr>
2022-03-10 18:05:41 +0100

    editoast: add generate speed section layer

--- editoast/src/views/mod.rs
+++ editoast/src/views/mod.rs
@@ -17,3 +17,23 @@ async fn infra_list(connection: DBConnection) -> Json<Vec<Infra>> {
         .map(Json)
         .expect("An error occurred")
 }
+
+#[post("/infra/<infra>")]
+async fn edit_infra(infra: u32, connection: DBConnection) -> Result<String, Custom<String>> {
+    // Retrieve infra
+    let infra = match connection
+        .run(move |c| Infra::retrieve(c, infra as i32))
+        .await
+    {
+        Ok(infra) => infra,
+        Err(error) => match error {
+            InfraError::NotFound(_) => return Err(Custom(Status::NotFound, error.to_string())),
+            e => return Err(Custom(Status::InternalServerError, e.to_string())),
+        },
+    };
+    // Parse json body
+    // Apply modifications
+    // Refresh layers
+    // Check for warnings and errors
+    Ok(format!("OK! {:?}", infra))
+}
