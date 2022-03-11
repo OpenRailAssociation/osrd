8c75b0c22bf690973bd0d0c7ad4adb6edd16eaaa
Thibault Guisnet <thibault.guisnet@epita.fr>
2022-03-11 16:31:27 +0100

    editoast: add update sql scripts

--- editoast/src/models/track_section_layer.rs
+++ editoast/src/models/track_section_layer.rs
@@ -1,7 +1,7 @@
 use crate::schema::osrd_infra_tracksectionlayer;
 use crate::schema::osrd_infra_tracksectionlayer::dsl::*;
 use diesel::result::Error;
-use diesel::sql_types::Integer;
+use diesel::sql_types::{Integer, Text};
 use diesel::{delete, prelude::*, sql_query};
 use rocket::serde::Serialize;
 
@@ -24,6 +24,18 @@ impl TrackSectionLayer {
     pub fn generate(conn: &PgConnection, infra: i32) -> Result<usize, Error> {
         sql_query(include_str!("sql/generate_track_section_layer.sql"))
             .bind::<Integer, _>(infra)
+            .execute(conn)
+    }
+
+    pub fn update(conn: &PgConnection, infra: i32, obj_ids: &Vec<String>) -> Result<usize, Error> {
+        let obj_ids = obj_ids.join(",");
+        sql_query("DELETE FROM osrd_infra_tracksectionlayer WHERE infra_id = $1 AND obj_id in ($2)")
+            .bind::<Integer, _>(infra)
+            .bind::<Text, _>(&obj_ids)
+            .execute(conn)?;
+        sql_query(include_str!("sql/update_track_section_layer.sql"))
+            .bind::<Integer, _>(infra)
+            .bind::<Text, _>(&obj_ids)
             .execute(conn)
     }
 }
