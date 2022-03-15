fa0875f7be508207c1596efa55812b09c2b0a379
Thibault Guisnet <thibault.guisnet@epita.fr>
2022-03-15 18:21:46 +0100

    editoast: add tests

--- editoast/src/client/postgres_config.rs
+++ editoast/src/client/postgres_config.rs
@@ -14,6 +14,16 @@ pub struct PostgresConfig {
     pub psql_port: u16,
 }
 
+impl Default for PostgresConfig {
+    fn default() -> Self {
+        Self { psql_database: "osrd".to_string(),
+        psql_username: "osrd".to_string(),
+        psql_password: "password".to_string(),
+        psql_host: "localhost".to_string(),
+        psql_port: 5432 }
+    }
+}
+
 impl PostgresConfig {
     pub fn url(&self) -> String {
         format!(
