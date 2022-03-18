29d67f78590a97760fc474c167419c9508691912
Thibault Guisnet <thibault.guisnet@epita.fr>
2022-03-18 19:16:29 +0100

    editoast: handle error for Operations

--- editoast/src/railjson/operation/mod.rs
+++ editoast/src/railjson/operation/mod.rs
@@ -5,11 +5,14 @@ mod update;
 use crate::infra_cache::InfraCache;
 
 use super::{ObjectType, TrackSection};
+use crate::response::ApiError;
 pub use create::CreateOperation;
 pub use delete::DeleteOperation;
+use diesel::result::Error as DieselError;
 use diesel::PgConnection;
 use rocket::serde::Deserialize;
 use std::collections::{HashMap, HashSet};
+use thiserror::Error;
 pub use update::UpdateOperation;
 
 #[derive(Clone, Deserialize)]
@@ -23,8 +26,33 @@ pub enum Operation {
     Delete(DeleteOperation),
 }
 
+#[derive(Debug, Error)]
+pub enum OperationError {
+    // To modify
+    #[error("Tracksection '{0}', could not be found")]
+    NotFound(String),
+    #[error("An internal diesel error occurred: '{}'", .0.to_string())]
+    Other(DieselError),
+}
+
+impl ApiError for OperationError {
+    fn get_code(&self) -> u16 {
+        match self {
+            OperationError::NotFound(_) => 404,
+            OperationError::Other(_) => 500,
+        }
+    }
+
+    fn get_type(&self) -> &'static str {
+        match self {
+            OperationError::NotFound(_) => "editoast:operation:NotFound",
+            OperationError::Other(_) => "editoast:operation:Other",
+        }
+    }
+}
+
 impl Operation {
-    pub fn apply(&self, infra_id: i32, conn: &PgConnection) {
+    pub fn apply(&self, infra_id: i32, conn: &PgConnection) -> Result<(), Box<dyn ApiError>> {
         match self {
             Operation::Delete(delete) => delete.apply(infra_id, conn),
             Operation::Create(create) => create.apply(infra_id, conn),
