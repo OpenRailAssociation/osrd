77220c34068c8246e06d9d659b9f60794d842776
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2022-03-28 17:49:50 +0200

    core: new-infra: add switch branch to track route range path

--- core/src/main/java/fr/sncf/osrd/new_infra/api/tracks/directed/DiTrackInfra.java
+++ core/src/main/java/fr/sncf/osrd/new_infra/api/tracks/directed/DiTrackInfra.java
@@ -2,6 +2,7 @@ package fr.sncf.osrd.new_infra.api.tracks.directed;
 
 import com.google.common.graph.ImmutableNetwork;
 import fr.sncf.osrd.new_infra.api.Direction;
+import fr.sncf.osrd.new_infra.api.tracks.undirected.TrackEdge;
 import fr.sncf.osrd.new_infra.api.tracks.undirected.TrackInfra;
 
 public interface DiTrackInfra extends TrackInfra {
@@ -11,4 +12,8 @@ public interface DiTrackInfra extends TrackInfra {
     /** Returns the DiTrackEdge matching the TrackSection with the given ID, throws if it can't be found.
      * Returns the edge going the same way as the TrackInfra if dir == Forward, otherwise the opposite. */
     DiTrackEdge getEdge(String id, Direction direction);
+
+    /** Returns the DiTrackEdge matching the given edge, throws if it can't be found.
+     * Returns the edge going the same way if dir == Forward, otherwise the opposite. */
+    DiTrackEdge getEdge(TrackEdge edge, Direction direction);
 }
