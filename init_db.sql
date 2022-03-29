5ca80f4c9210fff34ab84b4a47f88729debfb33f
Florian Amsallem <florian.amsallem@epita.fr>
2022-03-29 11:16:45 +0200

    api: remove chartos database from init_db

--- init_db.sql
+++ init_db.sql
@@ -1,12 +1,7 @@
-create user chartos password 'password';
 create user osrd password 'password';
-create database chartos;
 create database osrd;
 
 grant all privileges on database osrd to osrd;
-grant all privileges on database chartos to chartos;
 
-\c chartos
-create extension postgis;
 \c osrd
 create extension postgis;
