0c0bffd28daea469f795a1d6c89c5ed6aac5573f
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2022-05-06 17:44:47 +0200

    core: examples: added cross and double cross switches

--- core/examples/generated/lib/railjson_generator/__init__.py
+++ core/examples/generated/lib/railjson_generator/__init__.py
@@ -5,10 +5,10 @@ schema_path = Path(__file__).parents[5] / "api" / "osrd_infra" / "schemas"
 assert schema_path.exists()
 sys.path.append(str(schema_path))
 
-from railjson_generator.infra_builder import InfraBuilder  # noqa
-from railjson_generator.simulation_builder import SimulationBuilder  # noqa
-from railjson_generator.schema.infra.direction import ApplicableDirection  # noqa
-from railjson_generator.schema.location import Location  # noqa
+from .infra_builder import InfraBuilder  # noqa
+from .simulation_builder import SimulationBuilder  # noqa
+from .schema.infra.direction import ApplicableDirection  # noqa
+from .schema.location import Location  # noqa
 
 def get_output_dir() -> Path:
     return Path(sys.argv[1])
