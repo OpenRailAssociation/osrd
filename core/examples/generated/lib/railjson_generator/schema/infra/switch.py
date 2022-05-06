746882825dfad7e88adb89b1a5ac8f5e88cba0d2
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2022-05-06 17:44:47 +0200

    core: examples: added cross and double cross switches

--- core/examples/generated/lib/railjson_generator/schema/infra/switch.py
+++ core/examples/generated/lib/railjson_generator/schema/infra/switch.py
@@ -10,26 +10,57 @@ def _switch_id():
     Switch._INDEX += 1
     return res
 
-
 @dataclass
 class Switch:
-    base: TrackEndpoint
-    left: TrackEndpoint
-    right: TrackEndpoint
+    _INDEX = 0
+    # overriden by subclasses
+    PORT_NAMES = [] 
+    SWITCH_TYPE = ""
+
     label: str = field(default_factory=_switch_id)
     delay: float = field(default=0)
 
-    _INDEX = 0
-
     def set_coords(self, x: float, y: float):
-        for endpoint in (self.base, self.left, self.right):
-            endpoint.set_coords(x, y)
+        for port_name in self.PORT_NAMES:
+            getattr(self, port_name).set_coords(x, y)
 
     def to_rjs(self):
         return infra.Switch(
             id=self.label,
-            switch_type=infra.ObjectReference(id="classic_switch", type="SwitchType"),
+            switch_type=infra.ObjectReference(id=self.SWITCH_TYPE, type="SwitchType"),
             group_change_delay=self.delay,
-            ports={port: getattr(self, port).to_rjs() for port in ("base", "left", "right")},
+            ports={
+                port_name: getattr(self, port_name).to_rjs() 
+                for port_name in self.PORT_NAMES
+            },
             label=self.label
         )
+
+@dataclass
+class PointSwitch(Switch):
+    base: TrackEndpoint = None
+    left: TrackEndpoint = None
+    right: TrackEndpoint = None
+
+    PORT_NAMES = ["base", "left", "right"] 
+    SWITCH_TYPE = "point"
+    
+@dataclass
+class CrossSwitch(Switch):
+    north: TrackEndpoint = None
+    south: TrackEndpoint = None
+    east: TrackEndpoint = None
+    west: TrackEndpoint = None
+
+    PORT_NAMES = ["north", "south", "east", "west"] 
+    SWITCH_TYPE = "cross"
+    
+@dataclass
+class DoubleCrossSwitch(Switch):
+    north_1: TrackEndpoint = None
+    north_2: TrackEndpoint = None
+    south_1: TrackEndpoint = None
+    south_2: TrackEndpoint = None
+
+    PORT_NAMES = ["north_1", "north_2", "south_1", "south_2"] 
+    SWITCH_TYPE = "double_cross"
