c51ed0813c4f9a5ddc282a0ab8801e6cc354ffcd
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2022-05-11 11:01:01 +0200

    core: examples: rebuilt script, added signals + detectors

--- core/examples/generated/lib/railjson_generator/schema/infra/switch.py
+++ core/examples/generated/lib/railjson_generator/schema/infra/switch.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass, field
+from typing import Dict, Tuple
 
-from railjson_generator.schema.infra.endpoint import TrackEndpoint
-
+from railjson_generator.schema.infra.endpoint import TrackEndpoint, Endpoint
+from railjson_generator.schema.infra.direction import Direction
 import infra
+
+# Reference distances, in meters
+SIGNAL_TO_SWITCH = 200
+DETECTOR_TO_SWITCH = 180
 
 
 def _switch_id():
@@ -10,11 +15,12 @@ def _switch_id():
     Switch._INDEX += 1
     return res
 
+
 @dataclass
 class Switch:
     _INDEX = 0
     # overriden by subclasses
-    PORT_NAMES = [] 
+    PORT_NAMES = []
     SWITCH_TYPE = ""
 
     label: str = field(default_factory=_switch_id)
@@ -24,17 +30,49 @@ class Switch:
         for port_name in self.PORT_NAMES:
             getattr(self, port_name).set_coords(x, y)
 
+    def add_signals_detectors_to_ports(self, ports: Dict[str, Tuple[str, str]]):
+        """Add signals and detectors to given ports.
+        Args:
+            ports: A dictionary of port names to (detector_label, signal_label) pairs.
+        """
+        for port_name in self.PORT_NAMES:
+            if port_name not in ports:
+                continue
+            detector_label, signal_label = ports[port_name]
+            port: TrackEndpoint = getattr(self, port_name)
+            track_section = port.track_section
+
+            if port.endpoint == Endpoint.BEGIN:
+                detector_position = DETECTOR_TO_SWITCH
+                signal_position = SIGNAL_TO_SWITCH
+                signal_direction = Direction.STOP_TO_START
+            else:
+                detector_position = track_section.length - DETECTOR_TO_SWITCH
+                signal_position = track_section.length - SIGNAL_TO_SWITCH
+                signal_direction = Direction.START_TO_STOP
+
+            detector = track_section.add_detector(
+                label=detector_label, position=detector_position
+            )
+            track_section.add_signal(
+                label=signal_label,
+                position=signal_position,
+                direction=signal_direction,
+                linked_detector=detector,
+            )
+
     def to_rjs(self):
         return infra.Switch(
             id=self.label,
             switch_type=infra.ObjectReference(id=self.SWITCH_TYPE, type="SwitchType"),
             group_change_delay=self.delay,
             ports={
-                port_name: getattr(self, port_name).to_rjs() 
+                port_name: getattr(self, port_name).to_rjs()
                 for port_name in self.PORT_NAMES
             },
-            label=self.label
+            label=self.label,
         )
+
 
 @dataclass
 class PointSwitch(Switch):
@@ -42,9 +80,10 @@ class PointSwitch(Switch):
     left: TrackEndpoint = None
     right: TrackEndpoint = None
 
-    PORT_NAMES = ["base", "left", "right"] 
+    PORT_NAMES = ["base", "left", "right"]
     SWITCH_TYPE = "point"
-    
+
+
 @dataclass
 class CrossSwitch(Switch):
     north: TrackEndpoint = None
@@ -52,9 +91,10 @@ class CrossSwitch(Switch):
     east: TrackEndpoint = None
     west: TrackEndpoint = None
 
-    PORT_NAMES = ["north", "south", "east", "west"] 
+    PORT_NAMES = ["north", "south", "east", "west"]
     SWITCH_TYPE = "cross"
-    
+
+
 @dataclass
 class DoubleCrossSwitch(Switch):
     north_1: TrackEndpoint = None
@@ -62,5 +102,5 @@ class DoubleCrossSwitch(Switch):
     south_1: TrackEndpoint = None
     south_2: TrackEndpoint = None
 
-    PORT_NAMES = ["north_1", "north_2", "south_1", "south_2"] 
+    PORT_NAMES = ["north_1", "north_2", "south_1", "south_2"]
     SWITCH_TYPE = "double_cross"
