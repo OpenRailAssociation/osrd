a6febc52a9ef0a97e10d1194866cce165ae64ff3
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2022-05-12 10:55:53 +0200

    core: infra loading: add warning for broken routes

--- core/src/main/java/fr/sncf/osrd/infra/api/tracks/undirected/TrackLocation.java
+++ core/src/main/java/fr/sncf/osrd/infra/api/tracks/undirected/TrackLocation.java
@@ -1,3 +1,9 @@
 package fr.sncf.osrd.infra.api.tracks.undirected;
 
-public record TrackLocation(TrackSection track, double offset) {}
+public record TrackLocation(TrackSection track, double offset) {
+
+    /** Returns true if the tracks are equal, and the offset difference is lower than the given tolerance */
+    public boolean equalsWithTolerance(TrackLocation other, double tolerance) {
+        return other.track.equals(track) && Math.abs(offset - other.offset()) < tolerance;
+    }
+}
