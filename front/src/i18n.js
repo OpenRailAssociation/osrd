4b60b7867df117aef29ecc276d047d689e5d40cc
Alexandre Damiron <alexandredamiron@gmail.com>
2022-05-18 18:20:24 +0200

    remove debug trigger

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -1,8 +1,8 @@
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 // not like to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
@@ -20,8 +20,9 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'fr',
-    debug: true,
-
+    debug: false,
+    supportedLngs: ['fr'],
+    load: 'fr',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
