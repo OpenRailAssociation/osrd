c085e3e07a3260e4ed55207a7052bfd0025b3702
Alexis Jacomy <alexis.jacomy@gmail.com>
2022-06-16 15:33:37 +0200

    editor: drafts selection tool left panel

--- front/src/applications/editor/tools/selection/types.ts
+++ front/src/applications/editor/tools/selection/types.ts
@@ -5,6 +5,7 @@ export type SelectionState = CommonToolState & {
   selectionState:
     | { type: 'single' }
     | { type: 'polygon'; polygonPoints: [number, number][] }
-    | { type: 'rectangle'; rectangleTopLeft: [number, number] | null };
+    | { type: 'rectangle'; rectangleTopLeft: [number, number] | null }
+    | { type: 'edition'; previousState?: SelectionState['selectionState'] };
   selection: EditorEntity[];
 };
