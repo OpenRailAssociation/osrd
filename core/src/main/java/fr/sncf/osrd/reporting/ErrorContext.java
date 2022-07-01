87fbbea4a0ce782d8063db341a4d53dc24b8934f
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2022-07-01 11:30:47 +0200

    core: fix warnings by adding serial UID

--- core/src/main/java/fr/sncf/osrd/reporting/ErrorContext.java
+++ core/src/main/java/fr/sncf/osrd/reporting/ErrorContext.java
@@ -2,8 +2,10 @@ package fr.sncf.osrd.reporting;
 
 import com.squareup.moshi.Json;
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory;
+import java.io.Serial;
+import java.io.Serializable;
 
-public class ErrorContext {
+public class ErrorContext implements Serializable {
 
     public static final PolymorphicJsonAdapterFactory<ErrorContext> adapter = (
             PolymorphicJsonAdapterFactory.of(ErrorContext.class, "type")
@@ -11,8 +13,12 @@ public class ErrorContext {
                     .withSubtype(Allowance.class, "allowance")
                     .withSubtype(Signal.class, "signal")
     );
+    @Serial
+    private static final long serialVersionUID = 2059829018182790086L;
 
     public static class Train extends ErrorContext {
+        @Serial
+        private static final long serialVersionUID = 806035086782570303L;
         public final String id;
 
         public Train(String id) {
@@ -21,6 +27,8 @@ public class ErrorContext {
     }
 
     public static class Allowance extends ErrorContext {
+        @Serial
+        private static final long serialVersionUID = 6574267112831336260L;
         public final int index;
 
         public Allowance(int index) {
@@ -29,6 +37,8 @@ public class ErrorContext {
     }
 
     public static class Signal extends ErrorContext {
+        @Serial
+        private static final long serialVersionUID = 1307122869428730982L;
         @Json(name = "signal_id")
         public final String signalID;
 
