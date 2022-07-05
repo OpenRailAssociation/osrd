95c9f36e6967102216078810702f5bb158b8e23a
Florian Amsallem <florian.amsallem@epita.fr>
2022-07-05 17:51:50 +0200

    front: fix docker & config

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,7 +1,7 @@
 export const MAIN_API = {
   proxy: process.env.REACT_APP_API_URL || 'https://gateway.dev.dgexsol.fr/osrd',
   proxy_chartis: process.env.REACT_APP_CHARTIS_URL || 'https://gateway.dev.dgexsol.fr/chartos',
-  proxy_editoast: process.env.REACT_APP_EDITOAST_URL || 'https://gateway.dev.dgexsol.fr/chartos',
+  proxy_editoast: process.env.REACT_APP_EDITOAST_URL || 'https://gateway.dev.dgexsol.fr/editoast',
   version: '0.0.1.20200408.1',
   editor: {
     component_identifier: { database: 'gaia', name: 'Test' },
