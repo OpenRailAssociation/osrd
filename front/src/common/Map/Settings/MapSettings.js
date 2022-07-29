07c5d375520f4f9552c496280c3b9652d71d8190
Nicolas Wurtz <6003856+nicolaswurtz@users.noreply.github.com>
2022-07-29 13:11:35 +0200

    Nwz/speedlimitbytag (#1562)
    * front: corrected speed_limit name & adding speedlimittag reducer setting
    
    * front: added speed limits by tag, first try !
    
    * front: speed limits by tag ok
    
    Co-authored-by: Nicolas Wurtz <gitlab@nethenic.net>

--- front/src/common/Map/Settings/MapSettings.js
+++ front/src/common/Map/Settings/MapSettings.js
@@ -5,6 +5,7 @@ import MapSettingsMapStyle from 'common/Map/Settings/MapSettingsMapStyle';
 import MapSettingsShowOSM from 'common/Map/Settings/MapSettingsShowOSM';
 import MapSettingsShowOSMtracksections from 'common/Map/Settings/MapSettingsShowOSMtracksections';
 import MapSettingsSignals from 'common/Map/Settings/MapSettingsSignals';
+import MapSettingsSpeedLimits from 'common/Map/Settings/MapSettingsSpeedLimits';
 import MapSettingsTrackSources from 'common/Map/Settings/MapSettingsTrackSources';
 import { useTranslation } from 'react-i18next';
 
@@ -25,10 +26,12 @@ export default function MapSettings(props) {
       <MapSettingsShowOSM />
       <div className="my-1" />
       <MapSettingsShowOSMtracksections />
-      <div className="mb-1 mt-3 border-bottom">Signalisation</div>
+      <div className="mb-1 mt-3 border-bottom">{t('map-settings:signalisation')}</div>
       <MapSettingsSignals />
       <div className="mb-1 mt-3 border-bottom">{t('map-settings:layers')}</div>
       <MapSettingsLayers />
+      <div className="mb-1 mt-3 border-bottom">{t('map-settings:speedlimits')}</div>
+      <MapSettingsSpeedLimits />
       <div className="mt-2 d-flex flex-row-reverse w-100">
         <button className="btn btn-secondary btn-sm" type="button" onClick={toggleMapSettings}>
           {t('translation:common.close')}
