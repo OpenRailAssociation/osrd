af66ca95ea663fc3f362c283f2e51f768b9f406c
Alexandre Damiron <alexandredamiron@gmail.com>
2022-08-04 19:20:09 +0200

    chang mode on wrapper

--- front/src/applications/stdcm/views/OSRDSTDCM.js
+++ front/src/applications/stdcm/views/OSRDSTDCM.js
@@ -1,9 +1,9 @@
 import React, {useEffect}  from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
 import { MODES } from '../../osrd/consts';
 import OSRDConfig from '../../osrd/views/OSRDConfig/OSRDConfig';
 import { updateMode } from '../../../reducers/osrdconf';
+import { useDispatch } from 'react-redux';
 
 export default function OSRDSTDCM() {
 
