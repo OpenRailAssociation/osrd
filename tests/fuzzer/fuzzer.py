7827f2186019b9ce21467ebdc6796be97fc71bae
Eloi Charpentier <eloi.charpentier@epita.fr>
2022-08-16 13:41:07 +0200

    tests: fuzzer: adapt to api change

--- tests/fuzzer/fuzzer.py
+++ tests/fuzzer/fuzzer.py
@@ -118,7 +118,7 @@ def run(
                     print(json.dumps(e.args[0]), file=f)
 
 
-def get_random_rolling_stock(base_url: str) -> str:
+def get_random_rolling_stock(base_url: str) -> int:
     """
     Returns a random rolling stock ID
     :param base_url: Api url
@@ -129,7 +129,7 @@ def get_random_rolling_stock(base_url: str) -> str:
         raise RuntimeError(f"Rolling stock error {r.status_code}: {r.content}")
     stocks = r.json()["results"]
     rolling_stock = random.choice(stocks)
-    return rolling_stock["name"]
+    return rolling_stock["id"]
 
 
 def format_route_node(waypoint_id, direction):
@@ -356,7 +356,7 @@ def make_random_allowances(path_length: float) -> List[Dict]:
     return res
 
 
-def make_payload_schedule(base_url: str, infra: int, path: int, rolling_stock: str, path_length: float) -> Dict:
+def make_payload_schedule(base_url: str, infra: int, path: int, rolling_stock: int, path_length: float) -> Dict:
     """
     Makes the payload for the simulation
     :param base_url: Api URL
