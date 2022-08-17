a53511ad93f5d8dd7320476303d8c6a160c4fea3
Eloi Charpentier <eloi.charpentier@epita.fr>
2022-08-17 17:44:47 +0200

    tests: add stdcm integration tests with a full timetable

---
+++ tests/tests/utils/schedule.py
@@ -0,0 +1,20 @@
+from tests.get_timetable import get_timetable
+
+
+def make_payload_schedule(base_url, infra, path, rolling_stock, departure_time=0, timetable=None):
+    if timetable is None:
+        timetable = get_timetable(base_url, infra)
+    return {
+        "timetable": timetable,
+        "path": path,
+        "schedules": [
+            {
+                "train_name": "foo",
+                "labels": [],
+                "allowances": [],
+                "departure_time": departure_time,
+                "initial_speed": 0,
+                "rolling_stock": rolling_stock,
+            }
+        ],
+    }
