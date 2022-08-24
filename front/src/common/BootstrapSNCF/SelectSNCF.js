45c4a91d26d6844891e426dbb0f1659bebde846e
Nicolas Wurtz <gitlab@nethenic.net>
2022-08-24 18:34:18 +0200

    front: SelectSNCF hooked & simplified

--- front/src/common/BootstrapSNCF/SelectSNCF.js
+++ front/src/common/BootstrapSNCF/SelectSNCF.js
@@ -1,91 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import { MAP_MODES } from 'common/Map/const';
 import PropTypes from 'prop-types';
 
-export default class SelectSNCF extends Component {
-    static propTypes = {
-      id: PropTypes.string.isRequired,
-      title: PropTypes.string,
-      name: PropTypes.string,
-      options: PropTypes.array.isRequired,
-      selectedValue: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.string,
-      ]).isRequired,
-      onChange: PropTypes.func.isRequired,
-      labelKey: PropTypes.string,
-      selectStyle: PropTypes.string,
-      mode: PropTypes.string,
-      sm: PropTypes.bool,
-      // type: pt
-    }
+const renderOptions = (options, labelKey, selectedValue) => options.map((option) => {
+  if (typeof option === 'string') {
+    return (
+      <option
+        key={option}
+        value={option}
+        selected={option === selectedValue}
+      >
+        {option}
+      </option>
+    );
+  }
+  return (
+    <option
+      key={option.id || option.key}
+      value={JSON.stringify(option)}
+      selected={JSON.stringify(option) === JSON.stringify(selectedValue)}
+    >
+      {option[labelKey] || `${option.lastName} ${option.firstName}`}
+    </option>
+  );
+});
 
-    static defaultProps = {
-      title: '',
-      labelKey: 'name',
-      selectStyle: '',
-      name: '',
-      mode: '',
-      type: 'improved',
-      sm: false,
-    }
+export default function SelectSNCF(props) {
+  const {
+    id, title, name, options, selectedValue, onChange, labelKey, selectStyle, mode, sm,
+  } = props;
 
-    renderOptions = (options, labelKey, selectedValue) => options.map((option) => {
-      if (typeof option === 'string') {
-        return (
-          <option
-            key={option}
-            value={option}
-            selected = {option === selectedValue}
-          >
-            {option}
-          </option>
-        );
-      }
-      return (
-        <option
-          key={option.id || option.key}
-          value={JSON.stringify(option)}
-          selected = {JSON.stringify(option) === JSON.stringify(selectedValue)}
-        >
-          {option[labelKey] || `${option.lastName} ${option.firstName}`}
-        </option>
-      );
-    })
-
-    render() {
-      const {
-        id, title, name, options, selectedValue, onChange, labelKey, selectStyle, mode, sm,
-      } = this.props;
-
-      return (
-        <>
-          <label htmlFor={id}>{title}</label>
-          {mode === MAP_MODES.modification
-            ? (
-              <select
-                id={id}
-                name={name}
-                value={typeof selectedValue === 'string' ? selectedValue : JSON.stringify(selectedValue)}
-                onChange={onChange}
-                className={`${selectStyle} ${sm && 'sm'}`}
-              >
-                {this.renderOptions(options, labelKey, selectedValue)}
-              </select>
-            )
-            : (
-              <select
-                id={id}
-                name={name}
-                defaultValue={typeof selectedValue === 'string' ? selectedValue : JSON.stringify(selectedValue)}
-                onChange={onChange}
-                className={`${selectStyle} ${sm && 'sm'}`}
-              >
-                {this.renderOptions(options, labelKey, selectedValue)}
-              </select>
-            )}
-        </>
-      );
-    }
+  return (
+    <>
+      <label htmlFor={id}>{title}</label>
+      <select
+        id={id}
+        name={name}
+        defaultValue={typeof selectedValue === 'string' ? selectedValue : JSON.stringify(selectedValue)}
+        onChange={onChange}
+        className={`${selectStyle} ${sm && 'sm'}`}
+      >
+        {renderOptions(options, labelKey, selectedValue)}
+      </select>
+    </>
+  );
 }
+
+SelectSNCF.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  name: PropTypes.string,
+  options: PropTypes.array.isRequired,
+  selectedValue: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.string,
+  ]).isRequired,
+  onChange: PropTypes.func.isRequired,
+  labelKey: PropTypes.string,
+  selectStyle: PropTypes.string,
+  mode: PropTypes.string,
+  sm: PropTypes.bool,
+};
+
+SelectSNCF.defaultProps = {
+  title: '',
+  labelKey: 'name',
+  selectStyle: '',
+  name: '',
+  mode: '',
+  sm: false,
+};
