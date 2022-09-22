74d87c649f96110ac271eaf7e95045dd69b67ffe
Valentin Chanas <anisometropie@gmail.com>
2022-09-22 14:18:52 +0200

    no index in key

--- front/src/applications/osrd/components/Itinerary/DisplayVias.js
+++ front/src/applications/osrd/components/Itinerary/DisplayVias.js
@@ -51,60 +51,63 @@ export default function DisplayVias(props) {
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef}>
             {osrdconf.vias.map((place, index) => (
-              <Draggable key={`drag-key-${index}`} draggableId={`drag-vias-${index}`} index={index}>
-                    {(providedDraggable) => (
+              <Draggable
+                key={`drag-key-${place.id}`}
+                draggableId={`drag-vias-${index}`}
+                index={index}
+              >
+                {(providedDraggable) => (
+                  <div
+                    ref={providedDraggable.innerRef}
+                    {...providedDraggable.draggableProps}
+                    {...providedDraggable.dragHandleProps}
+                    className="d-flex align-items-center w-100 osrd-config-place osrd-config-place-via"
+                  >
+                    <i className="text-info icons-itinerary-bullet mr-2" />
+                    <div className="pl-1 hover w-100 d-flex align-items-center">
                       <div
-                        ref={providedDraggable.innerRef}
-                        {...providedDraggable.draggableProps}
-                        {...providedDraggable.dragHandleProps}
-                        className="d-flex align-items-center w-100 osrd-config-place osrd-config-place-via"
+                        className="flex-grow-1"
+                        onClick={() =>
+                          zoomToFeaturePoint(place.clickLngLat, place.id, place.source)
+                        }
+                        role="button"
+                        tabIndex={0}
                       >
-                        <i className="text-info icons-itinerary-bullet mr-2" />
-                        <div className="pl-1 hover w-100 d-flex align-items-center">
-                          <div
-                            className="flex-grow-1"
-                            onClick={() =>
-                              zoomToFeaturePoint(place.clickLngLat, place.id, place.source)
-                            }
-                            role="button"
-                            tabIndex={0}
-                          >
-                            <small className="font-weight-bold text-muted mr-1">{index + 1}</small>
-                            <small className="mr-1 text-nowrap">
-                              {`${place.name || `KM ${Math.round(place.position) / 1000}`}`}
-                            </small>
-                          </div>
-                          <div
-                            className="osrd-config-stoptime"
-                            role="button"
-                            tabIndex="-1"
-                            onClick={() => setIndexSelected(index)}
-                          >
-                            {index === indexSelected ? (
-                              <InputStopTime index={index} />
-                            ) : (
-                              <>
-                                {osrdconf.vias[index].duration ? osrdconf.vias[index].duration : 0}
-                                <i className="ml-2 icons-pencil" />
-                              </>
-                            )}
-                          </div>
-                          <button
-                            className="btn btn-sm btn-only-icon btn-white ml-auto"
-                            type="button"
-                            onClick={() => dispatch(deleteVias(index))}
-                          >
-                            <i className="icons-circle-delete" />
-                            <span className="sr-only" aria-hidden="true">
-                              Delete
-                            </span>
-                          </button>
-                        </div>
+                        <small className="font-weight-bold text-muted mr-1">{index + 1}</small>
+                        <small className="mr-1 text-nowrap">
+                          {`${place.name || `KM ${Math.round(place.position) / 1000}`}`}
+                        </small>
                       </div>
-                    )}
-                  </Draggable>
-                )
-            )}
+                      <div
+                        className="osrd-config-stoptime"
+                        role="button"
+                        tabIndex="-1"
+                        onClick={() => setIndexSelected(index)}
+                      >
+                        {index === indexSelected ? (
+                          <InputStopTime index={index} />
+                        ) : (
+                          <>
+                            {osrdconf.vias[index].duration ? osrdconf.vias[index].duration : 0}
+                            <i className="ml-2 icons-pencil" />
+                          </>
+                        )}
+                      </div>
+                      <button
+                        className="btn btn-sm btn-only-icon btn-white ml-auto"
+                        type="button"
+                        onClick={() => dispatch(deleteVias(index))}
+                      >
+                        <i className="icons-circle-delete" />
+                        <span className="sr-only" aria-hidden="true">
+                          Delete
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
             {provided.placeholder}
           </div>
         )}
