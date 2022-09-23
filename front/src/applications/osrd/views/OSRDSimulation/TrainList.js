3cc89da1514a129ed73fd8fdf29e5d462c1482dd
Valentin Chanas <anisometropie@gmail.com>
2022-09-23 13:25:17 +0200

    warnings

--- front/src/applications/osrd/views/OSRDSimulation/TrainList.js
+++ front/src/applications/osrd/views/OSRDSimulation/TrainList.js
@@ -118,12 +118,13 @@ export default function TrainsList(props) {
             <td>
               <div className="cell-inner">
                 <div className="custom-control custom-checkbox custom-checkbox-alone">
-                  <label htmlFor={`timetable-train-${idx}`} className="custom-control-label" />
                   <input
                     type="checkbox"
                     className="custom-control-input"
                     id={`timetable-train-${idx}`}
                   />
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label className="custom-control-label" htmlFor={`timetable-train-${idx}`} />
                 </div>
               </div>
             </td>
@@ -271,6 +272,7 @@ export default function TrainsList(props) {
                         className="custom-control-input"
                         id="timetable-sel-all-trains"
                       />
+                      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
                       <label className="custom-control-label" htmlFor="timetable-sel-all-trains">
                         <span className="sr-only">Select all</span>
                       </label>
