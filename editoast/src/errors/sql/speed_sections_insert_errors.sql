5b8bac6f9e27518d8ba1e6682f2deeb8d777e56d
Florian Amsallem <florian.amsallem@epita.fr>
2022-09-27 18:55:48 +0200

    editoast: adapt errors new format

--- editoast/src/errors/sql/speed_sections_insert_errors.sql
+++ editoast/src/errors/sql/speed_sections_insert_errors.sql
@@ -1,21 +1,16 @@
 WITH errors AS (
-    SELECT unnest($2) AS speed_id,
-        unnest($3) AS information
+    SELECT unnest($2) AS information
 )
 INSERT INTO osrd_infra_errorlayer (
         infra_id,
-        obj_id,
-        obj_type,
         geographic,
         schematic,
         information
     )
 SELECT $1 AS infra_id,
-    errors.speed_id AS obj_id,
-    'SpeedSection' AS obj_type,
     speeds.geographic,
     speeds.schematic,
     errors.information
 FROM errors
-    LEFT JOIN osrd_infra_speedsectionlayer AS speeds ON speeds.obj_id = errors.speed_id
+    LEFT JOIN osrd_infra_speedsectionlayer AS speeds ON speeds.obj_id = information->>'obj_id'
     AND speeds.infra_id = $1
