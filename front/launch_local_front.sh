16f655a0edf903c52e23f8f46b315dece538f7a0
Theo Macron <theo.macron0315@gmail.com>
2022-09-29 12:14:54 +0200

    docker: change chartis and front port from 7000 to 7070 for mac m1 and m2

--- front/launch_local_front.sh
+++ front/launch_local_front.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export REACT_APP_LOCAL_BACKEND=true
 export REACT_APP_API_URL=http://localhost:8000
-export REACT_APP_CHARTIS_URL=http://localhost:7000
+export REACT_APP_CHARTIS_URL=http://localhost:7070
 export REACT_APP_EDITOAST_URL=http://localhost:8090
 yarn start
