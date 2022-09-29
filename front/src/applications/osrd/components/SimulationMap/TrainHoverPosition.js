c1b57bb7bb68b02439750ea9ada6171b7bcba8ae
Valentin Chanas <anisometropie@gmail.com>
2022-09-29 16:03:25 +0200

    refacto

--- front/src/applications/osrd/components/SimulationMap/TrainHoverPosition.js
+++ front/src/applications/osrd/components/SimulationMap/TrainHoverPosition.js
@@ -1,74 +1,95 @@
-import { Marker } from 'react-map-gl';
-import PropTypes from 'prop-types';
 import React from 'react';
-import { datetime2time } from 'utils/timeManipulation';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Marker } from 'react-map-gl';
+import cx from 'classnames';
+import nextId from 'react-id-generator';
+
+import { datetime2time } from 'utils/timeManipulation';
+
+function TrainSection({ longitude, latitude, fill, fillOpacity, label }) {
+  return (
+    <Marker className="map-search-marker" longitude={longitude} latitude={latitude}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+        <circle
+          style={{
+            fill,
+            fillOpacity,
+          }}
+          cx="16"
+          cy="16"
+          r="8"
+        />
+      </svg>
+    </Marker>
+  );
+}
+
+TrainSection.propTypes = {
+  longitude: PropTypes.number.isRequired,
+  latitude: PropTypes.number.isRequired,
+  fill: PropTypes.string.isRequired,
+  fillOpacity: PropTypes.string.isRequired,
+  label: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+};
+
+TrainSection.defaultProps = {
+  label: null,
+};
 
 function TrainHoverPosition(props) {
-  const { point } = props;
+  const { point, isSelectedTrain } = props;
   const { selectedTrain, allowancesSettings } = useSelector((state) => state.osrdsimulation);
   const simulation = useSelector((state) => state.osrdsimulation.simulation.present);
   const trainID = simulation.trains[selectedTrain].id;
-
+  const tail =
+    point.properties.intermediaterMarkersPoints &&
+    point.properties.intermediaterMarkersPoints.map((intermediateMarkerPoint, i) => (
+      <TrainSection
+        longitude={intermediateMarkerPoint.geometry.coordinates[0]}
+        latitude={intermediateMarkerPoint.geometry.coordinates[1]}
+        fill={allowancesSettings[trainID].ecoBlocks ? '#82be00' : '#303383'}
+        fillOpacity="0.60"
+        key={nextId()}
+      />
+    ));
   return (
     <>
-      <Marker
-        className="map-search-marker"
+      <TrainSection
         longitude={point.geometry?.coordinates[0]}
         latitude={point.geometry?.coordinates[1]}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-          <circle
-            style={{
-              fill: allowancesSettings[trainID].ecoBlocks ? '#82be00' : '#303383',
-              fillOpacity: '0.75',
-            }}
-            cx="16"
-            cy="16"
-            r="8"
-          />
-        </svg>
-        <span
-          className={`small font-weight-bold ${
-            allowancesSettings[trainID] && allowancesSettings[trainID].ecoBlocks
-              ? 'text-secondary'
-              : 'text-primary'
-          }`}
-        >
-          {point.properties && Math.round(point.properties.speedTime?.speed ?? 0)}
-          km/h
-        </span>
-        <span className="ml-2 small">
-          {point.properties.speedTime && datetime2time(point.properties.speedTime.time)}
-        </span>
-      </Marker>
-      {point.properties.intermediaterMarkersPoints &&
-        point.properties.intermediaterMarkersPoints.map((intermediateMarkerPoint, i) => (
-          <Marker
-            className="map-search-marker"
-            longitude={intermediateMarkerPoint.geometry.coordinates[0]}
-            latitude={intermediateMarkerPoint.geometry.coordinates[1]}
-            key={`intermediateMarker-${i}`}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-              <circle
-                style={{
-                  fill: allowancesSettings[trainID].ecoBlocks ? '#82be00' : '#303383',
-                  fillOpacity: '0.60',
-                }}
-                cx="16"
-                cy="16"
-                r="8"
-              />
-            </svg>
-          </Marker>
-        ))}
+        fill={allowancesSettings[trainID].ecoBlocks ? '#82be00' : '#303383'}
+        fillOpacity="0.75"
+        label={
+          <>
+            <span
+              className={cx(
+                'small',
+                'font-weight-bold',
+                allowancesSettings[trainID]?.ecoBlocks ? 'text-secondary' : 'text-primary'
+              )}
+            >
+              {Math.round(point?.properties?.speedTime?.speed ?? 0)}
+              km/h
+            </span>
+            <span className="ml-2 small">
+              {point.properties.speedTime && datetime2time(point.properties.speedTime.time)}
+            </span>
+          </>
+        }
+      />
+      {tail}
     </>
   );
 }
 
 TrainHoverPosition.propTypes = {
   point: PropTypes.object.isRequired,
+  isSelectedTrain: PropTypes.bool,
+};
+
+TrainHoverPosition.defaultProps = {
+  isSelectedTrain: false,
 };
 
 export default TrainHoverPosition;
--- front/src/applications/osrd/components/SimulationMap/TrainHoverPositionOthers.js
+++ front/src/applications/osrd/components/SimulationMap/TrainHoverPositionOthers.js
@@ -5,48 +5,40 @@ import nextId from 'react-id-generator';
 
 const TrainHoverPositionOthers = (props) => {
   const { trainHoverPositionOthers } = props;
-  return trainHoverPositionOthers.map((point, headIndex) => (
-    <>
-      <Marker
-        className="map-search-marker"
-        longitude={point.geometry.coordinates[0]}
-        latitude={point.geometry.coordinates[1]}
-        key={nextId()}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-          <circle style={{ fill: '#333', fillOpacity: '0.75' }} cx="16" cy="16" r="8" />
-        </svg>
-        <small>{point.properties.name}</small>
-        <span className="small ml-1 font-weight-bold text-muted">
-          {Math.round(point.properties.speed)}
-          km/h
-        </span>
-      </Marker>
-      {point.properties.intermediaterMarkersPoints &&
-        point.properties.intermediaterMarkersPoints.map(
-          (intermediateMarkerPoint, followingIndex) => (
-            <Marker
-              className="map-search-marker"
-              longitude={intermediateMarkerPoint.geometry.coordinates[0]}
-              latitude={intermediateMarkerPoint.geometry.coordinates[1]}
-              key={nextId()}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                <circle
-                  style={{
-                    fill: '#333',
-                    fillOpacity: '0.60',
-                  }}
-                  cx="16"
-                  cy="16"
-                  r="8"
-                />
-              </svg>
-            </Marker>
-          )
-        )}
-    </>
-  ));
+  return trainHoverPositionOthers.map((point, headIndex) => {
+    const tail =
+      point.properties.intermediaterMarkersPoints &&
+      point.properties.intermediaterMarkersPoints.map((intermediateMarkerPoint) => (
+        <TrainSection
+          longitude={intermediateMarkerPoint.geometry.coordinates[0]}
+          latitude={intermediateMarkerPoint.geometry.coordinates[1]}
+          fill="#333"
+          fillOpacity="0.60"
+          key={nextId()}
+        />
+      ));
+    return (
+      <>
+        <TrainSection
+          longitude={point.geometry.coordinates[0]}
+          latitude={point.geometry.coordinates[1]}
+          fill="#333"
+          fillOpacity="0.75"
+          label={
+            <>
+              <small>{point.properties.name}</small>
+              <span className="small ml-1 font-weight-bold text-muted">
+                {Math.round(point.properties.speed)}
+                km/h
+              </span>
+            </>
+          }
+          key={nextId()}
+        />
+        {tail}
+      </>
+    );
+  });
 };
 
 TrainHoverPositionOthers.propTypes = {
