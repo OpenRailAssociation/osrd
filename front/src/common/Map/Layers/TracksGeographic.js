77e964a33ca011bd0f7b1dc64da1a7144991a0c5
Nicolas Wurtz <gitlab@nethenic.net>
2022-09-30 11:05:53 +0200

    front: adapt layers to railjson v3

--- front/src/common/Map/Layers/TracksGeographic.js
+++ front/src/common/Map/Layers/TracksGeographic.js
@@ -34,7 +34,7 @@ function TracksGeographic(props) {
           ...trackNameLayer(colors),
           layout: {
             ...trackNameLayer(colors).layout,
-            'text-field': '{track_name}',
+            'text-field': '{extensions_sncf_track_name}',
             'text-size': 11,
           },
         }}
@@ -47,7 +47,7 @@ function TracksGeographic(props) {
           ...trackNameLayer(colors),
           layout: {
             ...trackNameLayer(colors).layout,
-            'text-field': '{track_name}',
+            'text-field': '{extensions_sncf_track_name}',
             'text-size': 10,
           },
         }}
@@ -60,7 +60,7 @@ function TracksGeographic(props) {
           ...lineNumberLayer(colors),
           layout: {
             ...lineNumberLayer(colors).layout,
-            'text-field': '{line_code}',
+            'text-field': '{extensions_sncf_line_code}',
           },
         }}
         id="chartis/tracks-geo/number"
