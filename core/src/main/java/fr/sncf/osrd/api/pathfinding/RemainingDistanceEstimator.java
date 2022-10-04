8de6794a2032a0b195fb53e261fc157f4939b901
Eloi Charpentier <eloi.charpentier@epita.fr>
2022-10-04 11:55:08 +0200

    core: change pathfinding from Dijkstra to A*

---
+++ core/src/main/java/fr/sncf/osrd/api/pathfinding/RemainingDistanceEstimator.java
@@ -0,0 +1,40 @@
+package fr.sncf.osrd.api.pathfinding;
+
+import fr.sncf.osrd.infra.api.signaling.SignalingRoute;
+import fr.sncf.osrd.utils.geom.Point;
+import fr.sncf.osrd.utils.graph.Pathfinding;
+import java.util.Collection;
+import java.util.function.BiFunction;
+
+public class RemainingDistanceEstimator implements BiFunction<SignalingRoute, Double, Double> {
+
+    private final Collection<Point> targets;
+
+    /** Constructor */
+    public RemainingDistanceEstimator(Collection<Pathfinding.EdgeLocation<SignalingRoute>> edgeLocations) {
+        targets = edgeLocations.stream()
+                .map(loc -> routeOffsetToPoint(loc.edge(), loc.offset()))
+                .toList();
+    }
+
+    private Point routeOffsetToPoint(SignalingRoute route, double pointOffset) {
+        for (var trackRange : route.getInfraRoute().getTrackRanges()) {
+            if (pointOffset <= trackRange.getLength()) {
+                var trackLocation = trackRange.offsetLocation(pointOffset);
+                var normalizedOffset = trackLocation.offset() / trackLocation.track().getLength();
+                return trackLocation.track().getGeo().interpolateNormalized(normalizedOffset);
+            }
+            pointOffset -= trackRange.getLength();
+        }
+        throw new RuntimeException("Couldn't find offset on route");
+    }
+
+    @Override
+    public Double apply(SignalingRoute signalingRoute, Double offset) {
+        var res = Double.POSITIVE_INFINITY;
+        var point = routeOffsetToPoint(signalingRoute, offset);
+        for (var target : targets)
+            res = Double.min(res, point.distance(target));
+        return res;
+    }
+}
