0bb224f4a242e39d77781cd6d3f7ccc42a8e55c8
Benoit Simard <contact@bsimard.com>
2022-10-12 15:44:34 +0200

    front: entities can be undefined on geojson

--- front/src/common/Map/Layers/GeoJSONs.tsx
+++ front/src/common/Map/Layers/GeoJSONs.tsx
@@ -177,7 +177,7 @@ const GeoJSONs: FC<{
         (entities) =>
           ({
             type: 'FeatureCollection',
-            features: entities.map((e) => ({ ...e, type: 'Feature' })),
+            features: (entities || [])?.map((e) => ({ ...e, type: 'Feature' })),
           } as FeatureCollection)
       ),
     [editorData]
