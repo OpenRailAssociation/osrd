65a22e9f528ed57e07fe30c0a6dfc86466f716b6
Theo Macron <theo.macron0315@gmail.com>
2022-10-12 10:18:59 +0200

    front: drivertrainschedule: new json to translate

--- front/src/applications/osrd/views/OSRDSimulation/DriverTrainSchedule.js
+++ front/src/applications/osrd/views/OSRDSimulation/DriverTrainSchedule.js
@@ -45,7 +45,7 @@ function originStop(stop) {
 }
 
 export default function DriverTrainSchedule() {
-  const { t } = useTranslation(['rollingstock'], ['simulation'], ['map-search'], ['osrdconf']);
+  const { t } = useTranslation(['drivertrainschedule']);
   const { selectedTrain } = useSelector((state) => state.osrdsimulation);
   const simulation = useSelector((state) => state.osrdsimulation.simulation.present);
   const data = simulation.trains[selectedTrain].base.stops;
@@ -70,14 +70,14 @@ export default function DriverTrainSchedule() {
             </div>
             <div className="modal-body">
               <div className="container">
-                <div className="text-primary ml-2 mt-2">Détails du sillon</div>
+                <div className="text-primary ml-2 mt-2">{t('drivertrainschedule:dcmdetails')}</div>
                 <div className="row">
                   <div className="col py-3">
                     <div className="row ml-4">
-                      <p className="font-weight-normal">{t('osrdconf:origin')} :</p>
+                      <p className="font-weight-normal">{t('drivertrainschedule:origin')} :</p>
                     </div>
                     <div className="row ml-4">
-                      <p className="font-weight-normal">{t('osrdconf:destination')} :</p>
+                      <p className="font-weight-normal">{t('drivertrainschedule:destination')} :</p>
                     </div>
                   </div>
                   <div className="col py-3">
@@ -96,10 +96,12 @@ export default function DriverTrainSchedule() {
                   </div>
                   <div className="col py-3">
                     <div className="row ml-5">
-                      <p className="font-weight-normal">{t('osrdconf:rollingstock')} :</p>
+                      <p className="font-weight-normal">
+                        {t('drivertrainschedule:rollingstock')} :
+                      </p>
                     </div>
                     <div className="row ml-5">
-                      <p className="font-weight-normal">{t('rollingstock:weight')} :</p>
+                      <p className="font-weight-normal">{t('drivertrainschedule:weight')} :</p>
                     </div>
                   </div>
                   <div className="col py-3">
@@ -112,10 +114,12 @@ export default function DriverTrainSchedule() {
                   </div>
                   <div className="col py-3">
                     <div className="row ml-5">
-                      <p className="font-weight-normal">{t('osrdconf:speedLimitByTag')} :</p>
+                      <p className="font-weight-normal">
+                        {t('drivertrainschedule:speedLimitByTag')} :
+                      </p>
                     </div>
                     <div className="row ml-5">
-                      <p className="font-weight-normal">{t('osrdconf:composition')} :</p>
+                      <p className="font-weight-normal">{t('drivertrainschedule:composition')} :</p>
                     </div>
                   </div>
                   <div className="col py-3">
@@ -127,41 +131,40 @@ export default function DriverTrainSchedule() {
                     </div>
                   </div>
                 </div>
-                <div className="text-primary ml-2 mt-5">{t('simulation:timetable')}</div>
+                <div className="text-primary ml-2 mt-5">{t('drivertrainschedule:timetable')}</div>
                 <div className="text-right">
                   <p className="font-italic text-cyan font-weight-normal">
-                    Nombre de lignes :{' '}
-                    {
-                      data.map((stop, idx, train) =>
-                        formatStops(stop, idx, train, simulation.trains[selectedTrain])
-                      ).length
-                    }
+                    {t('drivertrainschedule:timetable')} : {data.length}
                   </p>
                   <div className="row">
                     <div className="table-wrapper simulation-drivertrainschedule">
                       <div className="table-scroller dragscroll">
-                        <table className="table" id="table">
+                        <table className="table">
                           <thead className="bg-light">
                             <tr>
                               <th scope="col ">
                                 <div className="text-primary">
-                                  {t('map-search:placeholderline')}
+                                  {t('drivertrainschedule:placeholderline')}
                                 </div>
                               </th>
                               <th scope="col">
-                                <div className="text-primary">{t('map-settings:speedlimits')}</div>
+                                <div className="text-primary">
+                                  {t('drivertrainschedule:speedlimits')}
+                                </div>
                               </th>
                               <th scope="col">
-                                <div className="text-primary">{t('map-search:pk')}</div>
+                                <div className="text-primary">{t('drivertrainschedule:pk')}</div>
                               </th>
                               <th scope="col">
-                                <div className="text-primary">{t('simulation:stopPlace')}</div>
+                                <div className="text-primary">{t('drivertrainschedule:place')}</div>
                               </th>
                               <th scope="col">
-                                <div className="text-primary">{t('simulation:stopTime')}</div>
+                                <div className="text-primary">{t('drivertrainschedule:time')}</div>
                               </th>
                               <th scope="col">
-                                <div className="text-primary">{t('map-search:trackname')}</div>
+                                <div className="text-primary">
+                                  {t('drivertrainschedule:trackname')}
+                                </div>
                               </th>
                             </tr>
                           </thead>
