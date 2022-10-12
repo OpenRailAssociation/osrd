f3552920d03716daed33e9f6b684f118f7c24680
Valentin Chanas <anisometropie@gmail.com>
2022-10-12 11:19:58 +0200

    Make the simulation tab map expandable (#2072)
    * map resize
    
    * don’t render hidden params
    
    * imports
    
    * fix map positioning
    
    * min map height
    
    * map max height

--- front/src/common/Map/Settings/MapSettings.js
+++ front/src/common/Map/Settings/MapSettings.js
@@ -12,29 +12,32 @@ import { useTranslation } from 'react-i18next';
 export default function MapSettings(props) {
   const { active, toggleMapSettings } = props;
   const { t } = useTranslation(['translation', 'map-settings']);
-  return (
-    <div className={`map-modal${active ? ' active' : ''}`}>
-      <div className="h2">{t('map-settings:mapSettings')}</div>
-      <MapSettingsMapStyle />
-      <div className="my-2" />
-      <MapSettingsTrackSources />
-      <div className="my-2" />
-      <MapSettingsShowOSM />
-      <div className="my-1" />
-      <MapSettingsShowOSMtracksections />
-      <div className="mb-1 mt-3 border-bottom">{t('map-settings:signalisation')}</div>
-      <MapSettingsSignals />
-      <div className="mb-1 mt-3 border-bottom">{t('map-settings:layers')}</div>
-      <MapSettingsLayers />
-      <div className="mb-1 mt-3 border-bottom">{t('map-settings:speedlimits')}</div>
-      <MapSettingsSpeedLimits />
-      <div className="mt-2 d-flex flex-row-reverse w-100">
-        <button className="btn btn-secondary btn-sm" type="button" onClick={toggleMapSettings}>
-          {t('translation:common.close')}
-        </button>
+  if (active) {
+    return (
+      <div className={`map-modal${active ? ' active' : ''}`}>
+        <div className="h2">{t('map-settings:mapSettings')}</div>
+        <MapSettingsMapStyle />
+        <div className="my-2" />
+        <MapSettingsTrackSources />
+        <div className="my-2" />
+        <MapSettingsShowOSM />
+        <div className="my-1" />
+        <MapSettingsShowOSMtracksections />
+        <div className="mb-1 mt-3 border-bottom">{t('map-settings:signalisation')}</div>
+        <MapSettingsSignals />
+        <div className="mb-1 mt-3 border-bottom">{t('map-settings:layers')}</div>
+        <MapSettingsLayers />
+        <div className="mb-1 mt-3 border-bottom">{t('map-settings:speedlimits')}</div>
+        <MapSettingsSpeedLimits />
+        <div className="mt-2 d-flex flex-row-reverse w-100">
+          <button className="btn btn-secondary btn-sm" type="button" onClick={toggleMapSettings}>
+            {t('translation:common.close')}
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return null;
 }
 
 MapSettings.propTypes = {
