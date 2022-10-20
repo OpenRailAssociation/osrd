081d9570daddddd2ec433d6946f9137bf7cb8faf
Alexandre Damiron <alexandredamiron@gmail.com>
2022-10-20 17:14:39 +0200

    Adn/fix zoom (#2117)
    * restore ok deps d3 and adapt behavior
    
    * front: define chart with v6 selection
    
    * use d3 select v6 on createChart
    
    * singleDispatch improvement
    
    * better wheelDelta granularity
    
    * prevent default on passing extent
    
    * restore updteMustRedraw one end

--- front/src/applications/osrd/components/Simulation/enableInteractivity.js
+++ front/src/applications/osrd/components/Simulation/enableInteractivity.js
@@ -1,4 +1,6 @@
 import * as d3 from 'd3';
+import { zoom as d3zoom } from 'd3-zoom';
+import { pointer } from 'd3-selection';
 
 import {
   gridX,
@@ -18,9 +20,13 @@ import drawGuideLines from 'applications/osrd/components/Simulation/drawGuideLin
 import { store } from 'Store';
 
 export const displayGuide = (chart, opacity) => {
-  chart.svg.selectAll('#vertical-line').style('opacity', opacity);
-  chart.svg.selectAll('#horizontal-line').style('opacity', opacity);
-  chart.svg.selectAll('.pointer').style('opacity', opacity);
+  if (chart.svg) {
+    chart.svg.selectAll('#vertical-line').style('opacity', opacity);
+    chart.svg.selectAll('#horizontal-line').style('opacity', opacity);
+    chart.svg.selectAll('.pointer').style('opacity', opacity);
+  } else {
+    console.warn('attempt to display guide with no whart.svg set, check order of impl.');
+  }
 };
 
 export const updatePointers = (chart, keyValues, listValues, positionValues, rotate) => {
@@ -44,7 +50,7 @@ export const updatePointers = (chart, keyValues, listValues, positionValues, rot
   });
 };
 
-const updateChart = (chart, keyValues, rotate) => {
+const updateChart = (chart, keyValues, rotate, event) => {
   // recover the new scale & test if movement under 0
   /* const newX = (d3.event.sourceEvent.shiftKey && rotate)
     || ((chart.x.domain()[0] - d3.event.transform.x) < 0 && d3.event.transform.k === 1 && rotate)
@@ -55,10 +61,8 @@ const updateChart = (chart, keyValues, rotate) => {
     ? chart.y
     : d3.event.transform.rescaleY(chart.y); */
 
-  const newX =
-    d3.event.sourceEvent.shiftKey && rotate ? chart.x : d3.event.transform.rescaleX(chart.x);
-  const newY =
-    d3.event.sourceEvent.shiftKey && !rotate ? chart.y : d3.event.transform.rescaleY(chart.y);
+  const newX = event.sourceEvent.shiftKey && rotate ? chart.x : event.transform.rescaleX(chart.x);
+  const newY = event.sourceEvent.shiftKey && !rotate ? chart.y : event.transform.rescaleY(chart.y);
 
   // update axes with these new boundaries
   const axisBottomX =
@@ -154,7 +158,7 @@ const updateChart = (chart, keyValues, rotate) => {
     chart.drawZone.selectAll('#gev-operationalPointsZone .op-text').attr('x', (d) => newX(d));
   }
 
-  chart.drawZone.selectAll('.curve-label').attr('transform', d3.event.transform);
+  chart.drawZone.selectAll('.curve-label').attr('transform', event.transform);
 
   chart.drawZone
     .selectAll('.conflictsPoints')
@@ -174,7 +178,7 @@ export const traceVerticalLine = (
   rotate,
   timePosition
 ) => {
-  if (chart !== undefined && d3.event === null) {
+  if (chart !== undefined) {
     displayGuide(chart, 1);
     if (rotate) {
       chart.svg
@@ -237,29 +241,47 @@ const enableInteractivity = (
   let newHoverPosition;
 
   let lastChartX;
-  const zoom = d3
-    .zoom(newHoverPosition)
+
+  // Ovverride the default wheelDelta computation to get smoother zoom
+  function wheelDelta(event) {
+    let factor = 1;
+    if (event.deltaMode === 1) {
+      factor = 0.005;
+    } else if (event.deltaMode) {
+      factor = 0.01;
+    } else {
+      factor = 0.002;
+    }
+
+    return -event.deltaY * factor;
+  }
+
+  const zoom = d3zoom(newHoverPosition)
     .scaleExtent([0.5, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
     .extent([
       [0, 0],
       [chart.width, chart.height],
     ])
-    .on('zoom', () => {
-      dispatch(updateContextMenu(undefined));
+    .wheelDelta(wheelDelta)
+    .on('zoom', (event) => {
       // Permit zoom if shift pressed, if only move or if factor > .5
-      if (d3.event.sourceEvent.ctrlKey || d3.event.sourceEvent.shiftKey) {
+      if (event.sourceEvent.ctrlKey || event.sourceEvent.shiftKey) {
         /* || d3.event.transform.k >= 1
         || zoomLevel >= 0.25)) { */
-        d3.event.sourceEvent.preventDefault();
-        setZoomLevel(zoomLevel * d3.event.transform.k);
-        setYPosition(yPosition + d3.event.transform.y);
-        const zoomFunctions = updateChart(chart, keyValues, rotate);
+        const eventTransform = event.transform;
+        event.sourceEvent.preventDefault();
+        setZoomLevel(zoomLevel * eventTransform.k);
+        setYPosition(yPosition + eventTransform.y);
+        const zoomFunctions = updateChart(chart, keyValues, rotate, event);
         const newChart = { ...chart, x: zoomFunctions.newX, y: zoomFunctions.newY };
         lastChartX = zoomFunctions.newX;
         setChart(newChart);
       }
     })
-    .filter(() => (d3.event.button === 0 || d3.event.button === 1) && d3.event.ctrlKey)
+    .filter((event) => (event.button === 0 || event.button === 1) && event.ctrlKey)
+    .on('start', () => {
+      dispatch(updateContextMenu(undefined));
+    })
     .on('end', () => {
       if (keyValues[1] === 'speed' || keyValues[1] === 'gradient') {
         dispatch(updateChartXGEV(lastChartX));
@@ -276,15 +298,16 @@ const enableInteractivity = (
     }, interval);
   }
 
-  const mousemove = () => {
+  const mousemove = (event, value) => {
     // If GET && not playing
     const { osrdsimulation } = store.getState();
     if (!osrdsimulation.isPlaying) {
       if (keyValues[0] === 'time') {
         // recover coordinate we need
+
         const timePositionLocal = rotate
-          ? chart.y.invert(d3.mouse(d3.event.currentTarget)[1])
-          : chart.x.invert(d3.mouse(d3.event.currentTarget)[0]);
+          ? chart.y.invert(pointer(event, event.currentTarget)[1])
+          : chart.x.invert(pointer(event, event.currentTarget)[0]);
 
         debounceUpdateTimePositionValues(timePositionLocal, null, 15);
         const immediatePositionsValuesForPointer = interpolateOnTime(
@@ -299,8 +322,8 @@ const enableInteractivity = (
       } else {
         // If GEV
         const positionLocal = rotate
-          ? chart.y.invert(d3.mouse(d3.event.currentTarget)[1])
-          : chart.x.invert(d3.mouse(d3.event.currentTarget)[0]);
+          ? chart.y.invert(pointer(event, event.currentTarget)[1])
+          : chart.x.invert(pointer(event, event.currentTarget)[0]);
         const timePositionLocal = interpolateOnPosition(dataSimulation, keyValues, positionLocal);
         if (timePositionLocal) {
           debounceUpdateTimePositionValues(timePositionLocal, null, 15);
@@ -310,19 +333,25 @@ const enableInteractivity = (
       // Update guideLines
       chart.svg
         .selectAll('#vertical-line')
-        .attr('x1', d3.mouse(d3.event.currentTarget)[0])
-        .attr('x2', d3.mouse(d3.event.currentTarget)[0]);
+        .attr('x1', pointer(event, event.currentTarget)[0])
+        .attr('x2', pointer(event, event.currentTarget)[0]);
       chart.svg
         .selectAll('#horizontal-line')
-        .attr('y1', d3.mouse(d3.event.currentTarget)[1])
-        .attr('y2', d3.mouse(d3.event.currentTarget)[1]);
+        .attr('y1', pointer(event, event.currentTarget)[1])
+        .attr('y2', pointer(event, event.currentTarget)[1]);
     }
   };
 
   chart.svg // .selectAll('.zoomable')
     .on('mouseover', () => displayGuide(chart, 1))
-    .on('mousemove', () => mousemove())
+    .on('mousemove', mousemove)
+    .on('wheel', (event) =>  {
+      if (event.ctrlKey || event.shiftKey) {
+        event.preventDefault();
+      }
+    })
     .call(zoom);
+
   drawGuideLines(chart);
 };
 
