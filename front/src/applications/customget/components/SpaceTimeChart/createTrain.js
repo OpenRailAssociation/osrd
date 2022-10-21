e6a1ca955a126cb4013ca0390c906dff07c27310
Nicolas Wurtz <6003856+nicolaswurtz@users.noreply.github.com>
2022-10-21 16:26:19 +0200

    Nwz/get for custom data (#2047)
    * front: begin to isolate spacetimechart for customget
    
    * front: custom GET WIP
    
    * front: custom get WIP 2
    
    * front: wip customget cleaning up
    
    * front: customGET functional WIP
    
    * front: customGET first working version OK
    
    * front: customget: added new curve, colors
    
    * front: customget: no max height for trainlist
    
    * front: customget rebase with dev & dependancies
    
    * front: customget: deleted unused data for testing purpose
    
    * front: take Estia's format in consideration
    
    * front: GET custom: adding test data
    
    * front: GET-Custom bugfix on import
    
    * front: GET Custom weird bug on reducer import
    
    * front: GET Custom weird bug 2 on import
    
    * front: GET Custom weird bug on import 3
    
    * front: GET Custom weird bug on import 4
    
    * front: avoid reducer file & folder with same name (osrdsimulation)
    
    Co-authored-by: Nicolas Wurtz <gitlab@nethenic.net>

---
+++ front/src/applications/customget/components/SpaceTimeChart/createTrain.js
@@ -0,0 +1,17 @@
+import { formatStepsWithTimeMulti } from 'applications/customget/components/ChartHelpers';
+
+export default function createTrain(dispatch, keyValues, simulationTrains, t) {
+  // Prepare data
+  const dataSimulation = simulationTrains.map((train, trainNumber) => {
+    const dataSimulationTrain = {};
+    dataSimulationTrain.id = train.id;
+    dataSimulationTrain.name = train.name;
+    dataSimulationTrain.color = train.color;
+    dataSimulationTrain.trainNumber = trainNumber;
+    dataSimulationTrain.headPosition = formatStepsWithTimeMulti(train.base.head_positions);
+    dataSimulationTrain.tailPosition = formatStepsWithTimeMulti(train.base.tail_positions);
+
+    return dataSimulationTrain;
+  });
+  return dataSimulation;
+}
