eb09c60c0804fafb50c7a100507060c0657f6403
Nicolas Wurtz <gitlab@nethenic.net>
2022-10-21 13:33:44 +0200

    front: take Estia's format in consideration

--- front/src/applications/customget/components/convertData.js
+++ front/src/applications/customget/components/convertData.js
@@ -39,22 +39,36 @@ const convertStops = (steps) => {
 const convertData = (trains) => {
   const newData = trains.map((train, idx) => ({
     id: idx,
-    name: train.name,
+    name: train.name ? train.name : train.train_metadata.name,
     color: randomColor(),
     base: {
       head_positions: [
-        train.space_time_curves.time_table.map((step) => ({
-          time: step.time,
-          position: step.position,
-        })),
+        train.space_time_curves.time_table
+          ? train.space_time_curves.time_table.map((step) => ({
+              time: step.time,
+              position: step.position,
+            }))
+          : train.space_time_curves[1].points.map((step) => ({
+              time: step.time,
+              position: step.position,
+            })),
       ],
       tail_positions: [
-        train.space_time_curves.actual.map((step) => ({
-          time: step.time,
-          position: step.position,
-        })),
+        train.space_time_curves.actual
+          ? train.space_time_curves.actual.map((step) => ({
+              time: step.time,
+              position: step.position,
+            }))
+          : train.space_time_curves[0].points.map((step) => ({
+              time: step.time,
+              position: step.position,
+            })),
       ],
-      stops: convertStops(train.space_time_curves.time_table),
+      stops: convertStops(
+        train.space_time_curves.time_table
+          ? train.space_time_curves.time_table
+          : train.space_time_curves[0].points
+      ),
     },
   }));
   return newData;
