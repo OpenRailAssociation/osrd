27328ae1226bdb882fc6e9c0be649ecdfd0e2e24
Florian Amsallem <florian.amsallem@epita.fr>
2022-10-26 11:50:13 +0200

    api: add lpv panel generated table

---
+++ api/osrd_infra/migrations/__init__.py
@@ -0,0 +1,26 @@
+import django
+from django.db import migrations, models
+
+
+def run_sql_add_foreign_key(model_name: str, field_name: str, link_model: str):
+    return migrations.RunSQL(
+        f"""ALTER TABLE osrd_infra_{model_name}
+                ADD {field_name}_id INTEGER,
+                ADD CONSTRAINT osrd_{link_model}_{model_name}_fkey FOREIGN KEY ({field_name}_id)
+                   REFERENCES osrd_infra_{link_model}(id) ON DELETE CASCADE
+            """,
+        state_operations=[
+            migrations.AddField(
+                model_name=model_name,
+                name=field_name,
+                field=models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to=f"osrd_infra.{link_model}",
+                ),
+            ),
+        ],
+    )
+
+
+def run_sql_add_foreign_key_infra(model_name: str):
+    return run_sql_add_foreign_key(model_name, "infra", "infra")
