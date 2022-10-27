cbf17242344b3d3fa9b42e8d896a85d87c7f84f3
Valentin Chanas <anisometropie@gmail.com>
2022-10-27 14:51:28 +0200

    ordered layer optional layer order prop

--- front/src/common/Map/Layers/OrderedLayer.tsx
+++ front/src/common/Map/Layers/OrderedLayer.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Layer, LayerProps } from 'react-map-gl';
 
 type OrderedLayerProps = LayerProps & {
-  layerOrder: number;
+  layerOrder?: number;
 };
 
 export default function OrderedLayer(props: OrderedLayerProps) {
   const { layerOrder, ...restOfLayerProps } = props;
-
-  return <Layer beforeId={`virtual-layer-${layerOrder + 1}`} {...restOfLayerProps} />;
+  const layerProps = {
+    ...(layerOrder && { beforeId: `virtual-layer-${layerOrder + 1}` }),
+    ...restOfLayerProps,
+  };
+  return <Layer {...layerProps} />;
 }
