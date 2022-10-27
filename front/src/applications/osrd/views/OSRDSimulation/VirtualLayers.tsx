563bd7eb138cf8ba198910ad6f5483889097c46c
Valentin Chanas <anisometropie@gmail.com>
2022-10-27 16:25:57 +0200

    add one extra virtual layer and last one doesn’t have a beforeId

--- front/src/applications/osrd/views/OSRDSimulation/VirtualLayers.tsx
+++ front/src/applications/osrd/views/OSRDSimulation/VirtualLayers.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Layer } from 'react-map-gl';
 import { range } from 'lodash';
 
-export const VIRTUAL_LAYERS = 7;
+export const VIRTUAL_LAYERS = 8;
 
 export default function VirtualLayers() {
   const layers = range(0, VIRTUAL_LAYERS)
     .reverse()
     .map((n) => {
       const before =
-        n < VIRTUAL_LAYERS
+        n < VIRTUAL_LAYERS - 1
           ? {
               beforeId: `virtual-layer-${n + 1}`,
             }
