7bb68ccf6eb5b1d22c5aa8e08a229746b831cb67
Valentin Chanas <anisometropie@gmail.com>
2022-11-03 11:57:28 +0100

    better defined types in util function

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -1,8 +1,11 @@
 import { upperFirst, mapKeys } from 'lodash';
 
 // eslint-disable-next-line import/prefer-default-export
-export function makeEnumBooleans<T = symbol>(list: Record<string, T>, value: T | null) {
-  const booleans = Object.keys(list).reduce(
+export function makeEnumBooleans<T = symbol>(
+  list: Record<string, T>,
+  value: T | null
+): Record<string, boolean> {
+  const booleans = Object.keys(list).reduce<Record<string, boolean>>(
     (acc, k) => ({
       ...acc,
       [k]: value === list[k],
