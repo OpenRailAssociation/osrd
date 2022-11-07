09ab535c1e05243e77c9a5327a7296a0fc68251a
Florian Amsallem <florian.amsallem@epita.fr>
2022-11-07 15:56:53 +0100

    editoast: adapt to railjson 3.0.1

--- editoast/src/infra.rs
+++ editoast/src/infra.rs
@@ -13,7 +13,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::{json, Map, Value};
 use thiserror::Error;
 
-pub const RAILJSON_VERSION: &str = "3.0.0";
+pub const RAILJSON_VERSION: &str = "3.0.1";
 
 #[derive(Clone, QueryableByName, Queryable, Debug, Serialize, Deserialize, Identifiable)]
 #[table_name = "osrd_infra_infra"]
