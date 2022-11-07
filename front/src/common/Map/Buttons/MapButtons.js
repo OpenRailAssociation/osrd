57db2029410bc5a8032122d707b0c6fb41595593
Math_R_ <25245040+Math-R@users.noreply.github.com>
2022-11-07 10:29:00 +0100

    front: Add infra selector button on map button container (#2196)
    Co-authored-by: Math_R_ <mathieu.richard@meritis.fr>

--- front/src/common/Map/Buttons/MapButtons.js
+++ front/src/common/Map/Buttons/MapButtons.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFailure } from 'reducers/main';
+import { useTranslation } from 'react-i18next';
+import { get } from 'common/requests';
 
 // Buttons
 import ButtonMapSearch from 'common/Map/Buttons/ButtonMapSearch';
@@ -12,12 +16,36 @@ import ButtonFullscreen from 'common/ButtonFullscreen';
 import MapSearch from 'common/Map/Search/MapSearch';
 import MapSettings from 'common/Map/Settings/MapSettings';
 import MapKey from 'common/Map/MapKey';
+import InfraSelectorModal from 'applications/osrd/components/InfraSelector/InfraSelectorModal';
+import ButtonMapInfras from './ButtonMapInfras';
+
+const infraURL = '/infra/';
 
 export default function MapButtons(props) {
   const { resetPitchBearing } = props;
   const [showSearch, setShowSearch] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
   const [showMapKey, setShowMapKey] = useState(false);
+
+  const dispatch = useDispatch();
+  const [infrasList, setInfrasList] = useState(undefined);
+
+  const { t } = useTranslation(['osrdconf']);
+
+  const getInfrasList = async () => {
+    try {
+      const infrasListQuery = await get(infraURL, {});
+      setInfrasList(infrasListQuery);
+    } catch (e) {
+      dispatch(
+        setFailure({
+          name: t('errorMessages.unableToRetrieveInfraList'),
+          message: e.message,
+        })
+      );
+      console.log('ERROR', e);
+    }
+  };
 
   return (
     <>
@@ -27,10 +55,12 @@ export default function MapButtons(props) {
         <ButtonMapKey toggleMapKey={() => setShowMapKey(!showMapKey)} />
         <ButtonResetViewport updateLocalViewport={resetPitchBearing} />
         <ButtonFullscreen />
+        <ButtonMapInfras toggleInfraSelector={getInfrasList} />
       </div>
       <MapSearch active={showSearch} toggleMapSearch={() => setShowSearch(!showSearch)} />
       <MapSettings active={showSettings} toggleMapSettings={() => setShowSettings(!showSettings)} />
       <MapKey active={showMapKey} toggleMapKey={() => setShowMapKey(!showMapKey)} />
+      <InfraSelectorModal infrasList={infrasList} from="map" />
     </>
   );
 }
