473ece8f9b837d446b8646ea5b4d304e427e3ec5
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2022-11-15 15:43:28 +0100

    core: stdcm: lazy edge evaluation to avoid computing useless allowances
    We avoid computing allowances and backtracking when
    computing an edge, we only compute this once needed.
    
    This significantly reduces the amount of allowances computed.

--- core/src/main/java/fr/sncf/osrd/utils/graph/functional_interfaces/TargetsOnEdge.java
+++ core/src/main/java/fr/sncf/osrd/utils/graph/functional_interfaces/TargetsOnEdge.java
@@ -1,9 +1,10 @@
 package fr.sncf.osrd.utils.graph.functional_interfaces;
 
+import fr.sncf.osrd.utils.graph.Pathfinding;
 import java.util.Collection;
 
 /** Functions that takes an edge and returns the offset of any target for the current step on the edge */
 @FunctionalInterface
 public interface TargetsOnEdge<EdgeT> {
-    Collection<Double> apply(EdgeT edge);
+    Collection<Pathfinding.EdgeLocation<EdgeT>> apply(EdgeT edge);
 }
