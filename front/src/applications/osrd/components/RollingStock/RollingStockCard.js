4aa0c850f8026df5782988c2b82490e259b831ac
Nicolas Wurtz <gitlab@nethenic.net>
2022-11-18 01:46:57 +0100

    front: rollingstock: adding img

--- front/src/applications/osrd/components/RollingStock/RollingStockCard.js
+++ front/src/applications/osrd/components/RollingStock/RollingStockCard.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { updateRollingStockID } from 'reducers/osrdconf';
 import { useDispatch } from 'react-redux';
-import ProgressSNCF from 'common/BootstrapSNCF/ProgressSNCF';
-import RollingStockCurve from 'applications/osrd/components/RollingStock/RollingStockCurve';
 import { BsLightningFill } from 'react-icons/bs';
 import { MdLocalGasStation } from 'react-icons/md';
 import { IoIosSpeedometer } from 'react-icons/io';
 import { FaWeightHanging } from 'react-icons/fa';
 import { AiOutlineColumnWidth } from 'react-icons/ai';
-import { powerClasses } from 'applications/osrd/components/RollingStock/consts';
+import RollingStockCardDetail from 'applications/osrd/components/RollingStock/RollingStockCardDetail';
+import RollingStock2Img from 'applications/osrd/components/RollingStock/RollingStock2Img';
 
 export default function RollingStockCard(props) {
   const dispatch = useDispatch();
@@ -45,138 +44,26 @@ export default function RollingStockCard(props) {
     <div
       className="rollingstock-container mb-3"
       role="button"
-      // onClick={() => setDetailDisplay(!detailDisplay)}
+      onClick={() => setDetailDisplay(!detailDisplay)}
       tabIndex={0}
     >
       <div className="rollingstock-header">
         <div className="rollingstock-title">
           <div>{data.name}</div>
-          <div>
+          <div className="rollingstock-img ml-1">
+            <RollingStock2Img name={data.name} />
+          </div>
+          <div className="sr-only">
             <small className="text-primary mr-1">ID</small>
             <span className="font-weight-lighter small">{data.id}</span>
           </div>
         </div>
       </div>
-      {detailDisplay ? (
-        <div className="rollingstock-body">
-          <div className="row pt-2">
-            <div className="col-sm-6">
-              <table className="rollingstock-details-table">
-                <tbody>
-                  <tr>
-                    <td className="text-primary">{t('startupTime')}</td>
-                    <td>
-                      {data.startup_time}
-                      <span className="small text-muted ml-1">s</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('startupAcceleration')}</td>
-                    <td>
-                      {data.startup_acceleration}
-                      <span className="small text-muted ml-1">m/s²</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('comfortAcceleration')}</td>
-                    <td>
-                      {data.comfort_acceleration}
-                      <span className="small text-muted ml-1">m/s²</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('intertiaCoefficient')}</td>
-                    <td>{data.inertia_coefficient}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('timetableGamma')}</td>
-                    <td>
-                      {data.gamma.value * -1}
-                      <span className="small text-muted ml-1">m/s²</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('electricOnly')}</td>
-                    <td>{data.electric_only === true ? t('yes') : t('no')}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">{t('compatibleVoltages')}</td>
-                    {data.compatible_voltages.length > 0 ? (
-                      <td>
-                        {data.compatible_voltages.join(' / ')}
-                        <span className="small text-muted ml-1">V</span>
-                      </td>
-                    ) : (
-                      t('noCompatibleVoltages')
-                    )}
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="col-sm-6">
-              {data.power_class && data.power_class < 7 ? (
-                <>
-                  <div>
-                    <small className="mr-1 text-primary">{t('powerClass')}</small>
-                    {data.power_class}
-                    <span>: </span>
-                    {powerClasses[data.power_class].a}
-                    <span>A / </span>
-                    {powerClasses[data.power_class].kw}
-                    <small>kW</small>
-                  </div>
-                  <ProgressSNCF value={Math.floor(data.power_class * 16.67)} small />
-                </>
-              ) : null}
-              {data.features && data.features.length > 0 ? (
-                <div>
-                  {t('features')}
-                  <span className="ml-1">{data.features.join(', ')}</span>
-                </div>
-              ) : null}
-              <div className="pt-1">
-                {t('rollingResistance')}
-                <div className="text-muted small">{t('rollingResistanceFormula')}</div>
-              </div>
-              <table className="rollingstock-details-table ml-2">
-                <tbody>
-                  <tr>
-                    <td className="text-primary">a</td>
-                    <td>
-                      {Math.floor(data.rolling_resistance.A * 10000) / 10000}
-                      <span className="small ml-1 text-muted">N</span>
-                    </td>
-                    <td className="text-primary">{t('rollingResistanceA')}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">b</td>
-                    <td>
-                      {Math.floor(data.rolling_resistance.B * 10000) / 10000}
-                      <span className="small ml-1 text-muted">N/(m/s)</span>
-                    </td>
-                    <td className="text-primary">{t('rollingResistanceB')}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-primary">c</td>
-                    <td>
-                      {Math.floor(data.rolling_resistance.C * 10000) / 10000}
-                      <span className="small ml-1 text-muted">N/(m/s²)</span>
-                    </td>
-                    <td className="text-primary">{t('rollingResistanceC')}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <div className="curve-container">
-                <RollingStockCurve data={data.effort_curve} />
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
+      <RollingStockCardDetail
+        id={data.id}
+        detailDisplay={detailDisplay}
+        setDetailDisplay={setDetailDisplay}
+      />
       <div className="rollingstock-footer py-2">
         <div className="row">
           <div className="col-5">
@@ -193,7 +80,9 @@ export default function RollingStockCard(props) {
                   </span>
                   <small>
                     {tractionModes.voltages.map((voltage) => (
-                      <span className="ml-1">{voltage}V</span>
+                      <span className="ml-1" key={`${voltage}${data.id}`}>
+                        {voltage}V
+                      </span>
                     ))}
                   </small>
                 </>
@@ -222,16 +111,6 @@ export default function RollingStockCard(props) {
             </div>
           </div>
         </div>
-        {detailDisplay ? (
-          <button
-            className="btn btn-primary btn-sm"
-            type="button"
-            data-dismiss="modal"
-            onClick={() => dispatch(updateRollingStockID(data.id))}
-          >
-            {t('selectRollingStock')}
-          </button>
-        ) : null}
       </div>
     </div>
   );
