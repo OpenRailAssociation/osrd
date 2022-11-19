d97751b89410fb76b3290437e89be350d5dc54e4
Nicolas Wurtz <gitlab@nethenic.net>
2022-11-19 14:11:25 +0100

    front: rollingstock: update mlgtraffic & helpers for um

---
+++ front/src/applications/osrd/components/RollingStock/RollingStockHelpers.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function checkUnit(data) {
+  if (data.unit && data.unit !== 'US') {
+    return <span className="rollingstock-infos-unit">{data.unit !== 'US' ? data.unit : null}</span>;
+  }
+  if (data.detail.search(/UM2|MUX/i) !== -1) {
+    return <span className="rollingstock-infos-unit">UM2</span>;
+  }
+  if (data.detail.search(/UM3/i) !== -1) {
+    return <span className="rollingstock-infos-unit">UM3</span>;
+  }
+  return null;
+}
+
+export function RollingStockInfos(props) {
+  const { data } = props;
+  const series = data.series ? (
+    <span className="rollingstock-infos-begin">
+      <span className="rollingstock-infos-series">{data.series}</span>
+      <span className="rollingstock-infos-subseries">
+        {data.series !== data.subseries ? data.subseries : null}
+      </span>
+      {checkUnit(data)}
+    </span>
+  ) : (
+    <span className="rollingstock-infos-begin">
+      <span className="rollingstock-infos-series">{data.reference}</span>
+      {checkUnit(data)}
+      <span className="rollingstock-infos-subseries">{data.detail}</span>
+    </span>
+  );
+  const family = data.series ? (
+    <span className="rollingstock-infos-middle">
+      {`${data.family} / ${data.type} / ${data.grouping}`}
+    </span>
+  ) : null;
+  return (
+    <div className="rollingstock-infos">
+      {series}
+      {family}
+      <span className="rollingstock-infos-end">{data.name}</span>
+    </div>
+  );
+}
+
+RollingStockInfos.propTypes = {
+  data: PropTypes.object.isRequired,
+};
