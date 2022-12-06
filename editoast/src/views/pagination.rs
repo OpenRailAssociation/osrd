2667720b9fbd820bb37bdbf65c8c5180bcfc351b
Younes Khoudli <younes.khoudli@epita.fr>
2022-12-06 14:08:40 +0100

    editoast: fix clippy format string lint

--- editoast/src/views/pagination.rs
+++ editoast/src/views/pagination.rs
@@ -36,7 +36,6 @@ pub fn paginate(query: String, page: i64, per_page: i64) -> SqlQuery {
     assert!(per_page > 0);
     let offset = (page - 1) * per_page;
     sql_query(format!(
-        "SELECT *, COUNT(*) OVER () FROM ({}) t LIMIT {} OFFSET {}",
-        query, per_page, offset
+        "SELECT *, COUNT(*) OVER () FROM ({query}) t LIMIT {per_page} OFFSET {offset}"
     ))
 }
