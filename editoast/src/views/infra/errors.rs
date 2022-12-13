16d741e93ba2c946982a1589050fbb3535a452e8
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2022-12-13 11:14:23 +0100

    editoast: add filters (level, error_type and object_id) to list_errors endpoint

--- editoast/src/views/infra/errors.rs
+++ editoast/src/views/infra/errors.rs
@@ -1,11 +1,9 @@
-use diesel::sql_types::{BigInt, Integer, Json};
+use crate::api_error::ApiError;
+use crate::views::pagination::{paginate, PaginationError};
+use diesel::sql_types::{BigInt, Integer, Json, Text};
 use diesel::{PgConnection, RunQueryDsl};
 use serde::Serialize;
 use serde_json::Value;
-
-use crate::api_error::ApiError;
-
-use crate::views::pagination::{paginate, PaginationError};
 
 #[derive(QueryableByName, Debug, Clone)]
 struct InfraErrorQueryable {
@@ -13,18 +11,34 @@ struct InfraErrorQueryable {
     pub count: i64,
     #[sql_type = "Json"]
     pub information: Value,
+    #[sql_type = "Json"]
+    pub geographic: Value,
+    #[sql_type = "Json"]
+    pub schematic: Value,
+}
+
+#[derive(Default, Debug, PartialEq, Eq, FromFormField)]
+pub enum Level {
+    Warnings,
+    Errors,
+    #[default]
+    All,
 }
 
 #[derive(Debug, Clone, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct InfraErrorModel {
     pub information: Value,
+    pub geographic: Value,
+    pub schematic: Value,
 }
 
 impl From<InfraErrorQueryable> for InfraErrorModel {
     fn from(error: InfraErrorQueryable) -> Self {
         Self {
             information: error.information,
+            geographic: error.geographic,
+            schematic: error.schematic,
         }
     }
 }
@@ -34,15 +48,29 @@ pub fn get_paginated_infra_errors(
     infra: i32,
     page: i64,
     per_page: i64,
-    exclude_warnings: bool,
+    level: Level,
+    error_type: Option<String>,
+    object_id: Option<String>,
 ) -> Result<(Vec<InfraErrorModel>, i64), Box<dyn ApiError>> {
     let mut query =
-        String::from("SELECT information::text FROM osrd_infra_errorlayer WHERE infra_id = $1");
-    if exclude_warnings {
+        String::from("SELECT information::text, ST_AsGeoJSON(ST_Transform(geographic, 4326))::json as geographic,
+        ST_AsGeoJSON(ST_Transform(schematic, 4326))::json as schematic FROM osrd_infra_errorlayer WHERE infra_id = $1");
+    if level == Level::Warnings {
+        query += " AND information->>'is_warning' = 'true'"
+    } else if level == Level::Errors {
         query += " AND information->>'is_warning' = 'false'"
     }
+    if error_type.is_some() {
+        query += " AND information->>'error_type' = $2"
+    }
+    if object_id.is_some() {
+        query += " AND information->>'obj_id' = $3"
+    }
+
     let infra_errors = paginate(query, page, per_page)
         .bind::<Integer, _>(infra)
+        .bind::<Text, _>(&error_type.unwrap_or_default())
+        .bind::<Text, _>(&object_id.unwrap_or_default())
         .load::<InfraErrorQueryable>(conn)?;
     let count = infra_errors.first().map(|e| e.count).unwrap_or_default();
     let infra_errors: Vec<InfraErrorModel> = infra_errors.into_iter().map(|e| e.into()).collect();
