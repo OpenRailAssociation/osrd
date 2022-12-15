3d35b6068d567859c291a206f8badca89adf00e4
Nicolas Wurtz <6003856+nicolaswurtz@users.noreply.github.com>
2022-12-15 09:53:28 +0100

    Nwz/crud infra (#2561)
    * front: crudInfra: set infraselector as single source of truth for api
    
    * front: crudInfra: continue isolation of component
    
    * front: crudInfra: added locked state
    
    * front: crudInfra: getInfra inside infraselectormodal
    
    * front: crudInfra: some cosmetics changes, icons, harmonize styles
    
    * front: crudInfra: Added lock/unlock & cosmetics changes
    
    * front: crudInfra: repair side damage on timetable selector
    
    * front: crudInfra: export railjson ok
    
    * front: crudInfra: delete ok
    
    * front: crudInfra: done !
    
    * front: have to desactivate resizing of map in simulation, we'll have to correct modal behaviour before
    
    * front: crudInfra: removed setMustRefresh because it was ugly

--- front/src/common/Map/Buttons/ButtonMapInfras.js
+++ front/src/common/Map/Buttons/ButtonMapInfras.js
@@ -3,15 +3,14 @@ import { GiRailRoad } from 'react-icons/gi';
 import PropTypes from 'prop-types';
 
 export default function ButtonMapInfras(props) {
-  const { toggleInfraSelector } = props;
+  const { modalID } = props;
 
   return (
     <button
       type="button"
       className="btn-rounded btn-rounded-white btn-map-infras"
-      onClick={toggleInfraSelector}
       data-toggle="modal"
-      data-target="#infra-selector-modal-map"
+      data-target={`#${modalID}`}
     >
       <span className="sr-only">Infrastructures</span>
       <GiRailRoad />
@@ -20,5 +19,5 @@ export default function ButtonMapInfras(props) {
 }
 
 ButtonMapInfras.propTypes = {
-  toggleInfraSelector: PropTypes.func.isRequired,
+  modalID: PropTypes.string.isRequired,
 };
