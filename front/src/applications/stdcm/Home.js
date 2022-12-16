3c374862a0265d6a117e3fee4cf897ff3eca4e75
Nicolas Wurtz <gitlab@nethenic.net>
2022-12-16 09:36:06 +0100

    front: hook & change icon for stdcm

--- front/src/applications/stdcm/Home.js
+++ front/src/applications/stdcm/Home.js
@@ -1,4 +1,4 @@
-import 'applications/osrd/osrd.scss';
+import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -6,54 +6,40 @@ import MastNavItemSNCF from 'common/BootstrapSNCF/MastNavItemSNCF';
 import MastNavSNCF from 'common/BootstrapSNCF/MastNavSNCF';
 import NavBarSNCF from 'common/BootstrapSNCF/NavBarSNCF';
 import { NotificationsState } from 'common/Notifications';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { connect } from 'react-redux';
-import logo from 'assets/logo_osrd_seul_blanc.svg';
-import { withTranslation } from 'react-i18next';
+import logo from 'assets/pictures/home/stdcm.svg';
+import { useTranslation } from 'react-i18next';
+import AboutOSRD from 'applications/osrd/About';
 import OSRDSTDCM from './views/OSRDSTDCM';
-import AboutOSRD from '../osrd/About';
+import 'applications/osrd/osrd.scss';
 
-class HomeStdcm extends React.Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-  };
+export default function HomeStdcm() {
+  const { t } = useTranslation(['home', 'translation']);
+  return (
+    <>
+      <MastNavSNCF
+        items={
+          <MastNavItemSNCF
+            link="/stdcm"
+            linkname={t('translation:osrd.nav.home')}
+            icon="icons-itinerary-train-station"
+          />
+        }
+        itemsBottom={
+          <MastNavItemSNCF
+            link="/osrd/contact"
+            linkname={t('translation:osrd.nav.contact')}
+            icon="icons-support"
+            bottom
+          />
+        }
+      />
+      <NavBarSNCF appName={t('home:stdcm')} logo={logo} />
+      <Routes>
+        <Route path="/about" element={<AboutOSRD />} />
 
-  render() {
-    const { t } = this.props;
-    return (
-      <>
-        <MastNavSNCF
-          items={
-            <MastNavItemSNCF
-              link="/stdcm"
-              linkname={t('osrd.nav.home')}
-              icon="icons-itinerary-train-station"
-            />
-          }
-          itemsBottom={
-            <MastNavItemSNCF
-              link="/osrd/contact"
-              linkname={t('osrd.nav.contact')}
-              icon="icons-support"
-              bottom
-            />
-          }
-        />
-        <NavBarSNCF appName="OSRD" logo={logo} />
-        <Routes>
-          <Route path="/about" element={<AboutOSRD />} />
-
-          <Route path="" element={<OSRDSTDCM />} />
-        </Routes>
-        <NotificationsState />
-      </>
-    );
-  }
+        <Route path="" element={<OSRDSTDCM />} />
+      </Routes>
+      <NotificationsState />
+    </>
+  );
 }
-
-const mapStateToProps = (state) => ({
-  osrdsimulation: state.osrdsimulation,
-});
-
-export default connect(mapStateToProps)(withTranslation()(HomeStdcm));
