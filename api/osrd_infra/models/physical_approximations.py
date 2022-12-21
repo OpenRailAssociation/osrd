019af686b984e12f633ca4c73123e0aefb7deecd
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2022-12-21 18:56:27 +0100

    api: remove enums for electrical profiles and rename

--- api/osrd_infra/models/physical_approximations.py
+++ api/osrd_infra/models/physical_approximations.py
@@ -1,11 +1,12 @@
 from django.contrib.gis.db import models
 from osrd_infra.utils import PydanticValidator
 
-from osrd_infra.schemas.physical_approximations import ElectricProfilesList as ElectricProfilesListSchema
+from osrd_infra.schemas.physical_approximations import ElectricalProfilesList
 
 
-class ElectricProfilesList(models.Model):
-    data = models.JSONField(validators=[PydanticValidator(ElectricProfilesListSchema)])
+class ElectricalProfilesSet(models.Model):
+    name = models.CharField(max_length=128)
+    data = models.JSONField(validators=[PydanticValidator(ElectricalProfilesList)])
 
     class Meta:
-        verbose_name_plural = "electric profiles lists"
+        verbose_name_plural = "Electrical profiles sets"
