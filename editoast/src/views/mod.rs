19a2429df579062a39e0851ba0dd12042cd5c40f
Younes Khoudli <younes.khoudli@epita.fr>
2023-01-04 14:29:00 +0100

    backend: embed version information

--- editoast/src/views/mod.rs
+++ editoast/src/views/mod.rs
@@ -1,15 +1,17 @@
 mod infra;
+use rocket::serde::json::Value as JsonValue;
 pub mod pagination;
 pub mod params;
 
 use diesel::{sql_query, RunQueryDsl};
 use rocket::{routes, Route};
 use std::collections::HashMap;
+use std::env;
 
 use crate::db_connection::DBConnection;
 
 pub fn routes() -> HashMap<&'static str, Vec<Route>> {
-    HashMap::from([("/", routes![health]), ("/infra", infra::routes())])
+    HashMap::from([("/", routes![health, version]), ("/infra", infra::routes())])
 }
 
 #[get("/health")]
@@ -20,12 +22,21 @@ pub async fn health(conn: DBConnection) -> &'static str {
     "ok"
 }
 
+#[get("/version")]
+pub fn version() -> JsonValue {
+    let mut res = HashMap::new();
+    let describe = env::var("OSRD_GIT_DESCRIBE").ok();
+    res.insert("git_describe", describe);
+    serde_json::to_value(res).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::client::PostgresConfig;
     use crate::create_server;
     use rocket::http::Status;
     use rocket::local::blocking::Client;
+    use std::collections::HashMap;
 
     /// Create a test editoast client
     /// This client create a single new connection to the database
@@ -43,5 +54,14 @@ mod tests {
         let client = create_test_client();
         let response = client.get("/health").dispatch();
         assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn version() {
+        let client = create_test_client();
+        let response = client.get("/version").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let json_response: HashMap<String, Option<String>> = response.into_json().unwrap();
+        assert!(json_response.contains_key("git_describe"))
     }
 }
