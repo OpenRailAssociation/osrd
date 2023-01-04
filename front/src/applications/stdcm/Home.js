ef693398f49f56fde924cae922509d5bca23dc90
Nicolas Wurtz <gitlab@nethenic.net>
2023-01-04 15:43:04 +0100

    front: removed about, refacto home, begin refacto scss

--- front/src/applications/stdcm/Home.js
+++ front/src/applications/stdcm/Home.js
@@ -6,7 +6,6 @@ import NavBarSNCF from 'common/BootstrapSNCF/NavBarSNCF';
 import { NotificationsState } from 'common/Notifications';
 import logo from 'assets/pictures/home/stdcm.svg';
 import { useTranslation } from 'react-i18next';
-import AboutOSRD from 'applications/osrd/About';
 import OSRDSTDCM from './views/OSRDSTDCM';
 import 'applications/osrd/osrd.scss';
 
@@ -16,8 +15,6 @@ export default function HomeStdcm() {
     <>
       <NavBarSNCF appName={t('home:stdcm')} logo={logo} />
       <Routes>
-        <Route path="/about" element={<AboutOSRD />} />
-
         <Route path="" element={<OSRDSTDCM />} />
       </Routes>
       <NotificationsState />
