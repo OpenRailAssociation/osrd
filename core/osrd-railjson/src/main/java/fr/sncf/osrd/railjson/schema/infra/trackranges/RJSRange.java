f8f71593ded28ebdccbe4ed855dad1089c116c33
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2023-01-05 16:36:58 +0100

    core: railjson: remove unused method

--- core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackranges/RJSRange.java
+++ core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/trackranges/RJSRange.java
@@ -15,14 +15,4 @@ public class RJSRange {
         this.begin = begin;
         this.end = end;
     }
-
-    /** Check whether a list of track ranges is overlaping */
-    public static boolean isOverlaping(List<? extends RJSRange> ranges) {
-        ranges.sort(Comparator.comparing(e -> e.begin));
-        for (var i = 0; i < ranges.size() - 1; i++) {
-            if (ranges.get(i).end > ranges.get(i + 1).begin)
-                return true;
-        }
-        return false;
-    }
 }
