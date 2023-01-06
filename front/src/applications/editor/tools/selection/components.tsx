21f5ce624cbc5e9b0a0e34b9b5622b19be71685c
Benoit Simard <contact@bsimard.com>
2023-01-06 13:19:38 +0100

    front: fixing linter issues on editor

--- front/src/applications/editor/tools/selection/components.tsx
+++ front/src/applications/editor/tools/selection/components.tsx
@@ -23,17 +23,15 @@ export const SelectionMessages: FC = () => {
   return t(`Editor.tools.select-items.help.${state.selectionState.type}-selection`);
 };
 
-const SelectionZone: FC<{ newZone?: Zone }> = ({ newZone }) => {
-  return (
-    <>
-      {newZone ? (
-        <Source type="geojson" data={zoneToFeature(newZone)} key="new-zone">
-          <Layer type="line" paint={{ 'line-color': '#666', 'line-dasharray': [3, 3] }} />
-        </Source>
-      ) : null}
-    </>
-  );
-};
+const SelectionZone: FC<{ newZone?: Zone }> = ({ newZone }) => (
+  <>
+    {newZone ? (
+      <Source type="geojson" data={zoneToFeature(newZone)} key="new-zone">
+        <Layer type="line" paint={{ 'line-color': '#666', 'line-dasharray': [3, 3] }} />
+      </Source>
+    ) : null}
+  </>
+);
 
 export const SelectionLayers: FC = () => {
   const {
