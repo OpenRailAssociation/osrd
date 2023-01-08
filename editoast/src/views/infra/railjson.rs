c41e4425c6bf0958a12b2f10a0ea4a7af40dea6b
Florian Amsallem <florian.amsallem@epita.fr>
2023-01-09 00:07:24 +0100

    editoast: improve get railjson endpoint code quality

--- editoast/src/views/infra/railjson.rs
+++ editoast/src/views/infra/railjson.rs
@@ -48,19 +48,15 @@ pub async fn get_railjson(
     exclude_extensions: bool,
     conn: DBConnection,
 ) -> ApiResult<Custom<String>> {
-    if exclude_extensions {
-        let query = include_str!("sql/get_infra_no_ext.sql");
-        let railjson: RailJsonData = conn
-            .run(move |conn| sql_query(query).bind::<Integer, _>(infra).get_result(conn))
-            .await?;
-        Ok(Custom(Status::Ok, railjson.railjson))
+    let query = if exclude_extensions {
+        include_str!("sql/get_infra_no_ext.sql")
     } else {
-        let query = include_str!("sql/get_infra_with_ext.sql");
-        let railjson: RailJsonData = conn
-            .run(move |conn| sql_query(query).bind::<Integer, _>(infra).get_result(conn))
-            .await?;
-        Ok(Custom(Status::Ok, railjson.railjson))
-    }
+        include_str!("sql/get_infra_with_ext.sql")
+    };
+    let railjson: RailJsonData = conn
+        .run(move |conn| sql_query(query).bind::<Integer, _>(infra).get_result(conn))
+        .await?;
+    Ok(Custom(Status::Ok, railjson.railjson))
 }
 
 /// Import an infra
