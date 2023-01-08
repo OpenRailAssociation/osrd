b6b4a565988f2f354a4d10b2cdc5cd5eb1cfc556
Nicolas Wurtz <gitlab@nethenic.net>
2023-01-08 22:14:51 +0100

    front: newWorkflow: beginning study page

--- front/src/applications/osrd/views/Study.js
+++ front/src/applications/osrd/views/Study.js
@@ -1,28 +1,163 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBarSNCF from 'common/BootstrapSNCF/NavBarSNCF';
 import logo from 'assets/pictures/views/study.svg';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import Loader from 'common/Loader';
+import nextId from 'react-id-generator';
+import dayjs from 'dayjs';
+import InputSNCF from 'common/BootstrapSNCF/InputSNCF';
+import OptionsSNCF from 'common/BootstrapSNCF/OptionsSNCF';
+import {
+  projectJSON,
+  studyJSON,
+} from 'applications/osrd/components/Helpers/genFakeDataForProjects';
 
-function BreadCrumbs() {
+dayjs.locale('fr');
+
+function BreadCrumbs(props) {
   const { t } = useTranslation('osrd/project');
+  const { projectName, studyName } = props;
   return (
     <div className="navbar-breadcrumbs">
       <Link to="/osrd">{t('projectsList')}</Link>
       <i className="icons-arrow-next icons-size-x75 text-muted" />
-      <Link to="/osrd/project">Nom du projet</Link>
+      <Link to="/osrd/project">{projectName}</Link>
       <i className="icons-arrow-next icons-size-x75 text-muted" />
-      Nom de l&apos;étude
+      {studyName}
     </div>
   );
 }
 
 export default function Study() {
+  const { t } = useTranslation('osrd/study');
+  const [projectDetails, setProjectDetails] = useState();
+  const [studyDetails, setStudyDetails] = useState();
+  const [scenariosList, setScenariosList] = useState();
+  const [filter, setFilter] = useState('');
+  const [sortOption, setSortOption] = useState('byName');
+
+  const sortOptions = [
+    {
+      label: t('sortOptions.byName'),
+      value: 'byName',
+    },
+    {
+      label: t('sortOptions.byRecentDate'),
+      value: 'byRecentDate',
+    },
+  ];
+
+  const handleSortOptions = (e) => {
+    setSortOption(e.target.value);
+    console.log(e);
+  };
+
+  useEffect(() => {
+    setProjectDetails(projectJSON());
+    setStudyDetails(studyJSON());
+  }, []);
   return (
     <>
-      <NavBarSNCF appName={<BreadCrumbs />} logo={logo} />
+      <NavBarSNCF
+        appName={
+          <BreadCrumbs
+            projectName={projectDetails ? projectDetails.name : null}
+            studyName={studyDetails ? studyDetails.name : null}
+          />
+        }
+        logo={logo}
+      />
       <main className="mastcontainer mastcontainer-no-mastnav">
-        Coucou étude, liste des scénarios
+        <div className="p-3">
+          {projectDetails && studyDetails ? (
+            <>
+              <div className="study-project-details">
+                <div className="study-project-details-img">
+                  <img src={projectDetails.image} alt="project logo" />
+                </div>
+                <div className="study-project-details-name">{projectDetails.name}</div>
+              </div>
+
+              <div className="study-details">
+                <div className="study-details-name">{studyDetails.name}</div>
+                <div className="study-details-type">{studyDetails.type}</div>
+                <div className="study-details-description">{studyDetails.description}</div>
+
+                <div className="study-details-financials">
+                  <div className="study-details-financials-infos">
+                    <div className="study-details-financials-infos-item">
+                      <h3>{t('geremiCode')}</h3>
+                      <div>{studyDetails.geremiCode}</div>
+                    </div>
+                    <div className="study-details-financials-infos-item">
+                      <h3>{t('affairCode')}</h3>
+                      <div>{studyDetails.affairCode}</div>
+                    </div>
+                  </div>
+                  <div className="study-details-financials-amount">
+                    <span className="study-details-financials-amount-text">{t('budget')}</span>
+                    {new Intl.NumberFormat('fr-FR', {
+                      style: 'currency',
+                      currency: 'EUR',
+                      maximumSignificantDigits: 2,
+                    }).format(studyDetails.budget)}
+                  </div>
+                </div>
+
+                <div className="study-details-footer">
+                  <div className="study-details-tags">
+                    {studyDetails.tags.map((tag) => (
+                      <div className="study-details-tags-tag" key={nextId()}>
+                        {tag}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="study-details-date">
+                    <span className="mr-1">{t('modifiedOn')}</span>
+                    {dayjs(studyDetails.lastModifiedDate)
+                      .format('D MMM YYYY HH:mm')
+                      .replace(/\./gi, '')}
+                  </div>
+                </div>
+              </div>
+
+              <div className="scenarios-toolbar">
+                <div className="">{`${scenariosList ? scenariosList.length : 0} ${t(
+                  'scenariosCount'
+                )}`}</div>
+                <div className="flex-grow-1">
+                  <InputSNCF
+                    type="text"
+                    name="projects-filter"
+                    value={filter}
+                    onChange={(e) => setFilter(e.target.value)}
+                    placeholder={t('filterPlaceholder')}
+                    whiteBG
+                    noMargin
+                    unit={<i className="icons-search" />}
+                    sm
+                  />
+                </div>
+                <OptionsSNCF
+                  name="projects-sort-filter"
+                  onChange={handleSortOptions}
+                  selectedValue={sortOption}
+                  options={sortOptions}
+                  sm
+                />
+                <button className="btn btn-primary btn-sm" type="button">
+                  <i className="icons-add" />
+                  <span className="ml-2">{t('createScenario')}</span>
+                </button>
+              </div>
+            </>
+          ) : (
+            <span className="mt-5">
+              <Loader position="center" />
+            </span>
+          )}
+        </div>
       </main>
     </>
   );
