7ff3fb1b6d45fe3cf5b6b32b37bd019d6d254e22
bloussou <brieuc.loussouarn96@gmail.com>
2023-01-10 15:17:59 +0100

    editoast: Add `mvt_view_metadata()` route to editoast

--- editoast/src/views/layers/mod.rs
+++ editoast/src/views/layers/mod.rs
@@ -1,8 +1,10 @@
 mod info;
+mod layer;
 
 use info::info_route;
+use layer::layer_view;
 use rocket::{routes, Route};
 
 pub fn routes() -> Vec<Route> {
-    routes![info_route]
+    routes![info_route, layer_view]
 }
