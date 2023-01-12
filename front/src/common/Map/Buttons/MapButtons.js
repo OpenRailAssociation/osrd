1a4cf3fd84ceaab914df35b6210a36599f1b668b
uriel <uriel.sautron@gmail.com>
2023-01-12 16:58:13 +0100

    Create new state for show modal. Use addEventListener in useEffect to close modal by click outside. Use dispatch to close pop-up map when a modal opens

--- front/src/common/Map/Buttons/MapButtons.js
+++ front/src/common/Map/Buttons/MapButtons.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // Buttons
@@ -12,28 +12,74 @@ import ButtonFullscreen from 'common/ButtonFullscreen';
 import MapSearch from 'common/Map/Search/MapSearch';
 import MapSettings from 'common/Map/Settings/MapSettings';
 import MapKey from 'common/Map/MapKey';
+import InfraSelector from 'common/InfraSelector/InfraSelector';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateFeatureInfoClickOSRD } from 'reducers/osrdconf';
+
+import { getFeatureInfoClick } from 'reducers/osrdconf/selectors';
 import ButtonMapInfras from './ButtonMapInfras';
 
 export default function MapButtons(props) {
   const { resetPitchBearing, withInfraButton } = props;
-  const [showSearch, setShowSearch] = useState(false);
-  const [showSettings, setShowSettings] = useState(false);
-  const [showMapKey, setShowMapKey] = useState(false);
+  const featureInfoClick = useSelector(getFeatureInfoClick);
+  const dispatch = useDispatch();
+
+  const mapModalKeywords = {
+    search: false,
+    settings: false,
+    key: false,
+  };
+
+  const [showMapModal, setShowMapModal] = useState(mapModalKeywords);
+  const toggleMapModal = (keyModal) => {
+    setShowMapModal({
+      ...mapModalKeywords,
+      [keyModal]: !showMapModal[keyModal],
+    });
+    if (featureInfoClick.displayPopup) {
+      dispatch(
+        updateFeatureInfoClickOSRD({
+          displayPopup: false,
+          feature: undefined,
+        })
+      );
+    }
+  };
+
+  const mapButtonsRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (
+        Object.values(showMapModal).some((show) => show === true) &&
+        !mapButtonsRef.current.contains(e.target)
+      ) {
+        setShowMapModal({ ...mapModalKeywords });
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showMapModal]);
 
   return (
-    <>
+    <div ref={mapButtonsRef}>
       <div className="btn-map-container">
-        <ButtonMapSearch toggleMapSearch={() => setShowSearch(!showSearch)} />
-        <ButtonMapSettings toggleMapSettings={() => setShowSettings(!showSettings)} />
-        <ButtonMapKey toggleMapKey={() => setShowMapKey(!showMapKey)} />
+        <ButtonMapSearch toggleMapSearch={() => toggleMapModal('search')} />
+        <ButtonMapSettings toggleMapSettings={() => toggleMapModal('settings')} />
+        <ButtonMapKey toggleMapKey={() => toggleMapModal('key')} />
         <ButtonResetViewport updateLocalViewport={resetPitchBearing} />
         <ButtonFullscreen />
         {withInfraButton && <ButtonMapInfras />}
       </div>
-      <MapSearch active={showSearch} toggleMapSearch={() => setShowSearch(!showSearch)} />
-      <MapSettings active={showSettings} toggleMapSettings={() => setShowSettings(!showSettings)} />
-      {showMapKey && <MapKey toggleMapKey={() => setShowMapKey(!showMapKey)} />}
-    </>
+      <MapSearch active={showMapModal.search} toggleMapSearch={() => toggleMapModal('search')} />
+      <MapSettings
+        active={showMapModal.settings}
+        toggleMapSettings={() => toggleMapModal('settings')}
+      />
+      <MapKey active={showMapModal.key} toggleMapKey={() => toggleMapModal('key')} />
+      <InfraSelector modalOnly modalID="infra-selector-modal-map" />
+    </div>
   );
 }
 
