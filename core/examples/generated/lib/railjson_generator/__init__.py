5c5d8135d13db18a3786b07ae62f481b661e52a4
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2023-01-12 15:38:22 +0100

    rename physical_approximations to external_generated_inputs

--- core/examples/generated/lib/railjson_generator/__init__.py
+++ core/examples/generated/lib/railjson_generator/__init__.py
@@ -6,7 +6,7 @@ assert api_schema_parent_path.exists()
 sys.path.append(str(api_schema_parent_path))
 
 from .infra_builder import InfraBuilder  # noqa
-from .physical_approximations import PhysicalApproximations  # noqa
+from .external_generated_inputs import ExternalGeneratedInputs  # noqa
 from .simulation_builder import SimulationBuilder  # noqa
 from .schema.infra.direction import ApplicableDirection  # noqa
 from .schema.location import Location  # noqa
