4ca9cb8e9740b51f007dd3e08006cca1b146551e
Florian Amsallem <florian.amsallem@epita.fr>
2023-01-12 11:16:07 +0100

    editoast: add pathfinding endpoint

--- editoast/src/infra_cache/graph.rs
+++ editoast/src/infra_cache/graph.rs
@@ -30,12 +30,6 @@ impl<'a> Graph<'a> {
     pub fn load(infra_cache: &'a InfraCache) -> Self {
         let mut graph = Self::default();
 
-        for link in infra_cache.track_section_links().values() {
-            let link = link.unwrap_track_section_link();
-            graph.link(None, &link.src, &link.dst);
-            graph.link(None, &link.dst, &link.src);
-        }
-
         for switch in infra_cache.switches().values() {
             let switch = switch.unwrap_switch();
             let switch_type = infra_cache
@@ -52,6 +46,14 @@ impl<'a> Graph<'a> {
                     graph.switches.insert(src, switch);
                     graph.switches.insert(dst, switch);
                 }
+            }
+        }
+
+        for link in infra_cache.track_section_links().values() {
+            let link = link.unwrap_track_section_link();
+            if !graph.links.contains_key(&link.src) && !graph.links.contains_key(&link.dst) {
+                graph.link(None, &link.src, &link.dst);
+                graph.link(None, &link.dst, &link.src);
             }
         }
 
@@ -78,6 +80,20 @@ impl<'a> Graph<'a> {
         self.links
             .get(&track_endpoint)
             .and_then(|groups| groups.get(&group).copied())
+    }
+
+    /// Given an endpoint return a list of groups.
+    /// If the endpoint has no neightbours return an empty `Vec`.
+    /// If the endpoint has as simple track section link return a `Vec` with a single `None` element.
+    /// Otherwise returns a `Vec` with all the switch groups.
+    pub fn get_neighbour_groups(
+        &'a self,
+        track_endpoint: &'a TrackEndpoint,
+    ) -> Vec<Option<&'a Identifier>> {
+        self.links
+            .get(&track_endpoint)
+            .map(|groups| groups.keys().map(|g| g.as_deref()).collect())
+            .unwrap_or_default()
     }
 }
 
@@ -136,5 +152,42 @@ mod tests {
                 }
             }
         }
+    }
+
+    #[test]
+    fn get_switch() {
+        let infra_cache = create_small_infra_cache();
+        let graph = Graph::load(&infra_cache);
+
+        let track_b_end = create_track_endpoint(Endpoint::End, "B");
+        let switch = graph.get_switch(&track_b_end).unwrap();
+        assert_eq!(switch.obj_id, "switch".to_string());
+
+        let track_a_begin = create_track_endpoint(Endpoint::Begin, "A");
+        assert!(graph.get_switch(&track_a_begin).is_none());
+
+        let track_a_end = create_track_endpoint(Endpoint::End, "A");
+        assert!(graph.get_switch(&track_a_end).is_none());
+    }
+
+    #[test]
+    fn get_neighbour_groups() {
+        let infra_cache = create_small_infra_cache();
+        let graph = Graph::load(&infra_cache);
+
+        let track_b_end = create_track_endpoint(Endpoint::End, "B");
+        let groups = graph.get_neighbour_groups(&track_b_end);
+        assert_eq!(groups.len(), 2);
+        assert!(groups.contains(&Some(&"LEFT".into())));
+        assert!(groups.contains(&Some(&"RIGHT".into())));
+
+        let track_a_begin = create_track_endpoint(Endpoint::Begin, "A");
+        let groups = graph.get_neighbour_groups(&track_a_begin);
+        assert_eq!(groups.len(), 0);
+
+        let track_a_end = create_track_endpoint(Endpoint::End, "A");
+        let groups = graph.get_neighbour_groups(&track_a_end);
+        assert_eq!(groups.len(), 1);
+        assert!(groups.contains(&None));
     }
 }
