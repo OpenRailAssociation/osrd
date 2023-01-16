aa90b7028944baf0b8846866d588ce775aa0e866
Florian Amsallem <florian.amsallem@epita.fr>
2023-01-16 01:42:27 +0100

    editoast: fix documentation warnings

--- editoast/src/client/redis_config.rs
+++ editoast/src/client/redis_config.rs
@@ -6,6 +6,6 @@ use derivative::Derivative;
 pub struct RedisConfig {
     #[derivative(Default(value = r#""redis://localhost:6379".into()"#))]
     #[clap(long, env, default_value = "redis://localhost:6379")]
-    /// Redis url like redis://[:PASSWORD@]HOST[:PORT][/DATABASE]
+    /// Redis url like `redis://[:PASSWORD@]HOST[:PORT][/DATABASE]`
     pub redis_url: String,
 }
