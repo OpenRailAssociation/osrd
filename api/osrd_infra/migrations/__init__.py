1b11cc3f803f0962202ccd39dd3587c307bf58d9
Clara Ni <clara.ni@student-cs.fr>
2023-01-18 11:42:30 +0100

    api: create endpoints to create and retrieve a livery and its compound image

--- api/osrd_infra/migrations/__init__.py
+++ api/osrd_infra/migrations/__init__.py
@@ -2,14 +2,14 @@ import django
 from django.db import migrations, models
 
 
-def run_sql_add_foreign_key(model_name: str, field_name: str, link_model: str, nullable: bool = False):
+def run_sql_complexe_add_foreign_key(table_name: str, field_name: str, model_name: str, link_model: str, link_table: str, nullable: bool):
     return migrations.RunSQL(
-        sql=[(f"""ALTER TABLE osrd_infra_{model_name}
+        sql=[(f"""ALTER TABLE osrd_infra_{table_name}
                 ADD {field_name}_id INTEGER {"NULL" if nullable else ""},
                 ADD CONSTRAINT osrd_{link_model}_{model_name}_fkey FOREIGN KEY ({field_name}_id)
-                   REFERENCES osrd_infra_{link_model}(id) ON DELETE CASCADE
+                   REFERENCES osrd_infra_{link_table}(id) ON DELETE CASCADE
             """)],
-        reverse_sql=[(f"""ALTER TABLE osrd_infra_{model_name}
+        reverse_sql=[(f"""ALTER TABLE osrd_infra_{table_name}
                 DROP CONSTRAINT osrd_{link_model}_{model_name}_fkey,
                 DROP COLUMN {field_name}_id
             """)],
@@ -25,6 +25,17 @@ def run_sql_add_foreign_key(model_name: str, field_name: str, link_model: str, n
                 ),
             ),
         ],
+    )
+
+
+def run_sql_add_foreign_key(model_name: str, field_name: str, link_model: str, nullable: bool = False):
+    return run_sql_complexe_add_foreign_key(
+        table_name=model_name,
+        field_name=field_name,
+        model_name=model_name,
+        link_model=link_model,
+        link_table=link_model,
+        nullable=nullable
     )
 
 
