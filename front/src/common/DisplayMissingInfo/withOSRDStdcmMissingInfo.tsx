455b99c28ded32f5d64ac9d55eb490af1d6aec82
Yohh <durandyohan@zaclys.net>
2023-01-19 10:34:29 +0100

    edit every useSelector with gets

--- front/src/common/DisplayMissingInfo/withOSRDStdcmMissingInfo.tsx
+++ front/src/common/DisplayMissingInfo/withOSRDStdcmMissingInfo.tsx
@@ -1,16 +1,23 @@
 import React, { ComponentType, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { CgNametag } from 'react-icons/cg';
 import { useSelector } from 'react-redux';
+import {
+  getInfraID,
+  getRollingStockID,
+  getSpeedLimitByTag,
+  getTimetableID,
+} from 'reducers/osrdconf/selectors';
 import DisplayMissingInfo from './DisplayMissingInfo';
 
 const withOSRDStdcmMissingInfo = <T extends {}>(Component: ComponentType<T>) => {
   return (hocProps: T) => {
     const { t } = useTranslation('missingInfo');
 
-    const infra = useSelector((state: any) => state.osrdconf.infraID);
-    const timeTable = useSelector((state: any) => state.osrdconf.timetableID);
-    const rollingStock = useSelector((state: any) => state.osrdconf.rollingStockID);
-    const speedLimitByTag = useSelector((state: any) => state.osrdconf.speedLimitByTag);
+    const infra = useSelector(getInfraID);
+    const timeTable = useSelector(getTimetableID);
+    const rollingStock = useSelector(getRollingStockID);
+    const speedLimitByTag = useSelector(getSpeedLimitByTag);
 
     const missingInfoList: string[] = [
       !infra ? t('missingInfra') : '',
