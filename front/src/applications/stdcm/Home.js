9aedf7f858894bac5481a1786322a0df640d9994
Yohh <durandyohan@zaclys.net>
2023-01-19 12:18:42 +0100

    remove MastNavSNCF from STDCM/Home.js, remove fullscreen class & add style in main in OSRDStdcmConfig.tsx

--- front/src/applications/stdcm/Home.js
+++ front/src/applications/stdcm/Home.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
-import MastNavItemSNCF from 'common/BootstrapSNCF/MastNavItemSNCF';
-import MastNavSNCF from 'common/BootstrapSNCF/MastNavSNCF';
 import NavBarSNCF from 'common/BootstrapSNCF/NavBarSNCF';
 import { NotificationsState } from 'common/Notifications';
 import logo from 'assets/pictures/home/stdcm.svg';
@@ -16,23 +14,6 @@ export default function HomeStdcm() {
   const { t } = useTranslation(['home', 'translation']);
   return (
     <>
-      <MastNavSNCF
-        items={
-          <MastNavItemSNCF
-            link="/stdcm"
-            linkname={t('translation:osrd.nav.home')}
-            icon="icons-itinerary-train-station"
-          />
-        }
-        itemsBottom={
-          <MastNavItemSNCF
-            link="/osrd/contact"
-            linkname={t('translation:osrd.nav.contact')}
-            icon="icons-support"
-            bottom
-          />
-        }
-      />
       <NavBarSNCF appName={t('home:stdcm')} logo={logo} />
       <Routes>
         <Route path="/about" element={<AboutOSRD />} />
