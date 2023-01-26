36cdcaed3b59c0448830b0285b55255765b7593d
Alexandre Damiron <alexandredamiron@gmail.com>
2023-01-26 17:10:21 +0100

    no generic import

--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,5 +1,4 @@
 import('@sncf/bootstrap-sncf.metier.reseau/dist/css/bootstrap-sncf.min.css');
-import './main.css'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
