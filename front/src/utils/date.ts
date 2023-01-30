cf2fceb10920e39bc98622c74988d40a29f2e180
Nicolas Wurtz <gitlab@nethenic.net>
2023-01-30 08:13:44 +0100

    front: newWorkflow2: memoize all cards, use timezone for dates

--- front/src/utils/date.ts
+++ front/src/utils/date.ts
@@ -1,9 +1,16 @@
 import dayjs from 'dayjs';
+import 'dayjs/locale/fr';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.locale('fr');
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export function formatIsoDate(date: Date) {
   return date.toISOString().substring(0, 10);
 }
 
 export function dateTimeFrenchFormatting(date: Date) {
-  return dayjs(date).format('D MMM YYYY HH:mm').replace(/\./gi, '');
+  return dayjs.utc(date).tz(dayjs.tz.guess()).format('D MMM YYYY HH:mm').replace(/\./gi, '');
 }
