b2fd277277dc028782f81d939d04d5e4cee9b528
Nicolas Wurtz <gitlab@nethenic.net>
2023-01-30 23:33:04 +0100

    front: newWorkflow2: fixes on sticky bar

--- front/src/applications/operationalStudies/components/SimulationResults/TrainDetails.tsx
+++ front/src/applications/operationalStudies/components/SimulationResults/TrainDetails.tsx
@@ -44,33 +44,33 @@ export default function TrainDetails() {
     <div className="d-flex">
       {positionValues.headPosition && timePosition && (
         <>
-          <div className="rounded px-1 train-detail small bg-blue text-white text-nowrap mr-1">
+          <div className="rounded px-1 train-detail small bg-blue text-white text-nowrap mr-2">
             <div className="font-weight-bold text-uppercase">{t('trainDetails.headPosition')}</div>
             {positionValues.headPosition && Math.round(positionValues.headPosition.position) / 1000}
             km
           </div>
-          <div className="rounded px-1 train-detail small bg-cyan text-white text-nowrap mr-1">
+          <div className="rounded px-1 train-detail small bg-cyan text-white text-nowrap mr-2">
             <div className="font-weight-bold text-uppercase">{t('trainDetails.tailPosition')}</div>
             {positionValues.tailPosition && Math.round(positionValues.tailPosition.position) / 1000}
             km
           </div>
           {occupancyBounds && (
             <>
-              <div className="rounded px-1 train-detail small bg-yellow text-black text-nowrap mr-1">
+              <div className="rounded px-1 train-detail small bg-success text-black text-nowrap mr-2">
                 <div className="font-weight-bold text-uppercase">
                   {t('trainDetails.routeBeginOccupancy')}
                 </div>
                 {Math.round(occupancyBounds[0]) / 1000}
                 km
               </div>
-              <div className="rounded px-1 train-detail small bg-red text-white text-nowrap mr-1">
+              <div className="rounded px-1 train-detail small bg-yellow text-black text-nowrap mr-2">
                 <div className="font-weight-bold text-uppercase">
                   {t('trainDetails.routeEndOccupancy')}
                 </div>
                 {Math.round(occupancyBounds[1]) / 1000}
                 km
               </div>
-              <div className="rounded px-1 train-detail small bg-secondary text-white text-nowrap mr-1">
+              <div className="rounded px-1 train-detail small bg-secondary text-white text-nowrap mr-2">
                 <div className="font-weight-bold text-uppercase">
                   {t('trainDetails.routeSizeOccupancy')}
                 </div>
