8b78af573750d0ca9775df71820ecea4baad335b
uriel <uriel.sautron@gmail.com>
2023-02-01 14:53:38 +0100

    front: playwright: InfraSelectorModalBodyStandard: add data-testid in button infra. stdcm-page-model: add translation file. stdcm-page.spec: add a loop to check the infra are well displayed

--- front/tests/stdcm-page.spec.ts
+++ front/tests/stdcm-page.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { PlaywrightHomePage } from './home-page-model';
-import { dataInfraFourItems } from './playwright-data-test';
+import playwrightDataTest from './playwright-data-test.json';
 import { PlaywrightSTDCMPage } from './stdcm-page-model';
 
+const { dataInfraFourItems } = playwrightDataTest;
+
 // Describe the test suite for the STDCM page
-test.describe.only('STDCM page', () => {
+test.describe('STDCM page', () => {
   // Declare the necessary variables for the test suite
   let playwrightHomePage: PlaywrightHomePage;
   let playwrightSTDCMPage: PlaywrightSTDCMPage;
@@ -12,12 +14,16 @@ test.describe.only('STDCM page', () => {
   // This function will run before all the tests in this suite
   test.beforeAll(async ({ browser }) => {
     const page = await browser.newPage();
+
     // Create an instance of the PlaywrightHomePage class
     playwrightHomePage = new PlaywrightHomePage(page);
+
     // Create an instance of the PlaywrightSTDCMPage class
     playwrightSTDCMPage = new PlaywrightSTDCMPage(page);
+
     // Go to the home page of OSDR
     await playwrightHomePage.goToHomePage();
+
     // Navigate to the "STDCM" page
     await playwrightHomePage.goToSTDCMPage();
   });
@@ -43,6 +49,33 @@ test.describe.only('STDCM page', () => {
     // Check that the number of infrastructure items is 4
     expect(playwrightSTDCMPage.getInfraListItems).toHaveCount(4);
 
+    // Loop on the infrastructures and check that the details are correct.
+    dataInfraFourItems.forEach((infra) => {
+      const { name, locked, id, railjson_version, version } = infra;
+      const infraButton = playwrightSTDCMPage.page.getByTestId(`infraslist-item-${id}`);
+
+      // Check if the infrastructure has the correct class for locked/unlocked
+      expect(infraButton).toHaveClass(locked ? /locked/ : /unlocked/);
+
+      // Check that the infrastructure is not selected
+      expect(infraButton).not.toHaveClass('active');
+
+      // Check if the infrastructure name, ID, RAILJSON version, and version are correctly displayed
+      expect(infraButton.locator('span')).toContainText([
+        name,
+        `ID ${id}`,
+        `RAILJSON V${railjson_version}`,
+        `V${version}`,
+      ]);
+    });
+
+    // Check the number of infrastructures displayed
     await playwrightSTDCMPage.checkNumberOfInfra(4);
+
+    // Click on the page body to close the modal
+    await playwrightSTDCMPage.getBody.click();
+
+    // Check if the modal is closed
+    await playwrightSTDCMPage.getModalClose();
   });
 });
