96febd910603e4daad8d5e8db8c692b57444f7fd
multun <victor.collod@epita.fr>
2023-02-01 19:04:00 +0100

    README: various improvements (#3057)
    - move acknowledgements to the website
    - make the project's description clearer
    - add working getting started instructions
    - make generate-infra.sh load small_infra by default
    - add sponsor logos

--- scripts/generate-infra.sh
+++ scripts/generate-infra.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-cd $(dirname $0)/..
-infra_name=${1:-tiny_infra}
-echo Loading $infra_name
-python3 core/examples/generated/generate.py /tmp/generated_infras/ > /dev/null
-docker cp "/tmp/generated_infras/${infra_name}/infra.json" osrd-api:/tmp/infra.json
-docker exec osrd-api python manage.py import_railjson "${infra_name}" /tmp/infra.json 2> /dev/null
-echo Generate layers
-docker exec osrd-editoast editoast generate > /dev/null
+cd "$(dirname "$0")/.."
 
-echo Loading example rolling stock
+infra_name="${1:-small_infra}"
+
+echo "Loading $infra_name"
+python3 core/examples/generated/generate.py /tmp/generated_infras/
+docker cp "/tmp/generated_infras/${infra_name}/infra.json" osrd-api:/tmp/infra.json
+docker exec osrd-api python manage.py import_railjson "${infra_name}" /tmp/infra.json
+
+echo "Generate layers"
+docker exec osrd-editoast editoast generate
+
+echo "Loading example rolling stock"
 docker cp api/static/example_rolling_stock.json osrd-api:/tmp/stock.json
-docker exec osrd-api python manage.py import_rolling_stock -f /tmp/stock.json 2> /dev/null
+docker exec osrd-api python manage.py import_rolling_stock -f /tmp/stock.json
