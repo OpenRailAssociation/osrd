7ad787d2423a131f02db73d46d9a66bd815bd8d5
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-02-08 14:37:38 +0100

    api : rebase and add fixes.

--- api/osrd_infra/migrations/0005_operationalstudy_project_remove_timetable_infra_and_more.py
+++ api/osrd_infra/migrations/0005_operationalstudy_project_remove_timetable_infra_and_more.py
@@ -10,7 +10,7 @@ from osrd_infra.migrations import run_sql_add_foreign_key
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("osrd_infra", "0003_rollingstockimages"),
+        ("osrd_infra", "0004_trainschedulemodel_options"),
     ]
 
     operations = [
