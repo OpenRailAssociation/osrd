6264726fe1a0322e3e2bd5aadbd7b0addda12c49
uriel <uriel.sautron@gmail.com>
2023-02-10 12:05:28 +0100

    front: MapSearch: remove div

--- front/src/common/Map/Search/MapSearch.js
+++ front/src/common/Map/Search/MapSearch.js
@@ -34,52 +34,50 @@ export default function MapSearch(props) {
   ];
 
   return (
-    <div className="div-modal">
-      <div className={`map-modal${active ? ' active' : ''}`}>
-        <div className="d-flex justify-content-between align-items-start">
-          <div className="h2">{t('translation:common.search')}</div>
-          <button type="button" className="close" onClick={toggleMapSearch}>
-            ×
-          </button>
-        </div>
+    <div className={`map-modal${active ? ' active' : ''}`}>
+      <div className="d-flex justify-content-between align-items-start">
+        <div className="h2">{t('translation:common.search')}</div>
+        <button type="button" className="close" onClick={toggleMapSearch}>
+          ×
+        </button>
+      </div>
 
-        <div className="actionbar">
-          <nav role="navigation" className="position-relative mt-2">
-            <ul className="nav nav navtabs mb-0 dragscroll" role="tablist" id="listeTitreOnglets">
-              {tabItems.map((tab, index) => (
-                <li className="navtabs-item pr-4" key={`tab-${tab.title}`}>
-                  <a
-                    href={`#tab${index + 1}`}
-                    className={tab.className}
-                    id={`titletab${index + 1}`}
-                    data-toggle="tab"
-                    role="tab"
-                    aria-controls={`tab${index + 1}`}
-                    aria-selected="true"
-                  >
-                    {tab.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+      <div className="actionbar">
+        <nav role="navigation" className="position-relative mt-2">
+          <ul className="nav nav navtabs mb-0 dragscroll" role="tablist" id="listeTitreOnglets">
+            {tabItems.map((tab, index) => (
+              <li className="navtabs-item pr-4" key={`tab-${tab.title}`}>
+                <a
+                  href={`#tab${index + 1}`}
+                  className={tab.className}
+                  id={`titletab${index + 1}`}
+                  data-toggle="tab"
+                  role="tab"
+                  aria-controls={`tab${index + 1}`}
+                  aria-selected="true"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
 
-        <div className="tab-content" id="myTabContent">
-          <div
-            className="tab-pane fade show active"
-            id="tab1"
-            role="tabpanel"
-            aria-labelledby="titletab1"
-          >
-            <MapSearchStation updateExtViewport={updateViewportChange} />
-          </div>
-          <div className="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="titletab2">
-            <MapSearchSignalBox updateExtViewport={updateViewportChange} />
-          </div>
-          <div className="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="titletab3">
-            <MapSearchSignal updateExtViewport={updateViewportChange} />
-          </div>
+      <div className="tab-content" id="myTabContent">
+        <div
+          className="tab-pane fade show active"
+          id="tab1"
+          role="tabpanel"
+          aria-labelledby="titletab1"
+        >
+          <MapSearchStation updateExtViewport={updateViewportChange} />
+        </div>
+        <div className="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="titletab2">
+          <MapSearchSignalBox updateExtViewport={updateViewportChange} />
+        </div>
+        <div className="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="titletab3">
+          <MapSearchSignal updateExtViewport={updateViewportChange} />
         </div>
       </div>
     </div>
