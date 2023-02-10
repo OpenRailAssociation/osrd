d984460977de406c768726f4b396bb0fdbf8971e
Nicolas Wurtz <gitlab@nethenic.net>
2023-02-10 17:30:00 +0100

    front: ElectricalProfiles: delete useless consts

--- front/src/applications/operationalStudies/components/operationalStudiesConsts.js
+++ front/src/applications/operationalStudies/components/operationalStudiesConsts.js
@@ -2,17 +2,3 @@ export const PROJECTS_URI = '/projects/';
 export const STUDIES_URI = '/studies/';
 export const SCENARIOS_URI = '/scenarios/';
 export const ELECTRICAL_PROFILE_SET_URI = '/editoast/electrical_profile_set/';
-
-// To be removed when API endpoint will be given to gather these two arrays
-export const STUDY_TYPES = [
-  'TimeTable',
-  'FlowRate',
-  'ParkSizing',
-  'GarageRequirement',
-  'OperationOrSizing',
-  'Operability',
-  'StrategicPlanning',
-  'ChartStability',
-  'Disturbance',
-];
-export const STUDY_STATES = ['Started', 'InProgress', 'Finish'];
