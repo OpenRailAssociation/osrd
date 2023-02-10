65150d377986f060cbe8af30ad1eb0cb48f70c34
Leo Valais <leo.valais97@gmail.com>
2023-02-10 18:00:14 +0100

    api:search: remove immutable_unaccent in init_db

--- init_db.sql
+++ init_db.sql
@@ -9,19 +9,4 @@ create extension unaccent;
 
 grant all privileges on schema public to osrd;
 
--- Defining C lang functions require elevation so that's why those functions
--- are defined here. Cf. https://stackoverflow.com/a/11007216
-
-CREATE OR REPLACE FUNCTION public.immutable_unaccent(regdictionary, text)
-    RETURNS text
-    LANGUAGE c IMMUTABLE PARALLEL SAFE STRICT AS
-        '$libdir/unaccent', 'unaccent_dict';
-
-CREATE OR REPLACE FUNCTION public.f_unaccent(text)
-    RETURNS text
-    LANGUAGE sql IMMUTABLE PARALLEL SAFE STRICT
-BEGIN ATOMIC
-    SELECT immutable_unaccent(regdictionary 'unaccent', $1);
-END;
-
 create database osrd TEMPLATE template_osrd;
