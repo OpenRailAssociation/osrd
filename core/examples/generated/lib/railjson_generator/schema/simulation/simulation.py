81a3114c9f939d26da12c0dd0a4c1c94c9e59484
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2023-02-17 10:31:40 +0100

    core: infra-generator: use common example rolling stock files

--- core/examples/generated/lib/railjson_generator/schema/simulation/simulation.py
+++ core/examples/generated/lib/railjson_generator/schema/simulation/simulation.py
@@ -1,10 +1,21 @@
 import json
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import List
 
-from railjson_generator.rjs_static import ROLLING_STOCKS
 from railjson_generator.schema.simulation.train_schedule import TrainSchedule
 from railjson_generator.schema.simulation.train_succession_table import TST
+
+
+ROLLING_STOCKS = {}
+for path in (
+    Path(__file__)
+    .parent.parent.parent.parent.parent.parent.joinpath("rolling_stocks")
+    .iterdir()
+):
+    with open(path) as f:
+        rs = json.load(f)
+        ROLLING_STOCKS[rs["name"]] = rs
 
 
 @dataclass
@@ -13,12 +24,13 @@ class Simulation:
     train_succession_tables: List[TST] = field(default_factory=list)
 
     def format(self):
+        rs_names = {train.rolling_stock for train in self.train_schedules}
         return {
             "train_schedules": [train.format() for train in self.train_schedules],
             "train_succession_tables": [
                 tst.format() for tst in self.train_succession_tables
             ],
-            "rolling_stocks": ROLLING_STOCKS,
+            "rolling_stocks": [ROLLING_STOCKS[name] for name in rs_names],
         }
 
     def save(self, path):
