4c39bed04f2747fe307a033d61f878df60c7b370
Victor "multun" Collod <victor.collod@epita.fr>
2023-02-18 11:44:45 +0100

    front: use swc for dev builds
    Swc is an faster alternative to babel. Babel is still used for production builds.
    Please refer to the documentation of @vitejs/plugin-react-swc

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
