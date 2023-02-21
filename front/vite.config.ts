39c7ba0312683f333b2d0196c0534ce6e54e3273
Victor "multun" Collod <victor.collod@epita.fr>
2023-02-21 18:07:45 +0100

    front: integrate linters with vite

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,14 @@ export default defineConfig({
     svgrPlugin(),
     ImportMetaEnvPlugin.vite({
       example: ".env.example",
+    }),
+    checker({
+      typescript: {
+        buildMode: true,
+      },
+      eslint: {
+        lintCommand: 'eslint --ext .ts,.tsx,.js,.jsx src',
+      },
     }),
   ],
   build: {
