813bdb5a50764dca830786c0296b15cc138bf129
uriel <uriel.sautron@gmail.com>
2023-02-22 15:11:16 +0100

    front: playwright: playwright-data-test: add project, study, scenario and rollingstock. stdcm-page-model: create locator and function for rollingstock selector test. stdcm-page.spec: add test to check the display of the rolling stock list.

--- front/tests/stdcm-page-model.ts
+++ front/tests/stdcm-page-model.ts
@@ -1,5 +1,5 @@
+import { expect, Locator, Page } from '@playwright/test';
 /* eslint-disable import/prefer-default-export */
-import { Locator, Page } from '@playwright/test';
 import infraManagement from '../public/locales/fr/infraManagement.json';
 
 export class PlaywrightSTDCMPage {
@@ -9,15 +9,85 @@ export class PlaywrightSTDCMPage {
 
   readonly translation: typeof infraManagement;
 
+  // Scenario Explorator
+  readonly getScenarioExplorator: Locator;
+
+  readonly getScenarioExploratorModal: Locator;
+
+  readonly getItemsScenarioExplorator: Locator;
+
+  // Rollingstock
+  readonly getRollingStockSelector: Locator;
+
+  readonly getRollingstockModal: Locator;
+
+  readonly getRollingStockSearch: Locator;
+
+  readonly getRollingStockSearchFilter: Locator;
+
+  readonly getRollingStockSearchList: Locator;
+
+  readonly getRollingStockListItem: Locator;
+
+  readonly getRollingstockSpanNames: Locator;
+
   constructor(page: Page) {
     this.page = page;
-
-    // Initialize locators
     this.getBody = page.locator('body');
     this.translation = infraManagement;
+
+    // Scenario Explorator
+    this.getScenarioExplorator = page.getByTestId('scenario-explorator');
+    this.getScenarioExploratorModal = page.locator('.scenario-explorator-modal');
+    this.getItemsScenarioExplorator = page.locator(
+      '.scenario-explorator-modal-part-itemslist-minicard'
+    );
+
+    // Rollingstock
+    this.getRollingstockModal = page.locator('.modal-dialog');
+    this.getRollingStockSelector = page.getByTestId('rollingstock-selector');
+    this.getRollingStockSearch = page.locator('.rollingstock-search');
+    this.getRollingStockSearchFilter = page.locator('.rollingstock-search-filters');
+    this.getRollingStockSearchList = page.locator('.rollingstock-search-list');
+    this.getRollingStockListItem = page.locator('.rollingstock-container');
+    this.getRollingstockSpanNames = this.getRollingStockListItem.locator('.rollingstock-infos-end');
   }
 
   getTranslations(key: keyof typeof infraManagement) {
     return this.translation[key];
+  }
+
+  // Scenario Explorator
+  async openScenarioExplorator() {
+    await this.getScenarioExplorator.click();
+  }
+
+  async getScenarioExploratorModalClose() {
+    await expect(this.getScenarioExploratorModal).not.toBeVisible();
+  }
+
+  async getScenarioExploratorModalOpen() {
+    await expect(this.getScenarioExploratorModal).toBeVisible();
+  }
+
+  getItemScenarioExploratorByName(itemName: string): Locator {
+    return this.getItemsScenarioExplorator.getByText(itemName);
+  }
+
+  async clickItemScenarioExploratorByName(itemName: string) {
+    await this.getItemScenarioExploratorByName(itemName).click();
+  }
+
+  // Rollingstock
+  async getRollingstockModalOpen() {
+    await expect(this.getRollingstockModal).toBeVisible();
+  }
+
+  async getRollingstockModalClose() {
+    await expect(this.getRollingstockModal).not.toBeVisible();
+  }
+
+  async openRollingstockModal() {
+    await this.getRollingStockSelector.click();
   }
 }
