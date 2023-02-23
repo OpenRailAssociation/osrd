64ddb4d9716013209d8e30e2fae68067db13ff8f
bloussou <brieuc.loussouarn96@gmail.com>
2023-02-23 15:40:19 +0100

    api: Split `TrainSchedule` input and output

--- python/api/osrd_infra/models/train_schedule.py
+++ python/api/osrd_infra/models/train_schedule.py
@@ -1,22 +1,23 @@
 from django.contrib.gis.db import models
 from osrd_schemas.rolling_stock import ComfortType
 from osrd_schemas.train_schedule import (
-    MRSP,
     Allowances,
     TrainScheduleLabels,
     TrainScheduleOptions,
 )
 
-from osrd_infra.models import PathModel, RollingStock, Timetable
 from osrd_infra.utils import PydanticValidator
 
+from .path import PathModel
+from .rolling_stock import RollingStock
+from .timetable import Timetable
 
-class TrainScheduleModel(models.Model):
+
+class TrainSchedule(models.Model):
     train_name = models.CharField(max_length=128)
     labels = models.JSONField(default=list, validators=[PydanticValidator(TrainScheduleLabels)])
     timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE, related_name="train_schedules")
 
-    # Simulation inputs
     rolling_stock = models.ForeignKey(RollingStock, on_delete=models.CASCADE)
     departure_time = models.FloatField()
     path = models.ForeignKey(PathModel, on_delete=models.CASCADE)
@@ -27,9 +28,3 @@ class TrainScheduleModel(models.Model):
     )
     speed_limit_tags = models.CharField(max_length=128, null=True)
     options = models.JSONField(null=True, blank=True, validators=[PydanticValidator(TrainScheduleOptions)])
-
-    # Simulation outputs
-    mrsp = models.JSONField(validators=[PydanticValidator(MRSP)])
-    base_simulation = models.JSONField()
-    eco_simulation = models.JSONField(null=True)
-    modes_and_profiles = models.JSONField(default=list)
