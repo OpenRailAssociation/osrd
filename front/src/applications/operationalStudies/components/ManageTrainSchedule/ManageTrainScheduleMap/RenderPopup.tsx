676f93dd6d9e5a1198490c37845fbe7271b94a69
Nicolas Wurtz <gitlab@nethenic.net>
2023-02-27 10:34:10 +0100

    front: correct bug on popup & add track & line details

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/ManageTrainScheduleMap/RenderPopup.tsx
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/ManageTrainScheduleMap/RenderPopup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { Popup } from 'react-map-gl';
 import { useSelector } from 'react-redux';
@@ -7,9 +8,15 @@ import { RiMapPin2Fill, RiMapPin3Fill, RiMapPin5Fill } from 'react-icons/ri';
 import setPointIti from 'applications/operationalStudies/components/ManageTrainSchedule/ManageTrainScheduleMap/setPointIti';
 import { useTranslation } from 'react-i18next';
 
-export default function RenderPopup() {
+type FeatureInfoClickType = {
+  displayPopup: boolean;
+  coordinates?: number[];
+  feature?: any;
+};
+
+function RenderPopup() {
   const { t } = useTranslation(['operationalStudies/manageTrainSchedule']);
-  const featureInfoClick = useSelector(getFeatureInfoClick);
+  const featureInfoClick: FeatureInfoClickType = useSelector(getFeatureInfoClick);
 
   if (
     featureInfoClick.displayPopup &&
@@ -27,34 +34,48 @@ export default function RenderPopup() {
         longitude={featureInfoClick.coordinates[0]}
         latitude={featureInfoClick.coordinates[1]}
         closeButton={false}
-        className="map-hover-custom-popup"
+        closeOnClick={false}
+        className="map-popup-click-select"
       >
-        <button
-          className="btn btn-sm btn-success"
-          type="button"
-          onClick={() => setPointIti('start', properties)}
-        >
-          <RiMapPin2Fill />
-          <span className="d-none">{t('origin')}</span>
-        </button>
-        <button
-          className="btn btn-sm btn-info"
-          type="button"
-          onClick={() => setPointIti('via', properties)}
-        >
-          <RiMapPin3Fill />
-          <span className="d-none">{t('via')}</span>
-        </button>
-        <button
-          className="btn btn-sm btn-warning"
-          type="button"
-          onClick={() => setPointIti('end', properties)}
-        >
-          <RiMapPin5Fill />
-          <span className="d-none">{t('destination')}</span>
-        </button>
+        <div className="details">
+          <div className="details-track">
+            {featureInfoClick.feature.properties.extensions_sncf_track_name}
+            <small>{featureInfoClick.feature.properties.extensions_sncf_line_code}</small>
+          </div>
+          <div className="details-line">
+            {featureInfoClick.feature.properties.extensions_sncf_line_name}
+          </div>
+        </div>
+        <div className="actions">
+          <button
+            className="btn btn-sm btn-success"
+            type="button"
+            onClick={() => setPointIti('start', properties)}
+          >
+            <RiMapPin2Fill />
+            <span className="d-none">{t('origin')}</span>
+          </button>
+          <button
+            className="btn btn-sm btn-info"
+            type="button"
+            onClick={() => setPointIti('via', properties)}
+          >
+            <RiMapPin3Fill />
+            <span className="d-none">{t('via')}</span>
+          </button>
+          <button
+            className="btn btn-sm btn-warning"
+            type="button"
+            onClick={() => setPointIti('end', properties)}
+          >
+            <RiMapPin5Fill />
+            <span className="d-none">{t('destination')}</span>
+          </button>
+        </div>
       </Popup>
     );
   }
   return null;
 }
+
+export default React.memo(RenderPopup);
