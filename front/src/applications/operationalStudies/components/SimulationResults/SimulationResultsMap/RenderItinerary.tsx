8ecb4ee9445490dcd882ba8e65bfd5b8b25fbcfd
uriel <uriel.sautron@gmail.com>
2023-02-28 18:15:53 +0100

    front: SearchContext: create serach context. RenderItynerary: refacto to use it for search line

--- front/src/applications/operationalStudies/components/SimulationResults/SimulationResultsMap/RenderItinerary.tsx
+++ front/src/applications/operationalStudies/components/SimulationResults/SimulationResultsMap/RenderItinerary.tsx
@@ -10,29 +10,31 @@ import OrderedLayer from 'common/Map/Layers/OrderedLayer';
 interface RenderItineraryProps {
   geojsonPath: Feature<LineString>;
   layerOrder: number;
+  isSearchLine?: boolean;
 }
 
 export default function RenderItinerary(props: RenderItineraryProps) {
-  const { geojsonPath, layerOrder } = props;
+  const { geojsonPath, layerOrder, isSearchLine } = props;
   const { selectedTrain, allowancesSettings } = useSelector(
     (state: RootState) => state.osrdsimulation
   );
   const simulation = useSelector((state: RootState) => state.osrdsimulation.simulation.present);
   const trainID = simulation.trains[selectedTrain].id;
+  const paint = isSearchLine
+    ? {
+        'line-width': 3,
+        'line-color':
+          allowancesSettings?.[trainID] && allowancesSettings[trainID].ecoBlocks
+            ? '#82be00'
+            : '#303383',
+      }
+    : {
+        'line-width': 3,
+        'line-color': '#82be00',
+      };
   return (
     <Source type="geojson" data={geojsonPath}>
-      <OrderedLayer
-        id="geojsonPath"
-        type="line"
-        paint={{
-          'line-width': 3,
-          'line-color':
-            allowancesSettings?.[trainID] && allowancesSettings[trainID].ecoBlocks
-              ? '#82be00'
-              : '#303383',
-        }}
-        layerOrder={layerOrder}
-      />
+      <OrderedLayer id="geojsonPath" type="line" paint={paint} layerOrder={layerOrder} />
     </Source>
   );
 }
