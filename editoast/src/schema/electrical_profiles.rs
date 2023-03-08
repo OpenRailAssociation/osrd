842252068cee3c657c4b81f60d02a9147a433efc
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-03-08 19:06:38 +0100

    editoast : refactor electrical_profiles strcutures to restrain fields

--- editoast/src/schema/electrical_profiles.rs
+++ editoast/src/schema/electrical_profiles.rs
@@ -1,16 +1,15 @@
+use crate::schema::TrackRange;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use crate::schema::TrackRange;
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct ElectricalProfile {
     pub value: String,
     pub power_class: String,
     pub track_ranges: Vec<TrackRange>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct ElectricalProfileSetData {
     pub levels: Vec<ElectricalProfile>,
     pub level_order: HashMap<String, Vec<String>>,
