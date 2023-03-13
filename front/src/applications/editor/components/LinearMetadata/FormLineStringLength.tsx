ddfb957892731ec913a4ec01661df21b8d6c5f12
Benoit Simard <contact@bsimard.com>
2023-03-13 15:45:44 +0100

    front: fix #3491

--- front/src/applications/editor/components/LinearMetadata/FormLineStringLength.tsx
+++ front/src/applications/editor/components/LinearMetadata/FormLineStringLength.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { WidgetProps } from '@rjsf/core';
 import { useTranslation } from 'react-i18next';
-
+import { isNil, toNumber, isEmpty } from 'lodash';
 import { getLineStringDistance, DISTANCE_ERROR_RANGE } from './data';
 
 export const FormLineStringLength: React.FC<WidgetProps> = (props) => {
   const { t } = useTranslation();
   const { id, value, required, readonly, onChange, formContext } = props;
 
-  const [length, setLength] = useState<number>(value || 0);
+  const [length, setLength] = useState<number | string>(value);
   const [min, setMin] = useState<number>(-Infinity);
   const [max, setMax] = useState<number>(Infinity);
   const [geoLength, setGeoLength] = useState<number>(0);
 
   useEffect(() => {
-    setLength(value || 0);
+    setLength(value);
   }, [value]);
 
   /**
@@ -42,16 +42,29 @@ export const FormLineStringLength: React.FC<WidgetProps> = (props) => {
           id={id}
           required={required}
           type="number"
-          value={length}
+          value={!isNil(length) ? length : ''}
           onChange={(e) => {
-            const nValue = parseFloat(e.target.value);
-            onChange(nValue);
+            setLength(e.target.value);
+            onChange(!isEmpty(e.target.value) ? toNumber(e.target.value) : null);
           }}
         />
       )}
-      {geoLength && (length === undefined || length < min || length > max) && (
+      {geoLength && (length < min || length > max) && (
         <p className="text-warning">
           {t('Editor.errors.length-out-of-sync-with-geometry', { min, max })}.{' '}
+          <button
+            type="button"
+            className="btn btn-link btn-secondary"
+            title={t('Editor.linear-metadata.sync-length-with-geometry')}
+            onClick={() => onChange(geoLength)}
+          >
+            {t('Editor.linear-metadata.sync-length-with-geometry')}
+          </button>
+        </p>
+      )}
+      {isNil(length) && (
+        <p className="text-danger">
+          {t('Editor.errors.length-number-and-required')}
           <button
             type="button"
             className="btn btn-link btn-secondary"
