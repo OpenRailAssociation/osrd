2e7a81936851d354ff1f358b290892a12a7de509
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-03-14 17:49:21 +0100

    editoast : migrate update endpoint for study

--- editoast/src/views/study.rs
+++ editoast/src/views/study.rs
@@ -8,6 +8,7 @@ use crate::models::StudyWithScenarios;
 use crate::views::pagination::{PaginatedResponse, PaginationQueryParam};
 use crate::DbPool;
 use actix_web::dev::HttpServiceFactory;
+use actix_web::patch;
 use actix_web::web::{self, Data, Json, Path, Query};
 use actix_web::{delete, get, post, HttpResponse};
 use chrono::NaiveDateTime;
@@ -21,7 +22,7 @@ use thiserror::Error;
 pub fn routes() -> impl HttpServiceFactory {
     web::scope("/studies")
         .service((create, list))
-        .service(web::scope("/{study}").service((delete, get)))
+        .service(web::scope("/{study}").service((delete, get, patch)))
 }
 
 #[derive(Debug, Error, EditoastError)]
@@ -40,7 +41,7 @@ enum StudyError {
 /// This structure is used by the post endpoint to create a study
 #[derive(Serialize, Deserialize, Derivative)]
 #[derivative(Default)]
-pub struct StudyCreateForm {
+struct StudyCreateForm {
     pub name: String,
     #[serde(default)]
     pub description: String,
@@ -171,6 +172,73 @@ async fn get(db_pool: Data<DbPool>, path: Path<(i64, i64)>) -> Result<Json<Study
     Ok(Json(study_scenarios))
 }
 
+/// This structure is used by the patch endpoint to patch a study
+#[derive(Serialize, Deserialize, Derivative)]
+#[derivative(Default)]
+struct StudyPatchForm {
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub start_date: Option<NaiveDateTime>,
+    pub expected_end_date: Option<NaiveDateTime>,
+    pub actual_end_date: Option<NaiveDateTime>,
+    pub business_code: Option<String>,
+    pub service_code: Option<String>,
+    pub budget: Option<i32>,
+    pub tags: Option<Vec<String>>,
+    pub state: Option<String>,
+    pub study_type: Option<String>,
+}
+
+impl StudyPatchForm {
+    fn into_study(self, project_id: i64, study_id: i64) -> Study {
+        Study {
+            id: Some(study_id),
+            name: self.name,
+            project_id: Some(project_id),
+            description: self.description,
+            start_date: Some(self.start_date),
+            expected_end_date: Some(self.expected_end_date),
+            actual_end_date: Some(self.actual_end_date),
+            budget: self.budget,
+            business_code: self.business_code,
+            service_code: self.service_code,
+            state: self.state,
+            tags: self.tags,
+            study_type: self.study_type,
+            ..Default::default()
+        }
+    }
+}
+
+#[patch("")]
+async fn patch(
+    data: Json<StudyPatchForm>,
+    path: Path<(i64, i64)>,
+    db_pool: Data<DbPool>,
+) -> Result<Json<StudyWithScenarios>> {
+    let (project_id, study_id) = path.into_inner();
+
+    // Check if project exists
+    let project = match Project::retrieve(db_pool.clone(), project_id).await? {
+        None => return Err(StudyError::ProjectNotFound { project_id }.into()),
+        Some(project) => project,
+    };
+
+    // Update study
+    let study = data.into_inner().into_study(project_id, study_id);
+    let study = match study.update(db_pool.clone()).await? {
+        Some(study) => study,
+        None => return Err(StudyError::NotFound { study_id }.into()),
+    };
+
+    // Update project last_modification field
+    let project = project.update_last_modified(db_pool.clone()).await?;
+    project.expect("Project should exist");
+
+    let study_scenarios = study.with_scenarios(db_pool).await?;
+    Ok(Json(study_scenarios))
+}
+
 #[cfg(test)]
 mod test {
     use crate::models::Project;
@@ -190,7 +258,7 @@ mod test {
         let project: Project = read_body_json(response).await;
         let project_id = project.id.unwrap();
         TestRequest::post()
-            .uri(format!("/projects/{project_id}/studies").as_str())
+            .uri(format!("/projects/{project_id}/studies/").as_str())
             .set_json(json!({ "name": "study_test" }))
             .to_request()
     }
@@ -214,6 +282,7 @@ mod test {
         )
         .await;
         assert_eq!(response.status(), StatusCode::NO_CONTENT);
+
         let response = call_service(&app, delete_project_request(study.project_id.unwrap())).await;
         assert_eq!(response.status(), StatusCode::NO_CONTENT);
     }
@@ -236,38 +305,35 @@ mod test {
     async fn project_get() {
         let app = create_test_service().await;
         let study: Study = call_and_read_body_json(&app, create_study_request().await).await;
-
-        let req = TestRequest::get()
-            .uri(
-                format!(
-                    "/projects/{}/studies/{}/",
-                    study.project_id.unwrap(),
-                    study.id.unwrap()
-                )
-                .as_str(),
-            )
-            .to_request();
+        let project_id = study.project_id.unwrap();
+        let study_id = study.id.unwrap();
+        let url = format!("/projects/{project_id}/studies/{study_id}/");
+        let req = TestRequest::get().uri(url.as_str()).to_request();
         let response = call_service(&app, req).await;
         assert_eq!(response.status(), StatusCode::OK);
 
-        let req = TestRequest::delete()
-            .uri(
-                format!(
-                    "/projects/{}/studies/{}/",
-                    study.project_id.unwrap(),
-                    study.id.unwrap()
-                )
-                .as_str(),
-            )
-            .to_request();
+        let req = TestRequest::delete().uri(url.as_str()).to_request();
         let response = call_service(&app, req).await;
         assert_eq!(response.status(), StatusCode::NO_CONTENT);
 
-        let response = call_service(
-            &app,
-            delete_study_request(study.project_id.unwrap(), study.id.unwrap()),
-        )
-        .await;
+        let response = call_service(&app, delete_study_request(project_id, study_id)).await;
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[actix_test]
+    async fn study_patch() {
+        let app = create_test_service().await;
+        let study: Study = call_and_read_body_json(&app, create_study_request().await).await;
+        let project_id = study.project_id.unwrap();
+        let study_id = study.id.unwrap();
+        let url = format!("/projects/{project_id}/studies/{study_id}");
+        let req = TestRequest::patch()
+            .uri(url.as_str())
+            .set_json(json!({"name": "rename_test", "budget":20000}))
+            .to_request();
+        let response = call_service(&app, req).await;
+        assert_eq!(response.status(), StatusCode::OK);
+        let response = call_service(&app, delete_study_request(project_id, study_id)).await;
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
     }
 }
