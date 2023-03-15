c7bd64a51131af69f88c20a11756c3b0ecfd6ba8
Nicolas Wurtz <gitlab@nethenic.net>
2023-03-15 15:54:33 +0100

    front: add settings for VITE warnings & errors overlay

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -7,35 +7,42 @@ import ImportMetaEnvPlugin from '@import-meta-env/unplugin';
 import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    svgrPlugin(),
-    ImportMetaEnvPlugin.vite({
-      example: '.env.example',
-    }),
-    checker({
-      typescript: {
-        buildMode: true,
-      },
-      eslint: {
-        lintCommand: 'eslint --ext .ts,.tsx,.js,.jsx src --max-warnings 203',
-      },
-    }),
-  ],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    open: true,
-  },
-  test: {
-    globals: true,
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    environment: 'happy-dom',
-    coverage: {
-      reportsDirectory: './tests/unit/coverage',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      ImportMetaEnvPlugin.vite({
+        example: '.env.example',
+      }),
+      checker({
+        typescript: {
+          buildMode: true,
+        },
+        eslint: {
+          lintCommand: 'eslint --ext .ts,.tsx,.js,.jsx src --max-warnings 203',
+        },
+        overlay: env.OSRD_VITE_OVERLAY !== 'false' && {
+          initialIsOpen: env.OSRD_VITE_OVERLAY_IS_OPEN === 'true',
+        },
+      }),
+    ],
+    build: {
+      outDir: 'build',
     },
-  },
+    server: {
+      open: true,
+      port: +env.OSRD_VITE_PORT || 3000,
+    },
+    test: {
+      globals: true,
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      environment: 'happy-dom',
+      coverage: {
+        reportsDirectory: './tests/unit/coverage',
+      },
+    },
+  };
 });
