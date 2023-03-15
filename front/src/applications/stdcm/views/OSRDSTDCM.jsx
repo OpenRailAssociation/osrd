e3ab1e23bfb757c34bf4d390c8e5cf52146112bd
alexandredamiron <alexandredamiron@gmail.com>
2023-03-15 01:10:44 +0100

    raz some params on stdcm leave

--- front/src/applications/stdcm/views/OSRDSTDCM.jsx
+++ front/src/applications/stdcm/views/OSRDSTDCM.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMode } from 'reducers/osrdconf/selectors';
-
+import { updateSelectedTrain, updateSelectedProjection } from 'reducers/osrdsimulation/actions';
 import { MODES, STDCM_REQUEST_STATUS } from 'applications/operationalStudies/consts';
 import { updateMode } from 'reducers/osrdconf';
 import OSRDStdcmConfig from './OSRDCStdcmConfig';
 import OSRDStdcmResults from './OSRDStdcmResults';
 import StdcmRequestModal from './StdcmRequestModal';
 
-
 export default function OSRDSTDCM() {
   const dispatch = useDispatch();
   const mode = useSelector(getMode);
-
   const [currentStdcmRequestStatus, setCurrentStdcmRequestStatus] = useState(
     STDCM_REQUEST_STATUS.idle
   );
@@ -21,6 +19,8 @@ export default function OSRDSTDCM() {
     if(mode !== MODES.stdcm) dispatch(updateMode(MODES.stdcm));
     return () => {
       dispatch(updateMode(MODES.simulation));
+      dispatch(updateSelectedTrain(0));
+      dispatch(updateSelectedProjection(undefined))
     }
   }, []);
 
