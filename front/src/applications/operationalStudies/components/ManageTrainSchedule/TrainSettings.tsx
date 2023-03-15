b365e78a5761fe561a684a0f6a3edc9e66d4fa13
Nicolas Wurtz <gitlab@nethenic.net>
2023-03-15 16:15:03 +0100

    front: updateTrain: remove ugly getOSRDconf

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/TrainSettings.tsx
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/TrainSettings.tsx
@@ -4,15 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateName, updateTrainStep, updateTrainCount, updateTrainDelta } from 'reducers/osrdconf';
 import InputSNCF from 'common/BootstrapSNCF/InputSNCF';
 
-import { getOSRDConf } from 'reducers/osrdconf/selectors';
+import { getName, getTrainStep, getTrainCount, getTrainDelta } from 'reducers/osrdconf/selectors';
 import { useDebounce } from 'utils/helpers';
 
 export default function TrainSettings() {
-  const osrdconf = useSelector(getOSRDConf);
-  const [name, setName] = useState(osrdconf.name);
-  const [trainStep, setTrainStep] = useState(osrdconf.trainStep);
-  const [trainCount, setTrainCount] = useState(osrdconf.trainCount);
-  const [trainDelta, setTrainDelta] = useState(osrdconf.trainDelta);
+  const [name, setName] = useState(useSelector(getName));
+  const [trainStep, setTrainStep] = useState(useSelector(getTrainStep));
+  const [trainCount, setTrainCount] = useState(useSelector(getTrainCount));
+  const [trainDelta, setTrainDelta] = useState(useSelector(getTrainDelta));
   const { t } = useTranslation(['operationalStudies/manageTrainSchedule']);
   const dispatch = useDispatch();
   const debouncedName = useDebounce(name, 500);
@@ -37,54 +36,52 @@ export default function TrainSettings() {
   }, [debouncedTrainDelta]);
 
   return (
-    osrdconf && (
-      <div className="osrd-config-item-container d-flex align-items-end mb-2">
-        <span className="mr-2 flex-grow-1">
-          <InputSNCF
-            type="text"
-            label={t('trainScheduleName')}
-            id="osrdconf-name"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-            value={name}
-            noMargin
-            sm
-          />
-        </span>
-        <span className="mr-2">
-          <InputSNCF
-            type="number"
-            label={t('trainScheduleStep')}
-            id="osrdconf-traincount"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainStep(+e.target.value)}
-            value={trainStep}
-            noMargin
-            sm
-          />
-        </span>
-        <span className="mr-2">
-          <InputSNCF
-            type="number"
-            label={t('trainScheduleCount')}
-            id="osrdconf-traincount"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainCount(+e.target.value)}
-            value={trainCount}
-            noMargin
-            sm
-          />
-        </span>
-        <span className="mr-2">
-          <InputSNCF
-            type="number"
-            label={t('trainScheduleDelta')}
-            id="osrdconf-delta"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainDelta(+e.target.value)}
-            value={trainDelta}
-            unit="min"
-            noMargin
-            sm
-          />
-        </span>
-      </div>
-    )
+    <div className="osrd-config-item-container d-flex align-items-end mb-2">
+      <span className="mr-2 flex-grow-1">
+        <InputSNCF
+          type="text"
+          label={t('trainScheduleName')}
+          id="osrdconf-name"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+          value={name}
+          noMargin
+          sm
+        />
+      </span>
+      <span className="mr-2">
+        <InputSNCF
+          type="number"
+          label={t('trainScheduleStep')}
+          id="osrdconf-traincount"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainStep(+e.target.value)}
+          value={trainStep}
+          noMargin
+          sm
+        />
+      </span>
+      <span className="mr-2">
+        <InputSNCF
+          type="number"
+          label={t('trainScheduleCount')}
+          id="osrdconf-traincount"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainCount(+e.target.value)}
+          value={trainCount}
+          noMargin
+          sm
+        />
+      </span>
+      <span className="mr-2">
+        <InputSNCF
+          type="number"
+          label={t('trainScheduleDelta')}
+          id="osrdconf-delta"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTrainDelta(+e.target.value)}
+          value={trainDelta}
+          unit="min"
+          noMargin
+          sm
+        />
+      </span>
+    </div>
   );
 }
