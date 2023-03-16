93e1282b2c511567e59982eebb8de545162028bb
Valentin Chanas <anisometropie@gmail.com>
2023-03-16 14:26:10 +0100

    front: editoast error format

--- front/scripts/generate-types.sh
+++ front/scripts/generate-types.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 npx @rtk-query/codegen-openapi src/config/openapi-middleware-config.ts
 npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.ts
-eslint --fix src/common/api/osrdMiddlewareApi.ts src/common/api/osrdEditoastApi.ts
+eslint --fix src/common/api/osrdMiddlewareApi.ts src/common/api/osrdEditoastApi.ts --no-ignore
