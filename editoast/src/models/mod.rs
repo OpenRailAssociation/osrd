f751adb229c471dac6deca34e649ca0eb0a85aea
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-03-23 06:32:45 +0100

    editoast: add get endpoint for scenario

--- editoast/src/models/mod.rs
+++ editoast/src/models/mod.rs
@@ -13,7 +13,7 @@ use diesel::PgConnection;
 
 pub use documents::Document;
 pub use projects::{Project, ProjectWithStudies};
-pub use scenario::{Scenario, ScenarioWithDetails};
+pub use scenario::{Scenario, ScenarioWithCountTrains, ScenarioWithDetails};
 pub use study::{Study, StudyWithScenarios};
 pub use timetable::{Timetable, TimetableWithSchedules};
 pub use train_schedule::TrainSchedule;
