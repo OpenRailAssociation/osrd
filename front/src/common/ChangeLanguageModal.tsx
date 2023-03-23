99639ece1c11dd3a5d0efdc77a0672df0d25b7d4
Nicolas Wurtz <gitlab@nethenic.net>
2023-03-23 16:49:06 +0100

    front: i18n: add menu entry & modal to change language

---
+++ front/src/common/ChangeLanguageModal.tsx
@@ -0,0 +1,80 @@
+// React Component displaying different applications versions and license attributions
+// List of applications : Editoast, Core, Api
+
+import React from 'react';
+import i18n from 'i18n';
+import { useTranslation } from 'react-i18next';
+import { DE, ES, FR, GB, IT, JP, RU, UA } from 'country-flag-icons/react/3x2';
+
+import ModalBodySNCF from 'common/BootstrapSNCF/ModalSNCF/ModalBodySNCF';
+import ModalHeaderSNCF from 'common/BootstrapSNCF/ModalSNCF/ModalHeaderSNCF';
+import { useModal } from './BootstrapSNCF/ModalSNCF';
+
+const SortedLanguages = () => {
+  const { t } = useTranslation('home/navbar');
+  const { closeModal } = useModal();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    closeModal();
+  };
+
+  const formatLanguageWithFlag = (lng: string) => {
+    const title = t(`language.${lng}`);
+    switch (lng) {
+      case 'de':
+        return <DE title={title} />;
+      case 'en':
+        return <GB title={title} />;
+      case 'es':
+        return <ES title={title} />;
+      case 'fr':
+        return <FR title={title} />;
+      case 'it':
+        return <IT title={title} />;
+      case 'jp':
+        return <JP title={title} />;
+      case 'ru':
+        return <RU title={title} />;
+      case 'uk':
+        return <UA title={title} />;
+      default:
+        return null;
+    }
+  };
+
+  const availablesLanguages = i18n.languages.map((lng) => ({
+    key: lng,
+    value: t(`language.${lng}`),
+  }));
+  availablesLanguages.sort((a, b) => a.value.localeCompare(b.value));
+  return (
+    <>
+      {availablesLanguages.map((lng) => (
+        <button
+          type="button"
+          className="btn btn-secondary btn-block language-choice-btn"
+          key={`language-btn-${lng.key}`}
+          disabled={i18n.language === lng.key}
+          onClick={() => changeLanguage(lng.key)}
+        >
+          {formatLanguageWithFlag(lng.key)}
+          {lng.value}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default function ChangeLanguageModal() {
+  const { t } = useTranslation('home/navbar');
+
+  return (
+    <div className="informations">
+      <ModalHeaderSNCF withCloseButton>
+        <h1>{t('language.languageChoice')}</h1>
+      </ModalHeaderSNCF>
+      <ModalBodySNCF>{i18n.languages && <SortedLanguages />}</ModalBodySNCF>
+    </div>
+  );
+}
