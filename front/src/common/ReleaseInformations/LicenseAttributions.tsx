bea25d320fefdd910b530a93334a4d6a2f427b2c
SharglutDev <p.filimon75@gmail.com>
2023-03-28 17:13:23 +0200

    front: releaseinformations: remove releaseInformations.scss; change .informations to .informations-modal; create _relaseInformations.scss in /styles
    - replace modalheadersncf children by withclosebutton props
    
    - navbarsncf  add lg size to props when calling openmodal; releaseinformation change display to have 2 columns on large screens; licenseattribution add scroll only for this column

--- front/src/common/ReleaseInformations/LicenseAttributions.tsx
+++ front/src/common/ReleaseInformations/LicenseAttributions.tsx
@@ -9,19 +9,19 @@ const LicenseAttributions = () => {
     <div className="d-flex flex-column align-items-start" key={name}>
       <div className="d-flex">
         <h3 className="mr-1">{name}</h3>
-        <span className="informations-version">{`(${version})`}</span>
+        <span className="informations-modal-version">{`(${version})`}</span>
       </div>
       <span className="pl-4 mb-4">{copyright || publisher || `${name} authors`}</span>
     </div>
   ));
 
   return (
-    <>
+    <div className="col-md-6 license-attributions">
       <h2 className="d-flex justify-content-center mt-5 mb-4">
         {t('informations.releaseInformations')}
       </h2>
       {attributions}
-    </>
+    </div>
   );
 };
 
