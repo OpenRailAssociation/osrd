c4e88d04b61b6bd88b36b3d38739e80be3792186
Theo Macron <theo.macron0315@gmail.com>
2023-03-28 20:21:05 +0200

    front: power restrictions: finishing selector
    front: powerRestriction: some light refacto kiss kiss
    
    front: power restrictions: add "no selection" option
    
    front: power restrictions: updating names

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/PowerRestrictionSelector.tsx
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/PowerRestrictionSelector.tsx
@@ -1,75 +1,59 @@
+/* eslint-disable no-restricted-syntax */
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { get } from 'common/requests';
-import icon from 'assets/pictures/components/electricity.svg';
+import { updatePowerRestriction } from 'reducers/osrdconf';
+import icon from 'assets/pictures/components/power_restrictions.svg';
 import SelectImprovedSNCF from 'common/BootstrapSNCF/SelectImprovedSNCF';
 import { getRollingStockID, getPowerRestriction } from 'reducers/osrdconf/selectors';
+import { osrdEditoastApi, LightRollingStock } from 'common/api/osrdEditoastApi';
 
-const ROLLINGSTOCK_URL = '/rolling_stock';
+type selectorOption = { key: string | undefined; value: string | undefined };
 
-export default function PowerNotchSelector() {
-  const rollingStockID = useSelector(getRollingStockID);
-  const powerRestriction = useSelector(getPowerRestriction);
-  const [rollingStockSelected, setRollingStockSelected] = useState<
-    { id: number; power_restrictions: Record<string, string> } | undefined
-  >(undefined);
-  const [options, setOptions] = useState<{ value: string; label: string }[]>([]);
+export default function PowerRestrictionSelector() {
   const { t } = useTranslation(['operationalStudies/manageTrainSchedule']);
+  const dispatch = useDispatch();
+  const rollingStockID: number | undefined = useSelector(getRollingStockID);
+  const powerRestriction = useSelector(getPowerRestriction);
+  const [powerRestrictions, setPowerRestrictions] = useState<selectorOption[] | undefined>();
+  const [getRollingStockById] = osrdEditoastApi.endpoints.getLightRollingStockById.useLazyQuery({});
 
-  const handleRollingStockChange = (selectedOption: { value: string; label: string }) => {
-    const { value, label } = selectedOption;
-    setRollingStockSelected({
-      id: rollingStockID!,
-      power_restrictions: { [value]: rollingStockSelected?.power_restrictions[value] ?? label },
-    });
-  };
-
-  const getRollingStock = async () => {
-    try {
-      const rollingStock = await get(`${ROLLINGSTOCK_URL}/${rollingStockID}/`);
-      const ops = Object.entries(rollingStock.power_restrictions).map(
-        ([key, value]: [string, any]) => ({
-          value: key,
-          label: value,
-        })
+  const getPowerRestrictionsList = async (rollingStockInfo: LightRollingStock) => {
+    if (rollingStockInfo.power_restrictions) {
+      const powerRestrictionsList = Object.entries(rollingStockInfo.power_restrictions).map(
+        ([key, _]: [string | undefined, string]) => ({ key, value: key })
       );
-      setOptions(ops);
-      setRollingStockSelected(rollingStock);
-    } catch (e) {
-      console.error('ERROR', e);
+      if (powerRestrictionsList.length > 0) {
+        powerRestrictionsList.unshift({ key: undefined, value: t('noPowerRestriction') });
+      }
+      setPowerRestrictions(powerRestrictionsList);
     }
   };
 
   useEffect(() => {
-    if (rollingStockID !== undefined) {
-      getRollingStock();
+    if (rollingStockID) {
+      dispatch(updatePowerRestriction(undefined));
+      getRollingStockById({ id: rollingStockID })
+        .unwrap()
+        .then((rollingStockInfo) => {
+          getPowerRestrictionsList(rollingStockInfo);
+        });
+      setPowerRestrictions(undefined);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rollingStockID]);
 
-  const selectedOptions = rollingStockSelected?.power_restrictions
-    ? Object.keys(rollingStockSelected.power_restrictions)[0]
-    : '';
-
-  return (
+  return powerRestrictions && powerRestrictions.length > 0 ? (
     <div className="osrd-config-item mb-2">
       <div className="osrd-config-item-container">
         <img width="32px" className="mr-2" src={icon} alt="PowerRestrictionIcon" />
         <span className="text-muted">{t('powerRestriction')}</span>
-        {options.length !== 0 ? (
-          <SelectImprovedSNCF
-            sm
-            options={options}
-            selectedValue={selectedOptions}
-            onChange={handleRollingStockChange}
-          />
-        ) : (
-          <div className="ml-2">{t('noPowerRestriction')}</div>
-        )}
+        <SelectImprovedSNCF
+          sm
+          options={powerRestrictions}
+          onChange={(e: selectorOption) => dispatch(updatePowerRestriction(e.key))}
+          selectedValue={powerRestriction || t('noPowerRestriction')}
+        />
       </div>
     </div>
-  );
+  ) : null;
 }
-
-PowerNotchSelector.propTypes = {};
