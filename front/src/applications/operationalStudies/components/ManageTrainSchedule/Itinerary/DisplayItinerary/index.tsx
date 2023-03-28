9428ff8b9ad793784c93bac0ca2e244dd600cbc0
alexandredamiron <alexandredamiron@gmail.com>
2023-03-28 19:53:48 +0200

    front: refacto to isolate displayVias index

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/Itinerary/DisplayItinerary/index.tsx
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/Itinerary/DisplayItinerary/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { ComponentType } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Position } from 'geojson';
 import cx from 'classnames';
 
 import { getOrigin, getDestination, getVias } from 'reducers/osrdconf/selectors';
 
 import Pathfinding from 'common/Pathfinding';
+import { PointOnMap } from 'applications/operationalStudies/consts';
+import { noop } from 'lodash';
+import { Dispatch } from 'redux';
 import Origin from './Origin';
 import Vias from './Vias';
 import Destination from './Destination';
@@ -14,19 +17,45 @@ interface DisplayItineraryProps {
   zoomToFeaturePoint: (lngLat?: Position, id?: string, source?: string) => void;
   zoomToFeature: (lngLat: Position, id?: undefined, source?: undefined) => void;
   viaModalContent: JSX.Element;
+  origin?: PointOnMap | undefined;
+  destination?: PointOnMap | undefined;
+  dispatch?: Dispatch;
+  vias?: PointOnMap[];
+}
+
+export function withOSRDData<T>(Component: ComponentType<T>) {
+  return (hocProps: T) => {
+    const origin = useSelector(getOrigin);
+    const destination = useSelector(getDestination);
+    const vias = useSelector(getVias);
+    const dispatch = useDispatch();
+    return (
+      <Component
+        {...(hocProps as T)}
+        dispatch={dispatch}
+        origin={origin}
+        destination={destination}
+        vias={vias}
+      />
+    );
+  };
 }
 
 export default function DisplayItinerary(props: DisplayItineraryProps) {
-  const { zoomToFeaturePoint, zoomToFeature, viaModalContent } = props;
-
-  const origin = useSelector(getOrigin);
-  const destination = useSelector(getDestination);
-  const vias = useSelector(getVias);
+  const {
+    zoomToFeaturePoint,
+    zoomToFeature,
+    viaModalContent,
+    origin,
+    destination,
+    vias,
+    dispatch = noop,
+  } = props;
 
   return (
     <div
       className={cx({
-        'osrd-config-anchor': !origin && !destination && vias.length < 1,
+        'osrd-config-anchor': !origin && !destination && vias && vias?.length < 1,
       })}
     >
       <Origin data-testid="itinerary-origin" zoomToFeaturePoint={zoomToFeaturePoint} />
