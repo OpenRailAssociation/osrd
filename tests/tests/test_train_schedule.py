8cf82fffad2f7586ab02d0a9cf88595849ff670e
Brieuc Loussouarn <32448554+bloussou@users.noreply.github.com>
2023-03-29 10:13:52 +0200

    tests: Remove `setup_dummy_db` (#3690)

--- tests/tests/test_train_schedule.py
+++ tests/tests/test_train_schedule.py
@@ -1,12 +1,34 @@
+import json
+from typing import Iterable
+
 import requests
 
-from .scenario import Scenario
 from .services import API_URL
-from .utils.simulation import run_simulation, update_simulation_with_mareco_allowances
 
 
-def test_get_and_update_schedule_result(dummy_scenario: Scenario):
-    schedule_id = run_simulation(API_URL, dummy_scenario)
+def _update_simulation_with_mareco_allowances(base_url, train_Schedule_id):
+    response = requests.get(base_url + f"train_schedule/{train_Schedule_id}/")
+    assert response.status_code == 200
+    train_schedule = response.json()
+    train_schedule["allowances"] = [
+        {
+            "allowance_type": "standard",
+            "distribution": "MARECO",
+            "default_value": {"value_type": "percentage", "percentage": 3},
+            "ranges": [],
+        }
+    ]
+    r = requests.patch(base_url + f"train_schedule/{train_Schedule_id}/", json=train_schedule)
+    if r.status_code // 100 != 2:
+        raise RuntimeError(f"Schedule error {r.status_code}: {r.content}, payload={json.dumps(train_schedule)}")
+    body = r.json()
+    assert len(body["allowances"]) == 1
+    assert body["allowances"][0]["distribution"] == "MARECO"
+    return body
+
+
+def test_get_and_update_schedule_result(west_to_south_east_simulation: Iterable[int]):
+    schedule_id = west_to_south_east_simulation[0]
     response = requests.get(f"{API_URL}train_schedule/{schedule_id}/result/")
     if response.status_code // 100 != 2:
         raise RuntimeError(f"Schedule error {response.status_code}: {response.content}, id={schedule_id}")
@@ -14,7 +36,7 @@ def test_get_and_update_schedule_result(dummy_scenario: Scenario):
     assert "base" in simulation_report
     assert "eco" not in simulation_report
 
-    response = update_simulation_with_mareco_allowances(API_URL, schedule_id)
+    response = _update_simulation_with_mareco_allowances(API_URL, schedule_id)
     response = requests.get(f"{API_URL}train_schedule/{schedule_id}/result/")
     if response.status_code // 100 != 2:
         raise RuntimeError(f"Schedule error {response.status_code}: {response.content}, id={schedule_id}")
