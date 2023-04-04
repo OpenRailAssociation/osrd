ae732bbd01b1d00ec718026ebfe45758b3a55b01
Younes Khoudli <younes.khoudli@epita.fr>
2023-04-04 16:58:23 +0200

    core: fix zone occupancy computation on train with stops

--- core/envelope-sim/src/main/java/fr/sncf/osrd/envelope/EnvelopeTimeInterpolate.java
+++ core/envelope-sim/src/main/java/fr/sncf/osrd/envelope/EnvelopeTimeInterpolate.java
@@ -5,10 +5,16 @@ public interface EnvelopeTimeInterpolate {
     /** Computes the time required to get to a given point of the envelope */
     double interpolateTotalTime(double position);
 
+    /** Computes the time required to get to a given point of the envelope in ms */
+    long interpolateTotalTimeMS(double position);
+
     /** Computes the time required to get to a given point of the envelope,
      * clamping the position to [0, envelope length] first */
     double interpolateTotalTimeClamp(double position);
 
     /** Returns the end position of the envelope */
     double getEndPos();
+
+    /** Returns the total time of the envelope */
+    double getTotalTime();
 }
