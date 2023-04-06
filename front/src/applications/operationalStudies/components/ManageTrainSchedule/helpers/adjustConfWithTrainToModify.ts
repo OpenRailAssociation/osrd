ae8d82ef292a18c3b690e33df1b57f0c591820bb
Nicolas Wurtz <gitlab@nethenic.net>
2023-04-06 19:47:06 +0200

    front: updateTrain: complete all fields is ok

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/helpers/adjustConfWithTrainToModify.ts
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/helpers/adjustConfWithTrainToModify.ts
@@ -7,13 +7,35 @@ import {
   updateDepartureTime,
   updateInitialSpeed,
   updatePathfindingID,
+  updateItinerary,
+  updateOrigin,
+  updateDestination,
 } from 'reducers/osrdconf';
 import { sec2time } from 'utils/timeManipulation';
 import { Dispatch } from 'redux';
 import { store } from 'Store';
-import { TrainSchedule } from 'common/api/osrdMiddlewareApi';
+import { Path, TrainSchedule } from 'common/api/osrdMiddlewareApi';
 
-export default function ajustConf2TrainToModify(trainSchedule: TrainSchedule, dispatch: Dispatch) {
+// Râle pas Mathieu c'est temporaire !
+function convertStep2PointOnMap(step: any) {
+  return {
+    ...step,
+    id: step.track,
+    extension_sncf_line_code: '',
+    extension_sncf_line_name: '',
+    extension_sncf_track_name: '',
+    extension_sncf_track_number: '',
+    loading_gauge_limits: '',
+    source: 'geo',
+    coordinates: step.geo.coordinates,
+  };
+}
+
+export default function ajustConf2TrainToModify(
+  trainSchedule: TrainSchedule,
+  path: Path,
+  dispatch: Dispatch
+) {
   const { osrdconf } = store.getState();
   if (trainSchedule.rolling_stock) dispatch(updateRollingStockID(trainSchedule.rolling_stock));
   if (
@@ -31,4 +53,9 @@ export default function ajustConf2TrainToModify(trainSchedule: TrainSchedule, di
   dispatch(updateInitialSpeed(trainSchedule.initial_speed || 0));
 
   dispatch(updatePathfindingID(trainSchedule.path));
+  dispatch(updateItinerary(path));
+  if (path.steps) {
+    dispatch(updateOrigin(convertStep2PointOnMap(path.steps[0])));
+    dispatch(updateDestination(convertStep2PointOnMap(path.steps.at(-1))));
+  }
 }
