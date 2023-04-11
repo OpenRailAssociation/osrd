01cf8d1ebaf0f4c9ff04158bbd2ca2a7c2fa2c96
Nicolas Wurtz <gitlab@nethenic.net>
2023-04-11 03:39:22 +0200

    front: updateTrain: change text with edit or add

--- front/src/applications/operationalStudies/components/Scenario/TimetableManageTrainSchedule.tsx
+++ front/src/applications/operationalStudies/components/Scenario/TimetableManageTrainSchedule.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { FaPlus } from 'react-icons/fa';
+import { FaPen, FaPlus } from 'react-icons/fa';
 import { MANAGE_TRAIN_SCHEDULE_TYPES } from 'applications/operationalStudies/consts';
 import { useTranslation } from 'react-i18next';
 
 type Props = {
+  displayTrainScheduleManagement: string;
   setDisplayTrainScheduleManagement: (type: string) => void;
 };
 
-export default function TimetableManageTrainSchedule({ setDisplayTrainScheduleManagement }: Props) {
+export default function TimetableManageTrainSchedule({
+  displayTrainScheduleManagement,
+  setDisplayTrainScheduleManagement,
+}: Props) {
   const { t } = useTranslation('operationalStudies/manageTrainSchedule');
+
+  const textContent = () => {
+    if (displayTrainScheduleManagement === MANAGE_TRAIN_SCHEDULE_TYPES.add) {
+      return (
+        <>
+          <span className="text-primary">
+            <FaPlus />
+          </span>
+          <span className="text-center">{t('addTrainSchedule')}</span>
+        </>
+      );
+    }
+    if (displayTrainScheduleManagement === MANAGE_TRAIN_SCHEDULE_TYPES.edit) {
+      return (
+        <>
+          <span className="text-orange">
+            <FaPen />
+          </span>
+          <span className="text-center flex-grow-1">{t('updateTrainSchedule')}</span>
+        </>
+      );
+    }
+    return null;
+  };
 
   return (
     <div
@@ -18,18 +46,16 @@ export default function TimetableManageTrainSchedule({ setDisplayTrainScheduleMa
       onClick={() => setDisplayTrainScheduleManagement(MANAGE_TRAIN_SCHEDULE_TYPES.none)}
     >
       <div className="scenario-timetable-managetrainschedule-header">
-        <div className="text-center mb-3">
-          <span className="text-primary mr-2">
-            <FaPlus />
-          </span>
-          {t('addTrainSchedule')}
+        <div className="d-flex gap-1 align-items-center justify-content-center mb-2 p-4 bg-white rounded h-100">
+          {textContent()}
         </div>
         <button
           className="btn btn-secondary btn-block"
           type="button"
           onClick={() => setDisplayTrainScheduleManagement(MANAGE_TRAIN_SCHEDULE_TYPES.none)}
         >
-          {t('cancelAddTrainSchedule')}
+          <i className="icons-arrow-prev mr-2" />
+          {t('returnToSimulationResults')}
         </button>
       </div>
       <div className="scenario-timetable-managetrainschedule-body" />
