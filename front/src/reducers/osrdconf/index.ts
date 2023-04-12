3629ed3c4e706c0d80b964ce5f1c9a2562d7b151
Nicolas Wurtz <gitlab@nethenic.net>
2023-04-12 03:30:46 +0200

    front: updateTrain: pathFinding more clever, running only when necessary

--- front/src/reducers/osrdconf/index.ts
+++ front/src/reducers/osrdconf/index.ts
@@ -33,6 +33,7 @@ export const UPDATE_SCENARIO_ID = 'osrdconf/UPDATE_SCENARIO_ID';
 export const UPDATE_INFRA_ID = 'osrdconf/UPDATE_INFRA_ID';
 export const UPDATE_SWITCH_TYPES = 'osrdconf/UPDATE_SWITCH_TYPES';
 export const UPDATE_PATHFINDING_ID = 'osrdconf/UPDATE_PATHFINDING_ID';
+export const UPDATE_SHOULD_RUN_PATHFINDING = 'osrdconf/UPDATE_SHOULD_RUN_PATHFINDING';
 export const UPDATE_TIMETABLE_ID = 'osrdconf/UPDATE_TIMETABLE_ID';
 export const UPDATE_ROLLINGSTOCK_ID = 'osrdconf/UPDATE_ROLLINGSTOCK_ID';
 export const UPDATE_ROLLINGSTOCK_COMFORT = 'osrdconf/UPDATE_ROLLINGSTOCK_COMFORT';
@@ -85,6 +86,7 @@ const defaultCommonConf = {
   origin: undefined,
   initialSpeed: 0,
   departureTime: '08:00:00',
+  shouldRunPathfinding: true,
   originDate: formatIsoDate(new Date()),
   originTime: '08:00:00',
   originUpperBoundDate: formatIsoDate(new Date()),
@@ -171,6 +173,9 @@ export default function reducer(inputState: OsrdMultiConfState | undefined, acti
         break;
       case UPDATE_PATHFINDING_ID:
         draft[section].pathfindingID = action.pathfindingID;
+        break;
+      case UPDATE_SHOULD_RUN_PATHFINDING:
+        draft[section].shouldRunPathfinding = action.shouldRunPathfinding;
         break;
       case UPDATE_TIMETABLE_ID:
         draft[section].timetableID = action.timetableID;
@@ -425,6 +430,14 @@ export function updatePathfindingID(pathfindingID?: number) {
     dispatch({
       type: UPDATE_PATHFINDING_ID,
       pathfindingID,
+    });
+  };
+}
+export function updateShouldRunPathfinding(shouldRunPathfinding: boolean) {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: UPDATE_SHOULD_RUN_PATHFINDING,
+      shouldRunPathfinding,
     });
   };
 }
