aac9af026f9d3512445e98e2e0b19a9a7a9d9acf
Valentin Chanas <anisometropie@gmail.com>
2023-04-13 21:59:49 +0200

    front: refactor search function

---
+++ front/src/common/RollingStockSelector/__tests__/RollingStockModal.spec.ts
@@ -0,0 +1,68 @@
+import { rollingStockPassesEnergeticModeFilters } from '../RollingStockModal';
+
+describe('rollingStockPassEnergeticModeFilters', () => {
+  describe('both false', () => {
+    it('should return true (all rolling stocks are accepted)', () => {
+      const filterElec = false;
+      const filterThermal = false;
+      const modes = {};
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(true);
+    });
+  });
+  describe('electrical true', () => {
+    it('should return true if one of the modes is electrical', () => {
+      const filterElec = true;
+      const filterThermal = false;
+      const modes = { '25000': { is_electric: true } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(true);
+    });
+    it('should return false if no mode is electrical', () => {
+      const filterElec = true;
+      const filterThermal = false;
+      const modes = { '25000': { is_electric: false } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(false);
+    });
+  });
+  describe('thermal true', () => {
+    it('should return true if one of the modes is thermal', () => {
+      const filterElec = false;
+      const filterThermal = true;
+      const modes = { '25000': { is_electric: false } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(true);
+    });
+    it('should return false if no mode is thermal', () => {
+      const filterElec = false;
+      const filterThermal = true;
+      const modes = { '25000': { is_electric: true } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(false);
+    });
+  });
+  describe('both true', () => {
+    it('should return true there exist both an electric and a thermal', () => {
+      const filterElec = true;
+      const filterThermal = true;
+      const modes = { '25000': { is_electric: true }, '99999': { is_electric: false } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(true);
+    });
+    it('should return false if there is no thermal', () => {
+      const filterElec = true;
+      const filterThermal = true;
+      const modes = { '25000': { is_electric: true }, '99999': { is_electric: true } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(false);
+    });
+    it('should return false if there is no electric', () => {
+      const filterElec = true;
+      const filterThermal = true;
+      const modes = { '25000': { is_electric: false }, '99999': { is_electric: false } };
+      const result = rollingStockPassesEnergeticModeFilters(filterElec, filterThermal, modes);
+      expect(result).toBe(false);
+    });
+  });
+});
