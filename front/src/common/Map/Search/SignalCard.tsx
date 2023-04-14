0225a83649a177f636e06669d9e89ef9fd38fa15
uriel <uriel.sautron@gmail.com>
2023-04-14 10:12:03 +0200

    front: move signalicons folder to public

--- front/src/common/Map/Search/SignalCard.tsx
+++ front/src/common/Map/Search/SignalCard.tsx
@@ -14,7 +14,7 @@ const SignalCard = ({ signalSearchResult, onResultClick }: SignalCardProps) => (
   >
     <div className="col-1">
       <img
-        src={`/src/assets/pictures/signalicons/${signalSearchResult.type?.replace(/ /g, '_')}.svg`}
+        src={`pictures/signals/${signalSearchResult.type?.replace(/ /g, '_')}.svg`}
         alt={signalSearchResult.type}
       />
     </div>
