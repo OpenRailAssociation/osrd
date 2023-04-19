433df2c97a7b9cac250d9412471167dc4b04c743
Paul Filimon <113104137+SharglutDev@users.noreply.github.com>
2023-04-19 16:58:27 +0200

    front: fix project image not displayed in stdcm (#3953)

--- front/src/common/ScenarioExplorator/ScenarioExploratorProject2Image.tsx
+++ front/src/common/ScenarioExplorator/ScenarioExploratorProject2Image.tsx
@@ -1,16 +1,15 @@
 import React, { useMemo, useState } from 'react';
-import { PROJECTS_URI } from 'applications/operationalStudies/components/operationalStudiesConsts';
-import { get } from 'common/requests';
+import { getDocument } from 'common/api/documentApi';
 import { MiniCardsImageProps } from './ScenarioExploratorTypes';
 
 export default function Project2Image({ project }: MiniCardsImageProps) {
   const [imageUrl, setImageUrl] = useState<string>();
 
   useMemo(async () => {
-    if (!project || !project.image_url) return;
+    if (!project || !project.image) return;
     try {
-      const image = await get(`${PROJECTS_URI}${project.id}/image/`, { responseType: 'blob' });
-      if (image) setImageUrl(URL.createObjectURL(image));
+      const blobImage = await getDocument(project.image as number);
+      if (blobImage) setImageUrl(URL.createObjectURL(blobImage));
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       console.error(error);
