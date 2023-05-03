f560ee1eca0c4c04dbaa67c1b1e798ee47a6258c
Younes Khoudli <younes.khoudli@epita.fr>
2023-05-03 12:05:04 +0200

    editoast: provide a dummy /timetable/{id}/conflicts endpoint

--- editoast/src/views/timetable.rs
+++ editoast/src/views/timetable.rs
@@ -5,10 +5,11 @@ use actix_web::dev::HttpServiceFactory;
 use actix_web::get;
 use actix_web::web::{self, Data, Json, Path};
 use editoast_derive::EditoastError;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 pub fn routes() -> impl HttpServiceFactory {
-    web::scope("/timetable/{timetable_id}").service(get)
+    web::scope("/timetable/{timetable_id}").service((get, get_conflicts))
 }
 
 #[derive(Debug, Error, EditoastError)]
@@ -34,4 +35,45 @@ async fn get(
     };
     let timetable_with_schedules = timetable.with_train_schedules(db_pool).await?;
     Ok(Json(timetable_with_schedules))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Conflict {
+    train_ids: Vec<i64>,
+    start_time: f64,
+    end_time: f64,
+    conflict_type: String,
+}
+
+// This is a dummy implementation for now
+#[get("conflicts")]
+async fn get_conflicts(
+    db_pool: Data<DbPool>,
+    timetable_id: Path<i64>,
+) -> Result<Json<Vec<Conflict>>> {
+    let timetable_id = timetable_id.into_inner();
+
+    // Return the timetable
+    let timetable = match Timetable::retrieve(db_pool.clone(), timetable_id).await? {
+        Some(timetable) => timetable,
+        None => return Err(TimetableError::NotFound { timetable_id }.into()),
+    };
+    let timetable_with_schedules = timetable.with_train_schedules(db_pool).await?;
+
+    if timetable_with_schedules.train_schedules.len() < 2 {
+        return Ok(Json(vec![]));
+    }
+
+    let train_ids = timetable_with_schedules
+        .train_schedules
+        .into_iter()
+        .take(2)
+        .map(|ts| ts.id)
+        .collect::<Vec<_>>();
+    Ok(Json(vec![Conflict {
+        train_ids,
+        start_time: 17.3,
+        end_time: 103.6,
+        conflict_type: "Spacing".to_owned(),
+    }]))
 }
