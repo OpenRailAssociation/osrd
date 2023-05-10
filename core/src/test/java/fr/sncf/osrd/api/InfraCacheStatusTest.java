46a7a8659628654a670a3a4d9186c2eb61c51448
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-05-10 15:46:59 +0200

    core: add infra states and allow returning InfraCacheStatus for a given infra

---
+++ core/src/test/java/fr/sncf/osrd/api/InfraCacheStatusTest.java
@@ -0,0 +1,47 @@
+package fr.sncf.osrd.api;
+
+import static fr.sncf.osrd.utils.takes.TakesUtils.readBodyResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import fr.sncf.osrd.reporting.warnings.DiagnosticRecorderImpl;
+import org.junit.jupiter.api.Test;
+import org.takes.rq.RqFake;
+import java.io.IOException;
+import java.util.Map;
+
+
+
+public class InfraCacheStatusTest extends ApiTest {
+
+    /**  */
+    public Map<String, InfraCacheStatusEndpoint.SerializedInfraCache>
+            runInfraCacheStatus(InfraCacheStatusEndpoint.InfraCacheRequest request)
+            throws IOException {
+        // serialize the request
+        var requestBody = InfraCacheStatusEndpoint.adapterRequest.toJson(request);
+
+        // process it
+        var rawResponse = readBodyResponse(new InfraCacheStatusEndpoint(infraHandlerMock)
+                        .act(new RqFake("POST", "/cache_status", requestBody))
+                );
+
+        // parse the response
+        var response = InfraCacheStatusEndpoint.adapter.fromJson(rawResponse);
+        assertNotNull(response);
+        return response;
+    }
+
+    @Test
+    public void simple() throws Exception {
+        var recorder = new DiagnosticRecorderImpl(false);
+        var query = new InfraCacheStatusEndpoint.InfraCacheRequest(
+                "tiny_infra/infra.json"
+        );
+        infraHandlerMock.load("tiny_infra/infra.json",
+                "1", recorder);
+        var res = runInfraCacheStatus(query);
+        var response = res.get("tiny_infra/infra.json");
+        assertEquals(InfraManager.InfraStatus.CACHED, response.status);
+    }
+}
