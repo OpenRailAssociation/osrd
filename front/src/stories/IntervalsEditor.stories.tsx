74d82719a59a601f18efa476e279625ea74c7274
Alexandre Damiron <alexandredamiron@gmail.com>
2023-05-24 10:38:32 +0200

    front: first story for IntervalsEditor

---
+++ front/src/stories/IntervalsEditor.stories.tsx
@@ -0,0 +1,7 @@
+import IntervalsEditor from 'common/IntervalsEditor/IntervalsEditor';
+
+const SAMPLE_DATA = ['First Category of Speed Limits', 'Second Category of Speed Limits'];
+
+export default { component: IntervalsEditor, tags: ['autodocs'] };
+
+export const Plain = { args: {} };
