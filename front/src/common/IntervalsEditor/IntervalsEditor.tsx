b48b54b6f3f6f374217048e7101e4005763905a0
Alexandre Damiron <alexandredamiron@gmail.com>
2023-05-24 10:38:32 +0200

    front: first story for IntervalsEditor

---
+++ front/src/common/IntervalsEditor/IntervalsEditor.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+
+interface IntervalsEditorProps {
+  text: string;
+}
+
+/**
+ * A tool to quickly set intervals on a linear range
+ *
+ * version 0.1
+ */
+export const IntervalsEditor: React.FC<IntervalsEditorProps> = (props) => {
+  const { text } = props;
+  return <div>Bonjour</div>;
+};
+
+export default IntervalsEditor;
