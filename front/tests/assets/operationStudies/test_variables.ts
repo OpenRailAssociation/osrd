2891997bc0d4d2c0b0bbab78e9d10acc33854972
Uriel <uriel.sautron@gmail.com>
2023-06-15 16:42:45 +0200

    front: test pathfinding done for small_infra
    front: test pathfinding infra_france
    
    front: test pathfinding add timeout for map
    
    front: upgrate mapgl
    
    front: upgrate mapgl
    
    front: remove firefox browser
    
    front: fix comments

--- front/tests/assets/operationStudies/test_variables.ts
+++ front/tests/assets/operationStudies/test_variables.ts
@@ -1,11 +1,43 @@
-export default {
-  infraName: process.env.CI ? 'small_infra' : 'France',
-  numberOfRollingstock: process.env.CI ? '3' : '446',
-  numberOfRollingstockWithElectrical: process.env.CI ? '3' : '265',
-  searchRollingstock: process.env.CI ? '7200' : 'tgv',
-  numberOfRollingstockWithSearch: process.env.CI ? '1' : '27',
-  rollingstockTestID: process.env.CI
-    ? 'rollingstock-_@Test BB 7200GVLOCOMOTIVES'
-    : 'rollingstock-1TGV2N2',
-  rollingStockInfo: process.env.CI ? /BB 7200GV/ : /TGV2N2US/,
-};
+export default process.env.CI
+  ? {
+      infraName: 'small_infra',
+      numberOfRollingstock: '3',
+      numberOfRollingstockWithElectrical: '3',
+      searchRollingstock: '7200',
+      numberOfRollingstockWithSearch: '1',
+      rollingstockTestID: 'rollingstock-_@Test BB 7200GVLOCOMOTIVES',
+      rollingStockInfo: /BB 7200GV/,
+      originSearch: {
+        stationName: 'south west',
+        stationItemName: /South_West_station/,
+        positionClick: { x: 415, y: 200 },
+      },
+      destinationSearch: {
+        stationName: 'nor',
+        stationItemName: /North_East_station/,
+        positionClick: { x: 415, y: 205 },
+      },
+      pathfindingDistance: '22 km',
+    }
+  : {
+      infraName: 'France',
+      numberOfRollingstock: '446',
+      numberOfRollingstockWithElectrical: '265',
+      searchRollingstock: 'tgv',
+      numberOfRollingstockWithSearch: '27',
+      rollingstockTestID: 'rollingstock-1TGV2N2',
+      rollingStockInfo: /TGV2N2US/,
+      originSearch: {
+        stationName: 'beaune',
+        stationItemName: 'BEA Beaune 87713545',
+        positionClick: { x: 430, y: 200 },
+      },
+
+      destinationSearch: {
+        stationName: 'miramas',
+        stationItemName: 'MAS Miramas A1 87753004',
+        positionClick: { x: 415, y: 200 },
+      },
+
+      pathfindingDistance: '461 km',
+    };
