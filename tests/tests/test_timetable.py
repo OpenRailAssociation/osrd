2ffc9511b9c4411760cea9fcf8bf0d0c1a98fd4a
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-06-22 00:18:27 +0200

    tests: update test_timetable.py

--- tests/tests/test_timetable.py
+++ tests/tests/test_timetable.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Iterable, Mapping, Optional
+from typing import Any, List, Mapping, Optional
 
 import pytest
 import requests
@@ -14,19 +14,32 @@ class _TrainScheduleDetails:
     id: int
     train_name: str
     departure_time: int
-    train_path: int
+    arrival_time: float
+    path_id: int
+    timetable_id: int
+    rolling_stock_id: int
+    initial_speed: int
+    labels: List[str]
+    allowances: List[dict]
+    speed_limit_tags: str | None
+    comfort: str
+    options: dict | None
+    power_restriction_ranges: List[dict] | None
+    mechanical_energy_consumed: dict
+    stops: int
+    path_length: float
 
 
 @dataclass(frozen=True)
 class _TimetableResponse:
     id: int
     name: str
-    train_schedules: Iterable[_TrainScheduleDetails]
+    detailed_train_schedules: List[dict]
 
 
 @dataclass(frozen=True)
 class _SimulationResponse:
-    ids: Iterable[int]
+    ids: List[int]
 
 
 def _two_train_simulation_payload(path_id: int, timetable_id: int, rolling_stock_id: int):
@@ -65,7 +78,6 @@ def _two_train_simulation_payload(path_id: int, timetable_id: int, rolling_stock
                 "message": "Timetable '-1', could not be found",
             },
         ),
-        (API_URL, {"detail": "Not found."}),
     ],
 )
 @pytest.mark.parametrize(["timetable_id", "status_code"], [(None, 200), (-1, 404)])
@@ -81,6 +93,7 @@ def test_get_timetable(
     timetable_id = timetable_id or small_scenario.timetable
 
     response = requests.get(f"{service_url}timetable/{timetable_id}/")
+    print(response.json())
     assert response.status_code == status_code
     if status_code == 404:
         assert expected_error == response.json()
@@ -88,7 +101,7 @@ def test_get_timetable(
 
     timetable = _TimetableResponse(**response.json())
     assert timetable.name == "timetable"
-    assert len(timetable.train_schedules) == 0
+    assert len(timetable.detailed_train_schedules) == 0
 
     # add simulation
     simulation_response = requests.post(
@@ -98,16 +111,51 @@ def test_get_timetable(
     assert simulation_response.status_code == 201
     simulation = _SimulationResponse(**simulation_response.json())
     assert len(simulation.ids) == 2
-
     response = requests.get(f"{service_url}timetable/{timetable_id}/")
     timetable = _TimetableResponse(**response.json())
     assert timetable.id == small_scenario.timetable
     assert timetable.name == "timetable"
-    assert len(timetable.train_schedules) == 2
+    assert len(timetable.detailed_train_schedules) == 2
     expected_schedules = [
-        _TrainScheduleDetails(simulation.ids[0], "West to South East 1", 3600, west_to_south_east_path.id),
-        _TrainScheduleDetails(simulation.ids[1], "West to South East 3", 5100, west_to_south_east_path.id),
+        _TrainScheduleDetails(
+            id=simulation.ids[0],
+            train_name="West to South East 1",
+            departure_time=3600,
+            arrival_time=4384.388061579815,
+            path_id=west_to_south_east_path.id,
+            timetable_id=small_scenario.timetable,
+            rolling_stock_id=fast_rolling_stock,
+            initial_speed=0,
+            labels=["new train", "west", "south east"],
+            allowances=[],
+            speed_limit_tags=None,
+            comfort="STANDARD",
+            options=None,
+            power_restriction_ranges=None,
+            mechanical_energy_consumed={"base": 6041639237.0, "eco": None},
+            stops=0,
+            path_length=45549.5653000392,
+        ),
+        _TrainScheduleDetails(
+            id=simulation.ids[1],
+            train_name="West to South East 3",
+            departure_time=5100,
+            arrival_time=5884.388061579815,
+            path_id=west_to_south_east_path.id,
+            timetable_id=small_scenario.timetable,
+            rolling_stock_id=fast_rolling_stock,
+            initial_speed=0,
+            labels=["new train", "west", "south east"],
+            allowances=[],
+            speed_limit_tags=None,
+            comfort="STANDARD",
+            options=None,
+            power_restriction_ranges=None,
+            mechanical_energy_consumed={"base": 6041639237.0, "eco": None},
+            stops=0,
+            path_length=45549.5653000392,
+        ),
     ]
     assert expected_schedules == [
-        _TrainScheduleDetails(**train_schedule) for train_schedule in timetable.train_schedules
+        _TrainScheduleDetails(**train_schedule) for train_schedule in timetable.detailed_train_schedules
     ]
