fe2075ca2c2200211a527b57424c0dd340a3394d
Uriel-Sautron <uriel.sautron@gmail.com>
2023-06-26 12:19:41 +0200

    front: test-e2e: SubmitConfAddTrainSchedule, TimetableManageTrainSchedule, ToastSNCF: Add data-testid. scenario-page-modal: Add locator and function for the test. operational-studies.spect: Add test to add a new train

--- front/tests/scenario-page-model.ts
+++ front/tests/scenario-page-model.ts
@@ -15,8 +15,6 @@ class PlaywrightScenarioPage {
 
   readonly getTrainLabels: Locator;
 
-  readonly getTrainSchedule: Locator;
-
   readonly getMapModule: Locator;
 
   readonly getSettingSimulationBtn: Locator;
@@ -24,6 +22,18 @@ class PlaywrightScenarioPage {
   readonly getResultPathfindingDistance: Locator;
 
   readonly getInfraLoadState: Locator;
+
+  readonly getAddTrainScheduleBtn: Locator;
+
+  readonly getTrainScheduleNameInput: Locator;
+
+  readonly getTrainTimetable: Locator;
+
+  readonly getReturnSimulationResultBtn: Locator;
+
+  readonly getToastSNCF: Locator;
+
+  readonly getToastSNCFTitle: Locator;
 
   constructor(readonly page: Page) {
     this.getRollingStockSelector = page.getByTestId('rollingstock-selector');
@@ -39,11 +49,18 @@ class PlaywrightScenarioPage {
       .getByTestId('display-itinerary')
       .getByTestId('itinerary-vias');
     this.getTrainLabels = page.getByTestId('add-train-labels');
-    this.getTrainSchedule = page.getByTestId('add-train-schedules');
     this.getMapModule = page.getByTestId('map');
     this.getSettingSimulationBtn = page.locator('span', { hasText: 'Paramètres de simulation' });
     this.getResultPathfindingDistance = page.getByTestId('result-pathfinding-distance');
     this.getInfraLoadState = page.locator('.infra-loading-state');
+    this.getAddTrainScheduleBtn = page.getByTestId('add-train-schedules');
+    this.getTrainScheduleNameInput = page.locator('#trainSchedule-name');
+    this.getTrainTimetable = page
+      .locator('.scenario-timetable-trains')
+      .locator('.scenario-timetable-train');
+    this.getReturnSimulationResultBtn = page.getByTestId('return-simulation-result');
+    this.getToastSNCF = page.getByTestId('toast-SNCF');
+    this.getToastSNCFTitle = this.getToastSNCF.getByTestId('toast-SNCF-title');
   }
 
   async openTabByText(text: string) {
@@ -56,6 +73,30 @@ class PlaywrightScenarioPage {
 
   async checkInfraLoaded() {
     await expect(this.getInfraLoadState).toHaveClass(/cached/);
+  }
+
+  async addTrainSchedule() {
+    await this.getAddTrainScheduleBtn.click();
+  }
+
+  async setTrainScheduleName(name: string) {
+    await this.getTrainScheduleNameInput.fill(name);
+  }
+
+  async checkNumberOfTrains(number: number) {
+    await expect(this.getTrainTimetable).toHaveCount(number);
+  }
+
+  async returnSimulationResult() {
+    await this.getReturnSimulationResultBtn.click();
+  }
+
+  async checkToastSNCFTitle(title: string | RegExp) {
+    await expect(this.getToastSNCFTitle).toHaveText(title);
+  }
+
+  getTrainTimetbleByName(name: string | RegExp) {
+    return this.page.getByRole('button', { name });
   }
 }
 
