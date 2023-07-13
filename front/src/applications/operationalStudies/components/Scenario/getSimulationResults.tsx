334638dba27e4ac0696b02fedf0249370c012214
SharglutDev <p.filimon75@gmail.com>
2023-07-13 13:13:53 +0200

    front: refacto gettimetable in tsx

---
+++ front/src/applications/operationalStudies/components/Scenario/getSimulationResults.tsx
@@ -0,0 +1,81 @@
+import {
+  updateAllowancesSettings,
+  updateIsUpdating,
+  updateSelectedProjection,
+  updateSelectedTrainId,
+  updateSimulation,
+} from 'reducers/osrdsimulation/actions';
+import { trainscheduleURI } from 'applications/operationalStudies/components/SimulationResults/simulationResultsConsts';
+import { get } from 'common/requests';
+import { setFailure } from 'reducers/main';
+import { store } from 'Store';
+import i18n from 'i18next';
+import { TimetableWithSchedulesDetails, osrdEditoastApi } from 'common/api/osrdEditoastApi';
+
+/**
+ * Recover the time table for all the trains
+ */
+
+export default async function getTimetable(timetable: TimetableWithSchedulesDetails) {
+  const { selectedProjection, allowancesSettings } = store.getState().osrdsimulation;
+  try {
+    store.dispatch(updateIsUpdating(true));
+    store.dispatch(updateSelectedTrainId(timetable.train_schedule_summaries[0].id));
+
+    const response = osrdEditoastApi.endpoints.getTrainScheduleResults.select({
+      pathId: 1,
+      trainIds: '1',
+    });
+    const trainSchedulesIDs = timetable.train_schedule_summaries.map((train) => train.id);
+
+    if (trainSchedulesIDs && trainSchedulesIDs.length > 0) {
+      let selectedProjectionPath;
+      if (!selectedProjection) {
+        const tempSelectedProjection = {
+          id: timetable.train_schedule_summaries[0].id,
+          path: timetable.train_schedule_summaries[0].path_id,
+        };
+        store.dispatch(updateSelectedProjection(tempSelectedProjection));
+        selectedProjectionPath = tempSelectedProjection.path;
+      } else if (selectedProjection) {
+        selectedProjectionPath = selectedProjection.path;
+      }
+      const simulationLocal = await get(`${trainscheduleURI}results/`, {
+        params: {
+          timetable_id: timetable.id,
+          path_id: selectedProjectionPath,
+        },
+      });
+      simulationLocal.sort((a, b) => a.base.stops[0].time > b.base.stops[0].time);
+      store.dispatch(updateSimulation({ trains: simulationLocal }));
+
+      // Create margins settings for each train if not set
+      const newAllowancesSettings = { ...allowancesSettings };
+      simulationLocal.forEach((train) => {
+        if (!newAllowancesSettings[train.id]) {
+          newAllowancesSettings[train.id] = {
+            base: true,
+            baseBlocks: true,
+            eco: true,
+            ecoBlocks: false,
+          };
+        }
+      });
+      store.dispatch(updateAllowancesSettings(newAllowancesSettings));
+      store.dispatch(updateIsUpdating(false));
+    } else {
+      store.dispatch(updateSimulation({ trains: [] }));
+      store.dispatch(updateIsUpdating(false));
+      store.dispatch(updateSelectedProjection(undefined));
+    }
+  } catch (e) {
+    store.dispatch(
+      setFailure({
+        name: i18n.t('simulation:errorMessages.unableToRetrieveTrainSchedule'),
+        message: `${e.message} `,
+      })
+    );
+    store.dispatch(updateIsUpdating(false));
+    console.error(e);
+  }
+}
