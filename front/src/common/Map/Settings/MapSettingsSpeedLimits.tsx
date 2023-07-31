bac7e07cf17d34dbad0e77f699be52b1e2e553fa
Uriel-Sautron <uriel.sautron@gmail.com>
2023-07-31 09:28:23 +0200

    front: MapSettingsSpeedLimits: refacto js to ts and use RTK

--- front/src/common/Map/Settings/MapSettingsSpeedLimits.tsx
+++ front/src/common/Map/Settings/MapSettingsSpeedLimits.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { updateLayersSettings } from 'reducers/map';
@@ -7,21 +6,44 @@ import { IoMdSpeedometer } from 'react-icons/io';
 import SwitchSNCF, { SWITCH_TYPES } from 'common/BootstrapSNCF/SwitchSNCF/SwitchSNCF';
 import SelectImprovedSNCF from 'common/BootstrapSNCF/SelectImprovedSNCF';
 import DotsLoader from 'common/DotsLoader/DotsLoader';
-import { get } from 'common/requests';
 import { setFailure } from 'reducers/main';
 import TIVsSVGFile from 'assets/pictures/layersicons/layer_tivs.svg';
 import { getInfraID } from 'reducers/osrdconf/selectors';
+import { getMap } from 'reducers/map/selectors';
+import { IconType } from 'react-icons';
+import { osrdEditoastApi } from 'common/api/osrdEditoastApi';
 import { FormatSwitch as SimpleFormatSwitch, Icon2SVG } from './MapSettingsLayers';
+import { ApiError } from 'common/api/emptyApi';
+import { SerializedError } from '@reduxjs/toolkit';
 
-function FormatSwitch(props) {
+type FormatSwitchProps = {
+  name: string;
+  icon: IconType;
+  color?: string;
+  disabled?: boolean;
+};
+
+const FormatSwitch = ({ name, icon: IconComponent, color = '', disabled }: FormatSwitchProps) => {
   const dispatch = useDispatch();
   const { t } = useTranslation(['map-settings']);
-  const { layersSettings } = useSelector((state) => state.map);
+  const { layersSettings } = useSelector(getMap);
   const infraID = useSelector(getInfraID);
-  const [speedLimitsTags, setSpeedLimitsTags] = useState(undefined);
-  const { name, icon, color, disabled = false } = props;
+  const {
+    data: tagsList,
+    isError: isGetSpeedLimitTagsError,
+    error: getSpeedLimitTagsError,
+  } = osrdEditoastApi.useGetInfraByIdSpeedLimitTagsQuery(
+    { id: infraID as number },
+    {
+      skip: !infraID,
+    }
+  );
 
-  const setLayerSettings = (setting) => {
+  const [speedLimitsTags, setSpeedLimitsTags] = useState<
+    { key: string; value: string }[] | undefined
+  >(undefined);
+
+  const setLayerSettings = (setting: keyof typeof layersSettings) => {
     dispatch(
       updateLayersSettings({
         ...layersSettings,
@@ -30,7 +52,7 @@ function FormatSwitch(props) {
     );
   };
 
-  const dispatchSetSpeedLimitsTags = (item) => {
+  const dispatchSetSpeedLimitsTags = (item: { key: string; value: string }) => {
     dispatch(
       updateLayersSettings({
         ...layersSettings,
@@ -39,31 +61,30 @@ function FormatSwitch(props) {
     );
   };
 
-  const getTagsList = async (_zoom, _params) => {
-    try {
-      const tagsList = await get(`/editoast/infra/${infraID}/speed_limit_tags/`);
-
-      // construct an object from array to add "undefined" value
-      const tagsListObject = [{ key: 'undefined', value: t('noSpeedLimitByTag') }].concat(
-        tagsList.map((tag) => ({ key: tag, value: tag }))
-      );
-
-      setSpeedLimitsTags(tagsListObject);
-    } catch (e) {
-      dispatch(
-        setFailure({
-          name: t('errorMessages.unableToRetrieveTags'),
-          message: `${e.message} : ${e.response && e.response.data.detail}`,
-        })
-      );
-    }
+  const changeTagsListToObject = (tagsListArray: string[]) => {
+    const tagsListObject = [{ key: t('noSpeedLimitByTag'), value: t('noSpeedLimitByTag') }].concat(
+      tagsListArray.slice().map((tag) => ({ key: tag, value: tag }))
+    );
+    setSpeedLimitsTags(tagsListObject);
   };
 
   useEffect(() => {
     setSpeedLimitsTags(undefined);
-    getTagsList();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [infraID]);
+    if (tagsList) changeTagsListToObject(tagsList);
+  }, [infraID, tagsList]);
+
+  useEffect(() => {
+    if (isGetSpeedLimitTagsError && getSpeedLimitTagsError) {
+      dispatch(
+        setFailure({
+          name: t('errorMessages.unableToRetrieveTags'),
+          message:
+            `${(getSpeedLimitTagsError as ApiError).data.message}` ||
+            `${(getSpeedLimitTagsError as SerializedError).message}}`,
+        })
+      );
+    }
+  }, [isGetSpeedLimitTagsError]);
 
   return (
     <>
@@ -73,11 +94,13 @@ function FormatSwitch(props) {
             id={`${name}-switch`}
             type={SWITCH_TYPES.switch}
             name={`${name}-switch`}
-            onChange={() => setLayerSettings(name)}
-            checked={layersSettings[name]}
+            onChange={() => setLayerSettings(name as keyof typeof layersSettings)}
+            checked={!!layersSettings[name as keyof typeof layersSettings]}
             disabled={disabled}
           />
-          <span className={`px-1 d-flex align-items-center ${color}`}>{icon}</span>
+          <span className={`px-1 d-flex align-items-center ${color}`}>
+            <IconComponent />
+          </span>
           <small>{t(name)}</small>
         </div>
       </div>
@@ -98,26 +121,15 @@ function FormatSwitch(props) {
       </div>
     </>
   );
-}
+};
 
 export default function MapSettingsLayers() {
   return (
     <div className="row">
-      <FormatSwitch name="speedlimits" icon={<IoMdSpeedometer />} />
+      <FormatSwitch name="speedlimits" icon={IoMdSpeedometer} />
       <div className="col-lg-6">
         <SimpleFormatSwitch name="sncf_lpv" icon={Icon2SVG(TIVsSVGFile, 'SNCF LPV TIV icon svg')} />
       </div>
     </div>
   );
 }
-
-FormatSwitch.propTypes = {
-  name: PropTypes.string.isRequired,
-  icon: PropTypes.object.isRequired,
-  color: PropTypes.string,
-  disabled: PropTypes.bool,
-};
-FormatSwitch.defaultProps = {
-  color: '',
-  disabled: false,
-};
