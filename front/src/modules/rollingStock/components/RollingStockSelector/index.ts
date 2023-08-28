cab1c522a6af721617c5d9e9a1e221c384d0dca8
SarahBellaha <sarah.bellaha@sncf.fr>
2023-08-28 17:01:28 +0200

    front: edit import of rollingstockselector

--- front/src/modules/rollingStock/components/RollingStockSelector/index.ts
+++ front/src/modules/rollingStock/components/RollingStockSelector/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export { default as RollingStockSelector } from 'modules/rollingStock/components/RollingStockSelector/RollingStockSelector';
+export { default as RollingStockSelector } from 'modules/rollingStock/components/RollingStockSelector/WithRollingStockSelector';
 export { default as RollingStock2Img } from 'modules/rollingStock/components/RollingStockSelector/RollingStock2Img';
 export { default as RollingStockCard } from 'modules/rollingStock/components/RollingStockSelector/RollingStockCard';
 export { default as RollingStockCardButtons } from 'modules/rollingStock/components/RollingStockSelector/RollingStockCardButtons';
@@ -8,4 +8,3 @@ export { default as RollingStockCurve } from 'modules/rollingStock/components/Ro
 export { default as RollingStockEmpty } from 'modules/rollingStock/components/RollingStockSelector/RollingStockEmpty';
 export { default as RollingStockModal } from 'modules/rollingStock/components/RollingStockSelector/RollingStockModal';
 export { default as SearchRollingStock } from 'modules/rollingStock/components/RollingStockSelector/SearchRollingStock';
-export { default as WithRollingStockSelector } from 'modules/rollingStock/components/RollingStockSelector/WithRollingStockSelector';
