ca80a3d14daef06e0f93d1d45b87b7f6aadcfd9f
Uriel-Sautron <uriel.sautron@gmail.com>
2023-08-30 15:31:28 +0200

    front: refacto: Migrate simulationResultsHelpers to TS

--- front/src/applications/operationalStudies/components/SimulationResults/simulationResultsHelpers.ts
+++ front/src/applications/operationalStudies/components/SimulationResults/simulationResultsHelpers.ts
@@ -1,13 +1,15 @@
+import { TrainSchedule } from 'common/api/osrdEditoastApi';
 import { get, patch } from 'common/requests';
 import { noop } from 'lodash';
 import { updateReloadTimetable } from 'reducers/osrdsimulation/actions';
+import { Train } from 'reducers/osrdsimulation/types';
 
 /**
  * Premare the params to override the trains details and save them
  * @param {object} simulationTrain
  * @returns
  */
-export function getTrainDetailsForAPI(simulationTrain) {
+export function getTrainDetailsForAPI(simulationTrain: Train) {
   const params = {
     id: simulationTrain.id,
     departure_time: simulationTrain.base.stops[0].time,
@@ -23,7 +25,7 @@ export function getTrainDetailsForAPI(simulationTrain) {
  * @param {object} details
  * @param {int} id
  */
-export async function changeTrain(details, id, dispatch = noop) {
+export async function changeTrain(details: TrainSchedule, id: number, dispatch = noop) {
   try {
     const trainDetail = await get(`/editoast/train_schedule/${id}/`);
     try {
@@ -33,9 +35,9 @@ export async function changeTrain(details, id, dispatch = noop) {
         departure_time: details.departure_time || trainDetail.departure_time,
         initial_speed: details.initial_speed || trainDetail.initial_speed,
         labels: details.labels || trainDetail.labels,
-        path: details.path || trainDetail.path,
-        rolling_stock: details.rolling_stock || trainDetail.rolling_stock,
-        timetable: details.timetable || trainDetail.timetable,
+        path: details.path_id || trainDetail.path,
+        rolling_stock: details.rolling_stock_id || trainDetail.rolling_stock,
+        timetable: details.timetable_id || trainDetail.timetable,
         train_name: details.train_name || trainDetail.train_name,
       };
       dispatch(updateReloadTimetable(true));
