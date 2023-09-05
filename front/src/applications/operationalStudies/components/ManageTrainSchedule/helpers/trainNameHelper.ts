28a2c84949c28fa110b479d43b536c6430ecd890
SharglutDev <p.filimon75@gmail.com>
2023-09-05 09:14:19 +0200

    front: migrate trainnamehelper to typescript

--- front/src/applications/operationalStudies/components/ManageTrainSchedule/helpers/trainNameHelper.ts
+++ front/src/applications/operationalStudies/components/ManageTrainSchedule/helpers/trainNameHelper.ts
@@ -1,10 +1,10 @@
-const trainNameWithNum = (name, actualTrainCount, total) => {
+const trainNameWithNum = (name: string, actualTrainCount: number, total: number): string => {
   if (total === 1) {
     return name;
   }
   // Test if numeric & integer in a good way
   if (/^\d+$/.test(name)) {
-    return parseInt(name, 10) + (actualTrainCount - 1);
+    return (parseInt(name, 10) + (actualTrainCount - 1)).toString();
   }
   return `${name} ${actualTrainCount}`;
 };
