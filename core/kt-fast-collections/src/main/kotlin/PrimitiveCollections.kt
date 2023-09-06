8977526094a487344a781e84733c232108f8bbd2
Younes Khoudli <younes.khoudli@epita.fr>
2023-09-06 12:13:33 +0200

    core: generate fast collection for primitive types

---
+++ core/kt-fast-collections/src/main/kotlin/PrimitiveCollections.kt
@@ -0,0 +1,41 @@
+@file:PrimitiveCollections(
+    primitive = Int::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = UInt::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = Long::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = ULong::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = Float::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = Double::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = Boolean::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+@file:PrimitiveCollections(
+    primitive = Char::class,
+    collections = ["Array", "ArrayList", "ArraySortedSet"],
+)
+
+package fr.sncf.osrd.fast_collections
