3eaf53dd46d537251d0fbe1226d0f62644679662
Younes Khoudli <younes.khoudli@epita.fr>
2023-09-06 15:51:37 +0200

    core: fast-collections: add a ring buffer

--- core/kt-fast-collections/src/main/kotlin/PrimitiveCollections.kt
+++ core/kt-fast-collections/src/main/kotlin/PrimitiveCollections.kt
@@ -1,36 +1,36 @@
 @file:PrimitiveCollections(
     primitive = Int::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = UInt::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = Long::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = ULong::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = Float::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = Double::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
     primitive = Boolean::class,
-    collections = ["Array", "ArrayList", "ArraySortedSet"],
+    collections = ["Array", "ArrayList", "ArraySortedSet", "RingBuffer"],
 )
 
 @file:PrimitiveCollections(
