c72b263393fbb9c96671f05993f83c6314d89011
Florian Amsallem <florian.amsallem@epita.fr>
2023-09-12 18:13:49 +0200

    scripts: adapt to drop api

--- scripts/generate-infra.sh
+++ scripts/generate-infra.sh
@@ -8,12 +8,12 @@ infra_name="${1:-small_infra}"
 
 echo "Loading $infra_name"
 python3 python/railjson_generator/railjson_generator/scripts/generate.py /tmp/generated_infras/
-docker cp "/tmp/generated_infras/${infra_name}/infra.json" osrd-api:/tmp/infra.json
-docker exec osrd-api python manage.py import_railjson "${infra_name}" /tmp/infra.json
+docker cp "/tmp/generated_infras/${infra_name}/infra.json" osrd-editoast:/tmp/infra.json
+docker exec osrd-editoast editoast import-railjson "${infra_name}" /tmp/infra.json
 
 echo "Generate layers"
 docker exec osrd-editoast editoast generate
 
 echo "Loading example rolling stock"
-docker cp python/api/static/example_rolling_stock.json osrd-api:/tmp/stock.json
-docker exec osrd-api python manage.py import_rolling_stock -f /tmp/stock.json
+docker cp editoast/src/tests/example_rolling_stock_1.json osrd-editoast:/tmp/stock.json
+docker exec osrd-editoast editoast import-rolling-stock /tmp/stock.json
