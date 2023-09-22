5a73255eb0a8f6678e7e0c2579e25d4f61ba51b9
Uriel-Sautron <uriel.sautron@gmail.com>
2023-09-22 09:29:27 +0200

    front: simulationResultsHelpers: fix Paul'comments and use i18n for error message

--- front/src/applications/operationalStudies/components/SimulationResults/simulationResultsHelpers.ts
+++ front/src/applications/operationalStudies/components/SimulationResults/simulationResultsHelpers.ts
@@ -1,3 +1,5 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { ApiError } from 'common/api/emptyApi';
 import {
   SimulationReport,
   TrainSchedule,
@@ -8,6 +10,7 @@ import { setFailure } from 'reducers/main';
 import { updateReloadTimetable } from 'reducers/osrdsimulation/actions';
 import { Train } from 'reducers/osrdsimulation/types';
 import { Dispatch } from 'redux';
+import i18next from 'i18next';
 
 /**
  * Premare the params to override the trains details and save them
@@ -21,6 +24,8 @@ export const getTrainDetailsForAPI = (
   departure_time: simulationTrain.base.stops[0].time,
   train_name: simulationTrain.base.stops[0].name || simulationTrain.name,
 });
+// Nedded to load the namespace for i18n
+i18next.loadNamespaces('operationalStudies/manageTrainSchedule');
 
 /**
  * Use the trainScheduleAPI to update train details on a specific computation
@@ -31,43 +36,53 @@ export const getTrainDetailsForAPI = (
  */
 export const changeTrain =
   (details: Partial<TrainSchedule>, id: number) => async (dispatch: Dispatch) => {
-    try {
-      const { data: trainDetail } = await dispatch(
-        osrdEditoastApi.endpoints.getTrainScheduleById.initiate({ id })
+    const {
+      data: trainDetails,
+      isError: isGetTrainDetailsError,
+      error: getTrainDetailsError,
+      isSuccess: isGetTrainDetailsSuccess,
+    } = await dispatch(osrdEditoastApi.endpoints.getTrainScheduleById.initiate({ id }));
+
+    if (isGetTrainDetailsSuccess) {
+      // TODO: add the other information of the trainSchedule (allowances...)
+      const trainSchedule: TrainSchedulePatch = {
+        id,
+        departure_time: details.departure_time || trainDetails?.departure_time,
+        initial_speed: details.initial_speed || trainDetails?.initial_speed,
+        labels: details.labels || trainDetails?.labels,
+        path_id: details.path_id || trainDetails?.path_id,
+        rolling_stock_id: details.rolling_stock_id || trainDetails?.rolling_stock_id,
+        train_name: details.train_name || trainDetails?.train_name,
+      };
+      dispatch(updateReloadTimetable(true));
+      const response = await dispatch(
+        osrdEditoastApi.endpoints.patchTrainSchedule.initiate({ body: [trainSchedule] })
       );
-      try {
-        // TODO: add the other information of the trainSchedule (allowances...)
-        const trainSchedule: TrainSchedulePatch = {
-          id,
-          departure_time: details.departure_time || trainDetail?.departure_time,
-          initial_speed: details.initial_speed || trainDetail?.initial_speed,
-          labels: details.labels || trainDetail?.labels,
-          path_id: details.path_id || trainDetail?.path_id,
-          rolling_stock_id: details.rolling_stock_id || trainDetail?.rolling_stock_id,
-          train_name: details.train_name || trainDetail?.train_name,
-        };
-        dispatch(updateReloadTimetable(true));
-        dispatch(osrdEditoastApi.endpoints.patchTrainSchedule.initiate({ body: [trainSchedule] }));
-        dispatch(updateReloadTimetable(false));
-      } catch (e) {
-        dispatch(updateReloadTimetable(false));
-        if (e instanceof Error) {
-          dispatch(
-            setFailure({
-              name: e.name,
-              message: e.message,
-            })
-          );
-        }
-      }
-    } catch (e) {
-      if (e instanceof Error) {
+      dispatch(updateReloadTimetable(false));
+      if ('error' in response) {
         dispatch(
           setFailure({
-            name: e.name,
-            message: e.message,
+            name: i18next.t(
+              'operationalStudies/manageTrainSchedule:errorMessages.unableToPatchTrain'
+            ),
+            message: `${
+              (response.error as ApiError)?.data?.message ||
+              (response.error as SerializedError)?.message
+            }`,
           })
         );
       }
+    } else if (isGetTrainDetailsError && getTrainDetailsError) {
+      dispatch(
+        setFailure({
+          name: i18next.t(
+            'operationalStudies/manageTrainSchedule:errorMessages.unableToRetrieveTrain'
+          ),
+          message: `${
+            (getTrainDetailsError as ApiError)?.data?.message ||
+            (getTrainDetailsError as SerializedError)?.message
+          }`,
+        })
+      );
     }
   };
