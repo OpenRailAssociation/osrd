a006526b3323b1e8384bc9809a11b5aa21fde0de
Clara Ni <clara.ni@student-cs.fr>
2023-10-05 11:43:10 +0200

    front: infra is displayed on the warped map

--- front/src/common/Map/WarpedMap/DataLoader.tsx
+++ front/src/common/Map/WarpedMap/DataLoader.tsx
@@ -18,6 +18,7 @@ import { getMap } from 'reducers/map/selectors';
 import { OSM_URL } from 'common/Map/const';
 import { simplifyFeature } from 'common/Map/WarpedMap/core/helpers';
 import OrderedLayer from 'common/Map/Layers/OrderedLayer';
+import { transformMapRequest } from 'reducers/map';
 
 const TIME_LABEL = 'Loading OSRD and OSM data around warped path';
 
@@ -141,6 +142,7 @@ const DataLoader: FC<{
             ref={setMapRef}
             mapStyle={osmBlankStyle}
             style={{ width: '100%', height: '100%' }}
+            transformRequest={transformMapRequest}
           >
             {state === 'render' && (
               <>
