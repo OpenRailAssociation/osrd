229554502ba6057f62d0895693119cfbf73c928c
Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>
2023-10-11 15:54:19 +0200

    fixup! editoast: modify schemas, delete track_link and adapt editoast

--- editoast/migrations/2023-10-11-222214_delete_track_links/up.sql
+++ editoast/migrations/2023-10-11-222214_delete_track_links/up.sql
@@ -1,4 +1,5 @@
 -- Your SQL goes here
-
 DROP TABLE infra_layer_track_section_link;
 DROP TABLE infra_object_track_section_link;
+DELETE FROM infra_layer_error
+WHERE information->>'obj_type' = 'TrackSectionLink';
--- editoast/src/generated_data/error/switches.rs
+++ editoast/src/generated_data/error/switches.rs
@@ -82,8 +82,6 @@ pub fn check_match_ports_type(
     }
 }
 
-// TODO : check overlapping switch link and classical switch ?
-
 /// Check if the switch ports are not already used by another switch
 fn check_overlapping(
     switch: &ObjectCache,
