5187774c4342446c1c0660ccebf303d17b67c48b
Florian Amsallem <florian.amsallem@epita.fr>
2023-10-12 17:26:01 +0200

    editoast: add tvm atlas and config

--- editoast/src/views/sprites.rs
+++ editoast/src/views/sprites.rs
@@ -15,11 +15,14 @@ crate::routes! {
 }
 
 #[derive(Debug, Error, EditoastError)]
-#[editoast_error(base_id = "scenario")]
+#[editoast_error(base_id = "sprites")]
 enum SpriteErrors {
     #[error("Unknown signaling system '{0}'")]
     #[editoast_error(status = 404)]
     UnknownSignalingSystem(String),
+    #[error("File '{0}' not found")]
+    #[editoast_error(status = 404)]
+    FileNotFound(String),
 }
 
 /// This endpoint returns the list of supported signaling systems
@@ -41,14 +44,14 @@ async fn signaling_systems() -> Result<Json<Vec<String>>> {
     tag = "sprites",
     params(
         ("signaling_system" = String, Path, description = "Signaling system name"),
-        ("file_name" = String, Path, description = "Atlas file name (json or png) respecting map libre documentation"),
+        ("file_name" = String, Path, description = "File name (json, png or svg)"),
     ),
     responses(
         (status = 200, description = "Atlas image of config"),
         (status = 404, description = "Signaling system not found"),
     ),
 )]
-#[get("/{signaling_system}/{file_name:sprites(@[23]x)?\\.(json|png)}")]
+#[get("/{signaling_system}/{file_name:[-_ @0-9A-Za-z]+\\.(json|png|svg)}")]
 async fn sprites(path: Path<(String, String)>) -> Result<NamedFile> {
     let (signaling_system, file_name) = path.into_inner();
     let sprite_configs = SpriteConfig::load();
@@ -56,6 +59,8 @@ async fn sprites(path: Path<(String, String)>) -> Result<NamedFile> {
         return Err(SpriteErrors::UnknownSignalingSystem(signaling_system).into());
     }
     let path = get_assets_path().join(format!("signal_sprites/{signaling_system}/{file_name}"));
-    assert!(path.is_file());
+    if !path.is_file() {
+        return Err(SpriteErrors::FileNotFound(file_name).into());
+    }
     Ok(NamedFile::open(path).unwrap().use_last_modified(false))
 }
