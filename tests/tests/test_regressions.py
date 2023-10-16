327905cac53e2afb28a7f7ad5154e5b821669362
Stéphane Henriot <shenriot.pro@gmail.com>
2023-10-16 12:53:51 +0200

    tests: export/import train requests as STDCM fuzzer prelude

--- tests/tests/test_regressions.py
+++ tests/tests/test_regressions.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import List
 
 import pytest
 import requests
@@ -11,7 +12,7 @@ REGRESSION_TESTS_DATA_FOLDER = Path(__file__).parent / "regression_tests_data"
 REGRESSION_TESTS_JSON_FILES = [json_file.name for json_file in REGRESSION_TESTS_DATA_FOLDER.resolve().glob("*.json")]
 
 
-def _load_infra(editoast_url, infra_id):
+def _load_infra(editoast_url: str, infra_id):
     r = requests.post(editoast_url + f"infra/{infra_id}/load")
     if r.status_code // 100 != 2:
         if r.status_code // 100 == 4:
@@ -19,7 +20,7 @@ def _load_infra(editoast_url, infra_id):
         raise RuntimeError(f"Infra load {r.status_code}: {r.content}")
 
 
-def _pathfinding_with_payload(editoast_url, payload, infra_id, accept_400):
+def _pathfinding_with_payload(editoast_url: str, payload, infra_id, accept_400):
     payload["infra"] = infra_id
     r = requests.post(editoast_url + "pathfinding/", json=payload)
     if r.status_code // 100 != 2:
@@ -29,13 +30,38 @@ def _pathfinding_with_payload(editoast_url, payload, infra_id, accept_400):
     return r.json()["id"]
 
 
-def _schedule_with_payload(editoast_url, payload, accept_400):
+def _schedule_with_payload(editoast_url: str, payload, accept_400):
     r = requests.post(editoast_url + "train_schedule/standalone_simulation/", json=payload)
     if r.status_code // 100 != 2:
         if r.status_code // 100 == 4 and accept_400:
             return None
         raise RuntimeError(f"Schedule error {r.status_code}: {r.content}")
     return r.json()[0]
+
+
+def _update_schedule_payload(payload, path_id, timetable, rolling_stock_id: int):
+    payload["path"] = path_id
+    payload["timetable"] = timetable
+    payload["schedules"][0]["rolling_stock"] = rolling_stock_id
+
+
+def _check_result(editoast_url: str, schedule_id):
+    r = requests.get(f"{EDITOAST_URL}train_schedule/{schedule_id}/result/")
+    if r.status_code // 100 != 2:
+        raise RuntimeError(f"Schedule error {r.status_code}: {r.content}, id={schedule_id}")
+
+
+def _apply_prelude(prelude: List, editoast_url: str, infra, timetable, rolling_stock_id: int):
+    for train in prelude:
+        assert "path_payload" in train and "schedule_payload" in train
+
+        path_id = _pathfinding_with_payload(editoast_url, train["path_payload"], infra, accept_400=False)
+
+        schedule_payload = train["schedule_payload"]
+        _update_schedule_payload(schedule_payload, path_id, timetable, rolling_stock_id)
+        schedule_id = _schedule_with_payload(editoast_url, schedule_payload, accept_400=False)
+
+        _check_result(editoast_url, schedule_id)
 
 
 def _stdcm_with_payload(editoast_url: str, payload):
@@ -50,15 +76,17 @@ def _reproduce_test(path_to_json: Path, scenario: Scenario, rolling_stock_id: in
     fuzzer_output = json.loads(path_to_json.read_bytes())
     _load_infra(EDITOAST_URL, scenario.infra)
 
+    assert fuzzer_output["infra_name"] in ["small_infra", "Small Infra"]
+    timetable = scenario.timetable
+
     if fuzzer_output["error_type"] == "STDCM":
+        _apply_prelude(fuzzer_output.get("prelude", []), EDITOAST_URL, scenario.infra, timetable, rolling_stock_id)
         _stdcm_with_payload(EDITOAST_URL, fuzzer_output["stdcm_payload"])
         return
 
     stop_after_pathfinding = fuzzer_output["error_type"] == "PATHFINDING"
     stop_after_schedule = fuzzer_output["error_type"] == "SCHEDULE"
 
-    assert fuzzer_output["infra_name"] in ["small_infra", "Small Infra"]
-    timetable = scenario.timetable
     path_id = _pathfinding_with_payload(
         EDITOAST_URL, fuzzer_output["path_payload"], scenario.infra, stop_after_pathfinding
     )
@@ -66,16 +94,12 @@ def _reproduce_test(path_to_json: Path, scenario: Scenario, rolling_stock_id: in
         return
 
     payload = fuzzer_output["schedule_payload"]
-    payload["path"] = path_id
-    payload["timetable"] = timetable
-    payload["schedules"][0]["rolling_stock"] = rolling_stock_id
+    _update_schedule_payload(payload, path_id, timetable, rolling_stock_id)
     schedule_id = _schedule_with_payload(EDITOAST_URL, payload, stop_after_schedule)
     if stop_after_schedule:
         return
 
-    r = requests.get(f"{EDITOAST_URL}train_schedule/{schedule_id}/result/")
-    if r.status_code // 100 != 2:
-        raise RuntimeError(f"Schedule error {r.status_code}: {r.content}, id={schedule_id}")
+    _check_result(EDITOAST_URL, schedule_id)
 
 
 @pytest.mark.parametrize("file_name", REGRESSION_TESTS_JSON_FILES)
