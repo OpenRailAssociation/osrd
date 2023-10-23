571213e915749c36627bfc73e0491f64afe835ce
Nicolas Wurtz <gitlab@nethenic.net>
2023-10-23 19:04:50 +0200

    front: kps signals: more concise map settings

--- front/src/common/Map/Settings/MapSettingsLayers.tsx
+++ front/src/common/Map/Settings/MapSettingsLayers.tsx
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { updateLayersSettings } from 'reducers/map';
 import { GiElectric, GiUnplugged } from 'react-icons/gi';
-import { AiOutlineBlock } from 'react-icons/ai';
-import { MdSpaceBar } from 'react-icons/md';
+
+import MapSettingsSignals from 'common/Map/Settings/MapSettingsSignals';
 
 import BufferStopSVGFile from 'assets/pictures/layersicons/bufferstop.svg';
 import OPsSVGFile from 'assets/pictures/layersicons/ops.svg';
@@ -60,44 +60,41 @@ export const Icon2SVG: FC<{ file: string; altName?: string }> = ({ file, altName
 );
 
 const MapSettingsLayers: FC<unknown> = () => (
-  <div className="row">
-    <div className="col-md-6">
-      <FormatSwitch name="catenaries" icon={<GiElectric />} />
+  <>
+    <MapSettingsSignals />
+    <div className="row mt-2">
+      <div className="col-md-6">
+        <FormatSwitch name="catenaries" icon={<GiElectric />} />
+      </div>
+      <div className="col-md-6">
+        <FormatSwitch name="neutral_sections" icon={<GiUnplugged />} />
+      </div>
+      <div className="col-md-6">
+        <FormatSwitch
+          name="operationalpoints"
+          icon={<Icon2SVG file={OPsSVGFile} altName="Operationnal points svg" />}
+        />
+      </div>
+      <div className="col-md-6">
+        <FormatSwitch
+          name="switches"
+          icon={<Icon2SVG file={SwitchesSVGFile} altName="Switches icon svg" />}
+        />
+      </div>
+      <div className="col-md-6">
+        <FormatSwitch
+          name="bufferstops"
+          icon={<Icon2SVG file={BufferStopSVGFile} altName="Buffer stop svg" />}
+        />
+      </div>
+      <div className="col-md-6">
+        <FormatSwitch
+          name="detectors"
+          icon={<Icon2SVG file={DetectorsSVGFile} altName="Detectors circles svg" />}
+        />
+      </div>
     </div>
-    <div className="col-md-6">
-      <FormatSwitch name="neutral_sections" icon={<GiUnplugged />} />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch name="signalingtype" icon={<AiOutlineBlock />} disabled />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch name="tvds" icon={<MdSpaceBar />} disabled />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch
-        name="operationalpoints"
-        icon={<Icon2SVG file={OPsSVGFile} altName="Operationnal points svg" />}
-      />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch
-        name="switches"
-        icon={<Icon2SVG file={SwitchesSVGFile} altName="Switches icon svg" />}
-      />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch
-        name="bufferstops"
-        icon={<Icon2SVG file={BufferStopSVGFile} altName="Buffer stop svg" />}
-      />
-    </div>
-    <div className="col-md-6">
-      <FormatSwitch
-        name="detectors"
-        icon={<Icon2SVG file={DetectorsSVGFile} altName="Detectors circles svg" />}
-      />
-    </div>
-  </div>
+  </>
 );
 
 export default MapSettingsLayers;
