b3279ec82c29b3e7e49d6bf14eba6e49789a0094
Stéphane Henriot <shenriot.pro@gmail.com>
2023-11-06 15:45:25 +0100

    python: fix some railjson pytypes (*args tuple)

--- python/railjson_generator/railjson_generator/simulation_builder.py
+++ python/railjson_generator/railjson_generator/simulation_builder.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Union
+from typing import Sequence, Union
 
 from railjson_generator.schema.infra.direction import Direction
 from railjson_generator.schema.location import DirectedLocation, Location
@@ -14,14 +14,14 @@ from railjson_generator.schema.simulation.train_schedule import (
 class SimulationBuilder:
     simulation: Simulation = field(default_factory=Simulation)
 
-    def add_train_schedule(self, *locations: List[Location], **kwargs) -> TrainSchedule:
+    def add_train_schedule(self, *locations: Location, **kwargs) -> TrainSchedule:
         """Creates a train schedule group containing only this train schedule."""
         train_schedule = TrainSchedule(**kwargs)
         self.add_train_schedule_group(locations, train_schedule)
         return train_schedule
 
     def add_train_schedule_group(
-        self, locations: Union[List[Location], List[DirectedLocation]], *train_schedules: List[TrainSchedule]
+        self, locations: Union[Sequence[Location], Sequence[DirectedLocation]], *train_schedules: TrainSchedule
     ) -> TrainScheduleGroup:
         if len(locations) < 2:
             raise ValueError(f"Expected at least 2 locations, got {len(locations)}")
@@ -31,7 +31,7 @@ class SimulationBuilder:
             ]
         else:
             locations = [[loc] for loc in locations]
-        train_schedule_group = TrainScheduleGroup(train_schedules, locations)
+        train_schedule_group = TrainScheduleGroup(list(train_schedules), locations)
         self.simulation.train_schedule_groups.append(train_schedule_group)
         return train_schedule_group
 
