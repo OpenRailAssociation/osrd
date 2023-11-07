8046235e94a1d2af938dff4e23910868f0f17e03
Stéphane Henriot <shenriot.pro@gmail.com>
2023-11-07 15:21:05 +0100

    python: skip some railjson pytypes (section)

--- python/railjson_generator/railjson_generator/schema/infra/speed_section.py
+++ python/railjson_generator/railjson_generator/schema/infra/speed_section.py
@@ -9,8 +9,8 @@ from railjson_generator.schema.infra.range_elements import (
 
 
 def _speed_section_id():
-    res = f"speed_section.{SpeedSection._INDEX}"
-    SpeedSection._INDEX += 1
+    res = f"speed_section.{SpeedSection._INDEX}"  # pytype: disable=name-error
+    SpeedSection._INDEX += 1  # pytype: disable=name-error
     return res
 
 
