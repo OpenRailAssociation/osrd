940320310496b553c3780f56a1c8fbb15818d0db
Victor "multun" Collod <victor.collod@epita.fr>
2023-11-08 15:32:15 +0100

    actions: make a ghcr cleanup script

--- .github/scripts/cleanup-ghcr.sh
+++ .github/scripts/cleanup-ghcr.sh
@@ -1,34 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# gh cli is buggy as of the writing of this script, and wants both
+# of these environment variables to understand it's a script
+export CLICOLOR=0 CLICOLOR_FORCE=0
+
+: ${ORG:=osrd-project}
+: ${RETENTION_PERIOD_DAYS:=30}
+: ${PACKAGE_FILTER_REGEX:='.*'}  # this regex matches everything
+: ${EXCLUDE_TAGS_REGEX:='^\b$'}  # this regex never matches anything
 
 # Define the retention period in milliseconds (30 days in this case)
-RETENTION_PERIOD_IN_DAYS=30
-RETENTION_PERIOD_MS=$((RETENTION_PERIOD_IN_DAYS * 24 * 60 * 60 * 1000))
-# Get the current Unix timestamp in milliseconds
-CURRENT_MS=$(($(date +%s%3N)))
+retention_period_seconds=$((RETENTION_PERIOD_DAYS * 24 * 60 * 60))
 
-# Function to get the age of the image in milliseconds
-get_age_ms() {
-    echo $((CURRENT_MS - $1))
+# The threshold time for package version deletion
+retention_threshold=$(($(date +%s) - retention_period_seconds))
+
+select_packages() {
+    jq 'map(select(.name | test($regex_filter)))' --arg regex_filter "$1"
 }
 
-# Fetch the list of tags for the image
-# Assuming you have jq installed and the GH CLI set up
-tags=$(gh cr list --namespace osrd-project --name $IMAGE_NAME --json tag --jq '.[].tag' | tr -d '"')
+# packages ORG [PACKAGE-TYPE]
+packages() {
+    gh api "orgs/$1/packages?package_type=${2:-container}"
+}
 
-# Loop over each tag
-for tag in $tags; do
-    # If the tag is a number (i.e., a Unix timestamp in milliseconds), check its age
-    if [[ $tag =~ ^[0-9]+$ ]]; then
-        age_ms=$(get_age_ms $tag)
+# takes a json list of packages, and outputs newline separated url-encoded package names
+packages_names() {
+    jq -r '.[].name | @uri'
+}
 
-        # If the age is greater than the retention period, delete the image
-        if [ "$age_ms" -ge "$RETENTION_PERIOD_MS" ]; then
-            echo "Deleting image osrd-project/$IMAGE_NAME:$tag..."
-            gh cr delete osrd-project/$IMAGE_NAME:$tag
-        else
-            echo "Keeping image osrd-project/$IMAGE_NAME:$tag..."
-        fi
-    else
-        echo "Skipping tag $tag as it does not represent a Unix timestamp in milliseconds."
-    fi
+# package_versions ORG PACKAGE [PACKAGE-TYPE]
+# the org and package names must be url-encoded
+package_versions() {
+    gh api "orgs/$1/packages/${3:-container}/$2/versions"
+}
+
+# takes a json array stream of package versions, and selects the ones last updated before
+# the given unix timestamp
+select_expired_versions() {
+    jq 'map(select(.updated_at | fromdate | . <= $exptime))' --argjson exptime "$1"
+}
+
+# takes a json list of package versions, and filter out those with an excluded tag
+exclude_tagged_versions() {
+    jq 'map(select(.metadata.container.tags | any(test($exclude_tags_regex)) | not))' --arg exclude_tags_regex "$1"
+}
+
+# delete_package_version ORG PACKAGE VERSION [PACKAGE-TYPE]
+delete_package_version() {
+    echo "Deleting package $2 version $3"
+    gh api --method DELETE "orgs/$1/packages/${4:-container}/$2/versions/$3"
+}
+
+delete_package_versions() {
+    jq '.[].id' | while IFS= read -r version; do
+        delete_package_version "$ORG" "$package" "$version"
+    done
+}
+
+# for all packages in the organization that match the regex
+packages "$ORG" | select_packages "${PACKAGE_FILTER_REGEX}" | packages_names | while IFS= read -r package; do
+    echo "Processing package $package..."
+    package_versions "$ORG" "$package" | \
+        select_expired_versions "$retention_threshold" | \
+        exclude_tagged_versions "${EXCLUDE_TAGS_REGEX}" | \
+        delete_package_versions
 done
