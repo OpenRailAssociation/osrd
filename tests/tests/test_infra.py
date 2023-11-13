b17669ec61b8696cb3109edb817b38e2187fc211
Stéphane Henriot <shenriot.pro@gmail.com>
2023-11-13 16:40:43 +0100

    tests: use paging to avoid flaky test (infra)

--- tests/tests/test_infra.py
+++ tests/tests/test_infra.py
@@ -30,8 +30,13 @@ class _InfraResponse:
 
 @pytest.mark.usefixtures("tiny_infra")
 def test_get_infra():
-    response = requests.get(EDITOAST_URL + "infra/")
-    assert response.status_code == 200
-    body = response.json()
-    infra_response = _InfraResponse(**body)
-    assert "tiny_infra" in [_InfraDetails(**infra).name for infra in infra_response.results]
+    page = 1
+    names = []
+    while page is not None:
+        response = requests.get(EDITOAST_URL + "infra/", params={"page": page})
+        assert response.status_code == 200
+        body = response.json()
+        infra_response = _InfraResponse(**body)
+        names.extend(_InfraDetails(**infra).name for infra in infra_response.results)
+        page = body.get("next")
+    assert "tiny_infra" in names
