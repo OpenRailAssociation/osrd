46224abb0e13fbcc4c373a28c21b8ca01ec60c68
Stéphane Henriot <shenriot.pro@gmail.com>
2023-11-13 17:13:03 +0100

    tests: improve runtime type checking (flake8 error)

--- tests/tests/utils/approximations.py
+++ tests/tests/utils/approximations.py
@@ -8,11 +8,11 @@ def recursive_approx(expected: Any, actual: Any, rel=1e-3):
     Calls `pytest.approx` recursively on elements of list and keys of dicts.
     Dict keys are not approximated, it's assumed that keys are strings.
     """
-    if type(expected) == list:
+    if isinstance(expected, list):
         for a, b in zip(expected, actual):
             recursive_approx(a, b)
         return
-    if type(expected) == dict:
+    if isinstance(expected, dict):
         assert expected.keys() == actual.keys()
         for key in expected:
             recursive_approx(expected[key], actual[key])
