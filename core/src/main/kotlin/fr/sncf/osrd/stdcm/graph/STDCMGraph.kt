4b1aa5090b3b3851475ea9201533c806438ad12d
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2023-12-19 16:59:24 +0100

    core: stdcm: implement and use new interface for infra exploration InfraExplorer
    tests: add a new infra with overlapping routes
    
    core: stdcm: add tests for infra explorer
    
    core: implement PathPropertiesView
    
    core: implement new path exploration class InfraExplorer
    
    core: stdcm: implement and use new interface for infra exploration InfraExplorer

--- core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/STDCMGraph.kt
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/STDCMGraph.kt
@@ -72,13 +72,14 @@ class STDCMGraph(
     }
 
     override fun getAdjacentEdges(node: STDCMNode): Collection<STDCMEdge> {
-        return if (node.detector == null)
-            STDCMEdgeBuilder.fromNode(this, node, node.locationOnBlock!!.edge).makeAllEdges()
+        return if (node.locationOnEdge != null)
+            STDCMEdgeBuilder.fromNode(this, node, node.infraExplorer).makeAllEdges()
         else {
             val res = ArrayList<STDCMEdge>()
-            val neighbors = blockInfra.getBlocksStartingAtDetector(node.detector)
-            for (neighbor in neighbors) {
-                res.addAll(STDCMEdgeBuilder.fromNode(this, node, neighbor).makeAllEdges())
+            val extendedPaths = node.infraExplorer.cloneAndExtendLookahead()
+            for (newPath in extendedPaths) {
+                newPath.moveForward()
+                res.addAll(STDCMEdgeBuilder.fromNode(this, node, newPath).makeAllEdges())
             }
             res
         }
--- core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/STDCMNode.kt
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/STDCMNode.kt
@@ -1,25 +1,22 @@
 package fr.sncf.osrd.stdcm.graph
 
-import fr.sncf.osrd.graph.PathfindingEdgeLocationId
 import fr.sncf.osrd.sim_infra.api.Block
-import fr.sncf.osrd.sim_infra.api.DirDetectorId
+import fr.sncf.osrd.stdcm.infra_exploration.InfraExplorer
+import fr.sncf.osrd.utils.units.Offset
 
 data class STDCMNode(
     val time: Double, // Time at the transition of the edge
     val speed: Double, // Speed at the end of the previous edge
-    val detector:
-        DirDetectorId?, // Detector that separates the blocks, this is the physical location of the
-    // node
+    val infraExplorer: InfraExplorer, // Instance used to explore the infra
     val totalPrevAddedDelay:
         Double, // Sum of all the delays we have added by shifting the departure time
     val maximumAddedDelay:
         Double, // Maximum delay we can add by delaying the start time without causing conflicts
     val previousEdge: STDCMEdge, // Edge that lead to this node
     val waypointIndex: Int, // Index of the last waypoint passed by the train
-    val locationOnBlock:
-        PathfindingEdgeLocationId<
+    val locationOnEdge:
+        Offset<
             Block
-        >?, // Position on a block, if this node isn't on the transition between blocks
-    // (stop)
+        >?, // Position on a block, if this node isn't on the transition between blocks (stop)
     val stopDuration: Double? // When the node is a stop, how long the train remains here
 )
