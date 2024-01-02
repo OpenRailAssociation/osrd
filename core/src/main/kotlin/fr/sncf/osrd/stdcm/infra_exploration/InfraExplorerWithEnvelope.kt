f897a4edf2cf8995951daa3eaec8419d2c65198a
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2024-01-02 15:56:25 +0100

    core: add `InfraExplorerWithEnvelope`

---
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/infra_exploration/InfraExplorerWithEnvelope.kt
@@ -0,0 +1,39 @@
+package fr.sncf.osrd.stdcm.infra_exploration
+
+import fr.sncf.osrd.envelope.Envelope
+import fr.sncf.osrd.envelope.EnvelopeTimeInterpolate
+import fr.sncf.osrd.graph.PathfindingEdgeLocationId
+import fr.sncf.osrd.sim_infra.api.Block
+import fr.sncf.osrd.sim_infra.api.BlockInfra
+import fr.sncf.osrd.sim_infra.api.RawInfra
+
+/** Explore the infra while running simulations. Builds one global envelope and path from the start of the train.
+ * The path can extend further than the part that has been simulated
+ * (see InfraExplorer for a more detailed explanation). */
+interface InfraExplorerWithEnvelope : InfraExplorer {
+
+    /** Access the full envelope from the train start. */
+    fun getFullEnvelope(): EnvelopeTimeInterpolate
+
+    /** Returns the envelope spanning over the last block. */
+    fun getLastEnvelope(): Envelope
+
+    /** Adds an envelope. */
+    fun addEnvelope(envelope: Envelope)
+
+    /** Only shallow copies are made.
+     * Used to enable backtracking by cloning explorers at each step. */
+    override fun clone(): InfraExplorerWithEnvelope
+
+    override fun cloneAndExtendLookahead(): Collection<InfraExplorerWithEnvelope>
+}
+
+/** Init all InfraExplorersWithEnvelope starting at the given location. */
+fun initInfraExplorerWithEnvelope(
+    rawInfra: RawInfra,
+    blockInfra: BlockInfra,
+    location: PathfindingEdgeLocationId<Block>
+): Collection<InfraExplorerWithEnvelope> {
+    return initInfraExplorer(rawInfra, blockInfra, location)
+        .map { explorer -> InfraExplorerWithEnvelopeImpl(explorer, ArrayList()) }
+}
---
+++ core/src/test/kotlin/fr/sncf/osrd/stdcm/infra_exploration/InfraExplorerWithEnvelopeTests.kt
@@ -0,0 +1,181 @@
+package fr.sncf.osrd.stdcm.infra_exploration
+
+import fr.sncf.osrd.envelope.Envelope
+import fr.sncf.osrd.envelope.EnvelopeTimeInterpolate
+import fr.sncf.osrd.envelope.part.EnvelopePart
+import fr.sncf.osrd.envelope_sim.EnvelopeProfile
+import fr.sncf.osrd.envelope_sim.TrainPhysicsIntegrator.arePositionsEqual
+import fr.sncf.osrd.graph.PathfindingEdgeLocationId
+import fr.sncf.osrd.utils.DummyInfra
+import fr.sncf.osrd.utils.units.Offset
+import fr.sncf.osrd.utils.units.meters
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class InfraExplorerWithEnvelopeTests {
+
+    @Test
+    fun testTwoDifferentPaths() {
+        /*
+                 c1
+                ^
+               /
+        a --> b
+               \
+                v
+                 c2
+         */
+        val infra = DummyInfra()
+        val blocks = listOf(
+            infra.addBlock("a", "b"),
+            infra.addBlock("b", "c1"),
+            infra.addBlock("c1", "d"),
+            infra.addBlock("b", "c2"),
+        )
+
+        // a --> b
+        val firstExplorers = initInfraExplorerWithEnvelope(infra, infra,
+            PathfindingEdgeLocationId(blocks[0], Offset(0.meters)))
+        assertEquals(1, firstExplorers.size)
+        val firstExplorer = firstExplorers.first()
+
+        // current block a->b, lookahead b->c1 and b->c2
+        val extended = firstExplorer.cloneAndExtendLookahead().toList()
+        assertEquals(2, extended.size)
+
+        // Move forward once, current block b->c1 and b->c2, lookahead c1->d->e and c2->d->e
+        extended[0].moveForward()
+        extended[0].addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(30.0, 30.0)
+            )
+        ))
+        extended[1].moveForward()
+        extended[1].addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(20.0, 20.0)
+            )
+        ))
+        extended[0].addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(30.0, 30.0)
+            )
+        ))
+        extended[1].addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(20.0, 20.0)
+            )
+        ))
+
+        testEnvelopeTimeEquality(
+            Envelope.make(EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 200.0),
+                doubleArrayOf(30.0, 30.0)
+            )),
+            extended[0].getFullEnvelope()
+        )
+        testEnvelopeTimeEquality(
+            Envelope.make(EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 200.0),
+                doubleArrayOf(20.0, 20.0)
+            )),
+            extended[1].getFullEnvelope()
+        )
+    }
+
+    @Test
+    fun testClone() {
+        /*
+        a --> b --> c
+         */
+        val infra = DummyInfra()
+        val blocks = listOf(
+            infra.addBlock("a", "b"),
+            infra.addBlock("b", "c"),
+        )
+
+        // a --> b
+        val firstExplorers = initInfraExplorerWithEnvelope(infra, infra,
+            PathfindingEdgeLocationId(blocks[0], Offset(0.meters)))
+        assertEquals(1, firstExplorers.size)
+        var explorer = firstExplorers.first()
+
+        // current block a->b, lookahead b->c
+        val extended = explorer.cloneAndExtendLookahead().toList()
+        assertEquals(1, extended.size)
+        explorer = extended.first()
+
+        // Move forward
+        explorer.moveForward()
+        explorer.addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(30.0, 30.0)
+            )
+        ))
+
+        val cloned = explorer.clone()
+
+        // Add different envelopes to each version
+        explorer.addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(30.0, 30.0)
+            )
+        ))
+        cloned.addEnvelope(Envelope.make(
+            EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0),
+                doubleArrayOf(42.0, 42.0)
+            )
+        ))
+
+
+        testEnvelopeTimeEquality(
+            Envelope.make(EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 200.0),
+                doubleArrayOf(30.0, 30.0)
+            )),
+            explorer.getFullEnvelope()
+        )
+        testEnvelopeTimeEquality(
+            Envelope.make(EnvelopePart.generateTimes(
+                listOf(EnvelopeProfile.CONSTANT_SPEED),
+                doubleArrayOf(0.0, 100.0, 100.1, 200.0),
+                doubleArrayOf(30.0, 30.0, 42.00, 42.0)
+            )),
+            cloned.getFullEnvelope()
+        )
+    }
+
+    /** Test that the envelope bounds are equal,
+     * then iterate by an arbitrary step length to test for interpolateTotalTime equality. */
+    private fun testEnvelopeTimeEquality(expected: Envelope, actual: EnvelopeTimeInterpolate) {
+        assertTrue { arePositionsEqual(expected.beginPos, actual.beginPos) }
+        assertTrue { arePositionsEqual(expected.endPos, actual.endPos) }
+        var position = expected.beginPos
+        while (position < expected.endPos) {
+            assertEquals(
+                expected.interpolateTotalTime(position),
+                actual.interpolateTotalTime(position),
+                1e-5
+            )
+            position += 1.0
+        }
+    }
+}
