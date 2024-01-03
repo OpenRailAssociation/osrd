7f39d0e37dd3c7b9e16e16272afacd508c6da2d1
Élysæ <101974839+ElysaSrc@users.noreply.github.com>
2024-01-03 10:44:04 +0100

    front: use browser relative path instead of env domain

--- front/src/vite-env.d.ts
+++ front/src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  OSRD_BACKEND_URL: string;
   OSRD_SENTRY_DSN: string;
   OSRD_SENTRY_ENVIRONMENT: string;
   OSRD_GIT_DESCRIBE: string;
