a6b59f607f52ee0d27ba8b95caef19c49ef5a4a3
Benoit Simard <contact@bsimard.com>
2024-01-17 10:40:05 +0100

    editoast: managing i18n for API errors
    - editoast generates API errors definition into openapi.yaml by looking
      at all object that derives from 'EditoastError'
    - frontend task "script:i18n-api-errors" reads the openapi.yml and i18n
      errors.json file to check if there are some missing or invalid values
    - frontend task is run on the CI, within the check of the openapi

--- front/src/utils/error.ts
+++ front/src/utils/error.ts
@@ -1,8 +1,19 @@
 import { SerializedError } from '@reduxjs/toolkit';
+
 import { ApiError } from 'common/api/baseGeneratedApis';
+import i18n from 'i18n';
 
 // eslint-disable-next-line import/prefer-default-export
-export const extractMessageFromError = (error: ApiError | SerializedError) =>
-  `${(error as ApiError)?.data?.message || (error as SerializedError)?.message}`;
+export const extractMessageFromError = (error: ApiError | SerializedError | Error): string => {
+  let message = i18n.t('default', { ns: ['errors'] });
+  if ('data' in error) {
+    const { type, context } = error.data;
+    const i18nId = type.split(':').join('.');
+    message = i18n.t(i18nId, error.data.message, { context, ns: ['errors'] });
+  } else if (error.message) {
+    message = error.message;
+  }
+  return message;
+};
 
 export const extractStatusFromError = (error: ApiError) => (error as ApiError)?.status;
