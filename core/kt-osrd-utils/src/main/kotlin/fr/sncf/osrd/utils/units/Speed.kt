41ec3e8bc9f12ab33132fd54792651bb6d9db8a4
Pierre-Etienne Bougué <bougue.pe@proton.me>
2024-02-19 19:14:29 +0100

    core: load track-section part of kotlin infra directly from railjson
    Notes:
    * RawInfraBuilder interface looses some parts (specific to adapter).
      So RawInfra*Adapter is now tied to corresponding RawInfra*Builder.
    * Dropping use of DiagnosticRecorder.
    * Some stitching is done to connect "new" loading of track-section and
      "old" loading of the rest of the infra.

--- core/kt-osrd-utils/src/main/kotlin/fr/sncf/osrd/utils/units/Speed.kt
+++ core/kt-osrd-utils/src/main/kotlin/fr/sncf/osrd/utils/units/Speed.kt
@@ -29,6 +29,16 @@ value class Speed(val millimetersPerSecond: ULong) : Comparable<Speed> {
         fun toMetersPerSecond(speed: Speed): Double {
             return speed.metersPerSecond
         }
+
+        fun min(a: Speed, b: Speed) =
+            Speed(
+                millimetersPerSecond = a.millimetersPerSecond.coerceAtMost(b.millimetersPerSecond)
+            )
+
+        fun max(a: Speed, b: Speed) =
+            Speed(
+                millimetersPerSecond = a.millimetersPerSecond.coerceAtLeast(b.millimetersPerSecond)
+            )
     }
 }
 
