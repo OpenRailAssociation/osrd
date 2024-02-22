80b7a8edc9733ecf696199eb11fe8bed2ee5d51f
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2024-02-22 10:27:44 +0100

    core: stdcm: handle grid margins

--- core/src/main/kotlin/fr/sncf/osrd/stdcm/preprocessing/implementation/BlockAvailability.kt
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/preprocessing/implementation/BlockAvailability.kt
@@ -2,6 +2,8 @@ package fr.sncf.osrd.stdcm.preprocessing.implementation
 
 import fr.sncf.osrd.api.FullInfra
 import fr.sncf.osrd.conflicts.IncrementalConflictDetector
+import fr.sncf.osrd.conflicts.TrainRequirements
+import fr.sncf.osrd.conflicts.incrementalConflictDetector
 import fr.sncf.osrd.envelope_utils.DoubleBinarySearch
 import fr.sncf.osrd.sim_infra.api.Path
 import fr.sncf.osrd.standalone_sim.result.ResultTrain.SpacingRequirement
@@ -9,12 +11,15 @@ import fr.sncf.osrd.stdcm.infra_exploration.InfraExplorerWithEnvelope
 import fr.sncf.osrd.stdcm.preprocessing.interfaces.BlockAvailabilityInterface
 import fr.sncf.osrd.utils.units.Distance
 import fr.sncf.osrd.utils.units.Offset
+import fr.sncf.osrd.utils.units.meters
 import kotlin.math.max
 import kotlin.math.min
 
 data class BlockAvailability(
     val fullInfra: FullInfra,
-    val incrementalConflictDetector: IncrementalConflictDetector
+    val incrementalConflictDetector: IncrementalConflictDetector,
+    val gridMarginBeforeTrain: Double,
+    val gridMarginAfterTrain: Double,
 ) : BlockAvailabilityInterface {
 
     override fun getAvailability(
@@ -26,6 +31,9 @@ data class BlockAvailability(
         val spacingRequirements = infraExplorer.getSpacingRequirements()
         val pathStartTime = startTime - infraExplorer.interpolateTimeClamp(startOffset)
         val endTime = infraExplorer.interpolateTimeClamp(endOffset) + pathStartTime
+
+        // Modify the spacing requirements to adjust for the start time,
+        // and filter out the ones that are outside the relevant time range
         val shiftedSpacingRequirements =
             spacingRequirements
                 .map {
@@ -70,11 +78,41 @@ data class BlockAvailability(
         explorer: InfraExplorerWithEnvelope,
         time: Double
     ): Offset<Path> {
+        if (time < 0.0) return Offset(0.meters)
         val envelope = explorer.getFullEnvelope()
+        if (time > envelope.totalTime) return explorer.getSimulatedLength()
         val search = DoubleBinarySearch(envelope.beginPos, envelope.endPos, time, 2.0, false)
         while (!search.complete()) search.feedback(envelope.interpolateTotalTimeClamp(search.input))
         return explorer
             .getIncrementalPath()
             .fromTravelledPath(Offset(Distance.fromMeters(search.result)))
     }
+}
+
+fun makeBlockAvailability(
+    infra: FullInfra,
+    requirements: List<SpacingRequirement>,
+    gridMarginBeforeTrain: Double = 0.0,
+    gridMarginAfterTrain: Double = 0.0,
+): BlockAvailabilityInterface {
+    var reqWithGridMargin = requirements
+    if (gridMarginAfterTrain != 0.0 || gridMarginBeforeTrain != 0.0) {
+        // The margin expected *after* the new train is added *before* the other train resource uses
+        reqWithGridMargin =
+            requirements.map {
+                SpacingRequirement(
+                    it.zone,
+                    it.beginTime - gridMarginAfterTrain,
+                    it.endTime + gridMarginBeforeTrain,
+                    it.isComplete
+                )
+            }
+    }
+    val trainRequirements = listOf(TrainRequirements(0L, reqWithGridMargin, listOf()))
+    return BlockAvailability(
+        infra,
+        incrementalConflictDetector(trainRequirements),
+        gridMarginBeforeTrain,
+        gridMarginAfterTrain,
+    )
 }
