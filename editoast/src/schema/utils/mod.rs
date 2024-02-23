a1dc6eb5e9361686948a4131290f466c31636824
Florian Amsallem <florian.amsallem@epita.fr>
2024-02-23 16:17:30 +0100

    editoast: add support serde support for chrono::duration
    Co-authored-by: Youness Chrifi Alaoui <youness.chrifi_alaoui@telecom-sudparis.eu>

--- editoast/src/schema/utils/mod.rs
+++ editoast/src/schema/utils/mod.rs
@@ -1,7 +1,9 @@
+pub mod duration;
 pub mod geometry;
 mod identifier;
 mod non_blank_string;
 
+pub use duration::Duration;
 pub use identifier::Identifier;
 pub use non_blank_string::NonBlankString;
 
