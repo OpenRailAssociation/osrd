ab04a785752c8f22e2e6907768d99ce465e6009a
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2024-03-06 11:14:34 +0100

    core: stdcm: ignore lookahead when caching sim results

--- core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/BlockSimulationParameters.kt
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/BlockSimulationParameters.kt
@@ -1,11 +1,15 @@
 package fr.sncf.osrd.stdcm.graph
 
 import fr.sncf.osrd.sim_infra.api.Block
-import fr.sncf.osrd.stdcm.infra_exploration.InfraExplorer
+import fr.sncf.osrd.sim_infra.api.BlockId
 import fr.sncf.osrd.utils.units.Offset
 
+/**
+ * This class is only used for caching simulations results, other parameters (that shouldn't be
+ * considered to evaluate if previous results can be used) can be added on the side
+ */
 data class BlockSimulationParameters(
-    val infraExplorer: InfraExplorer,
+    val block: BlockId,
     val initialSpeed: Double,
     val start: Offset<Block>,
     val stop: Offset<Block>?
