75bf2e9bba875f7aef451210959b3c996d219d5b
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2024-03-13 15:57:31 +0100

    core: spacing-resources: break off when path offset = sight offset
    When both are equal we enter into the "epsilon error"
    ranges of float operations in `callbacks.arrivalTimeInRange`

--- core/src/main/java/fr/sncf/osrd/conflicts/SpacingResourceGenerator.kt
+++ core/src/main/java/fr/sncf/osrd/conflicts/SpacingResourceGenerator.kt
@@ -278,7 +278,7 @@ class SpacingRequirementAutomaton(
             val signalOffset = incrementalPath.toTravelledPath(pathSignal.pathOffset)
             val sightOffset = signalOffset - rawInfra.getSignalSightDistance(physicalSignal)
             // If the train's simulation has reached the point where the signal is seen, bail out
-            if (callbacks.currentPathOffset < sightOffset) {
+            if (callbacks.currentPathOffset <= sightOffset) {
                 break
             }
             val sightTime = callbacks.arrivalTimeInRange(sightOffset, signalOffset)
