40f2bcef9c100505af20d03217a692357f32c261
Younes Khoudli <younes.khoudli@epita.fr>
2024-03-14 16:42:59 +0100

    scripts: fix sync-openapi.sh
    We don't tag images with osrd/editoast since a few month ago. We should
    probably just run with the local version anyways

--- scripts/sync-openapi.sh
+++ scripts/sync-openapi.sh
@@ -5,7 +5,7 @@ set -e
 root_path="$(realpath "$(dirname "$0")"/..)"
 
 echo "Generating editoast's openapi"
-docker run osrd/editoast editoast openapi > "${root_path}"/editoast/openapi.yaml
+( cd "${root_path}/editoast" && cargo run openapi > "${root_path}"/editoast/openapi.yaml )
 
 echo "Generating the typescript client"
 yarn --cwd "${root_path}"/front generate-types
