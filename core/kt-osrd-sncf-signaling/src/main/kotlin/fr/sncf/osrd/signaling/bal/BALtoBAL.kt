e11a6f69ee9b7937153e499b1b8d53ee1b58ad24
Younes Khoudli <younes.khoudli@epita.fr>
2024-03-20 07:09:00 +0100

    core: signals track distances to stop

--- core/kt-osrd-sncf-signaling/src/main/kotlin/fr/sncf/osrd/signaling/bal/BALtoBAL.kt
+++ core/kt-osrd-sncf-signaling/src/main/kotlin/fr/sncf/osrd/signaling/bal/BALtoBAL.kt
@@ -8,13 +8,16 @@ import fr.sncf.osrd.sim_infra.api.SigParameters
 import fr.sncf.osrd.sim_infra.api.SigSettings
 import fr.sncf.osrd.sim_infra.api.SigState
 import fr.sncf.osrd.sim_infra.api.SigStateSchema
+import fr.sncf.osrd.utils.units.Distance
+import fr.sncf.osrd.utils.units.meters
+import fr.sncf.osrd.utils.units.millimeters
 
 object BALtoBAL : SignalDriver {
     override val name = "BAL-BAL"
     override val inputSignalingSystem = "BAL"
     override val outputSignalingSystem = "BAL"
 
-    private fun cascadePrimaryAspect(aspect: String, parameters: SigParameters): String {
+    private fun cascadePrimaryAspect(aspect: String, parameters: SigParameters, nextDistanceToStop: Distance): String {
         return when (aspect) {
             "VL" -> "VL"
             "A" ->
@@ -24,7 +27,7 @@ object BALtoBAL : SignalDriver {
                     "VL"
                 }
             "(A)" ->
-                if (parameters.getFlag("jaune_cli")) {
+                if (parameters.getFlag("jaune_cli") && nextDistanceToStop > 0.meters && nextDistanceToStop < 500.meters) {
                     "(A)"
                 } else {
                     "VL"
@@ -49,15 +52,29 @@ object BALtoBAL : SignalDriver {
                     if (!signal.getFlag("Nf")) // This can happen when doing partial simulation.
                      value("aspect", "S") // We take the most restrictive available aspect
                     else value("aspect", "C")
+                    value("distance_to_stop", 0)
                 }
-                OCCUPIED -> value("aspect", "S")
+                OCCUPIED -> {
+                    value("aspect", "S")
+                    value("distance_to_stop", 0)
+                }
                 CLEAR -> {
                     if (!maView.hasNextSignal) {
                         value("aspect", "A")
+                        // TODO: maybe we should include the distance to the bufferstop in the MAView as the next signal distance?
+                        value("distance_to_stop", -1)
                     } else {
                         val nextSignalState = maView.nextSignalState
                         val nextAspect = nextSignalState.getEnum("aspect")
-                        value("aspect", cascadePrimaryAspect(nextAspect, parameters))
+                        val nextDistanceToStop = nextSignalState.getInt("distance_to_stop").millimeters
+                        value("aspect", cascadePrimaryAspect(nextAspect, parameters, nextDistanceToStop))
+                        if (nextDistanceToStop == (-1).millimeters)
+                            value("distance_to_stop", -1)
+                        else {
+                            val distanceToNextSignal = maView.distanceToNextSignal!!
+                            // FIXME: really, we should be able to encode longs, but this should work to demonstrate the concept
+                            value("distance_to_stop", (distanceToNextSignal + nextDistanceToStop).millimeters.toInt() )
+                        }
                     }
                 }
             }
