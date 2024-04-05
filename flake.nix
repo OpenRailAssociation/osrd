94088fe112c92136936c4e39be6c830f140286d2
Florian Amsallem <florian.amsallem@epita.fr>
2024-04-05 11:11:38 +0200

    nix: add taplo tool

--- flake.nix
+++ flake.nix
@@ -102,6 +102,7 @@
                 openssl
                 pkg-config
                 postgresql
+                taplo
 
                 # API
                 (python311.withPackages pythonPackages)
