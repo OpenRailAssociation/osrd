54f8d0e66f59812c61950a0d1a3abd509badaf50
hamz2a <atrari.hamza@gmail.com>
2024-04-11 15:35:49 +0200

    editoast: move track_offset to infra editoast_schemas

---
+++ editoast/editoast_schemas/src/infra.rs
@@ -0,0 +1,7 @@
+mod track_offset;
+
+pub use track_offset::TrackOffset;
+
+editoast_common::schemas! {
+    track_offset::schemas(),
+}
--- editoast/editoast_schemas/src/lib.rs
+++ editoast/editoast_schemas/src/lib.rs
@@ -1,11 +1,11 @@
+pub mod infra;
 pub mod primitives;
 pub mod rolling_stock;
-pub mod track_offset;
 pub mod train_schedule;
 
 editoast_common::schemas! {
     rolling_stock::schemas(),
     train_schedule::schemas(),
-    track_offset::schemas(),
     primitives::schemas(),
+    infra::schemas(),
 }
