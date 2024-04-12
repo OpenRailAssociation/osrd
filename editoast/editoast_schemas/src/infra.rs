721e99139275f001864787a5581dcdcfe507d976
hamz2a <atrari.hamza@gmail.com>
2024-04-12 11:56:58 +0200

    editoast: move directional_track_range to editoast_schemas

--- editoast/editoast_schemas/src/infra.rs
+++ editoast/editoast_schemas/src/infra.rs
@@ -1,11 +1,13 @@
 mod direction;
 mod track_location;
+mod directional_track_range;
 mod track_offset;
 mod track_range;
 mod waypoint;
 
 pub use direction::Direction;
 pub use track_location::TrackLocation;
+pub use directional_track_range::DirectionalTrackRange;
 pub use track_offset::TrackOffset;
 pub use track_range::TrackRange;
 pub use waypoint::Waypoint;
@@ -14,4 +16,5 @@ editoast_common::schemas! {
     track_offset::schemas(),
     direction::schemas(),
     track_location::schemas(),
+    directional_track_range::schemas(),
 }
