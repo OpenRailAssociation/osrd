72112904065e36abbdfe5d51c4477b383a4da110
hamz2a <atrari.hamza@gmail.com>
2024-04-17 14:27:30 +0200

    editoast: move Route cache to InfraCache

--- editoast/src/infra_cache/mod.rs
+++ editoast/src/infra_cache/mod.rs
@@ -20,12 +20,17 @@ use diesel_async::AsyncPgConnection as PgConnection;
 use diesel_async::RunQueryDsl;
 use editoast_derive::EditoastError;
 use editoast_schemas::infra::Crossing;
+use editoast_schemas::infra::Direction;
+use editoast_schemas::infra::DirectionalTrackRange;
 use editoast_schemas::infra::DoubleSlipSwitch;
 use editoast_schemas::infra::Electrification;
+use editoast_schemas::infra::Endpoint;
 use editoast_schemas::infra::Link;
 use editoast_schemas::infra::PointSwitch;
 use editoast_schemas::infra::SingleSlipSwitch;
 use editoast_schemas::infra::SwitchType;
+use editoast_schemas::infra::TrackEndpoint;
+use editoast_schemas::infra::Waypoint;
 use editoast_schemas::primitives::OSRDIdentified;
 use editoast_schemas::primitives::OSRDObject;
 use editoast_schemas::primitives::ObjectRef;
@@ -713,6 +718,97 @@ impl InfraCache {
                 obj_id: electrification_id.to_string(),
             })?
             .unwrap_electrification())
+    }
+
+    /// Return the track and position of a waypoint
+    fn get_waypoint_location(&self, waypoint: &Waypoint) -> Option<(&String, f64)> {
+        if waypoint.is_detector() {
+            let detector = self.detectors().get(waypoint.get_id())?;
+            let detector = detector.unwrap_detector();
+            Some((&detector.track, detector.position))
+        } else {
+            let bs = self.buffer_stops().get(waypoint.get_id())?;
+            let bs = bs.unwrap_buffer_stop();
+            Some((&bs.track, bs.position))
+        }
+    }
+
+    /// Compute the track ranges through which the route passes.
+    /// If the path cannot be computed (e.g. invalid topology), returns None.
+    pub fn compute_track_ranges_on_route(&self, route: &Route, graph: &Graph) -> Option<RoutePath> {
+        // Check if entry and exit points are the same
+        if route.entry_point == route.exit_point {
+            return None;
+        }
+
+        let mut cur_dir = route.entry_point_direction;
+        let (cur_track, mut cur_offset) = self.get_waypoint_location(&route.entry_point)?;
+        let (exit_track, exit_offset) = self.get_waypoint_location(&route.exit_point)?;
+
+        // Check that the track exists
+        let mut cur_track = self.track_sections().get(cur_track)?.unwrap_track_section();
+
+        // Save track ranges and used switches
+        let mut track_ranges = vec![];
+        let mut used_switches = HashMap::new();
+
+        // Check path validity
+        loop {
+            let cur_track_id = cur_track.get_id();
+
+            // Add track range
+            let end_offset = if cur_track_id == exit_track {
+                exit_offset
+            } else if cur_dir == Direction::StartToStop {
+                cur_track.length
+            } else {
+                0.
+            };
+            track_ranges.push(DirectionalTrackRange::new(
+                cur_track_id.clone(),
+                cur_offset.min(end_offset),
+                cur_offset.max(end_offset),
+                cur_dir,
+            ));
+
+            // Search for the exit_point
+            if cur_track_id == exit_track {
+                if (cur_dir == Direction::StartToStop && cur_offset > exit_offset)
+                    || (cur_dir == Direction::StopToStart && cur_offset < exit_offset)
+                {
+                    return None;
+                }
+                break;
+            }
+
+            // Search for the next track section
+            let endpoint = TrackEndpoint::from_track_and_direction(cur_track_id, cur_dir);
+            // No neighbour found
+            if !graph.has_neighbour(&endpoint) {
+                return None;
+            }
+
+            let switch = graph.get_switch(&endpoint)?;
+            let switch_id = switch.get_id();
+            // Check we found the switch in the route
+            let group = route.switches_directions.get(&switch_id.clone().into())?;
+            used_switches.insert(switch_id.clone().into(), group.clone());
+            let next_endpoint = graph.get_neighbour(&endpoint, group)?;
+
+            // Update current track section, offset and direction
+            cur_track = self
+                .track_sections()
+                .get(&next_endpoint.track.0)?
+                .unwrap_track_section();
+            (cur_dir, cur_offset) = match next_endpoint.endpoint {
+                Endpoint::Begin => (Direction::StartToStop, 0.),
+                Endpoint::End => (Direction::StopToStart, cur_track.length),
+            };
+        }
+        Some(RoutePath {
+            track_ranges,
+            switches_directions: used_switches,
+        })
     }
 }
 
