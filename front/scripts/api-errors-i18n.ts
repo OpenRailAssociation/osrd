086800d1f9d1511a17c151b7cb513cfafbe8899c
Benoit Simard <contact@bsimard.com>
2024-04-18 12:20:06 +0200

    front: fix api i18n checker for message variable with object
    When the script found an error definition in openapi.yml file, it tries to generate its message with a dummy variable object.
    And this object can't be well defined, specially for objects.
    This leads to an error for the script, even if it's valid.

--- front/scripts/api-errors-i18n.ts
+++ front/scripts/api-errors-i18n.ts
@@ -12,11 +12,14 @@ const i18n_error_path = '../public/locales/fr/errors.json';
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function checkI18N(error: any): Promise<string[]> {
-  const result: string[] = [];
+  const i18nErrors: string[] = [];
 
   // Error data for i18n
   const errorName = error.properties.type.enum[0];
   const errorId = errorName.split(':').join('.');
+  // This variable is used for testing the generation of the error message.
+  // It's a dummy (and dumb) error variables, it just generate an object with empty string
+  // perhaps we can find a better way to generate it, specially this the property type
   const errorVarExample = Object.keys(error.properties.context.properties || {}).reduce(
     (acc, curr) => {
       acc[curr] = '';
@@ -36,12 +39,19 @@ async function checkI18N(error: any): Promise<string[]> {
         },
       },
       parseMissingKeyHandler: () => {
-        result.push(
+        i18nErrors.push(
           `Error "${errorName}" has missing i18n message. Please add the key "${errorId}" to the file "${i18n_error_path}"`
         );
       },
       missingInterpolationHandler: (_text, value) => {
-        result.push(`Message for error "${errorName}" is using an unknown variable ${value[0]}`);
+        const varName = `${value[0]}`;
+        // Due to the dummy variable (see comment on `errorVarExample`), if we use a key of a property object
+        // the test will fail here. That's why we check here if the var  name used in the message contains a "."
+        if (!varName.includes('.')) {
+          i18nErrors.push(
+            `Message for error "${errorName}" is using an unknown variable ${value[0]}`
+          );
+        }
       },
     },
     noop
@@ -50,7 +60,7 @@ async function checkI18N(error: any): Promise<string[]> {
   // Generate the error message
   i18n.t(errorId, errorVarExample);
 
-  return result;
+  return i18nErrors;
 }
 
 async function run() {
