b6ada4593d235caf1e6b2286a04330c03a4c46bc
Leo Valais <leo.valais97@gmail.com>
2024-04-18 14:10:59 +0200

    editoast: add ToToken ModelV2 definition DeleteStaticImpl

---
+++ editoast/editoast_derive/src/modelv2/codegen/delete_static_impl.rs
@@ -0,0 +1,45 @@
+use quote::quote;
+use quote::ToTokens;
+
+use crate::modelv2::identifier::TypedIdentifier;
+
+pub(crate) struct DeleteStaticImpl {
+    pub(super) model: syn::Ident,
+    pub(super) table_name: syn::Ident,
+    pub(super) table_mod: syn::Path,
+    pub(super) identifier: TypedIdentifier,
+}
+
+impl ToTokens for DeleteStaticImpl {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let Self {
+            model,
+            table_name,
+            table_mod,
+            identifier,
+        } = self;
+        let ty = identifier.get_type();
+        let id_ident = identifier.get_lvalue();
+        let eqs = identifier.get_diesel_eqs();
+
+        tokens.extend(quote! {
+            #[automatically_derived]
+            #[async_trait::async_trait]
+            impl crate::modelsv2::DeleteStatic<#ty> for #model {
+                async fn delete_static(
+                    conn: &mut diesel_async::AsyncPgConnection,
+                    #id_ident: #ty,
+                ) -> crate::error::Result<bool> {
+                    use diesel::prelude::*;
+                    use diesel_async::RunQueryDsl;
+                    use #table_mod::dsl;
+                    diesel::delete(dsl::#table_name.#(filter(#eqs)).*)
+                        .execute(conn)
+                        .await
+                        .map(|n| n == 1)
+                        .map_err(Into::into)
+                }
+            }
+        });
+    }
+}
