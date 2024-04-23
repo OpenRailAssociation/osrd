7c1982abfb7d43b4584b4efbf24a613c7317b73a
Victor "multun" Collod <victor.collod@epita.fr>
2024-04-23 14:16:36 +0200

    core: remove dead code / obsolete docs

--- core/src/main/java/fr/sncf/osrd/utils/DeepComparable.java
+++
@@ -1,5 +0,0 @@
-package fr.sncf.osrd.utils;
-
-public interface DeepComparable<T> {
-    boolean deepEquals(T other);
-}
--- core/src/main/java/fr/sncf/osrd/utils/attrs/Attr.java
+++
@@ -1,9 +0,0 @@
-package fr.sncf.osrd.utils.attrs;
-
-/**
- * Instantiate this class to declare an attribute type. Attributes must be static final fields of
- * classes.
- *
- * @param <T> the type of the attribute
- */
-public class Attr<T> {}
