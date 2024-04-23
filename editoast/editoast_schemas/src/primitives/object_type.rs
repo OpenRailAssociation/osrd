b1cacc10b637100c096f28b3291fa2c2228f7c1a
hamz2a <atrari.hamza@gmail.com>
2024-04-23 10:40:14 +0200

    editoast: create object_type.rs in primitives folder

---
+++ editoast/editoast_schemas/src/primitives/object_type.rs
@@ -0,0 +1,39 @@
+use enum_map::Enum;
+use serde::Deserialize;
+use serde::Serialize;
+use strum::Display;
+use strum::EnumIter;
+use utoipa::ToSchema;
+
+editoast_common::schemas! {
+    ObjectType,
+}
+
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    Deserialize,
+    Hash,
+    Eq,
+    PartialEq,
+    Serialize,
+    Enum,
+    EnumIter,
+    Display,
+    ToSchema,
+)]
+#[serde(deny_unknown_fields)]
+pub enum ObjectType {
+    TrackSection,
+    Signal,
+    SpeedSection,
+    Detector,
+    NeutralSection,
+    Switch,
+    SwitchType,
+    BufferStop,
+    Route,
+    OperationalPoint,
+    Electrification,
+}
