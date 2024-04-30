00468322e2807c1a518af0b0ec2c23f80c7ce1d2
romainvalls <romain.valls95@gmail.com>
2024-04-30 14:15:27 +0200

    front: adapt import train schedule v2 payload

--- front/src/modules/trainschedule/components/ImportTrainSchedule/ImportTrainScheduleTrainsListV2.tsx
+++ front/src/modules/trainschedule/components/ImportTrainSchedule/ImportTrainScheduleTrainsListV2.tsx
@@ -11,8 +11,8 @@ import { Loader } from 'common/Loaders';
 // eslint-disable-next-line import/no-cycle
 import { ImportTrainScheduleTrainDetail } from 'modules/trainschedule/components/ImportTrainSchedule';
 import rollingstockOpenData2OSRD from 'modules/trainschedule/components/ImportTrainSchedule/rollingstock_opendata2osrd.json';
-import { setFailure, setSuccess } from 'reducers/main';
-import { useAppDispatch } from 'store';
+// import { setFailure, setSuccess } from 'reducers/main';
+// import { useAppDispatch } from 'store';
 
 import { generateV2TrainSchedulesPayloads } from './generateTrainSchedulesPayloads';
 
@@ -46,49 +46,41 @@ const ImportTrainScheduleTrainsListV2 = ({
 
   const { refetch: refetchTimetable } =
     osrdEditoastApi.endpoints.getV2TimetableById.useQuerySubscription({ id: timetableId });
-  const [postTrainSchedule] = osrdEditoastApi.endpoints.postV2TrainSchedule.useMutation();
+  const [postTrainSchedule] =
+    osrdEditoastApi.endpoints.postV2TimetableByIdTrainSchedule.useMutation();
 
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
   async function generateV2TrainSchedules() {
-    const payloads = generateV2TrainSchedulesPayloads(trainsList, timetableId);
+    const payloads = generateV2TrainSchedulesPayloads(trainsList);
 
-    const trainsCount = payloads.length;
-    let successfulTrainsCount = 0;
-    let errorsNb = 0;
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const payload of payloads) {
-      try {
-        await postTrainSchedule({ body: [payload] }).unwrap();
-        successfulTrainsCount += 1;
-      } catch (error) {
-        errorsNb += 1;
-      }
-    }
-    if (errorsNb > 0) {
-      dispatch(
-        setFailure({
-          name: t('failure'),
-          message: t('status.calculatingTrainScheduleCompleteFailure', {
-            trainsCount,
-            errorsNb,
-            count: trainsCount - errorsNb,
-          }),
-        })
-      );
-      refetchTimetable();
-    } else {
-      dispatch(
-        setSuccess({
-          title: t('success'),
-          text: t('status.calculatingTrainScheduleCompleteAllSuccess', {
-            successfulTrainsCount,
-            count: successfulTrainsCount,
-          }),
-        })
-      );
-    }
+    await postTrainSchedule({ id: timetableId, body: payloads }).unwrap();
+    refetchTimetable();
   }
+  //   if (errorsNb > 0) {
+  //     dispatch(
+  //       setFailure({
+  //         name: t('failure'),
+  //         message: t('status.calculatingTrainScheduleCompleteFailure', {
+  //           trainsCount,
+  //           errorsNb,
+  //           count: trainsCount - errorsNb,
+  //         }),
+  //       })
+  //     );
+  //     refetchTimetable();
+  //   } else {
+  //     dispatch(
+  //       setSuccess({
+  //         title: t('success'),
+  //         text: t('status.calculatingTrainScheduleCompleteAllSuccess', {
+  //           successfulTrainsCount,
+  //           count: successfulTrainsCount,
+  //         }),
+  //       })
+  //     );
+  //   }
+  // }
 
   return trainsList.length > 0 ? (
     <div className="container-fluid mb-2">
--- front/src/modules/trainschedule/components/ImportTrainSchedule/generateTrainSchedulesPayloads.ts
+++ front/src/modules/trainschedule/components/ImportTrainSchedule/generateTrainSchedulesPayloads.ts
@@ -63,10 +63,7 @@ export default function generateTrainSchedulesPayloads(
   return Object.values(trainSchedulesByPathID);
 }
 
-export function generateV2TrainSchedulesPayloads(
-  trains: TrainScheduleV2[],
-  timetableID: number
-): TrainScheduleForm[] {
+export function generateV2TrainSchedulesPayloads(trains: TrainScheduleV2[]): TrainScheduleBase[] {
   return trains.map((train) => {
     const { path, schedule } = train.steps.reduce(
       (acc, step, index) => {
@@ -93,7 +90,6 @@ export function generateV2TrainSchedulesPayloads(
       }
     );
     return {
-      timetable_id: timetableID,
       path,
       train_name: train.trainNumber,
       rolling_stock_name: train.rollingStock || '',
