80b67ae950f1f919c6ad8274316a164fa7f2c3d5
Egor <egor@berezify.fr>
2024-05-27 17:47:42 +0200

    editoast: refactor study tests

--- editoast/src/modelsv2/fixtures.rs
+++ editoast/src/modelsv2/fixtures.rs
@@ -3,6 +3,7 @@ use chrono::Utc;
 use crate::modelsv2::prelude::*;
 use crate::modelsv2::DbConnection;
 use crate::modelsv2::Project;
+use crate::modelsv2::Study;
 use crate::modelsv2::Tags;
 
 pub fn project_changeset(name: &str) -> Changeset<Project> {
@@ -19,4 +20,23 @@ pub async fn create_project(conn: &mut DbConnection, name: &str) -> Project {
         .create(conn)
         .await
         .expect("Failed to create project")
+}
+
+pub fn study_changeset(name: &str, project_id: i64) -> Changeset<Study> {
+    Study::changeset()
+        .name(name.to_owned())
+        .creation_date(Utc::now().naive_utc())
+        .creation_date(Utc::now().naive_utc())
+        .last_modification(Utc::now().naive_utc())
+        .budget(Some(0))
+        .tags(Tags::default())
+        .state("some_state".into())
+        .project_id(project_id)
+}
+
+pub async fn create_study(conn: &mut DbConnection, name: &str, project_id: i64) -> Study {
+    study_changeset(name, project_id)
+        .create(conn)
+        .await
+        .expect("Failed to create study")
 }
