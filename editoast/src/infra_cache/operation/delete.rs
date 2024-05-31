edbb3717c4d76180587fa803a94e579f0b4f419a
hamz2a <atrari.hamza@gmail.com>
2024-05-31 11:58:03 +0200

    editoast: add test_delete_object! and adapt tests

--- editoast/src/infra_cache/operation/delete.rs
+++ editoast/src/infra_cache/operation/delete.rs
@@ -62,25 +62,18 @@ impl From<ObjectRef> for DeleteOperation {
 
 #[cfg(test)]
 mod tests {
-    use diesel::sql_query;
     use diesel::sql_types::BigInt;
-    use diesel_async::RunQueryDsl;
-    use pretty_assertions::assert_eq;
-    use rstest::rstest;
-    use std::ops::DerefMut;
 
-    use crate::infra_cache::operation::delete::DeleteOperation;
-    use crate::modelsv2::fixtures::create_buffer_stop;
-    use crate::modelsv2::fixtures::create_detector;
-    use crate::modelsv2::fixtures::create_electrification;
-    use crate::modelsv2::fixtures::create_empty_infra;
-    use crate::modelsv2::fixtures::create_op;
-    use crate::modelsv2::fixtures::create_route;
-    use crate::modelsv2::fixtures::create_signal;
-    use crate::modelsv2::fixtures::create_speed;
-    use crate::modelsv2::fixtures::create_switch;
-    use crate::modelsv2::fixtures::create_track;
-    use crate::modelsv2::DbConnectionPoolV2;
+    use editoast_schemas::infra::BufferStop;
+    use editoast_schemas::infra::Detector;
+    use editoast_schemas::infra::Electrification;
+    use editoast_schemas::infra::NeutralSection;
+    use editoast_schemas::infra::OperationalPoint;
+    use editoast_schemas::infra::Route;
+    use editoast_schemas::infra::Signal;
+    use editoast_schemas::infra::SpeedSection;
+    use editoast_schemas::infra::Switch;
+    use editoast_schemas::infra::TrackSection;
     use editoast_schemas::primitives::OSRDIdentified;
     use editoast_schemas::primitives::OSRDObject;
 
@@ -90,217 +83,49 @@ mod tests {
         nb: i64,
     }
 
-    #[rstest]
-    async fn delete_track() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
+    macro_rules! test_delete_object {
+        ($obj:ident) => {
+            paste::paste! {
+                #[rstest::rstest]
+                async fn [<test_delete_$obj:snake>]() {
+                    use diesel_async::RunQueryDsl;
+                    use std::ops::DerefMut;
 
-        let track = create_track(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
+                    let db_pool = crate::modelsv2::DbConnectionPoolV2::for_tests();
+                    let infra = crate::modelsv2::fixtures::create_empty_infra(db_pool.get_ok().deref_mut()).await;
 
-        let track_deletion: DeleteOperation = track.get_ref().into();
+                    let railjson_object = crate::infra_cache::operation::create::RailjsonObject::$obj {
+                        railjson: $obj::default(),
+                    };
+                    let result = crate::infra_cache::operation::create::apply_create_operation(&railjson_object, infra.id, db_pool.get_ok().deref_mut()).await;
+                    assert!(result.is_ok(), "Failed to create a {}", stringify!($obj));
 
-        assert!(track_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
+                    let object_deletion: crate::infra_cache::operation::delete::DeleteOperation = railjson_object.get_ref().into();
+                    let result = object_deletion.apply(infra.id, db_pool.get_ok().deref_mut()).await;
+                    assert!(result.is_ok(), "Failed to delete a {}", stringify!($obj));
 
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_track_section WHERE obj_id = '{}' AND infra_id = {}",
-                track.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
+                    let res_del = diesel::sql_query(format!(
+                            "SELECT COUNT (*) AS nb FROM infra_object_{} WHERE obj_id = '{}' AND infra_id = {}",
+                            stringify!([<$obj:snake>]),
+                            railjson_object.get_id(),
+                            infra.id
+                        ))
+                        .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
 
-        assert_eq!(res_del.nb, 0);
+                    pretty_assertions::assert_eq!(res_del.nb, 0);
+                }
+            }
+        };
     }
 
-    #[rstest]
-    async fn delete_signal() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-
-        let signal =
-            create_signal(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-
-        let signal_deletion: DeleteOperation = signal.get_ref().into();
-
-        assert!(signal_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-            "SELECT COUNT (*) AS nb FROM infra_object_signal WHERE obj_id = '{}' AND infra_id = {}",
-            signal.get_id(),
-            infra.id
-        ))
-        .get_result::<Count>(db_pool.get_ok().deref_mut())
-        .await
-        .unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_speed() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let speed = create_speed(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-        let speed_deletion: DeleteOperation = speed.get_ref().into();
-
-        assert!(speed_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_speed_section WHERE obj_id = '{}' AND infra_id = {}",
-                speed.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_switch() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let switch =
-            create_switch(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-        let switch_deletion: DeleteOperation = switch.get_ref().into();
-
-        assert!(switch_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-            "SELECT COUNT (*) AS nb FROM infra_object_switch WHERE obj_id = '{}' AND infra_id = {}",
-            switch.get_id(),
-            infra.id
-        ))
-        .get_result::<Count>(db_pool.get_ok().deref_mut())
-        .await
-        .unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_detector() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let detector =
-            create_detector(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-        let detector_deletion: DeleteOperation = detector.get_ref().into();
-
-        assert!(detector_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_detector WHERE obj_id = '{}' AND infra_id = {}",
-                detector.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_buffer_stop() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let buffer_stop =
-            create_buffer_stop(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-        let buffer_stop_deletion: DeleteOperation = buffer_stop.get_ref().into();
-
-        assert!(buffer_stop_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_buffer_stop WHERE obj_id = '{}' AND infra_id = {}",
-                buffer_stop.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_route() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let route = create_route(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-        let route_deletion: DeleteOperation = route.get_ref().into();
-
-        assert!(route_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-            "SELECT COUNT (*) AS nb FROM infra_object_route WHERE obj_id = '{}' AND infra_id = {}",
-            route.get_id(),
-            infra.id
-        ))
-        .get_result::<Count>(db_pool.get_ok().deref_mut())
-        .await
-        .unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_op() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let op = create_op(db_pool.get_ok().deref_mut(), infra.id, Default::default()).await;
-
-        let op_deletion: DeleteOperation = op.get_ref().into();
-
-        assert!(op_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_operational_point WHERE obj_id = '{}' AND infra_id = {}",
-                op.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
-
-    #[rstest]
-    async fn delete_electrification() {
-        let db_pool = DbConnectionPoolV2::for_tests();
-        let infra = create_empty_infra(db_pool.get_ok().deref_mut()).await;
-        let electrification =
-            create_electrification(db_pool.get_ok().deref_mut(), infra.id, Default::default())
-                .await;
-        let op_deletion: DeleteOperation = electrification.get_ref().into();
-
-        assert!(op_deletion
-            .apply(infra.id, db_pool.get_ok().deref_mut())
-            .await
-            .is_ok());
-
-        let res_del = sql_query(format!(
-                "SELECT COUNT (*) AS nb FROM infra_object_electrification WHERE obj_id = '{}' AND infra_id = {}",
-                electrification.get_id(),
-                infra.id
-            ))
-            .get_result::<Count>(db_pool.get_ok().deref_mut()).await.unwrap();
-
-        assert_eq!(res_del.nb, 0);
-    }
+    test_delete_object!(TrackSection);
+    test_delete_object!(Signal);
+    test_delete_object!(SpeedSection);
+    test_delete_object!(Switch);
+    test_delete_object!(Detector);
+    test_delete_object!(BufferStop);
+    test_delete_object!(Route);
+    test_delete_object!(OperationalPoint);
+    test_delete_object!(Electrification);
+    test_delete_object!(NeutralSection);
 }
