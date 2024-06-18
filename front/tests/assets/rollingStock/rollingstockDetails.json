e0f4f415c8a781ec7e707455d78770337cb29296
maymanaf <med.aymen.naf@gmail.com>
2024-06-18 15:53:44 +0200

    front: refactor e2e tests creating and modifying a rolling stock
    front: tests: add beforeEarch for cleanup
    
    front: update handling of ToastSNCF

---
+++ front/tests/assets/rollingStock/rollingstockDetails.json
@@ -0,0 +1,73 @@
+{
+  "inputs": [
+    { "id": "name", "value": "Test_name" },
+    { "id": "detail", "value": "Test detail" },
+    { "id": "family", "value": "Test family" },
+    { "id": "grouping", "value": "Test grouping" },
+    { "id": "number", "value": 500 },
+    { "id": "reference", "value": "Test reference" },
+    { "id": "series", "value": "Test series" },
+    { "id": "subseries", "value": "Test subseries" },
+    { "id": "type", "value": "Test type" },
+    { "id": "unit", "value": "Test unit" },
+    { "id": "length", "value": 50 },
+    { "id": "mass-input", "value": 80, "isNumeric": true },
+    { "id": "maxSpeed-input", "value": 200, "isNumeric": true },
+    { "id": "startupTime", "value": 5 },
+    { "id": "startupAcceleration", "value": 0.1 },
+    { "id": "comfortAcceleration", "value": 1 },
+    { "id": "inertiaCoefficient", "value": 1 },
+    { "id": "gammaValue", "value": 1 },
+    { "id": "basePowerClass", "value": 7 },
+    { "id": "rollingResistanceA-input", "value": 15, "isNumeric": true },
+    { "id": "rollingResistanceB-input", "value": 0.3, "isNumeric": true },
+    { "id": "rollingResistanceC-input", "value": 0.005, "isNumeric": true }
+  ],
+  "updatedInputs": [
+    { "id": "name", "value": "Test_name_updated" },
+    { "id": "detail", "value": "Test detail_updated" },
+    { "id": "family", "value": "Test family_updated" },
+    { "id": "grouping", "value": "Test grouping_updated" },
+    { "id": "number", "value": 450 },
+    { "id": "reference", "value": "Test reference_updated" },
+    { "id": "series", "value": "Test series_updated" },
+    { "id": "subseries", "value": "Test subseries_updated" },
+    { "id": "type", "value": "Test type_updated" },
+    { "id": "unit", "value": "Test unit_updated" },
+    { "id": "length", "value": 40 },
+    { "id": "mass-input", "value": 70, "isNumeric": true },
+    { "id": "maxSpeed-input", "value": 190, "isNumeric": true },
+    { "id": "startupTime", "value": 4 },
+    { "id": "startupAcceleration", "value": 0.11 },
+    { "id": "comfortAcceleration", "value": 0.99 },
+    { "id": "inertiaCoefficient", "value": 1.3 },
+    { "id": "gammaValue", "value": 1.1 },
+    { "id": "basePowerClass", "value": 6 },
+    { "id": "rollingResistanceA-input", "value": 13, "isNumeric": true },
+    { "id": "rollingResistanceB-input", "value": 0.23, "isNumeric": true },
+    { "id": "rollingResistanceC-input", "value": 0.002, "isNumeric": true }
+  ],
+  "speedEffortData": [
+    { "velocity": "0", "effort": "500" },
+    { "velocity": "20", "effort": "700" },
+    { "velocity": "40", "effort": "600" },
+    { "velocity": "80", "effort": "800" }
+  ],
+  "speedEffortDataC1": [
+    { "velocity": "0", "effort": "900" },
+    { "velocity": "5", "effort": "800" },
+    { "velocity": "10", "effort": "909" },
+    { "velocity": "20", "effort": "800" }
+  ],
+  "speedEffortDataUpdated": [
+    { "velocity": "0", "effort": "234" },
+    { "velocity": "5", "effort": "400" },
+    { "velocity": "10", "effort": "492" },
+    { "velocity": "20", "effort": "493" },
+    { "velocity": "30", "effort": "344" }
+  ],
+  "additionalDetails": {
+    "electricalPowerStartupTime": 1,
+    "raisePantographTime": 20
+  }
+}
--- front/tests/pages/rollingstock-editor-page-model.ts
+++ front/tests/pages/rollingstock-editor-page-model.ts
@@ -1,9 +1,9 @@
-/* eslint-disable import/prefer-default-export */
 import { expect, type Locator, type Page } from '@playwright/test';
 
 import PlaywrightCommonPage from './common-page-model';
+import { fillAndCheckInputById } from '../assets/utils';
 
-export class PlaywrightRollingstockEditorPage extends PlaywrightCommonPage {
+export default class PlaywrightRollingstockEditorPage extends PlaywrightCommonPage {
   readonly getNewRollingstockButton: Locator;
 
   readonly getSubmitRollingstockButton: Locator;
@@ -12,11 +12,33 @@ export class PlaywrightRollingstockEditorPage extends PlaywrightCommonPage {
 
   readonly getSpeedEffortCurvesButton: Locator;
 
-  readonly getRollingStockSpreedsheet: Locator;
+  readonly getRollingStockSpreadsheet: Locator;
 
   readonly getRollingStockSearchInput: Locator;
 
   readonly getRollingStockEditorList: Locator;
+
+  readonly getRollingStockEditorButtons: Locator;
+
+  readonly getPowerRestrictionSelector: Locator;
+
+  readonly getElectricalProfileSelector: Locator;
+
+  readonly getLoadingGauge: Locator;
+
+  readonly getTractionModeSelector: Locator;
+
+  readonly getConfirmModalButtonYes: Locator;
+
+  readonly addPowerRestrictionButton: Locator;
+
+  readonly getPowerRestrictionModalBody: Locator;
+
+  readonly getSelectedElectricalProfileButton: Locator;
+
+  readonly deleteSelectedElectricalProfileButton: Locator;
+
+  readonly getDuplicateRollingStockButton: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -24,77 +46,243 @@ export class PlaywrightRollingstockEditorPage extends PlaywrightCommonPage {
     this.getSubmitRollingstockButton = page.getByTestId('submit-rollingstock-button');
     this.getRollingstockDetailsButton = page.getByTestId('tab-rollingstock-details');
     this.getSpeedEffortCurvesButton = page.getByTestId('tab-rollingstock-curves');
-    this.getRollingStockSpreedsheet = page.locator('.dsg-container');
+    this.getRollingStockSpreadsheet = page.locator('.dsg-container');
     this.getRollingStockSearchInput = page.locator('#searchfilter');
     this.getRollingStockEditorList = page.getByTestId('rollingstock-editor-list');
+    this.getRollingStockEditorButtons = page.locator('.rollingstock-editor-buttons');
+    this.getPowerRestrictionSelector = page.getByTestId('power-restriction-selector');
+    this.getElectricalProfileSelector = page.getByTestId('electrical-profile-selector');
+    this.getLoadingGauge = page.locator('#loadingGauge');
+    this.getTractionModeSelector = page.getByTestId('traction-mode-selector');
+    this.getConfirmModalButtonYes = page.getByTestId('confirm-modal-button-yes');
+    this.addPowerRestrictionButton = this.getPowerRestrictionSelector.getByRole('button').nth(1);
+    this.getPowerRestrictionModalBody = page.locator('#modal-body');
+    this.getSelectedElectricalProfileButton = this.getElectricalProfileSelector.getByRole('button');
+    this.deleteSelectedElectricalProfileButton = this.getSelectedElectricalProfileButton.getByRole(
+      'button',
+      { name: 'Delete item', exact: true }
+    );
+    this.getDuplicateRollingStockButton = this.getRollingStockEditorButtons
+      .locator('button')
+      .nth(1);
   }
 
+  // Navigate to the Rolling Stock Editor Page
   async navigateToPage() {
     await this.page.goto('/rolling-stock-editor/');
     await this.removeViteOverlay();
   }
 
+  // Click the New Rolling Stock Button
   async clickOnNewRollingstockButton() {
     await this.getNewRollingstockButton.click();
   }
 
+  // Search for a rolling stock
+  async searchRollingStock(rollingStockName: string) {
+    await this.getRollingStockSearchInput.fill(rollingStockName);
+  }
+
+  // Clear the search for a rolling stock
+  async clearSearchRollingStock() {
+    await this.getRollingStockSearchInput.clear();
+  }
+
+  // Select a rolling stock from the list
+  async selectRollingStock(rollingStockName: string) {
+    const rollingStockCard = this.page.getByTestId(`rollingstock-${rollingStockName}`);
+    await rollingStockCard.click();
+  }
+
+  // Edit a rolling stock
+  async editRollingStock(rollingStockName: string) {
+    await this.selectRollingStock(rollingStockName);
+    await this.getRollingStockEditorButtons.first().click();
+  }
+
+  // Click the Submit Rolling Stock Button
   async clickOnSubmitRollingstockButton() {
     await this.getSubmitRollingstockButton.click();
   }
 
+  // Click the Rolling Stock Details Button
   async clickOnRollingstockDetailsButton() {
     await this.getRollingstockDetailsButton.click();
   }
 
+  // Click the Speed Effort Curves Button
   async clickOnSpeedEffortCurvesButton() {
     await this.getSpeedEffortCurvesButton.click();
   }
 
-  getRollingStockInputById(id: string) {
-    return this.page.locator(`#${id}`);
-  }
-
-  getRollingStockInputByTestId(testId: string) {
-    return this.page.getByTestId(testId);
-  }
-
+  // Get Velocity cell by row number
   getVelocityCellByRow(row: number) {
-    return this.getRollingStockSpreedsheet.locator('.dsg-row').nth(row).locator('.dsg-cell').nth(1);
+    return this.getRollingStockSpreadsheet.locator('.dsg-row').nth(row).locator('.dsg-cell').nth(1);
   }
 
+  // Get Velocity cell value by row number
+  async getVelocityCellByRowValue(row: number) {
+    return this.getRollingStockSpreadsheet
+      .locator('.dsg-row')
+      .nth(row)
+      .locator('.dsg-cell')
+      .nth(1) // Assuming the second cell contains the velocity value
+      .locator('input')
+      .inputValue();
+  }
+
+  // Get Effort cell by row number
   getEffortCellByRow(row: number) {
-    return this.getRollingStockSpreedsheet.locator('.dsg-row').nth(row).locator('.dsg-cell').last();
+    return this.getRollingStockSpreadsheet.locator('.dsg-row').nth(row).locator('.dsg-cell').last();
   }
 
-  async setSpreedsheetCell(value: string, cell: Locator) {
-    const inputStrings = value.toString().split('');
+  // Get Effort cell value by row number
+  // Note: This method assumes there are at least three cells per row.
+  // If the structure changes, update the .nth() index accordingly.
+  async getEffortCellByRowValue(row: number) {
+    return this.getRollingStockSpreadsheet
+      .locator('.dsg-row')
+      .nth(row)
+      .locator('.dsg-cell')
+      .nth(2) // Assuming the third cell contains the effort value
+      .locator('input')
+      .inputValue();
+  }
+
+  // Set spreadsheet cell value
+  async setSpreadsheetCell(value: string, cell: Locator) {
     await cell.dblclick();
-    this.page.keyboard.press('Backspace');
+    await this.page.keyboard.press('Backspace');
     await Promise.all(
-      inputStrings.map(async (digit) => {
+      value.split('').map(async (digit) => {
         await this.page.keyboard.press(digit);
       })
     );
   }
 
-  async setSpreedsheetRow(data: { row: number; velocity: string; effort: string }[]) {
-    await Promise.all(
-      data.map(async ({ row, effort, velocity }) => {
-        const velocityCell = this.getVelocityCellByRow(row);
-        const effortCell = this.getEffortCellByRow(row);
-        await this.setSpreedsheetCell(velocity, velocityCell);
-        await this.setSpreedsheetCell(effort, effortCell);
-      })
+  // Set spreadsheet row value
+  async setSpreadsheetRow(data: { row: number; velocity: string; effort: string }[]) {
+    for (const { row, effort, velocity } of data) {
+      const velocityCell = this.getVelocityCellByRow(row);
+      const effortCell = this.getEffortCellByRow(row);
+      await this.setSpreadsheetCell(velocity, velocityCell);
+      await this.setSpreadsheetCell(effort, effortCell);
+    }
+  }
+
+  // Select gauge value
+  async selectLoadingGauge(value: string) {
+    await this.getLoadingGauge.selectOption(value);
+    expect(await this.getLoadingGauge.inputValue()).toBe(value);
+  }
+
+  // Fill speed effort curves with or without power restriction
+  async fillSpeedEffortCurves(
+    speedEffortData: { velocity: string; effort: string }[],
+    isPowerRestrictionSpecified: boolean,
+    powerRestrictionValue: string,
+    electricalProfilesValue: string
+  ) {
+    if (!isPowerRestrictionSpecified) {
+      await this.clickOnSpeedEffortCurvesButton();
+      await this.getTractionModeSelector.getByRole('button').click();
+      await this.getTractionModeSelector
+        .getByRole('button', { name: electricalProfilesValue, exact: true })
+        .click();
+      await expect(this.getTractionModeSelector.getByTitle(electricalProfilesValue)).toBeVisible();
+    }
+
+    await this.fillSpeedEffortData(
+      speedEffortData,
+      isPowerRestrictionSpecified,
+      powerRestrictionValue,
+      false
     );
   }
 
-  async fillAndCheckInputById(inputId: string, value: string | number, isTestId = false) {
-    const input = isTestId
-      ? this.getRollingStockInputByTestId(inputId)
-      : this.getRollingStockInputById(inputId);
+  // Fill speed effort data from JSON with or without power restriction
+  async fillSpeedEffortData(
+    data: { velocity: string; effort: string }[],
+    isPowerRestrictionSpecified: boolean,
+    powerRestrictionValue: string,
+    toBeUpdated: boolean
+  ) {
+    if (isPowerRestrictionSpecified && !toBeUpdated) {
+      await this.addPowerRestrictionButton.click();
+      await this.getPowerRestrictionModalBody
+        .getByTitle(powerRestrictionValue, { exact: true })
+        .click();
+      await expect(
+        this.getPowerRestrictionSelector.getByTitle(powerRestrictionValue.replace(/\s/g, ''))
+      ).toBeVisible();
+    }
+    if (toBeUpdated) {
+      await this.getPowerRestrictionSelector
+        .getByTitle(powerRestrictionValue, { exact: true })
+        .click();
+    }
+    for (const rowData of data) {
+      const rowIndex = data.indexOf(rowData) + 1;
+      const velocityCell = this.getVelocityCellByRow(rowIndex);
+      const effortCell = this.getEffortCellByRow(rowIndex);
+      await this.setSpreadsheetCell(rowData.velocity, velocityCell);
+      await this.setSpreadsheetCell(rowData.effort, effortCell);
+    }
+  }
 
-    await input.click();
-    await input.fill(`${value}`);
-    expect(await input.inputValue()).toBe(`${value}`);
+  // Verify speed effort curves
+  async verifySpeedEffortCurves(
+    expectedData: { velocity: string; effort: string }[],
+    isPowerRestrictionSpecified: boolean,
+    powerRestrictionValue: string
+  ) {
+    if (isPowerRestrictionSpecified) {
+      await this.getPowerRestrictionSelector
+        .getByRole('button', { name: powerRestrictionValue })
+        .click();
+    }
+
+    for (const rowData of expectedData) {
+      const rowIndex = expectedData.indexOf(rowData) + 1;
+      const velocityCell = await this.getVelocityCellByRowValue(rowIndex);
+      const effortCell = await this.getEffortCellByRowValue(rowIndex);
+      expect(velocityCell).toBe(rowData.velocity);
+      expect(effortCell).toBe(rowData.effort);
+    }
+  }
+
+  // Delete Electrical profile from the speed effort curves
+  async deleteElectricalProfile(electricalProfileValue: string) {
+    await this.getSelectedElectricalProfileButton.getByTitle(electricalProfileValue).hover();
+    await this.deleteSelectedElectricalProfileButton.click();
+    await this.getConfirmModalButtonYes.click();
+    await expect(
+      this.getSelectedElectricalProfileButton.getByTitle(electricalProfileValue)
+    ).toBeHidden();
+  }
+
+  // Fill additional details
+  async fillAdditionalDetails(details: {
+    electricalPowerStartupTime: number;
+    raisePantographTime: number;
+  }) {
+    await this.clickOnRollingstockDetailsButton();
+    await fillAndCheckInputById(
+      this.page,
+      'electricalPowerStartupTime',
+      details.electricalPowerStartupTime
+    );
+    await fillAndCheckInputById(this.page, 'raisePantographTime', details.raisePantographTime);
+  }
+
+  // Submit and confirm rolling stock creation
+  async submitRollingStock() {
+    await this.clickOnSubmitRollingstockButton();
+    await this.getConfirmModalButtonYes.click();
+  }
+
+  // Duplicate rolling stock creation
+  async duplicateRollingStock() {
+    await this.getDuplicateRollingStockButton.click();
   }
 }
