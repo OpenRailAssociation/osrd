776bca77b207ce503945726bc3629d3a7b92b638
hamz2a <atrari.hamza@gmail.com>
2024-06-25 15:58:00 +0200

    editoast: add tests for views::infra::lines

--- editoast/src/views/infra/lines.rs
+++ editoast/src/views/infra/lines.rs
@@ -69,3 +69,90 @@ async fn get_line_bbox(
 
     Ok(Json(zone))
 }
+
+#[cfg(test)]
+mod test {
+    use actix_web::http::StatusCode;
+    use actix_web::test::call_service;
+    use actix_web::test::read_body_json;
+    use actix_web::test::TestRequest;
+    use editoast_schemas::infra::TrackSectionSncfExtension;
+    use editoast_schemas::primitives::Identifier;
+    use geos::geojson::Geometry;
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+    use serde_json::json;
+    use std::str::FromStr;
+
+    use super::*;
+    use crate::fixtures::tests::db_pool;
+    use crate::fixtures::tests::empty_infra;
+    use crate::fixtures::tests::TestFixture;
+    use crate::infra_cache::operation::create::apply_create_operation;
+    use editoast_schemas::infra::TrackSection;
+    use editoast_schemas::infra::TrackSectionExtensions;
+    // use crate::modelsv2::fixtures::create_empty_infra;
+    // use crate::views::test_app::TestAppBuilder;
+    use crate::views::tests::create_test_service;
+
+    #[rstest]
+    async fn returns_correct_bbox_for_existing_line_code(
+        #[future] empty_infra: TestFixture<Infra>,
+    ) {
+        let empty_infra = empty_infra.await;
+        let app = create_test_service().await;
+
+        let line_code = 1234;
+        let geometry_json = json!({
+            "type": "LineString",
+            "coordinates": [[1., 2.], [3., 4.]]
+        })
+        .to_string();
+        let track_section = TrackSection {
+            id: Identifier::from("track_section_id"),
+            extensions: TrackSectionExtensions {
+                sncf: Some(TrackSectionSncfExtension {
+                    line_code,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            geo: Geometry::from_str(&geometry_json).unwrap(),
+            ..Default::default()
+        }
+        .into();
+        apply_create_operation(
+            &track_section,
+            empty_infra.id,
+            &mut db_pool().get().await.unwrap(),
+        )
+        .await
+        .expect("Failed to create track section object");
+
+        let req = TestRequest::get()
+            .uri(format!("/infra/{}/lines/{line_code}/bbox/", empty_infra.id).as_str())
+            .to_request();
+        let response = call_service(&app, req).await;
+        assert_eq!(response.status(), StatusCode::OK);
+        let bounding_box: BoundingBox = read_body_json(response).await;
+        assert_eq!(bounding_box, BoundingBox((1., 2.), (3., 4.)));
+    }
+
+    #[rstest]
+    async fn returns_400_when_line_code_not_found(#[future] empty_infra: TestFixture<Infra>) {
+        let empty_infra = empty_infra.await;
+        let app = create_test_service().await;
+        let not_existing_line_code = 123456789;
+        let req = TestRequest::get()
+            .uri(
+                format!(
+                    "/infra/{}/lines/{not_existing_line_code}/bbox/",
+                    empty_infra.id
+                )
+                .as_str(),
+            )
+            .to_request();
+        let response = call_service(&app, req).await;
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+}
