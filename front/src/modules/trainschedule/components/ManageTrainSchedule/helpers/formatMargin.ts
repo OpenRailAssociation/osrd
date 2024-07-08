1e07d20fa30362e700184eb7f1c4e429449043b8
theocrsb <theo_crosbie@yahoo.fr>
2024-07-08 16:06:32 +0200

    front: change margin logic

--- front/src/modules/trainschedule/components/ManageTrainSchedule/helpers/formatMargin.ts
+++ front/src/modules/trainschedule/components/ManageTrainSchedule/helpers/formatMargin.ts
@@ -1,21 +1,31 @@
 import type { Margin } from 'modules/trainschedule/components/ManageTrainSchedule/types';
 import type { PathStep } from 'reducers/osrdconf/types';
 
-const formatMargin = (pathSteps: PathStep[]): Margin => {
-  const margins: Margin = {
-    boundaries: [],
-    values: [],
-  };
+const formatMargin = (pathSteps: PathStep[]): Margin | undefined => {
+  const boundaries: string[] = [];
+  const values: string[] = [];
 
   pathSteps.forEach((step, index) => {
     if (index === 0) {
-      margins.values.push(step.theoreticalMargin || 'none');
-    } else if (step.theoreticalMargin !== pathSteps[index - 1].theoreticalMargin) {
-      margins.boundaries.push(step.id);
-      margins.values.push(step.theoreticalMargin || 'none');
+      values.push(step.theoreticalMargin || 'none');
+    } else if (index === pathSteps.length - 1) {
+      if (values.length === 1 && values[0] !== 'none') {
+        boundaries.push(step.id);
+        values.push('none');
+      }
+    }
+
+    // for the other steps, we add the margin if it's different from the previous one
+    else if (step.theoreticalMargin && step.theoreticalMargin !== values[values.length - 1]) {
+      boundaries.push(step.id);
+      values.push(step.theoreticalMargin);
     }
   });
-  return margins;
+
+  if (values.length === 1) {
+    return undefined;
+  }
+  return { boundaries, values };
 };
 
 export default formatMargin;
