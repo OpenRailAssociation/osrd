fa25d4e8bbbcb2d29b00747d365d0a42070a566c
Simon Ser <contact@emersion.fr>
2024-07-11 16:11:03 +0200

    front: re-format all files
    Run `yarn prettier . --write`.

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -1,17 +1,21 @@
 import { mergeConfig } from 'vite';
 
 module.exports = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   docs: {
-    autodocs: true
+    autodocs: true,
   },
   async viteFinal(config) {
     // Merge custom configuration into the default config
--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,12 +1,12 @@
-import("@sncf/bootstrap-sncf.metier.reseau/dist/css/bootstrap-sncf.min.css");
-import("styles/styles.scss");
+import('@sncf/bootstrap-sncf.metier.reseau/dist/css/bootstrap-sncf.min.css');
+import('styles/styles.scss');
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
