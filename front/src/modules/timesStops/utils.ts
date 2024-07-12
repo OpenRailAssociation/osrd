dad4e39384c26d5eb0a5d8c112d0ef90c19ba21c
Simon Ser <contact@emersion.fr>
2024-07-12 13:37:13 +0200

    front: ensure destination is last in formatSuggestedViasToRowVias()
    Apply the same logic as the origin, but for the destination.
    
    See https://github.com/OpenRailAssociation/osrd/pull/8032#discussion_r1675411754

--- front/src/modules/timesStops/utils.ts
+++ front/src/modules/timesStops/utils.ts
@@ -15,15 +15,29 @@ export const formatSuggestedViasToRowVias = (
   startTime?: string
 ): PathWaypointColumn[] => {
   const formattedOps = [...operationalPoints];
+
+  // If the origin is in the ops and isn't the first operational point, we need
+  // to move it to the first position
   const origin = pathSteps[0];
   const originIndexInOps = operationalPoints.findIndex((op) => matchPathStepAndOp(origin, op));
-  // If the origin is in the ops and isn't the first operational point, we need to move it to the first position
   if (originIndexInOps !== -1) {
     [formattedOps[0], formattedOps[originIndexInOps]] = [
       formattedOps[originIndexInOps],
       formattedOps[0],
     ];
   }
+
+  // Ditto: destination should be last
+  const dest = pathSteps[pathSteps.length - 1];
+  const destIndexInOps = operationalPoints.findIndex((op) => matchPathStepAndOp(dest, op));
+  if (destIndexInOps !== -1) {
+    const lastOpIndex = formattedOps.length - 1;
+    [formattedOps[lastOpIndex], formattedOps[destIndexInOps]] = [
+      formattedOps[destIndexInOps],
+      formattedOps[lastOpIndex],
+    ];
+  }
+
   return formattedOps.map((op, i) => {
     const pathStep = pathSteps.find((step) => matchPathStepAndOp(step, op));
     const { name } = pathStep || op;
