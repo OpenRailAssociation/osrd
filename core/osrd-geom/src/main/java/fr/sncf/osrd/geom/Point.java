f5a21c38f923bda9cbc1ec499884844e4c14e49b
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2024-08-07 16:08:06 +0200

    core: geo: project to EPSG:3857 to interpolate

--- core/osrd-geom/src/main/java/fr/sncf/osrd/geom/Point.java
+++ core/osrd-geom/src/main/java/fr/sncf/osrd/geom/Point.java
@@ -1,5 +1,6 @@
 package fr.sncf.osrd.geom;
 
+import static fr.sncf.osrd.geom.WGS84Interpolator.EARTH_RADIUS;
 import static java.lang.Math.*;
 
 public record Point(
@@ -12,14 +13,13 @@ public record Point(
      * Returns the distance between this point and another in meters. Uses equirectangular distance approximation (very fast but not 100% accurate)
      */
     public double distanceAsMeters(Point other) {
-        final var earthRadius = 6_378_160;
         var lon1 = toRadians(lon);
         var lon2 = toRadians(other.lon);
         var lat1 = toRadians(lat);
         var lat2 = toRadians(other.lat);
         var xDiff = (lon1 - lon2) * cos(0.5 * (lat1 + lat2));
         var yDiff = lat1 - lat2;
-        return earthRadius * sqrt(xDiff * xDiff + yDiff * yDiff);
+        return EARTH_RADIUS * sqrt(xDiff * xDiff + yDiff * yDiff);
     }
 
     @Override
