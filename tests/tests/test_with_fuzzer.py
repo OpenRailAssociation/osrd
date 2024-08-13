a11df3b0a48516a27c5fd376369e84ff9ab08c3e
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2024-08-13 15:59:32 +0200

    tests: adapt integration tests

--- tests/tests/test_with_fuzzer.py
+++ tests/tests/test_with_fuzzer.py
@@ -1,7 +1,7 @@
 import pytest
 
 # TODO: we should clean up the package and imports structure
-from fuzzer import fuzzer
+from fuzzer import fuzzer_v2
 
 from .scenario import Scenario
 from .services import EDITOAST_URL
@@ -10,4 +10,4 @@ from .services import EDITOAST_URL
 @pytest.mark.usefixtures("fast_rolling_stock")
 @pytest.mark.parametrize("seed", range(5))
 def test_with_fuzzer(tiny_scenario: Scenario, seed: int):
-    fuzzer.run(EDITOAST_URL, tiny_scenario, n_test=1, seed=seed + 1)
+    fuzzer_v2.run(EDITOAST_URL, tiny_scenario, n_test=1, seed=seed + 1)
