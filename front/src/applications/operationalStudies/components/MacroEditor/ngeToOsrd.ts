1a0379343cfa4a3ffc1374d39540b2465d90dbdd
Louis Greiner <greiner.louis@gmail.com>
2024-08-14 17:05:32 +0200

    front: change 2000ms instead of 500ms

--- front/src/applications/operationalStudies/components/MacroEditor/ngeToOsrd.ts
+++ front/src/applications/operationalStudies/components/MacroEditor/ngeToOsrd.ts
@@ -116,10 +116,10 @@ const handleTrainrunOperation = async ({
   }
 };
 
-const handleUpsertNode = debounce((timeTableId: number, node: Node) => {
+const handleUpdateNode = debounce((timeTableId: number, node: Node) => {
   const { betriebspunktName: trigram, positionX, positionY } = node;
   nodeStore.set(timeTableId, { trigram, positionX, positionY });
-}, 500);
+}, 2000);
 
 const handleNodeOperation = ({
   type,
@@ -133,7 +133,7 @@ const handleNodeOperation = ({
   switch (type) {
     case 'create':
     case 'update': {
-      handleUpsertNode(timeTableId, node);
+      handleUpdateNode(timeTableId, node);
       break;
     }
     case 'delete': {
