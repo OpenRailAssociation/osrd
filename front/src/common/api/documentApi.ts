eb956b21a8587e9211db0e72ebe5bdc2204cf975
Simon Ser <contact@emersion.fr>
2024-08-22 12:27:07 +0200

    front: use fetch API for documents
    Instead of depending on axios, use the standard fetch() API.

--- front/src/common/api/documentApi.ts
+++ front/src/common/api/documentApi.ts
@@ -1,24 +1,18 @@
-import axios, { type ResponseType } from 'axios';
-
 import mainConfig from 'config/config';
 
 export const getDocument = async (documentKey: number): Promise<Blob> => {
-  const config = {
-    responseType: 'blob' as ResponseType,
-  };
-  const path = `${mainConfig.proxy_editoast}/documents/${documentKey}`;
-  const res = await axios.get(path, config);
-  return res.data;
+  const res = await fetch(`${mainConfig.proxy_editoast}/documents/${documentKey}`);
+  return res.blob();
 };
 
 export const postDocument = async (image: Blob) => {
-  const config = {
+  const res = await fetch(`${mainConfig.proxy_editoast}/documents`, {
+    method: 'POST',
     headers: {
       'Content-Type': image.type || 'application/octet-stream',
     },
-  };
-  const path = `${mainConfig.proxy_editoast}/documents`;
-
-  const res = await axios.post(path, image, config);
-  return res.data.document_key;
+    body: image,
+  });
+  const data = await res.json();
+  return data.document_key;
 };
