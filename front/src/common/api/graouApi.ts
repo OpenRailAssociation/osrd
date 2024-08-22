51192762f1aa4eba3e90bea373cb4caadd077128
Simon Ser <contact@emersion.fr>
2024-08-22 12:42:56 +0200

    front: use fetch API for graou
    Instead of depending on axios, use the standard fetch() API.

--- front/src/common/api/graouApi.ts
+++ front/src/common/api/graouApi.ts
@@ -1,18 +1,16 @@
 import type { TrainScheduleImportConfig } from 'applications/operationalStudies/types';
 import type { ImportStation } from 'common/StationCard';
 
-import customAxios from './axios';
-
 export const GRAOU_URL = 'https://graou.info';
 
 export const getGraouTrainSchedules = async (config: TrainScheduleImportConfig) => {
-  const params = {
+  const params = new URLSearchParams({
     q: 'trains',
-    config,
-  };
+    config: JSON.stringify(config),
+  });
   try {
-    const result = await customAxios.get(`${GRAOU_URL}/api/trainschedules.php`, { params });
-    return result.data;
+    const res = await fetch(`${GRAOU_URL}/api/trainschedules.php?${params}`);
+    return res.json();
   } catch (error) {
     console.error(error);
     return null;
@@ -24,13 +22,13 @@ export const getGraouTrainSchedules = async (config: TrainScheduleImportConfig) 
  * (trigram if term.length < 3, by name otherwise)
  */
 export const searchGraouStations = async (term: string) => {
-  const params = {
+  const params = new URLSearchParams({
     q: 'stations',
     term,
-  };
+  });
   try {
-    const result = await customAxios.get(`${GRAOU_URL}/api/stations.php`, { params });
-    return result.data as ImportStation[];
+    const res = await fetch(`${GRAOU_URL}/api/stations.php?${params}`);
+    return (await res.json()) as ImportStation[];
   } catch (error) {
     console.error(error);
     return null;
