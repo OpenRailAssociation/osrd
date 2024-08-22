0551a06a47e77520f11e694454d1e40e2cd41fb1
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2024-08-22 02:41:35 +0200

    editoast: rename tables

---
+++ editoast/migrations/2024-08-21-213611_rename_tsv2_table/down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE scenario RENAME TO scenario_v2;
+ALTER TABLE timetable RENAME TO timetable_v2;
+ALTER TABLE train_schedule RENAME TO train_schedule_v2;
---
+++ editoast/migrations/2024-08-21-213611_rename_tsv2_table/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE scenario_v2 RENAME TO scenario;
+ALTER TABLE timetable_v2 RENAME TO timetable;
+ALTER TABLE train_schedule_v2 RENAME TO train_schedule;
