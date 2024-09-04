1cfd54c891384f77f217f4abece93c0d27ecd404
nncluzu <ngamenichaka@yahoo.fr>
2024-09-04 20:11:16 +0200

    front: use design system font in new stdcm interface.
    Signed-off-by: nncluzu <ngamenichaka@yahoo.fr>

--- front/src/applications/stdcmV2/components/StdcmEmptyConfigError.tsx
+++ front/src/applications/stdcmV2/components/StdcmEmptyConfigError.tsx
@@ -1,16 +1,14 @@
 import { Tools } from '@osrd-project/ui-icons';
 import { useTranslation } from 'react-i18next';
 
-import 'applications/stdcmV2/styles.scss';
-
 const StdcmEmptyConfigError = () => {
   const { t } = useTranslation('stdcm');
   return (
     <div className="stdcm-config-error">
-      <span className="mb-3 icon">
+      <span className="icon">
         <Tools size="lg" />
       </span>
-      <h2 className="my-3 mx-0">{t('noConfigurationFound.title')}</h2>
+      <h2 className="mx-0">{t('noConfigurationFound.title')}</h2>
       <p>{t('noConfigurationFound.text')}</p>
     </div>
   );
