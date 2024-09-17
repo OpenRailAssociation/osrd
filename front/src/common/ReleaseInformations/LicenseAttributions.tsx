0a8fa8019a7b99fd006aa80e87ff6fffd1c22beb
Clara Ni <clara.ni@outlook.fr>
2024-09-17 17:47:59 +0200

    front: add some data sources and licenses - add some data sources and licenses - refacto the licenseAttribution component to separe the data sources and the licenses

--- front/src/common/ReleaseInformations/LicenseAttributions.tsx
+++ front/src/common/ReleaseInformations/LicenseAttributions.tsx
@@ -2,57 +2,95 @@ import { useTranslation } from 'react-i18next';
 
 import attributionsLicenses from './json/licenses.json';
 
-type attributionsLicensesType = {
+type AttributionLicense = {
   name: string;
   version: string;
   copyright: string;
   publisher: string;
   url?: string;
-  licenses: string;
+  license: string;
 };
 
-const LicenseAttributions = () => {
-  const { t } = useTranslation('home/navbar');
-
-  // mglTraffic is not a lib from package.json, so we add it manually
-  const mglTrafficAttribution: attributionsLicensesType = {
-    licenses: 'CC-BY-NC-SA 3.0',
+// Libs which are not in the package.json, so we add it statically
+const DATA_SOURCES: AttributionLicense[] = [
+  {
+    license: 'https://geoservices.ign.fr/cgu-licences',
+    publisher: 'IGN',
+    copyright: '© IGN - 2021',
+    version: '',
+    name: 'IGN Ortho',
+    url: 'https://geoservices.ign.fr/services-web-experts-ortho',
+  },
+  {
+    license: 'https://data.sncf.com/pages/licence/#A1',
+    publisher: 'SNCF',
+    copyright: '',
+    version: '',
+    name: 'Open data SNCF',
+    url: 'https://ressources.data.sncf.com/pages/accueil/',
+  },
+  {
+    license: 'CC-BY-NC-SA 3.0',
     publisher: 'Marc Le Gad',
     copyright: '',
     version: '',
     name: 'MLG Traffic',
     url: 'http://www.mlgtraffic.net',
-  };
+  },
+  {
+    license: 'https://github.com/tilezen/joerd/blob/master/docs/attribution.md',
+    publisher: 'Mapzen',
+    copyright: '',
+    version: '',
+    name: 'Terrain Tiles',
+    url: 'https://registry.opendata.aws/terrain-tiles/',
+  },
+];
 
-  const attributionsArray = [
-    ...(Object.values(attributionsLicenses) as attributionsLicensesType[]),
-    mglTrafficAttribution,
-  ];
+type LicenseCardProps = { attribution: AttributionLicense };
+
+const LicenseCard = ({
+  attribution: { name, version, copyright, publisher, url, license },
+}: LicenseCardProps) => (
+  <a href={url} rel="noreferrer" target="blank">
+    <h3 className="d-flex mr-1 mb-0">
+      {name}
+      <small className="d-flex align-items-center ml-2">
+        {version}
+        {url && <i className="ml-2 icons-external-link" />}
+      </small>
+    </h3>
+    <div className="small ml-4 mb-2">
+      {copyright.replace('(c)', '©') || publisher || `${name} authors`}
+      {license && ` / ${license}`}
+    </div>
+  </a>
+);
+
+const LicenseAttributions = () => {
+  const { t } = useTranslation('home/navbar');
+
+  const attributionsArray = [...(Object.values(attributionsLicenses) as AttributionLicense[])];
 
   const attributions = attributionsArray
     .map((licence) => ({ ...licence, name: licence.name.replace('@', '') }))
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .map(({ name, version, copyright, publisher, url, licenses }) => (
-      <a key={name} href={url} rel="noreferrer">
-        <h3 className="d-flex mr-1 mb-0">
-          {name}
-          <small className="d-flex align-items-center ml-2">
-            {`${version}`}
-            {url && <i className="ml-2 icons-external-link" />}
-          </small>
-        </h3>
-        <div className="small ml-4 mb-2">
-          {copyright.replace('(c)', '©') || publisher || `${name} authors`}
-          {licenses && ` / ${licenses}`}
-        </div>
-      </a>
-    ));
+    .sort((a, b) => a.name.localeCompare(b.name));
 
   return (
-    <>
-      <h2 className="text-center mt-sm-1 mb-4">{t('informations.releaseInformations')}</h2>
-      <div className="license-attributions">{attributions}</div>
-    </>
+    <div className="col-md-6 h-100 d-flex flex-column">
+      <h2 className="text-center mb-4">{t('informations.dataSources')}</h2>
+      <div className="license-attributions">
+        {DATA_SOURCES.map((dataSource) => (
+          <LicenseCard attribution={dataSource} key={dataSource.name} />
+        ))}
+      </div>
+      <h2 className="text-center my-4">{t('informations.librairies')}</h2>
+      <div className="license-attributions licenses">
+        {attributions.map((attribution) => (
+          <LicenseCard attribution={attribution} key={attribution.name} />
+        ))}
+      </div>
+    </div>
   );
 };
 
