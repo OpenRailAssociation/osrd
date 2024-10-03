01593dc8734e215154a397c8316b155d654ab4ed
Yohh <durandyohan@zaclys.net>
2024-10-03 11:07:44 +0200

    front: switch from yarn to npm
    Signed-off-by: Yohh <durandyohan@zaclys.net>

--- front/scripts/generate-types.sh
+++ front/scripts/generate-types.sh
@@ -4,10 +4,10 @@ if [ $? -ne 0 ]; then
     echo "npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.ts command failed. Exit the script"
     exit 1
 fi
-yarn eslint --fix src/common/api/generatedEditoastApi.ts --no-ignore
+npm exec eslint -- --fix src/common/api/generatedEditoastApi.ts --no-ignore
 npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.ts
 if [ $? -ne 0 ]; then
     echo "npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.ts command failed. Exit the script"
     exit 1
 fi
-yarn eslint --fix src/common/api/osrdGatewayApi.ts --no-ignore
+npm exec eslint -- --fix src/common/api/osrdGatewayApi.ts --no-ignore
