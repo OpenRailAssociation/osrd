cfe4aba3eaeed6b9bec5b0a75faa0c990811f9d0
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2024-10-07 14:35:30 +0200

    front: adapt front
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>

--- front/src/modules/trainschedule/components/Timetable/consts.ts
+++ front/src/modules/trainschedule/components/Timetable/consts.ts
@@ -7,8 +7,13 @@ export const specialCodeDictionary: { [key: string]: string } = {
 export const invalidTrainValues: {
   [key in InvalidReason]: InvalidReason;
 } = {
-  pathfinding_not_found: 'pathfinding_not_found',
   pathfinding_failure: 'pathfinding_failure',
-  pathfinding_input_error: 'pathfinding_input_error',
   simulation_failed: 'simulation_failed',
+  not_found_in_blocks: 'not_found_in_blocks',
+  not_found_in_routes: 'not_found_in_routes',
+  not_found_in_tracks: 'not_found_in_tracks',
+  incompatible_constraints: 'incompatible_constraints',
+  rolling_stock_not_found: 'rolling_stock_not_found',
+  not_enough_path_items: 'not_enough_path_items',
+  invalid_path_items: 'invalid_path_items',
 };
