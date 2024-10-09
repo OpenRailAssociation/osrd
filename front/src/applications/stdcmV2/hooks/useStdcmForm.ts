ec7a5cc54e87ff81741b4f077909f1450c3c47f5
Clara Ni <clara.ni@outlook.fr>
2024-10-09 14:07:25 +0200

    front: remove setCurrentSimulationInputs in stdcm form
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

---
+++ front/src/applications/stdcmV2/hooks/useStdcmForm.ts
@@ -0,0 +1,36 @@
+import { useMemo } from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { useOsrdConfSelectors } from 'common/osrdContext';
+import { useStoreDataForRollingStockSelector } from 'modules/rollingStock/components/RollingStockSelector/useStoreDataForRollingStockSelector';
+import type { StdcmConfSelectors } from 'reducers/osrdconf/stdcmConf/selectors';
+import { extractDateAndTimefromISO } from 'utils/date';
+
+import type { StdcmSimulationInputs } from '../types';
+
+const useStdcmForm = (): StdcmSimulationInputs => {
+  const { getPathSteps, getSpeedLimitByTag } = useOsrdConfSelectors() as StdcmConfSelectors;
+  const pathSteps = useSelector(getPathSteps);
+  const speedLimitByTag = useSelector(getSpeedLimitByTag);
+  const { rollingStock } = useStoreDataForRollingStockSelector();
+
+  const currentSimulationInputs = useMemo(() => {
+    const origin = pathSteps.at(0);
+    const originArrival = origin?.arrival ? extractDateAndTimefromISO(origin.arrival) : undefined;
+
+    return {
+      pathSteps,
+      departureDate: originArrival?.arrivalDate,
+      departureTime: originArrival?.arrivalTime,
+      consist: {
+        tractionEngine: rollingStock,
+        speedLimitByTag,
+      },
+    };
+  }, [pathSteps, rollingStock, speedLimitByTag]);
+
+  return currentSimulationInputs;
+};
+
+export default useStdcmForm;
