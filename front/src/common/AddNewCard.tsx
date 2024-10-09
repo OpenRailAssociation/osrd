9dbcd0a176b749fd9f2034e7fe70c17061f72e4f
nncluzu <ngamenichaka@yahoo.fr>
2024-10-09 17:36:02 +0200

    store required user roles for feature access in REQUIRED_USER_ROLES_FOR variable
    Centralize the roles required for accessing different views, user profiles, and features within the REQUIRED_USER_ROLES_FOR variable. This allows for easier maintenance and role-checking across the application.
    
    Signed-off-by: nncluzu <ngamenichaka@yahoo.fr>

--- front/src/common/AddNewCard.tsx
+++ front/src/common/AddNewCard.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { FaPlus } from 'react-icons/fa';
 
 import RoleBasedContent from './authorization/components/RoleBasedContent';
+import { REQUIRED_USER_ROLES_FOR } from './authorization/roleBaseAccessControl';
 import { useModal } from './BootstrapSNCF/ModalSNCF';
 
 type AddNewCardProps = {
@@ -25,7 +26,10 @@ const AddNewCard = ({
   const { openModal } = useModal();
 
   return (
-    <RoleBasedContent requiredRoles={['OpsWrite']} disableIfUnauthorized>
+    <RoleBasedContent
+      requiredRoles={REQUIRED_USER_ROLES_FOR.FEATURES.CREATE_NEW_PROJECT_STUDY_SCENARIO}
+      disableIfUnauthorized
+    >
       <div
         data-testid={testId}
         className={className}
