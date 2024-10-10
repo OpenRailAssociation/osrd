eddafc0b11ec14060976392ba3ab667293df818d
Simon Ser <contact@emersion.fr>
2024-10-10 15:03:57 +0200

    front: pick OP with BV/00/null when ch is missing for NGE
    When a path item location is missing a secondary code, editoast
    will default to BV/00/null. Reproduce that behavior here.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Closes: https://github.com/OpenRailAssociation/osrd/issues/8926

--- front/src/applications/operationalStudies/components/MacroEditor/utils.ts
+++ front/src/applications/operationalStudies/components/MacroEditor/utils.ts
@@ -7,21 +7,31 @@ import { ISO8601Duration2sec } from 'utils/timeManipulation';
 export const findOpFromPathItem = (
   pathItem: PathItemLocation,
   searchResults: SearchResultItemOperationalPoint[]
-) =>
-  searchResults.find((searchResult) => {
+) => {
+  // When a path item doesn't specify a secondary code, mimick what editoast
+  // does: pick 'BV', '00' or an OP without a ch.
+  let chs: (string | null)[] = [];
+  if ('uic' in pathItem || 'trigram' in pathItem) {
+    if (pathItem.secondary_code) {
+      chs = [pathItem.secondary_code];
+    } else {
+      chs = ['BV', '00', null];
+    }
+  }
+
+  return searchResults.find((searchResult) => {
     if ('uic' in pathItem) {
-      return searchResult.uic === pathItem.uic && searchResult.ch === pathItem.secondary_code;
+      return searchResult.uic === pathItem.uic && chs.includes(searchResult.ch);
     }
     if ('trigram' in pathItem) {
-      return (
-        searchResult.trigram === pathItem.trigram && searchResult.ch === pathItem.secondary_code
-      );
+      return searchResult.trigram === pathItem.trigram && chs.includes(searchResult.ch);
     }
     if ('operational_point' in pathItem) {
       return searchResult.obj_id === pathItem.operational_point;
     }
     return false;
   });
+};
 
 export const addDurationToDate = (date: Date, duration: string) =>
   new Date(date.getTime() + ISO8601Duration2sec(duration) * 1000);
