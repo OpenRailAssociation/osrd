604cc3d4f0ac7ef00dbf8e3f7dce01e8113d58df
romainvalls <romain.valls95@gmail.com>
2024-10-15 14:21:29 +0200

    front: disable micro macro buttons when infra loads

--- front/src/applications/operationalStudies/components/MicroMacroSwitch.tsx
+++ front/src/applications/operationalStudies/components/MicroMacroSwitch.tsx
@@ -1,6 +1,8 @@
 import cx from 'classnames';
 import { useTranslation } from 'react-i18next';
 
+import useInfraStatus from 'modules/pathfinding/hooks/useInfraStatus';
+
 type MicroMacroSwitchProps = {
   isMacro: boolean;
   setIsMacro: (isMacro: boolean) => void;
@@ -8,6 +10,7 @@ type MicroMacroSwitchProps = {
 
 const MicroMacroSwitch = ({ isMacro, setIsMacro }: MicroMacroSwitchProps) => {
   const { t } = useTranslation('operationalStudies/scenario');
+  const infraData = useInfraStatus();
   return (
     <div className="micro-macro-buttons">
       <button
@@ -15,6 +18,7 @@ const MicroMacroSwitch = ({ isMacro, setIsMacro }: MicroMacroSwitchProps) => {
         className={cx('micro-button', { active: !isMacro })}
         tabIndex={0}
         onClick={() => setIsMacro(false)}
+        disabled={!infraData.isInfraLoaded}
       >
         {t('microscopic')}
       </button>
@@ -24,6 +28,7 @@ const MicroMacroSwitch = ({ isMacro, setIsMacro }: MicroMacroSwitchProps) => {
         className={cx('macro-button', { active: isMacro })}
         tabIndex={0}
         onClick={() => setIsMacro(true)}
+        disabled={!infraData.isInfraLoaded}
       >
         {t('macroscopic')}
       </button>
