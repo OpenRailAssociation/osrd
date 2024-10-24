964dfe371bff497e9abf9fa60ce4583fbc658121
maymanaf <med.aymen.naf@gmail.com>
2024-10-24 17:05:03 +0200

    front: refacto op rolling stock tab tests
    Signed-off-by: maymanaf <med.aymen.naf@gmail.com>

--- front/tests/007-op-rollingstock-tab.spec.ts
+++ front/tests/007-op-rollingstock-tab.spec.ts
@@ -1,111 +1,108 @@
-import { expect, test } from '@playwright/test';
+import { expect } from '@playwright/test';
 
-import type { Project, RollingStock, Scenario, Study } from 'common/api/osrdEditoastApi';
+import type { LightRollingStock, Project, Scenario, Study } from 'common/api/osrdEditoastApi';
 
 import OperationalStudiesPage from './pages/operational-studies-page-model';
-import RollingStockSelectorPage from './pages/rollingstock-selector-page';
-import ScenarioPage from './pages/scenario-page-model';
+import RollingStockSelectorPage from './pages/rollingstock-selector-page-model';
+import test from './test-logger';
 import { getRollingStock } from './utils/api-setup';
-import setupScenario from './utils/scenario';
+import createScenario from './utils/scenario';
+import { deleteScenario } from './utils/teardown-utils';
 
-let project: Project;
-let study: Study;
-let scenario: Scenario;
-let rollingStock: RollingStock;
+test.describe('Rolling stock Tab Verification', () => {
+  let project: Project;
+  let study: Study;
+  let scenario: Scenario;
+  let rollingStock: LightRollingStock;
 
-const dualModeRollingStockName = 'dual-mode_rolling_stock_test_e2e';
-const electricRollingStockName = 'electric_rolling_stock_test_e2e';
+  const dualModeRollingStockName = 'dual-mode_rolling_stock_test_e2e';
+  const electricRollingStockName = 'electric_rolling_stock_test_e2e';
 
-test.beforeAll(async () => {
-  rollingStock = await getRollingStock(electricRollingStockName);
+  test.beforeAll('Set up a scenario before all tests', async () => {
+    rollingStock = await getRollingStock(electricRollingStockName);
+    ({ project, study, scenario } = await createScenario());
+  });
 
-  // Create a new scenario
-  ({ project, study, scenario } = await setupScenario());
-});
+  test.afterAll('Delete the created scenario', async () => {
+    await deleteScenario(project.id, study.id, scenario.name);
+  });
 
-test.beforeEach(async ({ page }) => {
-  const scenarioPage = new ScenarioPage(page);
+  test.beforeEach(' Navigate to the scenario page', async ({ page }) => {
+    const operationalStudiesPage = new OperationalStudiesPage(page);
+    await page.goto(
+      `/operational-studies/projects/${project.id}/studies/${study.id}/scenarios/${scenario.id}`
+    );
+    // Ensure infrastructure is loaded before each test
+    await operationalStudiesPage.checkInfraLoaded();
+  });
 
-  // Navigate to the created scenario page
-  await page.goto(
-    `/operational-studies/projects/${project.id}/studies/${study.id}/scenarios/${scenario.id}`
-  );
-  // Verify that the infrastructure is correctly loaded
-  await scenarioPage.checkInfraLoaded();
-});
-
-test.describe('Verifying that all elements in the rolling stock tab are loaded correctly', () => {
-  test('should correctly select a rolling stock for operational study', async ({ page }) => {
+  /** *************** Test 1 **************** */
+  test('Select a rolling stock for operational study', async ({ page }) => {
     const operationalStudiesPage = new OperationalStudiesPage(page);
     const rollingStockSelector = new RollingStockSelectorPage(page);
 
-    // Click on add train button
-    await operationalStudiesPage.clickOnAddTrainBtn();
-
-    // Verify the presence of warnings in Rolling Stock and Route Tab
+    // Add a train and verify the presence of warnings
+    await operationalStudiesPage.clickOnAddTrainButton();
     await operationalStudiesPage.verifyTabWarningPresence();
 
-    // Open Rolling Stock Selector and search for the added train
-    await operationalStudiesPage.openEmptyRollingStockSelector();
+    // Open the Rolling Stock Selector and search for the dual-mode rolling stock
+    await rollingStockSelector.openEmptyRollingStockSelector();
     await rollingStockSelector.searchRollingstock(dualModeRollingStockName);
+
+    // Locate the rolling stock card and verify its inactive state
     const rollingstockCard = rollingStockSelector.getRollingstockCardByTestID(
       `rollingstock-${dualModeRollingStockName}`
     );
-
-    // Verify that the rolling stock card is inactive
     await expect(rollingstockCard).toHaveClass(/inactive/);
 
-    // Verify that the rolling stock card is active after clicking on it
+    // Click on the rolling stock card to activate it
     await rollingstockCard.click();
     await expect(rollingstockCard).not.toHaveClass(/inactive/);
 
-    // Select the comfort AIR_CONDITIONING
+    // Select the comfort option (AIR_CONDITIONING) and confirm the selection
     const comfortACRadioText = await rollingStockSelector.comfortACButton.innerText();
     await rollingStockSelector.comfortACButton.click();
-
-    // Select the rolling stock
     await rollingstockCard.locator('button').click();
 
-    // Verify that the correct comfort type is displayed
-    const selectedComfortACText = await rollingStockSelector.getSelectedComfortType.innerText();
+    // Verify that the correct comfort type is displayed after selection
+    const selectedComfortACText = await rollingStockSelector.selectedComfortType.innerText();
     expect(selectedComfortACText).toMatch(new RegExp(comfortACRadioText, 'i'));
   });
 
-  test('should correctly modify a rolling stock for operational study', async ({ page }) => {
+  /** *************** Test 2 **************** */
+  test('Modify a rolling stock for operational study', async ({ page }) => {
     const operationalStudiesPage = new OperationalStudiesPage(page);
     const rollingStockSelector = new RollingStockSelectorPage(page);
 
-    // Click on add train button
-    await operationalStudiesPage.clickOnAddTrainBtn();
-
-    // Open Rolling Stock Selector, search for the added train, and select it
-    await operationalStudiesPage.openEmptyRollingStockSelector();
+    // Add a train and select the electric rolling stock
+    await operationalStudiesPage.clickOnAddTrainButton();
+    await rollingStockSelector.openEmptyRollingStockSelector();
     await rollingStockSelector.searchRollingstock(electricRollingStockName);
     const fastRollingstockCard = rollingStockSelector.getRollingstockCardByTestID(
       `rollingstock-${rollingStock.name}`
     );
+
+    // Select the rolling stock and confirm the selection
     await fastRollingstockCard.click();
     await fastRollingstockCard.locator('button').click();
-    expect(await rollingStockSelector.getSelectedRollingStockName.innerText()).toEqual(
+    expect(await rollingStockSelector.selectedRollingStockName.innerText()).toEqual(
       electricRollingStockName
     );
 
-    // Reopen the Rolling Stock selector from the selected card
-    await operationalStudiesPage.openRollingStockSelector();
+    // Reopen the rolling stock selector and apply filters
+    await rollingStockSelector.openRollingstockModal();
+    await rollingStockSelector.setThermalRollingStockFilter();
+    await rollingStockSelector.setElectricRollingStockFilter();
 
-    // Apply thermal and electrical filters
-    await rollingStockSelector.thermalRollingStockFilter();
-    await rollingStockSelector.electricRollingStockFilter();
-
-    // Select the dual mode rolling stock
+    // Select the dual-mode rolling stock and confirm the selection
     const dualModeRollingstockCard = rollingStockSelector.getRollingstockCardByTestID(
       `rollingstock-${dualModeRollingStockName}`
     );
     await dualModeRollingstockCard.click();
     await dualModeRollingstockCard.locator('button').click();
 
-    // Verify that the correct rolling stock name is displayed
-    expect(await rollingStockSelector.getSelectedRollingStockName.innerText()).toEqual(
+    // Verify that the correct dual-mode rolling stock is displayed
+    expect(await rollingStockSelector.selectedRollingStockName.innerText()).toEqual(
       dualModeRollingStockName
     );
   });
