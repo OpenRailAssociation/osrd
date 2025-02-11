cd34c83193e7a476831bd0cdbc4be255cd36e64c
Simon Ser <contact@emersion.fr>
2025-02-11 12:00:09 +0100

    front: unexport matchPathStepAndOp()
    Everybody uses pathStepId now.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/modules/pathfinding/utils.ts
+++ front/src/modules/pathfinding/utils.ts
@@ -16,10 +16,7 @@ import { getPointOnTrackCoordinates } from 'utils/geometry';
 
 import getStepLocation from './helpers/getStepLocation';
 
-export const matchPathStepAndOp = (
-  step: PathItemLocation,
-  op: Pick<SuggestedOP, 'opId' | 'uic' | 'ch' | 'trigram' | 'track' | 'offsetOnTrack'>
-) => {
+const matchPathStepAndOp = (step: PathItemLocation, op: SuggestedOP) => {
   if ('operational_point' in step) {
     return step.operational_point === op.opId;
   }
