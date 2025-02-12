3f2d3ceb199356180bf3254fe5ff867964fa982f
Clara Ni <clara.ni@outlook.fr>
2025-02-12 18:10:05 +0100

    front: import utils functions for layers from common/Map/Layers/InfraObjectLayers
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/common/Map/Layers/InfraObjectLayers/index.ts
+++ front/src/common/Map/Layers/InfraObjectLayers/index.ts
@@ -2,6 +2,13 @@ import InfraObjectLayers from './InfraObjectLayers';
 
 export { default as GeoJSONs, EditorSource, SourcesDefinitionsIndex } from './GeoJSONs';
 export { default as OperationalPointsLayer } from './OperationalPoints';
+export {
+  getRoutesLineLayerProps,
+  getRoutesPointLayerProps,
+  getRoutesTextLayerProps,
+} from './Routes';
+export { getSpeedSectionsNameString } from './SpeedLimits';
+export { getSwitchesLayerProps, getSwitchesNameLayerProps } from './Switches';
 export { default as NeutralSectionsLayer } from './extensions/SNCF/NeutralSections';
 
 export default InfraObjectLayers;
