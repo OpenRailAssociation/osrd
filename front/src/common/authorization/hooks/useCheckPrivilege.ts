fdea3b4c56dcfa9e287d43a5f84661d1a865479b
nncluzu <ngamenichaka@yahoo.fr>
2025-02-28 14:43:10 +0100

    front: refactor permission handling and add new hooks for grant and privilege checks
    Signed-off-by: nncluzu <ngamenichaka@yahoo.fr>

---
+++ front/src/common/authorization/hooks/useCheckPrivilege.ts
@@ -0,0 +1,24 @@
+import { type Privilege, type ResourceType } from 'common/api/mock/mockEditoastApi';
+
+import useResourcePermissions from './useResourcePermissions';
+
+export type PrivilegeConfig = {
+  resourceType: ResourceType;
+  resourceId?: number;
+  requiredPrivilege: Privilege;
+};
+
+export const useCheckPrivilege = ({
+  resourceType,
+  resourceId,
+  requiredPrivilege,
+}: PrivilegeConfig) => {
+  const { userGrant, resourceGrants } = useResourcePermissions(resourceType, resourceId);
+  if (userGrant === 'NONE' || !resourceGrants) {
+    return false;
+  }
+
+  return resourceGrants[userGrant].includes(requiredPrivilege);
+};
+
+export default useCheckPrivilege;
---
+++ front/src/common/authorization/hooks/useProtectedActionByPrivilege.ts
@@ -0,0 +1,37 @@
+import { useTranslation } from 'react-i18next';
+
+import { setFailure } from 'reducers/main';
+import { useAppDispatch } from 'store';
+
+import { useCheckPrivilege, type PrivilegeConfig } from './useCheckPrivilege';
+
+export const useProtectedActionByPrivilege = ({
+  resourceType,
+  resourceId,
+  requiredPrivilege,
+}: PrivilegeConfig) => {
+  const { t } = useTranslation('common');
+  const dispatch = useAppDispatch();
+
+  const hasPrivilege = useCheckPrivilege({
+    resourceType,
+    resourceId,
+    requiredPrivilege,
+  });
+
+  return (action: () => void) => {
+    if (hasPrivilege) {
+      action();
+      return;
+    }
+
+    dispatch(
+      setFailure({
+        name: t('authorization.permission'),
+        message: t('authorization.permissionDenied'),
+      })
+    );
+  };
+};
+
+export default useProtectedActionByPrivilege;
