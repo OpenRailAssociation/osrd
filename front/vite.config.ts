8934cbfdb55978d6d4a93530760059a4bf276bc3
Simon Ser <contact@emersion.fr>
2025-03-17 17:28:30 +0100

    front: disable ESLint cache
    Unfortunately, ESLint's cache is pretty bare-bones and doesn't
    invalidate dependent files. For example, if a warning appears in
    foo.ts but is caused by a mistake in bar.ts, fixing bar.ts will not
    cause ESLint to re-check foo.ts and the warning will linger.
    
    I've seen multiple occurences of this happening. Deleting the
    ESLint cache directory and restarting ESLint helps, but it pretty
    cumbersome.
    
    Slow checks are better than invalid/stale warnings, so let's switch
    back to no cache.
    
    See the typescript-eslint docs, which also recommend to disable
    the cache:
    https://typescript-eslint.io/troubleshooting/faqs/eslint/#can-i-use-eslints---cache-with-typescript-eslint
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -25,8 +25,7 @@ export default defineConfig(({ mode }) => {
           buildMode: true,
         },
         eslint: {
-          lintCommand:
-            'eslint --cache --ext .ts,.tsx,.js,.jsx src scripts tests *.ts --max-warnings 0',
+          lintCommand: 'eslint --ext .ts,.tsx,.js,.jsx src scripts tests *.ts --max-warnings 0',
         },
         overlay: env.OSRD_VITE_OVERLAY !== 'false' && {
           initialIsOpen: env.OSRD_VITE_OVERLAY_OPEN_BY_DEFAULT === 'true',
