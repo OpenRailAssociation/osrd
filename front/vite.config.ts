466b754161a8062232c7a89b2d1b89faaa6c1841
Simon Ser <contact@emersion.fr>
2025-03-21 15:50:14 +0100

    front: set vitest directory to src/
    Without this, vitest scans/watches the whole root directory. This
    takes a good while when build/ exists.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -61,7 +61,8 @@ export default defineConfig(({ mode }) => {
     },
     test: {
       globalSetup: './vitest.global-setup.ts',
-      include: ['src/**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      dir: 'src',
+      include: ['**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       environment: 'happy-dom',
       coverage: {
         all: true,
