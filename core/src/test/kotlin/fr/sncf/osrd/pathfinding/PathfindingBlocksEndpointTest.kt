aaf5f485361fb0ab5cd585b1fc1e9266a6740d8d
Erashin <alwenn.charpentier@gmail.com>
2025-04-03 17:27:42 +0200

    core: use tsv2 in PathfindingBlocksEndpointTest.kt
    Signed-off-by: Erashin <alwenn.charpentier@gmail.com>

--- core/src/test/kotlin/fr/sncf/osrd/pathfinding/PathfindingBlocksEndpointTest.kt
+++ core/src/test/kotlin/fr/sncf/osrd/pathfinding/PathfindingBlocksEndpointTest.kt
@@ -1,15 +1,15 @@
 package fr.sncf.osrd.pathfinding
 
 import fr.sncf.osrd.api.FullInfra
-import fr.sncf.osrd.api.pathfinding.findWaypointBlocks
-import fr.sncf.osrd.api.pathfinding.request.PathfindingWaypoint
+import fr.sncf.osrd.api.api_v2.TrackLocation
+import fr.sncf.osrd.api.api_v2.pathfinding.findWaypointBlocks
 import fr.sncf.osrd.graph.Pathfinding.EdgeLocation
 import fr.sncf.osrd.graph.PathfindingEdgeLocationId
-import fr.sncf.osrd.railjson.schema.common.graph.EdgeDirection
 import fr.sncf.osrd.reporting.exceptions.ErrorType
 import fr.sncf.osrd.reporting.exceptions.OSRDError
 import fr.sncf.osrd.sim_infra.api.Block
 import fr.sncf.osrd.sim_infra.api.BlockId
+import fr.sncf.osrd.utils.Direction
 import fr.sncf.osrd.utils.Helpers
 import fr.sncf.osrd.utils.units.Offset
 import fr.sncf.osrd.utils.units.meters
@@ -17,39 +17,31 @@ import java.util.stream.Stream
 import kotlin.test.assertEquals
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PathfindingBlocksEndpointTest {
-    private var smallInfra: FullInfra? = null
-
-    @BeforeAll
-    fun setUp() {
-        smallInfra = Helpers.smallInfra
-    }
+    private var smallInfra: FullInfra = Helpers.smallInfra
 
     @ParameterizedTest
     @MethodSource("testFindWaypointBlocksArgs")
     fun testFindWaypointBlocks(
-        pathfindingWaypoint: PathfindingWaypoint,
+        pathfindingWaypoint: TrackLocation,
+        direction: Direction,
         expectedEdgeLocations: Set<PathfindingEdgeLocationId<Block>>
     ) {
-        val blocks = findWaypointBlocks(smallInfra!!, pathfindingWaypoint)
+        val blocks = findWaypointBlocks(smallInfra, pathfindingWaypoint, direction)
         Assertions.assertThat(blocks).containsExactlyInAnyOrderElementsOf(expectedEdgeLocations)
     }
 
     @Test
     fun findWaypointBlocks_throws_givenIncoherentWaypoint() {
-        val incoherentWaypoint =
-            PathfindingWaypoint("TA3", 100000000.0, EdgeDirection.START_TO_STOP)
+        val incoherentWaypoint = TrackLocation("TA3", Offset(100000000.meters))
         val exception =
             assertThrows(OSRDError::class.java) {
-                findWaypointBlocks(smallInfra!!, incoherentWaypoint)
+                findWaypointBlocks(smallInfra, incoherentWaypoint, Direction.INCREASING)
             }
         assertEquals(ErrorType.InvalidWaypointLocation, exception.osrdErrorType)
     }
@@ -59,11 +51,13 @@ class PathfindingBlocksEndpointTest {
         fun testFindWaypointBlocksArgs(): Stream<Arguments> {
             return Stream.of(
                 Arguments.of(
-                    PathfindingWaypoint("TA3", 10.0, EdgeDirection.START_TO_STOP),
+                    TrackLocation("TA3", Offset(10.meters)),
+                    Direction.INCREASING,
                     mutableSetOf(EdgeLocation(BlockId(8U), Offset<Block>(190.meters)))
                 ),
                 Arguments.of(
-                    PathfindingWaypoint("TA5", 20.0, EdgeDirection.STOP_TO_START),
+                    TrackLocation("TA5", Offset(20.meters)),
+                    Direction.DECREASING,
                     mutableSetOf(
                         EdgeLocation(BlockId(19U), Offset<Block>(210.meters)),
                         EdgeLocation(BlockId(18U), Offset<Block>(210.meters))
