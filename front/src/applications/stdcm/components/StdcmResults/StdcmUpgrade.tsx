4bd3b003343a3f1fdc09f4c63d72d7af6196cc07
Simon Ser <contact@emersion.fr>
2025-04-09 11:00:06 +0200

    front: use customized name for STDCM in "Help us" message
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Closes: https://github.com/OpenRailAssociation/osrd/issues/11327

--- front/src/applications/stdcm/components/StdcmResults/StdcmUpgrade.tsx
+++ front/src/applications/stdcm/components/StdcmResults/StdcmUpgrade.tsx
@@ -2,13 +2,16 @@ import { Button } from '@osrd-project/ui-core';
 import { Comment } from '@osrd-project/ui-icons';
 import { useTranslation } from 'react-i18next';
 
+import useDeploymentSettings from 'utils/hooks/useDeploymentSettings';
+
 const StdcmUpgrade = () => {
   const { t } = useTranslation('stdcm', { keyPrefix: 'simulation.results.upgrade' });
+  const { stdcmName } = useDeploymentSettings() ?? { stdcmName: 'STDCM' };
   return (
     <div className="upgrade">
       <div className="text">
         <div className="title">
-          {t('helpUs')}
+          {t('helpUs', { stdcmName })}
           <div className="comment-icon">
             <Comment />
           </div>
