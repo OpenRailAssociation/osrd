dfbbd8207a5e999d4f72b5f374c6141ae638057f
Clara Ni <clara.ni@outlook.fr>
2025-04-11 18:01:34 +0200

    front: clean upsertMapWaypointInOperationalPoints sample data
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/applications/operationalStudies/__tests__/upsertMapWaypointsInOperationalPoints.spec.ts
+++ front/src/applications/operationalStudies/__tests__/upsertMapWaypointsInOperationalPoints.spec.ts
@@ -1,26 +1,86 @@
 import type { TFunction } from 'i18next';
 import { describe, it, expect } from 'vitest';
 
-import {
-  pathInputsEndingWithTwoWaypointsByMap,
-  pathInputsWithNoMapWaypoint,
-  pathInputsWithOneMapWaypoint,
-  pathInputWithWaypointsByMapOnly,
-  sampleWithMultipleOperationalPoints,
-  sampleWithOneOperationalPoint,
-} from './sampleData';
+import type { PathProperties } from 'common/api/osrdEditoastApi';
+
 import { upsertMapWaypointsInOperationalPoints } from '../helpers/upsertMapWaypointsInOperationalPoints';
 
 const tMock = ((key: string) => key) as TFunction;
 
+type Op = {
+  name: string;
+  uic: number;
+  track: string;
+  positionOnTrack: number;
+  positionOnPath: number;
+};
+
+const getOperationalPoints = (inputs: Op[]): NonNullable<PathProperties['operational_points']> =>
+  inputs.map((op) => ({
+    id: op.name,
+    part: {
+      track: op.track,
+      position: op.positionOnTrack,
+    },
+    extensions: {
+      identifier: {
+        name: op.name,
+        uic: op.uic,
+      },
+    },
+    position: op.positionOnPath,
+    weight: null,
+  }));
+
+const OPERATIONAL_POINTS = getOperationalPoints([
+  {
+    name: 'West_station',
+    uic: 2,
+    track: 'TA1',
+    positionOnTrack: 500,
+    positionOnPath: 0,
+  },
+  {
+    name: 'Mid_West_station',
+    uic: 3,
+    track: 'TC1',
+    positionOnTrack: 550,
+    positionOnPath: 12050000,
+  },
+  {
+    name: 'Mid_East_station',
+    uic: 4,
+    track: 'TD0',
+    positionOnTrack: 14000,
+    positionOnPath: 26500000,
+  },
+]);
+
 describe('upsertMapWaypointsInOperationalPoints', () => {
   it('should add waypoints at the good position in a path with operational points', () => {
+    const pathSteps = [
+      {
+        id: '1',
+        secondary_code: 'BV',
+        uic: 2,
+      },
+      {
+        id: '2',
+        offset: 7746000,
+        track: 'TA6',
+      },
+      {
+        id: '3',
+        secondary_code: 'BV',
+        uic: 4,
+      },
+    ];
     const pathItemPositions = [0, 9246000, 26500000];
 
     const operationalPointsWithAllWaypoints = upsertMapWaypointsInOperationalPoints(
-      pathInputsWithOneMapWaypoint,
+      pathSteps,
       pathItemPositions,
-      sampleWithMultipleOperationalPoints,
+      OPERATIONAL_POINTS,
       tMock
     );
 
@@ -89,12 +149,37 @@ describe('upsertMapWaypointsInOperationalPoints', () => {
   });
 
   it('should add waypoints properly even when the last two come from map clicks', () => {
+    const pathSteps = [
+      {
+        id: '1',
+        offset: 6481000,
+        track: 'TA6',
+      },
+      {
+        id: '2',
+        offset: 679000,
+        track: 'TC0',
+      },
+      {
+        id: '3',
+        offset: 883000,
+        track: 'TC0',
+      },
+    ];
     const pathItemPositions = [0, 4198000, 4402000];
 
     const operationalPointsWithAllWaypoints = upsertMapWaypointsInOperationalPoints(
-      pathInputsEndingWithTwoWaypointsByMap,
+      pathSteps,
       pathItemPositions,
-      sampleWithOneOperationalPoint,
+      getOperationalPoints([
+        {
+          name: 'Mid_West_station',
+          uic: 3,
+          track: 'TC0',
+          positionOnTrack: 550,
+          positionOnPath: 4069000,
+        },
+      ]),
       tMock
     );
 
@@ -163,10 +248,22 @@ describe('upsertMapWaypointsInOperationalPoints', () => {
   });
 
   it('should add waypoints properly when there is no op on path', () => {
+    const pathSteps = [
+      {
+        id: '1',
+        offset: 6481000,
+        track: 'TA6',
+      },
+      {
+        id: '2',
+        offset: 4733000,
+        track: 'TA6',
+      },
+    ];
     const pathItemPositions = [0, 1748000];
 
     const operationalPointsWithAllWaypoints = upsertMapWaypointsInOperationalPoints(
-      pathInputWithWaypointsByMapOnly,
+      pathSteps,
       pathItemPositions,
       [],
       tMock
@@ -207,12 +304,29 @@ describe('upsertMapWaypointsInOperationalPoints', () => {
   });
 
   it('should return the same array if there is no waypoints added by map click', () => {
+    const pathSteps = [
+      {
+        id: '1',
+        secondary_code: 'BV',
+        uic: 2,
+      },
+      {
+        id: '2',
+        secondary_code: 'BV',
+        uic: 3,
+      },
+      {
+        id: '3',
+        secondary_code: 'BV',
+        uic: 4,
+      },
+    ];
     const pathItemPositions = [0, 12050000, 26500000];
 
     const operationalPointsWithAllWaypoints = upsertMapWaypointsInOperationalPoints(
-      pathInputsWithNoMapWaypoint,
+      pathSteps,
       pathItemPositions,
-      sampleWithMultipleOperationalPoints,
+      OPERATIONAL_POINTS,
       tMock
     );
 
