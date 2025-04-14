0442451ed0f290125d0a591c9200ff9b4c06c020
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2025-04-14 09:51:36 +0200

    scripts: update download_timetable script
    Signed-off-by: Eloi Charpentier <eloi.charpentier.42@gmail.com>

--- scripts/download_timetable.py
+++ scripts/download_timetable.py
@@ -16,29 +16,34 @@ TIMETABLE_ID = 1
 OUT_PATH = "timetable.json"
 
 
-def download_timetable(timetable_id: int) -> List[Dict]:
-    response = requests.get(f"{EDITOAST_URL}api/timetable/{timetable_id}/", cookies=COOKIES, verify=False)
-    response.raise_for_status()
-    json_response = response.json()
-    train_ids = json_response["train_ids"]
-    return download_trains(train_ids)
+def get_paginated(url: str, *args, **kwargs) -> List[Dict]:
+    page = 1
+    res = []
+    while page is not None:
+        response = requests.get(url.replace("$page", str(page)), *args, **kwargs)
+        response.raise_for_status()
+        json_response = response.json()
+        res += json_response["results"]
+        page = json_response.get("next", None)
+    return res
 
 
-def download_trains(train_ids: List[int]) -> List[Dict]:
-    payload = {
-        "ids": train_ids
+def download_timetable(timetable_id: int) -> Dict:
+    url = f"{EDITOAST_URL}api/timetable/{timetable_id}/train_schedules/?page=$page"
+    results = get_paginated(url, cookies=COOKIES, verify=False)
+    schedules = []
+    for schedule in results:
+        del schedule["id"]
+        del schedule["timetable_id"]
+        schedules.append(schedule)
+    return {
+        "train_schedules": schedules,
+        "paced_trains": []
     }
-    response = requests.post(f"{EDITOAST_URL}api/train_schedule/", cookies=COOKIES, json=payload, verify=False)
-    response.raise_for_status()
-    json_response = response.json()
-    for path_item in json_response:
-        del path_item["id"]
-        del path_item["timetable_id"]
-    return json_response
 
 
 if __name__ == "__main__":
     trains = download_timetable(TIMETABLE_ID)
     with open(OUT_PATH, "w", encoding="utf-8") as jsonfile:
         json.dump(trains, jsonfile, ensure_ascii=False, indent=4)
-    print(f"dumped timetable {TIMETABLE_ID} ({len(trains)} trains) to {OUT_PATH}")
+    print(f"dumped timetable {TIMETABLE_ID} ({len(trains['train_schedules'])} trains) to {OUT_PATH}")
