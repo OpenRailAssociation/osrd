c721a0afcc8376f98a099829bd6adbca3155e261
Simon Ser <contact@emersion.fr>
2025-04-30 09:16:16 +0200

    front: drop ms2min()
    The Duration type can already do this.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/utils/timeManipulation.ts
+++ front/src/utils/timeManipulation.ts
@@ -16,10 +16,6 @@ export function ms2sec(ms: number) {
   return ms / 1000;
 }
 
-export function ms2min(ms: number) {
-  return ms / 1000 / 60;
-}
-
 export function minToMs(min: number) {
   return min * 60 * 1000;
 }
