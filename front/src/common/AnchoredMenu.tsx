21ed99fc2014a5a26ae37f221ecb2d156831a1c4
SharglutDev <p.filimon75@gmail.com>
2025-05-02 13:36:16 +0200

    front: refacto manchette menu by using a new generic hook
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

---
+++ front/src/common/AnchoredMenu.tsx
@@ -0,0 +1,78 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
+import { createPortal } from 'react-dom';
+
+import useModalFocusTrap from '../utils/hooks/useModalFocusTrap';
+
+type AnchoreMenuParams = {
+  children?: React.ReactNode;
+  anchorRef: React.RefObject<HTMLElement>;
+  onDismiss: () => void;
+};
+
+/**
+ * Creates an overlay on the viewport and displays a menu.
+ *
+ * It takes in the children to be rendered, a reference to the anchor element, and a callback function to dismiss the menu.
+ *
+ * Clicking outside the menu will trigger the onDismiss callback (most of the time to close the menu).
+ *
+ * The focus is trapped inside the menu when it is open, and the first focusable element is focused when the menu opens.
+ *
+ * It handles the space needed by the menu to know if the children should be positioned above or below the anchor element.
+ */
+const AnchoredMenu = ({ children, anchorRef, onDismiss }: AnchoreMenuParams) => {
+  const [menuPosition, setMenuPosition] = useState<{
+    top?: number;
+    left: number;
+    bottom?: number;
+  }>();
+
+  const menuRef = useRef<HTMLDivElement>(null);
+  const shouldDisplayMenu = Boolean(children);
+
+  useLayoutEffect(() => {
+    const anchorRefBoundingRect = anchorRef.current?.getBoundingClientRect();
+    const menuRefBoundingRect = menuRef.current?.getBoundingClientRect();
+
+    if (anchorRefBoundingRect && menuRefBoundingRect) {
+      // Check if there is enough space below the anchor element
+      const isSpaceBelow =
+        window.innerHeight - anchorRefBoundingRect.bottom > menuRefBoundingRect.height;
+
+      setMenuPosition({
+        top: isSpaceBelow ? anchorRefBoundingRect.bottom : undefined,
+        left: anchorRefBoundingRect.left,
+        bottom: isSpaceBelow ? undefined : window.innerHeight - anchorRefBoundingRect.top,
+      });
+    }
+  }, [anchorRef, shouldDisplayMenu]);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) onDismiss();
+  };
+
+  useModalFocusTrap(menuRef, onDismiss, { focusOnFirstElement: true });
+
+  if (!shouldDisplayMenu) return null;
+
+  return createPortal(
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div className="menu-overlay" onClick={handleClick}>
+      <div
+        style={{
+          top: menuPosition?.top,
+          left: menuPosition?.left,
+          bottom: menuPosition?.bottom,
+          position: 'fixed',
+        }}
+        ref={menuRef}
+      >
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default AnchoredMenu;
