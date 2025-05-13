d79e5d684b523363a2a9180b09f7a617ec761dd4
Erashin <alwenn.charpentier@gmail.com>
2025-05-13 10:21:49 +0200

    core: rename etcs classes, objects and move methods around
    Signed-off-by: Erashin <alwenn.charpentier@gmail.com>

--- core/envelope-sim/src/main/kotlin/fr/sncf/osrd/envelope_sim/etcs/ETCSBrakingSimulator.kt
+++ core/envelope-sim/src/main/kotlin/fr/sncf/osrd/envelope_sim/etcs/ETCSBrakingSimulator.kt
@@ -1,9 +1,12 @@
 package fr.sncf.osrd.envelope_sim.etcs
 
 import fr.sncf.osrd.envelope.Envelope
+import fr.sncf.osrd.envelope.OverlayEnvelopeBuilder
 import fr.sncf.osrd.envelope_sim.EnvelopeSimContext
+import fr.sncf.osrd.envelope_sim.etcs.BrakingType.IND
 import fr.sncf.osrd.sim_infra.api.TravelledPath
 import fr.sncf.osrd.utils.units.Offset
+import java.util.*
 
 /**
  * In charge of computing and adding the ETCS braking curves. Formulas are found in `SUBSET-026-3
@@ -35,30 +38,51 @@ interface ETCSBrakingSimulator {
     fun computeSlowdownBrakingCurves(
         mrsp: Envelope,
         limitsOfAuthority: Collection<LimitOfAuthority>
-    ): List<Map<BrakingCurveType, ETCSBrakingCurve?>>
+    ): LOABrakingCurves
 
     /** Compute the ETCS braking curves for each EoA, ordered by EoA offset. */
     fun computeStopBrakingCurves(
         mrsp: Envelope,
         endsOfAuthority: Collection<EndOfAuthority>
-    ): List<Map<BrakingCurveType, ETCSBrakingCurve?>>
+    ): EOABrakingCurves
 }
+
+typealias BrakingCurves = EnumMap<BrakingType, BrakingCurve?>
+
+typealias LOABrakingCurves = NavigableMap<LimitOfAuthority, BrakingCurves>
+
+typealias EOABrakingCurves = NavigableMap<EndOfAuthority, BrakingCurves>
+
+data class BrakingCurve(val brakingType: BrakingType, val brakingCurve: Envelope)
 
 data class LimitOfAuthority(
     val offset: Offset<TravelledPath>,
     val speed: Double,
-) {
+) : Comparable<LimitOfAuthority> {
     init {
         assert(speed > 0)
+    }
+
+    override fun compareTo(other: LimitOfAuthority): Int {
+        if (offset != other.offset) return offset.compareTo(other.offset)
+        return speed.compareTo(other.speed)
     }
 }
 
 data class EndOfAuthority(
     val offsetEOA: Offset<TravelledPath>,
     val offsetSVL: Offset<TravelledPath>?,
-) {
+) : Comparable<EndOfAuthority> {
     init {
         if (offsetSVL != null) assert(offsetSVL >= offsetEOA)
+    }
+
+    override fun compareTo(other: EndOfAuthority): Int {
+        if (offsetEOA != other.offsetEOA) return offsetEOA.compareTo(other.offsetEOA)
+        if (offsetSVL == null && other.offsetSVL == null) return 0
+        if (offsetSVL == null) return -1
+        if (other.offsetSVL == null) return 1
+        return offsetSVL.compareTo(other.offsetSVL)
     }
 }
 
@@ -67,27 +91,72 @@ class ETCSBrakingSimulatorImpl(override val context: EnvelopeSimContext) : ETCSB
         envelope: Envelope,
         limitsOfAuthority: Collection<LimitOfAuthority>
     ): Envelope {
-        return addBrakingCurvesAtLOAs(envelope, context, limitsOfAuthority)
+        val sortedLimitsOfAuthority = limitsOfAuthority.sorted()
+        val beginPos = envelope.beginPos
+        var envelopeWithLoaBrakingCurves = envelope
+        var builder = OverlayEnvelopeBuilder.forward(envelopeWithLoaBrakingCurves)
+
+        for (limitOfAuthority in sortedLimitsOfAuthority) {
+            val ebdBrakingCurves =
+                computeBrakingCurvesAtLOA(
+                    limitOfAuthority,
+                    context,
+                    envelopeWithLoaBrakingCurves,
+                    beginPos
+                )
+            val indicationCurve = ebdBrakingCurves[IND] ?: continue
+            indicationCurve.brakingCurve.stream().forEach { builder.addPart(it) }
+
+            // We build the LOAs along the path, and they don't all have the same target speeds. To
+            // handle intersections with the next LOA, it is needed to add this LOA braking curve to
+            // the
+            // overlay builder that will be used to compute the following LOAs.
+            envelopeWithLoaBrakingCurves = builder.build()
+            builder = OverlayEnvelopeBuilder.forward(envelopeWithLoaBrakingCurves)
+        }
+        return envelopeWithLoaBrakingCurves
     }
 
     override fun addStopBrakingCurves(
         envelope: Envelope,
         endsOfAuthority: Collection<EndOfAuthority>
     ): Envelope {
-        return addBrakingCurvesAtEOAs(envelope, context, endsOfAuthority)
+        val sortedEndsOfAuthority = endsOfAuthority.sorted()
+        var beginPos = envelope.beginPos
+        val builder = OverlayEnvelopeBuilder.forward(envelope)
+        for (endOfAuthority in sortedEndsOfAuthority) {
+            val eoaBrakingCurves =
+                computeBrakingCurvesAtEOA(endOfAuthority, context, envelope, beginPos)
+            val indicationCurve = eoaBrakingCurves[IND] ?: continue
+            indicationCurve.brakingCurve.stream().forEach { builder.addPart(it) }
+
+            // We build EOAs along the path. We need to handle overlaps with the next EOA. To do so,
+            // we
+            // shift the left position constraint, beginPos, to this EOA's target position.
+            beginPos = endOfAuthority.offsetEOA.distance.meters
+        }
+        return builder.build()
     }
 
     override fun computeSlowdownBrakingCurves(
         mrsp: Envelope,
         limitsOfAuthority: Collection<LimitOfAuthority>
-    ): List<Map<BrakingCurveType, ETCSBrakingCurve?>> {
-        return computeBrakingCurvesAtLOAs(mrsp, context, limitsOfAuthority)
+    ): LOABrakingCurves {
+        val res: LOABrakingCurves = TreeMap()
+        for (limitOfAuthority in limitsOfAuthority) {
+            res[limitOfAuthority] = computeBrakingCurvesAtLOA(limitOfAuthority, context, mrsp, 0.0)
+        }
+        return res
     }
 
     override fun computeStopBrakingCurves(
         mrsp: Envelope,
         endsOfAuthority: Collection<EndOfAuthority>
-    ): List<Map<BrakingCurveType, ETCSBrakingCurve?>> {
-        return computeBrakingCurvesAtEOAs(mrsp, context, endsOfAuthority)
+    ): EOABrakingCurves {
+        val res: EOABrakingCurves = TreeMap()
+        for (endOfAuthority in endsOfAuthority) {
+            res[endOfAuthority] = computeBrakingCurvesAtEOA(endOfAuthority, context, mrsp, 0.0)
+        }
+        return res
     }
 }
