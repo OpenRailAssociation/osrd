a7fee364a5e92b84670f3d88021e94e9658ea1b3
Egor Berezovskiy <egor@berezify.fr>
2025-05-28 11:11:26 +0200

    editoast: paced train path and simulation implementation
    Signed-off-by: Egor Berezovskiy <egor@berezify.fr>

--- editoast/src/models/paced_train.rs
+++ editoast/src/models/paced_train.rs
@@ -57,6 +57,54 @@ pub struct PacedTrain {
 }
 
 impl PacedTrain {
+    pub fn apply_exceptions(&self, exceptions: &[PacedTrainException]) -> Vec<TrainSchedule> {
+        exceptions
+            .iter()
+            .map(|exception| self.apply_exception(exception))
+            .collect()
+    }
+
+    pub fn apply_exception(&self, exception: &PacedTrainException) -> TrainSchedule {
+        let mut train_schedule = self.clone().into_first_occurrence();
+
+        if let Some(change_group) = &exception.train_name {
+            train_schedule.train_name = change_group.value.clone();
+        }
+        if let Some(change_group) = &exception.rolling_stock {
+            train_schedule.comfort = change_group.comfort;
+            train_schedule.rolling_stock_name = change_group.rolling_stock_name.clone();
+        }
+        if let Some(change_group) = &exception.rolling_stock_category {
+            train_schedule.category = change_group.value.clone().map(TrainCategory);
+        }
+        if let Some(change_group) = &exception.labels {
+            train_schedule.labels = change_group.value.iter().cloned().map(Some).collect();
+        }
+        if let Some(change_group) = &exception.speed_limit_tag {
+            train_schedule.speed_limit_tag = change_group.value.clone().map(|value| value.0);
+        }
+        if let Some(change_group) = &exception.start_time {
+            train_schedule.start_time = change_group.value
+        }
+        if let Some(change_group) = &exception.constraint_distribution {
+            train_schedule.constraint_distribution = change_group.value;
+        }
+        if let Some(change_group) = &exception.initial_speed {
+            train_schedule.initial_speed = change_group.value;
+        }
+        if let Some(change_group) = &exception.options {
+            train_schedule.options = change_group.value.clone();
+        }
+        if let Some(change_group) = &exception.path_and_schedule {
+            train_schedule.margins = change_group.margins.clone();
+            train_schedule.path = change_group.path.clone();
+            train_schedule.power_restrictions = change_group.power_restrictions.clone();
+            train_schedule.schedule = change_group.schedule.clone();
+        }
+
+        train_schedule
+    }
+
     pub fn into_first_occurrence(self) -> TrainSchedule {
         TrainSchedule {
             id: self.id,
@@ -146,5 +194,134 @@ impl From<PacedTrain> for paced_train::PacedTrain {
                 interval: paced_train.interval.try_into().unwrap(),
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use chrono::DateTime;
+    use chrono::Utc;
+    use editoast_models::rolling_stock::TrainCategory;
+    use editoast_schemas::paced_train::PacedTrainException;
+    use editoast_schemas::train_schedule::Comfort;
+    use editoast_schemas::train_schedule::Distribution;
+    use editoast_schemas::train_schedule::Margins;
+    use editoast_schemas::train_schedule::TrainScheduleOptions;
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
+    use crate::models::PacedTrain;
+    use crate::models::Tags;
+    use crate::models::fixtures::create_created_exception_with_change_groups;
+    use crate::models::fixtures::create_modified_exception_with_change_groups;
+
+    pub fn create_paced_train(exceptions: Vec<PacedTrainException>) -> PacedTrain {
+        PacedTrain {
+            id: 1,
+            timetable_id: 1,
+            train_name: "train_name".to_string(),
+            rolling_stock_name: "R2D2".to_string(),
+            comfort: Comfort::Standard,
+            initial_speed: 25.0,
+            category: Some(TrainCategory(
+                editoast_schemas::rolling_stock::TrainCategory::HighSpeedTrain,
+            )),
+            constraint_distribution: Distribution::Standard,
+            labels: Tags::new(vec![]),
+            margins: Margins {
+                boundaries: vec![Default::default()],
+                ..Default::default()
+            },
+            path: vec![],
+            power_restrictions: vec![],
+            schedule: vec![],
+            speed_limit_tag: None,
+            options: TrainScheduleOptions::default(),
+            start_time: DateTime::<Utc>::from_str("2024-09-17T20:05:00+02:00").unwrap(),
+            time_window: chrono::Duration::try_hours(2).unwrap(),
+            interval: chrono::Duration::try_minutes(30).unwrap(),
+            exceptions,
+        }
+    }
+
+    #[rstest]
+    #[case::created(create_created_exception_with_change_groups("key_1"))]
+    #[case::modified(create_modified_exception_with_change_groups("key_2", 0))]
+    async fn paced_train_apply_exception(#[case] exception: PacedTrainException) {
+        let paced_train = create_paced_train(vec![exception.clone()]);
+        let paced_train_exception = paced_train.apply_exception(&exception);
+
+        assert_eq!(
+            paced_train_exception.train_name,
+            exception.train_name.unwrap().value
+        );
+        assert_eq!(
+            paced_train_exception.rolling_stock_name,
+            exception.rolling_stock.clone().unwrap().rolling_stock_name
+        );
+        assert_eq!(
+            paced_train_exception.comfort,
+            exception.rolling_stock.unwrap().comfort
+        );
+        assert_eq!(
+            paced_train_exception.initial_speed,
+            exception.initial_speed.unwrap().value
+        );
+        assert_eq!(
+            paced_train_exception.category,
+            exception
+                .rolling_stock_category
+                .unwrap()
+                .value
+                .map(TrainCategory)
+        );
+        assert_eq!(
+            paced_train_exception.constraint_distribution,
+            exception.constraint_distribution.unwrap().value
+        );
+        assert_eq!(
+            paced_train_exception.labels,
+            exception
+                .labels
+                .unwrap()
+                .value
+                .into_iter()
+                .map(Some)
+                .collect::<Vec<Option<String>>>()
+        );
+        assert_eq!(
+            paced_train_exception.margins,
+            exception.path_and_schedule.clone().unwrap().margins
+        );
+        assert_eq!(
+            paced_train_exception.path,
+            exception.path_and_schedule.clone().unwrap().path
+        );
+        assert_eq!(
+            paced_train_exception.power_restrictions,
+            exception
+                .path_and_schedule
+                .clone()
+                .unwrap()
+                .power_restrictions
+        );
+        assert_eq!(
+            paced_train_exception.schedule,
+            exception.path_and_schedule.clone().unwrap().schedule
+        );
+        assert_eq!(
+            paced_train_exception.speed_limit_tag,
+            exception
+                .speed_limit_tag
+                .unwrap()
+                .value
+                .map(|v| v.to_string())
+        );
+        assert_eq!(
+            paced_train_exception.options,
+            exception.options.unwrap().value
+        );
     }
 }
