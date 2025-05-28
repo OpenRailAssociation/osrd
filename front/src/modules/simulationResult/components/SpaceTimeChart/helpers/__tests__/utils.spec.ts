f6f7c696146cdf44927d2d0ba76d7c5c2795f604
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-05-28 13:14:48 +0200

    front: adds batchFetchTrackOccupancy utils
    This utils will ease implementing batch loading the occupancy zones in
    an upcoming commit.
    
    Details:
    - Adds new helper batchFetchTrackOccupancy
    - Adds various unit tests for it
    
    Signed-off-by: Alexis Jacomy <alexis.jacomy@gmail.com>

--- front/src/modules/simulationResult/components/SpaceTimeChart/helpers/__tests__/utils.spec.ts
+++ front/src/modules/simulationResult/components/SpaceTimeChart/helpers/__tests__/utils.spec.ts
@@ -1,6 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 
-import { cutSpaceTimeRect } from '../utils';
+import { cutSpaceTimeRect, batchFetchTrackOccupancy } from '../utils';
 
 describe('interpolateRange', () => {
   it('should return null if the interpolated range ends before the cut space', () => {
@@ -82,5 +82,88 @@ describe('interpolateRange', () => {
       timeStart: 120,
       timeEnd: 140,
     });
+  });
+});
+
+describe('batchFetch', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should fetch sequentially', async () => {
+    const fetchSpy = vi.fn().mockResolvedValueOnce(['a', 'b']).mockResolvedValueOnce(['c', 'd']);
+
+    const onComplete = vi.fn();
+    batchFetchTrackOccupancy(['1', '2', '3', '4'], fetchSpy, { batchSize: 2, onComplete });
+
+    await vi.waitFor(() => expect(onComplete).toHaveBeenCalled());
+
+    expect(fetchSpy).toHaveBeenCalledTimes(2);
+    expect(fetchSpy).toHaveBeenNthCalledWith(1, ['1', '2']);
+    expect(fetchSpy).toHaveBeenNthCalledWith(2, ['3', '4']);
+    expect(onComplete).toHaveBeenCalledWith(['a', 'b', 'c', 'd']);
+  });
+
+  it('should stop fetching once the abort function is called', async () => {
+    const fetchSpy = vi.fn().mockImplementation(
+      () =>
+        new Promise((resolve) => {
+          requestAnimationFrame(() => {
+            resolve(['a', 'b']);
+          });
+        })
+    );
+    const onComplete = vi.fn();
+
+    const abort = batchFetchTrackOccupancy(['1', '2', '3', '4'], fetchSpy, {
+      batchSize: 2,
+      onComplete,
+    });
+
+    // Abort after first batch
+    await vi.waitFor(() => expect(fetchSpy).toHaveBeenCalledTimes(1));
+    abort();
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 50);
+    });
+
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+    expect(onComplete).not.toHaveBeenCalled();
+  });
+
+  it('should call onProgress after each batch, and onComplete on the end', async () => {
+    const fetchSpy = vi.fn().mockResolvedValueOnce(['a', 'b']).mockResolvedValueOnce(['c', 'd']);
+
+    const onProgress = vi.fn();
+    const onComplete = vi.fn();
+
+    batchFetchTrackOccupancy(['1', '2', '3', '4'], fetchSpy, {
+      batchSize: 2,
+      onProgress,
+      onComplete,
+    });
+
+    await vi.waitFor(() => expect(onComplete).toHaveBeenCalled());
+
+    expect(onProgress).toHaveBeenCalledTimes(2);
+    expect(onProgress).toHaveBeenNthCalledWith(1, ['a', 'b']);
+    expect(onProgress).toHaveBeenNthCalledWith(2, ['a', 'b', 'c', 'd']);
+    expect(onComplete).toHaveBeenCalledWith(['a', 'b', 'c', 'd']);
+  });
+
+  it('should call onError (and not call onComplete) when an error occurs', async () => {
+    const error = new Error('Fetch failed');
+    const fetchSpy = vi.fn().mockRejectedValue(error);
+
+    const onError = vi.fn();
+    const onComplete = vi.fn();
+
+    batchFetchTrackOccupancy(['1', '2'], fetchSpy, { onError, onComplete });
+
+    await vi.waitFor(() => expect(onError).toHaveBeenCalled());
+
+    expect(onError).toHaveBeenCalledWith(error);
+    expect(onComplete).not.toHaveBeenCalled();
   });
 });
