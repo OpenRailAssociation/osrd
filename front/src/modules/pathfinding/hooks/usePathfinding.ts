c1004b4b30a4b21d93b239b0f793b746b058fa04
Alice Khoudli <alice.khoudli@polytechnique.org>
2025-06-10 13:06:16 +0200

    front: fetch path step names when pathfinding fails
    Signed-off-by: Alice Khoudli <alice.khoudli@polytechnique.org>

--- front/src/modules/pathfinding/hooks/usePathfinding.ts
+++ front/src/modules/pathfinding/hooks/usePathfinding.ts
@@ -68,7 +68,16 @@ const usePathfinding = ({
 
   const [postSearch] = osrdEditoastApi.endpoints.postSearch.useMutation();
 
-  const fetchOperationalPoints = useCallback(
+  /**
+   * Fetches operational point data for the given path steps using a search API call
+   * and updates each step with the matched operational point's name (if found).
+   *
+   * This is useful when the pathfinding can not be ran or fails.
+   * If pathfinding is successful, calling this function is unnecessary.
+   *
+   * @param steps - An array of `PathStep` objects to be enriched with operational point data.
+   */
+  const fetchPathStepsOperationalPointNames = useCallback(
     async (steps: PathStep[]) => {
       if (!infraId || !steps.length) return;
 
@@ -264,7 +273,7 @@ const usePathfinding = ({
 
       if (!pathfindingInput) {
         setIsMissingParam();
-        await fetchOperationalPoints(pathSteps.filter((step) => step !== null));
+        await fetchPathStepsOperationalPointNames(steps.filter((step) => step !== null));
         return;
       }
 
@@ -276,6 +285,8 @@ const usePathfinding = ({
           setIsDone();
           return;
         }
+
+        await fetchPathStepsOperationalPointNames(steps.filter((step) => step !== null));
 
         const incompatibleConstraintsCheck =
           pathfindingResult.failed_status === 'pathfinding_not_found' &&
