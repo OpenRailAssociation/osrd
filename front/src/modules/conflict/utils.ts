af04efb7aa56a7357c061e8ede551bdc1517bdd6
theocrsb <theo_crosbie@yahoo.fr>
2025-06-11 16:34:22 +0200

    front: take into account the refacto of conflicts
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- front/src/modules/conflict/utils.ts
+++ front/src/modules/conflict/utils.ts
@@ -2,42 +2,73 @@ import type { Conflict } from 'common/api/osrdEditoastApi';
 import type { TimetableItemWithDetails } from 'modules/trainschedule/components/Timetable/types';
 import computeOccurrenceName from 'modules/trainschedule/helpers/computeOccurrenceName';
 import type { TimetableItemId } from 'reducers/osrdconf/types';
-import { formatEditoastIdToTrainScheduleId, formatEditoastIdToPacedTrainId } from 'utils/trainId';
+import {
+  formatEditoastIdToTrainScheduleId,
+  formatEditoastIdToPacedTrainId,
+  isPacedTrainWithDetails,
+} from 'utils/trainId';
 
 import type { ConflictWithTrainNames } from './types';
 
 function getConflictTrainNames(
   conflict: Conflict,
-  trainNameMap: Map<TimetableItemId, string>
+  trainMap: Map<TimetableItemId, TimetableItemWithDetails>
 ): string[] {
-  const trainScheduleNames = conflict.train_schedule_ids.map((id) =>
-    trainNameMap.get(formatEditoastIdToTrainScheduleId(id))
+  const trainScheduleNames = conflict.train_schedule_ids.map(
+    (id) => trainMap.get(formatEditoastIdToTrainScheduleId(id))?.name
   );
+
   const occurrenceNames = conflict.paced_train_occurrence_ids.map((occurrence) => {
-    const pacedTrainName = trainNameMap.get(
-      formatEditoastIdToPacedTrainId(occurrence.paced_train_id)
-    );
-    if (!pacedTrainName) return undefined;
-    if ('index' in occurrence) {
-      return computeOccurrenceName(pacedTrainName, occurrence.index);
+    const pacedTrain = trainMap.get(formatEditoastIdToPacedTrainId(occurrence.paced_train_id));
+    if (!pacedTrain || !isPacedTrainWithDetails(pacedTrain)) return undefined;
+
+    if (!('exception_key' in occurrence)) {
+      // Standard occurrence
+      return computeOccurrenceName(pacedTrain.name, occurrence.index);
     }
-    // TODO handle paced train created exceptions
-    return undefined;
+
+    if ('index' in occurrence) {
+      // Updated exception
+      // Check if the exception has a name change group
+      // Otherwise, compute the occurrence name
+      const namedException = pacedTrain.exceptions.find(
+        (exception) => exception.occurrence_index === occurrence.index && exception.train_name
+      );
+      if (namedException) {
+        return namedException.train_name!.value;
+      }
+      return computeOccurrenceName(pacedTrain.name, occurrence.index);
+    }
+
+    // Added exception
+    // Check if the exception has a name change group
+    // Otherwise, the name is `${pacedTrainName}/+`
+
+    const namedException = pacedTrain.exceptions.find(
+      (exception) => exception.key === occurrence.exception_key && exception.train_name
+    );
+    if (namedException) {
+      return namedException.train_name!.value;
+    }
+    return `${pacedTrain.name}/+`;
   });
-  return [...trainScheduleNames, ...occurrenceNames].filter((name) => name !== undefined);
+
+  const trainNames = [...trainScheduleNames, ...occurrenceNames];
+  return trainNames.filter((name): name is string => name !== undefined);
 }
 
 export default function addTrainNamesToConflicts(
   conflicts: Conflict[],
   timetableItems: TimetableItemWithDetails[]
 ): ConflictWithTrainNames[] {
-  const trainNameMap: Map<TimetableItemId, string> = new Map();
+  const trainMap: Map<TimetableItemId, TimetableItemWithDetails> = new Map();
+
   for (const timetableItem of timetableItems) {
-    trainNameMap.set(timetableItem.id, timetableItem.name);
+    trainMap.set(timetableItem.id, timetableItem);
   }
 
   return conflicts.map((conflict) => ({
     ...conflict,
-    trainNames: getConflictTrainNames(conflict, trainNameMap),
+    trainNames: getConflictTrainNames(conflict, trainMap),
   }));
 }
