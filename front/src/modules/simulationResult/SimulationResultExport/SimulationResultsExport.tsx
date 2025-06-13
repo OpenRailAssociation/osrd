33ad4ef8bd88aaa02193699c751f2416d43dd6fa
Clara Ni <clara.ni@outlook.fr>
2025-06-13 11:14:36 +0200

    front: create exportTrainPDF function
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/modules/simulationResult/SimulationResultExport/SimulationResultsExport.tsx
+++ front/src/modules/simulationResult/SimulationResultExport/SimulationResultsExport.tsx
@@ -1,11 +1,10 @@
-import { useCallback, useMemo } from 'react';
-
 import { Button } from '@osrd-project/ui-core';
 import { Download, File } from '@osrd-project/ui-icons';
 import { pdf } from '@react-pdf/renderer';
 import { useTranslation } from 'react-i18next';
 
 import type {
+  OperationalPointWithTimeAndSpeed,
   PathPropertiesFormatted,
   SimulationResponseSuccess,
 } from 'applications/operationalStudies/types';
@@ -17,8 +16,39 @@ import type { TimetableItem } from 'reducers/osrdconf/types';
 
 import exportTrainCSV from './exportTrainCSV';
 import SimulationReportSheetScenario from './SimulationReportSheetScenario';
-import type { SimulationSheetData } from './types';
 import { useFormattedOperationalPoints } from '../hooks/useFormattedOperationalPoints';
+
+const exportTrainPDF = async (
+  path: PathfindingResultSuccess,
+  scenarioData: { name: string; infraName: string },
+  timetableItem: TimetableItem,
+  simulation: SimulationResponseSuccess,
+  rollingStock: RollingStockWithLiveries,
+  operationalPoints: OperationalPointWithTimeAndSpeed[],
+  mapCanvas?: string
+) => {
+  const doc = (
+    <SimulationReportSheetScenario
+      path={path}
+      scenarioData={scenarioData}
+      trainData={{
+        trainName: timetableItem.train_name,
+        departure_time: '',
+        simulation,
+        creationDate: new Date(),
+        rollingStock,
+        speedLimitByTag: timetableItem.speed_limit_tag,
+      }}
+      operationalPointsList={operationalPoints}
+      mapCanvas={mapCanvas}
+    />
+  );
+
+  const blob = await pdf(doc).toBlob();
+  const url = URL.createObjectURL(blob);
+  window.open(url, '_blank');
+  URL.revokeObjectURL(url);
+};
 
 type SimulationResultExportProps = {
   path: PathfindingResultSuccess;
@@ -47,46 +77,26 @@ const SimulationResultExport = ({
     pathProperties
   );
 
-  const simulationSheetData: SimulationSheetData = useMemo(
-    () => ({
-      trainName: timetableItem.train_name,
-      departure_time: '',
-      simulation,
-      creationDate: new Date(),
-      rollingStock,
-      speedLimitByTag: timetableItem.speed_limit_tag,
-    }),
-    [simulation]
-  );
-
-  const exportTrainPDF = useCallback(async () => {
-    const doc = (
-      <SimulationReportSheetScenario
-        path={path}
-        scenarioData={scenarioData}
-        trainData={simulationSheetData}
-        operationalPointsList={operationalPoints}
-        mapCanvas={mapCanvas}
-      />
-    );
-    const blob = await pdf(doc).toBlob();
-    const url = URL.createObjectURL(blob);
-    window.open(url, '_blank');
-    URL.revokeObjectURL(url);
-  }, [path, scenarioData, simulationSheetData, operationalPoints, mapCanvas]);
-
   return (
     <div className="simulation-sheet-container">
-      {/* Export simulation PDF */}
       <Button
-        onClick={exportTrainPDF}
+        onClick={() =>
+          exportTrainPDF(
+            path,
+            scenarioData,
+            timetableItem,
+            simulation,
+            rollingStock,
+            operationalPoints,
+            mapCanvas
+          )
+        }
         variant="Quiet"
         label={t('simulationResults.simulationSheet')}
         size="medium"
         leadingIcon={<File />}
       />
 
-      {/* Export simulation CSV */}
       <Button
         onClick={() =>
           exportTrainCSV(
