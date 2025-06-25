4d619f5b2cfdce2dc0d689a48939175a7517c15d
SharglutDev <p.filimon75@gmail.com>
2025-06-25 17:23:39 +0200

    front: remove useEffect from useOccurrences
    Since we no longer make any api call in the hook, the logic is not async
    anymore so we don't need the useEffect and useState, we can switch back to a
    useMemo.
    
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/src/modules/trainschedule/components/Timetable/PacedTrain/hooks/useOccurrences.ts
+++ front/src/modules/trainschedule/components/Timetable/PacedTrain/hooks/useOccurrences.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 import dayjs from 'dayjs';
 import { omit, sortBy } from 'lodash';
@@ -37,118 +37,115 @@ const useOccurrences = (
     category: pacedTrainCategory,
   } = pacedTrain;
 
-  const [occurrences, setOccurrences] = useState<Occurrence[]>([]);
   const occurrencesCount = getOccurrencesNb(paced);
 
-  // Add to the count the added exceptions and substract the disabled ones
-  const occurrenceCountLabel = exceptions.reduce((acc, exception) => {
-    if (exception.disabled) acc -= 1;
-    return acc;
-  }, occurrences.length);
+  const occurrences = useMemo(() => {
+    const computedOccurrences: Occurrence[] = [];
 
-  useEffect(() => {
-    const buildOccurrences = async () => {
-      const computedOccurrences: Occurrence[] = [];
+    // Handle indexed occurrences
+    for (let i = 0; i < occurrencesCount; i += 1) {
+      const occurrenceId = formatPacedTrainIdToIndexedOccurrenceId(id, i);
+      const occurrenceStartTime = dayjs(startTime)
+        .add(i * paced.interval.ms, 'ms')
+        .toDate();
+      const occurrenceArrivalTime = dayjs(arrivalTime)
+        .add(i * paced.interval.ms, 'ms')
+        .toDate();
 
-      // Handle indexed occurrences
-      for (let i = 0; i < occurrencesCount; i += 1) {
-        const occurrenceId = formatPacedTrainIdToIndexedOccurrenceId(id, i);
-        const occurrenceStartTime = dayjs(startTime)
-          .add(i * paced.interval.ms, 'ms')
-          .toDate();
-        const occurrenceArrivalTime = dayjs(arrivalTime)
-          .add(i * paced.interval.ms, 'ms')
-          .toDate();
+      const correspondingException = findExceptionWithOccurrenceId(exceptions, occurrenceId);
 
-        const correspondingException = findExceptionWithOccurrenceId(exceptions, occurrenceId);
-
-        let occurrenceRollingStock = rollingStock;
-        if (correspondingException?.rolling_stock && rollingStockList) {
-          const rollingStockName = correspondingException.rolling_stock.rolling_stock_name;
-          occurrenceRollingStock = rollingStockList.find((rs) => rs.name === rollingStockName);
-        }
-
-        computedOccurrences.push({
-          id: occurrenceId,
-          trainName: correspondingException?.train_name?.value ?? computeOccurrenceName(name, i),
-          rollingStock: occurrenceRollingStock,
-          startTime: correspondingException?.start_time?.value
-            ? new Date(correspondingException.start_time.value)
-            : occurrenceStartTime,
-          stopsCount: correspondingException?.path_and_schedule
-            ? correspondingException.path_and_schedule.schedule.filter((step) => step.stop_for)
-                .length
-            : stopsCount,
-          disabled: correspondingException?.disabled,
-          // In the model, we can currently have a null category value so we need to handle this case
-          category: correspondingException?.rolling_stock_category
-            ? correspondingException.rolling_stock_category.value
-            : pacedTrainCategory,
-          occurrenceIndex: i,
-          exceptionChangeGroups: correspondingException
-            ? omit(correspondingException, ['key', 'occurrence_index', 'disabled'])
-            : undefined,
-          ...(isValid
-            ? {
-                isValid: true,
-                // TODO exceptions : update the arrival time if the exception is in the paced train summaries
-                arrivalTime: occurrenceArrivalTime,
-                pathLength,
-                mechanicalEnergyConsumed,
-                duration,
-              }
-            : {
-                isValid: false,
-                invalidReason,
-              }),
-        });
+      let occurrenceRollingStock = rollingStock;
+      if (correspondingException?.rolling_stock && rollingStockList) {
+        const rollingStockName = correspondingException.rolling_stock.rolling_stock_name;
+        occurrenceRollingStock = rollingStockList.find((rs) => rs.name === rollingStockName);
       }
 
-      // Handle added exceptions
-      exceptions.forEach((exception) => {
-        if (exception.occurrence_index !== undefined) return;
-
-        let occurrenceRollingStock = rollingStock;
-        if (exception.rolling_stock && rollingStockList) {
-          const rollingStockName = exception.rolling_stock.rolling_stock_name;
-          occurrenceRollingStock = rollingStockList.find((rs) => rs.name === rollingStockName);
-        }
-
-        computedOccurrences.push({
-          id: formatPacedTrainIdToExceptionId(id, exception.key),
-          trainName: exception.train_name?.value ?? `${name}/+`,
-          rollingStock: occurrenceRollingStock,
-          // An added exception will always have a least a start time in its exceptions
-          startTime: new Date(exception.start_time!.value),
-          stopsCount: exception.path_and_schedule
-            ? exception.path_and_schedule.schedule.filter((step) => step.stop_for).length
-            : stopsCount,
-          // In the model, we can currently have a null category value so we need to handle this case
-          category: exception.rolling_stock_category
-            ? exception.rolling_stock_category.value
-            : pacedTrainCategory,
-          exceptionChangeGroups: omit(exception, ['key', 'disabled', 'occurrence_index']),
-          ...(isValid
-            ? {
-                isValid: true,
-                // TODO exceptions : update the arrival time if the exception is in the paced train summaries
-                arrivalTime: new Date(Date.parse(exception.start_time!.value) + duration!.ms),
-                pathLength,
-                mechanicalEnergyConsumed,
-                duration,
-              }
-            : {
-                isValid: false,
-              }),
-        });
+      computedOccurrences.push({
+        id: occurrenceId,
+        trainName: correspondingException?.train_name?.value ?? computeOccurrenceName(name, i),
+        rollingStock: occurrenceRollingStock,
+        startTime: correspondingException?.start_time?.value
+          ? new Date(correspondingException.start_time.value)
+          : occurrenceStartTime,
+        stopsCount: correspondingException?.path_and_schedule
+          ? correspondingException.path_and_schedule.schedule.filter((step) => step.stop_for).length
+          : stopsCount,
+        disabled: correspondingException?.disabled,
+        // In the model, we can currently have a null category value so we need to handle this case
+        category: correspondingException?.rolling_stock_category
+          ? correspondingException.rolling_stock_category.value
+          : pacedTrainCategory,
+        occurrenceIndex: i,
+        exceptionChangeGroups: correspondingException
+          ? omit(correspondingException, ['key', 'occurrence_index', 'disabled'])
+          : undefined,
+        ...(isValid
+          ? {
+              isValid: true,
+              // TODO exceptions : update the arrival time if the exception is in the paced train summaries
+              arrivalTime: occurrenceArrivalTime,
+              pathLength,
+              mechanicalEnergyConsumed,
+              duration,
+            }
+          : {
+              isValid: false,
+              invalidReason,
+            }),
       });
+    }
 
-      const sortedOccurrences = sortBy(computedOccurrences, 'startTime');
-      setOccurrences(sortedOccurrences);
-    };
+    // Handle added exceptions
+    exceptions.forEach((exception) => {
+      if (exception.occurrence_index !== undefined) return;
 
-    buildOccurrences();
-  }, [pacedTrain]);
+      let occurrenceRollingStock = rollingStock;
+      if (exception.rolling_stock && rollingStockList) {
+        const rollingStockName = exception.rolling_stock.rolling_stock_name;
+        occurrenceRollingStock = rollingStockList.find((rs) => rs.name === rollingStockName);
+      }
+
+      computedOccurrences.push({
+        id: formatPacedTrainIdToExceptionId(id, exception.key),
+        trainName: exception.train_name?.value ?? `${name}/+`,
+        rollingStock: occurrenceRollingStock,
+        // An added exception will always have a least a start time in its exceptions
+        startTime: new Date(exception.start_time!.value),
+        stopsCount: exception.path_and_schedule
+          ? exception.path_and_schedule.schedule.filter((step) => step.stop_for).length
+          : stopsCount,
+        // In the model, we can currently have a null category value so we need to handle this case
+        category: exception.rolling_stock_category
+          ? exception.rolling_stock_category.value
+          : pacedTrainCategory,
+        exceptionChangeGroups: omit(exception, ['key', 'disabled', 'occurrence_index']),
+        ...(isValid
+          ? {
+              isValid: true,
+              // TODO exceptions : update the arrival time if the exception is in the paced train summaries
+              arrivalTime: new Date(Date.parse(exception.start_time!.value) + duration!.ms),
+              pathLength,
+              mechanicalEnergyConsumed,
+              duration,
+            }
+          : {
+              isValid: false,
+            }),
+      });
+    });
+
+    return sortBy(computedOccurrences, 'startTime');
+  }, [pacedTrain, rollingStockList]);
+
+  // Add to the count the added exceptions and substract the disabled ones
+  const occurrenceCountLabel = useMemo(
+    () =>
+      exceptions.reduce((acc, exception) => {
+        if (exception.disabled) acc -= 1;
+        return acc;
+      }, occurrences.length),
+    [occurrences.length, exceptions]
+  );
 
   return { occurrencesCount: occurrenceCountLabel, occurrences };
 };
