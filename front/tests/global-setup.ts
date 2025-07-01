49b412fd4bbef99ab8b4a1fdfa100efb4d13cf02
maymanaf <med.aymen.naf@gmail.com>
2025-07-01 17:13:37 +0200

    front: remove retrieving project language from e2e tests
    Signed-off-by: maymanaf <med.aymen.naf@gmail.com>

--- front/tests/global-setup.ts
+++ front/tests/global-setup.ts
@@ -5,22 +5,14 @@ import ROLLING_STOCK_NAMES, {
   trainScheduleProjectName,
 } from './assets/constants/project-const';
 import { logger } from './logging-fixture';
-import HomePage from './pages/home-page';
 import { createDataForTests } from './utils/setup-utils';
 import { deleteProject, deleteRollingStocks } from './utils/teardown-utils';
 
-setup('setup', async ({ page }) => {
+setup('setup', async () => {
   logger.info('Starting test data setup ...');
 
   await Promise.all([deleteProject(trainScheduleProjectName), deleteProject(globalProjectName)]);
   await deleteRollingStocks(ROLLING_STOCK_NAMES);
 
   await createDataForTests();
-
-  logger.info('Retrieving project language ...');
-  const homePage = new HomePage(page);
-  await homePage.goToHomePage();
-  process.env.PROJECT_LANGUAGE = await homePage.getOSRDLanguage();
-
-  logger.info('Test data setup completed successfully.');
 });
