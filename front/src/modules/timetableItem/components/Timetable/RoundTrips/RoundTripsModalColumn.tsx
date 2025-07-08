b21ad281aebd83cb1cb053185f2daf9a8f8fce39
SharglutDev <p.filimon75@gmail.com>
2025-07-08 12:58:34 +0200

    front: adapt types for round trips
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/src/modules/timetableItem/components/Timetable/RoundTrips/RoundTripsModalColumn.tsx
+++ front/src/modules/timetableItem/components/Timetable/RoundTrips/RoundTripsModalColumn.tsx
@@ -4,31 +4,34 @@ import { useTranslation } from 'react-i18next';
 
 import type { PairingItem } from '../types';
 
-type RoundTripsModalColumnProps = {
-  columnType: 'todo' | 'oneWays' | 'roundTrips';
-  pairingItems: PairingItem[];
-};
+type RoundTripsModalColumnProps =
+  | {
+      type: 'todo' | 'oneWays';
+      pairingItems: PairingItem[];
+    }
+  | {
+      type: 'roundTrips';
+      pairingItems: { pair: [PairingItem, PairingItem]; isValid: boolean }[];
+    };
 
-const RoundTripsModalColumn = ({ columnType, pairingItems }: RoundTripsModalColumnProps) => {
+const RoundTripsModalColumn = ({ type, pairingItems }: RoundTripsModalColumnProps) => {
   const { t } = useTranslation('operational-studies', { keyPrefix: 'main.roundTripsModal' });
 
   return (
     <section
       className={cx('round-trips-modal-column', {
-        'round-trips-column': columnType === 'roundTrips',
+        'round-trips-column': type === 'roundTrips',
       })}
     >
       <h2 className="column-title">
-        {columnType === 'oneWays' && <ArrowRight />}
-        {columnType === 'roundTrips' && <ArrowSwitch />}
-        <span>{t(columnType)}</span>
+        {type === 'oneWays' && <ArrowRight />}
+        {type === 'roundTrips' && <ArrowSwitch />}
+        <span>{t(type)}</span>
         <div className="item-count">{pairingItems.length}</div>
       </h2>
       <div className="column-wrapper">
-        {pairingItems.length === 0 && columnType !== 'roundTrips' && (
-          <div className="card-placeholder" />
-        )}
-        {pairingItems.length === 0 && columnType === 'roundTrips' && (
+        {pairingItems.length === 0 && type !== 'roundTrips' && <div className="card-placeholder" />}
+        {pairingItems.length === 0 && type === 'roundTrips' && (
           <>
             <div className="card-placeholder" />
             <div className="separator" />
