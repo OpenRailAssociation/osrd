4538c86a6e8fe1ddb237dd62568f2ad772cb1a6c
Clara Ni <clara.ni@outlook.fr>
2025-07-17 15:10:48 +0200

    front: handle the summary in formatTimetableItemWithDetails
    Will be useful for the following commits.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/modules/timetableItem/helpers/formatTimetableItemWithDetails.ts
+++ front/src/modules/timetableItem/helpers/formatTimetableItemWithDetails.ts
@@ -1,11 +1,60 @@
-import type { LightRollingStockWithLiveries } from 'common/api/osrdEditoastApi';
-import type { TimetableItemWithDetails } from 'modules/timetableItem/components/Timetable/types';
+import { isTooFast, isScheduledPointsNotHonored } from 'applications/operationalStudies/utils';
+import type {
+  LightRollingStockWithLiveries,
+  SimulationSummaryResult,
+} from 'common/api/osrdEditoastApi';
+import type {
+  TimetableItemWithDetails,
+  SimulationSummary,
+} from 'modules/timetableItem/components/Timetable/types';
 import type {
   PacedTrainWithPacedTrainId,
   TimetableItem,
   TrainScheduleWithTrainId,
 } from 'reducers/osrdconf/types';
 import { Duration } from 'utils/duration';
+import { jouleToKwh } from 'utils/physics';
+import { formatKmValue } from 'utils/strings';
+
+const formatSummary = (
+  timetableItem: TimetableItem,
+  summary?: SimulationSummaryResult
+): SimulationSummary | undefined => {
+  // not loaded
+  if (!summary) {
+    return undefined;
+  }
+
+  // invalid train
+  if (summary.status !== 'success')
+    return {
+      isValid: false,
+      invalidReason:
+        summary.status === 'pathfinding_not_found' || summary.status === 'pathfinding_input_error'
+          ? summary.error_type
+          : summary.status,
+    };
+
+  // valid train
+  let notHonoredReason: Extract<
+    NonNullable<TimetableItemWithDetails['summary']>,
+    { isValid: true }
+  >['notHonoredReason'];
+  if (isTooFast(timetableItem, summary)) notHonoredReason = 'trainTooFast';
+  if (isScheduledPointsNotHonored(timetableItem, summary)) notHonoredReason = 'scheduleNotHonored';
+  return {
+    isValid: true,
+    duration: new Duration({ milliseconds: summary.time }),
+    pathLength: formatKmValue(summary.length, 'millimeters', 1),
+    mechanicalEnergyConsumed: jouleToKwh(summary.energy_consumption, true),
+    notHonoredReason,
+    pathItemTimes: {
+      base: summary.path_item_times_base,
+      provisional: summary.path_item_times_provisional,
+      final: summary.path_item_times_final,
+    },
+  };
+};
 
 const extractBaseTimetableItemProps = (timetableItem: TimetableItem) => ({
   name: timetableItem.train_name,
@@ -19,7 +68,8 @@ const extractBaseTimetableItemProps = (timetableItem: TimetableItem) => ({
 
 export const formatTrainScheduleWithDetails = (
   trainSchedule: TrainScheduleWithTrainId,
-  rollingStock?: LightRollingStockWithLiveries
+  rollingStock?: LightRollingStockWithLiveries,
+  summary?: SimulationSummaryResult
 ): TimetableItemWithDetails => {
   // we omit the following props since they're not expected in TimetableItemWithDetails
   const {
@@ -34,12 +84,14 @@ export const formatTrainScheduleWithDetails = (
     ...trainScheduleProps,
     ...extractBaseTimetableItemProps(trainSchedule),
     rollingStock,
+    summary: formatSummary(trainSchedule, summary),
   };
 };
 
 export const formatPacedTrainWithDetails = (
   pacedTrain: PacedTrainWithPacedTrainId,
-  rollingStock?: LightRollingStockWithLiveries
+  rollingStock?: LightRollingStockWithLiveries,
+  pacedTrainSummary?: SimulationSummaryResult
 ): TimetableItemWithDetails => {
   // we omit the following props since they're not expected in TimetableItemWithDetails
   const {
@@ -59,5 +111,6 @@ export const formatPacedTrainWithDetails = (
       timeWindow: Duration.parse(paced.time_window),
       interval: Duration.parse(paced.interval),
     },
+    summary: formatSummary(pacedTrain, pacedTrainSummary),
   };
 };
