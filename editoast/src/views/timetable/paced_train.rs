0fd37966868945e3463594ba6920d9ce4e60da06
Florian Amsallem <florian.amsallem@gmail.com>
2025-07-18 15:17:29 +0200

    editoast: add missing authz checks on infra for projection on tracks
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/src/views/timetable/paced_train.rs
+++ editoast/src/views/timetable/paced_train.rs
@@ -598,6 +598,13 @@ async fn project_path(
         return Err(AuthorizationError::Forbidden.into());
     }
 
+    auth.check_authorization(async |authorizer| {
+        authorizer
+            .authorize_infra_read(&authz::Infra(infra_id))
+            .await
+    })
+    .await?;
+
     let conn = &mut db_pool.get().await?;
 
     let paced_trains: Vec<models::PacedTrain> =
@@ -706,6 +713,13 @@ async fn occupancy_blocks(
     if !authorized {
         return Err(AuthorizationError::Forbidden.into());
     }
+
+    auth.check_authorization(async |authorizer| {
+        authorizer
+            .authorize_infra_read(&authz::Infra(infra_id))
+            .await
+    })
+    .await?;
 
     let infra = &Infra::retrieve_real_or_fail(db_pool.get().await?, infra_id, || {
         PacedTrainError::InfraNotFound { infra_id }
