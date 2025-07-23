c2e028a4a4f32937a3510a05dc6829cee15289bc
Simon Ser <contact@emersion.fr>
2025-07-23 15:46:26 +0200

    front: split off useTimetableItemsWithPathOps()
    We'll need to do the same operation from NGE, however NGE doesn't
    use hooks. Split the pre- and post-processing steps into helper
    functions.
    
    Simplify timetableItemsWithKeys by computing the keys when we need
    them after the HTTP request is done.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/applications/operationalStudies/hooks/useTimetableItemsWithPathOps.ts
+++ front/src/applications/operationalStudies/hooks/useTimetableItemsWithPathOps.ts
@@ -1,43 +1,19 @@
 import { useMemo } from 'react';
 
-import {
-  osrdEditoastApi,
-  type OperationalPoint,
-  type PathItemLocation,
-} from 'common/api/osrdEditoastApi';
+import { osrdEditoastApi } from 'common/api/osrdEditoastApi';
 import type { TimetableItem, TimetableItemWithPathOps } from 'reducers/osrdconf/types';
 
-import { isOperationalPointReference } from '../utils';
+import { getUniqueOpRefsFromTimetableItems, addPathOpsToTimetableItems } from '../utils';
 
 const useTimetableItemsWithPathOps = (
   infraId: number,
   timetableItems: TimetableItem[] | undefined
-) => {
+): TimetableItemWithPathOps[] => {
   // Extract all unique PathItemLocation from timetableItems.path
-  const { timetableOpRefs, timetableItemsWithKeys } = useMemo(() => {
-    if (!timetableItems) return { timetableOpRefs: [], timetableItemsWithKeys: [] };
-    const uniqueSteps = new Map<string, PathItemLocation>();
-    const timetableItemsDict: { timetableItem: TimetableItem; pathStepKeys: string[] }[] = [];
-    for (const timetableItem of timetableItems) {
-      const pathStepKeys = [];
-      for (const pathItem of timetableItem.path) {
-        const { id: _id, deleted: _deleted, ...cleanPathItem } = pathItem;
-        const pathItemLocation: PathItemLocation = cleanPathItem;
-        const key = JSON.stringify(pathItemLocation);
-        uniqueSteps.set(key, pathItemLocation);
-        pathStepKeys.push(key);
-      }
-      // Keep for each timetableItem, the keys of its path steps, this will help later
-      // to match them with their corresponding operational points
-      timetableItemsDict.push({ timetableItem, pathStepKeys });
-    }
-    return {
-      timetableOpRefs: Array.from(uniqueSteps.values()).filter((pathItem) =>
-        isOperationalPointReference(pathItem)
-      ),
-      timetableItemsWithKeys: timetableItemsDict,
-    };
-  }, [timetableItems]);
+  const timetableOpRefs = useMemo(
+    () => getUniqueOpRefsFromTimetableItems(timetableItems ?? []),
+    [timetableItems]
+  );
 
   const { currentData: timetableOperationalPoints } =
     osrdEditoastApi.endpoints.matchAllOperationalPoints.useQuery(
@@ -48,31 +24,12 @@ const useTimetableItemsWithPathOps = (
       { skip: timetableOpRefs.length === 0 }
     );
 
-  const timetableItemsWithOps: TimetableItemWithPathOps[] = useMemo(() => {
-    if (!timetableOperationalPoints || timetableOperationalPoints.length === 0) {
+  return useMemo(() => {
+    if (!timetableItems || !timetableOperationalPoints || timetableOperationalPoints.length === 0) {
       return [];
     }
-
-    // Map each operational point reference (path step) to its corresponding operational points
-    const opsByKey = new Map<string, OperationalPoint[]>();
-    timetableOperationalPoints.forEach((ops, i) => {
-      const key = JSON.stringify(timetableOpRefs[i]);
-      opsByKey.set(key, ops);
-    });
-
-    // For each timetable item, fill the pathOps property with
-    // their corresponding operational points
-    return timetableItemsWithKeys.map(({ timetableItem, pathStepKeys }) => {
-      // For each pathStepKeys, find its corresponding operational points :
-      // 1. if found, return the operational points
-      // 2. if key exists but no operational points were found, return an empty array
-      // 3. if key does not exist in opsByKey (meaning it's a track offset), return an empty array
-      const pathOps = pathStepKeys.map((key) => opsByKey.get(key) ?? []);
-      return { ...timetableItem, pathOps };
-    });
-  }, [timetableOperationalPoints]);
-
-  return timetableItemsWithOps;
+    return addPathOpsToTimetableItems(timetableItems, timetableOpRefs, timetableOperationalPoints);
+  }, [timetableItems, timetableOperationalPoints]);
 };
 
 export default useTimetableItemsWithPathOps;
