b3fced4a7942091d6d5364f5fa3eaa2a5895e076
theocrsb <theo_crosbie@yahoo.fr>
2025-07-24 11:35:49 +0200

    front: add subcategories in scenario select
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- front/src/modules/rollingStock/hooks/useCategoryOptions.ts
+++ front/src/modules/rollingStock/hooks/useCategoryOptions.ts
@@ -1,23 +1,75 @@
 import { useTranslation } from 'react-i18next';
 
-import type { TrainMainCategory } from 'common/api/osrdEditoastApi';
+import {
+  osrdEditoastApi,
+  type SubCategory,
+  type TrainMainCategory,
+} from 'common/api/osrdEditoastApi';
 import { TrainMainCategoryDict } from 'modules/rollingStock/consts';
 
-export type CategoryOptionWithId = { id: TrainMainCategory; label: string };
+export type TrainCategory =
+  | {
+      main_category: TrainMainCategory;
+    }
+  | {
+      sub_category_code: string;
+    };
 
-export type CategoryOption = { id?: TrainMainCategory; label: string };
+export type CategoryOptionWithId = {
+  id: TrainMainCategory | string;
+  label: string;
+};
 
 export default function useCategoryOptions(withPlaceholder = true) {
   const { t } = useTranslation();
 
-  const options = Object.values(TrainMainCategoryDict).map((category) => ({
-    id: category,
-    label: t(`rollingStock.categoriesOptions.${category}`),
-  }));
+  const { data: _data } = osrdEditoastApi.endpoints.getSubCategory.useQuery({
+    page: 1,
+    pageSize: 100,
+  });
 
-  if (withPlaceholder) {
-    return [{ label: t('rollingStock.categoriesOptions.choose') }, ...options];
+  const subCategoriesMocked: SubCategory[] = [
+    {
+      code: 'uuid-123',
+      color: '#FFFFFF',
+      main_category: 'FREIGHT_TRAIN',
+      name: 'ABC',
+    },
+    {
+      code: 'uuid-456',
+      color: '#AAAAAA',
+      main_category: 'HIGH_SPEED_TRAIN',
+      name: '123',
+    },
+  ];
+
+  const validMainCategories = Object.keys(TrainMainCategoryDict) as TrainMainCategory[];
+
+  const groupedOptions: CategoryOptionWithId[] = [];
+
+  for (const mainCategory of validMainCategories) {
+    groupedOptions.push({
+      id: mainCategory,
+      label: t(`rollingStock.categoriesOptions.${mainCategory}`),
+    });
+
+    const subCategories = subCategoriesMocked.filter((sub) => sub.main_category === mainCategory);
+
+    groupedOptions.push(
+      ...subCategories.map((sub) => ({
+        id: sub.code,
+        label: sub.name,
+      }))
+    );
   }
 
-  return options as CategoryOptionWithId[];
+  if (withPlaceholder) {
+    return [
+      {
+        label: t('rollingStock.categoriesOptions.choose'),
+      },
+      ...groupedOptions,
+    ];
+  }
+  return groupedOptions;
 }
