5187a86bcbc4fa826bac64711db408980aa9d650
Clara Ni <clara.ni@outlook.fr>
2025-07-29 17:13:59 +0200

    front: move some RollingStockCurve's utils into /helpers
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/modules/rollingStock/helpers/curves.ts
+++ front/src/modules/rollingStock/helpers/curves.ts
@@ -1,7 +1,13 @@
 import type { Comfort } from 'common/api/osrdEditoastApi';
+import COLORS from 'modules/rollingStock/components/RollingStockSelector/consts/colors';
 import { geti18nKeyForNull } from 'utils/strings';
 
-// eslint-disable-next-line import/prefer-default-export
+import type { TransformedCurves, ParsedCurve } from '../types';
+
+const hoveredOpacityCode = 'B3'; // 70% opacity
+const lowOpacityCode = '40'; // 25% opacity
+const colorsListLength = Object.keys(COLORS).length;
+
 export const getCurveName = (
   name: string,
   comfort: Comfort,
@@ -15,3 +21,81 @@ export const getCurveName = (
     : '';
   return `${name} ${comfort}${electricalProfile}${powerRestriction}`;
 };
+
+// Format RollingStock Curves to NIVO format
+export const parseData = (
+  label: string,
+  color: string,
+  curve: TransformedCurves['index']
+): ParsedCurve => {
+  // Have to transform data, will change when we'll have multiples curves,
+  // so initial transformation is commented :
+  // const curveFormatted = curve.map((item)
+  // => ({ x: item.speed * 3.6, y: item.max_effort / 1000 }));
+
+  const curveFormatted = curve.speeds.map((speed: number, index: number) => ({
+    x: speed * 3.6,
+    y: curve.max_efforts[index] / 1000,
+  }));
+
+  const curveFormattedSorted = curveFormatted.sort((a, b) => (a.x > b.x ? 1 : -1));
+
+  return {
+    id: label,
+    color,
+    mode: curve.mode,
+    comfort: curve.comfort,
+    data: curveFormattedSorted,
+    electrical_profile_level: geti18nKeyForNull(curve.electricalProfile),
+    power_restriction: geti18nKeyForNull(curve.powerRestriction),
+  };
+};
+
+/** Choose cyclic color for curves depending on curve number */
+export function curveColor(
+  index: number,
+  electricalReferenceForOpacity: string | null,
+  hoveredElectricalParam?: string | null,
+  selectedElectricalParam?: string | null
+) {
+  const indexShort = index % colorsListLength;
+  if (hoveredElectricalParam) {
+    const isHovered = electricalReferenceForOpacity === hoveredElectricalParam;
+    const isSelected = electricalReferenceForOpacity === selectedElectricalParam;
+
+    return `${Object.keys(COLORS)[indexShort]}${
+      isHovered && !isSelected ? hoveredOpacityCode : ''
+    }${!isHovered && !isSelected ? lowOpacityCode : ''}`;
+  }
+  return Object.keys(COLORS)[indexShort];
+}
+
+export function setupCurvesVisibility(
+  data: TransformedCurves,
+  previousCurvesVisibility: { [key: string]: boolean } = {}
+) {
+  const nextCurvesVisibility: { [key: string]: boolean } = {};
+  Object.keys(data).forEach((id) => {
+    nextCurvesVisibility[id] = id in previousCurvesVisibility ? previousCurvesVisibility[id] : true;
+  });
+  return nextCurvesVisibility;
+}
+
+export function initialComfortsState(curvesComfortList: string[]) {
+  const comfortsState: { [key: string]: boolean } = {};
+  curvesComfortList.forEach((id) => {
+    comfortsState[id] = true;
+  });
+  return comfortsState;
+}
+
+export function getCurvesByComfortState(
+  transformedData: TransformedCurves,
+  comfortsStates: {
+    [key: string]: boolean;
+  }
+) {
+  return Object.keys(transformedData).filter(
+    (curve) => comfortsStates[transformedData[curve].comfort]
+  );
+}
