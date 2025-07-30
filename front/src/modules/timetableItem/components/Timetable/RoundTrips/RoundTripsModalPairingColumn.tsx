8ceeb7056388da9e993b3cfa65b4271c2d279718
SharglutDev <p.filimon75@gmail.com>
2025-07-30 17:33:27 +0200

    front: add filter input in roundtrips pairing column
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/src/modules/timetableItem/components/Timetable/RoundTrips/RoundTripsModalPairingColumn.tsx
+++ front/src/modules/timetableItem/components/Timetable/RoundTrips/RoundTripsModalPairingColumn.tsx
@@ -1,5 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
+import { Input } from '@osrd-project/ui-core';
+import { Filter } from '@osrd-project/ui-icons';
 import { useTranslation } from 'react-i18next';
 
 import type { SubCategory } from 'common/api/osrdEditoastApi';
@@ -27,26 +29,54 @@ const RoundTripsModalPairingColumn = ({
 
   const modalRef = useRef<HTMLDivElement>(null);
 
+  const [filter, setFilter] = useState('');
+
   useOutsideClick(modalRef, closePairingMode);
 
   return (
     <>
       <div className="round-trips-pairing-overlay" />
       <div ref={modalRef} className="round-trips-pairing-column">
+        <Input
+          id="candidates-filter"
+          small
+          narrow
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          withIcons={[
+            {
+              icon: <Filter size="sm" />,
+              action: () => {},
+              className: 'filter-input-icon',
+            },
+          ]}
+        />
         {suggestions.length > 0 && (
           <section>
             <h3 className="title">{t('suggestions')}</h3>
-            {suggestions.map((item) => (
-              <RoundTripsModalCard key={item.id} pairingItem={item} subCategories={subCategories} />
-            ))}
+            {suggestions
+              .filter((item) => item.name.toLowerCase().includes(filter.toLowerCase()))
+              .map((item) => (
+                <RoundTripsModalCard
+                  key={item.id}
+                  pairingItem={item}
+                  subCategories={subCategories}
+                />
+              ))}
           </section>
         )}
         {others.length > 0 && (
           <section>
             <h3 className="title">{t('others')}</h3>
-            {others.map((item) => (
-              <RoundTripsModalCard key={item.id} pairingItem={item} subCategories={subCategories} />
-            ))}
+            {others
+              .filter((item) => item.name.toLowerCase().includes(filter.toLowerCase()))
+              .map((item) => (
+                <RoundTripsModalCard
+                  key={item.id}
+                  pairingItem={item}
+                  subCategories={subCategories}
+                />
+              ))}
           </section>
         )}
       </div>
