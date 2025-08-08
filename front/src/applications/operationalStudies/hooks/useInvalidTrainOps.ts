75458956d3a90e0890c7e8af237d910970a07c1d
Elise Chin <c.chin.elise@gmail.com>
2025-08-08 11:04:59 +0200

    front: introduce useInvalidTrainOps()
    This will be useful to display invalid train's operational points in the output table.
    
    Signed-off-by: Elise Chin <c.chin.elise@gmail.com>

---
+++ front/src/applications/operationalStudies/hooks/useInvalidTrainOps.ts
@@ -0,0 +1,64 @@
+import { useMemo } from 'react';
+
+import type { PathPropertiesFormatted } from 'applications/operationalStudies/types';
+import {
+  osrdEditoastApi,
+  type OperationalPointPart,
+  type OperationalPointReference,
+} from 'common/api/osrdEditoastApi';
+import type { Train } from 'reducers/osrdconf/types';
+
+import { getStationFromOps, isOperationalPointReference } from '../utils';
+
+const useInvalidTrainOps = (
+  infraId: number,
+  isValid: boolean,
+  train?: Train
+): PathPropertiesFormatted['operationalPoints'] => {
+  const operationalPointReferences: OperationalPointReference[] = useMemo(() => {
+    if (isValid || !train) return [];
+    return train.path.reduce<OperationalPointReference[]>((acc, pathItem) => {
+      if (isOperationalPointReference(pathItem)) {
+        const { id: _id, deleted: _deleted, ...cleanOperationalPointReference } = pathItem;
+        acc.push(cleanOperationalPointReference);
+      }
+      return acc;
+    }, []);
+  }, [train]);
+
+  const { data: operationalPoints } =
+    osrdEditoastApi.endpoints.postInfraByInfraIdMatchOperationalPoints.useQuery(
+      {
+        infraId,
+        body: {
+          operational_point_references: operationalPointReferences,
+        },
+      },
+      { skip: operationalPointReferences.length === 0 }
+    );
+
+  // Get this type will facilitate the logic in TimesStopsOutput
+  const formattedOperationalPoints: PathPropertiesFormatted['operationalPoints'] = useMemo(() => {
+    if (
+      !operationalPoints?.related_operational_points ||
+      operationalPoints.related_operational_points.length === 0
+    ) {
+      return [];
+    }
+
+    // We have at least one related operational point
+    return operationalPoints.related_operational_points
+      .filter((ops) => ops.length !== 0) // To remove empty arrays related to invalid step
+      .map((ops) => ({
+        ...getStationFromOps(ops)!,
+        // The following properties will not be used for invalid trains output table data
+        part: { position: 0, track: 'unknown' } as OperationalPointPart,
+        position: 0,
+        weight: null,
+      }));
+  }, [operationalPoints]);
+
+  return formattedOperationalPoints;
+};
+
+export default useInvalidTrainOps;
