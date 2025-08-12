d0b52cdb0c739a09d3b923b1bb3c08ba8bdc99b8
Leo Valais <leo.valais97@gmail.com>
2025-08-12 21:42:28 +0200

    editoast: remove some unused EditoastError imports
    Signed-off-by: Leo Valais <leo.valais97@gmail.com>

--- editoast/src/models/prelude/create.rs
+++ editoast/src/models/prelude/create.rs
@@ -4,8 +4,6 @@ use std::result::Result;
 use database::DbConnection;
 use editoast_models::model;
 
-use crate::error::EditoastError;
-
 use super::Model;
 
 /// Describes how a [Model] can be created in the database
