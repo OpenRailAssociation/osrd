a145d5c9de08a12d723af47d131cbd9700a8be10
Clara Ni <clara.ni@outlook.fr>
2025-08-13 16:24:19 +0200

    ui-charts: use internalHeight instead of height in SpeedSpaceChart
    Previously, the chart managed its own height, which could vary either when the user enlarged it or when they added/
    removed linear layers (dynamicalHeight was private to the chart).
    
    We now need to control the entire height of the graph from the parent component, hence the introduction of internalHeight,
    which corresponds to the height of the graph (without the linear layers and with its top and bottom margins) and is used
    in the various layers to replace height and dynamicalHeight.
    
    Co-authored-by: Louis <iLTmpas@users.noreply.github.com>
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/ui/ui-charts/src/speedSpaceChart/components/layers/ReticleLayer.tsx
+++ front/ui/ui-charts/src/speedSpaceChart/components/layers/ReticleLayer.tsx
@@ -2,33 +2,32 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import type { TrainDetails, Store } from '../../types';
 import DetailsBox from '../common/DetailsBox';
+import { MARGINS } from '../const';
 import { drawCursor } from '../helpers/drawElements/reticle';
-import { clearCanvas, getAdaptiveHeight } from '../utils';
+import { clearCanvas } from '../utils';
 
 type ReticleLayerProps = {
   width: number;
-  height: number;
-  heightOffset: number;
+  internalHeight: number;
   store: Store;
 };
 
-const ReticleLayer = ({ width, height, heightOffset, store }: ReticleLayerProps) => {
+const ReticleLayer = ({ width, internalHeight: height, store }: ReticleLayerProps) => {
   const canvas = useRef<HTMLCanvasElement>(null);
   const [trainDetails, setTrainDetails] = useState<TrainDetails | null>(null);
-  const maxCursorHeight = getAdaptiveHeight(heightOffset, store.layersDisplay, false);
 
   useEffect(() => {
     const currentCanvas = canvas.current as HTMLCanvasElement;
     const ctx = currentCanvas.getContext('2d') as CanvasRenderingContext2D;
     // The tooltip shouldn't be displayed when hovering on the linear layers
-    if (store.cursor.y && store.cursor.y < maxCursorHeight) {
+    if (store.cursor.y && store.cursor.y < height - MARGINS.MARGIN_TOP - MARGINS.MARGIN_BOTTOM) {
       const detailsBox = drawCursor({ ctx, width, height, store });
       setTrainDetails(detailsBox || null);
     } else {
       clearCanvas(ctx, width, height);
       setTrainDetails(null);
     }
-  }, [width, height, store, maxCursorHeight]);
+  }, [width, height, store]);
 
   return (
     <>
