e7ebadb7c8a45a9c4d10dac8fe8b851b0e1f6c74
Achrafmohye <a.mohyeddine@gmail.com>
2025-08-14 19:30:39 +0200

    front: fix train list order after updating a service
    Signed-off-by: Achrafmohye <a.mohyeddine@gmail.com>

--- front/src/applications/operationalStudies/hooks/useScenarioData.ts
+++ front/src/applications/operationalStudies/hooks/useScenarioData.ts
@@ -145,7 +145,7 @@ const useScenarioData = (scenario: ScenarioResponse, infra: InfraWithState) => {
         ? formatPacedTrainWithDetails(timetableItem, rollingStock)
         : formatTrainScheduleWithDetails(timetableItem, rollingStock);
     });
-    return sortBy(items, 'startTime');
+    return sortBy(items, ['startTime', 'name', 'id']);
   }, [timetableItems, rollingStocksByName, simulatedTrainsById]);
 
   const projectedTrains = useMemo(
