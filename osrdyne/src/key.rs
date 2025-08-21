a8f46b1e563d8641e916bb16c87ed442cb1d79a6
Younes Khoudli <younes.khoudli@epita.fr>
2025-08-21 15:29:02 +0200

    osrdyne: switch from smallvec to smoll_str
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

--- osrdyne/src/key.rs
+++ osrdyne/src/key.rs
@@ -1,33 +1,36 @@
 use std::fmt::{Debug, Display};
 
 use serde::{Deserialize, Serialize};
-use smallvec::SmallVec;
+use smol_str::SmolStr;
 
 #[derive(Hash, Clone, PartialOrd, Ord, PartialEq, Eq)]
-pub struct Key(SmallVec<[u8; 16]>);
+pub struct Key(SmolStr);
 
 impl Serialize for Key {
     fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.collect_str(&self.encode())
+        self.0.serialize(serializer)
     }
 }
 
 impl<'de> Deserialize<'de> for Key {
     fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let key = String::deserialize(deserializer)?;
+        let key = SmolStr::deserialize(deserializer)?;
         Ok(Key::new(&key))
     }
 }
 
 impl Display for Key {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.encode())
+        f.write_str(self.encode())
     }
 }
 
-impl From<&[u8]> for Key {
-    fn from(e: &[u8]) -> Self {
-        Key(e.into())
+impl TryFrom<&[u8]> for Key {
+    type Error = std::str::Utf8Error;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let key_str = str::from_utf8(value)?;
+        Ok(Key::new(key_str))
     }
 }
 
@@ -39,10 +42,10 @@ impl Debug for Key {
 
 impl Key {
     pub fn new(key: &str) -> Self {
-        Key(key.as_bytes().into())
+        Key(SmolStr::new(key))
     }
 
-    pub(crate) fn encode(&self) -> String {
-        self.0.iter().map(|&b| b as char).collect()
+    pub(crate) fn encode(&self) -> &str {
+        &self.0
     }
 }
