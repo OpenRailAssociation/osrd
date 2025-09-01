b918dd738ec6f72ad51a9e7cc888357d4e8b79af
Alice K. <alice.khoudli@gmail.com>
2025-09-01 17:02:07 +0200

    front: add reducers to unset selected projected ids
    Add reducers to check whether the selected and projected train ids in the store
    match with a given scheduled or paced train id,
    an occurence of a given paced train id,
    or an occurence of a given paced train id not listed in a list of present ids.
    
    These will be used to properly unset selected and projected train ids when
    deleting timetable items or occurences (possibly simply by changing paced train pacing parameters)
    without needing all components that perform such operations to consume the selected and projected train ids.
    
    Ids were currently only unset in a few places but not others,
    causing both app crashes and unintended user experience.
    
    Signed-off-by: Alice K. <alice.khoudli@gmail.com>

--- front/src/reducers/simulationResults/index.ts
+++ front/src/reducers/simulationResults/index.ts
@@ -3,6 +3,7 @@ import type { Draft } from 'immer';
 
 import type { OccurrenceId, PacedTrainId, TrainId, TrainScheduleId } from 'reducers/osrdconf/types';
 import type { ProjectionType, SimulationResultsState } from 'reducers/simulationResults/types';
+import { extractPacedTrainIdFromOccurrenceId, isOccurrenceId } from 'utils/trainId';
 
 export const simulationResultsInitialState: SimulationResultsState = {
   chart: undefined,
@@ -33,10 +34,56 @@ export const simulationResultsSlice = createSlice({
     ) {
       state.projectionType = action.payload;
     },
+    unsetTrainIdsMatching(
+      state: Draft<SimulationResultsState>,
+      action: PayloadAction<TrainScheduleId | PacedTrainId | OccurrenceId>
+    ) {
+      const idToUnset = action.payload;
+
+      const isIdMatchingOccurence = (
+        id: TrainScheduleId | OccurrenceId | PacedTrainId | undefined
+      ) => id && isOccurrenceId(id) && extractPacedTrainIdFromOccurrenceId(id) === idToUnset;
+
+      if (
+        state.trainIdUsedForProjection === idToUnset ||
+        isIdMatchingOccurence(state.trainIdUsedForProjection)
+      ) {
+        state.trainIdUsedForProjection = undefined;
+      }
+      if (state.selectedTrainId === idToUnset || isIdMatchingOccurence(state.selectedTrainId)) {
+        state.selectedTrainId = undefined;
+      }
+    },
+    unsetTrainIdsMatchingMissingOccurencesOf(
+      state: Draft<SimulationResultsState>,
+      action: PayloadAction<{ pacedTrainId: PacedTrainId; occurrencesPresent: OccurrenceId[] }>
+    ) {
+      const { pacedTrainId, occurrencesPresent } = action.payload;
+
+      const isIdMatchingMissingOccurence = (
+        id: TrainScheduleId | OccurrenceId | PacedTrainId | undefined
+      ) =>
+        id &&
+        isOccurrenceId(id) &&
+        extractPacedTrainIdFromOccurrenceId(id) === pacedTrainId &&
+        !occurrencesPresent.includes(id);
+
+      if (isIdMatchingMissingOccurence(state.trainIdUsedForProjection)) {
+        state.trainIdUsedForProjection = undefined;
+      }
+      if (isIdMatchingMissingOccurence(state.selectedTrainId)) {
+        state.selectedTrainId = undefined;
+      }
+    },
   },
 });
 
-export const { updateSelectedTrainId, updateTrainIdUsedForProjection, updateProjectionType } =
-  simulationResultsSlice.actions;
+export const {
+  updateSelectedTrainId,
+  updateTrainIdUsedForProjection,
+  updateProjectionType,
+  unsetTrainIdsMatching,
+  unsetTrainIdsMatchingMissingOccurencesOf,
+} = simulationResultsSlice.actions;
 
 export default simulationResultsSlice.reducer;
