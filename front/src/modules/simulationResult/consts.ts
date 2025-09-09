ca196ac5e17b4fd784346c8e823f315065f7c95b
Mathieu <mathieu.coulibaly@sncf.fr>
2025-09-09 12:02:00 +0200

    front: fix categories colors according to the mockup
    Signed-off-by: Mathieu <mathieu.coulibaly@sncf.fr>

--- front/src/modules/simulationResult/consts.ts
+++ front/src/modules/simulationResult/consts.ts
@@ -43,14 +43,14 @@ export const TRAIN_MAIN_CATEGORY_PATH_COLORS: Record<
   TrainMainCategory,
   { normal: string; hovered: string; background: string }
 > = {
-  HIGH_SPEED_TRAIN: { normal: '#E5221A', hovered: '#A62924', background: '#FAE7E6' },
-  INTERCITY_TRAIN: { normal: '#E02271', hovered: '#9A2053', background: '#FFE8F2' },
-  REGIONAL_TRAIN: { normal: '#C75300', hovered: '#943E00', background: '#FFE7D6' },
-  NIGHT_TRAIN: { normal: '#B2539E', hovered: '#732963', background: '#FAE6F6' },
-  COMMUTER_TRAIN: { normal: '#506AE3', hovered: '#314297', background: '#E0ECFF' },
+  HIGH_SPEED_TRAIN: { normal: '#E5221A', hovered: '#912420', background: '#FAE7E6' },
+  INTERCITY_TRAIN: { normal: '#B2539E', hovered: '#732963', background: '#FAE6F6' },
+  REGIONAL_TRAIN: { normal: '#C75300', hovered: '#803500', background: '#FFE7D6' },
+  NIGHT_TRAIN: { normal: '#8757E6', hovered: '#58318F', background: '#E5E7FF' },
+  COMMUTER_TRAIN: { normal: '#127DB8', hovered: '#165070', background: '#D9F2FF' },
   FREIGHT_TRAIN: { normal: '#54823B', hovered: '#2C4F19', background: '#E4EDDF' },
   FAST_FREIGHT_TRAIN: { normal: '#13857B', hovered: '#085953', background: '#DAF7EE' },
-  TRAM_TRAIN: { normal: '#127DB8', hovered: '#195B80', background: '#D9F2FF' },
-  TOURISTIC_TRAIN: { normal: '#8A714B', hovered: '#6B532E', background: '#EEE7D9' },
-  WORK_TRAIN: { normal: '#996E00', hovered: '#705400', background: '#FFF2CC' },
+  TRAM_TRAIN: { normal: '#687C5C', hovered: '#444D3C', background: '#E1EDD8' },
+  TOURISTIC_TRAIN: { normal: '#8A714B', hovered: '#594525', background: '#EEE7D9' },
+  WORK_TRAIN: { normal: '#996E00', hovered: '#634A00', background: '#FCEEC2' },
 };
