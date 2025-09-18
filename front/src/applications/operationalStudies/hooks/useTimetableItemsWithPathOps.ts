1cd489ecaf2f5ce889d6234c82ae7342d4d5918e
theocrsb <theo_crosbie@yahoo.fr>
2025-09-18 10:22:51 +0200

    front: fix opening round trip modal
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- front/src/applications/operationalStudies/hooks/useTimetableItemsWithPathOps.ts
+++ front/src/applications/operationalStudies/hooks/useTimetableItemsWithPathOps.ts
@@ -15,7 +15,7 @@ const useTimetableItemsWithPathOps = (
     [timetableItems]
   );
 
-  const { currentData: timetableOperationalPoints } =
+  const { currentData: timetableOperationalPoints, isSuccess } =
     osrdEditoastApi.endpoints.matchAllOperationalPoints.useQuery(
       {
         infraId,
@@ -25,15 +25,16 @@ const useTimetableItemsWithPathOps = (
     );
 
   return useMemo(() => {
-    if (!timetableItems) {
+    if (!timetableItems || (!isSuccess && timetableOpRefs.length !== 0)) {
       return [];
     }
+
     return addPathOpsToTimetableItems(
       timetableItems,
       timetableOpRefs,
       timetableOperationalPoints ?? []
     );
-  }, [timetableItems, timetableOperationalPoints, timetableOpRefs]);
+  }, [timetableItems, timetableOperationalPoints, timetableOpRefs, isSuccess]);
 };
 
 export default useTimetableItemsWithPathOps;
