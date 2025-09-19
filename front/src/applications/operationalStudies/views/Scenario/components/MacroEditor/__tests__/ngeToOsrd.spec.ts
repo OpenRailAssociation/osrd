fdcd17549ddc8c24a6ab23ad88bc773b65ed1f33
Alice Khoudli <alice.khoudli@polytechnique.org>
2025-09-19 16:45:53 +0200

    front: handle disjointed trains in nge import
    Signed-off-by: Alice Khoudli <alice.khoudli@polytechnique.org>

--- front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/__tests__/ngeToOsrd.spec.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/__tests__/ngeToOsrd.spec.ts
@@ -12,11 +12,14 @@ async function readJsonFile(path: string): Promise<unknown> {
 vi.setSystemTime(new Date('2025-06-25T13:00:00.000Z'));
 
 describe('convertNgeDtoToOsrd', () => {
-  test.each(['roundTrip', 'oneWay', 'duplicateTrigrams'])('ngeToOsrd-inputDto-$0', async (name) => {
-    const dto = (await readJsonFile(`./ngeToOsrd-inputDto-${name}.json`)) as NetzgrafikDto;
-    const expected = await readJsonFile(`./ngeToOsrd-output-${name}.json`);
-    const result = convertNgeDtoToOsrd(dto);
-    // Go through JSON encoding to discard undefined fields
-    expect(JSON.parse(JSON.stringify(result))).toEqual(expected);
-  });
+  test.each(['roundTrip', 'oneWay', 'duplicateTrigrams', 'discontinuousTrainrun'])(
+    'ngeToOsrd-inputDto-$0',
+    async (name) => {
+      const dto = (await readJsonFile(`./ngeToOsrd-inputDto-${name}.json`)) as NetzgrafikDto;
+      const expected = await readJsonFile(`./ngeToOsrd-output-${name}.json`);
+      const result = convertNgeDtoToOsrd(dto);
+      // Go through JSON encoding to discard undefined fields
+      expect(JSON.parse(JSON.stringify(result))).toEqual(expected);
+    }
+  );
 });
