8b8cce89e7fa1e580bf03b1955b37a95a28847f8
Simon Ser <contact@emersion.fr>
2025-09-19 16:48:44 +0200

    ui-core: add missing exports
    Some stories need these types.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/ui/ui-core/src/index.ts
+++ front/ui/ui-core/src/index.ts
@@ -13,6 +13,7 @@ export {
   CheckboxProps,
   CheckboxesTree,
   CheckboxesTreeProps,
+  CheckboxTreeItem,
 } from './components/inputs/Checkbox';
 export {
   DatePicker,
@@ -25,12 +26,14 @@ export { default as Input, InputProps } from './components/inputs/Input';
 export { default as PasswordInput, PasswordInputProps } from './components/inputs/PasswordInput';
 export { default as RadioButton, RadioButtonProps } from './components/inputs/RadioButton';
 export { default as RadioGroup, RadioGroupProps } from './components/inputs/RadioGroup';
+export { type StatusWithMessage } from './components/inputs/StatusMessage';
 export { default as Select, SelectProps } from './components/Select';
 export { default as TextArea, TextAreaProps } from './components/inputs/TextArea';
-export { default as TimePicker } from './components/inputs/TimePicker';
-export { default as Slider } from './components/inputs/Slider';
+export { default as TimePicker, type TimePickerProps } from './components/inputs/TimePicker';
+export { default as Slider, SliderProps } from './components/inputs/Slider';
 export {
   default as TolerancePicker,
   type TolerancePickerProps,
+  type ToleranceValues,
 } from './components/inputs/tolerancePicker/TolerancePicker';
 export { default as TokenInput, TokenInputProps } from './components/inputs/TokenInput';
