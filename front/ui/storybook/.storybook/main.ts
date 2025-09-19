e41b96da75caf43503d0b80433269baed85a3721
Simon Ser <contact@emersion.fr>
2025-09-19 16:55:52 +0200

    storybook: wire up vite-plugin-checker
    We never checked TypeScript or ESLint in storybook. Ensure we don't
    accumulate errors like we did before.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/ui/storybook/.storybook/main.ts
+++ front/ui/storybook/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import vitePluginChecker from 'vite-plugin-checker';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -6,11 +8,21 @@ const config: StorybookConfig = {
   addons: ['@storybook/addon-links', '@storybook/addon-docs'],
   framework: '@storybook/react-vite',
 
-  typescript: {
-    check: true,
-  },
-
   staticDirs: ['../public'],
   logLevel: 'debug',
+
+  async viteFinal(viteConfig) {
+    return mergeConfig(viteConfig, {
+      plugins: [
+        vitePluginChecker({
+          typescript: true,
+          eslint: {
+            useFlatConfig: true,
+            lintCommand: 'eslint stories .storybook --max-warnings 0',
+          },
+        }),
+      ],
+    });
+  },
 };
 export default config;
