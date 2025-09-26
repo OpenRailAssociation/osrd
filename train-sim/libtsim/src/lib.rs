90fce0de29f6d3d837b253eb157b7a60e8a6dec5
Hubert Hirtz <hubert@hirtz.pm>
2025-09-26 13:54:03 +0200

    wip work on range to discard maybe

--- train-sim/libtsim/src/lib.rs
+++ train-sim/libtsim/src/lib.rs
@@ -1,7 +1,6 @@
 // TODO add lint for missing docs
 
 pub use crate::range::Range;
-pub use crate::range::RangeKind;
 pub use crate::range::RangeMap;
 
 mod integration;
--- train-sim/libtsim/src/range.rs
+++ train-sim/libtsim/src/range.rs
@@ -1,24 +1,70 @@
-use std::collections::BTreeMap;
+//! Port of Google's RangeMap.
+//!
+//! Ref: https://guava.dev/releases/22.0/api/docs/com/google/common/collect/RangeMap.html
 
-pub enum RangeKind {
-    Open,
-    Closed,
-    OpenClosed,
-    ClosedOpen,
-    GreaterThan,
-    AtLeast,
-    LessThan,
-    AtMost,
+#![warn(missing_docs)]
+
+use std::collections::BTreeMap;
+use std::fmt;
+
+/// A range (or "interval") defines the boundaries around a contiguous span of values of some
+/// Comparable type; for example, "integers from 1 to 100 inclusive."
+///
+/// Unlike [std::ops] ranges, this type can represent ranges of mixed openness, as well as more
+/// kinds of ranges.
+///
+/// Note that it is not possible to iterate over these contained values.
+pub enum Range<T> {
+    /// `(start..end)` , or `{x | start < x < end}`
+    Open { start: T, end: T },
+
+    /// `[start..end]` , or `{x | start <= x <= end}`
+    Closed { start: T, end: T },
+
+    /// `(start..end]` , or `{x | start < x <= end}`
+    OpenClosed { start: T, end: T },
+
+    /// `[start..end)` , or `{x | start <= x < end}`
+    ClosedOpen { start: T, end: T },
+
+    /// `(start..+∞)` , or `{x | start < x}`
+    GreaterThan { start: T },
+
+    /// `[start..+∞)` , or `{x | start <= x}`
+    AtLeast { start: T },
+
+    /// `(-∞..end)` , or `{x | x < end}`
+    LessThan { end: T },
+
+    /// `(-∞..end]` , or `{x | x <= end}`
+    AtMost { end: T },
+
+    /// `(-∞..+∞)` , or `{x}`
     All,
 }
 
-pub struct Range<K> {
-    start: K,
-    end: K,
-    kind: RangeKind,
+impl<T> fmt::Debug for Range<T>
+where
+    T: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Open { start, end } => write!(f, "({start:?}..{end:?})"),
+            Self::Closed { start, end } => write!(f, "[{start:?}..{end:?}]"),
+            Self::OpenClosed { start, end } => write!(f, "({start:?}..{end:?}]"),
+            Self::ClosedOpen { start, end } => write!(f, "[{start:?}..{end:?})"),
+            Self::GreaterThan { start } => write!(f, "({start:?}..+∞)"),
+            Self::AtLeast { start } => write!(f, "[{start:?}..+∞)"),
+            Self::LessThan { end } => write!(f, "(-∞..{end:?})"),
+            Self::AtMost { end } => write!(f, "(-∞..{end:?}]"),
+            Self::All => write!(f, "(-∞..+∞)"),
+        }
+    }
 }
 
-// https://guava.dev/releases/22.0/api/docs/com/google/common/collect/RangeMap.html
+/// A mapping from disjoint nonempty ranges to non-null values.
+///
+/// Queries look up the value associated with the range (if any) that contains a specified key.
 pub struct RangeMap<K, V> {
     inner: BTreeMap<Range<K>, V>,
 }
@@ -32,6 +78,7 @@ impl<K, V> Default for RangeMap<K, V> {
 }
 
 impl<K, V> RangeMap<K, V> {
+    /// Creates a new empty [`RangeMap`].
     pub fn new() -> Self {
         Self::default()
     }
