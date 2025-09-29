0e86bd96516dbf13e8cb81ecd675f0b421189e17
Theo Macron <theo.macron0315@gmail.com>
2025-09-29 17:15:53 +0200

    front: adapt alertBox component to suit the new ui
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/src/common/AlertBox.tsx
+++ front/src/common/AlertBox.tsx
@@ -1,23 +1,34 @@
-import { Alert } from '@osrd-project/ui-icons';
+import { useState } from 'react';
+
+import { Alert, X } from '@osrd-project/ui-icons';
 
 type AlertType = 'warning';
 
 type AlertBoxProps = {
   type?: AlertType;
-  message: string;
+  message?: string;
+  closeable?: boolean;
 };
 
 const iconByType = {
   warning: <Alert variant="fill" size="lg" className="alert-box__icon--warning" />,
 };
 
-const AlertBox = ({ type = 'warning', message }: AlertBoxProps) => {
+const AlertBox = ({ type = 'warning', message, closeable }: AlertBoxProps) => {
+  const [visible, setVisible] = useState(true);
   const icon = iconByType[type];
+
+  if (!visible) return null;
 
   return (
     <div className={`alert-box alert-box--${type}`}>
       {icon}
       <span className={`alert-box__text--${type}`}>{message}</span>
+      {closeable && (
+        <button className="alert-box__close-button" onClick={() => setVisible(false)} type="button">
+          <X />
+        </button>
+      )}
     </div>
   );
 };
