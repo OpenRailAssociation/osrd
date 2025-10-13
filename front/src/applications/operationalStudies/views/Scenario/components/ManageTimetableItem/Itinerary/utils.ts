d5fb4d980886058f21296abc57a1b32a82e5a885
SharglutDev <p.filimon75@gmail.com>
2025-10-13 14:21:13 +0200

    front: display path step markers on itinerary modal map
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/src/applications/operationalStudies/views/Scenario/components/ManageTimetableItem/Itinerary/utils.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/ManageTimetableItem/Itinerary/utils.ts
@@ -2,3 +2,24 @@ import type { PathStepMetadata } from 'reducers/osrdconf/types';
 
 export const isOpRefMetadata = (pathStepMetadata: PathStepMetadata | undefined) =>
   !!pathStepMetadata && !pathStepMetadata.isInvalid && pathStepMetadata.type === 'opRef';
+
+export const computePathStepCoordinates = (pathStepMetadata: PathStepMetadata) => {
+  if (pathStepMetadata.isInvalid) return [];
+  if (pathStepMetadata.type === 'trackOffset') {
+    return [pathStepMetadata.coordinates];
+  }
+  if (pathStepMetadata.secondaryCode && pathStepMetadata.trackName) {
+    const locationMetadata = pathStepMetadata.locationsBySecondaryCode
+      .get(pathStepMetadata.secondaryCode)
+      ?.find((loc) => loc.trackName === pathStepMetadata.trackName);
+    return locationMetadata ? [locationMetadata.coordinates] : [];
+  }
+  if (pathStepMetadata.secondaryCode) {
+    const locationMetadata = pathStepMetadata.locationsBySecondaryCode.get(
+      pathStepMetadata.secondaryCode
+    );
+    return (locationMetadata ?? []).map((loc) => loc.coordinates);
+  }
+  const allMetadata = Array.from(pathStepMetadata.locationsBySecondaryCode.values()).flat();
+  return allMetadata.map((metadata) => metadata.coordinates);
+};
