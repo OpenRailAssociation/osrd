9d95af67d7662f4789c1a924804ce007c94316dc
Simon Ser <contact@emersion.fr>
2025-10-17 09:00:42 +0200

    front: stop post-processing generated code with ESLint
    ESLint doesn't change the generated code anymore. These files are
    ignored by the main ESLint config anyways.
    
    This speeds up the script.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/scripts/generate-types.sh
+++ front/scripts/generate-types.sh
@@ -3,9 +3,7 @@ if ! npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.cts; then
     echo "npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.cts command failed. Exit the script"
     exit 1
 fi
-npx eslint --fix src/common/api/generatedEditoastApi.ts --no-ignore
 if ! npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.cts; then
     echo "npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.cts command failed. Exit the script"
     exit 1
 fi
-npx eslint --fix src/common/api/osrdGatewayApi.ts --no-ignore
