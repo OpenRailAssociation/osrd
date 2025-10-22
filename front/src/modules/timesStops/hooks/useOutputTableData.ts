856538182bfb5afa596b79c2dc517829c49b59ed
Simon Ser <contact@emersion.fr>
2025-10-22 13:55:54 +0200

    front: inline formatPathStepRows() in useOutputTableData()
    This is a now-synchronous function called only once, no need to
    wrap the logic in a function anymore. Plus, the function name
    was misleading.
    
    No functional change. This commit is better reviewed by hiding
    whitespace changes in the diff view.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/modules/timesStops/hooks/useOutputTableData.ts
+++ front/src/modules/timesStops/hooks/useOutputTableData.ts
@@ -60,102 +60,98 @@ const useOutputTableData = (
 
   // Format input path step rows
   useEffect(() => {
-    const formatPathStepRows = (train: Train): Map<string, TimesStopsRow> => {
-      const startDatetime = new Date(train.start_time);
-      let lastReferenceDate = startDatetime;
-
-      return new Map(
-        train.path.map((pathStep, stepIndex) => {
-          const matchingOperationalPoint = pathStepOps.find((op) => matchOpRefAndOp(op, pathStep));
-
-          const name = getOperationalPointName(
-            matchingOperationalPoint,
-            pathStep,
-            stepIndex,
-            train.path.length,
-            t
-          );
-          const trackName =
-            'track' in pathStep
-              ? trackSections[pathStep.track]?.extensions?.sncf?.track_name
-              : getTrackReferenceLabel(pathStep.track_reference);
-
-          const schedule = scheduleByAt[pathStep.id];
-          const computedArrival = simulatedPathItemTimes
-            ? new Date(startDatetime.getTime() + simulatedPathItemTimes.final[stepIndex])
-            : undefined;
-          const { stopFor, shortSlipDistance, onStopSignal, calculatedDeparture } = formatSchedule(
-            computedArrival,
-            schedule
-          );
-          const { theoreticalArrival, arrival, departure, refDate } = computeInputDatetimes(
-            startDatetime,
-            lastReferenceDate,
-            schedule,
-            {
-              isDeparture: stepIndex === 0,
-            }
-          );
-          lastReferenceDate = refDate;
-
-          const {
-            theoreticalMargin,
-            isTheoreticalMarginBoundary,
-            theoreticalMarginSeconds,
-            calculatedMargin,
-            diffMargins,
-          } = computeMargins(
-            theoreticalMargins,
-            train,
-            scheduleByAt,
-            stepIndex,
-            simulatedPathItemTimes
-          );
-
-          const isOnTime =
-            theoreticalArrival && computedArrival
-              ? Duration.subtractDate(theoreticalArrival, computedArrival).abs() <=
-                ARRIVAL_TIME_ACCEPTABLE_ERROR
-              : false;
-          const calculatedArrival = computedArrival
-            ? isOnTime
-              ? theoreticalArrival!
-              : computedArrival
-            : undefined;
-
-          const pathStepRow = {
-            pathStepId: pathStep.id,
-            opId: matchingOperationalPoint?.id,
-            name,
-            ch: matchingOperationalPoint?.extensions?.sncf?.ch,
-            trackName,
-
-            arrival,
-            departure,
-            stopFor,
-            onStopSignal,
-            shortSlipDistance,
-            theoreticalMargin,
-            isTheoreticalMarginBoundary,
-
-            theoreticalMarginSeconds,
-            calculatedMargin,
-            diffMargins,
-            calculatedArrival,
-            calculatedDeparture,
-          };
-
-          return [pathStepRow.pathStepId, pathStepRow];
-        })
-      );
-    };
-
     if (!selectedTrain) {
       setRows([]);
       return;
     }
 
-    const pathStepRowsById = formatPathStepRows(selectedTrain);
+    const startDatetime = new Date(selectedTrain.start_time);
+    let lastReferenceDate = startDatetime;
+
+    const pathStepRowsById = new Map(
+      selectedTrain.path.map((pathStep, stepIndex) => {
+        const matchingOperationalPoint = pathStepOps.find((op) => matchOpRefAndOp(op, pathStep));
+
+        const name = getOperationalPointName(
+          matchingOperationalPoint,
+          pathStep,
+          stepIndex,
+          selectedTrain.path.length,
+          t
+        );
+        const trackName =
+          'track' in pathStep
+            ? trackSections[pathStep.track]?.extensions?.sncf?.track_name
+            : getTrackReferenceLabel(pathStep.track_reference);
+
+        const schedule = scheduleByAt[pathStep.id];
+        const computedArrival = simulatedPathItemTimes
+          ? new Date(startDatetime.getTime() + simulatedPathItemTimes.final[stepIndex])
+          : undefined;
+        const { stopFor, shortSlipDistance, onStopSignal, calculatedDeparture } = formatSchedule(
+          computedArrival,
+          schedule
+        );
+        const { theoreticalArrival, arrival, departure, refDate } = computeInputDatetimes(
+          startDatetime,
+          lastReferenceDate,
+          schedule,
+          {
+            isDeparture: stepIndex === 0,
+          }
+        );
+        lastReferenceDate = refDate;
+
+        const {
+          theoreticalMargin,
+          isTheoreticalMarginBoundary,
+          theoreticalMarginSeconds,
+          calculatedMargin,
+          diffMargins,
+        } = computeMargins(
+          theoreticalMargins,
+          selectedTrain,
+          scheduleByAt,
+          stepIndex,
+          simulatedPathItemTimes
+        );
+
+        const isOnTime =
+          theoreticalArrival && computedArrival
+            ? Duration.subtractDate(theoreticalArrival, computedArrival).abs() <=
+              ARRIVAL_TIME_ACCEPTABLE_ERROR
+            : false;
+        const calculatedArrival = computedArrival
+          ? isOnTime
+            ? theoreticalArrival!
+            : computedArrival
+          : undefined;
+
+        const pathStepRow = {
+          pathStepId: pathStep.id,
+          opId: matchingOperationalPoint?.id,
+          name,
+          ch: matchingOperationalPoint?.extensions?.sncf?.ch,
+          trackName,
+
+          arrival,
+          departure,
+          stopFor,
+          onStopSignal,
+          shortSlipDistance,
+          theoreticalMargin,
+          isTheoreticalMarginBoundary,
+
+          theoreticalMarginSeconds,
+          calculatedMargin,
+          diffMargins,
+          calculatedArrival,
+          calculatedDeparture,
+        };
+
+        return [pathStepRow.pathStepId, pathStepRow];
+      })
+    );
 
     let formattedRows: TimesStopsRow[] = [];
 
@@ -164,7 +160,7 @@ const useOutputTableData = (
       for (const op of operationalPointsOnPath) {
         const trackName = trackSections[op.part.track]?.extensions?.sncf?.track_name;
 
-        // early return if the op matches a pathStep (handled in formatPathStepRows)
+        // early return if the op matches a pathStep (handled above)
         // only add the trackName which has been found by the pathfinding (if not precised in the pathStep)
         const matchingPathStep = selectedTrain.path.find((pathStep) =>
           matchPathStepAndOp(pathStep, {
