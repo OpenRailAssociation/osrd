f6906af35a86b02f0074a5c1fa193e2f2956f2ee
Hubert Hirtz <hubert@hirtz.pm>
2025-10-24 10:50:27 +0200

    core: add PhysicsRollingStock.lowerPantographTime
    in case it's different from raisePantographTime... we should get rid of
    this property since we don't have the data i think?
    
    Co-authored-by: Martin Bourbier <mbourbier28@gmail.com>
    Signed-off-by: Hubert Hirtz <hubert@hirtz.pm>

--- core/osrd-mp/src/commonMain/kotlin/fr/sncf/osrd/envelope_sim/PhysicsRollingStock.kt
+++ core/osrd-mp/src/commonMain/kotlin/fr/sncf/osrd/envelope_sim/PhysicsRollingStock.kt
@@ -62,6 +62,8 @@ interface PhysicsRollingStock {
 
     /** Time to raise the pantograph from a fully lowered position */
     val raisePantographTime: Double?
+    val lowerPantographTime: Double?
+        get() = raisePantographTime
 
     /** The maximum acceleration, in m/s^2, which can be applied at a given speed, in m/s */
     data class TractiveEffortPoint(val speed: Double, val maxEffort: Double)
