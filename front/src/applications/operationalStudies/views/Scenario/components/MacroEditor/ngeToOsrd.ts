54ff0853a552d7b3a4c7acdd12e1e8cf353f2cae
Clara Ni <clara.ni@outlook.fr>
2025-11-05 16:26:53 +0100

    front: populate secondary code when updating the trigram of a macro node
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/ngeToOsrd.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/ngeToOsrd.ts
@@ -367,6 +367,25 @@ const generatePathAndSchedule = (
   return { start_time: startDate.toISOString(), path, schedule };
 };
 
+const fetchStationSecondaryCode = async (
+  trigram: string,
+  infraId: number,
+  dispatch: AppDispatch
+) => {
+  const searchPayload = {
+    object: 'operationalpoint',
+    query: ['and', ['=', ['infra_id'], infraId], ['=', ['trigram'], trigram]],
+  };
+  const searchResults = (await dispatch(
+    osrdEditoastApi.endpoints.postSearch.initiate({
+      searchPayload,
+    })
+  ).unwrap()) as SearchResultItemOperationalPoint[];
+
+  const stationOp = searchResults.find((op) => op.ch === 'BV' || op.ch === '00');
+  return stationOp?.ch;
+};
+
 // TODO: drop this function once this PR is merged:
 // https://github.com/OpenRailAssociation/osrd/pull/10325
 const populateSecondaryCodesInPath = async (
@@ -378,19 +397,7 @@ const populateSecondaryCodesInPath = async (
     if (!('trigram' in pathItem) || pathItem.secondary_code) {
       return;
     }
-
-    const searchPayload = {
-      object: 'operationalpoint',
-      query: ['and', ['=', ['infra_id'], infraId], ['=', ['trigram'], pathItem.trigram]],
-    };
-    const searchResults = (await dispatch(
-      osrdEditoastApi.endpoints.postSearch.initiate({
-        searchPayload,
-      })
-    ).unwrap()) as SearchResultItemOperationalPoint[];
-
-    const stationOp = searchResults.find((op) => op.ch === 'BV' || op.ch === '00');
-    pathItem.secondary_code = stationOp?.ch;
+    pathItem.secondary_code = await fetchStationSecondaryCode(pathItem.trigram, infraId, dispatch);
   });
 
   await Promise.all(promises);
@@ -983,12 +990,24 @@ const handleNodeOperation = async ({
         if (indexNode.dbId) {
           // Update the key if trigram has changed and key is based on it
           let nodeKey = indexNode.path_item_key;
-          if (nodeKey.startsWith('trigram:') && indexNode.trigram !== node.betriebspunktName) {
-            nodeKey = `trigram:${node.betriebspunktName}`;
+          let trigram = node.betriebspunktName;
+          if (nodeKey.startsWith('trigram:') && indexNode.trigram !== trigram) {
+            if (!trigram.includes('/')) {
+              const secondaryCode = await fetchStationSecondaryCode(
+                trigram,
+                state.infraId,
+                dispatch
+              );
+              if (secondaryCode) {
+                trigram = `${trigram}/${secondaryCode}`;
+              }
+            }
+            nodeKey = `trigram:${trigram}`;
           }
           await updateMacroNode(state, dispatch, {
             ...indexNode,
             ...castNgeNode(node, netzgrafikDto.labels),
+            trigram,
             dbId: indexNode.dbId,
             path_item_key: nodeKey,
           });
