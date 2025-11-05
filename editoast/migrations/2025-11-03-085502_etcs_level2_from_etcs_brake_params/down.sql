6d2f5bf931d242583de586674eb2909dcebe5485
Egor <egor@berezify.fr>
2025-11-05 11:54:36 +0100

    editoast: format migration scripts and make them more robust

--- editoast/migrations/2025-11-03-085502_etcs_level2_from_etcs_brake_params/down.sql
+++ editoast/migrations/2025-11-03-085502_etcs_level2_from_etcs_brake_params/down.sql
@@ -3,15 +3,16 @@ ADD COLUMN etcs_brake_params jsonb;
 
 UPDATE rolling_stock
 SET etcs_brake_params = (
-  SELECT value->'brake_params'
-  FROM jsonb_array_elements(supported_signaling_systems) AS elem(value)
-  WHERE jsonb_typeof(value) = 'object'
-    AND value ? 'brake_params'
-  LIMIT 1
+    SELECT value->'brake_params'
+    FROM jsonb_array_elements(supported_signaling_systems) AS elem(value)
+    WHERE jsonb_typeof(value) = 'object'
+        AND value ? 'brake_params'
+    LIMIT 1
 );
 
 UPDATE rolling_stock
-SET etcs_brake_params = 'null'::jsonb where etcs_brake_params is null;
+SET etcs_brake_params = 'null'::jsonb
+WHERE etcs_brake_params IS NULL;
 
 ALTER TABLE rolling_stock
 ALTER COLUMN etcs_brake_params SET NOT NULL,
@@ -22,9 +23,9 @@ ADD COLUMN supported_signaling_systems_text text[];
 
 UPDATE rolling_stock
 SET supported_signaling_systems_text = (
-  SELECT array_agg(trim(both '"' from elem.value::text))
-  FROM jsonb_array_elements(supported_signaling_systems) AS elem(value)
-  WHERE jsonb_typeof(elem.value) = 'string'
+    SELECT array_agg(trim(both '"' from elem.value::text))
+    FROM jsonb_array_elements(supported_signaling_systems) AS elem(value)
+    WHERE jsonb_typeof(elem.value) = 'string'
 );
 
 ALTER TABLE rolling_stock
--- editoast/migrations/2025-11-03-085502_etcs_level2_from_etcs_brake_params/up.sql
+++ editoast/migrations/2025-11-03-085502_etcs_level2_from_etcs_brake_params/up.sql
@@ -14,7 +14,8 @@ RENAME COLUMN supported_signaling_systems_jsonb TO supported_signaling_systems;
 
 UPDATE rolling_stock
 SET supported_signaling_systems = supported_signaling_systems || jsonb_build_array(jsonb_build_object('brake_params', etcs_brake_params))
-WHERE etcs_brake_params IS NOT null
-  AND jsonb_typeof(etcs_brake_params) = 'object';
+WHERE etcs_brake_params IS NOT NULL
+    AND  etcs_brake_params != 'null'::jsonb
+    AND jsonb_typeof(etcs_brake_params) = 'object';
 
 ALTER TABLE rolling_stock DROP COLUMN etcs_brake_params;
