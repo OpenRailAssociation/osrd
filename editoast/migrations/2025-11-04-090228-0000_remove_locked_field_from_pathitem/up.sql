39ab2fb9981e7aa4df14879f273af1c14f1dba1d
Florian Amsallem <florian.amsallem@gmail.com>
2025-11-06 14:04:43 +0100

    editoast: fix migration that remove locked and deleted fields
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/migrations/2025-11-04-090228-0000_remove_locked_field_from_pathitem/up.sql
+++ editoast/migrations/2025-11-04-090228-0000_remove_locked_field_from_pathitem/up.sql
@@ -1,14 +1,64 @@
 -- Your SQL goes here
 UPDATE train_schedule
 SET schedule = (
-  SELECT jsonb_agg(elem - 'locked')
-  FROM jsonb_array_elements(schedule) AS elem
-)
-WHERE schedule IS NOT NULL;
+    SELECT COALESCE(jsonb_agg(elem - 'locked'), '[]'::jsonb)
+    FROM jsonb_array_elements(schedule) AS elem
+);
 
 UPDATE train_schedule
 SET path = (
-  SELECT jsonb_agg(elem - 'deleted')
-  FROM jsonb_array_elements(path) AS elem
+    SELECT COALESCE(jsonb_agg(elem - 'deleted'), '[]'::jsonb)
+    FROM jsonb_array_elements(path) AS elem
+);
+
+-- For paced_train table
+UPDATE paced_train
+SET schedule = (
+    SELECT COALESCE(jsonb_agg(elem - 'locked'), '[]'::jsonb)
+    FROM jsonb_array_elements(schedule) AS elem
+);
+
+UPDATE paced_train
+SET path = (
+    SELECT COALESCE(jsonb_agg(elem - 'deleted'), '[]'::jsonb)
+    FROM jsonb_array_elements(path) AS elem
+);
+
+-- Also need to clean exceptions from these fields: remove "deleted" from each element of exception.path_and_schedule.path
+UPDATE paced_train
+SET exceptions = (
+    SELECT COALESCE(jsonb_agg(
+        CASE
+            WHEN ex->'path_and_schedule' IS NULL THEN ex
+            ELSE jsonb_set(
+                ex,
+                '{path_and_schedule,path}',
+                (
+                    SELECT COALESCE(jsonb_agg(path_item - 'deleted'), '[]'::jsonb)
+                    FROM jsonb_array_elements(ex->'path_and_schedule'->'path') AS path_item
+                )
+            )
+        END
+    ), '[]'::jsonb)
+    FROM jsonb_array_elements(exceptions) AS ex
 )
-WHERE path IS NOT NULL;
+WHERE exceptions IS NOT NULL;
+
+UPDATE paced_train
+SET exceptions = (
+    SELECT COALESCE(jsonb_agg(
+        CASE
+            WHEN ex->'path_and_schedule' IS NULL THEN ex
+            ELSE jsonb_set(
+                ex,
+                '{path_and_schedule,schedule}',
+                (
+                    SELECT COALESCE(jsonb_agg(sch - 'locked'), '[]'::jsonb)
+                    FROM jsonb_array_elements(ex->'path_and_schedule'->'schedule') AS sch
+                )
+            )
+        END
+    ), '[]'::jsonb)
+    FROM jsonb_array_elements(exceptions) AS ex
+)
+WHERE exceptions IS NOT NULL;
