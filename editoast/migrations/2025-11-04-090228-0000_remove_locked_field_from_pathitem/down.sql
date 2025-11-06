ec60375135fe595daeac8f3b9add3131b7517c5f
Florian Amsallem <florian.amsallem@gmail.com>
2025-11-06 14:04:43 +0100

    editoast: fix migration that remove locked and deleted fields
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/migrations/2025-11-04-090228-0000_remove_locked_field_from_pathitem/down.sql
+++ editoast/migrations/2025-11-04-090228-0000_remove_locked_field_from_pathitem/down.sql
@@ -1,14 +1,1 @@
--- This file should undo anything in `up.sql`
-UPDATE train_schedule
-SET schedule = (
-    SELECT jsonb_agg(jsonb_set(elem, '{locked}', 'true'::jsonb, true))
-    FROM jsonb_array_elements(schedule) AS elem
-)
-WHERE schedule IS NOT NULL;
-
-UPDATE train_schedule
-SET path = (
-    SELECT jsonb_agg(jsonb_set(elem, '{deleted}', 'false'::jsonb, true))
-    FROM jsonb_array_elements(path) AS elem
-)
-WHERE path IS NOT NULL;
+-- Not required since locked and deleted fields had a default values
