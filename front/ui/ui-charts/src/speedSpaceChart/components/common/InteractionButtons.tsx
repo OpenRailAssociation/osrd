904465d08dcc2b440ea20f035cf4276543028dc7
romainvalls <romain.valls95@gmail.com>
2025-11-14 12:20:16 +0100

    front: use correct icons in speedspacechart
    Signed-off-by: romainvalls <romain.valls95@gmail.com>

--- front/ui/ui-charts/src/speedSpaceChart/components/common/InteractionButtons.tsx
+++ front/ui/ui-charts/src/speedSpaceChart/components/common/InteractionButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Iterations, Dash, Plus, KebabHorizontal } from '@osrd-project/ui-icons';
+import { Iterations, Sliders } from '@osrd-project/ui-icons';
 import cx from 'classnames';
 
 import type { Store } from '../../types';
@@ -19,37 +19,24 @@ const InteractionButtons = ({
   testIdPrefix,
 }: InteractionButtonsProps) => (
   <div id="interaction-button-container" className="z-10">
-    <div className="zoom-buttons">
-      <button
-        className={cx('interaction-button reset-button', {
-          inactive: store.ratioX == 1 && store.leftOffset == 0,
-        })}
-        onClick={() => reset()}
-        data-testid={testIdPrefix ? `${testIdPrefix}-reset` : undefined}
-      >
-        <Iterations />
-      </button>
-      <button
-        className="interaction-button plus-button inactive"
-        data-testid={testIdPrefix ? `${testIdPrefix}-zoom-in` : undefined}
-      >
-        <Plus />
-      </button>
-      <button
-        className="interaction-button inactive"
-        data-testid={testIdPrefix ? `${testIdPrefix}-zoom-out` : undefined}
-      >
-        <Dash />
-      </button>
-    </div>
     <button
-      className="interaction-button elipsis-button"
-      data-testid={testIdPrefix ? `${testIdPrefix}-settings` : undefined}
-      onClick={() => openSettingsPanel()}
+      type="button"
+      className={cx('reset-button', {
+        'reset-button-disabled': store.ratioX == 1 && store.leftOffset == 0,
+      })}
+      onClick={reset}
+      data-testid={testIdPrefix ? `${testIdPrefix}-reset` : undefined}
     >
-      <KebabHorizontal />
+      <Iterations />
+    </button>
+    <button
+      type="button"
+      className="menu-button"
+      data-testid={testIdPrefix ? `${testIdPrefix}-settings` : undefined}
+      onClick={openSettingsPanel}
+    >
+      <Sliders />
     </button>
   </div>
 );
-
 export default InteractionButtons;
