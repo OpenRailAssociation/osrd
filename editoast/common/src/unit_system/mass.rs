455f7675ebaeb3739ce577dbde8f0fa6d195e708
Loup Federico <16464925+Sh099078@users.noreply.github.com>
2025-11-17 14:34:37 +0100

    editoast: uom: define a custom system of units for Editoast
    - It has the same base units as the International System of Units (ISQ)
      but different default measurement units (ex: meter -> centimeter).
      This will allow us to use u64 as the storage type for the different
      quantities without any precision loss. For more information on the
      subject, check the [design paragraph in the uom official
      documentation](https://docs.rs/uom/latest/uom/#design) explaining the
      issue faced when using the ISQ (based on meters) to store smaller
      values (millimeters in our case) in integers.
    - It allows us to own the quantity types of the units system and
      implement any trait we want on them directly without bumping into the
      orphan rule.
    - The available measurements are drastically reduced for maintenability
      purposes: only the ones that are used in Editoast have been kept in
      the new system.
    
    
    Signed-off-by: Loup Federico <16464925+Sh099078@users.noreply.github.com>

---
+++ editoast/common/src/unit_system/mass.rs
@@ -0,0 +1,13 @@
+quantity! {
+    /// Mass (base unit kilogram, kg).
+    quantity: Mass; "mass";
+    /// Dimension of mass, M (base unit kilogram, kg).
+    dimension: ESQ<
+        Z0,     // length
+        P1,     // mass
+        Z0>;    // time
+
+    units {
+        @kilogram: 1.0; "kg", "kilogram", "kilograms";
+    }
+}
