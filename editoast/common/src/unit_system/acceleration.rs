fb7989fc131dda3d6404f52e12077c02b95b9d4d
Loup Federico <16464925+Sh099078@users.noreply.github.com>
2025-11-17 15:43:28 +0100

    editoast: uom: extend the ESQ with the composed units used in Editoast
    Add to the Editoast System of Quantities (ESQ) the different composed
    units that are used in osrd.
    - These units are named in a similar fashion as they are in the
      International System of Quantities.
    - Keep the existing type aliases made to the SI units when they are
      known / used under a different name in the context of OSRD (example:
      the type alias `SolidFriction` for `Force`).
    - Only keep the measurements used in OSRD.
    
    
    Signed-off-by: Loup Federico <16464925+Sh099078@users.noreply.github.com>

---
+++ editoast/common/src/unit_system/acceleration.rs
@@ -0,0 +1,15 @@
+//! Acceleration (base unit meter per second squared, m · s⁻²).
+
+quantity! {
+    /// Acceleration (base unit meter per second squared, m · s⁻²).
+    quantity: Acceleration; "acceleration";
+    /// Dimension of acceleration, LT⁻² (base unit meter per second squared, m · s⁻²).
+    dimension: ESQ<
+        P1,     // length
+        Z0,     // mass
+        N2>;    // time
+
+    units {
+        @meter_per_second_squared: prefix!(none); "m/s²", "meter per second squared", "meters per second squared";
+    }
+}
