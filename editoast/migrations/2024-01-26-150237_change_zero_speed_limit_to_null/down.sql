b00962963af528a57606a6ac830868310790eb53
Loup Federico <16464925+Sh099078@users.noreply.github.com>
2025-11-19 17:20:53 +0100

    editoast: add diesel migration revert to change_zero_speed_limit_to_null
    Implement the revert script that had been left empty for that migration.
    
    Signed-off-by: Loup Federico <16464925+Sh099078@users.noreply.github.com>

--- editoast/migrations/2024-01-26-150237_change_zero_speed_limit_to_null/down.sql
+++ editoast/migrations/2024-01-26-150237_change_zero_speed_limit_to_null/down.sql
@@ -1,1 +1,3 @@
--- This file should undo anything in `up.sql`
+UPDATE infra_object_speed_section
+SET data = jsonb_set(data, '{speed_limit}', to_jsonb(0))
+WHERE (data->>'speed_limit')::float IS NULL;
