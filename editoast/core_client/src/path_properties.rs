462d3f04b13a2e3334a91b9347208e7e3b3aa9d5
aschantraine <aschantraine@free.fr>
2025-11-26 11:32:17 +0100

    editoast: core path_properties openapi schema namespacing
    Signed-off-by: aschantraine <aschantraine@free.fr>

--- editoast/core_client/src/path_properties.rs
+++ editoast/core_client/src/path_properties.rs
@@ -42,6 +42,7 @@ pub struct PathPropertiesResponse {
 
 /// Property f64 values along a path. Each value is associated to a range of the path.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[schema(as = core::PropertyValuesF64)]
 #[cfg_attr(feature = "mocking_client", derive(PartialEq))]
 pub struct PropertyValuesF64 {
     /// List of `n` boundaries of the ranges.
@@ -61,6 +62,7 @@ impl PropertyValuesF64 {
 
 /// Electrification property along a path. Each value is associated to a range of the path.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[schema(as = core::PropertyElectrificationValues)]
 #[cfg_attr(feature = "mocking_client", derive(PartialEq))]
 pub struct PropertyElectrificationValues {
     /// List of `n` boundaries of the ranges.
@@ -80,6 +82,7 @@ impl PropertyElectrificationValues {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[schema(as = core::PropertyElectrificationValue)]
 #[cfg_attr(feature = "mocking_client", derive(PartialEq))]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum PropertyElectrificationValue {
@@ -93,6 +96,7 @@ pub enum PropertyElectrificationValue {
 
 /// Operational point along a path.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[schema(as = core::OperationalPointOnPath)]
 #[cfg_attr(feature = "mocking_client", derive(PartialEq))]
 pub struct OperationalPointOnPath {
     /// Id of the operational point
@@ -132,6 +136,7 @@ impl OperationalPointOnPath {
 
 /// Zones along a path. Each value is associated to a range of the path.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+#[schema(as = core::PropertyZoneValues)]
 pub struct PropertyZoneValues {
     /// List of `n` boundaries of the ranges.
     /// A boundary is a distance from the beginning of the path in mm.
