OpenRailAssociation/osrd

commit c8fe8ccd04f45da73025d207f73973f4f459f438

front: drop unnecessary trigram fallback in Graou import
If a step doesn't have a valid UIC, the Graou import logic falls
back to the trigram.

This fallback was introduced in 02ea4601ad80 ("front: fix open
data import") to handle XML files. Now XML import has been decoupled
from Graou import, but this remnant hasn't been dropped.

Remove the fallback because Graou always returns the UIC.

Signed-off-by: Simon Ser <contact@emersion.fr>

front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/generateTrainSchedulesPayloads.ts
@@ -13,23 +13,16 @@ function generateTrainSchedulePayload(train: GraouTrainSchedule): TrainSchedule 
     (acc, step) => {
       const stepId = uuidV4();
 
-      const validUICNumber = !Number.isNaN(step.uic);
-
-      if (validUICNumber) {
-        acc.path.push({
-          id: stepId,
-          location: {
-            operational_point: { uic: Number(step.uic), secondary_code: step.chCode },
-          },
-        });
-      } else {
-        acc.path.push({
-          id: stepId,
-          location: {
-            operational_point: { trigram: step.name, secondary_code: step.chCode },
-          }, // we use ocpRef when uic is NaN
-        });
+      if (!Number.isNaN(step.uic)) {
+        throw new Error('Invalid UIC');
       }
+
+      acc.path.push({
+        id: stepId,
+        location: {
+          operational_point: { uic: Number(step.uic), secondary_code: step.chCode },
+        },
+      });
 
       if (acc.path.length > 1) {
         const arrivalTime = new Date(step.arrivalTime);

commit 580d12e55c68b372c8b0adca1cb8ba5b79d9c363

front: fix rejected UICs in Graou imports
I messed up with an inverted condition when pushing the last commit
in #14203. We want to reject NaNs, not accept them.

Signed-off-by: Simon Ser <contact@emersion.fr>
Fixes: c8fe8ccd04f4 ("front: drop unnecessary trigram fallback in Graou import")

front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/generateTrainSchedulesPayloads.ts
@@ -13,14 +13,15 @@ function generateTrainSchedulePayload(train: GraouTrainSchedule): TrainSchedule 
     (acc, step) => {
       const stepId = uuidV4();
 
-      if (!Number.isNaN(step.uic)) {
+      const uic = Number(step.uic);
+      if (Number.isNaN(uic)) {
         throw new Error('Invalid UIC');
       }
 
       acc.path.push({
         id: stepId,
         location: {
-          operational_point: { uic: Number(step.uic), secondary_code: step.chCode },
+          operational_point: { uic, secondary_code: step.chCode },
         },
       });
 