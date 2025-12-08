OpenRailAssociation/osrd

commit ec6bdc82f17a225895a633b37a14795e761d11c3

front: keep lonely nodes in the Macro mode
- do not delete the empty nodes when loading the nodes saved in the db
- create the dict ngeNodesByPathKey from all the nodes in the state, and not
only when handling the trainrunSections (otherwise lonely nodes are not displayed)

Signed-off-by: Clara Ni <clara.ni@outlook.fr>

front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/osrdToNge.ts
@@ -28,7 +28,6 @@ import {
 } from './consts';
 import MacroEditorState, { type NodeIndexed } from './MacroEditorState';
 import {
-  deleteMacroNodeByDbId,
   getDefaultTrainrunFrequencies,
   getNetzgrafikColors,
   getSavedMacroNodes,
@@ -325,7 +324,8 @@ export const loadAndIndexNge = async (
       if (state.getNodeByKey(n.path_item_key) !== null) {
         state.updateNodeDataByKey(n.path_item_key, { ...n, dbId: n.id });
       } else {
-        await deleteMacroNodeByDbId(state, dispatch, n.id);
+        state.indexNodeByKey(n.path_item_key, { ...n, dbId: n.id, ngeId: nbNodesIndexed });
+        nbNodesIndexed += 1;
       }
     })
   );
@@ -442,6 +442,11 @@ const getNgeTrainrunSectionsWithNodes = (
 
   // Track nge nodes
   const ngeNodesByPathKey: Record<string, NetzgrafikDto['nodes'][0]> = {};
+  for (const node of state.nodes) {
+    if (!node) continue;
+    ngeNodesByPathKey[node.path_item_key] = castNodeToNge(state, node, labels);
+  }
+
   let trainrunSectionId = 0;
   const trainrunSections: TrainrunSectionDto[] = groupedTimetableItems.flatMap(
     ([timetableItem, returnTimetableItem], index) => {
@@ -465,25 +470,8 @@ const getNgeTrainrunSectionsWithNodes = (
         // list to the start.
         const returnIndex = timetableItem.path.length - 1 - i;
 
-        // Get the source node or created it
-        if (!ngeNodesByPathKey[sourceNodeKey]) {
-          ngeNodesByPathKey[sourceNodeKey] = castNodeToNge(
-            state,
-            state.getNodeByKey(sourceNodeKey)!,
-            labels
-          );
-        }
         const sourceNode = ngeNodesByPathKey[sourceNodeKey];
-
-        // Get the target node or created it
         const targetNodeKey = pathNodeKeys[i + 1];
-        if (!ngeNodesByPathKey[targetNodeKey]) {
-          ngeNodesByPathKey[targetNodeKey] = castNodeToNge(
-            state,
-            state.getNodeByKey(targetNodeKey)!,
-            labels
-          );
-        }
         const targetNode = ngeNodesByPathKey[targetNodeKey];
 
         // Adding port

commit 66ddd628cba842e642f5fa7bcda965a9781c367b

front: fix NGE node ID collision
We insert new nodes in two spots: first we iterate over all path
steps from all trains, then we grab saved macro nodes from the
database. The first loop uses indices as NGE IDs, while the second
loop uses a counter.

If some nodes were skipped by the first loop, the last path step
index would be larger than the number of new nodes. This causes a
collision: the second loop would use NGE IDs already used by the
first loop.

Fix this by using a counter everywhere.

Signed-off-by: Simon Ser <contact@emersion.fr>
Closes: https://github.com/OpenRailAssociation/osrd/issues/13608
Fixes: ec6bdc82f17a ("front: keep lonely nodes in the Macro mode")

front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/osrdToNge.ts
@@ -300,11 +300,11 @@ export const loadAndIndexNge = async (
   let nbNodesIndexed = 0;
   timetableItems
     .flatMap((timetableItem) => timetableItem.path)
-    .forEach((pathItem, index) => {
+    .forEach((pathItem) => {
       const key = MacroEditorState.getPathKey(pathItem.location);
       if (!state.getNodeByKey(key)) {
         const macroNode: NodeIndexed = {
-          ngeId: index,
+          ngeId: nbNodesIndexed,
           path_item_key: key,
           trigram:
             'operational_point' in pathItem.location &&