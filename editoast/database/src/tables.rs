a6c27c2d4065649f895d962efc79b93516bdf419
Florian Amsallem <florian.amsallem@gmail.com>
2025-12-09 17:48:10 +0100

    editoast: add migration to move train schedules to paced trains
    - Make paced fields nullable
    - Move train schedules to paced trains
    - Move round trips with them
    
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/database/src/tables.rs
+++ editoast/database/src/tables.rs
@@ -495,8 +495,8 @@ diesel::table! {
         speed_limit_tag -> Nullable<Varchar>,
         power_restrictions -> Jsonb,
         options -> Jsonb,
-        time_window -> Interval,
-        interval -> Interval,
+        time_window -> Nullable<Interval>,
+        interval -> Nullable<Interval>,
         main_category -> Nullable<TrainMainCategory>,
         exceptions -> Jsonb,
         #[max_length = 255]
