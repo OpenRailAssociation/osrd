b0faa58562ba21602fd550ad02235e2450ff87fa
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2025-12-11 10:00:14 +0100

    editoast: add get train_schedule_set and catalogue_entries endpoints skeletons
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>

--- editoast/src/views/mod.rs
+++ editoast/src/views/mod.rs
@@ -461,9 +461,11 @@ fn service_router() -> router::DocumentedRouter {
             //
             .nests("/train_schedule_set", |path| {
                 path.route("/", post!(train_schedule_set::post))
+                    .route("/", get!(train_schedule_set::get))
             })
             .nests("/catalogue_entry", |path| {
                 path.route("/", post!(catalogue_entry::post))
+                    .route("/", get!(catalogue_entry::get))
             })
     })
 }
