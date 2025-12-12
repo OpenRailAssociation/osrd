OpenRailAssociation/osrd

commit fb24ab4ce207b725f9071b51792b40e14e8e203f

front: persist nodes from file import in nge
Signed-off-by: romainvalls <romain.valls95@gmail.com>

front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/utils.ts
@@ -149,6 +149,24 @@ export const storeRoundTrip = async (
 };
 
 /**
+ * Check if every new nodes (from import) are already in the DB (by path_item_key).
+ * If not we persist them.
+ */
+export const storeTrainPathNodes = async (state: MacroEditorState, dispatch: AppDispatch) => {
+  const allNodes = state.nodes.filter((n) => !n?.dbId);
+
+  if (!allNodes.length) return;
+
+  const payload = allNodes.map((n) => omit(n, ['ngeId', 'geocoord']));
+
+  await dispatch(
+    osrdEditoastApi.endpoints.postMacroNodes.initiate({
+      macroNodeBatchForm: { macro_nodes: payload, scenario_id: state.scenarioId },
+    })
+  ).unwrap();
+};
+
+/**
  * Return the default TrainrunFrequencies with their translated names.
  */
 export const getDefaultTrainrunFrequencies = (

front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/osrdToNge.ts
@@ -39,6 +39,7 @@ import {
   getTrainrunCategoryId,
   getTrainrunFrequencyFromTimetableItem,
   getTrainrunTimeCategoryFromFrequency,
+  storeTrainPathNodes,
 } from './utils';
 import {
   type PortDto,
@@ -372,6 +373,8 @@ export const loadAndIndexNge = async (
 
   // Now that we have all nodes, we apply a layout
   applyLayout(state, timetableItems);
+
+  await storeTrainPathNodes(state, dispatch);
 };
 
 /**

front/tests/023-osrd-nge.spec.ts
@@ -124,8 +124,8 @@ test.describe('Verify osrd nge conversion', () => {
       await ngePage.enableMacroViewWithDefaultTrainList();
     });
 
-    await test.step('Verify NGE graph is empty (no nodes or train lines)', async () => {
-      await ngePage.verifyNodeAndLinesCount({ nodes: 0, lines: 0 });
+    await test.step('Verify NGE lines are deleted (no train lines but nodes are persisted)', async () => {
+      await ngePage.verifyNodeAndLinesCount({ nodes: 3, lines: 0 });
     });
   });
 
@@ -151,9 +151,9 @@ test.describe('Verify osrd nge conversion', () => {
       await scenarioTimetableSection.verifyTimetableIsEmpty(frTranslations.timetable.noTrain);
     });
 
-    await test.step('Re-toggle macro layout and re-assert graph is empty', async () => {
+    await test.step('Re-toggle macro layout and re-assert graph is empty (no lines, nodes are persisted)', async () => {
       await ngePage.enableMacroViewWithDefaultTrainList();
-      await ngePage.verifyNodeAndLinesCount({ nodes: 0, lines: 0 });
+      await ngePage.verifyNodeAndLinesCount({ nodes: 3, lines: 0 });
     });
   });
 });

commit e68ad7b34a3fffb07bb40fbd0c41088403e03d91

front: avoid sending empty nodes in storeTrainPathNodes()
MacroEditorState.nodes includes null entries in case some nodes
got dedup'ed. The filter() condition retains these null nodes,
resulting in empty objects sent to editoast and a 400 HTTP error
when opening the macro editor.

Ignore null nodes in the filter() condition to fix this.

Signed-off-by: Simon Ser <contact@emersion.fr>
Fixes: fb24ab4ce207 ("front: persist nodes from file import in nge")
Closes: https://github.com/OpenRailAssociation/osrd/issues/14425

front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/utils.ts
@@ -153,7 +153,7 @@ export const storeRoundTrip = async (
  * If not we persist them.
  */
 export const storeTrainPathNodes = async (state: MacroEditorState, dispatch: AppDispatch) => {
-  const allNodes = state.nodes.filter((n) => !n?.dbId);
+  const allNodes = state.nodes.filter((n) => n && !n.dbId);
 
   if (!allNodes.length) return;
 