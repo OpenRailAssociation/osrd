0ebcb26490aa035e022e50f692cd149c5ad93b93
Benoit Simard <contact@bsimard.com>
2025-12-16 15:08:46 +0100

    ui-core: fix select component for tab selection

--- front/ui/ui-core/src/components/Select.tsx
+++ front/ui/ui-core/src/components/Select.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import cx from 'classnames';
 
 import FieldWrapper, { type FieldWrapperProps } from './inputs/FieldWrapper';
+import useFocusByTab from '../hooks/useFocusByTab';
 
 export type SelectProps<T> = Omit<
   React.InputHTMLAttributes<HTMLSelectElement>,
@@ -34,17 +35,25 @@ const Select = <T,>({
   getOptionLabel,
   getOptionValue,
   onChange,
+  onBlur,
+  onKeyUp,
   ...props
 }: SelectProps<T>) => {
   const [selectedOption, setSelectedOption] = useState<T | undefined>(value);
-  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newSelectedOption =
-      e.target.value === PLACEHOLDER_VALUE
-        ? undefined
-        : options.find((option) => getOptionValue(option) === e.target.value);
-    setSelectedOption(newSelectedOption);
-    onChange(newSelectedOption);
-  };
+  const { handleKeyUp, handleBlur, isFocusByTab } = useFocusByTab({ onBlur, onKeyUp });
+  console.log('focus on tab', isFocusByTab);
+
+  const handleOnChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const newSelectedOption =
+        e.target.value === PLACEHOLDER_VALUE
+          ? undefined
+          : options.find((option) => getOptionValue(option) === e.target.value);
+      setSelectedOption(newSelectedOption);
+      onChange(newSelectedOption);
+    },
+    [options, getOptionValue, onChange]
+  );
 
   useEffect(() => {
     setSelectedOption(value);
@@ -60,17 +69,25 @@ const Select = <T,>({
       disabled={disabled}
       small={small}
       narrow={narrow}
+      className={cx('ui-input-field-wrapper')}
     >
       <select
         id={id}
-        className={cx('ui-select', statusWithMessage?.status, {
-          'placeholder-selected': placeholder && !selectedOption,
-          small,
-          'read-only': readOnly,
-        })}
+        className={cx(
+          'ui-select',
+          statusWithMessage?.status,
+          {
+            'placeholder-selected': placeholder && !selectedOption,
+            small,
+            'read-only': readOnly,
+          },
+          isFocusByTab && 'focused-by-tab'
+        )}
         value={selectedOption ? getOptionValue(selectedOption) : undefined}
         required={required}
         disabled={disabled || readOnly}
+        onKeyUp={handleKeyUp}
+        onBlur={handleBlur}
         onChange={handleOnChange}
         {...props}
       >
