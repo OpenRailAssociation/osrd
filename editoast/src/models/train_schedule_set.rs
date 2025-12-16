36b453318d12d8f9497d37c58627dc66e23deec1
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2025-12-16 05:29:34 +0100

    editoast: add database connexion to catalog_entry endpoints
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>

--- editoast/src/models/train_schedule_set.rs
+++ editoast/src/models/train_schedule_set.rs
@@ -1,8 +1,12 @@
+use editoast_derive::Model;
 use serde::Deserialize;
 use serde::Serialize;
 use utoipa::ToSchema;
 
-#[derive(Deserialize, Serialize, ToSchema)]
+#[derive(Deserialize, Serialize, ToSchema, Debug, Clone, Model)]
+#[model(table = database::tables::train_schedule_set)]
+#[model(gen(ops = crud, batch_ops = crud, list))]
+#[model(row(derive(diesel::QueryableByName)))]
 pub struct TrainScheduleSet {
     pub id: i64,
     pub catalog_entry_id: Option<i64>,
