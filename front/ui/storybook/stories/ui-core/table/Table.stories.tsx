062b7377e2c785db9fc20e75d579b1d9de51671d
Theo Macron <theo.macron0315@gmail.com>
2026-01-07 17:20:27 +0100

    ui: fix table style to match osrd use and sample datas
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/ui/storybook/stories/ui-core/table/Table.stories.tsx
+++ front/ui/storybook/stories/ui-core/table/Table.stories.tsx
@@ -3,39 +3,11 @@ import React from 'react';
 import { Table } from '@osrd-project/ui-core';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { SAMPLE_COLUMNS, SAMPLE_DATA, TABLE_ROW_BACKGROUNDS } from './assets/sampleTableData';
+import { SAMPLE_COLUMNS, SAMPLE_DATA } from './assets/sampleTableData';
 
-const TableStoryWrapper = ({
-  ambientMode,
-  columns,
-  data,
-}: {
-  ambientMode: keyof typeof TABLE_ROW_BACKGROUNDS;
-  columns: typeof SAMPLE_COLUMNS;
-  data: typeof SAMPLE_DATA;
-}) => {
-  const theme = TABLE_ROW_BACKGROUNDS[ambientMode];
-
-  return (
-    <div
-      className="ambient-wrapper"
-      style={
-        {
-          '--mix-color-1': theme.mix1,
-          '--mix-color-2': theme.mix2,
-          '--row-bg-odd': theme.odd,
-          '--row-bg-even': theme.even,
-        } as React.CSSProperties
-      }
-    >
-      <Table columns={columns} data={data} />
-    </div>
-  );
-};
-
-const meta: Meta<typeof TableStoryWrapper> = {
+const meta: Meta<typeof Table> = {
   title: 'Core/Table',
-  component: TableStoryWrapper,
+  component: Table,
   decorators: [(Story) => <Story />],
   parameters: {
     backgrounds: {
@@ -49,34 +21,34 @@ const meta: Meta<typeof TableStoryWrapper> = {
     },
   },
   args: {
-    ambientMode: 'ambientB',
     columns: SAMPLE_COLUMNS,
     data: SAMPLE_DATA,
+    ambientVariant: 'B',
   },
   argTypes: {
-    ambientMode: {
+    ambientVariant: {
       control: 'select',
-      options: ['ambientA', 'ambientB', 'ambientC', 'ambientD'],
+      options: ['A', 'B', 'C', 'D'],
     },
   },
-  render: (args) => <TableStoryWrapper {...args} />,
+  render: (args) => <Table {...args} />,
   tags: ['autodocs'],
 };
 
 export default meta;
 
-type Story = StoryObj<typeof TableStoryWrapper>;
+type Story = StoryObj<typeof Table>;
 
 export const DefaultTable: Story = {
   args: {
-    ambientMode: 'ambientB',
     columns: SAMPLE_COLUMNS,
     data: SAMPLE_DATA,
+    ambientVariant: 'B',
   },
   argTypes: {
-    ambientMode: {
+    ambientVariant: {
       control: 'select',
-      options: ['ambientA', 'ambientB', 'ambientC', 'ambientD'],
+      options: ['A', 'B', 'C', 'D'],
     },
   },
 };
