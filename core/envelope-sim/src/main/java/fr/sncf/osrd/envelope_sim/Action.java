fdfb0fa8f6d7eb35a3ebadbfc59b11fb66c56ee7
Hubert Hirtz <hubert@hirtz.pm>
2026-01-12 10:46:25 +0100

    core: convert Action to kotlin
    Needed in order to move it to KMP for the train simulation module
    
    Signed-off-by: Hubert Hirtz <hubert@hirtz.pm>

--- core/envelope-sim/src/main/java/fr/sncf/osrd/envelope_sim/Action.java
+++
@@ -1,8 +0,0 @@
-package fr.sncf.osrd.envelope_sim;
-
-public enum Action {
-    ACCELERATE,
-    BRAKE,
-    MAINTAIN,
-    COAST
-}
---
+++ core/envelope-sim/src/main/java/fr/sncf/osrd/envelope_sim/Action.kt
@@ -0,0 +1,8 @@
+package fr.sncf.osrd.envelope_sim
+
+enum class Action {
+    ACCELERATE,
+    BRAKE,
+    MAINTAIN,
+    COAST,
+}
