592d1af58dc3d26a83dc79f95c50f08ef71c4816
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2026-01-13 11:01:22 +0100

    scripts: download-timetable: use async tooling from other script
    Signed-off-by: Eloi Charpentier <eloi.charpentier.42@gmail.com>

--- scripts/download_timetable.py
+++ scripts/download_timetable.py
@@ -1,11 +1,18 @@
+#! /usr/bin/env -S uv run --script
 #
 # This script can be used to download the content of a timetable knowing only its ID,
 # as a JSON file that can be re-imported in OSRD.
 #
+# /// script
+# dependencies = ["click", "aiohttp"]
+# ///
+import asyncio
 
-import requests
+import aiohttp
 import json
-from typing import Dict, List
+from typing import Dict
+
+from download_stdcm_requirements import get_paginated, make_connector
 
 EDITOAST_URL = "https://rec-osrd.reseau.sncf.fr/"
 COOKIES = {
@@ -16,45 +23,26 @@ TIMETABLE_ID = 1
 OUT_PATH = "timetable.json"
 
 
-def get_paginated(url: str, *args, **kwargs) -> List[Dict]:
-    page = 1
-    res = []
-    while page is not None:
-        try:
-            response = requests.get(url.replace("$page", str(page)), *args, **kwargs)
-            response.raise_for_status()
-            json_response = response.json()
-            res += json_response["results"]
-            page = json_response.get("next", None)
-        except Exception as e:
-            print(e)
-            time.sleep(1)
-    return res
-
-
-def download_timetable(timetable_id: int) -> Dict:
-    train_schedules_url = f"{EDITOAST_URL}api/timetable/{timetable_id}/train_schedules/?page=$page"
+async def download_timetable(timetable_id: int) -> Dict:
     paced_trains_url = f"{EDITOAST_URL}api/timetable/{timetable_id}/paced_trains/?page=$page"
-    raw_schedules = get_paginated(train_schedules_url, cookies=COOKIES, verify=False)
-    raw_paced_trains = get_paginated(paced_trains_url, cookies=COOKIES, verify=False)
-    schedules = []
+    cookies, connector = make_connector(COOKIES["gateway"])
+    async with aiohttp.ClientSession(
+        trust_env=True, raise_for_status=True, cookies=cookies, connector=connector
+    ) as session:
+        raw_paced_trains = await get_paginated(paced_trains_url, session)
     paced_trains = []
-    for schedule in raw_schedules:
-        del schedule["id"]
-        del schedule["timetable_id"]
-        schedules.append(schedule)
     for paced_train in raw_paced_trains:
         del paced_train["id"]
         del paced_train["timetable_id"]
         paced_trains.append(paced_train)
     return {
-        "train_schedules": schedules,
+        "train_schedules": [],
         "paced_trains":paced_trains,
     }
 
 
 if __name__ == "__main__":
-    trains = download_timetable(TIMETABLE_ID)
+    trains = asyncio.run(download_timetable(TIMETABLE_ID))
     with open(OUT_PATH, "w", encoding="utf-8") as jsonfile:
         json.dump(trains, jsonfile, ensure_ascii=False, indent=4)
     print(f"dumped timetable {TIMETABLE_ID} to {OUT_PATH}")
