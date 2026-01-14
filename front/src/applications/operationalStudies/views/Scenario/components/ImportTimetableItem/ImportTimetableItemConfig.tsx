d8f90a4507e61da27956bc22bcd47ce715863aae
Alice Khoudli <alice.khoudli@polytechnique.org>
2026-01-14 01:42:50 +0100

    front: drop graou import usage
    Signed-off-by: Alice Khoudli <alice.khoudli@polytechnique.org>

--- front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/ImportTimetableItemConfig.tsx
+++ front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/ImportTimetableItemConfig.tsx
@@ -1,34 +1,19 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 
-import { Download, Search } from '@osrd-project/ui-icons';
+import { Download } from '@osrd-project/ui-icons';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
-import { useScenarioContext } from 'applications/operationalStudies/hooks/useScenarioContext';
 import type { TimetableJsonPayload } from 'applications/operationalStudies/types';
-import {
-  type GraouStation,
-  type GraouTrainScheduleConfig,
-  getGraouTrainSchedules,
-} from 'common/api/graouApi';
-import { osrdEditoastApi } from 'common/api/osrdEditoastApi';
 import { osrdRailwayManagerApi } from 'common/api/osrdRailwayManagerApi';
-import InputSNCF from 'common/BootstrapSNCF/InputSNCF';
 import { ModalContext } from 'common/BootstrapSNCF/ModalSNCF/ModalProvider';
-import StationCard from 'common/StationCard';
 import UploadFileModal from 'common/uploadFileModal';
-import { setFailure, setWarning } from 'reducers/main';
+import { setFailure } from 'reducers/main';
 import { getRailwayManagerInterfaceUrl } from 'reducers/main/mainSelector';
 import { useAppDispatch } from 'store';
-import { formatLocalDate } from 'utils/date';
 import { castErrorToFailure } from 'utils/error';
 
-import {
-  generateTrainSchedulesPayloads,
-  populateMissingSecondaryCodes,
-} from './helpers/parseGraouTrains';
 import parseXML from './helpers/parseXML';
-import StationSelector from './ImportTimetableItemStationSelector';
 import {
   handleFileReadingError,
   processJsonFile,
@@ -45,98 +30,10 @@ const ImportTimetableItemConfig = ({
 }: ImportTimetableItemConfigProps) => {
   const { t } = useTranslation('operational-studies', { keyPrefix: 'importTrains' });
   const railwayManagerUrl = useSelector(getRailwayManagerInterfaceUrl);
-  const { infraId } = useScenarioContext();
-  const [from, setFrom] = useState<GraouStation | undefined>();
-  const [fromSearchString, setFromSearchString] = useState('');
-  const [to, setTo] = useState<GraouStation | undefined>();
-  const [toSearchString, setToSearchString] = useState('');
-  const [date, setDate] = useState(formatLocalDate(new Date()));
-  const [startTime, setStartTime] = useState('00:00');
-  const [endTime, setEndTime] = useState('23:59');
   const dispatch = useAppDispatch();
   const { openModal, closeModal } = useContext(ModalContext);
   const [postTransformTimetable] =
     osrdRailwayManagerApi.endpoints.postTransformTimetable.useMutation();
-  const [postInfraByInfraIdMatchOperationalPoints] =
-    osrdEditoastApi.endpoints.postInfraByInfraIdMatchOperationalPoints.useLazyQuery();
-
-  async function getTrainsFromOpenData(config: GraouTrainScheduleConfig) {
-    try {
-      setIsLoading(true);
-      setTrainsJsonData({ train_schedules: [], paced_trains: [] });
-
-      const {
-        trainSchedules: graouTrains,
-        rejectedTrainsCount,
-        modifiedTrainsNames,
-      } = await getGraouTrainSchedules(config);
-      if (rejectedTrainsCount)
-        dispatch(
-          setWarning({
-            title: t('warningMessages.warning'),
-            text: t('warningMessages.warningRejectedTrainsImport', {
-              rejectedTrainsCount,
-            }),
-          })
-        );
-      if (modifiedTrainsNames.length)
-        dispatch(
-          setWarning({
-            title: t('warningMessages.warning'),
-            text: t('warningMessages.warningFilteredStepImport', {
-              modifiedTrainsNames,
-            }),
-          })
-        );
-
-      const trainSchedulesPayloads = generateTrainSchedulesPayloads(graouTrains);
-      await populateMissingSecondaryCodes(
-        trainSchedulesPayloads,
-        infraId,
-        postInfraByInfraIdMatchOperationalPoints
-      );
-      setTrainsJsonData({ train_schedules: trainSchedulesPayloads, paced_trains: [] });
-    } catch (error) {
-      dispatch(setFailure(castErrorToFailure(error)));
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  function defineConfig() {
-    let error = false;
-    if (!from) {
-      dispatch(
-        setFailure({ name: t('errorMessages.error'), message: t('errorMessages.errorNoFrom') })
-      );
-    }
-    if (!to) {
-      dispatch(
-        setFailure({ name: t('errorMessages.error'), message: t('errorMessages.errorNoTo') })
-      );
-    }
-    if (!date) {
-      dispatch(
-        setFailure({ name: t('errorMessages.error'), message: t('errorMessages.errorNoDate') })
-      );
-    }
-    if (JSON.stringify(from) === JSON.stringify(to)) {
-      dispatch(
-        setFailure({ name: t('errorMessages.error'), message: t('errorMessages.errorSameFromTo') })
-      );
-      error = true;
-    }
-
-    if (from && to && date && !error) {
-      getTrainsFromOpenData({
-        from,
-        to,
-        date,
-        startTime,
-        endTime,
-      });
-    }
-  }
 
   const locallyProcessXmlFile = async (fileContent: string) => {
     try {
