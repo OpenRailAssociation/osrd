5b169e16907cca4e5a6df2b2fcbde8d250bcef84
Alice Khoudli <alice.khoudli@polytechnique.org>
2026-01-16 23:35:59 +0100

    front: importtrainlist: move payload generation to helper file
    Signed-off-by: Alice Khoudli <alice.khoudli@polytechnique.org>

--- front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/helpers/generatePayloads.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/ImportTimetableItem/helpers/generatePayloads.ts
@@ -1,3 +1,17 @@
+import type {
+  PacedTrainFromJson,
+  TrainScheduleFromJson,
+} from 'applications/operationalStudies/types';
+import type {
+  PacedTrain,
+  SubCategory,
+  TrainCategory,
+  TrainMainCategory,
+  TrainSchedule,
+} from 'common/api/osrdEditoastApi';
+import { TrainMainCategoryDict } from 'modules/rollingStock/consts';
+import isMainCategory from 'modules/rollingStock/helpers/category';
+
 export const generateRoundTripsPayload = <T>(
   roundTripsIndexes: ([number, number] | [number, null])[],
   trainIds: { id: T }[],
@@ -23,3 +37,82 @@ export const generateRoundTripsPayload = <T>(
     },
   };
 };
+
+const isTrainMainCategory = (v: string): v is TrainMainCategory => v in TrainMainCategoryDict;
+
+const checkCategory = (
+  category: TrainCategory | string | null | undefined,
+  allowedSubCategories: SubCategory[]
+): TrainCategory | null => {
+  if (!category) return null;
+
+  // This condition is added for train imports that still use the old format: `category: string`, in particular imports from nge
+  if (typeof category === 'string') {
+    if (isTrainMainCategory(category)) return { main_category: category };
+    let correspondingSubCategory = allowedSubCategories.find(
+      (subCategory) => subCategory.code === category
+    );
+    if (!correspondingSubCategory)
+      correspondingSubCategory = allowedSubCategories.find(
+        (subCategory) => subCategory.name === category
+      );
+    return correspondingSubCategory ? { sub_category_code: correspondingSubCategory.code } : null;
+  }
+
+  if (isMainCategory(category)) {
+    return isTrainMainCategory(category.main_category) ? category : null;
+  }
+
+  const hasValidSubCategory = allowedSubCategories.some(
+    (subCategory) => subCategory.code === category.sub_category_code
+  );
+  return hasValidSubCategory ? category : null;
+};
+
+const unrecognizedCategoryToLabel = (
+  category: TrainCategory | string | null | undefined,
+  allowedSubCategories: SubCategory[]
+): string | null => {
+  if (!category || checkCategory(category, allowedSubCategories)) return null;
+  if (typeof category === 'string') {
+    return category;
+  }
+  if (isMainCategory(category)) {
+    return category.main_category;
+  }
+  return category.sub_category_code;
+};
+
+const buildLabels = (
+  labels: string[] | undefined,
+  category: TrainCategory | string | null | undefined,
+  allowedSubCategories: SubCategory[]
+): string[] | undefined => {
+  const unrecognizedCategoryLabel = unrecognizedCategoryToLabel(category, allowedSubCategories);
+  if (!unrecognizedCategoryLabel) return labels;
+  if (!labels) return [unrecognizedCategoryLabel];
+  if (labels.includes(unrecognizedCategoryLabel)) return labels;
+  return [...labels, unrecognizedCategoryLabel];
+};
+
+export function generateTrainPayloads(
+  parsedPacedTrains: PacedTrainFromJson[],
+  parsedTrainSchedules: TrainScheduleFromJson[],
+  allowedSubCategories: SubCategory[]
+): {
+  pacedTrainsPayload: PacedTrain[];
+  trainSchedulesPayload: TrainSchedule[];
+} {
+  return {
+    pacedTrainsPayload: parsedPacedTrains.map((pacedTrain) => ({
+      ...pacedTrain,
+      category: checkCategory(pacedTrain.category, allowedSubCategories),
+      labels: buildLabels(pacedTrain.labels, pacedTrain.category, allowedSubCategories),
+    })),
+    trainSchedulesPayload: parsedTrainSchedules.map((trainSchedule) => ({
+      ...trainSchedule,
+      category: checkCategory(trainSchedule.category, allowedSubCategories),
+      labels: buildLabels(trainSchedule.labels, trainSchedule.category, allowedSubCategories),
+    })),
+  };
+}
