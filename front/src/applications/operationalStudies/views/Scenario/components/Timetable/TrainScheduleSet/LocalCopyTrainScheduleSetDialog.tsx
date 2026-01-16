e3d696401e8fbbb996e5a6792b96a00e732d6057
Benoit Simard <contact@bsimard.com>
2026-01-16 16:21:58 +0100

    front: local copy of train schedule set in scenario
    Signed-off-by: Benoit Simard <contact@bsimard.com>

---
+++ front/src/applications/operationalStudies/views/Scenario/components/Timetable/TrainScheduleSet/LocalCopyTrainScheduleSetDialog.tsx
@@ -0,0 +1,77 @@
+import { useCallback, useState } from 'react';
+
+import { Button, Dialog } from '@osrd-project/ui-core';
+import { Blocked } from '@osrd-project/ui-icons';
+import cx from 'classnames';
+import { useTranslation } from 'react-i18next';
+
+import type { TrainScheduleSet } from 'common/api/osrdEditoastApi';
+import { getErrorMessage } from 'utils/error';
+
+type LocalCopyTrainScheduleSetDialogProps = {
+  trainScheduleSet: TrainScheduleSet;
+  onCancel: () => void;
+  onSubmit: () => Promise<void>;
+};
+
+const LocalCopyTrainScheduleSetDialog = ({
+  trainScheduleSet,
+  onCancel,
+  onSubmit,
+}: LocalCopyTrainScheduleSetDialogProps) => {
+  const { t } = useTranslation('operational-studies', {
+    keyPrefix: 'main.timetable.trainScheduleSets',
+  });
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const confirm = useCallback(async () => {
+    setLoading(true);
+    try {
+      await onSubmit();
+      onCancel();
+    } catch (e) {
+      const errorMessage = getErrorMessage(e);
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  }, [onSubmit, onCancel]);
+
+  return (
+    <Dialog
+      className="train-schedule-set-dialog"
+      style={{ maxWidth: '680px' }}
+      header={<h5>{t('transformToLocalCopyDialogTitle')}</h5>}
+      footer={
+        <>
+          <div className="submit-error">
+            {error && (
+              <>
+                <Blocked variant="fill" size="lg" />
+                <span>{error}</span>
+              </>
+            )}
+          </div>
+          <div className="footer-buttons">
+            <Button variant="Cancel" label={t('cancel')} onClick={onCancel} isDisabled={loading} />
+            <Button
+              label={t('transformToLocalCopySubmit')}
+              form="train-schedule-set"
+              type="submit"
+              className={cx('submit-button')}
+              onClick={confirm}
+              isLoading={loading}
+            />
+          </div>
+        </>
+      }
+      footerClassname={cx('train-schedule-set-dialog-footer', error !== null && 'with-error')}
+    >
+      <span>{t('transformToLocalCopyDialogText', { name: trainScheduleSet.name })}</span>
+    </Dialog>
+  );
+};
+
+export default LocalCopyTrainScheduleSetDialog;
