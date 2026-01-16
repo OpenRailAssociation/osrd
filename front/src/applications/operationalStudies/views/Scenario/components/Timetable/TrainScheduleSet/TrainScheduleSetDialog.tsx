4b86bd522dc953dc68ff1580292dc0c09c0d61c7
Benoit Simard <contact@bsimard.com>
2026-01-16 17:19:01 +0100

    ront: train schedule set form component
    Making the `TrainScheduleSetDialog` component generic so it can be
    reused for edition and publish.
    
    The name of the field "name" has changed, this has been view with
    thibaut.
    
    Signed-off-by: Benoit Simard <contact@bsimard.com>

--- front/src/applications/operationalStudies/views/Scenario/components/Timetable/TrainScheduleSet/TrainScheduleSetDialog.tsx
+++ front/src/applications/operationalStudies/views/Scenario/components/Timetable/TrainScheduleSet/TrainScheduleSetDialog.tsx
@@ -1,241 +1,100 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 
-import { Button, Dialog, Input, Select, TextArea } from '@osrd-project/ui-core';
+import { Button, Dialog } from '@osrd-project/ui-core';
 import { Blocked } from '@osrd-project/ui-icons';
 import cx from 'classnames';
-import { useTranslation } from 'react-i18next';
+import { noop } from 'lodash';
 
-import {
-  osrdEditoastApi,
-  type CatalogEntry,
-  type TrainScheduleSetForm,
-} from 'common/api/osrdEditoastApi';
+import type { TrainScheduleSetFormData } from 'applications/operationalStudies/hooks/useScenarioTrainScheduleSet';
+import type { CatalogEntry, TrainScheduleSet } from 'common/api/osrdEditoastApi';
+import { LoaderFill } from 'common/Loaders';
 import { getErrorMessage } from 'utils/error';
+import { useAsyncMemo } from 'utils/useAsyncMemo';
 
-import { MOCK_CATALOG } from '../../../mockTrainScheduleSets';
+import TrainScheduleSetForm from './TrainScheduleSetForm';
 
 type TrainScheduleSetDialogProps = {
+  trainScheduleSet?: TrainScheduleSet;
   onCancel: () => void;
+  onSubmit: (data: TrainScheduleSetFormData) => Promise<void>;
+  getCatalogEntries: () => Promise<CatalogEntry[]>;
+  labels: {
+    title: string;
+    submit: string;
+    cancel: string;
+  };
 };
 
-const TrainScheduleSetDialog = ({ onCancel }: TrainScheduleSetDialogProps) => {
-  const { t } = useTranslation('operational-studies', {
-    keyPrefix: 'main.timetable.trainScheduleSets',
-  });
+const TrainScheduleSetDialog = ({
+  trainScheduleSet,
+  onSubmit,
+  onCancel,
+  getCatalogEntries,
+  labels,
+}: TrainScheduleSetDialogProps) => {
+  const catalog = useAsyncMemo(() => getCatalogEntries(), [getCatalogEntries]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [formHasError, setFormHasError] = useState<boolean>(false);
 
-  const [name, setName] = useState('');
-  const [isNameMissing, setIsNameMissing] = useState(false);
-  const [catalogEntry, setCatalogEntry] = useState<CatalogEntry>();
-  const [description, setDescription] = useState('');
-  const [catalogEntryMode, setCatalogEntryMode] = useState<'select' | 'create'>('select');
-  const [newCatalogEntryName, setNewCatalogEntryName] = useState('');
-  const [catalogEntryError, setCatalogEntryError] = useState<'duplicate' | 'missing' | 'none'>(
-    'none'
-  );
-  const [formError, setFormError] = useState<string>();
-
-  const { data: catalogueEntryResult } = osrdEditoastApi.endpoints.getCatalogEntry.useQuery(
-    {
-      pageSize: 1000,
-    },
-    { skip: !!MOCK_CATALOG }
-  );
-
-  const [postTrainScheduleSet, { isLoading: isPostTrainScheduleSetLoading }] =
-    osrdEditoastApi.endpoints.postTrainScheduleSet.useMutation();
-  const [postCatalogEntry, { isLoading: isPostCatalogEntryLoading }] =
-    osrdEditoastApi.endpoints.postCatalogEntry.useMutation();
-
-  const catalogEntries = useMemo(() => {
-    // TODO Package : return [] when back ready
-    if (!catalogueEntryResult) return MOCK_CATALOG;
-    return catalogueEntryResult.results.sort((a, b) =>
-      a.name && b.name ? a.name.localeCompare(b.name) : 0
-    );
-  }, [catalogueEntryResult, MOCK_CATALOG]);
-
-  const catalogEntryNameExists = useMemo(() => {
-    if (catalogEntryMode === 'create') {
-      return catalogEntries.some((entry) => entry.name === newCatalogEntryName);
-    }
-    return false;
-  }, [catalogEntryMode, catalogEntries, newCatalogEntryName]);
-
-  const getCatalogEntryErrorMessage = () => {
-    if (catalogEntryNameExists) {
-      return {
-        status: 'error' as const,
-        message: t('duplicateCatalogEntry'),
-      };
-    }
-    if (catalogEntryError === 'missing') {
-      return {
-        status: 'error' as const,
-      };
-    }
-    return;
-  };
-
-  const handleSubmit = async () => {
-    setFormError(undefined);
-
-    if (!name) {
-      setIsNameMissing(true);
-      return;
-    }
-
-    let catalogEntryId = catalogEntry?.id ?? null;
-    if (catalogEntryMode === 'create') {
-      if (!newCatalogEntryName) {
-        setCatalogEntryError('missing');
-        return;
-      }
-      if (catalogEntryNameExists) {
-        setCatalogEntryError('duplicate');
-        return;
-      }
-
+  const submit = useCallback(
+    async (data: TrainScheduleSetFormData) => {
+      setLoading(true);
       try {
-        const newCatalogEntry = await postCatalogEntry({
-          catalogEntryForm: { name: newCatalogEntryName },
-        }).unwrap();
-        catalogEntryId = newCatalogEntry.id;
-      } catch (error) {
-        const errorMessage = getErrorMessage(error);
-        setFormError(errorMessage);
-        return;
+        await onSubmit(data);
+        onCancel();
+      } catch (e) {
+        const errorMessage = getErrorMessage(e);
+        setError(errorMessage);
+      } finally {
+        setLoading(false);
       }
-    }
-
-    const payload: TrainScheduleSetForm = {
-      name,
-      description,
-      catalog_entry_id: catalogEntryId,
-      published: false, // new train schedule sets are always local by default
-    };
-
-    try {
-      postTrainScheduleSet({ trainScheduleSetForm: payload });
-    } catch (error) {
-      const errorMessage = getErrorMessage(error);
-      setFormError(errorMessage);
-      return;
-    }
-    onCancel();
-  };
+    },
+    [onSubmit, onCancel]
+  );
 
   return (
     <Dialog
       className="train-schedule-set-dialog"
-      header={<h5>{t('newLocalTrainScheduleSet')}</h5>}
+      header={<h5>{labels.title}</h5>}
       footer={
         <>
-          {formError && (
-            <div className="submit-error">
-              <Blocked variant="fill" size="lg" />
-              <span>{formError}</span>
-            </div>
-          )}
+          <div className="submit-error">
+            {error && (
+              <>
+                <Blocked variant="fill" size="lg" />
+                <span>{error}</span>
+              </>
+            )}
+          </div>
           <div className="footer-buttons">
             <Button
               variant="Cancel"
-              label={t('cancel')}
+              label={labels.cancel}
               onClick={onCancel}
-              isDisabled={isPostTrainScheduleSetLoading || isPostCatalogEntryLoading}
+              isDisabled={loading}
             />
             <Button
-              label={t('addTrainScheduleSet')}
-              className={cx('submit-button', {
-                'wizz-effect': catalogEntryError !== 'none' || isNameMissing,
-              })}
-              onClick={handleSubmit}
-              isLoading={isPostTrainScheduleSetLoading || isPostCatalogEntryLoading}
+              label={labels.submit}
+              form="train-schedule-set"
+              type="submit"
+              className={cx('submit-button', formHasError && 'wizz-effect')}
+              onClick={noop}
+              isLoading={loading}
             />
           </div>
         </>
       }
-      footerClassname={cx('train-schedule-set-dialog-footer', {
-        'with-error': formError,
-      })}
+      footerClassname={cx('train-schedule-set-dialog-footer', error !== null && 'with-error')}
     >
-      <div className="train-schedule-set-name">
-        <Input
-          id="train-schedule-set-name"
-          label={t('name')}
-          required
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-            if (isNameMissing) {
-              setIsNameMissing(false);
-            }
-          }}
-          statusWithMessage={
-            isNameMissing
-              ? {
-                  status: 'error',
-                }
-              : undefined
-          }
-        />
-      </div>
-      {catalogEntryMode === 'select' && (
-        <div className="catalog-entry">
-          <Select
-            id="catalogue-entry"
-            label={t('catalogEntry')}
-            placeholder={t('choose')}
-            value={catalogEntry}
-            onChange={(e) => {
-              setCatalogEntry(e);
-            }}
-            options={catalogEntries}
-            getOptionLabel={(option) => option.name ?? ''}
-            getOptionValue={(option) => option.name ?? ''}
-          />
-          <button
-            className="catalog-entry-mode-button"
-            onClick={() => setCatalogEntryMode('create')}
-          >
-            {t('createNewCatalogEntry')}
-          </button>
-        </div>
-      )}
-      {catalogEntryMode === 'create' && (
-        <div className="catalog-entry">
-          <div className="new-catalog-entry-input">
-            <Input
-              id="new-catalog-entry-name"
-              label={t('newCatalogEntry')}
-              required
-              value={newCatalogEntryName}
-              onChange={(e) => {
-                setNewCatalogEntryName(e.target.value);
-                if (catalogEntryError !== 'none') {
-                  setCatalogEntryError('none');
-                }
-              }}
-              statusWithMessage={getCatalogEntryErrorMessage()}
-            />
-          </div>
-          <button
-            className="catalog-entry-mode-button existing"
-            onClick={() => {
-              setCatalogEntryMode('select');
-              setNewCatalogEntryName('');
-              setCatalogEntryError('none');
-            }}
-          >
-            {t('existingCatalogEntry')}
-          </button>
-        </div>
-      )}
-      <TextArea
-        id="train-schedule-set-description"
-        label={t('description')}
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+      <TrainScheduleSetForm
+        formId="train-schedule-set"
+        trainScheduleSet={trainScheduleSet}
+        catalog={catalog.type === 'ready' ? catalog.data : []}
+        onSubmit={submit}
+        onValidation={(hasError) => setFormHasError(hasError)}
       />
+      {catalog.type === 'loading' && <LoaderFill />}
     </Dialog>
   );
 };
