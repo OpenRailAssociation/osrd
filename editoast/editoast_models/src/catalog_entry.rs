d88bfead58bcc77730b5f44ba8652370b91036b4
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2026-01-20 15:20:23 +0100

    editoast: add catalog entry post test
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>

--- editoast/editoast_models/src/catalog_entry.rs
+++ editoast/editoast_models/src/catalog_entry.rs
@@ -4,7 +4,7 @@ use serde::Serialize;
 use utoipa::ToSchema;
 
 use crate as editoast_models;
-#[derive(Deserialize, Serialize, ToSchema, Debug, Clone, Model)]
+#[derive(Deserialize, Serialize, ToSchema, Debug, Clone, Model, PartialEq)]
 #[model(table = database::tables::catalog_entry)]
 #[model(gen(ops = crud, batch_ops = crud, list))]
 #[model(row(derive(diesel::QueryableByName)))]
