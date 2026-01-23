337a4cfc9632f5f2a474d25568b1a750e0b9f049
aschantraine <aschantraine@free.fr>
2026-01-23 11:19:55 +0100

    editoast: handle out_of_range error with auto-fix for operational points
    Signed-off-by: aschantraine <aschantraine@free.fr>

--- editoast/src/views/infra/auto_fixes/operational_point.rs
+++ editoast/src/views/infra/auto_fixes/operational_point.rs
@@ -2,10 +2,13 @@ use itertools::Itertools;
 use json_patch::Patch;
 use json_patch::PatchOperation;
 use json_patch::RemoveOperation;
+use json_patch::ReplaceOperation;
+use ordered_float::OrderedFloat;
 use schemas::primitives::OSRDIdentified;
 use schemas::primitives::OSRDObject as _;
 use schemas::primitives::ObjectRef;
 use schemas::primitives::ObjectType;
+use serde_json::json;
 use std::collections::HashMap;
 use tracing::debug;
 
@@ -20,7 +23,7 @@ use crate::infra_cache::operation::DeleteOperation;
 use crate::infra_cache::operation::Operation;
 use crate::infra_cache::operation::UpdateOperation;
 
-fn invalid_reference_to_ordered_operation(
+fn invalid_part_to_ordered_operation(
     operational_point: &OperationalPointCache,
     object_ref: &ObjectRef,
 ) -> Option<OrderedOperation> {
@@ -30,6 +33,26 @@ fn invalid_reference_to_ordered_operation(
         .enumerate()
         .find(|(_idx, part)| part.track.as_str() == object_ref.obj_id)?;
     Some(OrderedOperation::RemoveTrackRef { track_refs })
+}
+
+fn out_of_range_part_to_ordered_operation(
+    operational_point: &OperationalPointCache,
+    new_cache: &mut OperationalPointCache,
+    object_ref: &ObjectRef,
+    expected_range: &[f64; 2],
+) -> Option<OrderedOperation> {
+    let (track_refs, part) = operational_point
+        .parts
+        .iter()
+        .enumerate()
+        .find(|(_idx, part)| part.track.as_str() == object_ref.obj_id)?;
+    // Update cache
+    let new_position = part.position.clamp(expected_range[0], expected_range[1]);
+    new_cache.parts[track_refs].position = new_position;
+    Some(OrderedOperation::UpdatePosition {
+        track_refs,
+        new_position: OrderedFloat(new_position),
+    })
 }
 
 pub fn fix_operational_point(
@@ -46,7 +69,19 @@ pub fn fix_operational_point(
                 new_op
                     .parts
                     .retain(|part| part.track.as_str() != reference.obj_id);
-                invalid_reference_to_ordered_operation(operational_point, reference)
+                invalid_part_to_ordered_operation(operational_point, reference)
+            }
+            InfraErrorType::OutOfRange {
+                reference,
+                expected_range,
+                ..
+            } if reference.obj_type == ObjectType::TrackSection => {
+                out_of_range_part_to_ordered_operation(
+                    operational_point,
+                    &mut new_op,
+                    reference,
+                    expected_range,
+                )
             }
             _ => {
                 debug!("error not (yet) fixable for '{}'", infra_error.get_type());
@@ -64,9 +99,17 @@ pub fn fix_operational_point(
                     path: format!("/parts/{track_refs}").parse().unwrap(),
                 })]),
             }),
-            OrderedOperation::UpdatePosition { .. } => {
-                panic!("We should not update operational point position")
-            }
+            OrderedOperation::UpdatePosition {
+                track_refs,
+                new_position,
+            } => Operation::Update(UpdateOperation {
+                obj_id: operational_point.get_id().clone(),
+                obj_type: operational_point.get_type(),
+                railjson_patch: Patch(vec![PatchOperation::Replace(ReplaceOperation {
+                    path: format!("/parts/{track_refs}/position").parse().unwrap(),
+                    value: json!(new_position),
+                })]),
+            }),
             OrderedOperation::Delete => {
                 Operation::Delete(DeleteOperation::from(operational_point.get_ref()))
             }
@@ -205,5 +248,56 @@ mod tests {
         };
         assert_eq!(object_ref.obj_id, "operational_point_id");
         assert_eq!(object_ref.obj_type, ObjectType::OperationalPoint);
+    }
+
+    #[test]
+    fn out_of_range_operational_point() {
+        let op_cache = OperationalPointCache {
+            obj_id: "operational_point_id".into(),
+            parts: vec![
+                OperationalPointPartCache {
+                    track: Identifier::from("track_section_id_1"),
+                    position: 1500.0, // out of range
+                },
+                OperationalPointPartCache {
+                    track: Identifier::from("track_section_id_2"),
+                    position: 500.0, // valid
+                },
+            ],
+        };
+        let error_op = InfraError::new_out_of_range(
+            &op_cache,
+            "parts.0.position",
+            1500.0,
+            [0.0, 1000.0],
+            ObjectRef::new(ObjectType::TrackSection, "track_section_id_1"),
+        );
+
+        let operations = super::fix_operational_point(&op_cache, vec![error_op].into_iter());
+
+        assert_eq!(operations.len(), 1);
+
+        let (operation, cache_operation) = operations.get(&op_cache.get_ref()).unwrap();
+        let Operation::Update(update_operation) = operation else {
+            panic!("not an `Operation::Update`");
+        };
+        assert_eq!(update_operation.obj_id, "operational_point_id");
+        assert!(matches!(
+            update_operation.obj_type,
+            ObjectType::OperationalPoint
+        ));
+        assert_eq!(
+            update_operation.railjson_patch,
+            serde_json::from_str::<Patch>(
+                r#"[{"op":"replace","path":"/parts/0/position","value":1000.0}]"#
+            )
+            .unwrap()
+        );
+        let CacheOperation::Update(ObjectCache::OperationalPoint(op)) = cache_operation else {
+            panic!("not a `CacheOperation::Update(ObjectCache::OperationalPoint())`");
+        };
+        assert_eq!(op.parts.len(), 2);
+        assert_eq!(op.parts[0].track.0, "track_section_id_1");
+        assert_eq!(op.parts[0].position, 1000.0);
     }
 }
