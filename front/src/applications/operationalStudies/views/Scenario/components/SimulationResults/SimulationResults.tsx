f3e32e98aae40c9e4f812473d4fcbb406ec06619
Theo Macron <theo.macron0315@gmail.com>
2026-01-23 14:41:34 +0100

    front: adapt osrd to use new table style into it
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/src/applications/operationalStudies/views/Scenario/components/SimulationResults/SimulationResults.tsx
+++ front/src/applications/operationalStudies/views/Scenario/components/SimulationResults/SimulationResults.tsx
@@ -286,7 +286,7 @@ const SimulationResults = ({
           {/* TIME STOPS TABLE */}
           <BoardWrapper
             hidden={!activeBoards.has('tables')}
-            name={t('simulationResults.timetableOutput')}
+            name={simulationResults.train.train_name}
             items={[
               {
                 title: displayOnlyPathSteps
