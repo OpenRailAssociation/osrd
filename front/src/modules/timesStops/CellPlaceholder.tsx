51dd610f4c7baee32c56ad74a243b27fbd487081
Theo Macron <theo.macron0315@gmail.com>
2026-01-29 16:23:02 +0100

    front: create common placeholder component for input cells in table
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

---
+++ front/src/modules/timesStops/CellPlaceholder.tsx
@@ -0,0 +1,13 @@
+import { Plus } from '@osrd-project/ui-icons';
+
+type CellPlaceholderProps = {
+  onClick: () => void;
+};
+
+const CellPlaceholder = ({ onClick }: CellPlaceholderProps) => (
+  <div className="input-cell-placeholder" onClick={onClick} role="button" tabIndex={-1}>
+    <Plus />
+  </div>
+);
+
+export default CellPlaceholder;
