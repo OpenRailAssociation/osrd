16fea5fdcbeccc76ea859928f5c15bfa9dc73339
romainvalls <romain.valls95@gmail.com>
2026-01-30 12:26:13 +0100

    front: add buildOpSuggestion function
    Signed-off-by: romainvalls <romain.valls95@gmail.com>

---
+++ front/src/applications/operationalStudies/views/Scenario/components/ManageTimetableItem/helpers/buildOpSuggestion.ts
@@ -0,0 +1,48 @@
+import type { SearchResultItemOperationalPoint } from 'common/api/osrdEditoastApi';
+
+import type {
+  OpSecondaryCode,
+  OperationalPointSuggestion,
+} from '../Itinerary/ComboBoxCustomList/ListElementComponent';
+
+const sortSecondaryCodeList = (secondaryCodeList: OpSecondaryCode[]) => {
+  const priority: Record<string, number> = { BV: 0, '00': 1 };
+
+  secondaryCodeList.sort((a, b) => {
+    const pa = priority[a.code] ?? 99;
+    const pb = priority[b.code] ?? 99;
+
+    if (pa !== pb) return pa - pb;
+    return a.code.localeCompare(b.code);
+  });
+};
+
+export const buildOpSuggestion = (
+  results: SearchResultItemOperationalPoint[]
+): OperationalPointSuggestion[] => {
+  const map = new Map<string, OperationalPointSuggestion>();
+
+  for (const r of results) {
+    const key = `${r.trigram}||${r.name}`;
+    const existing = map.get(key);
+
+    if (!existing) {
+      const secondaryCodeList = r.ch ? [{ code: r.ch, opId: r.obj_id }] : [];
+
+      map.set(key, {
+        id: key,
+        trigram: r.trigram,
+        name: r.name,
+        secondaryCodeList,
+      });
+    } else if (r.ch && !existing.secondaryCodeList.some((c) => c.code === r.ch)) {
+      existing.secondaryCodeList.push({ code: r.ch, opId: r.obj_id });
+    }
+  }
+
+  for (const suggestion of map.values()) {
+    sortSecondaryCodeList(suggestion.secondaryCodeList);
+  }
+
+  return [...map.values()];
+};
