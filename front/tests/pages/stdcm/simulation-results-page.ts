c51decbb1b7181c212b20887823dca5cd08d94f3
Uriel-Sautron <uriel.sautron@gmail.com>
2026-02-12 10:56:47 +0100

    front: remove track in stdcm table
    Signed-off-by: Uriel-Sautron <uriel.sautron@gmail.com>

--- front/tests/pages/stdcm/simulation-results-page.ts
+++ front/tests/pages/stdcm/simulation-results-page.ts
@@ -70,12 +70,11 @@ class SimulationResultPage {
           index: Number(cells[0]?.textContent?.trim()) || 0,
           operationalPoint: cells[1]?.textContent?.trim() || '',
           code: cells[2]?.textContent?.trim() || '',
-          track: cells[3]?.textContent?.trim() || '',
-          endStop: cells[4]?.textContent?.trim() || '',
-          passageStop: cells[5]?.textContent?.trim() || '',
-          startStop: cells[6]?.textContent?.trim() || '',
-          weight: cells[7]?.textContent?.trim() || '',
-          refEngine: cells[8]?.textContent?.trim() || '',
+          endStop: cells[3]?.textContent?.trim() || '',
+          passageStop: cells[4]?.textContent?.trim() || '',
+          startStop: cells[5]?.textContent?.trim() || '',
+          weight: cells[6]?.textContent?.trim() || '',
+          refEngine: cells[7]?.textContent?.trim() || '',
         };
       })
     );
@@ -89,9 +88,9 @@ class SimulationResultPage {
         logger.error(`Row ${index + 1} is missing in the HTML table`);
         return;
       }
+
       expect(tableRow.operationalPoint).toBe(jsonRow.operationalPoint);
       expect(tableRow.code).toBe(jsonRow.code);
-      expect(tableRow.track).toBe(jsonRow.track);
       expect(tableRow.endStop).toBe(jsonRow.endStop);
       expect(tableRow.passageStop).toBe(jsonRow.passageStop);
       expect(tableRow.startStop).toBe(jsonRow.startStop);
