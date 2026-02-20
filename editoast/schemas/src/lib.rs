289f7bbace68a0f1af5aa5ed61d2ca37ecd30a9d
Egor <egor@berezify.fr>
2026-02-20 15:07:22 +0100

    editoast: add train schedule exception table and create endpoint
    Signed-off-by: Egor <egor@berezify.fr>

--- editoast/schemas/src/lib.rs
+++ editoast/schemas/src/lib.rs
@@ -4,6 +4,7 @@ pub mod paced_train;
 pub mod primitives;
 pub mod rolling_stock;
 pub mod train_schedule;
+pub mod train_schedule_exception;
 
 #[cfg(feature = "testing")]
 pub mod fixtures;
@@ -11,3 +12,5 @@ pub mod fixtures;
 pub use rolling_stock::RollingStock;
 pub use rolling_stock::TowedRollingStock;
 pub use train_schedule::TrainOccurrence;
+pub use train_schedule_exception::TrainScheduleException;
+pub use train_schedule_exception::TrainScheduleExceptionChangeGroups;
