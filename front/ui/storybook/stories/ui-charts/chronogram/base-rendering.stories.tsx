a190bce133939a70d90aa02fea2d2391866fd141
SarahBellaha <sarah.bellaha@sncf.fr>
2026-03-03 13:49:42 +0100

    storybook: update the chronogram story
    Signed-off-by: SarahBellaha <sarah.bellaha@sncf.fr>

--- front/ui/storybook/stories/ui-charts/chronogram/base-rendering.stories.tsx
+++ front/ui/storybook/stories/ui-charts/chronogram/base-rendering.stories.tsx
@@ -1,33 +1,23 @@
 import React from 'react';
 
-import { Chronogram } from '@osrd-project/ui-charts';
+import { Chronogram, type LevelCrossingData } from '@osrd-project/ui-charts';
 import type { Meta } from '@storybook/react-vite';
 
-import { X_ZOOM_LEVEL } from '../spaceTimeChart/helpers/utils';
-
+import { levelCrossingData, START_DATE } from './level-crossing-data';
 import '@osrd-project/ui-charts/dist/theme.css';
 import '@osrd-project/ui-core/dist/theme.css';
 
 type WrapperProps = {
-  xZoomLevel: number;
-  xOffset: number;
-  yOffset: number;
+  levelCrossingsData: LevelCrossingData[];
+  height: number;
 };
 
 /**
  * This story aims at showcasing how to render an empty Chronogram.
  */
-const Wrapper = ({ xZoomLevel, xOffset, yOffset }: WrapperProps) => (
+const Wrapper = ({ levelCrossingsData, height }: WrapperProps) => (
   <div className="absolute inset-0">
-    <Chronogram
-      className="h-full"
-      timeOrigin={+new Date('2024/04/02')}
-      timeScale={60000 / xZoomLevel}
-      xOffset={xOffset}
-      yOffset={yOffset}
-      levelCrossingsNames={[]}
-      levelCrossingsOccupancies={[]}
-    ></Chronogram>
+    <Chronogram timeOrigin={+START_DATE} levelCrossingData={levelCrossingsData} height={height} />
   </div>
 );
 
@@ -35,32 +25,19 @@ export default {
   title: 'Chronogram/Rendering',
   component: Wrapper,
   argTypes: {
-    xZoomLevel: {
-      name: 'X zoom level',
-      description: '(in pixels/minute)',
-      defaultValue: 0.4,
-      control: { type: 'range', min: 0.1, max: 75, step: 0.1 },
-    },
-    xOffset: {
-      name: 'X offset',
-      description: '(in pixels)',
-      defaultValue: 0,
-      control: { type: 'number', step: 10 },
-    },
-    yOffset: {
-      name: 'Y offset',
-      description: '(in pixels)',
-      defaultValue: 0,
-      control: { type: 'number', step: 10 },
+    height: {
+      name: 'Height',
+      description: 'Height of the Chronogram (in pixels)',
+      defaultValue: 450,
+      control: { type: 'range', min: 400, max: 1000, step: 50 },
     },
   },
 } as Meta<typeof Wrapper>;
 
-export const DefaultArgs = {
-  name: 'Default arguments',
+export const WithFakeData = {
+  name: 'Default rendering with fake data',
   args: {
-    xZoomLevel: X_ZOOM_LEVEL,
-    xOffset: 0,
-    yOffset: 0,
+    levelCrossingsData: levelCrossingData,
+    height: 450,
   },
 };
