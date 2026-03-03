82318a5445348db2b722fb2f0d175c788e73a1df
SarahBellaha <sarah.bellaha@sncf.fr>
2026-03-03 12:19:36 +0100

    storybook: move time consts in common folder
    Signed-off-by: SarahBellaha <sarah.bellaha@sncf.fr>

--- front/ui/storybook/stories/ui-charts/spaceTimeChart/helpers/consts.ts
+++ front/ui/storybook/stories/ui-charts/spaceTimeChart/helpers/consts.ts
@@ -14,10 +14,5 @@ export const OCCUPANCY_FREE = '#CAEDDB';
 export const OCCUPANCY_SEMAPHORE = '#FFD4D8';
 export const OCCUPANCY_WARNING = '#FFEABF';
 
-// Here are some helpers to write code about time in ms that is humanly readable:
-export const SECOND = 1000;
-export const MINUTE = 60 * SECOND;
-export const HOUR = 60 * MINUTE;
-
 // Same for distances in millimeters:
 export const KILOMETER = 1000000;
--- front/ui/storybook/stories/ui-charts/spaceTimeChart/layers.stories.tsx
+++ front/ui/storybook/stories/ui-charts/spaceTimeChart/layers.stories.tsx
@@ -14,16 +14,15 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import {
   KILOMETER,
-  MINUTE,
   OCCUPANCY_FREE,
   OCCUPANCY_SEMAPHORE,
   OCCUPANCY_WARNING,
 } from './helpers/consts';
 import { OPERATIONAL_POINTS, PATHS, START_DATE } from './helpers/paths';
 import { X_ZOOM_LEVEL, Y_ZOOM_LEVEL } from './helpers/utils';
-
 import '@osrd-project/ui-core/dist/theme.css';
 import '@osrd-project/ui-charts/dist/theme.css';
+import { MINUTE } from '../common/const';
 
 const CONFLICTS = [
   {
