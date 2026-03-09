d0783830f6207c8fd703fb5fc21bfdae812ef016
Tristram Gräbener <tristram+git@tristramg.eu>
2026-03-09 11:45:43 +0100

    front: map: remove unused constants
    Signed-off-by: Tristram Gräbener <tristram+git@tristramg.eu>

--- front/src/common/Map/const.ts
+++ front/src/common/Map/const.ts
@@ -5,14 +5,9 @@ import { absoluteUrl } from 'utils/strings';
 export const MAP_URL = `${config.proxy_editoast}/layers`;
 export const SPRITES_URL = absoluteUrl(`${config.proxy_editoast}/sprites`);
 export const FONTS_URL = `${config.proxy_editoast}/fonts`;
+
 export const OSM_URL = 'pmtiles://https://tuiles.enliberte.fr/planet.pmtiles';
 export const TERRAIN_URL = 'pmtiles://https://tuiles.enliberte.fr/terrain-europe.pmtiles';
-
-export const MAP_MODES = {
-  display: 'display',
-  modification: 'modification',
-  creation: 'creation',
-};
 
 export const CUSTOM_ATTRIBUTION = '© SNCF Réseau';
 
@@ -22,46 +17,6 @@ export const CUSTOM_ATTRIBUTION = '© SNCF Réseau';
 export const MAP_TRACK_SOURCE = 'track_sections';
 export const MAP_TRACK_SOURCES = {
   geographic: MAP_TRACK_SOURCE,
-};
-
-export const ELEMENT_TYPES = {
-  objects: 'objects',
-  groups: 'groups',
-};
-
-// Objects constants
-// Labels
-export const FIELDS_LABEL = {
-  L_code: 'Code ligne',
-  V_nom: 'Nom voie',
-  pk_sncf: 'PK',
-  LP_sensLecture: 'Sens de lecture',
-  TIF_mnemo: 'Type',
-  RA_libelle: 'Libellé',
-  RA_poste: 'Poste',
-  RA_gare: 'Gare',
-  extremites: 'Extrémités',
-  RLJDZ_sens: 'RLJDZ_sens',
-  RLE_sens: 'RLE_sens',
-  is_utilisable_isolement: 'Utilisable isolement',
-  Z_ttx: 'Train travaux interdits',
-  RA_libelle_signal_destination: 'Signal de destination',
-  RA_libelle_signal_origine: "Signal d'origine",
-  BAPV_direction: 'Direction',
-  BAPV_codeBranche: 'Numero de branche',
-  branches: 'Branches',
-  adv_libelle: 'Libellé',
-  adv_positionnes: 'Aiguilles',
-  L_ordre: 'Ordre',
-  CAV_rang: 'Rang',
-  codeBranche_debut: 'Code branche début',
-  codeBranche_fin: 'Code branche fin',
-  codeBranche: 'Code branche',
-  BAPV_pointObservation: "Point d'observation",
-  cles_protections: 'Clé protections',
-  cle: 'Clé',
-  itineraires: 'Itinéraires',
-  itineraires_incompatibles: 'Itinéraire Incompatibles',
 };
 
 // mapKey
