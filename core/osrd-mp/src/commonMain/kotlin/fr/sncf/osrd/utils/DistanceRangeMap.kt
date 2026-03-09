0a7481ef008b9c1033972d9a44d834951544eb8d
Hubert Hirtz <hubert@hirtz.pm>
2026-03-09 09:40:34 +0100

    core: fast path for new DistanceRangeMap from sorted entries
    when building a `DistanceRangeMapImpl` from a list of entries that are
    sorted and cover a contiguous range we can build the range map more
    easily.
    
    Signed-off-by: Hubert Hirtz <hubert@hirtz.pm>

--- core/osrd-mp/src/commonMain/kotlin/fr/sncf/osrd/utils/DistanceRangeMap.kt
+++ core/osrd-mp/src/commonMain/kotlin/fr/sncf/osrd/utils/DistanceRangeMap.kt
@@ -18,7 +18,9 @@ import fr.sncf.osrd.utils.units.Distance
 interface DistanceRangeMap<T> : Iterable<DistanceRangeMap.RangeMapEntry<T>> {
 
     /** When iterating over the values of the map, this represents one range of constant value */
-    data class RangeMapEntry<T>(val lower: Distance, val upper: Distance, val value: T)
+    data class RangeMapEntry<T>(val lower: Distance, val upper: Distance, val value: T) {
+        fun isEmpty(): Boolean = lower >= upper
+    }
 
     /** Sets the value between the lower and upper distances */
     fun put(lower: Distance, upper: Distance, value: T)
