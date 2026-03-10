9009b1aeddda8f272c82286c947dbece250318fa
Egor <egor@berezify.fr>
2026-03-10 11:22:37 +0100

    editoast: adapt get train schedules exceptions (#15502)
    fix https://github.com/OpenRailAssociation/osrd/issues/15428
    
    This PR adapts the `GET /timetable/{id}/train_schedules` endpoint to
    retrieve the new exceptions.
    
    To reduce the amount of refactoring, we convert the new exceptions into
    the old ones; later, we will replace them entirely.
    
    This PR targets a feature branch and breaks e2e tests. We will fix the
    tests later (normally, they should fix themselves when we complete the
    refactoring).
    
    Signed-off-by: Egor <egor@berezify.fr>

--- front/src/applications/operationalStudies/views/Scenario/components/Timetable/PacedTrain/hooks/useOccurrences.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/Timetable/PacedTrain/hooks/useOccurrences.ts
@@ -67,7 +67,7 @@ const useOccurrences = (
           : pacedTrainCategory,
         occurrenceIndex: i,
         exceptionChangeGroups: correspondingException
-          ? omit(correspondingException, ['key', 'occurrence_index', 'disabled', 'summary'])
+          ? omit(correspondingException, ['key', 'occurrence_index', 'disabled', 'id', 'summary'])
           : undefined,
         summary: correspondingException?.summary ?? summary,
       });
@@ -98,7 +98,13 @@ const useOccurrences = (
         category: exception.rolling_stock_category
           ? exception.rolling_stock_category.value
           : pacedTrainCategory,
-        exceptionChangeGroups: omit(exception, ['key', 'disabled', 'occurrence_index', 'summary']),
+        exceptionChangeGroups: omit(exception, [
+          'key',
+          'disabled',
+          'occurrence_index',
+          'id',
+          'summary',
+        ]),
         summary: exception.summary ?? summary,
       });
     });
