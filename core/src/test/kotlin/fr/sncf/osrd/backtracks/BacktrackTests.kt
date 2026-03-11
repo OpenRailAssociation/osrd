e3ea7789f2cdd7e4b1e5bf4c5ea0523db3251ae9
Pierre-Etienne Bougué <bougue.pe@proton.me>
2026-03-11 09:23:42 +0100

    core: handle backtracking for zone occupation
    Also add tests with or without overlapping on zone border
    
    Signed-off-by: Pierre-Etienne Bougué <bougue.pe@proton.me>

--- core/src/test/kotlin/fr/sncf/osrd/backtracks/BacktrackTests.kt
+++ core/src/test/kotlin/fr/sncf/osrd/backtracks/BacktrackTests.kt
@@ -4,6 +4,7 @@ import fr.sncf.osrd.api.FullInfra
 import fr.sncf.osrd.api.RangeValues
 import fr.sncf.osrd.api.path_properties.makePathPropResponse
 import fr.sncf.osrd.api.standalone_sim.SimulationScheduleItem
+import fr.sncf.osrd.envelope_sim_infra.computeMRSP
 import fr.sncf.osrd.path.implementations.PartialBlockRange
 import fr.sncf.osrd.path.implementations.buildRangeList
 import fr.sncf.osrd.path.implementations.buildTrainPathFromBlockRanges
@@ -16,8 +17,11 @@ import fr.sncf.osrd.railjson.schema.schedule.RJSTrainStop.RJSReceptionSignal.OPE
 import fr.sncf.osrd.railjson.schema.schedule.RJSTrainStop.RJSReceptionSignal.SHORT_SLIP_STOP
 import fr.sncf.osrd.sim_infra.api.Block
 import fr.sncf.osrd.sim_infra.api.BlockId
+import fr.sncf.osrd.sim_infra.api.ZoneId
 import fr.sncf.osrd.sim_infra.api.getLogicalSignalName
+import fr.sncf.osrd.standalone_sim.ZoneOccupationChangeEvent
 import fr.sncf.osrd.standalone_sim.runStandaloneSimulation
+import fr.sncf.osrd.standalone_sim.zoneOccupationChangeEvents
 import fr.sncf.osrd.train.RollingStock
 import fr.sncf.osrd.train.TestTrains.REALISTIC_FAST_TRAIN
 import fr.sncf.osrd.utils.Helpers.fullInfraFromFile
@@ -28,6 +32,7 @@ import fr.sncf.osrd.utils.units.Offset
 import fr.sncf.osrd.utils.units.meters
 import fr.sncf.osrd.utils.units.seconds
 import fr.sncf.osrd.utils.units.sumDistances
+import java.util.Objects
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Disabled
@@ -155,6 +160,71 @@ class BacktrackTests {
         )
     }
 
+    private fun buildPathBacktrackingOverRouteDelimiter(
+        infra: FullInfra,
+        rollingStockLength: Distance,
+    ): TrainPath {
+        val firstSignalNameList = listOf("s.right.a1", "s.right.a2", "s.right.a3", "s.right.c2")
+        val secondSignalNameList = listOf("s.left.c3", "s.left.c1", "s.left.b2", "s.left.b1")
+
+        val firstBlocks = signalsToBlocks(firstSignalNameList)
+        val secondBlocks = signalsToBlocks(secondSignalNameList)
+
+        // This is verbose, but it makes it easier to follow along
+        val blockRanges =
+            listOf(
+                // a1 -> a2
+                PartialBlockRange(
+                    firstBlocks[0].first,
+                    Offset(100.meters), // Start at offset 100m on first block
+                    firstBlocks[0].second,
+                    firstBlocks[0].second,
+                ),
+                // a2 -> a3
+                PartialBlockRange(
+                    firstBlocks[1].first,
+                    Offset(0.meters),
+                    firstBlocks[1].second,
+                    firstBlocks[1].second,
+                ),
+                // a3 -> c2
+                PartialBlockRange(
+                    firstBlocks[2].first,
+                    Offset(0.meters),
+                    // Last block before backtrack ends at c2 (offset 3000m), and backtrack is at
+                    // offset 1100m
+                    firstBlocks[2].second - (3000.meters - 1100.meters),
+                    firstBlocks[2].second,
+                ),
+                // Backtrack there: on track t.center at offset 1100m
+                // c1 -> b2
+                PartialBlockRange(
+                    secondBlocks[1].first,
+                    // c1 is at offset 1000m, 400m of train length
+                    Offset(1000.meters - 1100.meters + rollingStockLength),
+                    secondBlocks[1].second,
+                    secondBlocks[1].second,
+                ),
+                // b2 -> b1
+                PartialBlockRange(
+                    secondBlocks[2].first,
+                    Offset(0.meters),
+                    secondBlocks[2].second - 400.meters, // stop 400m before the end of the block
+                    secondBlocks[2].second,
+                ),
+            )
+        val backtrackLocation =
+            Offset<PhysicsPath>(blockRanges.subList(0, 3).map { it.length }.sumDistances())
+
+        return buildTrainPathFromBlockRanges(
+            infra.rawInfra,
+            infra.blockInfra,
+            buildRangeList(blockRanges),
+            routeNames = listOf("rt.bf.a->det.a3", "rt.det.a3->bf.c", "rt.det.c1->bf.b"),
+            backtrackLocations = listOf(backtrackLocation),
+        )
+    }
+
     @Test
     fun testPathProperties() {
         // Smoke test, we only test for uncaught exceptions and failed asserts
@@ -186,6 +256,85 @@ class BacktrackTests {
         val allBlocks = allBlockRanges.map { it.value }.toSet()
         assert(firstBlocks.intersect(secondBlocks).isEmpty())
         assertEquals(allBlocks, firstBlocks.union(secondBlocks))
+    }
+
+    class ZoneOccupation(val entry: Offset<PhysicsPath>, var exit: Offset<PhysicsPath>?) {
+        override fun equals(other: Any?): Boolean {
+            if (other !is ZoneOccupation) return false
+            return entry == other.entry && exit == other.exit
+        }
+
+        override fun hashCode(): Int {
+            return Objects.hash(entry, exit)
+        }
+    }
+
+    fun getZoneOccupations(
+        zoneOccupationChangeEvents: List<ZoneOccupationChangeEvent>
+    ): Map<ZoneId, List<ZoneOccupation>> {
+        val mapZoneOccupations = mutableMapOf<ZoneId, MutableList<ZoneOccupation>>()
+        zoneOccupationChangeEvents.forEach { event ->
+            val updated = mapZoneOccupations.getOrDefault(event.zone, mutableListOf())
+            if (event.isEntry) {
+                val lastOccupation = updated.lastOrNull()
+                // A zone is never entered twice in a row
+                assert(lastOccupation == null || lastOccupation.exit != null)
+                updated.addLast(ZoneOccupation(event.offset, null))
+            } else {
+                // A zone is always entered before exited
+                assert(updated.last().exit == null)
+                updated.last().exit = event.offset
+            }
+            mapZoneOccupations[event.zone] = updated
+        }
+        return mapZoneOccupations
+    }
+
+    @Test
+    fun testZoneOccupationBacktrackSingleZone() {
+        val path = buildPathBacktrackingOverNothing(infra, rollingStock.length.meters)
+        val mrsp = computeMRSP(path, rollingStock, true, null, null, distanceRangeMapOf(), true)
+        val zoneOccupationChangeEvents = zoneOccupationChangeEvents(path, mrsp, 400.meters)
+
+        assertEquals(
+            mapOf(
+                Pair(ZoneId(1u), listOf(ZoneOccupation(Offset(0.meters), Offset(3300.meters)))),
+                Pair(ZoneId(2u), listOf(ZoneOccupation(Offset(2900.meters), Offset(6300.meters)))),
+                Pair(
+                    ZoneId(3u),
+                    listOf(
+                        ZoneOccupation(Offset(5900.meters), Offset(8300.meters)),
+                        ZoneOccupation(Offset(10500.meters), Offset(12900.meters)),
+                    ),
+                ),
+                Pair(ZoneId(7u), listOf(ZoneOccupation(Offset(7900.meters), Offset(10900.meters)))),
+                Pair(
+                    ZoneId(6u),
+                    listOf(ZoneOccupation(Offset(12500.meters), Offset(15900.meters))),
+                ),
+                Pair(ZoneId(5u), listOf(ZoneOccupation(Offset(15500.meters), Offset(18500.meters)))),
+            ),
+            getZoneOccupations(zoneOccupationChangeEvents),
+        )
+    }
+
+    @Test
+    fun testZoneOccupationBacktrackingOverRouteDelimiter() {
+        val path = buildPathBacktrackingOverRouteDelimiter(infra, rollingStock.length.meters)
+        val mrsp = computeMRSP(path, rollingStock, true, null, null, distanceRangeMapOf(), true)
+        val zoneOccupationChangeEvents = zoneOccupationChangeEvents(path, mrsp, 400.meters)
+
+        assertEquals(
+            mapOf(
+                Pair(ZoneId(1u), listOf(ZoneOccupation(Offset(0.meters), Offset(3300.meters)))),
+                Pair(ZoneId(2u), listOf(ZoneOccupation(Offset(2900.meters), Offset(6300.meters)))),
+                Pair(ZoneId(3u), listOf(ZoneOccupation(Offset(5900.meters), Offset(10100.meters)))),
+                Pair(ZoneId(7u), listOf(ZoneOccupation(Offset(7900.meters), Offset(8100.meters)))),
+                Pair(ZoneId(6u), listOf(ZoneOccupation(Offset(9700.meters), Offset(13100.meters)))),
+                Pair(ZoneId(5u), listOf(ZoneOccupation(Offset(12700.meters), Offset(15700.meters)))),
+            ),
+            getZoneOccupations(zoneOccupationChangeEvents),
+        )
     }
 
     @Test
