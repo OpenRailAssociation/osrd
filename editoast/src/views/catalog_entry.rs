cd25b6efe3f047c24ddce5f20e4e65b4ad8f5fcd
Leo Valais <leo.valais97@gmail.com>
2026-03-12 16:50:24 +0100

    editoast: cargo fmt
    Signed-off-by: Leo Valais <leo.valais97@gmail.com>

--- editoast/src/views/catalog_entry.rs
+++ editoast/src/views/catalog_entry.rs
@@ -190,7 +190,8 @@ pub(in crate::views) async fn delete(
 
 #[cfg(test)]
 mod tests {
-    use crate::models::fixtures::{create_catalog_entry, create_catalog_entry_with_name};
+    use crate::models::fixtures::create_catalog_entry;
+    use crate::models::fixtures::create_catalog_entry_with_name;
     use crate::views::test_app::TestAppBuilder;
 
     use super::*;
