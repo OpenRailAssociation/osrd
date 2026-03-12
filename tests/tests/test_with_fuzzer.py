4e9015f1a2ee1c754fa80474128becd6c79a80c9
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2026-03-12 11:31:09 +0100

    tests: run second fuzzer script in CI
    Just to avoid having the script silently breaking with API changes
    
    Signed-off-by: Eloi Charpentier <eloi.charpentier.42@gmail.com>

--- tests/tests/test_with_fuzzer.py
+++ tests/tests/test_with_fuzzer.py
@@ -1,7 +1,7 @@
 import pytest
 
 # TODO: we should clean up the package and imports structure
-from fuzzer import fuzzer
+from fuzzer import fuzzer, fuzzer_stdcm_single_timetable
 from requests import Session
 
 from .scenario import Scenario
@@ -12,3 +12,21 @@ from .services import EDITOAST_URL
 @pytest.mark.parametrize("seed", range(5))
 def test_with_fuzzer(tiny_scenario: Scenario, seed: int, session: Session):
     fuzzer.run(EDITOAST_URL, tiny_scenario, session, n_test=1, seed=seed + 1)
+
+
+@pytest.mark.usefixtures("fast_rolling_stock")
+@pytest.mark.parametrize("seed", range(5))
+def test_with_stdcm_fuzzer(tiny_scenario: Scenario, seed: int, session: Session):
+    """
+    Just a smoke test to make sure the fuzzer is updated on breaking API changes.
+    It won't catch everything though, some invalid inputs are returned as
+    "preprocessing pathfinding errors", which are ignored (the generated paths aren't always valid).
+    """
+    fuzzer_stdcm_single_timetable.run(
+        editoast_url=EDITOAST_URL,
+        towed_rs=None,
+        scenario=tiny_scenario,
+        session=session,
+        n_test=1,
+        seed=seed + 1,
+    )
