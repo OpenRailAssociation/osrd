58a80302c7e65370ae9cfb3adba71c868878d86d
Younes Khoudli <younes.khoudli@epita.fr>
2026-03-13 02:50:23 +0100

    [WIP] disable checking when building CI images
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

--- front/ui/rollup-base.config.js
+++ front/ui/rollup-base.config.js
@@ -12,38 +12,40 @@ const rootDir = path.join(process.cwd(), '..', '..');
 const name = path.basename(process.cwd());
 
 /** @type {import("rollup").RollupOptions} */
-const generateRollupBaseConfig = () => ({
-  input: 'src/index.ts',
-  output: formats.map((format) => ({
-    file: `dist/index.${format}.js`,
-    format,
-    name,
-    sourcemap: true,
-  })),
-  plugins: [
-    nodeResolve({ rootDir }),
-    eslint({
-      throwOnError: !process.env.ROLLUP_WATCH,
-      throwOnWarning: !process.env.ROLLUP_WATCH,
-    }),
-    typescript(),
-    postcss({
-      extract: 'theme.css',
-      sourceMap: true,
-      plugins: [],
-    }),
-    process.env.ROLLUP_WATCH &&
-      livereload({
-        watch: 'dist',
+const generateRollupBaseConfig = () => {
+  const isCI = process.env.CI === 'true';
+  return {
+    input: 'src/index.ts',
+    output: formats.map((format) => ({
+      file: `dist/index.${format}.js`,
+      format,
+      name,
+      sourcemap: true,
+    })),
+    plugins: [
+      nodeResolve({ rootDir }),
+      !isCI && eslint({
+        throwOnError: !process.env.ROLLUP_WATCH,
+        throwOnWarning: !process.env.ROLLUP_WATCH,
       }),
-  ],
-  external: (id, parent, isResolved) => {
-    if (!isResolved) return false;
-    if (id.endsWith('.css')) return false;
-    const rel = path.relative(rootDir, id);
-    const filenames = rel.split(path.sep);
-    return filenames[0] === 'node_modules' || filenames[2] === 'dist';
-  },
-});
+      typescript(),
+      postcss({
+        extract: 'theme.css',
+        sourceMap: true,
+        plugins: [],
+      }),
+      process.env.ROLLUP_WATCH &&
+        livereload({
+          watch: 'dist',
+        }),
+    ],
+    external: (id, parent, isResolved) => {
+      if (!isResolved) return false;
+      if (id.endsWith('.css')) return false;
+      const rel = path.relative(rootDir, id);
+      const filenames = rel.split(path.sep);
+      return filenames[0] === 'node_modules' || filenames[2] === 'dist';
+    },
+}};
 
 export default generateRollupBaseConfig;
