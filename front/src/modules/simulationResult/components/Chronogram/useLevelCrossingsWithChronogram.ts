a885ba1f665d5361c407873c424d9f97437c43f8
SarahBellaha <sarah.bellaha@sncf.fr>
2026-03-17 15:57:49 +0100

    front: create new hook to retrieve level crossing occupancies
    Signed-off-by: SarahBellaha <sarah.bellaha@sncf.fr>

---
+++ front/src/modules/simulationResult/components/Chronogram/useLevelCrossingsWithChronogram.ts
@@ -0,0 +1,78 @@
+import { useEffect, useMemo } from 'react';
+
+import { skipToken } from '@reduxjs/toolkit/query';
+
+import { osrdEditoastApi } from 'common/api/osrdEditoastApi';
+import { useInfraID } from 'common/osrdContext';
+import type { TimetableItemWithDetails } from 'modules/timetableItem/types';
+import { setFailure } from 'reducers/main';
+import { useAppDispatch } from 'store';
+import { castErrorToFailure } from 'utils/error';
+import { extractEditoastIdFromPacedTrainId } from 'utils/trainId';
+
+import { formatLevelCrossingOccupanciesForChronogram } from './buildOccupancyBlocks';
+
+type UseLevelCrossingsWithChronogramProps = {
+  trains: TimetableItemWithDetails[];
+};
+
+const useLevelCrossingsWithChronogram = ({ trains }: UseLevelCrossingsWithChronogramProps) => {
+  const infraId = useInfraID();
+  const dispatch = useAppDispatch();
+
+  const timeOrigin = useMemo(() => new Date(trains[0].startTime).getTime(), [trains]);
+
+  const trainIds = useMemo(
+    () => trains.map((train) => extractEditoastIdFromPacedTrainId(train.id)),
+    [trains]
+  );
+
+  const { currentData: levelCrossingsIds } =
+    osrdEditoastApi.endpoints.getInfraByInfraIdObjectsAndObjectTypeIds.useQuery(
+      infraId
+        ? {
+            infraId,
+            objectType: 'LevelCrossing',
+          }
+        : skipToken
+    );
+
+  const { currentData: levelCrossings } =
+    osrdEditoastApi.endpoints.postInfraByInfraIdObjectsAndObjectType.useQuery(
+      infraId && levelCrossingsIds && levelCrossingsIds.ids.length
+        ? {
+            infraId,
+            objectType: 'LevelCrossing',
+            body: levelCrossingsIds.ids,
+          }
+        : skipToken
+    );
+
+  const { data: levelCrossingOccupancies, error } =
+    osrdEditoastApi.endpoints.postLevelCrossingOccupancy.useQuery(
+      infraId && trainIds.length && levelCrossingsIds && levelCrossingsIds.ids.length
+        ? {
+            body: {
+              infra_id: infraId,
+              train_ids: trainIds,
+              level_crossing_ids: levelCrossingsIds.ids,
+            },
+          }
+        : skipToken
+    );
+
+  const levelCrossingData = useMemo(() => {
+    if (!levelCrossingOccupancies || !levelCrossings) return [];
+    return formatLevelCrossingOccupanciesForChronogram(levelCrossingOccupancies, levelCrossings);
+  }, [levelCrossingOccupancies, levelCrossings]);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(setFailure(castErrorToFailure(error)));
+    }
+  }, [error, dispatch]);
+
+  return { levelCrossingData, timeOrigin };
+};
+
+export default useLevelCrossingsWithChronogram;
