148d5ef70e913a2f9c0b96226350f6e0cae6292a
Egor <egor@berezify.fr>
2026-03-20 11:48:25 +0100

    editoast: adapt etcs braking curves endpoint with new exceptions
    Signed-off-by: Egor <egor@berezify.fr>

--- front/src/common/api/generatedEditoastApi.ts
+++ front/src/common/api/generatedEditoastApi.ts
@@ -1327,7 +1327,7 @@ const injectedRtkApi = api
           params: {
             infra_id: queryArg.infraId,
             electrical_profile_set_id: queryArg.electricalProfileSetId,
-            exception_key: queryArg.exceptionKey,
+            exception_id: queryArg.exceptionId,
           },
         }),
         providesTags: ['paced_train', 'etcs_braking_curves'],
@@ -2618,7 +2618,7 @@ export type GetTrainSchedulesByIdEtcsBrakingCurvesApiArg = {
   id: number;
   infraId: number;
   electricalProfileSetId?: number;
-  exceptionKey?: string;
+  exceptionId?: number;
 };
 export type GetTrainSchedulesByIdPathApiResponse = /** status 200 The path */ PathfindingResult;
 export type GetTrainSchedulesByIdPathApiArg = {
