b94851e64ac82956c79e861a2d7bfb86d0e772c1
Theo Macron <theo.macron0315@gmail.com>
2026-03-25 13:49:49 +0100

    front: handle first row arrival requested case
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/src/modules/timesStops/TimesStopsTable.tsx
+++ front/src/modules/timesStops/TimesStopsTable.tsx
@@ -237,6 +237,7 @@ const TimesStopsTable = ({
                 focusRequestedCellOnTab(info.row.index, 'requestedArrival', direction)
               }
               onCommit={(date) => onArrival(row, date)}
+              disableClear={info.row.index === 0}
             />
           );
         },
