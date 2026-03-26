229b1626b0fad87000890fda79ebb40ac2c6678c
Egor <egor@berezify.fr>
2026-03-26 09:59:30 +0100

    editoast: adapt paced train simulation summary (#15591)
    fix https://github.com/OpenRailAssociation/osrd/issues/15592
    part of #15202
    
    Adapt GET /paced_train/simulation_summary endpoint with new exceptions
    
    Add timetable_id parameter to the simulation summary endpoint, because
    exceptions are now linked to a timetable
    Fix tests and frontend
    
    > [!NOTE]
    > This PR targets a feature branch and may contain all its commits, as
    it has not been rebased yet. Only the last commit need to be reviewed.
    
    Signed-off-by: Egor <egor@berezify.fr>

--- front/src/common/api/generatedEditoastApi.ts
+++ front/src/common/api/generatedEditoastApi.ts
@@ -2562,6 +2562,7 @@ export type PostTrainSchedulesSimulationSummaryApiArg = {
     electrical_profile_set_id?: number | null;
     ids: number[];
     infra_id: number;
+    timetable_id: number;
   };
 };
 export type PostTrainSchedulesTrackOccupancyApiResponse =
