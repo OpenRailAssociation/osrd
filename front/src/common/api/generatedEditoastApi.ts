feef50301773a52154ec0ca5a8393ac3ee7f6803
Egor <egor@berezify.fr>
2026-03-30 15:55:05 +0200

    editoast: adapt occupancy blocks endpoint (#15765)
    Part of https://github.com/OpenRailAssociation/osrd/issues/15202
    
    This PR adapt the `/train_schedules/occupancy_blocks`endpoint to use the
    new exceptions table
    It also requires the timetable_id as a parameter, as exceptions are now
    related to a timetable and not just to a train schedule
    Rename exception_key into exception_id
    Fix tests and frontend
    
    > [!NOTE]
    > This PR targets a feature branch and may contain all its commits, as
    it has not been rebased yet. Only the last commit need to be reviewed.
    
    Signed-off-by: Egor <egor@berezify.fr>

--- front/src/common/api/generatedEditoastApi.ts
+++ front/src/common/api/generatedEditoastApi.ts
@@ -5000,6 +5000,7 @@ export type OccupancyBlockForm = {
   ids: number[];
   infra_id: number;
   path: CoreTrainPath;
+  timetable_id: number;
 };
 export type SpaceTimeCurve = {
   /** List of positions of a train in mm
