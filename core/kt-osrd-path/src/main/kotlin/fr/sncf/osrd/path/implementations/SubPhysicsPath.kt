5df99d88a1e88d1dd583247b566e816ee6d0c97b
Younes Khoudli <younes.khoudli@epita.fr>
2026-03-30 12:02:25 +0200

    core: add a SubPhysicsPath implementation
    This will be necessary to call the simulation on various sections of the
    path to handle consist changes.
    
    Co-authored-by: Loup Federico <16464925+Sh099078@users.noreply.github.com>
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

---
+++ core/kt-osrd-path/src/main/kotlin/fr/sncf/osrd/path/implementations/SubPhysicsPath.kt
@@ -0,0 +1,68 @@
+package fr.sncf.osrd.path.implementations
+
+import com.google.common.collect.ImmutableRangeMap
+import fr.sncf.osrd.path.interfaces.Electrification
+import fr.sncf.osrd.path.interfaces.PhysicsPath
+import fr.sncf.osrd.utils.DistanceRangeMap
+import fr.sncf.osrd.utils.distanceRangeMapOf
+import fr.sncf.osrd.utils.units.meters
+
+class SubPhysicsPath(val begin: Double, val end: Double, val path: PhysicsPath) : PhysicsPath {
+    init {
+        require(begin in 0.0..<end && end <= path.length)
+    }
+
+    override val length: Double
+        get() = end - begin
+
+    override fun getAverageGrade(begin: Double, end: Double): Double {
+        require(end <= this.length)
+        require(begin >= 0)
+        val newBegin = begin + this.begin
+        val newEnd = end + this.begin
+        return path.getAverageGrade(newBegin, newEnd)
+    }
+
+    override fun getMinGrade(begin: Double, end: Double): Double {
+        require(end <= this.length)
+        require(begin >= 0)
+        val newBegin = begin + this.begin
+        val newEnd = end + this.begin
+        return path.getMinGrade(newBegin, newEnd)
+    }
+
+    override fun getElectrificationMap(
+        basePowerClass: String?,
+        powerRestrictionMap: DistanceRangeMap<String>?,
+        powerRestrictionToPowerClass: Map<String, String>?,
+        ignoreElectricalProfiles: Boolean,
+    ): DistanceRangeMap<Electrification> {
+        val powerRestrictionMapBuilder = ImmutableRangeMap.Builder<Double, String>()
+        val newPowerRestrictionMap =
+            powerRestrictionMap
+                ?.map {
+                    DistanceRangeMap.RangeMapEntry(
+                        it.lower + begin.meters,
+                        it.upper + begin.meters,
+                        it.value,
+                    )
+                }
+                ?.let { distanceRangeMapOf(it) }
+        val electrificationMap =
+            path.getElectrificationMap(
+                basePowerClass,
+                newPowerRestrictionMap,
+                powerRestrictionToPowerClass,
+                ignoreElectricalProfiles,
+            )
+        return electrificationMap
+            .map {
+                DistanceRangeMap.RangeMapEntry(
+                    it.lower - begin.meters,
+                    it.upper - begin.meters,
+                    it.value,
+                )
+            }
+            .let { distanceRangeMapOf(it) }
+    }
+}
