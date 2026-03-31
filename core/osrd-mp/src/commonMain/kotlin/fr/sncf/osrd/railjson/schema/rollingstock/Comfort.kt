640373eddaff291c63392919b2c01285f8126922
Hubert Hirtz <hubert@hirtz.pm>
2026-03-31 11:30:15 +0200

    core: move Comfort to osrd-mp
    Needed because the new train simulation module depends on
    `TrainPhysicsIntegrator` which depends on `PhysicsRollingStock` which
    depends on it.
    
    Signed-off-by: Hubert Hirtz <hubert@hirtz.pm>

---
+++ core/osrd-mp/src/commonMain/kotlin/fr/sncf/osrd/railjson/schema/rollingstock/Comfort.kt
@@ -0,0 +1,7 @@
+package fr.sncf.osrd.railjson.schema.rollingstock
+
+enum class Comfort {
+    AIR_CONDITIONING,
+    HEATING,
+    STANDARD,
+}
--- core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/rollingstock/Comfort.java
+++
@@ -1,7 +0,0 @@
-package fr.sncf.osrd.railjson.schema.rollingstock;
-
-public enum Comfort {
-    AIR_CONDITIONING,
-    HEATING,
-    STANDARD
-}
