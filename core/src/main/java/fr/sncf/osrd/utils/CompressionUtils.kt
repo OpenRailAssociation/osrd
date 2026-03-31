40564d135586eb01db77029a54272fac4e974e7c
Eloi Charpentier <eloi.charpentier.42@gmail.com>
2026-03-31 15:03:00 +0200

    core: save loaded infras to s3 when available
    Signed-off-by: Eloi Charpentier <eloi.charpentier.42@gmail.com>

--- core/src/main/java/fr/sncf/osrd/utils/CompressionUtils.kt
+++ core/src/main/java/fr/sncf/osrd/utils/CompressionUtils.kt
@@ -1,10 +1,12 @@
 package fr.sncf.osrd.utils
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonWriter
 import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
-import java.util.zip.ZipEntry
-import java.util.zip.ZipOutputStream
+import okio.buffer
+import okio.sink
 
 /** GZIP compression */
 fun ByteArray.compress(): ByteArray {
@@ -18,13 +20,14 @@ fun ByteArray.decompress(): ByteArray {
     return GZIPInputStream(this.inputStream()).use { it.readBytes() }
 }
 
-/** ZIP compression */
-fun ByteArray.compressToZip(innerFilename: String): ByteArray {
-    val outputStream = ByteArrayOutputStream(this.size)
-    ZipOutputStream(outputStream).use {
-        val entry = ZipEntry(innerFilename)
-        it.putNextEntry(entry)
-        it.write(this)
+/** GZIP compression of json data, skipping the String intermediate copy. */
+fun <T> JsonAdapter<T>.compress(data: T): ByteArray {
+    val outputStream = ByteArrayOutputStream()
+    GZIPOutputStream(outputStream).use {
+        val sink = it.sink().buffer()
+        val writer = JsonWriter.of(sink)
+        this.toJson(writer, data)
+        writer.flush()
     }
     return outputStream.toByteArray()
 }
