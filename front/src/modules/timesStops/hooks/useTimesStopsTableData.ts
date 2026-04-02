99703a7659e881f526ae4714d1e735789d533ee0
nncluzu <ngamenichaka@yahoo.fr>
2026-04-02 11:48:19 +0200

    front: optimize TimesStopsTable row filtering
    Signed-off-by: nncluzu <ngamenichaka@yahoo.fr>

--- front/src/modules/timesStops/hooks/useTimesStopsTableData.ts
+++ front/src/modules/timesStops/hooks/useTimesStopsTableData.ts
@@ -227,7 +227,7 @@ const useTimesStopsTableData = (
     fetchTrackSections();
   }, [trackIds]);
 
-  const rows = useMemo(() => {
+  const allRows = useMemo(() => {
     const startDate = new Date(selectedTrain.start_time);
     const scheduleByAt = keyBy(selectedTrain.schedule, 'at');
 
@@ -335,7 +335,7 @@ const useTimesStopsTableData = (
             track: trackName,
             opOnPathIndex: opIndex,
           });
-        } else if (!displayOnlyPathSteps) {
+        } else {
           let computedArrival: Duration | undefined;
           if (stableTrain) {
             const matchingReportTrainIndex = stableTrain.positions.findIndex(
@@ -399,11 +399,15 @@ const useTimesStopsTableData = (
     stablePathItemRespect,
     trackSections,
     pathStepOps,
-    displayOnlyPathSteps,
     t,
   ]);
 
-  return { rows, stableIsValid };
+  const filteredRows = useMemo(
+    () => (displayOnlyPathSteps ? allRows.filter((row) => row.isPathStep) : allRows),
+    [allRows, displayOnlyPathSteps]
+  );
+
+  return { rows: filteredRows, stableIsValid };
 };
 
 export default useTimesStopsTableData;
