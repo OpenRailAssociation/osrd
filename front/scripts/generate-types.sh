ce43989495247d84d494066aaa8758742d12f9e4
Stanislas Signoud (Signez) <signez@stanisoft.net>
2026-04-03 12:05:35 +0200

    front: change .cts files to proper .json
    Even though codegen-openapi documentation suggests that it can accept module .ts files when used with npx, it actually doesn't in our current project configuration.
    
    As I have to get rid of those CJS modules for some Typescript configuration, let's move those into .json files. It will also make sure the code generator do not use esbuild or ts-node just to understand a static file :)
    
    Signed-off-by: Stanislas Signoud (Signez) <signez@stanisoft.net>

--- front/scripts/generate-types.sh
+++ front/scripts/generate-types.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-if ! npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.cts; then
-    echo "npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.cts command failed. Exit the script"
+if ! npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.json; then
+    echo "npx @rtk-query/codegen-openapi src/config/openapi-editoast-config.json command failed. Exit the script"
     exit 1
 fi
-if ! npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.cts; then
-    echo "npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.cts command failed. Exit the script"
+if ! npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.json; then
+    echo "npx @rtk-query/codegen-openapi src/config/openapi-gateway-config.json command failed. Exit the script"
     exit 1
 fi
-if ! npx @rtk-query/codegen-openapi src/config/openapi-railway-manager-interface-config.cts; then
-    echo "npx @rtk-query/codegen-openapi src/config/openapi-railway-manager-interface-config.cts command failed. Exit the script"
+if ! npx @rtk-query/codegen-openapi src/config/openapi-railway-manager-interface-config.json; then
+    echo "npx @rtk-query/codegen-openapi src/config/openapi-railway-manager-interface-config.json command failed. Exit the script"
     exit 1
 fi
 npx oxfmt src/common/api/ --write
