f858689def3ceae2e125c7589e02a17d5cd6ac19
Theo Macron <theo.macron0315@gmail.com>
2026-04-07 09:48:08 +0200

    front: adapt calculateTimeDifferenceInDays function and tests
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/src/utils/__tests__/timeManipulation.spec.ts
+++ front/src/utils/__tests__/timeManipulation.spec.ts
@@ -3,11 +3,6 @@ import { describe, it, expect } from 'vitest';
 import { calculateTimeDifferenceInDays } from 'utils/timeManipulation';
 
 describe('calculateTimeDifferenceInDays', () => {
-  it('should handle undefined dates', () => {
-    expect(calculateTimeDifferenceInDays(undefined, new Date(2024, 1, 1, 15))).toEqual(undefined);
-    expect(calculateTimeDifferenceInDays(new Date(2024, 1, 1, 15), undefined)).toEqual(undefined);
-    expect(calculateTimeDifferenceInDays(undefined, undefined)).toEqual(undefined);
-  });
   it('should handle 2 dates on the same day', () => {
     expect(
       calculateTimeDifferenceInDays(new Date(2024, 1, 1, 10), new Date(2024, 1, 1, 15))
