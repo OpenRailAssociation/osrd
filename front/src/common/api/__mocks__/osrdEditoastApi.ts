34ceb81358696afd26db387760388afd14cd265a
Simon Ser <contact@emersion.fr>
2026-04-16 10:46:15 +0200

    front: introduce mocking framework for osrdEditoastApi
    Up until now, we've been mocking directly the API exposed by
    rtk-query: useQuery(), useMutation(), useLazyQuery() and friends.
    This is fragile because:
    
    - The relies on the exact way the tested function uses rtk-query.
      If the tested function switches from useLazyQuery() or initiate()
      to useQuery(), tests break.
    - rtk-query's complicated API needs to be replicated at multiple
      levels: hooks return objects with unwrap() functions. Only a
      small part of the API can reasonably be mocked, and this falls
      apart when the tested function starts using more.
    - None of this is type-safe. If the API changes, either tests
      fail with potentially hard-to-debug errors, either they silently
      pass and become outdated. Nothing prevents mocked functions from
      returning garbage.
    
    This commit introduces a new mocking framework which operates at a
    lower level: it integrates with rtk-query with enhanceEndpoints()
    and defines a custom queryFn called in place of the HTTP request.
    
    The framework exposes a type-safe collection of mocked functions
    with well-defined argument and return types automatically
    extracted from the original osrdEditoastApi. Any value passed to
    mockResolvedValue() is checked by TypeScript.
    
    Unfortunately two casts cannot be eliminated (one due to objects
    being cumbersome to manipulate in TypeScript, the other due to an
    rtk-query shortcoming).
    
    This commit is the result of multiple days of head banging against
    the table, dozens of litres of tea, countless yells in the direction
    of Microsoft headquarters, multiple cups of tears, and a handful of
    shower thoughts. But it was all worth it in the end!
    
    Earlier iterations of this work ended up being massively more
    complicated, and I only realized quite late in the process how
    all of this mess could be simplified. For posterity:
    https://paste.sr.ht/~emersion/ab23c2c3cd32a4daa1be228d6e6b5fceed33d76f#testEditoastApi.ts
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

---
+++ front/src/common/api/__mocks__/osrdEditoastApi.ts
@@ -0,0 +1,88 @@
+import type { QueryReturnValue } from '@reduxjs/toolkit/query';
+import { vi, type Mock } from 'vitest';
+
+import type { ApiError } from '../baseGeneratedApis';
+import { osrdEditoastApi } from '../osrdEditoastApi';
+
+/**
+ * Object type holding all defined API endpoints.
+ */
+type Endpoints = (typeof osrdEditoastApi)['endpoints'];
+
+/**
+ * Given an endpoint type, extract its argument and result types and build a
+ * mock function type.
+ *
+ * This allows us to type-check that the mocked endpoints get the proper query
+ * arguments and returns a type that reflects the one that would actually be
+ * returned by the endpoint if it wasn't mocked.
+ */
+type MockQueryFn<E> = E extends { Types: { QueryArg: infer A; ResultType: infer R } }
+  ? Mock<(arg: A) => QueryReturnValue<R, ApiError>>
+  : never;
+
+/**
+ * Mapped type providing a typed mock function for each defined API endpoint.
+ */
+type MockApi = {
+  [Name in keyof Endpoints]: MockQueryFn<Endpoints[Name]>;
+};
+
+// Create a mock function for each endpoint
+const mockEndpointEntries = Object.keys(osrdEditoastApi.endpoints).map(
+  (name) => [name, vi.fn()] as const
+);
+
+/**
+ * Exposes type-safe mocked functions for each endpoint.
+ *
+ * vitest's .mockResolvedValue() and expect().toHaveBeenCalledWith() functions
+ * can be used on these mocked functions.
+ *
+ * To use these mocks from a test, renderHookWithStore() needs to be used
+ * because rtk-query requires a Redux store.
+ */
+const mockEndpoints = Object.fromEntries(mockEndpointEntries) as MockApi;
+// ↑ Unfortunately we need a cast here: TypeScript disregards
+// Object.fromEntries() input types and always produces objects with string
+// keys.
+
+/**
+ * Given a function taking any number of arguments, return a function which
+ * only accepts the first one.
+ *
+ * rtk-query calls queryFn with 4 arguments, but we only care about the first
+ * one which contains the QueryArg. This allows us to leverage
+ * expect().toHaveBeenCalledWith() to only check QueryArg.
+ */
+function callWithOnlyFirstParam<F extends (arg: unknown) => unknown>(f: F) {
+  return (arg: Parameters<F>[0]) => f(arg);
+}
+
+// Override endpoint query functions with mocks
+const mockApi = osrdEditoastApi.enhanceEndpoints({
+  endpoints: Object.fromEntries(
+    mockEndpointEntries.map(([name, f]) => [
+      name,
+      {
+        query: undefined,
+        queryFn: callWithOnlyFirstParam(f),
+      },
+    ])
+  ),
+}) as unknown as typeof osrdEditoastApi;
+// ↑ Unfortunately we need a cast here, because .enhanceEndpoints() never
+// returns the exact same type as the original even if no new
+// tags/endpoints got added, and Typescript can't know we didn't do that.
+
+export {
+  // This is the test-facing part of the API mocking framework.
+  mockEndpoints as mockOsrdEditoastEndpoints,
+
+  // This is the hook/component-facing part of the mocked API, that will be
+  // used in place of the real osrdEditoastApi.
+  //
+  // Because this file is placed in a __mocks__ directory, vitest will load it
+  // by default when vi.mock('common/api/osrdEditoastApi') is called.
+  mockApi as osrdEditoastApi,
+};
