035caa069560eba2332532c93a1cbb3e0c42788e
Léo VALAIS <leovalais+git@proton.me>
2026-04-20 17:57:38 +0200

    editoast: authz: merge two old tests in `authz::v2`
    Old group tests use interfaces we will remove soon.
    With the new test in `v2` and the previously existsing ones
    everything tested by removed tests is still covered.
    
    Signed-off-by: Léo VALAIS <leovalais+git@proton.me>

--- editoast/authz/src/v2/roles.rs
+++ editoast/authz/src/v2/roles.rs
@@ -86,8 +86,8 @@ pub fn remove_roles(subject: Subject, roles: HashSet<Role>) -> Protected<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::v2::TestClientExt as _;
     use crate::v2::special_authorizers::Authorize;
+    use crate::v2::{TestClientExt as _, add_members};
 
     use super::*;
 
@@ -252,6 +252,109 @@ mod tests {
         assert_eq!(
             openfga.subject_roles(&Subject::user(1)).await,
             HashSet::from_iter([])
+        );
+    }
+
+    #[tokio::test]
+    async fn inherited_roles_from_group() {
+        let openfga = crate::authz_client!();
+        let authorize = Authorize(&openfga);
+
+        // 1: Admin
+        // 2: nothing
+        // 10: Stdcm w/ 1 & 2
+        add_roles(Subject::user(1), HashSet::from_iter([Role::Admin]))
+            .authorize(&authorize)
+            .await
+            .unwrap()
+            .unwrap_authorized()
+            .await;
+        add_roles(Subject::group(10), HashSet::from_iter([Role::Stdcm]))
+            .authorize(&authorize)
+            .await
+            .unwrap()
+            .unwrap_authorized()
+            .await;
+        add_members(Group(10), HashSet::from_iter([User(1), User(2)]))
+            .authorize(&authorize)
+            .await
+            .unwrap()
+            .unwrap_authorized()
+            .await;
+
+        assert_eq!(
+            openfga.subject_roles(&Subject::group(10)).await,
+            HashSet::from_iter([Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(1)).await,
+            HashSet::from_iter([Role::Admin, Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(2)).await,
+            HashSet::from_iter([Role::Stdcm])
+        );
+
+        // 11: OperationalStudies w/ 2
+        add_roles(
+            Subject::group(11),
+            HashSet::from_iter([Role::OperationalStudies]),
+        )
+        .authorize(&authorize)
+        .await
+        .unwrap()
+        .unwrap_authorized()
+        .await;
+        add_members(Group(11), HashSet::from_iter([User(2)]))
+            .authorize(&authorize)
+            .await
+            .unwrap()
+            .unwrap_authorized()
+            .await;
+
+        assert_eq!(
+            openfga.subject_roles(&Subject::group(10)).await,
+            HashSet::from_iter([Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::group(11)).await,
+            HashSet::from_iter([Role::OperationalStudies])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(1)).await,
+            HashSet::from_iter([Role::Admin, Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(2)).await,
+            HashSet::from_iter([Role::Stdcm, Role::OperationalStudies])
+        );
+
+        // 11: nothing w/ 2
+        remove_roles(
+            Subject::group(11),
+            HashSet::from_iter([Role::OperationalStudies]),
+        )
+        .authorize(&authorize)
+        .await
+        .unwrap()
+        .unwrap_authorized()
+        .await;
+
+        assert_eq!(
+            openfga.subject_roles(&Subject::group(10)).await,
+            HashSet::from_iter([Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::group(11)).await,
+            HashSet::new()
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(1)).await,
+            HashSet::from_iter([Role::Admin, Role::Stdcm])
+        );
+        assert_eq!(
+            openfga.subject_roles(&Subject::user(2)).await,
+            HashSet::from_iter([Role::Stdcm])
         );
     }
 }
