464a9bbe8023a81b85cb6b26178bd094d0879886
Florian Amsallem <florian.amsallem@gmail.com>
2026-04-24 12:22:54 +0200

    editoast: add max pool size for valkey
    This allow to define a max pool size. Default was
    `cpu_core_count * 2`.
    
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/cache/src/client.rs
+++ editoast/cache/src/client.rs
@@ -29,6 +29,7 @@ pub enum Config {
     Valkey {
         url: Url,
         response_timeout: Duration,
+        pool_size: usize,
     },
 }
 
@@ -41,9 +42,15 @@ impl Client {
                 Config::Valkey {
                     url,
                     response_timeout,
+                    pool_size,
                 } => ClientInner::Tokio(
                     deadpool_redis::Config::from_url(url)
-                        .create_pool(Some(Runtime::Tokio1))
+                        .builder()
+                        .unwrap()
+                        .runtime(Runtime::Tokio1)
+                        .max_size(pool_size)
+                        .wait_timeout(Some(std::time::Duration::from_millis(100)))
+                        .build()
                         .unwrap(),
                     response_timeout,
                 ),
