629b73f97d2906edb81f6aebbf5e425bde82282a
Jean SIMARD <woshilapin@tuziwo.info>
2026-04-27 16:57:06 +0200

    editoast: simplify traces for authentication validation middleware
    Signed-off-by: Jean SIMARD <woshilapin@tuziwo.info>

--- editoast/src/views/mod.rs
+++ editoast/src/views/mod.rs
@@ -553,6 +553,7 @@ async fn authentication_extraction_middleware(
 /// - the impersonator must be an admin
 /// - the impersonated user must already exist in the database, we do not create it if it does not
 /// - push the roles of the origin user in the request extensions
+#[tracing::instrument(skip_all)]
 async fn authentication_validation_middleware(
     Extension(authn): Extension<crate::authentication::Authentication>,
     State(AppState {
@@ -668,21 +669,12 @@ async fn authentication_validation_middleware(
     }
 
     let user_id = user.as_ref().map(|editoast_models::User { id, .. }| *id);
-    Ok(tracing::info_span!(
-        "authentication validation",
-        user_id,
-        ?roles,
-        is_admin = roles.contains(&Role::Admin) // easy way to filter real user requests in logs
-    )
-    .in_scope(move || {
-        req.extensions_mut().insert(roles);
-        // for `fga` queries and `authz` interface
-        req.extensions_mut().insert(user_id.map(::authz::User));
-        // database model, also carries the user name
-        req.extensions_mut().insert(user);
-        next.run(req).in_current_span()
-    })
-    .await)
+    req.extensions_mut().insert(roles);
+    // for `fga` queries and `authz` interface
+    req.extensions_mut().insert(user_id.map(::authz::User));
+    // database model, also carries the user name
+    req.extensions_mut().insert(user);
+    Ok(next.run(req).await)
 }
 
 /// Represents the bundle of information about the issuer of a request
