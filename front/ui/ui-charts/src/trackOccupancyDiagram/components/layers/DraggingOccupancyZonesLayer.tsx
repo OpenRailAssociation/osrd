50eda8d8f942ad05bf6dc97c2b1738e60608dd33
Simon Ser <contact@emersion.fr>
2026-05-15 16:15:20 +0200

    ui-charts: add draggingOccupancyZones prop to TrackOccupancyCanvas
    This new property can be used to render occupancy zones currently
    being dragged by the user. They are rendered over other occupancy
    zones in blue, delimited with two dashed vertical lines.
    
    The caller is responsible for passing this property because only
    the caller knows the set of zones that need to start being dragged
    (in OSRD, this will depend on multiple factors such as selection
    mode). Additionally, this allows the caller to be in full control
    of the start and end of the drag operation.
    
    In the TOD story, a new isDragging arg can be toggled to test this
    property.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

---
+++ front/ui/ui-charts/src/trackOccupancyDiagram/components/layers/DraggingOccupancyZonesLayer.tsx
@@ -0,0 +1,69 @@
+import { useCallback, useContext } from 'react';
+
+import { useDraw } from '../../../common/hooks/useCanvas';
+import type { DrawingFunction } from '../../../common/types';
+import {
+  SpaceTimeChartCanvasContext,
+  type SpaceTimeChartContextType,
+} from '../../../spaceTimeChart';
+import { MouseContext } from '../../../spaceTimeChart/lib/context';
+import { COLORS } from '../../lib/consts';
+import type { OccupancyZone } from '../../lib/types';
+
+const DraggingOccupancyZonesLayer = ({ occupancyZones }: { occupancyZones: OccupancyZone[] }) => {
+  const mouseContext = useContext(MouseContext);
+
+  const draw = useCallback<DrawingFunction<SpaceTimeChartContextType>>(
+    (ctx, { getTimePixel, height: viewportHeight }) => {
+      for (const zone of occupancyZones) {
+        const xStart = getTimePixel(zone.startTime);
+        const xEnd = getTimePixel(zone.endTime);
+
+        const height = 5;
+        const radius = 2;
+        const rectY = mouseContext.position.y - height / 2;
+        ctx.beginPath();
+        ctx.roundRect(xStart, rectY, xEnd - xStart, height, radius);
+
+        // Draw zone box
+        ctx.save();
+        ctx.fillStyle = COLORS.PRIMARY_80;
+        ctx.shadowBlur = 8;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 5;
+        ctx.shadowColor = 'rgba(60, 138, 255, 0.55)';
+        ctx.fill();
+        ctx.restore();
+
+        // Draw shadow under the zone box
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'rgb(105, 255, 254)';
+        ctx.stroke();
+        ctx.restore();
+
+        // Draw two vertical dashed lines around the zone box
+        ctx.save();
+        ctx.lineWidth = 1.5;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = COLORS.PRIMARY_50;
+        ctx.setLineDash([0, 4]);
+        const verticalLineSpacing = 2.5;
+        ctx.beginPath();
+        ctx.moveTo(xStart - verticalLineSpacing, 0);
+        ctx.lineTo(xStart - verticalLineSpacing, viewportHeight);
+        ctx.moveTo(xEnd + verticalLineSpacing, 0);
+        ctx.lineTo(xEnd + verticalLineSpacing, viewportHeight);
+        ctx.stroke();
+        ctx.restore();
+      }
+    },
+    [occupancyZones, mouseContext.position.y]
+  );
+
+  useDraw(SpaceTimeChartCanvasContext, 'overlay', draw);
+
+  return null;
+};
+
+export default DraggingOccupancyZonesLayer;
