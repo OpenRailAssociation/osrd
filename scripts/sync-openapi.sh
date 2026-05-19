1e8ecbf1f45fb72d456587651c5cf0d8f84e67fa
Pierre-Etienne Bougué <bougue.pe@proton.me>
2026-05-19 15:16:35 +0200

    editoast: update script to sync openapi
    after https://github.com/OpenRailAssociation/osrd/pull/16687
    
    Signed-off-by: Pierre-Etienne Bougué <bougue.pe@proton.me>

--- scripts/sync-openapi.sh
+++ scripts/sync-openapi.sh
@@ -5,7 +5,19 @@ set -eu
 root_path="$(realpath "$(dirname "$0")"/..)"
 
 echo "Generating editoast's openapi"
-( cd "${root_path}/editoast" && cargo run openapi > "${root_path}"/editoast/openapi.yaml )
+(
+    cd "${root_path}/editoast"
+    cargo run openapi > "${root_path}"/editoast/openapi.yaml
+)
 
 echo "Generating the typescript client"
-( cd "${root_path}"/front && npm run generate-types )
+(
+    cd "${root_path}"/front
+    npm run generate-types
+)
+
+echo "Generating osrd_schemas_auto"
+(
+    cd "${root_path}"/osrd_schemas_auto
+    ./generate-types.sh > osrd_schemas_auto/models.py
+)
