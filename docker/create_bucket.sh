a643eeab929d6c11a007c97c61cb6405e816944f
Gaëtan Duchaussois <gaetan-osrd@tcgi.fr>
2026-05-21 12:04:08 +0200

    docker: add lifecycle for s3 bucket
    Signed-off-by: Gaëtan Duchaussois <gaetan-osrd@tcgi.fr>

--- docker/create_bucket.sh
+++ docker/create_bucket.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
+expiration_days=60
+
 mc alias set my_s3 "$AWS_ENDPOINT_URL_S3" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
 mc mb -p "my_s3/$1"
+mc ilm import "my_s3/$1" <<EOF
+{
+  "Rules": [
+    {
+      "Expiration": {
+        "Days": $expiration_days
+      },
+      "ID": "Default_rule",
+      "Status": "Enabled"
+    }
+  ]
+}
+EOF
