1a1d63a7fc07cf7c0ee512d8929686437b967269
Younes Khoudli <younes.khoudli@epita.fr>
2026-05-22 14:50:33 +0200

    core: convert RJSSwitch to kotlin
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

---
+++ core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/RJSSwitch.kt
@@ -0,0 +1,15 @@
+package fr.sncf.osrd.railjson.schema.infra
+
+import com.squareup.moshi.Json
+import fr.sncf.osrd.railjson.schema.common.Identified
+
+data class RJSSwitch(
+    /** The switch ID */
+    override var id: String,
+    /** The type of the switch */
+    @Json(name = "switch_type") var switchType: String,
+    /** The track sections connected to the ports of the switch */
+    val ports: Map<String, RJSTrackEndpoint>,
+    /** The delay when changing the position in seconds */
+    @Json(name = "group_change_delay") var groupChangeDelay: Double,
+) : Identified
