96434aa7d20f57461648c8da824d8ad33806eb44
Younes Khoudli <younes.khoudli@epita.fr>
2026-05-22 14:25:44 +0200

    core: convert RJSRoute to kotlin
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

---
+++ core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/RJSRoute.kt
@@ -0,0 +1,35 @@
+package fr.sncf.osrd.railjson.schema.infra
+
+import com.squareup.moshi.Json
+import fr.sncf.osrd.railjson.schema.common.Identified
+import fr.sncf.osrd.railjson.schema.common.RJSWaypointRef
+import fr.sncf.osrd.railjson.schema.common.graph.EdgeDirection
+import fr.sncf.osrd.railjson.schema.infra.trackobjects.RJSTrainDetector
+
+/**
+ * Routes are described as a list of TVD Sections, Switches in specific positions, and an entry
+ * point
+ */
+data class RJSRoute(
+    override val id: String,
+    @Json(name = "entry_point") val entryPoint: RJSWaypointRef<RJSTrainDetector>,
+    @Json(name = "entry_point_direction") val entryPointDirection: EdgeDirection,
+    @Json(name = "exit_point") val exitPoint: RJSWaypointRef<RJSTrainDetector>,
+    @Json(name = "release_detectors") val releaseDetectors: List<String>,
+    @Json(name = "switches_directions") val switchesDirections: Map<String, String>,
+) : Identified {
+    constructor(
+        id: String,
+        entryPoint: RJSWaypointRef<RJSTrainDetector>,
+        entryPointDirection: EdgeDirection,
+        exitPoint: RJSWaypointRef<RJSTrainDetector>,
+        switchesDirections: Map<String, String>,
+    ) : this(
+        id = id,
+        entryPoint = entryPoint,
+        entryPointDirection = entryPointDirection,
+        exitPoint = exitPoint,
+        releaseDetectors = listOf(),
+        switchesDirections = switchesDirections,
+    )
+}
