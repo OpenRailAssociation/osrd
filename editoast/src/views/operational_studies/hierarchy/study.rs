fcfc912b6de50cb427be195f65afed74ac3d53de
Léo VALAIS <leovalais+git@proton.me>
2026-05-22 15:54:51 +0200

    editoast: views: move `Ordering` in `operational_studies::hierarchy`
    Work schedules endpoint wrongfully uses this enum as well. This should
    be addressed at a later time.
    
    Signed-off-by: Léo VALAIS <leovalais+git@proton.me>

--- editoast/src/views/operational_studies/hierarchy/study.rs
+++ editoast/src/views/operational_studies/hierarchy/study.rs
@@ -18,10 +18,10 @@ use thiserror::Error;
 use utoipa::IntoParams;
 use utoipa::ToSchema;
 
+use super::OperationalStudiesOrderingParam;
 use super::project::ProjectError;
 use crate::error::InternalError;
 use crate::error::Result;
-use crate::views::operational_studies::OperationalStudiesOrderingParam;
 use crate::views::pagination::PaginatedList as _;
 use crate::views::pagination::PaginationQueryParams;
 use crate::views::pagination::PaginationStats;
