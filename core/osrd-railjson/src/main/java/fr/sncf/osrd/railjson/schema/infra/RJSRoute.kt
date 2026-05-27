f42e8880cb5df35ce440b9328f5192b23f4ee13f
Younes Khoudli <younes.khoudli@epita.fr>
2026-05-27 17:30:09 +0200

    core: allow routes to refer to bufferstops
    It seems the typesafety here is never actually used, but route can refer
    to bufferstops as their start/end.
    
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

--- core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/RJSRoute.kt
+++ core/osrd-railjson/src/main/java/fr/sncf/osrd/railjson/schema/infra/RJSRoute.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import fr.sncf.osrd.railjson.schema.common.Identified
 import fr.sncf.osrd.railjson.schema.common.RJSWaypointRef
 import fr.sncf.osrd.railjson.schema.common.graph.EdgeDirection
-import fr.sncf.osrd.railjson.schema.infra.trackobjects.RJSTrainDetector
+import fr.sncf.osrd.railjson.schema.infra.trackobjects.RJSRouteWaypoint
 
 /**
  * Routes are described as a list of TVD Sections, Switches in specific positions, and an entry
@@ -12,17 +12,17 @@ import fr.sncf.osrd.railjson.schema.infra.trackobjects.RJSTrainDetector
  */
 data class RJSRoute(
     override val id: String,
-    @Json(name = "entry_point") val entryPoint: RJSWaypointRef<RJSTrainDetector>,
+    @Json(name = "entry_point") val entryPoint: RJSWaypointRef<out RJSRouteWaypoint>,
     @Json(name = "entry_point_direction") val entryPointDirection: EdgeDirection,
-    @Json(name = "exit_point") val exitPoint: RJSWaypointRef<RJSTrainDetector>,
+    @Json(name = "exit_point") val exitPoint: RJSWaypointRef<out RJSRouteWaypoint>,
     @Json(name = "release_detectors") val releaseDetectors: List<String>,
     @Json(name = "switches_directions") val switchesDirections: Map<String, String>,
 ) : Identified {
     constructor(
         id: String,
-        entryPoint: RJSWaypointRef<RJSTrainDetector>,
+        entryPoint: RJSWaypointRef<out RJSRouteWaypoint>,
         entryPointDirection: EdgeDirection,
-        exitPoint: RJSWaypointRef<RJSTrainDetector>,
+        exitPoint: RJSWaypointRef<out RJSRouteWaypoint>,
         switchesDirections: Map<String, String>,
     ) : this(
         id = id,
