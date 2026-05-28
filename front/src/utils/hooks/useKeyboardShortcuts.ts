7bdd99df7a8c1ca3f9350065605f3f79e995ecf7
Yohh <durandyohan@zaclys.net>
2026-05-28 08:40:54 +0200

    front, test: add unit test for handleKeyboardEvents
    - extract handleKeyboardEvents from useKeyboardShortcuts and export it
    - create useKeyboardShortcuts.spec.ts and add unit tests for handleKeyboardEvents
    
    Signed-off-by: Yohh <durandyohan@zaclys.net>

--- front/src/utils/hooks/useKeyboardShortcuts.ts
+++ front/src/utils/hooks/useKeyboardShortcuts.ts
@@ -14,7 +14,7 @@ export type Shortcut = {
   handler: () => void;
 };
 
-function shortcutRegistryKey(code: string, optionalKeys?: OptionalKeys): string {
+export function shortcutRegistryKey(code: string, optionalKeys?: OptionalKeys): string {
   return [
     optionalKeys?.ctrlKey && 'Ctrl',
     optionalKeys?.shiftKey && 'Shift',
@@ -25,7 +25,53 @@ function shortcutRegistryKey(code: string, optionalKeys?: OptionalKeys): string 
     .join('-');
 }
 
-export default function useKeyboardShortcuts(shortcuts?: Shortcut[]) {
+// Listen keyboard event on document
+export const handleKeyboardEvents = (
+  event: KeyboardEvent,
+  registery: React.RefObject<Record<string, Shortcut>>
+) => {
+  if (event.target instanceof Element) {
+    // prevent keyboard shortcut execution in text inputs.
+    if (event.target.matches('input, textarea')) return;
+
+    // disable the handler for chrome developer tools
+    if (event.code === 'KeyI' && event.metaKey && event.altKey) return;
+
+    // disable the handler for copy keyboard shortcut (ctrl+c, cmd+c)
+    if (event.code === 'KeyC' && (event.metaKey || event.ctrlKey)) return;
+  }
+
+  const shortcutKey = shortcutRegistryKey(event.code, {
+    shiftKey: event.shiftKey,
+    ctrlKey: event.ctrlKey,
+    metaKey: event.metaKey,
+  });
+  const shortcut = registery.current[shortcutKey];
+  if (shortcut) {
+    shortcut.handler();
+    event.preventDefault();
+  }
+};
+
+/**
+ * Custom hook to manage keyboard shortcuts application.
+ * It allows you to register and unregister keyboard shortcuts,
+ * and handles the execution of the corresponding handlers when the specified key combinations are pressed.
+ *
+ * Registry is stored in a ref to avoid unnecessary re-renders when shortcuts are registered or unregistered.
+ *
+ * The hook listens for keyboard events on the document body and checks if the pressed key combination matches any registered shortcut.
+ * If a match is found, the corresponding handler is executed, and the default behavior of the event is prevented.
+ *
+ * @param shortcuts - An optional array of shortcuts to register when the hook is initialized.
+ * Each shortcut should include a `code` representing the key code, an optional `optionalKeys` object specifying modifier keys (shift, ctrl, meta), and a `handler` function to execute when the shortcut is triggered.
+ * @return An object containing the current registry of shortcuts (as a read-only record), and functions to register and unregister shortcuts.
+ */
+export default function useKeyboardShortcuts(shortcuts?: Shortcut[]): {
+  registery: Readonly<Record<string, Shortcut>>;
+  register: (shortcut: Shortcut) => void;
+  unRegister: (shortcut: Shortcut) => void;
+} {
   const registery = useRef<Record<string, Shortcut>>({});
 
   const register = useCallback(
@@ -50,34 +96,12 @@ export default function useKeyboardShortcuts(shortcuts?: Shortcut[]) {
     // register shortcuts given at initialization
     shortcuts?.forEach((s) => register(s));
 
-    // Listen keyboard event on document
-    const handleKeyboardEvents = (event: KeyboardEvent) => {
-      if (event.target instanceof Element) {
-        // prevent keyboard shortcut execution in text inputs.
-        if (event.target.matches('input, textarea')) return;
-
-        // disable the handler for chrome developer tools
-        if (event.code === 'KeyI' && event.metaKey && event.altKey) return;
-
-        // disable the handler for copy keyboard shortcut (ctrl+c, cmd+c)
-        if (event.code === 'KeyC' && (event.metaKey || event.ctrlKey)) return;
-      }
-
-      const shortcutKey = shortcutRegistryKey(event.code, {
-        shiftKey: event.shiftKey,
-        ctrlKey: event.ctrlKey,
-        metaKey: event.metaKey,
-      });
-      const shortcut = registery.current[shortcutKey];
-      if (shortcut) {
-        shortcut.handler();
-        event.preventDefault();
-      }
-    };
-    document.body.addEventListener('keydown', handleKeyboardEvents);
+    document.body.addEventListener('keydown', (event) => handleKeyboardEvents(event, registery));
     return () => {
       registery.current = {};
-      document.body.removeEventListener('keydown', handleKeyboardEvents);
+      document.body.removeEventListener('keydown', (event) =>
+        handleKeyboardEvents(event, registery)
+      );
     };
   }, []);
 
