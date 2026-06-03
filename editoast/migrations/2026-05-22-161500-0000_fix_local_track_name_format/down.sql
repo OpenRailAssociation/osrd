7db6139d4d84701dd54c9d1bae153c6e15cc6148
Duc Nguyen <nguyen@emersion.fr>
2026-06-03 16:46:40 +0200

    editoast: make fix_local_track_name_format down migration runnable
    Make 2026-05-22-161500-0000_fix_local_track_name_format migration revertable by adding a noop.
    
    Data is not recoverable but we can still run the down migration since there are no breaking changes. This allows previous migration to be reverted.
    
    Signed-off-by: Duc Nguyen <nguyen@emersion.fr>

--- editoast/migrations/2026-05-22-161500-0000_fix_local_track_name_format/down.sql
+++ editoast/migrations/2026-05-22-161500-0000_fix_local_track_name_format/down.sql
@@ -1,2 +1,5 @@
 -- Irreversible data correction migration.
 -- It normalizes malformed JSON values in-place.
+-- Add a noop so the migration can still be reversed
+-- since it won't cause any breaking change.
+SELECT 1;
