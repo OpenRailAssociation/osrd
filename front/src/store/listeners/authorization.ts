37a306fc55edd0be718c2bf8b665a75e0d39033a
Benoit Simard <contact@bsimard.com>
2026-06-12 17:44:18 +0200

    front: check rolling permission on scenario page
    - List of rolling stock is not filtered (yet) following the user
      privileges. So the filter is done on the front side for now. The `RollingStockContext` has been changed  for that.
    - On scenario page we can have some API request with a 403 which is not
      a terminail state. So we need to bypass the 403 listener for the
      application. To make that, I added a query param named
      `__skipGlobal403` on the `getRollingStockByName` to bypass the
      listener
    
    Signed-off-by: Benoit Simard <contact@bsimard.com>
    Co-authored-by: Duc Nguyen <nguyen@emersion.fr>

--- front/src/store/listeners/authorization.ts
+++ front/src/store/listeners/authorization.ts
@@ -1,14 +1,37 @@
 import { isRejectedWithValue, type PayloadAction } from '@reduxjs/toolkit';
 import { type FetchBaseQueryError } from '@reduxjs/toolkit/query';
 
+import type { osrdEditoastApi } from 'common/api/osrdEditoastApi';
+
 import { type AppStartListening } from './types';
+
+type EditoastEndpointName = keyof typeof osrdEditoastApi.endpoints;
+
+type RTKQueryRejectedAction = PayloadAction<
+  FetchBaseQueryError,
+  string,
+  {
+    arg: {
+      endpointName: EditoastEndpointName;
+    };
+  }
+>;
+
+/**
+ * Endpoints for which a 403 response should NOT trigger a global redirect.
+ * Add an endpoint name here when a 403 is expected and handled locally.
+ */
+const ENDPOINTS_SKIP_403_REDIRECT = new Set<EditoastEndpointName>([
+  'getRollingStockNameByRollingStockName',
+  'getTrainSchedulesByIdPath',
+]);
 
 export default function add403HttpErrorListener(startListening: AppStartListening) {
   startListening({
     matcher: isRejectedWithValue,
-    effect: (action: PayloadAction<unknown>) => {
-      const error = action.payload as FetchBaseQueryError;
-      if (error?.status === 403) {
+    effect: (action) => {
+      const { payload: error, meta } = action as RTKQueryRejectedAction;
+      if (error?.status === 403 && !ENDPOINTS_SKIP_403_REDIRECT.has(meta.arg.endpointName)) {
         window.location.href = '/403';
       }
     },
