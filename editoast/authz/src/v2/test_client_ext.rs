8570e7ba723b2eb22b54e03216cf2d089606f1ee
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2026-06-16 10:46:13 +0200

    editoast: list rolling_stocks privileges
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>

--- editoast/authz/src/v2/test_client_ext.rs
+++ editoast/authz/src/v2/test_client_ext.rs
@@ -76,6 +76,11 @@ pub trait TestClientExt {
         subject: Subject,
         grant: RollingStockGrant,
     );
+    async fn rolling_stock_list(
+        &self,
+        user: User,
+        privilege: RollingStockPrivilege,
+    ) -> ResourcesList<RollingStock>;
 }
 
 impl TestClientExt for fga::Client {
@@ -273,6 +278,20 @@ impl TestClientExt for fga::Client {
         let authorize = special_authorizers::Authorize(self);
         authorize
             .access_value(crate::v2::infra::infra_list(user, privilege))
+            .await
+            .unwrap()
+    }
+
+    async fn rolling_stock_list(
+        &self,
+        user: User,
+        privilege: RollingStockPrivilege,
+    ) -> ResourcesList<RollingStock> {
+        let authorize = special_authorizers::Authorize(self);
+        authorize
+            .access_value(crate::v2::rolling_stock::rolling_stock_list(
+                user, privilege,
+            ))
             .await
             .unwrap()
     }
