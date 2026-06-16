7fc1b1ee692bf9d8651ecc0453bddc2a4357f8d2
Pivouane (Pierre Santamaria) <pierrenumber1@outlook.com>
2026-06-16 18:26:35 +0200

    front: link TrainHeader warning when rolling stock and category are an unusual match
    Signed-off-by: Pivouane (Pierre Santamaria) <pierrenumber1@outlook.com>

--- front/src/modules/rollingStock/helpers/category.ts
+++ front/src/modules/rollingStock/helpers/category.ts
@@ -1,5 +1,26 @@
-import type { TrainCategory } from 'common/api/osrdEditoastApi';
+import type { LightRollingStock, SubCategory, TrainCategory } from 'common/api/osrdEditoastApi';
 
 export default function isMainCategory(category: TrainCategory | null) {
   return category === null || 'main_category' in category;
+}
+
+export function checkCategoryWarning(
+  rollingStock: LightRollingStock | undefined,
+  currentCategory: TrainCategory | null,
+  currentSubCategory?: SubCategory
+): boolean {
+  if (!rollingStock || !currentCategory) return false;
+
+  if (isMainCategory(currentCategory)) {
+    return (
+      currentCategory.main_category !== rollingStock.primary_category &&
+      !rollingStock.other_categories.includes(currentCategory.main_category)
+    );
+  }
+
+  if (currentSubCategory) {
+    return currentSubCategory.main_category !== rollingStock.primary_category;
+  }
+
+  return false;
 }
