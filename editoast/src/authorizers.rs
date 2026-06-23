495f1e7b6be75f2ba120a174c2460ba993880d73
Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
2026-06-23 22:12:58 +0200

    editoast: implement authorizers::require utility function
    Signed-off-by: Youness CHRIFI ALAOUI <youness.chrifi@gmail.com>
    Signed-off-by: Loup Federico <16464925+Sh099078@users.noreply.github.com>

--- editoast/src/authorizers.rs
+++ editoast/src/authorizers.rs
@@ -2,6 +2,7 @@ use std::convert::Infallible;
 use std::marker::PhantomData;
 use std::ops::Not as _;
 
+use crate::views::AuthorizationError;
 use authz::ProjectGrant;
 use authz::v2::Access;
 use authz::v2::Actor;
@@ -280,6 +281,36 @@ impl Authorizer for UserAuthorizer<'_> {
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
 pub struct Error(#[from] pub authz::v2::OpenFgaError);
+
+/// Ensures the issuer holds a privilege satisfying `required`.
+/// `protected` is an operation yielding the set of privileges the issuer holds on a resource.
+/// Access is granted when the operation is authorized and the issuer holds a privilege equal to `required`.
+pub async fn require<I, U>(
+    authorizer: &U,
+    protected: Protected<I>,
+    required: &<I as IntoIterator>::Item,
+) -> Result<(), AuthorizationError>
+where
+    I: IntoIterator,
+    <I as IntoIterator>::Item: PartialEq,
+    U: Authorizer<Error = Error>,
+{
+    let access = authorizer
+        .authorize(protected)
+        .await
+        .map_err(|e| AuthorizationError::from(e.0))?;
+    let Ok(privileges) = access.access().await? else {
+        return Err(AuthorizationError::Forbidden);
+    };
+    if privileges
+        .into_iter()
+        .any(|privilege| privilege == *required)
+    {
+        Ok(())
+    } else {
+        Err(AuthorizationError::Forbidden)
+    }
+}
 
 #[cfg(test)]
 mod tests {
