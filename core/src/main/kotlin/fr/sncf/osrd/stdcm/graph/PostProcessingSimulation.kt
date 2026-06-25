9d49cb4b2f8b49e2266fdb92714167f22f1b53be
Pierre-Etienne Bougué <bougue.pe@proton.me>
2026-06-26 00:15:05 +0200

    [wip] core: get the final envelope even if conflicting mismatch
    Signed-off-by: Pierre-Etienne Bougué <bougue.pe@proton.me>

--- core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/PostProcessingSimulation.kt
+++ core/src/main/kotlin/fr/sncf/osrd/stdcm/graph/PostProcessingSimulation.kt
@@ -118,6 +118,7 @@ fun buildFinalEnvelope(
         )
     require(concatenateAndShiftEnvelopes(maxSpeedEnvelopes).continuous)
     val maxIterations = edges.size * 2 // just to avoid infinite loops on bugs or edge cases
+    var lastSim: FinalEnvelopeResult? = null
     repeat(maxIterations) {
         try {
             val newEnvelope =
@@ -131,6 +132,7 @@ fun buildFinalEnvelope(
                         comfort,
                     )
                 )
+            lastSim = FinalEnvelopeResult(newEnvelope, allowanceRanges)
             val conflictOffset =
                 findConflictOffsets(newEnvelope, blockAvailability, edges, updatedTimeData)
                     ?: return FinalEnvelopeResult(newEnvelope, allowanceRanges)
@@ -193,10 +195,19 @@ fun buildFinalEnvelope(
                 // For example when the train stops during/after a coasting.
                 postProcessingLogger.warn("Impossible simulation")
                 return@repeat
-            } else throw e
+            } else {
+                if (lastSim != null) return lastSim
+                throw e
+            }
+        } catch (e: Exception) {
+            if (lastSim != null) return lastSim
+            throw e
         }
     }
-    throw RuntimeException("Failed to compute a standard allowance that wouldn't cause conflicts")
+    if (lastSim != null) return lastSim
+    throw RuntimeException(
+        "Failed to compute a standard allowance that wouldn't cause conflicts PEB"
+    )
 }
 
 /** Initialize all fixed points at stop locations, including stop durations. */
