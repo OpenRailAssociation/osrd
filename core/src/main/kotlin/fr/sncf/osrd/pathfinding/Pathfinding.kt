f34e7f2d418291361da3e23b449b021da4659488
Pierre-Etienne Bougué <bougue.pe@proton.me>
2026-06-30 08:59:14 +0200

    fixup! core, editoast: hack: plug backtrackings for stdcm

--- core/src/main/kotlin/fr/sncf/osrd/pathfinding/Pathfinding.kt
+++ core/src/main/kotlin/fr/sncf/osrd/pathfinding/Pathfinding.kt
@@ -89,7 +89,7 @@ class Pathfinding(
                 it.locations,
                 it.duration,
                 it.stop,
-                if (index > 0 && index < inputTargets.size - 1) it.canBacktrack else false,
+                if (index > 0 && index < inputTargets.size - 1) true else false,
                 it.plannedTimingData,
             )
         }
