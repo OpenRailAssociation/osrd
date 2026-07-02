e19b945c02273cb814d1084bf0e7030dad823515
Angelina Kuntz <angelina.kuntz@epita.fr>
2026-07-02 11:02:37 +0200

    editoast: fix unicity constraints migration
    Signed-off-by: Angelina Kuntz <angelina.kuntz@epita.fr>

--- editoast/migrations/2026-06-10-120651-0000_add_unique_constraint_on_op_fields/up.sql
+++ editoast/migrations/2026-06-10-120651-0000_add_unique_constraint_on_op_fields/up.sql
@@ -13,7 +13,7 @@ WHERE data->>'secondary_name' = 'OVERTAKE';
 
 CREATE SEQUENCE secondary_code_seq;
 UPDATE infra_object_operational_point i
--- Add the ID as suffix to secondary code to the duplicated domestic codes
+-- Add an integer as suffix to secondary code to the duplicated domestic codes
 SET data = jsonb_set(data, '{secondary_code}', to_jsonb(CONCAT(COALESCE(data->>'secondary_code', 'secondary_code'), '_', nextval('secondary_code_seq'))))
 FROM (
      SELECT infra_id,
@@ -30,6 +30,24 @@ WHERE i.infra_id = duplicates.infra_id
      AND i.data->>'main_code' IS NOT DISTINCT FROM duplicates.main_code
      AND i.data->>'secondary_code' IS NOT DISTINCT FROM duplicates.secondary_code
      AND i.data->>'country_code' IS NOT DISTINCT FROM duplicates.country_code;
+DROP SEQUENCE secondary_code_seq;
+
+CREATE SEQUENCE secondary_code_seq;
+UPDATE infra_object_operational_point i
+-- Add an integer as suffix to secondary code to the duplicated uic codes
+SET data = jsonb_set(data, '{secondary_code}', to_jsonb(CONCAT(COALESCE(data->>'secondary_code', 'secondary_code'), '_', nextval('secondary_code_seq'))))
+FROM (
+     SELECT infra_id,
+     data->>'uic' AS uic,
+     data->>'secondary_code' AS secondary_code
+     FROM infra_object_operational_point
+     GROUP BY infra_id,
+     data->>'uic',
+     data->>'secondary_code'
+     HAVING COUNT(*) > 1) AS duplicates
+WHERE i.infra_id = duplicates.infra_id
+     AND i.data->>'uic' IS NOT DISTINCT FROM duplicates.uic
+     AND i.data->>'secondary_code' IS NOT DISTINCT FROM duplicates.secondary_code;
 DROP SEQUENCE secondary_code_seq;
 
 CREATE UNIQUE INDEX domestic_unique_index ON infra_object_operational_point(infra_id, (data->>'country_code'), (data->>'main_code'), (data->>'secondary_code')) NULLS NOT DISTINCT;
