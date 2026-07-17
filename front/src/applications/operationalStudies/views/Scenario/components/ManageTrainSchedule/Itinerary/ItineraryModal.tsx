9f510378208a176d5f2e855d7a057701e1cff97f
Mathieu <mathieu.coulibaly@sncf.fr>
2026-07-17 10:01:07 +0200

    front: fix path item field disappearing on clear in itinerary modal
    Signed-off-by: Mathieu <mathieu.coulibaly@sncf.fr>

--- front/src/applications/operationalStudies/views/Scenario/components/ManageTrainSchedule/Itinerary/ItineraryModal.tsx
+++ front/src/applications/operationalStudies/views/Scenario/components/ManageTrainSchedule/Itinerary/ItineraryModal.tsx
@@ -802,7 +802,6 @@ const ItineraryModal = ({
                     }}
                     onOpClear={() => {
                       clearStep(pathStep.id);
-                      handleDeletePathStep(pathStep.id);
                     }}
                     onOpFocus={() => {
                       markEditing(pathStep.id);
