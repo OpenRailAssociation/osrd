02d1bb249f2944922abb9c739c907dde1c99ee55
Yohh <durandyohan@zaclys.net>
2026-07-28 11:36:16 +0200

    front, test: add i18n setup for integration tests
    no need for now to setup a full provider integration
    because we don't need to wrap any component, we
    actually test hooks, but in the future we could use
    `RenderHookWithStore()` and add `i18n` following the
    documentation in order to add component integration tests,
    we should maybe rename it by `RenderHookWithProviders()`.
    
    Signed-off-by: Yohh <durandyohan@zaclys.net>

--- front/vitest.setup.ts
+++ front/vitest.setup.ts
@@ -1,3 +1,16 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { vi } from 'vitest';
+
+await i18n.use(initReactI18next).init({
+  lng: 'cimode',
+  debug: false,
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
 
 vi.mock('common/api/osrdEditoastApi');
