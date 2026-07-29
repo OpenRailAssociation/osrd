069a2a1b003f87096b69e444cac630517b73d607
Pivouane (Pierre Santamaria) <pierrenumber1@outlook.com>
2026-07-29 10:13:01 +0200

    step: change chronogram imports
    Changed imports from lib to common
    
    Found a raw number in chronogram format.ts for number of ms in a second
    
    Signed-off-by: Pivouane (Pierre Santamaria) <pierrenumber1@outlook.com>

--- front/ui/ui-charts/src/chronogram/components/OccupancyBlocksLayer.tsx
+++ front/ui/ui-charts/src/chronogram/components/OccupancyBlocksLayer.tsx
@@ -11,7 +11,7 @@ import {
 } from '../../common/helpers/colors';
 import { useDraw } from '../../common/hooks/useCanvas';
 import type { TimeChartContextType, DrawingFunction } from '../../common/types';
-import { CHRONOGRAM_HEADER_HEIGHT, LEVEL_CROSSING_ITEM_HEIGHT } from '../lib/const';
+import { LEVEL_CROSSING_ITEM_HEIGHT, CHRONOGRAM_HEADER_HEIGHT } from "../../common/consts";
 import { ChronogramContext } from '../lib/context';
 import type { OccupancyBlock } from '../lib/types';
 import { formatDuration } from '../utils/format';
