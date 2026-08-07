5754053686d4bde13ae6e2a5ca284d8e2a068047
Simon Ser <contact@emersion.fr>
2026-08-07 12:34:13 +0200

    front: cleanup react testing library after each test
    The cleanup docs [1] state:
    
    > Failing to call cleanup when you've called render could result in a
    > memory leak and tests which are not "idempotent" (which can lead to
    > difficult to debug errors in your tests).
    
    See also the setup guide for vitest [2].
    
    [1]: https://testing-library.com/docs/react-testing-library/api#cleanup
    [2]: https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/vitest.setup.ts
+++ front/vitest.setup.ts
@@ -1,6 +1,11 @@
+import { cleanup } from '@testing-library/react';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { vi } from 'vitest';
+import { vi, afterEach } from 'vitest';
+
+afterEach(() => {
+  cleanup();
+});
 
 await i18n.use(initReactI18next).init({
   lng: 'cimode',
