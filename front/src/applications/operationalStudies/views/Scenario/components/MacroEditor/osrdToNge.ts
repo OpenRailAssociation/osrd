091dbffb28146b51674a5cbc9ee95d30d098d84c
Duc Nguyen <nguyen@emersion.fr>
2026-08-07 10:58:22 +0200

    front: nge, remove country_code prefix from trigram
    fix #18033
    
    Signed-off-by: Duc Nguyen <nguyen@emersion.fr>

--- front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/osrdToNge.ts
+++ front/src/applications/operationalStudies/views/Scenario/components/MacroEditor/osrdToNge.ts
@@ -337,7 +337,7 @@ export const loadAndIndexNge = async (
     for (const pathKey of MacroEditorState.getPathKeys(op)) {
       state.updateNodeDataByKey(pathKey, {
         full_name: op.name,
-        trigram: MacroEditorState.encodeDomesticReference({ ...op, type: 'domestic' }),
+        trigram: op.main_code + (op.secondary_code ? `/${op.secondary_code}` : ''),
         geocoord: op.geo ? { lng: op.geo.coordinates[0], lat: op.geo.coordinates[1] } : undefined,
       });
     }
