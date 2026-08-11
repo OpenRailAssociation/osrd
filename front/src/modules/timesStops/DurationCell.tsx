c7520465459bf65cf071ca0edb0b54d3a65a3091
babu bahir <rohit.logicsquare@gmail.com>
2026-08-11 18:22:15 +0530

    front: fix remove technical icon titles
    Remove the `title` attribute from the generated icons to prevent having an unwanted tooltip pointing on the icon path when the component title is not defined. - Add the `title` prop for arrival, duration and departure cell for the clear icon in the timestops table
    
    Signed-off-by: babu bahir <rohit.logicsquare@gmail.com>

--- front/src/modules/timesStops/DurationCell.tsx
+++ front/src/modules/timesStops/DurationCell.tsx
@@ -397,12 +397,14 @@ export type DurationCellHandle = {
 
 const DurationCell = ({
   disabled,
+  clearButtonTitle,
   ref,
   ...props
 }: CellContext<TimesStopsRowNew, Duration | null> &
   Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> & {
     onCommit?: (seconds: number | null, propagationMode: StopPropagationMode) => void;
     disabled?: boolean;
+    clearButtonTitle?: string;
     ref?: React.Ref<DurationCellHandle>;
   }) => {
   const { onCommit, getValue, row, table } = props || {};
@@ -567,7 +569,12 @@ const DurationCell = ({
           disableToDestination={isLastRow}
         />
       </div>
-      <ClearButton isVisible={state.isEditing} containerRef={containerRef} onClear={handleClear} />
+      <ClearButton
+        isVisible={state.isEditing}
+        title={clearButtonTitle}
+        containerRef={containerRef}
+        onClear={handleClear}
+      />
     </>
   );
 };
