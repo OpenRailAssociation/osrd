4ac388c699bb86f46be8abee485518b67d7a142c
Simon Ser <contact@emersion.fr>
2026-08-12 17:11:19 +0200

    front: adapt ExtraOccurrenceForm for hourly timetables
    TimePicker is not suitable for hourly timetables because it displays
    an absolute time capped at 24h, and which may have an AM/PM selector.
    Use DurationInput for hourly timetables.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/modules/trainHeader/ExtraOccurrenceForm.tsx
+++ front/src/modules/trainHeader/ExtraOccurrenceForm.tsx
@@ -1,11 +1,13 @@
-import { Button, DatePicker, TimePicker } from '@osrd-project/ui-core';
+import { Button, DatePicker, TimePicker, DurationInput } from '@osrd-project/ui-core';
 import { useTranslation } from 'react-i18next';
+
+import { Duration, type StartTime } from 'utils/duration';
 
 import { ANY_DATE_SLOT } from './ExpandedTrainForm';
 
 type ExtraOccurrenceFormProps = {
-  addedExceptionDate: Date;
-  setAddedExceptionDate: (newDate: Date) => void;
+  addedExceptionDate: StartTime;
+  setAddedExceptionDate: (newDate: StartTime) => void;
   onCreateAddedException: () => void;
 };
 
@@ -18,45 +20,64 @@ const ExtraOccurrenceForm = ({
 
   return (
     <div className="train-add-extra-occurrence-form">
-      <DatePicker
-        testIdPrefix="train-header-extra-occurrence-date"
-        value={addedExceptionDate}
-        onDateChange={(newDate) => {
-          if (newDate) {
-            const dateWithTime = new Date(newDate.getTime());
-            dateWithTime.setHours(
-              addedExceptionDate.getHours(),
-              addedExceptionDate.getMinutes(),
-              addedExceptionDate.getSeconds()
-            );
-            setAddedExceptionDate(dateWithTime);
-          }
-        }}
-        selectableSlot={ANY_DATE_SLOT}
-        inputProps={{
-          id: 'train-add-extra-occurrence-date',
-          label: t('manageTrainSchedule.trainHeader.form.departureDate'),
-          small: true,
-        }}
-      />
-      <TimePicker
-        id={'train-add-extra-occurrence-time'}
-        testIdPrefix="train-header-extra-occurrence-time"
-        label={t('manageTrainSchedule.trainHeader.form.departureTime')}
-        hours={addedExceptionDate.getHours()}
-        minutes={addedExceptionDate.getMinutes()}
-        seconds={addedExceptionDate.getSeconds()}
-        displaySeconds={true}
-        onTimeChange={({ hours, minutes, seconds }) => {
-          const newDate = new Date(addedExceptionDate.getTime());
-          newDate.setHours(hours);
-          newDate.setMinutes(minutes);
-          newDate.setSeconds(seconds ?? 0);
-
-          setAddedExceptionDate(newDate);
-        }}
-        small
-      />
+      {addedExceptionDate instanceof Date ? (
+        <>
+          <DatePicker
+            testIdPrefix="train-header-extra-occurrence-date"
+            value={addedExceptionDate}
+            onDateChange={(newDate) => {
+              if (newDate) {
+                const dateWithTime = new Date(newDate.getTime());
+                dateWithTime.setHours(
+                  addedExceptionDate.getHours(),
+                  addedExceptionDate.getMinutes(),
+                  addedExceptionDate.getSeconds()
+                );
+                setAddedExceptionDate(dateWithTime);
+              }
+            }}
+            selectableSlot={ANY_DATE_SLOT}
+            inputProps={{
+              id: 'train-add-extra-occurrence-date',
+              label: t('manageTrainSchedule.trainHeader.form.departureDate'),
+              small: true,
+            }}
+          />
+          <TimePicker
+            id={'train-add-extra-occurrence-time'}
+            testIdPrefix="train-header-extra-occurrence-time"
+            label={t('manageTrainSchedule.trainHeader.form.departureTime')}
+            hours={addedExceptionDate.getHours()}
+            minutes={addedExceptionDate.getMinutes()}
+            seconds={addedExceptionDate.getSeconds()}
+            displaySeconds={true}
+            onTimeChange={({ hours, minutes, seconds }) => {
+              const newDate = new Date(addedExceptionDate.getTime());
+              newDate.setHours(hours);
+              newDate.setMinutes(minutes);
+              newDate.setSeconds(seconds ?? 0);
+              setAddedExceptionDate(newDate);
+            }}
+            small
+          />
+        </>
+      ) : (
+        <DurationInput
+          id={'train-add-extra-occurrence-time'}
+          label={t('manageTrainSchedule.trainHeader.form.departureTime')}
+          value={addedExceptionDate.ms}
+          units={[
+            { key: 'h', label: ':' },
+            { key: 'm', label: ':' },
+            { key: 's', label: '' },
+          ]}
+          padChar="0"
+          onChange={(milliseconds) => {
+            setAddedExceptionDate(new Duration({ milliseconds }));
+          }}
+          small
+        />
+      )}
       <div className="actions">
         <Button
           id={'train-add-extra-occurrence-time-add'}
