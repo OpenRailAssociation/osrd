e02c80f47c34d5d74ef36cc03e41f9a8bedcd607
Simon Ser <contact@emersion.fr>
2026-08-12 16:34:22 +0200

    front: drop hardcoded date/time formatting in ExtraOccurrenceRow
    Use Date.toLocale{Date,Time}String() for proper localization.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/modules/trainHeader/ExtraOccurrenceRow.tsx
+++ front/src/modules/trainHeader/ExtraOccurrenceRow.tsx
@@ -1,46 +1,38 @@
 import { Trash } from '@osrd-project/ui-icons';
 
+import { useDateTimeLocale } from 'utils/date';
+
 type ExtraOccurrenceRowProps = {
   startTime: Date;
   onDelete: () => void;
 };
 
-function formatDateString(date: Date) {
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = String(date.getFullYear()).slice(-2);
-  return `${day}/${month}/${year}`;
-}
+const ExtraOccurrenceRow = ({ startTime, onDelete }: ExtraOccurrenceRowProps) => {
+  const dateTimeLocale = useDateTimeLocale();
 
-function formatTimeString(date: Date) {
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-const ExtraOccurrenceRow = ({ startTime, onDelete }: ExtraOccurrenceRowProps) => (
-  <div className="train-extra-occurrence" data-testid="train-header-extra-occurrence-row">
-    <button
-      className="train-extra-occurrence-delete"
-      onClick={onDelete}
-      data-testid="train-header-extra-occurrence-delete-button"
-    >
-      <Trash />
-    </button>
-    <div
-      className="train-extra-occurrence-date"
-      data-testid="train-header-extra-occurrence-date-value"
-    >
-      {formatDateString(startTime)}
+  return (
+    <div className="train-extra-occurrence" data-testid="train-header-extra-occurrence-row">
+      <button
+        className="train-extra-occurrence-delete"
+        onClick={onDelete}
+        data-testid="train-header-extra-occurrence-delete-button"
+      >
+        <Trash />
+      </button>
+      <div
+        className="train-extra-occurrence-date"
+        data-testid="train-header-extra-occurrence-date-value"
+      >
+        {startTime.toLocaleDateString(dateTimeLocale)}
+      </div>
+      <div
+        className="train-extra-occurrence-time"
+        data-testid="train-header-extra-occurrence-time-value"
+      >
+        {startTime.toLocaleTimeString(dateTimeLocale)}
+      </div>
     </div>
-    <div
-      className="train-extra-occurrence-time"
-      data-testid="train-header-extra-occurrence-time-value"
-    >
-      {formatTimeString(startTime)}
-    </div>
-  </div>
-);
+  );
+};
 
 export default ExtraOccurrenceRow;
