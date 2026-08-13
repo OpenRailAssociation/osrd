8a400757e11c6e4ee582b0b3b76887ac8d5bf679
Florian Amsallem <florian.amsallem@gmail.com>
2026-08-13 10:35:38 +0200

    editoast: front: adapt nge node path item key
    The idea is to add the country code at the end of the path_item_key.
    In addition we adapt nodes trigram so the import/export works again.
    
    Signed-off-by: Florian Amsallem <florian.amsallem@gmail.com>

--- editoast/migrations/2026-08-06-164808-0000_adapt_node_path_item_key/down.sql
+++ editoast/migrations/2026-08-06-164808-0000_adapt_node_path_item_key/down.sql
@@ -1,2 +1,3 @@
 DELETE FROM macro_node WHERE path_item_key LIKE 'trigram:%';
-UPDATE macro_node SET path_item_key = 'trigram:' || SUBSTRING(path_item_key, 13) WHERE path_item_key LIKE 'domestic:FR-%';
+UPDATE macro_node SET path_item_key = 'trigram:' || SUBSTRING(path_item_key, 10, LENGTH(path_item_key) - 12) WHERE path_item_key LIKE 'domestic:%#__';
+UPDATE macro_node SET trigram = SUBSTRING(trigram, 1, LENGTH(trigram) - 3) WHERE trigram LIKE '%#__';
--- editoast/migrations/2026-08-06-164808-0000_adapt_node_path_item_key/up.sql
+++ editoast/migrations/2026-08-06-164808-0000_adapt_node_path_item_key/up.sql
@@ -1,2 +1,2 @@
-DELETE FROM macro_node WHERE path_item_key LIKE 'domestic:FR-%';
-UPDATE macro_node SET path_item_key = 'domestic:FR-' || SUBSTRING(path_item_key, 9) WHERE path_item_key LIKE 'trigram:%';
+DELETE FROM macro_node WHERE path_item_key LIKE 'domestic:%#FR';
+UPDATE macro_node SET path_item_key = 'domestic:' || SUBSTRING(path_item_key, 9) || '#FR', trigram = trigram || '#FR' WHERE path_item_key LIKE 'trigram:%';
