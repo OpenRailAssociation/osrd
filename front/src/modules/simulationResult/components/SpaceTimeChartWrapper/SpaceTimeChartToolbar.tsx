d4f45ba16f5dd8bb369b14c3da58d55dbf615bb6
nncluzu <ngamenichaka@yahoo.fr>
2026-08-13 13:22:19 +0200

    front: restyle the linking button in the STD toolbar
    Signed-off-by: nncluzu <ngamenichaka@yahoo.fr>

--- front/src/modules/simulationResult/components/SpaceTimeChartWrapper/SpaceTimeChartToolbar.tsx
+++ front/src/modules/simulationResult/components/SpaceTimeChartWrapper/SpaceTimeChartToolbar.tsx
@@ -48,37 +48,39 @@ const SpaceTimeChartToolbar = ({
           <Linking variant={linkingMode ? 'fill' : 'base'} />
         </button>
       )}
-      <button
-        data-testid="zoom-reset-button"
-        type="button"
-        className={cx('reset-button', {
-          'reset-button-disabled': isResetButtonDisabled,
-        })}
-        onClick={onResetClick}
-        disabled={isResetButtonDisabled}
-      >
-        <Iterations />
-      </button>
-      <button
-        data-testid="zoom-button"
-        type="button"
-        className={cx('zoom-button', {
-          'zoom-button-clicked': zoomMode,
-          'zoom-button-disabled': disableZoom,
-        })}
-        onClick={toggleZoomMode}
-        disabled={disableZoom}
-      >
-        <ZoomIn className="icon" />
-      </button>
-      <button
-        type="button"
-        data-testid="menu-button"
-        className="menu-button"
-        onClick={() => setShowSettingsPanel(true)}
-      >
-        <Sliders />
-      </button>
+      <div className="toolbar-group">
+        <button
+          data-testid="zoom-reset-button"
+          type="button"
+          className={cx('reset-button', {
+            'reset-button-disabled': isResetButtonDisabled,
+          })}
+          onClick={onResetClick}
+          disabled={isResetButtonDisabled}
+        >
+          <Iterations />
+        </button>
+        <button
+          data-testid="zoom-button"
+          type="button"
+          className={cx('zoom-button', {
+            'zoom-button-clicked': zoomMode,
+            'zoom-button-disabled': disableZoom,
+          })}
+          onClick={toggleZoomMode}
+          disabled={disableZoom}
+        >
+          <ZoomIn className="icon" />
+        </button>
+        <button
+          type="button"
+          data-testid="menu-button"
+          className="menu-button"
+          onClick={() => setShowSettingsPanel(true)}
+        >
+          <Sliders />
+        </button>
+      </div>
     </div>
   );
 };
