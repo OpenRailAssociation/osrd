5dfbdee48f63a3d83b29d1097ea295e212add9b1
Léo VALAIS <leovalais+git@proton.me>
2026-08-18 20:50:24 +0200

    editoast: views: add missing round trip tests
    Signed-off-by: Léo VALAIS <leovalais+git@proton.me>

--- editoast/src/views/round_trips.rs
+++ editoast/src/views/round_trips.rs
@@ -41,6 +41,7 @@ enum RoundTripsError {
 
 /// Represents a collection of round trips and one-way
 #[derive(Debug, Default, Clone, Deserialize, Serialize, ToSchema)]
+#[cfg_attr(test, derive(PartialEq))]
 pub(in crate::views) struct RoundTrips {
     /// List of one-way trains
     #[serde(default)]
@@ -160,6 +161,7 @@ pub(in crate::views) async fn delete(
 
 /// Paginated list of round trips / one-way
 #[derive(Serialize, ToSchema)]
+#[cfg_attr(test, derive(Debug, Deserialize, PartialEq))]
 pub(in crate::views) struct RoundTripsPage {
     #[serde(flatten)]
     stats: PaginationStats,
@@ -206,10 +208,234 @@ pub(in crate::views) async fn list(
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    use database::DbConnection;
+    use pretty_assertions::assert_eq;
+    use reqwest::StatusCode;
+
+    use crate::fixtures::create_simple_paced_train;
+    use crate::fixtures::create_timetable_with_train_schedule_set;
+    use crate::views::test_app;
+    use crate::views::test_app::TestRequestExt as _;
+
+    async fn create_dummy_schedules(
+        conn: &mut DbConnection,
+        train_schedule_set_id: i64,
+        count: usize,
+    ) -> Vec<models::TrainSchedule> {
+        let mut train_schedules = Vec::with_capacity(count);
+        for _ in 0..count {
+            train_schedules.push(create_simple_paced_train(conn, train_schedule_set_id).await);
+        }
+        train_schedules
+    }
+
+    async fn retrieve_round_trips(
+        conn: &mut DbConnection,
+        train_schedule_ids: impl IntoIterator<Item = i64> + Send,
+    ) -> HashSet<(i64, Option<i64>)> {
+        TrainScheduleRoundTrips::retrieve_from_train_schedule_ids(conn, train_schedule_ids)
+            .await
+            .expect("Failed to retrieve round trips")
+            .into_iter()
+            .map(|round_trip| (round_trip.left_id, round_trip.right_id))
+            .collect()
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn upsert_round_trips() {
+        let app = test_app!().build();
+        let db_pool = app.db_pool();
+        let user = app
+            .user("user", "User")
+            .with_roles([authz::Role::OperationalStudies])
+            .create()
+            .await;
+        let (_, train_schedule_set) =
+            create_timetable_with_train_schedule_set(&mut db_pool.get_ok()).await;
+        let train_schedules =
+            create_dummy_schedules(&mut db_pool.get_ok(), train_schedule_set.id, 4).await;
+        let train_ids = train_schedules
+            .iter()
+            .map(|train_schedule| train_schedule.id)
+            .collect::<Vec<_>>();
+
+        app.post("/round_trips/")
+            .by_user(user.as_ref())
+            .json(&RoundTrips {
+                one_ways: vec![train_ids[0]],
+                round_trips: vec![(train_ids[1], train_ids[2])],
+            })
+            .await
+            .assert_status(StatusCode::NO_CONTENT);
+
+        assert_eq!(
+            retrieve_round_trips(&mut db_pool.get_ok(), train_ids.clone()).await,
+            HashSet::from([(train_ids[0], None), (train_ids[1], Some(train_ids[2]))])
+        );
+
+        app.post("/round_trips/")
+            .by_user(user.as_ref())
+            .json(&RoundTrips {
+                one_ways: vec![train_ids[1]],
+                round_trips: vec![(train_ids[3], train_ids[0])],
+            })
+            .await
+            .assert_status(StatusCode::NO_CONTENT);
+
+        assert!(train_ids[3] > train_ids[0]);
+        assert_eq!(
+            retrieve_round_trips(&mut db_pool.get_ok(), train_ids.clone()).await,
+            HashSet::from([(train_ids[1], None), (train_ids[0], Some(train_ids[3]))]) // round trips are ordered by id
+        );
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn delete_round_trips() {
+        let app = test_app!().build();
+        let db_pool = app.db_pool();
+        let user = app
+            .user("user", "User")
+            .with_roles([authz::Role::OperationalStudies])
+            .create()
+            .await;
+        let (_, train_schedule_set) =
+            create_timetable_with_train_schedule_set(&mut db_pool.get_ok()).await;
+        let train_schedules =
+            create_dummy_schedules(&mut db_pool.get_ok(), train_schedule_set.id, 5).await;
+        let train_ids = train_schedules
+            .iter()
+            .map(|train_schedule| train_schedule.id)
+            .collect::<Vec<_>>();
+
+        TrainScheduleRoundTrips::create_batch::<_, Vec<_>>(
+            &mut db_pool.get_ok(),
+            [
+                TrainScheduleRoundTrips::changeset()
+                    .left_id(train_ids[0])
+                    .right_id(Some(train_ids[1])),
+                TrainScheduleRoundTrips::changeset().left_id(train_ids[2]),
+                TrainScheduleRoundTrips::changeset()
+                    .left_id(train_ids[3])
+                    .right_id(Some(train_ids[4])),
+            ],
+        )
+        .await
+        .expect("Failed to create round trips");
+
+        app.post("/round_trips/delete")
+            .by_user(user.as_ref())
+            .json(&[train_ids[1], train_ids[2]])
+            .await
+            .assert_status(StatusCode::NO_CONTENT);
+
+        assert_eq!(
+            retrieve_round_trips(&mut db_pool.get_ok(), train_ids.clone()).await,
+            HashSet::from([(train_ids[3], Some(train_ids[4]))])
+        );
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn list_round_trips_paginated() {
+        let app = test_app!().build();
+        let db_pool = app.db_pool();
+        let user = app
+            .user("user", "User")
+            .with_roles([authz::Role::OperationalStudies])
+            .create()
+            .await;
+        let (timetable, train_schedule_set) =
+            create_timetable_with_train_schedule_set(&mut db_pool.get_ok()).await;
+        let train_schedules =
+            create_dummy_schedules(&mut db_pool.get_ok(), train_schedule_set.id, 6).await;
+        let train_ids = train_schedules
+            .iter()
+            .map(|train_schedule| train_schedule.id)
+            .collect::<Vec<_>>();
+
+        TrainScheduleRoundTrips::create_batch::<_, Vec<_>>(
+            &mut db_pool.get_ok(),
+            [
+                TrainScheduleRoundTrips::changeset().left_id(train_ids[0]),
+                TrainScheduleRoundTrips::changeset()
+                    .left_id(train_ids[1])
+                    .right_id(Some(train_ids[2])),
+                TrainScheduleRoundTrips::changeset().left_id(train_ids[3]),
+                TrainScheduleRoundTrips::changeset()
+                    .left_id(train_ids[4])
+                    .right_id(Some(train_ids[5])),
+            ],
+        )
+        .await
+        .expect("Failed to create round trips");
+
+        let (_, other_train_schedule_set) =
+            create_timetable_with_train_schedule_set(&mut db_pool.get_ok()).await;
+        let other_train_schedule =
+            create_simple_paced_train(&mut db_pool.get_ok(), other_train_schedule_set.id).await;
+        TrainScheduleRoundTrips::create_batch::<_, Vec<_>>(
+            &mut db_pool.get_ok(),
+            [TrainScheduleRoundTrips::changeset().left_id(other_train_schedule.id)],
+        )
+        .await
+        .expect("Failed to create round trip for another timetable");
+
+        let first_page: RoundTripsPage = app
+            .get(&format!("/timetable/{}/round_trips", timetable.id))
+            .by_user(user.as_ref())
+            .add_query_param("page", 1)
+            .add_query_param("page_size", 2)
+            .await
+            .assert_status_ok()
+            .json();
+        assert_eq!(
+            first_page,
+            RoundTripsPage {
+                stats: PaginationStats {
+                    count: 4,
+                    page_size: 2,
+                    page_count: 2,
+                    current: 1,
+                    previous: None,
+                    next: Some(2),
+                },
+                results: RoundTrips {
+                    one_ways: vec![train_ids[0]],
+                    round_trips: vec![(train_ids[1], train_ids[2])],
+                },
+            }
+        );
+
+        let second_page: RoundTripsPage = app
+            .get(&format!("/timetable/{}/round_trips", timetable.id))
+            .by_user(user.as_ref())
+            .add_query_param("page", 2)
+            .add_query_param("page_size", 2)
+            .await
+            .assert_status_ok()
+            .json();
+        assert_eq!(
+            second_page,
+            RoundTripsPage {
+                stats: PaginationStats {
+                    count: 4,
+                    page_size: 2,
+                    page_count: 2,
+                    current: 2,
+                    previous: Some(1),
+                    next: None,
+                },
+                results: RoundTrips {
+                    one_ways: vec![train_ids[3]],
+                    round_trips: vec![(train_ids[4], train_ids[5])],
+                },
+            }
+        );
+    }
+
     #[test]
     fn test_round_trips_duplicates() {
-        use super::RoundTrips;
-
         let round_trips = RoundTrips {
             one_ways: (1..10_000).collect(),
             round_trips: (10_000..20_000).step_by(2).map(|i| (i, i + 1)).collect(),
