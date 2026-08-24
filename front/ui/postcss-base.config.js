22363a4a4a89027cdba808ea7d962147b0f55c3f
Stanislas Signoud (Signez) <signez@stanisoft.net>
2026-08-24 14:06:28 +0200

    front, ui: use tsdown workspace flag to build all ui packages
    To do so, we simply have to be explicit on inline paths; using relative, we make them relative to the main.css file (not the one where we use "inline").
    
    Signed-off-by: Stanislas Signoud (Signez) <signez@stanisoft.net>

--- front/ui/postcss-base.config.js
+++ front/ui/postcss-base.config.js
@@ -1,7 +1,9 @@
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-assets': {},
+    'postcss-assets': {
+      relative: 'true',
+    },
     'postcss-preset-env': {
       features: { 'nesting-rules': false },
     },
