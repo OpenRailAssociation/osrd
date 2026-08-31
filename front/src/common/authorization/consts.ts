d3375bb9cff30a1b76146db22584b3fff3405b9a
theocrsb <theo_crosbie@yahoo.fr>
2026-08-31 09:31:44 +0200

    front: disable restricted reader grant
    back is not ready to support it
    
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- front/src/common/authorization/consts.ts
+++ front/src/common/authorization/consts.ts
@@ -1,7 +1,8 @@
 // The order is important here, as it is used to determine the order of the grants
 export enum GRANTS_LABEL {
   NONE = 'none',
-  RESTRICTED_READER = 'restricted_read',
+  // TODO_GRANT: Able RESTRICTED_READER when backend supports it
+  // RESTRICTED_READER = 'restricted_read',
   READER = 'read',
   WRITER = 'edit',
   OWNER = 'full',
