2fb50bdefa359fecdbabfa529abc65425965499f
Stanislas Signoud (Signez) <signez@stanisoft.net>
2026-09-01 17:37:45 +0200

    front, ui: use standard case for values to follow stylelint standard
    Signed-off-by: Stanislas Signoud (Signez) <signez@stanisoft.net>

--- front/stylelint.config.ts
+++ front/stylelint.config.ts
@@ -32,6 +32,5 @@ export default {
     'selector-not-notation': null,
     'selector-pseudo-element-colon-notation': null,
     'shorthand-property-no-redundant-values': null,
-    'value-keyword-case': null,
   },
 } satisfies Config;
