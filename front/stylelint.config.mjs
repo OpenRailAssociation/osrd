4ee649643ea58f2ad57d6de038b9042be3e925be
Stanislas Signoud (Signez) <signez@stanisoft.net>
2026-09-01 16:21:10 +0200

    front, ui: fix font names to follow stylelint standard
    Signed-off-by: Stanislas Signoud (Signez) <signez@stanisoft.net>

--- front/stylelint.config.mjs
+++ front/stylelint.config.mjs
@@ -12,8 +12,6 @@ export default {
     'declaration-block-no-duplicate-properties': null,
     'declaration-block-no-redundant-longhand-properties': null,
     'declaration-block-no-shorthand-property-overrides': null,
-    'font-family-name-quotes': null,
-    'font-family-no-missing-generic-family-keyword': null,
     'keyframes-name-pattern': null,
     'length-zero-no-unit': null,
     'media-feature-range-notation': null,
