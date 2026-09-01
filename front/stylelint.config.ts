0d9e58a3573db6cec5a6784e9faba1cddbe1f7ec
Stanislas Signoud (Signez) <signez@stanisoft.net>
2026-09-01 16:23:16 +0200

    front, ui: remove useless vendor prefixes to follow stylelint standard
    Signed-off-by: Stanislas Signoud (Signez) <signez@stanisoft.net>

--- front/stylelint.config.ts
+++ front/stylelint.config.ts
@@ -22,7 +22,6 @@ export default {
     'no-irregular-whitespace': null,
     'number-max-precision': null,
     'property-no-deprecated': null,
-    'property-no-vendor-prefix': null,
     'scss/at-extend-no-missing-placeholder': null,
     'scss/at-mixin-argumentless-call-parentheses': null,
     'scss/at-mixin-pattern': null,
@@ -40,6 +39,5 @@ export default {
     'selector-pseudo-element-colon-notation': null,
     'shorthand-property-no-redundant-values': null,
     'value-keyword-case': null,
-    'value-no-vendor-prefix': null,
   },
 } satisfies Config;
