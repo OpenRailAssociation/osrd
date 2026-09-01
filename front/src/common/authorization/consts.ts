37359dafdeeaabe574c36786919fd1311c144b16
Younes Khoudli <younes.khoudli@epita.fr>
2026-09-01 11:24:26 +0200

    front: revert "disable restricted reader grant"
    This reverts commit d3375bb9cff30a1b76146db22584b3fff3405b9a.
    
    Signed-off-by: Younes Khoudli <younes.khoudli@epita.fr>

--- front/src/common/authorization/consts.ts
+++ front/src/common/authorization/consts.ts
@@ -1,8 +1,7 @@
 // The order is important here, as it is used to determine the order of the grants
 export enum GRANTS_LABEL {
   NONE = 'none',
-  // TODO_GRANT: Able RESTRICTED_READER when backend supports it
-  // RESTRICTED_READER = 'restricted_read',
+  RESTRICTED_READER = 'restricted_read',
   READER = 'read',
   WRITER = 'edit',
   OWNER = 'full',
