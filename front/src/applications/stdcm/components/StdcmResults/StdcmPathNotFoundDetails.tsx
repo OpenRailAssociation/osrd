1afcf5c0c196c6fda4e5470caedb85a708fdc785
achrafmohye <a.mohyeddine@gmail.com>
2026-09-02 11:12:11 +0200

    front: name the operational point of the blocking work schedules
    Signed-off-by: achrafmohye <a.mohyeddine@gmail.com>

--- front/src/applications/stdcm/components/StdcmResults/StdcmPathNotFoundDetails.tsx
+++ front/src/applications/stdcm/components/StdcmResults/StdcmPathNotFoundDetails.tsx
@@ -22,10 +22,11 @@ const StdcmPathNotFoundDetails = ({
 
   const lastReachedOperationalPoint = outputs.last_reached_operational_point;
   // The work schedules delaying the train the most, then the one nearest to the destination.
-  // The same work can be planned on several days, only display it once.
+  // The same work schedule can be reported in both lists, only display it once.
   const workSchedules = uniqBy(
     [...outputs.most_blocking_work_schedules, ...outputs.nearest_to_destination_work_schedules],
-    'obj_id'
+    ({ last_op, start_date_time, end_date_time }) =>
+      `${last_op.id}-${start_date_time}-${end_date_time}`
   );
 
   return (
@@ -47,11 +48,12 @@ const StdcmPathNotFoundDetails = ({
         <div className="blocking-work-schedules" data-testid="stdcm-blocking-work-schedules">
           <span className="conflicts-title">{t('mainConflicts')}</span>
           <ul>
-            {workSchedules.map(({ id, start_date_time, end_date_time }) => (
-              <li key={id}>
+            {workSchedules.map(({ last_op, start_date_time, end_date_time }) => (
+              <li key={`${last_op.id}-${start_date_time}-${end_date_time}`}>
                 <span>
                   &bull;&nbsp;
                   {t('blockingWorkSchedule', {
+                    name: last_op.name,
                     startTime: formatTime(start_date_time),
                     endTime: formatTime(end_date_time),
                   })}
