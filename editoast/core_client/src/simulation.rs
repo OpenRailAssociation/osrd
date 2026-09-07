4c9902daa22dbaeaee3ba5e579d489c5e29cdfcc
Arthur Bougeard <covillesheer@gmail.com>
2026-09-07 14:15:25 +0200

    editoast: fix `path_item_times` docstring
    Signed-off-by: Arthur Bougeard <covillesheer@gmail.com>

--- editoast/core_client/src/simulation.rs
+++ editoast/core_client/src/simulation.rs
@@ -151,14 +151,18 @@ pub struct SimulationPowerRestrictionItem {
 pub struct ReportTrain {
     /// List of positions of a train
     /// Both positions (in mm) and times (in ms) must have the same length
+    /// The length of positions and times is arbitrary and comes from a curve simplification
+    /// made in core
     pub positions: Vec<u64>,
+    /// List of times of a train
+    /// The first value is always `0` and the last one is always the total duration of the simulation.
     pub times: Vec<u64>,
     /// List of speeds associated to a position
     pub speeds: Vec<f64>,
     /// Total energy consumption
     pub energy_consumption: f64,
     /// Time in ms at which the train *arrives* at each path item given as input of the pathfinding
-    /// The first value is always `0` (beginning of the path) and the last one, the total time of the simulation (end of the path)
+    /// The first value is always `0` (beginning of the path) and the last one, the arrival time of the path's last step.
     ///
     /// In case multiple path items are at the same position, the stop duration
     /// of the earlier ones are added to the path item time of the next. For
--- osrd_schemas/osrd_schemas/models.py
+++ osrd_schemas/osrd_schemas/models.py
@@ -254,7 +254,7 @@ class CoreReportTrain(BaseModel):
     path_item_times: list[PathItemTime]
     """
     Time in ms at which the train *arrives* at each path item given as input of the pathfinding
-    The first value is always `0` (beginning of the path) and the last one, the total time of the simulation (end of the path)
+    The first value is always `0` (beginning of the path) and the last one, the arrival time of the path's last step.
 
     In case multiple path items are at the same position, the stop duration
     of the earlier ones are added to the path item time of the next. For
@@ -266,12 +266,18 @@ class CoreReportTrain(BaseModel):
     """
     List of positions of a train
     Both positions (in mm) and times (in ms) must have the same length
+    The length of positions and times is arbitrary and comes from a curve simplification
+    made in core
     """
     speeds: list[float]
     """
     List of speeds associated to a position
     """
     times: list[Time]
+    """
+    List of times of a train
+    The first value is always `0` and the last one is always the total duration of the simulation.
+    """
 
 
 class CoreRoutingZoneRequirement(BaseModel):
