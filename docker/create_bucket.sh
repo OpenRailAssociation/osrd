a0d8606699c88c47eef7f58e5f60ec859567453e
Gaëtan Duchaussois <gaetan-osrd@tcgi.fr>
2026-09-14 10:27:37 +0200

    ci: switch from mc to rc for s3 tests
    Signed-off-by: Gaëtan Duchaussois <gaetan-osrd@tcgi.fr>

--- docker/create_bucket.sh
+++ docker/create_bucket.sh
@@ -2,18 +2,6 @@
 
 expiration_days=60
 
-mc alias set my_s3 "$AWS_ENDPOINT_URL_S3" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
-mc mb -p "my_s3/$1"
-mc ilm import "my_s3/$1" <<EOF
-{
-  "Rules": [
-    {
-      "Expiration": {
-        "Days": $expiration_days
-      },
-      "ID": "Default_rule",
-      "Status": "Enabled"
-    }
-  ]
-}
-EOF
+rc alias set my_s3 "$AWS_ENDPOINT_URL_S3" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
+rc mb -p "my_s3/$1"
+rc ilm rule add "my_s3/$1" --expiry-days $expiration_days
