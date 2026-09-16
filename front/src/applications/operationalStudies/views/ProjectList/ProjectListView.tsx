f217165ab99b77abb10e3fb0af6b084ca58f994c
Quentix <quentin.hay-kergrohenn@epita.fr>
2026-09-16 11:14:16 +0200

    front: remove authorized project filter
    To avoid duplication of behavior and unburden heavy requests to OpenFga,
    remove the front filter for authorized projects.
    
    Signed-off-by: Quentix <quentin.hay-kergrohenn@epita.fr>

--- front/src/applications/operationalStudies/views/ProjectList/ProjectListView.tsx
+++ front/src/applications/operationalStudies/views/ProjectList/ProjectListView.tsx
@@ -13,14 +13,13 @@ import {
   type ProjectWithStudies,
   type SearchResultItemProject,
 } from 'common/api/osrdEditoastApi';
-import useAuthz from 'common/authorization/hooks/useAuthz';
 import OptionsSNCF from 'common/BootstrapSNCF/OptionsSNCF';
 import { Spinner } from 'common/Loaders';
 import NavBar from 'common/NavBar';
 import SelectionToolbar from 'common/SelectionToolbar';
 import AddOrEditProjectModal from 'modules/project/components/AddOrEditProjectModal';
 import cleanLocalStorageByProject from 'modules/project/helpers/cleanLocalStorageByProject';
-import { getFeatureFlag, getUserSafeWord } from 'reducers/user/userSelectors';
+import { getUserSafeWord } from 'reducers/user/userSelectors';
 import { useAppDispatch } from 'store';
 
 import ProjectCard from './ProjectCard';
@@ -37,7 +36,6 @@ const ProjectListView = () => {
   const { t } = useTranslation('operational-studies');
   const dispatch = useAppDispatch();
   const safeWord = useSelector(getUserSafeWord);
-  const projectGrantsActivated = useSelector(getFeatureFlag('projectGrants'));
   const [sortOption, setSortOption] = useState<SortOptions>('LastModifiedDesc');
   const [filter, setFilter] = useState('');
   const [filterChips, setFilterChips] = useState('');
@@ -73,7 +71,6 @@ const ProjectListView = () => {
     ordering: sortOption,
     pageSize: 1000,
   });
-  const { getUserPrivileges } = useAuthz();
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -89,18 +86,6 @@ const ProjectListView = () => {
   ];
 
   const getProjectList = async () => {
-    if (!allProjects || allProjects.results.length === 0) return setProjectsList([]);
-    const privileges = projectGrantsActivated
-      ? await getUserPrivileges({
-          project: allProjects.results.map((project) => project.id),
-        })
-      : {};
-    // The project resource only exposes NONE/OWNER grants, so 'has_access' tells if the user can see it
-    const accessibleProjectIds = new Set(
-      Object.entries(privileges.project ?? {})
-        .filter(([, userPrivileges]) => userPrivileges.has('has_access'))
-        .map(([id]) => Number(id))
-    );
     setIsLoading(true);
     if (filter || safeWord !== '') {
       const payload: PostSearchApiArg = {
@@ -121,9 +106,6 @@ const ProjectListView = () => {
       };
       try {
         let filteredData = (await postSearch(payload).unwrap()) as SearchResultItemProject[];
-        filteredData = projectGrantsActivated
-          ? filteredData.filter((project) => accessibleProjectIds.has(project.id))
-          : filteredData;
         if (sortOption === 'LastModifiedDesc') {
           filteredData = [...filteredData].sort((a, b) =>
             b.last_modification.localeCompare(a.last_modification)
@@ -136,11 +118,7 @@ const ProjectListView = () => {
         console.error('filter projetcs error : ', error);
       }
     } else {
-      setProjectsList(
-        projectGrantsActivated
-          ? allProjects.results.filter((project) => accessibleProjectIds.has(project.id))
-          : allProjects.results
-      );
+      setProjectsList(allProjects?.results || []);
     }
     setIsLoading(false);
   };
@@ -187,7 +165,7 @@ const ProjectListView = () => {
 
   useEffect(() => {
     getProjectList();
-  }, [sortOption, filter, safeWord, allProjects, projectGrantsActivated]);
+  }, [sortOption, filter, safeWord, allProjects]);
 
   return (
     <>
