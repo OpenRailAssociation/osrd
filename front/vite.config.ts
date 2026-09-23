719c44d0867106f741b2d85e9cd5e6cd1de3f916
Duc Nguyen <nguyen@emersion.fr>
2026-09-23 11:25:34 +0200

    front: enable React Compiler in annotation mode
    Add React Compiler dependency and enable annotation mode.
    Using the compiler is opt in by adding 'use memo' at the beginning of
    the component definition.
    
    Signed-off-by: Duc Nguyen <nguyen@emersion.fr>

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,7 +2,8 @@
 import { createRequire } from 'node:module';
 import * as path from 'node:path';
 
-import react from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 //import { deprecations } from 'sass';
 import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
@@ -74,6 +75,9 @@ export default defineConfig(({ mode }) => {
             rename: { stripBase: true },
           },
         ],
+      }),
+      babel({
+        presets: [reactCompilerPreset({ compilationMode: 'annotation' })],
       }),
     ],
     build: {
