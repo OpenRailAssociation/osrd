08dae11758bb23836a50091509746ccd312b674c
Alice Khoudli <alice.khoudli@polytechnique.org>
2026-09-24 14:52:11 +0200

    front: timesStops: show selected power restriction even if incompatible with rs
    Signed-off-by: Alice Khoudli <alice.khoudli@polytechnique.org>

--- front/src/modules/timesStops/TimesStopsTable.tsx
+++ front/src/modules/timesStops/TimesStopsTable.tsx
@@ -313,7 +313,7 @@ const TimesStopsTable = ({
 
   const returnPowerRestrictionCell = (info: CellContext<TimesStopsRowNew, string | null>) => {
     const {
-      availablePowerRestrictions: codes,
+      availablePowerRestrictions: rawCodes,
       powerRestrictionBlocks: blocks,
       onPowerRestrictionChange: onRestrictionChange,
     } = info.table.options.meta!;
@@ -327,6 +327,12 @@ const TimesStopsTable = ({
     const showPropagated =
       value === null && blockInfo?.isBlockStart && blockInfo.propagatedValue !== null;
     const displayedValue = showPropagated ? blockInfo.propagatedValue : value;
+
+    const valueNotInCodes = // Happens if the power restriction is incompatible with the rs, for example after a rs change by user
+      displayedValue &&
+      displayedValue !== NO_POWER_RESTRICTION &&
+      !rawCodes.includes(displayedValue);
+    const codes = valueNotInCodes ? [...rawCodes, displayedValue] : rawCodes;
 
     return (
       <div
